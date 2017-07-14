9df0203105e30ed6557ecbdddfdce9f72f66b7ec
Igor Kroitor <igor.kroitor@gmail.com>
2017-07-14 06:06:34 +0300

    test.js verbose mode off, proxy off

--- test.js
+++ test.js
@@ -21,9 +21,9 @@ try {
 
 ccxt.markets.forEach (id => {
     markets[id] = new (ccxt)[id] ({
-        verbose: true,
+        verbose: false,
         // proxy: 'https://crossorigin.me/',
-        proxy: 'https://cors-anywhere.herokuapp.com/',
+        // proxy: 'https://cors-anywhere.herokuapp.com/',
         // proxy: 'http://cors-proxy.htmldriven.com/?url=',
     })
 })
