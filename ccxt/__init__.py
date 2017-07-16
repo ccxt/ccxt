139a46a688c3bd1b673015a48e1416eb3d440d23
Igor Kroitor <igor.kroitor@gmail.com>
2017-07-16 18:33:46 +0300

    version updated

--- ccxt/__init__.py
+++ ccxt/__init__.py
@@ -70,7 +70,7 @@ markets = [
 
 __all__ = markets + [ 'markets', 'Market', 'DDoSProtectionError', 'MarketNotAvailabileError' ]
 
-__version__ = '1.0.108'
+__version__ = '1.0.109'
 
 # Python 2 & 3
 import base64
