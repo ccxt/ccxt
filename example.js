36e282bb295276711b6dfe3e05f14d18fc346b68
Igor Kroitor <igor.kroitor@gmail.com>
2017-07-22 06:46:42 +0300

    edited example.js for runkit 2

--- example.js
+++ example.js
@@ -5,20 +5,16 @@ let exchange = new ccxt.bitfinex ({
     secret: 'Rgxxw1BGnq6NLTLq1hn0oQ5FBDRLYFFVRzyEKYbz1io',
 })
 
-(async function () {
+await exchange.loadProducts ()
+console.log (exchange.symbols)
 
-	await exchange.loadProducts ()
-	console.log (exchange.symbols)
-	
-	let symbol = exchange.symbols[0]
-	
-	let ticker = await exchange.fetchTicker (symbol)
-	console.log (symbol, ticker)
-	
-	let orderbook = await exchange.fetchTicker (symbol)
-	console.log (symbol, orderbook)
-	
-	let balance = await exchange.fetchBalance ()
-	console.log (symbol, balance)
+let symbol = exchange.symbols[0]
 
-}) ()
+let ticker = await exchange.fetchTicker (symbol)
+console.log (symbol, ticker)
+
+let orderbook = await exchange.fetchTicker (symbol)
+console.log (symbol, orderbook)
+
+let balance = await exchange.fetchBalance ()
+console.log (symbol, balance)
