925891f40d9b38d4de8bce903613601b194bd7d0
Igor Kroitor <igor.kroitor@gmail.com>
2017-07-22 06:10:10 +0300

    added example.js

---
+++ example.js
@@ -0,0 +1,16 @@
+let ccxt = require ('ccxt')
+let exchange = new ccxt.bitfinex ({
+    apiKey: '5A1VikyEtDpmFkrzX4e52LsaVgCxXyMnY9JElqvNMxo', 
+    secret: 'Rgxxw1BGnq6NLTLq1hn0oQ5FBDRLYFFVRzyEKYbz1io',
+})
+(async () => {
+	await exchange.loadProducts ()
+	console.log (exchange.symbols)
+	let symbol = exchange.symbols[0]
+	let ticker = await exchange.fetchTicker (symbol)
+	console.log (symbol, ticker)
+	let orderbook = await exchange.fetchTicker (symbol)
+	console.log (symbol, orderbook)
+	let balance = await exchange.fetchBalance ()
+	console.log (symbol, balance)	
+}) ()
