160ff86d0876123fe05eb9323066ed44a9a8789c
Travis CI <travis@travis-ci.org>
2017-07-23 18:43:52 +0000

    1.1.62
    [ci skip]

--- ccxt.php
+++ ccxt.php
@@ -12,7 +12,7 @@ class EndpointNotAvailableError  extends NotAvailableError {}
 class OrderBookNotAvailableError extends NotAvailableError {}
 class TickerNotAvailableError    extends NotAvailableError {}
 
-$version = '1.1.61';
+$version = '1.1.62';
 
 class Market {
 
