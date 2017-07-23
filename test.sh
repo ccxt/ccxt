a25ed3492c231f41ef26adae6c64b275a2a97a72
xpl <rocket.mind@gmail.com>
2017-07-23 23:35:13 +0300

    fused build with test, added single ./test.sh script which runs tests for every language with same params

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e # to fail on first error
+
+echo
+echo ----------------- JavaScript ------------------
+node test $1 $2
+
+echo
+echo ----------------- PHP ------------------
+echo
+python test.py $1 $2
+
+echo
+echo ----------------- Python ------------------
+php -f test.php $1 $2
