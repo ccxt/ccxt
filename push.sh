8f90fad9ccc8b239e979024a5071c8317786ef46
xpl <rocket.mind@gmail.com>
2017-07-23 19:42:14 +0300

    pull requests now don't trigger deploy scripts, extracted CI pushback script to push.sh

--- push.sh
+++ push.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-cd ccxt.wiki
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
-git commit -a -m ${COMMIT_MESSAGE}
+git commit -a -m "${COMMIT_MESSAGE}" -m "[ci skip]"
 git remote remove origin
-git remote add origin https://${GITHUB_TOKEN}@github.com/kroitor/ccxt.wiki.git
+git remote add origin https://${GITHUB_TOKEN}@github.com/kroitor/ccxt.git
 git push origin HEAD:master
