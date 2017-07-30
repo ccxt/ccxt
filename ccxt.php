c1bd35d480fbdb122215c6a183011bc79a626651
Travis CI <travis@travis-ci.org>
2017-07-30 14:44:35 +0000

    1.1.140
    [ci skip]

--- ccxt.php
+++ ccxt.php
@@ -10,7 +10,7 @@ class MarketError                extends CCXTError {}
 class MarketNotAvailableError    extends MarketError {}
 class EndpointError              extends MarketError {}
 
-$version = '1.1.139';
+$version = '1.1.140';
 
 $curl_errors = array (
     0 => 'CURLE_OK',
