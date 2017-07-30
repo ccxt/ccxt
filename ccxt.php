bb2f28d1d1b4b8923078dec240f851646fb5a01e
Travis CI <travis@travis-ci.org>
2017-07-30 23:18:17 +0000

    1.1.145
    [ci skip]

--- ccxt.php
+++ ccxt.php
@@ -10,7 +10,7 @@ class MarketError                extends CCXTError {}
 class MarketNotAvailableError    extends MarketError {}
 class EndpointError              extends MarketError {}
 
-$version = '1.1.144';
+$version = '1.1.145';
 
 $curl_errors = array (
     0 => 'CURLE_OK',
@@ -5907,7 +5907,10 @@ class cex extends Market {
     public function request ($path, $type = 'public', $method = 'GET', $params = array (), $headers = null, $body = null) {
         $url = $this->urls['api'] . '/' . $this->implode_params ($path, $params);
         $query = $this->omit ($params, $this->extract_params ($path));
-        if ($type != 'public') {
+        if ($type == 'public') {
+            if ($query)
+                $url .= '?' . $this->urlencode ($query);
+        } else {
             if (!$this->uid)
                 throw new AuthenticationError ($this->id . ' requires `' . $this->id . '.uid` property for authentication');
             $nonce = (string) $this->nonce ();
