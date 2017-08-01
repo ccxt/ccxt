5c93ad290a9b92f38a6048042d7e8f29cd5ac375
Igor Kroitor <igor.kroitor@gmail.com>
2017-08-01 10:10:41 +0300

    gemini minor fix

--- ccxt.js
+++ ccxt.js
@@ -8603,8 +8603,8 @@ var gemini = {
                 'nonce': nonce,
             }, query);
             let payload = this.json (request);
-            payload = this.encode (payload);
             payload = this.stringToBase64 (payload);
+            payload = this.encode (payload);
             let signature = this.hmac (payload, this.encode (this.secret), 'sha384');
             headers = {
                 'Content-Type': 'text/plain',
