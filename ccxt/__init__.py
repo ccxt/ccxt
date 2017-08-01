fbb34b08732b54b50948bc9433f580884e68a8ce
Travis CI <travis@travis-ci.org>
2017-08-01 03:41:48 +0000

    1.2.5
    [ci skip]

--- ccxt/__init__.py
+++ ccxt/__init__.py
@@ -86,7 +86,7 @@ __all__ = markets + [
     'TickerNotAvailableError',
 ]
 
-__version__ = '1.2.4'
+__version__ = '1.2.5'
 
 # Python 2 & 3
 import base64
@@ -1648,12 +1648,7 @@ class bitbays (Market):
         if 'status' in response:
             if response['status'] == 200:
                 return response
-        error = None
-        if 'message' in response:
-            error = response['message']
-        else:
-            error = self.json (response)
-        raise MarketError (self.id + ' ' + error)
+        raise MarketError (self.id + ' ' + self.json (response))
 
 #------------------------------------------------------------------------------
 
@@ -3081,7 +3076,11 @@ class bitso (Market):
             signature = self.hmac (self.encode (request), self.encode (self.secret))
             auth = self.apiKey + ':' + nonce + ':' + signature
             headers = { 'Authorization': "Bitso " + auth }
-        return self.fetch (url, method, headers, body)
+        response = self.fetch (url, method, headers, body)
+        if 'success' in response:
+            if response['success']:
+                return response
+        raise MarketError (self.id + ' ' + self.json (response))
 
 #------------------------------------------------------------------------------
 
