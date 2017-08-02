bffaa72f3189d1d020216745303d247702a3f918
Travis CI <travis@travis-ci.org>
2017-08-02 14:12:48 +0000

    1.2.40
    [ci skip]

--- ccxt.js
+++ ccxt.js
@@ -4,7 +4,7 @@
 
 //-----------------------------------------------------------------------------
 
-var version = '1.2.39'
+var version = '1.2.40'
 var isNode  = (typeof window === 'undefined')
 var isReactNative = (typeof navigator !== 'undefined') && navigator &&
     navigator.product && navigator.product == 'ReactNative' || false
--- ccxt/__init__.py
+++ ccxt/__init__.py
@@ -86,7 +86,7 @@ __all__ = markets + [
     'TickerNotAvailableError',
 ]
 
-__version__ = '1.2.39'
+__version__ = '1.2.40'
 
 # Python 2 & 3
 import base64
@@ -9980,7 +9980,10 @@ class luno (Market):
             auth = self.encode (self.apiKey + ':' + self.secret)
             auth = base64.b64encode (auth)
             headers = { 'Authorization': 'Basic ' + self.decode (auth) }
-        return self.fetch (url, method, headers, body)
+        response = self.fetch (url, method, headers, body)
+        if 'error' in response:
+            raise MarketError (self.id + ' ' + self.json (response))
+        return response
 
 #------------------------------------------------------------------------------
 
