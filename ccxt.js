ea6bb3447778ac5748304597cc05c8f26b8279d4
Travis CI <travis@travis-ci.org>
2017-08-05 09:47:12 +0000

    1.2.74
    [ci skip]

--- ccxt.js
+++ ccxt.js
@@ -4,7 +4,7 @@
 
 //-----------------------------------------------------------------------------
 
-var version = '1.2.73'
+var version = '1.2.74'
 var isNode  = (typeof window === 'undefined')
 var isReactNative = (typeof navigator !== 'undefined') && navigator &&
     navigator.product && navigator.product == 'ReactNative' || false
