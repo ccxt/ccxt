f7b4d6286eff110a10715905135c976ddd57c86f
Travis CI <travis@travis-ci.org>
2017-08-13 15:56:18 +0000

    1.3.71
    [ci skip]

--- ccxt.js
+++ ccxt.js
@@ -4,7 +4,7 @@
 
 //-----------------------------------------------------------------------------
 
-var version = '1.3.70'
+var version = '1.3.71'
 var isNode  = (typeof window === 'undefined')
 var isReactNative = (typeof navigator !== 'undefined') && navigator &&
     navigator.product && navigator.product == 'ReactNative' || false
--- ccxt.php
+++ ccxt.php
@@ -10,7 +10,7 @@ class DDoSProtection       extends NetworkError {}
 class RequestTimeout       extends NetworkError {}
 class ExchangeNotAvailable extends NetworkError {}
 
-$version = '1.3.70';
+$version = '1.3.71';
 
 $curl_errors = array (
     0 => 'CURLE_OK',
