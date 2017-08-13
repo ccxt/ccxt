cc1628d744ed879110ba1d01079c6263cc03b32f
Igor Kroitor <igor.kroitor@gmail.com>
2017-08-13 18:55:52 +0300

    #106 removed whitespace from Python examples

--- examples/py/README.md
+++ examples/py/README.md
@@ -1,7 +1,9 @@
 # CCXT Python Examples
 
-To run Python examples from any folder type in console:
+To run Python examples from any folder, type in console:
 
 ```shell
 python path/to/example.py # substitute for actual filename here
 ```
+
+The examples are compatible with Python 2 and 3.
--- examples/py/symbols.py
+++ examples/py/symbols.py
@@ -1,59 +1,57 @@
-# coding=utf-8
-
-def style     (s, style): return style + s + '\033[0m'
-def green     (s): return style (s, '\033[92m')
-def blue      (s): return style (s, '\033[94m')
-def yellow    (s): return style (s, '\033[93m')
-def red       (s): return style (s, '\033[91m')
-def pink      (s): return style (s, '\033[95m')
-def bold      (s): return style (s, '\033[1m')
-def underline (s): return style (s, '\033[4m')
+# -*- coding: utf-8 -*-
 
 import os
 import sys
-root = os.path.dirname (os.path.dirname (os.path.dirname (os.path.abspath (__file__))))
-sys.path.append (root)
+root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root)
 
-import ccxt
+import ccxt  # noqa: E402
 
-def dump (*args):
-    print (' '.join ([str (arg) for arg in args]))
+def style(s, style): return style + s + '\033[0m'
+def green(s): return style(s, '\033[92m')
+def blue(s): return style(s, '\033[94m')
+def yellow(s): return style(s, '\033[93m')
+def red(s): return style(s, '\033[91m')
+def pink(s): return style(s, '\033[95m')
+def bold(s): return style(s, '\033[1m')
+def underline(s): return style(s, '\033[4m')
 
-def print_supported_exchanges ():
-    dump ('Supported exchanges:', green (', '.join (ccxt.exchanges)))
+def dump(*args):
+    print(' '.join([str(arg) for arg in args]))
+
+def print_supported_exchanges():
+    dump('Supported exchanges:', green(', '.join(ccxt.exchanges)))
 
 try:
 
-    id = sys.argv[1] # get exchange id from command line arguments
+    id = sys.argv[1]  # get exchange id from command line arguments
 
     # check if the exchange is supported by ccxt
     exchange_found = id in ccxt.exchanges
 
     if exchange_found:
-        
-        dump ('Instantiating', green (id), 'exchange exchange')
-        
+        dump('Instantiating', green(id), 'exchange exchange')
+
         # instantiate the exchange by id
-        exchange = getattr (ccxt, id) ()
-        
+        exchange = getattr(ccxt, id)()
+
         # load all markets from the exchange
-        markets = exchange.load_markets ()
-        
+        markets = exchange.load_markets()
+
         # output a list of all market symbols
-        dump (green (id), 'has', len (exchange.symbols), 'symbols:', yellow (', '.join (exchange.symbols)))
+        dump(green(id), 'has', len(exchange.symbols), 'symbols:', yellow(', '.join(exchange.symbols)))
 
         # output a table of all markets
-        dump (pink ('{:<15} {:<15} {:<15} {:<15}'.format ('id', 'symbol', 'base', 'quote')))
-        tuples = list (ccxt.Exchange.keysort (markets).items ())
+        dump(pink('{:<15} {:<15} {:<15} {:<15}'.format('id', 'symbol', 'base', 'quote')))
+        tuples = list(ccxt.Exchange.keysort(markets).items())
         for (k, v) in tuples:
-            dump ('{:<15} {:<15} {:<15} {:<15}'.format (v['id'], v['symbol'], v['base'], v['quote']))
+            dump('{:<15} {:<15} {:<15} {:<15}'.format(v['id'], v['symbol'], v['base'], v['quote']))
 
     else:
 
-        dump ('Exchange ' + red (id) + ' not found')
-        print_supported_exchanges ()
+        dump('Exchange ' + red(id) + ' not found')
+        print_supported_exchanges()
 
 except:
-    
-    dump ("Usage: python " + sys.argv[0], green ('id'))
-    print_supported_exchanges ()
+    dump("Usage: python " + sys.argv[0], green('id'))
+    print_supported_exchanges()
