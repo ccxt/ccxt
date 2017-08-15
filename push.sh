02f6039a5db0b6f8a44f412117c2aee39d022c7f
xpl <rocket.mind@gmail.com>
2017-08-15 22:53:07 +0300

    fixed push.sh (wrong git add order)

--- push.sh
+++ push.sh
@@ -2,10 +2,10 @@
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
-git commit -a -m "${COMMIT_MESSAGE}" -m "[ci skip]"
-git remote remove origin
-git remote add origin https://${GITHUB_TOKEN}@github.com/kroitor/ccxt.git
 git add --force build/ccxt.es5.js
 git add --force build/ccxt.php
 git add --force ccxt/__init__.py
+git commit -a -m "${COMMIT_MESSAGE}" -m "[ci skip]"
+git remote remove origin
+git remote add origin https://${GITHUB_TOKEN}@github.com/kroitor/ccxt.git
 git push origin HEAD:master
