bdfcd3e88332ec35003a30f85b98623559bb1842
xpl <rocket.mind@gmail.com>
2017-08-15 22:35:37 +0300

    added force pushing of built files to travis push script

--- push.sh
+++ push.sh
@@ -5,4 +5,7 @@ git config --global user.name "Travis CI"
 git commit -a -m "${COMMIT_MESSAGE}" -m "[ci skip]"
 git remote remove origin
 git remote add origin https://${GITHUB_TOKEN}@github.com/kroitor/ccxt.git
+git add --force build/ccxt.es5.js
+git add --force build/ccxt.php
+git add --force ccxt/__init__.py
 git push origin HEAD:master
