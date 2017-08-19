8d2746c108522e63ea320462d5e0e6adcb0e30b4
Igor Kroitor <igor.kroitor@gmail.com>
2017-08-19 05:43:10 +0300

    jubi fix for non-JSON responses

--- ccxt.py
+++ ccxt.py
@@ -337,6 +337,8 @@ class Exchange (object):
     def handle_response(self, url, method='GET', headers=None, body=None):
         try:
             return json.loads(body)
+        except ValueError as e:
+            raise ExchangeError (' '.join([self.id, method, url, body, str(e)]))
         except Exception as e:
             ddos_protection = re.search('(cloudflare|incapsula)', body, flags=re.IGNORECASE)
             exchange_not_available = re.search('(offline|busy|retry|wait|unavailable|maintain|maintenance|maintenancing)', body, flags=re.IGNORECASE)
