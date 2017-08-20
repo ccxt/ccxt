6815c3179913388e4b0a7fc0ccad9718fddac4f0
Igor Kroitor <igor.kroitor@gmail.com>
2017-08-20 19:39:53 +0300

    force-added gitignored py files

--- push.sh
+++ push.sh
@@ -6,6 +6,8 @@ git add --force build/ccxt.es5.js
 git add --force build/ccxt.browser.js
 git add --force build/ccxt.php
 git add --force ccxt/version.py
+git add --force ccxt/exchanges.py
+git add --force ccxt/async/exchanges.py
 git commit -a -m "${COMMIT_MESSAGE}" -m "[ci skip]"
 git remote remove origin
 git remote add origin https://${GITHUB_TOKEN}@github.com/kroitor/ccxt.git
