d957c0bc4df48637fe77fb2868daf01023837d05
Igor Kroitor <igor.kroitor@gmail.com>
2017-08-20 15:12:15 +0300

    added examples/py/async.py for testing

---
+++ examples/py/async.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import logging
+
+#------------------------------------------------------------------------------
+
+logging.basicConfig(
+    level=getattr(logging, loglevel),
+    format='%(asctime)s %(levelname)s: %(message)s')
+
+#------------------------------------------------------------------------------
+
+root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root)
+
+#------------------------------------------------------------------------------
+
+import ccxt  # noqa: E402
+
+#------------------------------------------------------------------------------
+
+async def main():
+    p = ccxt.poloniex()
+    result = await p.fetch_markets()
+    logging.info(result)
+
+#------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
