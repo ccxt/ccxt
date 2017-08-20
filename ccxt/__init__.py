4c218fedf73e7cf661420cbd7bd4e4edf16c9c7f
Igor Kroitor <igor.kroitor@gmail.com>
2017-08-21 02:03:42 +0300

    fixes #132 ''' → """

--- ccxt/__init__.py
+++ ccxt/__init__.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-'''
+"""
 MIT License
 
 Copyright (c) 2017 Igor Kroitor
@@ -22,7 +22,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-'''
+"""
 
 #------------------------------------------------------------------------------
 
