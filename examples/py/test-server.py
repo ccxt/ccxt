baa8bcddd3557d5b4634c31c732bbc7575de79bc
Igor Kroitor <igor.kroitor@gmail.com>
2017-08-20 12:41:43 +0300

    test-server.py PEP8

--- examples/py/test-server.py
+++ examples/py/test-server.py
@@ -3,11 +3,14 @@
 # Python HTTPServer server for response code testing (localhost:8080 by default)
 
 try:
+
     # Python 3
     from http.server import HTTPServer, SimpleHTTPRequestHandler, test as test_orig
     import sys
+
     def test(*args):
-        test_orig(*args, port = int(sys.argv[1]) if len(sys.argv) > 1 else 8080)
+        test_orig(*args, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8080)
+
 except ImportError: # Python 2
     from BaseHTTPServer import HTTPServer, test
     from SimpleHTTPServer import SimpleHTTPRequestHandler
