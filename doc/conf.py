76b08607f93612e57526b812f47a95c42de7319e
Thijs Triemstra <info@collab.nl>
2017-08-22 16:19:26 +0200

    ignore html_static_path

--- doc/conf.py
+++ doc/conf.py
@@ -101,7 +101,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
