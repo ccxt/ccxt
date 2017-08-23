974a4fed964d5ef5ff0790564145a354478adcbb
Igor Kroitor <igor.kroitor@gmail.com>
2017-08-23 10:40:11 +0300

    added initial support for Binance #142

--- ccxt.js
+++ ccxt.js
@@ -1339,6 +1339,223 @@ var anxpro = {
 
 //-----------------------------------------------------------------------------
 
+var binance = {
+
+    'id': 'binance',
+    'name': 'Binance',
+    'countries': 'CN', // China
+    'rateLimit': 1000, // once every 350 ms ≈ 180 requests per minute ≈ 3 requests per second
+    'version': 'v1',
+    'hasFetchTickers': true,
+    'urls': {
+        'logo': 'https://user-images.githubusercontent.com/1294454/29604020-d5483cdc-87ee-11e7-94c7-d1a8d9169293.jpg',
+        'api': 'https://api.exmo.com',
+        'www': 'https://www.binance.com',
+        'doc': 'https://www.binance.com/restapipub.html',
+    },
+    'api': {
+        'public': {
+            'get': [
+                'currency',
+                'order_book',
+                'pair_settings',
+                'ticker',
+                'trades',
+            ],
+        },
+        'private': {
+            'post': [
+                'user_info',
+                'order_create',
+                'order_cancel',
+                'user_open_orders',
+                'user_trades',
+                'user_cancelled_orders',
+                'order_trades',
+                'required_amount',
+                'deposit_address',
+                'withdraw_crypt',
+                'withdraw_get_txid',
+                'excode_create',
+                'excode_load',
+                'wallet_history',
+            ],
+        },
+    },
+
+    async fetchMarkets () {
+        let markets = await this.publicGetPairSettings ();
+        let keys = Object.keys (markets);
+        let result = [];
+        for (let p = 0; p < keys.length; p++) {
+            let id = keys[p];
+            let market = markets[id];
+            let symbol = id.replace ('_', '/');
+            let [ base, quote ] = symbol.split ('/');
+            result.push ({
+                'id': id,
+                'symbol': symbol,
+                'base': base,
+                'quote': quote,
+                'info': market,
+            });
+        }
+        return result;
+    },
+
+    async fetchBalance () {
+        await this.loadMarkets ();
+        let response = await this.privatePostUserInfo ();
+        let result = { 'info': response };
+        for (let c = 0; c < this.currencies.length; c++) {
+            let currency = this.currencies[c];
+            let account = {
+                'free': undefined,
+                'used': undefined,
+                'total': undefined,
+            };
+            if (currency in response['balances'])
+                account['free'] = parseFloat (response['balances'][currency]);
+            if (currency in response['reserved'])
+                account['used'] = parseFloat (response['reserved'][currency]);
+            account['total'] = this.sum (account['free'], account['used']);
+            result[currency] = account;
+        }
+        return result;
+    },
+
+    async fetchOrderBook (market, params = {}) {
+        await this.loadMarkets ();
+        let p = this.market (market);
+        let response = await this.publicGetOrderBook (this.extend ({
+            'pair': p['id'],
+        }, params));
+        let orderbook = response[p['id']];
+        let timestamp = this.milliseconds ();
+        let result = {
+            'bids': [],
+            'asks': [],
+            'timestamp': timestamp,
+            'datetime': this.iso8601 (timestamp),
+        };
+        let sides = { 'bids': 'bid', 'asks': 'ask' };
+        let keys = Object.keys (sides);
+        for (let k = 0; k < keys.length; k++) {
+            let key = keys[k];
+            let side = sides[key];
+            let orders = orderbook[side];
+            for (let i = 0; i < orders.length; i++) {
+                let order = orders[i];
+                let price = parseFloat (order[0]);
+                let amount = parseFloat (order[1]);
+                result[key].push ([ price, amount ]);
+            }
+        }
+        return result;
+    },
+
+    parseTicker (ticker, market) {
+        let timestamp = ticker['updated'] * 1000;
+        return {
+            'timestamp': timestamp,
+            'datetime': this.iso8601 (timestamp),
+            'high': parseFloat (ticker['high']),
+            'low': parseFloat (ticker['low']),
+            'bid': parseFloat (ticker['buy_price']),
+            'ask': parseFloat (ticker['sell_price']),
+            'vwap': undefined,
+            'open': undefined,
+            'close': undefined,
+            'first': undefined,
+            'last': parseFloat (ticker['last_trade']),
+            'change': undefined,
+            'percentage': undefined,
+            'average': parseFloat (ticker['avg']),
+            'baseVolume': parseFloat (ticker['vol']),
+            'quoteVolume': parseFloat (ticker['vol_curr']),
+            'info': ticker,
+        };
+    },
+
+    async fetchTickers (currency = 'USD') {
+        await this.loadMarkets ();
+        let response = await this.publicGetTicker ();
+        let result = {};
+        let ids = Object.keys (response);
+        for (let i = 0; i < ids.length; i++) {
+            let id = ids[i];
+            let market = this.markets_by_id[id];
+            let symbol = market['symbol'];
+            let ticker = response[id];
+            result[symbol] = this.parseTicker (ticker, market);
+        }
+        return result;
+    },
+
+    async fetchTicker (market) {
+        await this.loadMarkets ();
+        let response = await this.publicGetTicker ();
+        let p = this.market (market);
+        return this.parseTicker (response[p['id']], p);
+    },
+
+    async fetchTrades (market, params = {}) {
+        await this.loadMarkets ();
+        return this.publicGetTrades (this.extend ({
+            'pair': this.marketId (market),
+        }, params));
+    },
+
+    async createOrder (market, type, side, amount, price = undefined, params = {}) {
+        await this.loadMarkets ();
+        let prefix = '';
+        if (type == 'market')
+            prefix = 'market_';
+        let order = {
+            'pair': this.marketId (market),
+            'quantity': amount,
+            'price': price || 0,
+            'type': prefix + side,
+        };
+        let response = await this.privatePostOrderCreate (this.extend (order, params));
+        return {
+            'info': response,
+            'id': response['order_id'].toString (),
+        };
+    },
+
+    async cancelOrder (id) {
+        await this.loadMarkets ();
+        return this.privatePostOrderCancel ({ 'order_id': id });
+    },
+
+    async request (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
+        let url = this.urls['api'] + '/' + this.version + '/' + path;
+        if (api == 'public') {
+            if (Object.keys (params).length)
+                url += '?' + this.urlencode (params);
+        } else {
+            let nonce = this.nonce ();
+            body = this.urlencode (this.extend ({ 'nonce': nonce }, params));
+            headers = {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': body.length,
+                'Key': this.apiKey,
+                'Sign': this.hmac (this.encode (body), this.encode (this.secret), 'sha512'),
+            };
+        }
+        let response = await this.fetch (url, method, headers, body);
+        if ('result' in response) {
+            if (response['result'])
+                return response;
+            throw new ExchangeError (this.id + ' ' + this.json (response));
+        }
+        return response;
+    },
+}
+
+//-----------------------------------------------------------------------------
+
 var bit2c = {
 
     'id': 'bit2c',
@@ -8540,7 +8757,6 @@ var exmo = {
         'logo': 'https://user-images.githubusercontent.com/1294454/27766491-1b0ea956-5eda-11e7-9225-40d67b481b8d.jpg',
         'api': 'https://api.exmo.com',
         'www': 'https://exmo.me',
-        'markets': 'https://exmo.me/en/trade#?pair=BTC_USD',
         'doc': [
             'https://exmo.me/ru/api_doc',
             'https://github.com/exmo-dev/exmo_api_lib/tree/master/nodejs',
@@ -15122,6 +15338,7 @@ var exchanges = {
     '_1broker':      _1broker,
     '_1btcxe':       _1btcxe,
     'anxpro':        anxpro,
+    'binance',       binance,
     'bit2c':         bit2c,
     'bitbay':        bitbay,
     'bitbays':       bitbays,
