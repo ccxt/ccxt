917a7dcd9d4e25f3e4a994040a1ecb9ba26f7a90
Travis CI <travis@travis-ci.org>
2017-08-23 11:50:56 +0000

    1.4.92
    [ci skip]

--- build/ccxt.php
+++ build/ccxt.php
@@ -44,7 +44,7 @@ class DDoSProtection       extends NetworkError  {}
 class RequestTimeout       extends NetworkError  {}
 class ExchangeNotAvailable extends NetworkError  {}
 
-$version = '1.4.91';
+$version = '1.4.92';
 
 $curl_errors = array (
     0 => 'CURLE_OK',
