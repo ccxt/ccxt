917649a285edadccf33e07eb66185072952fb9e1
Igor Kroitor <igor.kroitor@gmail.com>
2017-08-28 09:13:42 +0300

    added .md to sphinx exclude patterns for the docs

--- doc/conf.py
+++ doc/conf.py
@@ -73,7 +73,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.md']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
