b5b08a07d72f76340607c95dfbd32f41b5a12711
Igor Kroitor <igor.kroitor@gmail.com>
2017-09-01 19:26:45 +0300

    added examples/py/basic-chart.py

---
+++ examples/py/basic-chart.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+#------------------------------------------------------------------------------
+
+this_folder = os.path.dirname(os.path.abspath(__file__))
+root_folder = os.path.dirname(os.path.dirname(this_folder))
+sys.path.append(root_folder)
+sys.path.append(this_folder)
+
+#------------------------------------------------------------------------------
+
+import asciichart
+import ccxt # noqa: E402
+
+#------------------------------------------------------------------------------
+
+kraken = ccxt.kraken()
+ohlcv = kraken.fetch_ohlcv ('BTC/USD', 60, kraken.seconds()-86400)
+index = 4 # [ timestamp, open, high, low, close, volume ]
+last = ohlcv[len(ohlcv) - 1][index] # closing price
+series = [x[index] for x in ohlcv]
+
+rate = '₿ = $' + str(last)
+print(asciichart.plot(series[0:120], {'height': 30}))
+print("\n" + rate + "\n")
