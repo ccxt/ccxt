ebf00bcf38acf4c0c87735a5cade5d0503d01cc2
Igor Kroitor <igor.kroitor@gmail.com>
2017-09-02 02:18:21 +0300

    minor edits

--- examples/py/basic-chart.py
+++ examples/py/basic-chart.py
@@ -19,14 +19,39 @@ import ccxt # noqa: E402
 
 kraken = ccxt.kraken()
 
-# get a list of ohlcv candles
-ohlcv = kraken.fetch_ohlcv('BTC/USD', 300)
+#------------------------------------------------------------------------------
+
+symbol = 'BTC/USD'
+
+#------------------------------------------------------------------------------
 
 # each ohlcv candle is a list of [ timestamp, open, high, low, close, volume ]
 index = 4 # use close price from each ohlcv candle
-series = [x[index] for x in ohlcv]
 
-print("\n" + asciichart.plot(series[-120:], {'height': 20})) # print the chart
+#------------------------------------------------------------------------------
 
-last = ohlcv[len(ohlcv) - 1][index] # last closing price
-print("\n" + kraken.name + " ₿ = $" + str(last) + "\n") # print it
+def print_chart(exchange, symbol, timeframe, human_timeframe):
+
+    print("\n" + exchange.name + ' ' + human_timeframe + ' chart:')
+
+    # get a list of ohlcv candles
+    ohlcv = exchange.fetch_ohlcv(symbol, timeframe)
+
+    # get the ohlCv (closing price, index == 4)
+    series = [x[index] for x in ohlcv]
+
+    # print the chart
+    print("\n" + asciichart.plot(series[-120:], {'height': 20})) # print the chart
+
+    last = ohlcv[len(ohlcv) - 1][index] # last closing price
+    return last
+
+#------------------------------------------------------------------------------
+
+last = print_chart(kraken, 'BTC/USD', 60, '1m')
+last = print_chart(kraken, 'BTC/USD', 300, '5m')
+last = print_chart(kraken, 'BTC/USD', 3600, '1h')
+
+#------------------------------------------------------------------------------
+
+print("\n" + kraken.name + " ₿ = $" + str(last) + "\n") # print last closing price
