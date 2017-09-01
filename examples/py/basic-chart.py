86f21b6fb62949e26abca826305c2f24fd1fbc08
Igor Kroitor <igor.kroitor@gmail.com>
2017-09-02 02:09:07 +0300

    minor edit to examples/py/basic-chart.py

--- examples/py/basic-chart.py
+++ examples/py/basic-chart.py
@@ -20,13 +20,13 @@ import ccxt # noqa: E402
 kraken = ccxt.kraken()
 
 # get a list of ohlcv candles
-ohlcv = kraken.fetch_ohlcv('BTC/USD', 60)
+ohlcv = kraken.fetch_ohlcv('BTC/USD', 300)
 
 # each ohlcv candle is a list of [ timestamp, open, high, low, close, volume ]
 index = 4 # use close price from each ohlcv candle
 series = [x[index] for x in ohlcv]
 
-print(asciichart.plot(series[-120:], {'height': 30})) # print the chart
+print("\n" + asciichart.plot(series[-120:], {'height': 20})) # print the chart
 
 last = ohlcv[len(ohlcv) - 1][index] # last closing price
 print("\n" + kraken.name + " ₿ = $" + str(last) + "\n") # print it
