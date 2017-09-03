70f427f9eb64ed42b70029dac83f7e06881879ea
Igor Kroitor <igor.kroitor@gmail.com>
2017-09-04 02:08:44 +0300

    testing push.sh

--- push-wiki.sh
+++ push-wiki.sh
@@ -3,8 +3,9 @@
 cd ccxt.wiki
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
-git tag "${COMMIT_MESSAGE}"
 git commit -a -m ${COMMIT_MESSAGE}
+git tag "${COMMIT_MESSAGE}"
 git remote remove origin
 git remote add origin https://${GITHUB_TOKEN}@github.com/kroitor/ccxt.wiki.git
 git push origin --tags HEAD:master
+# git push origin HEAD:master
--- push.sh
+++ push.sh
@@ -8,8 +8,9 @@ git add --force build/ccxt.php
 git add --force ccxt/version.py
 git add --force ccxt/exchanges.py
 git add --force ccxt/async/exchanges.py
-git tag -a "${COMMIT_MESSAGE}" -m '[ci skip]'
 git commit -a -m "${COMMIT_MESSAGE} [ci skip]"
+git tag "${COMMIT_MESSAGE}" -m '[ci skip]'
 git remote remove origin
 git remote add origin https://${GITHUB_TOKEN}@github.com/kroitor/ccxt.git
 git push origin --tags HEAD:master
+# git push origin HEAD:master
