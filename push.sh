6b3536af5fce7b1c785beebb474bcf304efefd74
Igor Kroitor <igor.kroitor@gmail.com>
2017-09-04 01:48:55 +0300

    minor edits to push.sh

--- push.sh
+++ push.sh
@@ -8,7 +8,7 @@ git add --force build/ccxt.php
 git add --force ccxt/version.py
 git add --force ccxt/exchanges.py
 git add --force ccxt/async/exchanges.py
-git tag '${COMMIT_MESSAGE}'
+git tag -a '${COMMIT_MESSAGE}' -m '[ci skip]'
 git commit -a -m '${COMMIT_MESSAGE} [ci skip]'
 git remote remove origin
 git remote add origin https://${GITHUB_TOKEN}@github.com/kroitor/ccxt.git
