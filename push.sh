e25d35d995628abc7fc8941ac8e30e6405346402
Igor Kroitor <igor.kroitor@gmail.com>
2017-09-04 01:57:29 +0300

    ci 3

--- push.sh
+++ push.sh
@@ -8,8 +8,8 @@ git add --force build/ccxt.php
 git add --force ccxt/version.py
 git add --force ccxt/exchanges.py
 git add --force ccxt/async/exchanges.py
-git tag -a '${COMMIT_MESSAGE}' -m '[ci skip]'
-git commit -a -m '${COMMIT_MESSAGE} [ci skip]'
+git tag -a "${COMMIT_MESSAGE}" -m '[ci skip]'
+git commit -a -m "${COMMIT_MESSAGE} [ci skip]"
 git remote remove origin
 git remote add origin https://${GITHUB_TOKEN}@github.com/kroitor/ccxt.git
 git push origin --tags HEAD:master
