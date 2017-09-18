45330be73717c9b211d41dc5da83b1786726670b
Igor Kroitor <igor.kroitor@gmail.com>
2017-09-18 21:12:49 +0300

    test_async PEP8

--- test_async.py
+++ test_async.py
@@ -283,4 +283,4 @@ async def main():
 
 # ------------------------------------------------------------------------------
 
-asyncio.get_event_loop().run_until_complete (main())
+asyncio.get_event_loop().run_until_complete(main())
