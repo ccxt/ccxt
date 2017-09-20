083b64ef96da7910865aa2e3e3519b21b2f24f7a
Igor Kroitor <igor.kroitor@gmail.com>
2017-09-20 18:31:51 +0300

    added wex exchange #220 closed

--- ccxt.js
+++ ccxt.js
@@ -17797,6 +17797,54 @@ var virwox = {
 
 //-----------------------------------------------------------------------------
 
+var wex = extend (liqui, {
+
+    'id': 'wex',
+    'name': 'WEX',
+    'countries': 'NZ', // New Zealand
+    'version': '3',
+    'hasFetchTickers': true,
+    'urls': {
+        'logo': 'https://user-images.githubusercontent.com/1294454/30652751-d74ec8f8-9e31-11e7-98c5-71469fcef03e.jpg',
+        'api': {
+            'public': 'https://wex.nz/api',
+            'private': 'https://wex.nz/tapi',
+        },
+        'www': 'https://wex.nz',
+        'doc': [
+            'https://wex.nz/api/3/docs',
+            'https://wex.nz/tapi/docs',
+        ],
+    },
+    'api': {
+        'public': {
+            'get': [
+                'info',
+                'ticker/{pair}',
+                'depth/{pair}',
+                'trades/{pair}',
+            ],
+        },
+        'private': {
+            'post': [
+                'getInfo',
+                'Trade',
+                'ActiveOrders',
+                'OrderInfo',
+                'CancelOrder',
+                'TradeHistory',
+                'TransHistory',
+                'CoinDepositAddress',
+                'WithdrawCoin',
+                'CreateCoupon',
+                'RedeemCoupon',
+            ],
+        }
+    },
+})
+
+//-----------------------------------------------------------------------------
+
 var xbtce = {
 
     'id': 'xbtce',
@@ -18791,6 +18839,7 @@ var exchanges = {
     'vaultoro':           vaultoro,
     'vbtc':               vbtc,
     'virwox':             virwox,
+    'wex',                wex,
     'xbtce':              xbtce,
     'yobit':              yobit,
     'yunbi':              yunbi,
