e1f9c32de3c624bd0c9ebac941d9c64eff8dbba6
xpl <rocket.mind@gmail.com>
2017-09-24 16:26:00 +0300

    updated devDeps, set maxConcurrency to 10 (for debugging purposes)

--- run-tests.js
+++ run-tests.js
@@ -32,7 +32,7 @@ const keys = {
 
 let exchanges = []
 let symbol = 'all'
-let maxConcurrency = 50
+let maxConcurrency = 10
 
 for (const arg of args) {
     if (arg.startsWith ('--'))               { keys[arg] = true }
