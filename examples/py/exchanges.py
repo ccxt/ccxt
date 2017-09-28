9c664bcdfeca3833f3b808468d254120b07f1309
Igor Kroitor <igor.kroitor@gmail.com>
2017-09-28 08:59:23 +0300

    pep8 e704

--- examples/py/exchanges.py
+++ examples/py/exchanges.py
@@ -7,14 +7,29 @@ sys.path.append(root)
 
 import ccxt  # noqa: E402
 
-def style(s, style): return style + s + '\033[0m'
-def green(s): return style(s, '\033[92m')
-def blue(s): return style(s, '\033[94m')
-def yellow(s): return style(s, '\033[93m')
-def red(s): return style(s, '\033[91m')
-def pink(s): return style(s, '\033[95m')
-def bold(s): return style(s, '\033[1m')
-def underline(s): return style(s, '\033[4m')
+def style(s, style):
+    return style + s + '\033[0m'
+
+def green(s):
+    return style(s, '\033[92m')
+
+def blue(s):
+    return style(s, '\033[94m')
+
+def yellow(s):
+    return style(s, '\033[93m')
+
+def red(s):
+    return style(s, '\033[91m')
+
+def pink(s):
+    return style(s, '\033[95m')
+
+def bold(s):
+    return style(s, '\033[1m')
+
+def underline(s):
+    return style(s, '\033[4m')
 
 def log(*args):
     print(' '.join([str(arg) for arg in args]))
--- test/test_async.py
+++ test/test_async.py
@@ -38,14 +38,29 @@ exchanges = {}
 # ------------------------------------------------------------------------------
 # string coloring functions
 
-def style(s, style): return str(s)  # style + str (s) + '\033[0m'
-def green(s): return style(s, '\033[92m')
-def blue(s): return style(s, '\033[94m')
-def yellow(s): return style(s, '\033[93m')
-def red(s): return style(s, '\033[91m')
-def pink(s): return style(s, '\033[95m')
-def bold(s): return style(s, '\033[1m')
-def underline(s): return style(s, '\033[4m')
+def style(s, style):
+    return str(s) # style + str (s) + '\033[0m'
+
+def green(s):
+    return style(s, '\033[92m')
+
+def blue(s):
+    return style(s, '\033[94m')
+
+def yellow(s):
+    return style(s, '\033[93m')
+
+def red(s):
+    return style(s, '\033[91m')
+
+def pink(s):
+    return style(s, '\033[95m')
+
+def bold(s):
+    return style(s, '\033[1m')
+
+def underline(s):
+    return style(s, '\033[4m')
 
 # print a colored string
 def dump(*args):
