437bb7d7389804f3e2a82b0c9d0ea97d2fdb0ea8
Igor Kroitor <igor.kroitor@gmail.com>
2017-09-28 15:36:27 +0300

    pypi table fix

--- update-badges.js
+++ update-badges.js
@@ -15,7 +15,7 @@ let rst = fs.readFileSync (readmeRst, 'utf8')
 let rstNew =
     rst.replace (/\`([^\`]+)\s\<\#[^\`]+\>\`\_\_/g, '$1') // PyPI doesn't like urls containing anchor hash symbol '#', strip it off to plain text
         .replace (/\\\|/g, '|') // PyPI doesn't like escaped vertical bars
-        .replace (/\\\_/g, '_') // PyPI doesn't like escaped underscores
+        .replace (/\\\_/g, ' _') // PyPI doesn't like escaped underscores
         .replace (/\|(\_[^\|]+)\|([\ ]+)\|/g, '|$1| $2|')
         // .replace (/\|\\(\_[^\|]+)\|/g, '|$1|')
 
