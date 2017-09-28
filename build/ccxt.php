8aede82b6d2b098775ce25d0d2f7ad10d0621566
Travis CI <travis@travis-ci.org>
2017-09-28 10:31:35 +0000

    1.8.59
    [ci skip]

--- build/ccxt.php
+++ build/ccxt.php
@@ -44,7 +44,7 @@ class DDoSProtection       extends NetworkError  {}
 class RequestTimeout       extends NetworkError  {}
 class ExchangeNotAvailable extends NetworkError  {}
 
-$version = '1.8.57';
+$version = '1.8.59';
 
 $curl_errors = array (
     0 => 'CURLE_OK',
