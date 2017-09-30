109d7a38bb016ac120ffc360d410ca077cffe705
Igor Kroitor <igor.kroitor@gmail.com>
2017-09-30 15:45:58 +0300

    paymium + fetch2

--- ccxt.js
+++ ccxt.js
@@ -16763,7 +16763,7 @@ var paymium = {
         }, params));
     },
 
-    async request (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
+    sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
         let url = this.urls['api'] + '/' + this.version + '/' + this.implodeParams (path, params);
         let query = this.omit (params, this.extractParams (path));
         if (api == 'public') {
@@ -16780,7 +16780,11 @@ var paymium = {
                 'Content-Type': 'application/json',
             };
         }
-        let response = await this.fetch (url, method, headers, body);
+        return { 'url': url, 'method': method, 'body': body, 'headers': headers };
+    },
+
+    async request (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
+        let response = await this.fetch2 (path, api, method, params, headers, body);
         if ('errors' in response)
             throw new ExchangeError (this.id + ' ' + this.json (response));
         return response;
