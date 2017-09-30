19546d4b3077f3b58f5957a5cdd1188cfaf7ec2b
Igor Kroitor <igor.kroitor@gmail.com>
2017-09-30 17:39:38 +0300

    anxpro + fetch2

--- ccxt.js
+++ ccxt.js
@@ -1951,7 +1951,7 @@ var anxpro = {
         return this.milliseconds ();
     },
 
-    async request (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
+    sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
         let request = this.implodeParams (path, params);
         let query = this.omit (params, this.extractParams (path));
         let url = this.urls['api'] + '/' + this.version + '/' + request;
@@ -1970,7 +1970,11 @@ var anxpro = {
                 'Rest-Sign': this.decode (signature),
             };
         }
-        let response = await this.fetch (url, method, headers, body);
+        return { 'url': url, 'method': method, 'body': body, 'headers': headers };
+    },
+
+    async request (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
+        let response = await this.fetch2 (path, api, method, params, headers, body);
         if ('result' in response)
             if (response['result'] == 'success')
                 return response;
