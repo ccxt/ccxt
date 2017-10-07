f7290af75e47339866571e55f288d7f84e8329ab
Travis CI <travis@travis-ci.org>
2017-10-07 16:04:46 +0000

    1.9.63
    [ci skip]

--- ccxt.js
+++ ccxt.js
@@ -38,7 +38,7 @@ const CryptoJS = require ('crypto-js')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.9.62'
+const version = '1.9.63'
 
 //-----------------------------------------------------------------------------
 // platform detection
--- ccxt.php
+++ ccxt.php
@@ -44,7 +44,7 @@ class DDoSProtection       extends NetworkError  {}
 class RequestTimeout       extends NetworkError  {}
 class ExchangeNotAvailable extends NetworkError  {}
 
-$version = '1.9.62';
+$version = '1.9.63';
 
 $curl_errors = array (
     0 => 'CURLE_OK',
