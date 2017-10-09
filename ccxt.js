7411cdde2e147d22af9b1ca9902e9f925b653e59
Igor Kroitor <igor.kroitor@gmail.com>
2017-10-09 17:50:40 +0300

    bittrex ticker baseVolume ←→ quoteVolume fixes #292

--- ccxt.js
+++ ccxt.js
@@ -6105,8 +6105,8 @@ var bittrex = {
             'change': undefined,
             'percentage': undefined,
             'average': undefined,
-            'baseVolume': parseFloat (ticker['BaseVolume']),
-            'quoteVolume': parseFloat (ticker['Volume']),
+            'baseVolume': parseFloat (ticker['Volume']),
+            'quoteVolume': parseFloat (ticker['BaseVolume']),
             'info': ticker,
         };
     },
