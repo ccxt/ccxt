5c99556cd29801c2fefaf1de70a8f91108bc290a
Igor Kroitor <igor.kroitor@gmail.com>
2017-10-14 23:22:54 +0300

    liqui addon fix

--- ccxt.js
+++ ccxt.js
@@ -16939,24 +16939,6 @@ var liqui = {
         let response = undefined;
         try {
             response = await this.privatePostCancelOrder (this.extend ({
-                'txid': id,
-            }, params));
-        } catch (e) {
-            if (this.last_json_response) {
-                let message = this.safeString (this.last_json_response, 'error');
-                if (message.indexOf ('EOrder:Unknown order') >= 0)
-                    throw new InvalidOrder (this.id + ' cancelOrder() error: ' + this.last_http_response);
-            }
-            throw e;
-        }
-        return response;
-    },
-
-    async cancelOrder (id, symbol = undefined, params = {}) {
-        await this.loadMarkets ();
-        let response = undefined;
-        try {
-            response = await this.privatePostCancelOrder (this.extend ({
                 'order_id': parseInt (id),
             }, params));
             if (id in this.orders)
