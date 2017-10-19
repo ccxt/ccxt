e3e1126d1ae853707202e38d27556269552b817e
Igor Kroitor <igor.kroitor@gmail.com>
2017-10-19 13:41:35 +0300

    more yobit symbols inconsistencies fixes #350

--- ccxt.js
+++ ccxt.js
@@ -20942,22 +20942,25 @@ var yobit = extend (btce, {
     },
 
     commonCurrencyCode (currency) {
-        if (currency == 'PAY')
-            return 'EPAY';
-        if (currency == 'OMG')
-            return 'OMGame';
-        if (currency == 'REP')
-            return 'Republicoin';
-        if (currency == 'NAV')
-            return 'NavajoCoin';
-        if (currency == 'LIZI')
-            return 'LiZi';
-        if (currency == 'BCC')
-            return 'BCH';
-        if (currency == 'ANI')
-            return 'ANICoin';
-        if (currency == 'BTS')
-            return 'Bitshares2';
+        let substitutions = {
+            'AIR': 'AirCoin',
+            'ANI': 'ANICoin',
+            'ANT': 'AntsCoin',
+            'ATM': 'Autumncoin',
+            'BCC': 'BCH',
+            'BTS': 'Bitshares2',
+            'DCT': 'Discount',
+            'DGD': 'DarkGoldCoin',
+            'ICN': 'iCoin',
+            'LIZI': 'LiZi',
+            'LUN': 'LunarCoin',
+            'NAV': 'NavajoCoin',
+            'OMG': 'OMGame',
+            'PAY': 'EPAY',
+            'REP': 'Republicoin',
+        };
+        if (currency in substitutions)
+            return substitutions[currency];
         return currency;
     },
 
