06de8537ffc60f7b7d9199f68af23df93c893764
Sejeng <sejengwi@mail.com>
2017-10-23 14:20:19 +1000

    Log out nonce info

--- ccxt.js
+++ ccxt.js
@@ -5196,6 +5196,7 @@ var bitmex = {
         let url = this.urls['api'] + query;
         if (api == 'private') {
             let nonce = this.nonce ().toString ();
+            console.log('signing request, nonce = ' + nonce);
             if (method == 'POST')
                 if (Object.keys (params).length)
                     body = this.json (params);
