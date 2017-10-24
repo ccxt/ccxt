801ec6fca172176e3e1f9110aebd86ad82f267e4
Travis CI <travis@travis-ci.org>
2017-10-24 19:03:03 +0000

    1.9.248
    [ci skip]

--- ccxt.js
+++ ccxt.js
@@ -38,7 +38,7 @@ const CryptoJS = require ('crypto-js')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.9.247'
+const version = '1.9.248'
 
 //-----------------------------------------------------------------------------
 // platform detection
