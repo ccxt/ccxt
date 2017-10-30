7acf1713059afe175b08210a8094d1abf67fbcbc
Igor Kroitor <igor.kroitor@gmail.com>
2017-10-30 18:41:01 +0300

    wip

--- python/ccxt/__init__.py
+++ python/ccxt/__init__.py
@@ -51,7 +51,7 @@ from ccxt.exchanges import *  # noqa: F403
 
 # -----------------------------------------------------------------------------
 
-__all__ = exchange.__all__ + exchanges + errors.__all__  # noqa: F405
+__all__ = exchange.__all__ + exchanges.__all__ + errors.__all__  # noqa: F405
 __version__ = version.__version__
 
 # =============================================================================
