4c47a2fe14d7d91d2db347e2f7417e3d52e6a9ac
Igor Kroitor <igor.kroitor@gmail.com>
2017-11-01 01:01:01 +0300

    fixing release tags #430

--- push.sh
+++ push.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-LAST_GIT_COMMIT_MESSAGE="$(git log --no-merges -2 --pretty=%B)"
+LAST_COMMIT_MESSAGE="$(git log --no-merges -1 --pretty=%B)"
+COMPILED_MESSAGE="${LAST_COMMIT_MESSAGE}"$'\n'"[ci skip]"
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git add --force build/ccxt.browser.js
 git commit -a -m "${COMMIT_MESSAGE}" -m '[ci skip]'
-git tag -a "${COMMIT_MESSAGE}" -m "${LAST_COMMIT_MESSAGE}"$'\n'"[ci skip]"
+git tag -a "${COMMIT_MESSAGE}" -m "${COMPILED_MESSAGE}"
 git remote remove origin
 git remote add origin https://${GITHUB_TOKEN}@github.com/ccxt/ccxt.git
 git push origin --tags HEAD:master
