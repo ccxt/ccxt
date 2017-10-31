d829ed62d3f3ea3bec3be6393b3ea83227ef3b15
xpl <rocket.mind@gmail.com>
2017-10-31 23:42:33 +0300

    edited push.sh (removed unneeded --force adds)

--- push.sh
+++ push.sh
@@ -3,10 +3,6 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git add --force build/ccxt.browser.js
-git add --force build/ccxt.php
-git add --force ccxt/version.py
-git add --force ccxt/exchanges.py
-git add --force ccxt/async/exchanges.py
 git commit -a -m "${COMMIT_MESSAGE}" -m '[ci skip]'
 git tag -a "${COMMIT_MESSAGE}" -m "${COMMIT_MESSAGE}" -m '[ci skip]'
 git remote remove origin
