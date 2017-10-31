240d57823635ea10f78e7f83888e79b48806a983
Igor Kroitor <igor.kroitor@gmail.com>
2017-10-31 22:56:36 +0300

    trying travis tags #428

--- push.sh
+++ push.sh
@@ -8,7 +8,7 @@ git add --force ccxt/version.py
 git add --force ccxt/exchanges.py
 git add --force ccxt/async/exchanges.py
 git commit -a -m "${COMMIT_MESSAGE}" -m '[ci skip]'
-git tag -a "${COMMIT_MESSAGE}" -m '[ci skip]'
+git tag -a "${COMMIT_MESSAGE}" -m "${COMMIT_MESSAGE}" -m '[ci skip]'
 git remote remove origin
 git remote add origin https://${GITHUB_TOKEN}@github.com/ccxt/ccxt.git
 git push origin --tags HEAD:master
