4d1b1be9ce76b5a5a8600ede27c7781f81187a27
Vitaly Gordon <rocket.mind@gmail.com>
2017-11-01 12:41:17 +0300

    should improve tag descriptions readbility
    [ci-skip]

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git add --force build/ccxt.browser.js
 git commit -a -m "${COMMIT_MESSAGE}" -m '[ci skip]'
-git tag -a "${COMMIT_MESSAGE}" -m "${LAST_COMMIT_MESSAGE}" -m "[ci skip]"
+git tag -a "${COMMIT_MESSAGE}" -m "${LAST_COMMIT_MESSAGE}" -m "" -m "[ci skip]"
 git remote remove origin
 git remote add origin https://${GITHUB_TOKEN}@github.com/ccxt/ccxt.git
 git push origin --tags HEAD:master
