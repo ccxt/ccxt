d12dccdb5109eb923109a0e5bdc28671622fd9fa
Igor Kroitor <igor.kroitor@gmail.com>
2017-11-01 07:01:50 +0300

    updated examples/js/builtin-rate-limiting-rest-poller.js

--- examples/js/builtin-rate-limiting-rest-poller.js
+++ examples/js/builtin-rate-limiting-rest-poller.js
@@ -7,13 +7,13 @@ const asTable = require ('as-table')
 
 async function test () {
 
-    const exchange = new ccxt.gdax ({ enableRateLimit: true, verbose: true })
+    const exchange = new ccxt.gdax ({ enableRateLimit: true })
     const symbol   = 'BTC/USD'
     const repeat   = 100;
 
     for (let i = 0; i < repeat; i++) {
         let ticker = await exchange.fetchTicker (symbol)
-        log (exchange.id.green, exchange.lastRestRequestTimestamp, ticker['datetime'], symbol.green, ticker['last'])
+        log (exchange.id.green, exchange.iso8601 (exchange.milliseconds ()), ticker['datetime'], symbol.green, ticker['last'])
     }
 }
 
