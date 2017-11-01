f3ee64aa9b2acd30ee4badd3570a0eab885689c4
Igor Kroitor <igor.kroitor@gmail.com>
2017-11-01 03:03:30 +0300

    cleanup in base/exchange.py

--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -390,23 +390,6 @@ class Exchange(object):
             return result
         return {}
 
-    # @staticmethod
-    # def deep_extend(dct, merge_dct):
-    #     """ Recursive dict merge. Inspired by :meth:``dict.update()``, instead of
-    #     updating only top-level keys, dict_merge recurses down into dicts nested
-    #     to an arbitrary depth, updating keys. The ``merge_dct`` is merged into
-    #     ``dct``.
-    #     :param dct: dict onto which the merge is executed
-    #     :param merge_dct: dct merged into dct
-    #     :return: None
-    #     """
-    #     for k, v in merge_dct.iteritems():
-    #         if (k in dct and isinstance(dct[k], dict)
-    #                 and isinstance(merge_dct[k], collections.Mapping)):
-    #             dict_merge(dct[k], merge_dct[k])
-    #         else:
-    #             dct[k] = merge_dct[k]
-
     @staticmethod
     def deep_extend(*args):
         result = None
