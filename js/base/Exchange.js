4bb89a891fa011ca27c27abf30e32a3e57c31d1b
Igor Kroitor <igor.kroitor@gmail.com>
2017-11-01 09:50:26 +0300

    fixing aggregate in fetchL2OrderBook

--- js/base/Exchange.js
+++ js/base/Exchange.js
@@ -11,7 +11,8 @@ const { deepExtend
       , extend
       , sleep
       , timeout
-      , sortBy } = functions
+      , sortBy
+      , aggregate } = functions
 
 const { ExchangeError
       , NotSupported
