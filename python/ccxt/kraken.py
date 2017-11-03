c1cc3e6120361657e367e367e4aeb79f2cf18ea1
Travis CI <travis@travis-ci.org>
2017-11-03 18:31:35 +0000

    1.9.357
    [ci skip]

--- python/ccxt/kraken.py
+++ python/ccxt/kraken.py
@@ -17,7 +17,7 @@ class kraken (Exchange):
             'name': 'Kraken',
             'countries': 'US',
             'version': '0',
-            'rateLimit': 3000,
+            'rateLimit': 2000,
             'hasCORS': False,
             'hasFetchTickers': True,
             'hasFetchOHLCV': True,
