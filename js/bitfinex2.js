5b7108b6ced23065560574f00ca035a92ce0723a
Igor Kroitor <igor.kroitor@gmail.com>
2017-11-06 08:35:34 +0300

    bitfinex2 withdraw fee USD → USDT #486

--- js/bitfinex2.js
+++ js/bitfinex2.js
@@ -185,7 +185,7 @@ module.exports = class bitfinex2 extends bitfinex {
                         'DAT': 1.0,
                         'AVT': 0.5,
                         'SAN': 0.1,
-                        // 'USD': 5.0,
+                        'USDT': 5.0,
                     },
                 },
             },
