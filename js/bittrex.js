de01220600e50db77ea02a7743d760ead73b6442
Igor Kroitor <igor.kroitor@gmail.com>
2017-11-12 18:05:56 +0300

    restored bittrex CancelInitiated → canceled order status

--- js/bittrex.js
+++ js/bittrex.js
@@ -417,7 +417,7 @@ module.exports = class bittrex extends Exchange {
         if (order['Closed']) {
             status = 'closed';
         } else if (order['CancelInitiated']) {
-            status = 'open';
+            status = 'canceled';
         }
         let symbol = undefined;
         if (!market) {
