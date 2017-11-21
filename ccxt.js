cfcda38ea9f75e9e2916ea4fd279441f83a123ab
Travis CI <travis@travis-ci.org>
2017-11-21 01:30:24 +0000

    1.10.178
    [ci skip]

--- ccxt.js
+++ ccxt.js
@@ -37,7 +37,7 @@ const errors    = require ('./js/base/errors')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.10.177'
+const version = '1.10.178'
 
 Exchange.ccxtVersion = version
 
--- php/coinmarketcap.php
+++ php/coinmarketcap.php
@@ -178,6 +178,11 @@ class coinmarketcap extends Exchange {
 
     public function request ($path, $api = 'public', $method = 'GET', $params = array (), $headers = null, $body = null) {
         $response = $this->fetch2 ($path, $api, $method, $params, $headers, $body);
+        if (array_key_exists ('error', $response)) {
+            if ($response['error']) {
+                throw new ExchangeError ($this->id . ' ' . $this->json ($response));
+            }
+        }
         return $response;
     }
 }
