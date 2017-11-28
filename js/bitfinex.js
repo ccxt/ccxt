655cc199620516b0ff003bc0e563358f8652e1ec
Igor Kroitor <igor.kroitor@gmail.com>
2017-11-28 18:12:31 +0300

    edits to bitfinex fetchOpenOrders/fetchClosedOrders filter by symbol #700

--- js/bitfinex.js
+++ js/bitfinex.js
@@ -410,18 +410,10 @@ module.exports = class bitfinex extends Exchange {
     async fetchOpenOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
         await this.loadMarkets ();
         let response = await this.privatePostOrders (params);
-        let allOrders = this.parseOrders (response);
-        if (symbol == undefined) {
-            return allOrders;
-        }
-        let marketOrders = [];
-        for (let i = 0; i < allOrders.length; i++) {
-            let order = allOrders[i];
-            if (order.symbol == symbol) {
-                marketOrders.push(order);
-            }
-        }
-        return marketOrders;
+        let orders = this.parseOrders (response);
+        if (symbol)
+            return this.filterBy (orders, 'symbol', symbol);
+        return orders;
     }
 
     async fetchClosedOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
@@ -430,7 +422,10 @@ module.exports = class bitfinex extends Exchange {
         if (limit)
             request['limit'] = limit;
         let response = await this.privatePostOrdersHist (this.extend (request, params));
-        return this.parseOrders (response);
+        let orders = this.parseOrders (response);
+        if (symbol)
+            return this.filterBy (orders, 'symbol', symbol);
+        return orders;
     }
 
     async fetchOrder (id, symbol = undefined, params = {}) {
