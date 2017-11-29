c39ffc919032a26289b638eef13ba43176db66f8
Igor Kroitor <igor.kroitor@gmail.com>
2017-11-29 23:14:26 +0300

    hitbtc2 withdraw #406

--- js/hitbtc2.js
+++ js/hitbtc2.js
@@ -534,7 +534,7 @@ module.exports = class hitbtc2 extends hitbtc {
         amount = parseFloat (amount);
         let response = await this.privatePostAccountCryptoWithdraw (this.extend ({
             'currency': currencyId,
-            'amount': amount.toString (),
+            'amount': amount,
             'address': address,
         }, params));
         return {
