3ebe0af49112107fcf31217c3f414cc90fd769f2
Max Kutny <mkutny@gmail.com>
2017-11-30 04:03:38 +0200

    one more transpiler fix... maybe

--- js/bitfinex.js
+++ js/bitfinex.js
@@ -578,8 +578,9 @@ module.exports = class bitfinex extends Exchange {
     }
 
     handleErrors (code, reason, url, method, headers, body) {
-        if (this.verbose)
+        if (this.verbose) {
             console.log (this.id, method, url, code, reason, body ? ("\nResponse:\n" + body) : '');
+        }
         if (code == 400) {
             if (body[0] == "{") {
                 let response = JSON.parse (body);
