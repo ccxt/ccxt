c12698b3ac39ca0ac11fce32a1d936bed728605a
Max Kutny <mkutny@gmail.com>
2017-12-03 13:42:20 +0200

    qryptos: maker/taker fees: string -> number

--- js/qryptos.js
+++ js/qryptos.js
@@ -79,8 +79,8 @@ module.exports = class qryptos extends Exchange {
             let base = market['base_currency'];
             let quote = market['quoted_currency'];
             let symbol = base + '/' + quote;
-            let maker = market['maker_fee'];
-            let taker = market['taker_fee'];
+            let maker = parseFloat(market['maker_fee']);
+            let taker = parseFloat(market['taker_fee']);
             let active = !market['disabled'];
             result.push ({
                 'id': id,
