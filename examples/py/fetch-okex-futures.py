96f100e69523ac7b94bd31a15a13ecdaf3754431
nigelboucher <nigelboucher@qq.com>
2017-12-06 14:15:19 +0800

    work

--- examples/py/fetch-okex-futures.py
+++ examples/py/fetch-okex-futures.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import time
+import json
 
 # ------------------------------------------------------------------------------
 
@@ -11,15 +12,33 @@ sys.path.append(root + '/python')
 
 # ------------------------------------------------------------------------------
 
-import ccxt  # noqa: E402
+from python import ccxt  # noqa: E402
+
+# import ccxt.async as ccxt
 
 # ------------------------------------------------------------------------------
 
-exchange = ccxt.okex()
+fileName = 'key.json'
+path = os.path.abspath(os.path.dirname(__file__))
+fileName = os.path.join(path, fileName)
+# 解析json文件
+with open(fileName) as data_file:
+    setting = json.load(data_file)
+    data_file.close()
+apikey = str(setting['apiKey'])
+secretkey = str(setting['secretKey'])
+okcoinRESTURL = 'www.okex.com'
+exchange = ccxt.okex({
+    'apiKey': apikey,
+    'secret': secretkey,
+})
 exchange.load_markets()
+
 for symbol in exchange.markets:
     market = exchange.markets[symbol]
-    if market['future']:
+    if market['future'] and symbol == 'ETC/USD':
         print('----------------------------------------------------')
         print(symbol, exchange.fetchTicker(symbol))
+        balance = exchange.fetch_balance()
+        # exchange.create_order()
         time.sleep(exchange.rateLimit / 1000)
