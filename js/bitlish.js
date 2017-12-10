03261e65f7f4e715681acf835f694984b3c651c3
illera88 <agarciaillera@gmail.com>
2017-12-10 17:16:38 +0900

    added tier and type of commision for bitlish
    We should this sintaxis for the rest of the exchnges

--- js/bitlish.js
+++ js/bitlish.js
@@ -32,10 +32,14 @@ module.exports = class bitlish extends Exchange {
             },
             'fees': {
                 'trading': { // for verified account. Anonymous 0.3 on taker
+                    'tierBased': false, //false for tier based
+                    'percentage': true,
                     'taker': 0.2 / 100,
                     'maker': 0 / 100,
                 },
                 'funding': {
+                    'tierBased': false,  //true for tier based/progressive
+                    'percentage': false, //fixed commission
                     'withdraw': {
                         'BTC': 0.001,                        
                         'LTC': 0.001,
