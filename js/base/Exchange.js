f49ae156842c38732233880d6d3ee4735cf89c0a
Igor Kroitor <igor.kroitor@gmail.com>
2017-12-10 11:43:47 +0300

    typo in exchange.js

--- js/base/Exchange.js
+++ js/base/Exchange.js
@@ -726,7 +726,7 @@ module.exports = class Exchange {
 
     parseOrders (orders, market = undefined, since = undefined, limit = undefined) {
         let result = Object.values (orders).map (order => this.parseOrder (order, market))
-        result = this.filterBySinceLimit (result, since, limit)
+        return this.filterBySinceLimit (result, since, limit)
     }
 
     filterOrdersBySymbol (orders, symbol = undefined) {
