6c098e11448746b51ffe4e552791634068757154
illera88 <agarciaillera@gmail.com>
2017-12-10 20:09:33 +0900

    small fix

--- js/bitmarket.js
+++ js/bitmarket.js
@@ -107,8 +107,8 @@ module.exports = class bitmarket extends Exchange {
                 'trading': { // for verified account. Anonymous 0.3 on taker
                     'tierBased': true, 
                     'percentage': true,
-                    'taker' 0.45 / 100,
-                    'maker' 0.15 / 100,
+                    'taker': 0.45 / 100,
+                    'maker': 0.15 / 100,
                     'tiers': {
                         'taker': [
                             [0, 0.45 / 100],
