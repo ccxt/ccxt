c79a55cb56723a3410a2343c71a9b25f71cc7be6
Igor Kroitor <igor.kroitor@gmail.com>
2017-12-11 19:43:44 +0300

    git-added getbtc

---
+++ php/getbtc.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ccxt;
+
+include_once ('_1btcxe.php');
+
+class getbtc extends _1btcxe {
+
+    public function describe () {
+        return array_replace_recursive (parent::describe (), array (
+            'id' => 'getbtc',
+            'name' => 'GetBTC',
+            'countries' => array ( 'VC', 'RU' ), // Saint Vincent and the Grenadines, Russia, CIS
+            'rateLimit' => 1000,
+            'urls' => array (
+                'logo' => 'https://user-images.githubusercontent.com/1294454/33801902-03c43462-dd7b-11e7-992e-077e4cd015b9.jpg',
+                'api' => 'https://getbtc.org/api',
+                'www' => 'https://getbtc.org',
+                'doc' => 'https://getbtc.org/api-docs.php',
+            ),
+            'markets' => array (
+                'BTC/EUR' => array ( 'id' => 'EUR', 'symbol' => 'BTC/EUR', 'base' => 'BTC', 'quote' => 'EUR', 'precision' => array ( 'amount' => 8, 'price' => 8 ), 'lot' => 0.00000001, 'limits' => array ( 'amount' => array ( 'min' => 0.00000001, 'max' => null ), 'price' => array ( 'min' => 0.00000001, 'max' => null ))),
+                'BTC/RUB' => array ( 'id' => 'RUB', 'symbol' => 'BTC/RUB', 'base' => 'BTC', 'quote' => 'RUB', 'precision' => array ( 'amount' => 8, 'price' => 8 ), 'lot' => 0.00000001, 'limits' => array ( 'amount' => array ( 'min' => 0.00000001, 'max' => null ), 'price' => array ( 'min' => 0.00000001, 'max' => null ))),
+                'BTC/USD' => array ( 'id' => 'USD', 'symbol' => 'BTC/USD', 'base' => 'BTC', 'quote' => 'USD', 'precision' => array ( 'amount' => 8, 'price' => 8 ), 'lot' => 0.00000001, 'limits' => array ( 'amount' => array ( 'min' => 0.00000001, 'max' => null ), 'price' => array ( 'min' => 0.00000001, 'max' => null ))),
+            ),
+            'fees' => array (
+                'trading' => array (
+                    'taker' => 0.20 / 100,
+                    'maker' => 0.20 / 100,
+                ),
+            ),
+        ));
+    }
+}
+
+?>
