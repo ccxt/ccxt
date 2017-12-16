4ba30eaea01f0ac1b6c9b2e40a6b47074eae7a09
Travis CI <travis@travis-ci.org>
2017-12-16 23:18:59 +0000

    1.10.394
    [ci skip]

--- ccxt.js
+++ ccxt.js
@@ -37,7 +37,7 @@ const errors    = require ('./js/base/errors')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.10.393'
+const version = '1.10.394'
 
 Exchange.ccxtVersion = version
 
--- php/quadrigacx.php
+++ php/quadrigacx.php
@@ -212,6 +212,8 @@ class quadrigacx extends Exchange {
 
     public function request ($path, $api = 'public', $method = 'GET', $params = array (), $headers = null, $body = null) {
         $response = $this->fetch2 ($path, $api, $method, $params, $headers, $body);
+        if (gettype ($response) == 'string')
+            return $response;
         if (is_array ($response) && array_key_exists ('error', $response))
             throw new ExchangeError ($this->id . ' ' . $this->json ($response));
         return $response;
