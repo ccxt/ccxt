af7ee338028149538b0bdd711177bc9e6af82a6b
Igor Kroitor <igor.kroitor@gmail.com>
2017-12-17 01:13:06 +0300

    quadrigacx string response handling

--- js/quadrigacx.js
+++ js/quadrigacx.js
@@ -215,6 +215,8 @@ module.exports = class quadrigacx extends Exchange {
 
     async request (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
         let response = await this.fetch2 (path, api, method, params, headers, body);
+        if (typeof response == 'string')
+            return response;
         if ('error' in response)
             throw new ExchangeError (this.id + ' ' + this.json (response));
         return response;
