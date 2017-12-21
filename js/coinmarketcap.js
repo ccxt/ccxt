efb8c4f65c62606945320cc3c9ff63e2dd041abf
Igor Kroitor <igor.kroitor@gmail.com>
2017-12-21 05:21:58 +0300

    more robust parseTicker for coinmarketcap

--- js/coinmarketcap.js
+++ js/coinmarketcap.js
@@ -113,22 +113,16 @@ module.exports = class coinmarketcap extends Exchange {
         if ('last_updated' in ticker)
             if (ticker['last_updated'])
                 timestamp = parseInt (ticker['last_updated']) * 1000;
-        let change = undefined;
-        let changeKey = 'percent_change_24h';
-        if (changeKey in ticker)
-            change = parseFloat (ticker[changeKey]);
+        let change = this.safeFloat (ticker, 'percent_change_24h');
         let last = undefined;
         let symbol = undefined;
         let volume = undefined;
         if (market) {
-            let price = 'price_' + market['quoteId'];
-            if (price in ticker)
-                if (ticker[price])
-                    last = parseFloat (ticker[price]);
+            let priceKey = 'price_' + market['quoteId'];
+            last = this.safeFloat (ticker, priceKey);
             symbol = market['symbol'];
             let volumeKey = '24h_volume_' + market['quoteId'];
-            if (volumeKey in ticker)
-                volume = parseFloat (ticker[volumeKey]);
+            volume = this.safeFloat (ticker, volumeKey);
         }
         return {
             'symbol': symbol,
