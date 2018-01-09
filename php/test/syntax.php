9283510f4366ddd826796d7a122383353a1f507b
Igor Kroitor <igor.kroitor@gmail.com>
2018-01-09 06:54:53 +0300

    modified test/syntax.php

--- php/test/syntax.php
+++ php/test/syntax.php
@@ -7,6 +7,13 @@ echo "Read these rules very carefully and follow them LITERALLY:\n\n";
 echo "- https://github.com/ccxt/ccxt/blob/master/CONTRIBUTING.md#how-to-contribute-code\n";
 echo "- https://github.com/ccxt/ccxt/blob/master/CONTRIBUTING.md#derived-exchange-classes\n";
 
+function on_error ($errno, $message, $file, $line) {
+    $message = "$message in $file on line $line";
+    throw new ErrorException ($message, $errno);
+}
+
+set_error_handler ('on_error');
+
 include_once 'ccxt.php';
 
 foreach (\ccxt\Exchange::$exchanges as $id) {
