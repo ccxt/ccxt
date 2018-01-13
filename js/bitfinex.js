ecaca68a66bfc87398510033cc61f946b67a0e35
Igor Kroitor <igor.kroitor@gmail.com>
2018-01-13 23:28:01 +0300

    bitfinex minor edit for decomposing assignment #1241

--- js/bitfinex.js
+++ js/bitfinex.js
@@ -468,7 +468,7 @@ module.exports = class bitfinex extends Exchange {
         let orderType = order['type'];
         let exchange = orderType.indexOf ('exchange ') >= 0;
         if (exchange) {
-            [ prefix, orderType ] = order['type'].split (' ');
+            let [ prefix, orderType ] = order['type'].split (' ');
         }
         let timestamp = parseInt (parseFloat (order['timestamp']) * 1000);
         let result = {
