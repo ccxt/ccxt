3273966a75bfca42fe0cc4a9f37cf87f24c43355
Igor Kroitor <igor.kroitor@gmail.com>
2018-01-18 03:42:58 +0300

    cryptopia withdraw tag and linting #1313

--- js/cryptopia.js
+++ js/cryptopia.js
@@ -576,7 +576,7 @@ module.exports = class cryptopia extends Exchange {
         };
     }
 
-    async withdraw (currency, amount, address, params = {}) {
+    async withdraw (currency, amount, address, tag = undefined, params = {}) {
         let currencyId = this.currencyId (currency);
         let response = await this.privatePostSubmitWithdraw (this.extend ({
             'Currency': currencyId,
