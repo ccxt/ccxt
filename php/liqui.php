350c12092d850f899bc7c1254b2a846a8a0fc212
Travis CI <travis@travis-ci.org>
2018-01-20 21:55:05 +0000

    1.10.785
    [ci skip]

--- php/liqui.php
+++ php/liqui.php
@@ -622,7 +622,9 @@ class liqui extends Exchange {
     }
 
     public function handle_errors ($httpCode, $reason, $url, $method, $headers, $body) {
-        if ((gettype ($body) != 'string') && $body && (strlen ($body) < 2))
+        if (gettype ($body) != 'string')
+            return; // fallback to default error handler
+        if (strlen ($body) < 2)
             return; // fallback to default error handler
         if (($body[0] === '{') || ($body[0] === '[')) {
             $response = json_decode ($body, $as_associative_array = true);
