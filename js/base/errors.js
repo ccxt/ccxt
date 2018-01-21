6440fe5e328859373fa3816cd11e319aaa5562ed
Igor Kroitor <igor.kroitor@gmail.com>
2018-01-21 10:28:49 +0300

    missing strict es-rule for js/base/errors.js

--- js/base/errors.js
+++ js/base/errors.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /*  ------------------------------------------------------------------------ */
 
 module.exports = subclass (
@@ -24,7 +26,7 @@ module.exports = subclass (
                 , 'RequestTimeout':         {}
                 , 'ExchangeNotAvailable':   {}
                 }
-            } 
+            }
         }
     }
 )
@@ -36,7 +38,7 @@ function subclass (BaseClass, classes, namespace = {}) {
     for (const [$class, subclasses] of Object.entries (classes)) {
 
         const Class = Object.assign (namespace, {
-            
+
         /*  By creating a named property, we trick compiler to assign our class constructor function a name.
             Otherwise, all our error constructors would be shown as [Function: Error] in the debugger! And
             the super-useful `e.constructor.name` magic wouldn't work — we then would have no chance to
@@ -45,14 +47,14 @@ function subclass (BaseClass, classes, namespace = {}) {
             [$class]: class extends BaseClass {
 
                 constructor (message) {
-            
+
                     super (message)
-            
+
                 /*  A workaround to make `instanceof` work on custom Error classes in transpiled ES5.
                     See my blog post for the explanation of this hack:
-            
+
                     https://medium.com/@xpl/javascript-deriving-from-error-properly-8d2f8f315801        */
-            
+
                     this.constructor = Class
                     this.__proto__   = Class.prototype
                     this.message     = message
