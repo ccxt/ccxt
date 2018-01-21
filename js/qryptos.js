33b17ae27a1e0f7a5c063752622aeaeea5dd59d4
Igor Kroitor <igor.kroitor@gmail.com>
2018-01-21 13:57:48 +0300

    qryptos edits to order status in fetchOrders for python safety

--- js/qryptos.js
+++ js/qryptos.js
@@ -305,15 +305,18 @@ module.exports = class qryptos extends Exchange {
             market = this.market (symbol);
             request['product_id'] = market['id'];
         }
-        let status = params['status'];
-        if (status === 'open') {
-            request['status'] = 'live';
-        } else if (status === 'closed') {
-            request['status'] = 'filled';
-        } else if (status === 'canceled') {
-            request['status'] = 'cancelled';
+        let status = this.safeValue (params['status']);
+        if (status) {
+            params = this.omit (params, 'status');
+            if (status === 'open') {
+                request['status'] = 'live';
+            } else if (status === 'closed') {
+                request['status'] = 'filled';
+            } else if (status === 'canceled') {
+                request['status'] = 'cancelled';
+            }
         }
-        let result = await this.privateGetOrders (request);
+        let result = await this.privateGetOrders (this.extend (request, params));
         let orders = result['models'];
         return this.parseOrders (orders, market, since, limit);
     }
