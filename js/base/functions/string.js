a969696c7a91d4d4f6143f43b340646b01c2f2ae
Vitaly Gordon <rocket.mind@gmail.com>
2018-01-23 01:35:07 +0300

    uuid fix

--- js/base/functions/string.js
+++ js/base/functions/string.js
@@ -2,10 +2,12 @@
 
 /*  ------------------------------------------------------------------------ */
 
+const uuid = a => a ? (a ^ Math.random () * 16 >> a / 4).toString (16)
+                    : ([1e7]+-1e3+-4e3+-8e3+-1e11).replace (/[018]/g, uuid)
+
 module.exports =
     
-    { uuid: a => a ? (a ^ Math.random () * 16 >> a / 4).toString (16)
-                   : ([1e7]+-1e3+-4e3+-8e3+-1e11).replace (/[018]/g, uuid)
+    { uuid
         
     , unCamelCase: s => s.replace (/[a-z0-9][A-Z]/g, x => x[0] + '_' + x[1]).toLowerCase () // hasFetchOHLCV → has_fetch_ohlcv
 
