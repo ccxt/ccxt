a8cb392fceca510c770ca19d02f5a4bfb5179c21
Igor Kroitor <igor.kroitor@gmail.com>
2018-01-24 02:52:41 +0300

    ci retrigger

--- js/test/test.js
+++ js/test/test.js
@@ -866,6 +866,7 @@ let tryAllProxies = async function (exchange, proxies) {
             testSymbol (exchange, exchangeSymbol)
 
     } else {
+
         await tryAllProxies (exchange, proxies)
     }
 
