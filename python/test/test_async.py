4aaa64cba27f56f5ad94f11e41152d9e9ea6a31f
Igor Kroitor <igor.kroitor@gmail.com>
2018-01-23 22:53:24 +0300

    python tests partially skipped on Travis

--- python/test/test_async.py
+++ python/test/test_async.py
@@ -406,6 +406,12 @@ async def main():
         exchange = exchanges[argv.exchange]
         symbol = argv.symbol
 
+        if exchange.id in config:
+            if 'skip' in config[exchange.id]
+                if config[exchange.id]['skip']:
+                    print('skipped.')
+                    sys.exit()
+
         if hasattr(exchange, 'skip') and exchange.skip:
             dump(green(exchange.id), 'skipped')
         else:
