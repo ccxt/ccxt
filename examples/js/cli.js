2c7e0bcd4d2212054a7704be6e867672786c6f00
Igor Kroitor <igor.kroitor@gmail.com>
2018-01-24 06:14:12 +0300

    more verbose output in cli + deep human-readable printing

--- examples/js/cli.js
+++ examples/js/cli.js
@@ -94,7 +94,7 @@ async function main () {
 
                 } else {
 
-                    log (result)
+                    log.maxDepth (10).maxArrayLength (1000) (result)
                 }
 
 
