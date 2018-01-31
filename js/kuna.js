bf3c9219b158d2aa59a52d1756424c7edd2c92fb
Max Kutny <mkutny@gmail.com>
2018-01-31 12:46:39 +0200

    kuna: handleErrors moved to base class (acx)

--- js/kuna.js
+++ js/kuna.js
@@ -3,12 +3,11 @@
 // ---------------------------------------------------------------------------
 
 const acx = require ('./acx.js');
-const { ExchangeError, InsufficientFunds, OrderNotFound } = require ('./base/errors');
+const { ExchangeError } = require ('./base/errors');
 
 // ---------------------------------------------------------------------------
 
 module.exports = class kuna extends acx {
-
     describe () {
         return this.deepExtend (super.describe (), {
             'id': 'kuna',
@@ -91,19 +90,6 @@ module.exports = class kuna extends acx {
                 },
             },
         });
-    }
-
-    handleErrors (code, reason, url, method, headers, body) {
-        if (code === 400) {
-            let response = JSON.parse (body);
-            let error = this.safeValue (response, 'error');
-            let errorCode = this.safeInteger (error, 'code');
-            if (errorCode === 2002) {
-                throw new InsufficientFunds ([ this.id, method, url, code, reason, body ].join (' '));
-            } else if (errorCode === 2003) {
-                throw new OrderNotFound ([ this.id, method, url, code, reason, body ].join (' '));
-            }
-        }
     }
 
     async fetchOrderBook (symbol, params = {}) {
