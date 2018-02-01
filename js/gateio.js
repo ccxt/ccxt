49bd5bd110dac6be8ca1c60898360798471ed889
Boris Momčilović <boris@firstbeatmedia.com>
2018-02-01 17:47:53 +0100

    gateio: fetch/create deposit address #1705

--- js/gateio.js
+++ js/gateio.js
@@ -14,6 +14,14 @@ module.exports = class gateio extends bter {
             'name': 'Gate.io',
             'countries': 'CN',
             'rateLimit': 1000,
+            'has': {
+                'CORS': false,
+                'createMarketOrder': false,
+                'fetchTickers': true,
+                'withdraw': true,
+                'createDepositAddress': true,
+                'fetchDepositAddress': true,
+            },
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/31784029-0313c702-b509-11e7-9ccc-bc0da6a0e435.jpg',
                 'api': {
@@ -25,6 +33,38 @@ module.exports = class gateio extends bter {
                 'fees': 'https://gate.io/fee',
             },
         });
+    }
+
+    async createDepositAddress (currency, params = {}) {
+        let response = await this.privatePostNewAddress (this.extend ({
+            'currency': currency,
+        }, params));
+        let address = undefined;
+        if ('addr' in response) {
+            address = this.safeString (response['addr'], 0);
+        }
+        return {
+            'currency': currency,
+            'address': address,
+            'status': address !== undefined ? 'ok' : 'none',
+            'info': response,
+        };
+    }
+
+    async fetchDepositAddress (currency, params = {}) {
+        let response = await this.privatePostDepositAddress (this.extend ({
+            'currency': currency,
+        }, params));
+        let address = undefined;
+        if ('addr' in response) {
+            address = this.safeString (response['addr'], 0);
+        }
+        return {
+            'currency': currency,
+            'address': address,
+            'status': address !== undefined ? 'ok' : 'none',
+            'info': response,
+        };
     }
 
     parseTrade (trade, market) {
