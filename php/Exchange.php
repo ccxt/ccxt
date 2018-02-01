098c2b46e19968d000357ee46f0bdf8ce9e6e36d
brmn <brmn@afkxxx.com>
2018-02-01 17:23:57 +0200

    curl timeouts in ms

--- php/Exchange.php
+++ php/Exchange.php
@@ -751,9 +751,8 @@ abstract class Exchange {
         curl_setopt ($this->curl, CURLOPT_URL, $url);
 
         if ($this->timeout) {
-            $seconds = intval ($this->timeout / 1000);
-            curl_setopt ($this->curl, CURLOPT_CONNECTTIMEOUT, $seconds);
-            curl_setopt ($this->curl, CURLOPT_TIMEOUT, $seconds);
+            curl_setopt ($this->curl, CURLOPT_CONNECTTIMEOUT_MS, (int)$this->timeout);
+            curl_setopt ($this->curl, CURLOPT_TIMEOUT_MS, (int)$this->timeout);
         }
 
         curl_setopt ($this->curl, CURLOPT_RETURNTRANSFER, true);
