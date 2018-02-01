6b18ba17ce3881ac9d1a193272cbc7fcf91ade69
Max Kutny <mkutny@gmail.com>
2018-02-01 10:14:04 +0200

    southxchange: linting

--- js/southxchange.js
+++ js/southxchange.js
@@ -8,7 +8,6 @@ let { ExchangeError } = require ('./base/errors');
 //  ---------------------------------------------------------------------------
 
 module.exports = class southxchange extends Exchange {
-
     describe () {
         return this.deepExtend (super.describe (), {
             'id': 'southxchange',
@@ -251,4 +250,4 @@ module.exports = class southxchange extends Exchange {
         let response = await this.fetch2 (path, api, method, params, headers, body);
         return response;
     }
-}
+};
