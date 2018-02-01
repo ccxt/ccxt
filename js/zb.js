8ea42eaa54d633791f71f3c7e14582599e3ec3ec
redindian <red_indian@hotmail.com>
2018-02-01 22:00:04 +0100

    Update zb.js

--- js/zb.js
+++ js/zb.js
@@ -17,8 +17,6 @@ module.exports = class zb extends Exchange {
             'version': 'v1',
             'has': {
                 'CORS': false,
-                'fetchOHLCV': true,
-                'fetchTickers': false,
                 'fetchOrder': true,
                 'withdraw': true,
             },
@@ -36,6 +34,32 @@ module.exports = class zb extends Exchange {
                 '1d': '1day',
                 '3d': '3day',
                 '1w': '1week',
+            },
+            'errors': {
+                '1001': 'General error message',
+                '1002': 'internal error',
+                '1003': 'Verification does not pass',
+                '1004': 'Funding security password lock',
+                '1005': 'Funds security password is incorrect, please confirm and re-enter.',
+                '1006': 'Real-name certification pending approval or audit does not pass',
+                '1009': 'This interface is under maintenance',
+                '2001': 'RMB account balance is not enough',
+                '2002': 'There is not enough bitcoin account balance',
+                '2003': 'Litecoin account balance is not enough',
+                '2005': 'Ethereum account balance is not enough',
+                '2006': 'ETC currency account balance is not enough',
+                '2007': 'BTS currency account balance is not enough',
+                '2009': 'Account balance is not enough',
+                '3001': 'Pending orders not found',
+                '3002': 'Invalid amount',
+                '3003': 'Invalid number',
+                '3004': 'User does not exist',
+                '3005': 'Invalid parameter',
+                '3006': 'Invalid IP or inconsistent with the bound IP',
+                '3007': 'The request time has expired',
+                '3008': 'Transaction records not found',
+                '4001': 'API interface is locked or not enabled',
+                '4002': 'Request too often',
             },
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/32859187-cd5214f0-ca5e-11e7-967d-96568e2e2bd1.jpg',
@@ -188,16 +212,30 @@ module.exports = class zb extends Exchange {
     async fetchBalance (params = {}) {
         await this.loadMarkets ();
         let response = await this.privatePostGetAccountInfo ();
-        let balances = response['result']['coins'];
+        let balances = response['result'];
+        let coins = balances['coins'];
         let result = { 'info': balances };
-        for (let i = 0; i < balances.length; i++) {
-            let balance = balances[i];
-            let currency = balance['key'];
-            if (currency in this.currencies)
-                currency = this.currencies[currency]['code'];
+        let currencies = Object.keys (this.currencies);
+        for (let i = 0; i < currencies.length; i++) {
+            let currency = currencies[i];
             let account = this.account ();
-            account['free'] = parseFloat (balance['available']);
-            account['used'] = parseFloat (balance['freez']);
+            let coinBalance = undefined;
+            for (let j = 0; i < coins.length; j++) {
+                let coin = coins[j];
+                coin['key'] = coin['key'].toUpperCase ();
+                if (coin['key'] === 'BCC') {
+                    coin['key'] = 'BCH';
+                    coin['cnName'] = 'BCH';
+                    coin['enName'] = 'BCH';
+                    coin['unitTag'] = 'BCH';
+                }
+                if (coin['key'] === currency) {
+                    coinBalance = coin;
+                    break;
+                }
+            }
+            account['free'] = parseFloat (coinBalance['available']);
+            account['used'] = parseFloat (coinBalance['freez']);
             account['total'] = this.sum (account['free'], account['used']);
             result[currency] = account;
         }
@@ -310,12 +348,14 @@ module.exports = class zb extends Exchange {
 
     async createOrder (symbol, type, side, amount, price = undefined, params = {}) {
         await this.loadMarkets ();
-        let paramString = '&price=' + price.toString ();
-        paramString += '&amount=' + amount.toString ();
-        let tradeType = (side === 'buy') ? '1' : '0';
-        paramString += '&tradeType=' + tradeType;
-        paramString += '&currency=' + this.marketId (symbol);
-        let response = await this.privatePostOrder (paramString);
+        let order = {
+            'price': price.toString (),
+            'amount': amount.toString (),
+            'tradeType': (side === 'buy') ? '1' : '0',
+            'currency': this.marketId (symbol),
+        };
+        order = this.extend (order, params);
+        let response = await this.privatePostOrder (order);
         return {
             'info': response,
             'id': response['id'],
@@ -324,18 +364,121 @@ module.exports = class zb extends Exchange {
 
     async cancelOrder (id, symbol = undefined, params = {}) {
         await this.loadMarkets ();
-        let paramString = '&id=' + id.toString ();
-        if ('currency' in params)
-            paramString += '&currency=' + params['currency'];
-        return await this.privatePostCancelOrder (paramString);
+        let order = {
+            'id': id.toString (),
+            'currency': this.marketId (symbol),
+        };
+        order = this.extend (order, params);
+        return await this.privatePostCancelOrder (order);
     }
 
     async fetchOrder (id, symbol = undefined, params = {}) {
         await this.loadMarkets ();
-        let paramString = '&id=' + id.toString ();
-        if ('currency' in params)
-            paramString += '&currency=' + params['currency'];
-        return await this.privatePostGetOrder (paramString);
+        let order = {
+            'id': id.toString (),
+            'currency': this.marketId (symbol),
+        };
+        order = this.extend (order, params);
+        let response = await this.privatePostGetOrder (order);
+        return this.parseOrder (response, undefined, true);
+    }
+
+    async fetchOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
+        if (!symbol)
+            throw new ExchangeError (this.id + 'fetchOrders requires a symbol parameter');
+        await this.loadMarkets ();
+        let market = this.market (symbol);
+        let request = {
+            'currency': market['id'],
+        };
+        // pageIndex 页数 默认1; pageSize 每页数量 默认50
+        let defaultParams = {
+            'pageIndex': 1,
+            'pageSize': 50,
+        };
+        // 默认请求方法，不分买卖类型 (default method GetOrdersIgnoreTradeType)
+        let method = 'privatePostGetOrdersIgnoreTradeType';
+        // 如果传入了status，则查未完成的订单；如果传入了tradeType，则查买单或者卖单(if status in parmas,change method to GetUnfinishedOrdersIgnoreTradeType); status === 1表示完成的订单,zb api不提供这样的查询.(status ===1 means get finished orders, the zb exchange did not support query finished orders )
+        let hasStatus = ('status' in params);
+        if (hasStatus && params['status'] === 0) {
+            method = 'privatePostGetUnfinishedOrdersIgnoreTradeType';
+            defaultParams['pageSize'] = 10; // fixed to 10
+        } else if ('tradeType' in params) {
+            method = 'privatePostGetOrdersNew';
+            // tradeType 交易类型1/0[buy/sell]
+            request['tradeType'] = params['tradeType'];
+        }
+        request = this.extend (request, defaultParams, params);
+        let response = await this[method] (request);
+        return this.parseOrders (response, market, since, limit);
+    }
+
+    async fetchOpenOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
+        let open = 0; // 0 for unfilled orders, 1 for filled orders
+        return await this.fetchOrders (symbol, undefined, undefined, this.extend ({
+            'status': open,
+        }, params));
+    }
+
+    parseOrder (order, market = undefined, isSingle = false) {
+        let side = order['type'] === 1 ? 'buy' : 'sell';
+        let type = 'limit'; // market order is not availalbe in ZB
+        let timestamp = undefined;
+        let createDateField = this.getCreateDateField ();
+        if (createDateField in order)
+            timestamp = order[createDateField];
+        let symbol = undefined;
+        if ('currency' in order) {
+            // get symbol from currency
+            market = this.marketsById[order['currency']];
+        }
+        if (market)
+            symbol = market['symbol'];
+        let price = order['price'];
+        let average = order['trade_price'];
+        let filled = order['trade_amount'];
+        let amount = order['total_amount'];
+        let remaining = amount - filled;
+        let cost = order['trade_money'];
+        let status = this.parseOrderStatus (order['status'], filled, isSingle);
+        let result = {
+            'info': order,
+            'id': order['id'],
+            'timestamp': timestamp,
+            'datetime': this.iso8601 (timestamp),
+            'symbol': symbol,
+            'type': type,
+            'side': side,
+            'price': price,
+            'average': average,
+            'cost': cost,
+            'amount': amount,
+            'filled': filled,
+            'remaining': remaining,
+            'status': status,
+            'fee': undefined,
+        };
+        return result;
+    }
+
+    parseOrderStatus (status, filled, isSingle) {
+        if (status === 1)
+            return 'canceled';
+        if (status === 3 && isSingle) {
+            // when fetch single order detail, status == 3 means partial or open
+            return filled === 0 ? 'open' : 'partial';
+        }
+        if (status === 3)
+            return 'partial';
+        if (status === 0)
+            return 'open';
+        if (status === 2)
+            return 'closed';
+        return status;
+    }
+
+    getCreateDateField () {
+        return 'trade_date';
     }
 
     nonce () {
@@ -349,10 +492,13 @@ module.exports = class zb extends Exchange {
             if (Object.keys (params).length)
                 url += '?' + this.urlencode (params);
         } else {
-            this.checkRequiredCredentials ();
+            let query = this.keysort (this.extend ({
+                'method': path,
+                'accesskey': this.apiKey,
+            }, params));
             let nonce = this.nonce ();
-            let auth = 'accesskey=' + this.apiKey;
-            auth += '&' + 'method=' + path;
+            query = this.keysort (query);
+            let auth = this.rawencode (query);
             let secret = this.hash (this.encode (this.secret), 'sha1');
             let signature = this.hmac (this.encode (auth), this.encode (secret), 'md5');
             let suffix = 'sign=' + signature + '&reqTime=' + nonce.toString ();
@@ -385,9 +531,17 @@ module.exports = class zb extends Exchange {
 
     async request (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
         let response = await this.fetch2 (path, api, method, params, headers, body);
-        if (api === 'private')
-            if ('code' in response)
-                throw new ExchangeError (this.id + ' ' + this.json (response));
+        if (api === 'private') {
+            if ('code' in response) {
+                let code = response['code'];
+                if (code !== 1000) {
+                    if (this.errors[code]) {
+                        response['message'] = this.errors[code];
+                    }
+                    throw new ExchangeError (this.id + ' ' + this.json (response));
+                }
+            }
+        }
         return response;
     }
 };
