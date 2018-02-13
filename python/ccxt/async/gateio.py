195583bf8a884ef7960941be22101971cba72a68
Samuel Holt <vertokli74@gmail.com>
2018-02-13 13:10:53 +0000

    Added proxy kwargs to request for atomic proxy support

--- python/ccxt/async/gateio.py
+++ python/ccxt/async/gateio.py
@@ -294,8 +294,8 @@ class gateio (Exchange):
             }
         return {'url': url, 'method': method, 'body': body, 'headers': headers}
 
-    async def request(self, path, api='public', method='GET', params={}, headers=None, body=None):
-        response = await self.fetch2(path, api, method, params, headers, body)
+    async def request(self, path, api='public', method='GET', params={}, headers=None, body=None, proxy=''):
+        response = await self.fetch2(path, api, method, params, headers, body, proxy)
         if 'result' in response:
             if response['result'] != 'true':
                 raise ExchangeError(self.id + ' ' + self.json(response))
