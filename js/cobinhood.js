722c914b9832181f57673f5a4a953963e509289d
Igor Kroitor <igor.kroitor@gmail.com>
2018-02-26 02:33:14 +0300

    updated cobinhood fetchOpenOrders

--- js/cobinhood.js
+++ js/cobinhood.js
@@ -447,7 +447,10 @@ module.exports = class cobinhood extends Exchange {
         if (typeof symbol !== 'undefined')
             market = this.market (symbol);
         let result = await this.privateGetTradingOrders (params);
-        return this.parseOrders (result['result']['orders'], market, since, limit);
+        let orders = this.parseOrders (result['result']['orders'], market, since, limit);
+        if (typeof symbol !== 'undefined')
+            return this.filterOrdersBySymbol (orders, symbol);
+        return orders;
     }
 
     async fetchOrderTrades (id, symbol = undefined, params = {}) {
