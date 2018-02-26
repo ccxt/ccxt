7a0aff274053a6d37b2cf1786b2e29f8bd5985dc
Igor Kroitor <igor.kroitor@gmail.com>
2018-02-26 23:06:11 +0300

    relaxed aiohttp version #2057

--- python/setup.py
+++ python/setup.py
@@ -72,7 +72,7 @@ setup(
 
     extras_require={
         ':python_version>="3.5.3"': [
-            'aiohttp==3.0.1',
+            'aiohttp',
             'cchardet==2.1.1',
             'aiodns==1.1.1',
             'yarl==1.1.0'
