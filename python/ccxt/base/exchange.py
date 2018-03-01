3208f0b7040844bf721549d8cadc25f64622c182
Igor Kroitor <igor.kroitor@gmail.com>
2018-03-01 07:08:41 +0300

    added find_market, find_symbol to exchange.py

--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -1101,6 +1101,22 @@ class Exchange(object):
             return self.currencies[code]
         self.raise_error(ExchangeError, details='Does not have currency code ' + str(code))
 
+    def find_market(self, string):
+        if not self.markets:
+            self.raise_error(ExchangeError, details='Markets not loaded')
+        if isinstance(string, basestring):
+            if string in self.markets_by_id:
+                return self.markets_by_id[string]
+            if string in self.markets:
+                return self.markets[string]
+        return string
+
+    def find_symbol(self, string):
+        market = self.find_market(string)
+        if isinstance(market, dict)
+            return market['symbol']
+        return market
+
     def market(self, symbol):
         if not self.markets:
             self.raise_error(ExchangeError, details='Markets not loaded')
