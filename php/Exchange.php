35b658eaeb4cc84372c6e21b9297ae5284971611
Travis CI <travis@travis-ci.org>
2018-03-04 19:24:27 +0000

    1.11.8
    [ci skip]

--- php/Exchange.php
+++ php/Exchange.php
@@ -30,7 +30,7 @@ SOFTWARE.
 
 namespace ccxt;
 
-$version = '1.11.7';
+$version = '1.11.8';
 
 abstract class Exchange {
 
--- python/ccxt/async/liqui.py
+++ python/ccxt/async/liqui.py
@@ -532,7 +532,7 @@ class liqui (Exchange):
         await self.load_markets()
         request = {}
         market = None
-        if symbol:
+        if symbol is not None:
             market = self.market(symbol)
             request['pair'] = market['id']
         response = await self.privatePostActiveOrders(self.extend(request, params))
