bf9f213bbba8003c361676a9046f2e2b842d03ce
Igor Kroitor <igor.kroitor@gmail.com>
2018-03-06 00:15:41 +0300

    okex fetch_order in php fix #2128

--- js/okcoinusd.js
+++ js/okcoinusd.js
@@ -530,7 +530,8 @@ module.exports = class okcoinusd extends Exchange {
         method += 'OrderInfo';
         let response = await this[method] (this.extend (request, params));
         let ordersField = this.getOrdersField ();
-        if (response[ordersField].length > 0)
+        let numOrders = response[ordersField].length;
+        if (numOrders > 0)
             return this.parseOrder (response[ordersField][0]);
         throw new OrderNotFound (this.id + ' order ' + id + ' not found');
     }
