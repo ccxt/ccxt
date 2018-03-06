0ccf92d7cefdb2ccbc3ad3ec40ce4622a9deb7d7
lfern <lfern70@gmail.com>
2018-03-06 09:58:58 +0100

    Update ccxt.js

--- ccxt.js
+++ ccxt.js
@@ -46,6 +46,7 @@ Exchange.ccxtVersion = version
 const exchanges = {
     '_1broker':                require ('./js/_1broker.js'),
     '_1btcxe':                 require ('./js/_1btcxe.js'),
+    'abucoins':                require ('./js/abucoins.js'),
     'acx':                     require ('./js/acx.js'),
     'allcoin':                 require ('./js/allcoin.js'),
     'anxpro':                  require ('./js/anxpro.js'),
