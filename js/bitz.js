109f21ed1cff193aa431879843899dd53351d17e
Igor Kroitor <igor.kroitor@gmail.com>
2018-03-06 16:56:33 +0300

    added bit-z "balances" endpoint #2142

--- js/bitz.js
+++ js/bitz.js
@@ -48,6 +48,7 @@ module.exports = class bitz extends Exchange {
                 },
                 'private': {
                     'post': [
+                        'balances',
                         'tradeAdd',
                         'tradeCancel',
                         'openOrders',
