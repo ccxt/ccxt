854e14567e36641a9546879777ba27971fce5124
Keith Garrod <kpgarrod@gmail.com>
2018-03-08 13:23:32 +0100

    Added Rollup

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+
+export default {
+  input: 'ccxt.js',
+  output: {
+    name: "ccxt",
+    file: 'build/ccxt.module.js',
+    format: 'iife'
+  },
+  plugins: [
+    nodeResolve({
+      jsnext: true,
+      main: true
+    }),
+
+    commonjs({
+
+      // if false then skip sourceMap generation for CommonJS modules
+      sourceMap: false
+      /*,  // Default: true
+      // non-CommonJS modules will be ignored, but you can also
+      // specifically include/exclude files
+      include: 'node_modules/**',  // Default: undefined
+      exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],  // Default: undefined
+      // these values can also be regular expressions
+      // include: /node_modules/
+
+      // search for files other than .js files (must already
+      // be transpiled by a previous plugin!)
+      extensions: [ '.js', '.coffee' ],  // Default: [ '.js' ]
+
+      // if true then uses of `global` won't be dealt with by this plugin
+      ignoreGlobal: false,  // Default: false
+
+
+      // explicitly specify unresolvable named exports
+      // (see below for more details)
+      namedExports: { './module.js': ['foo', 'bar' ] } /*,  // Default: undefined
+
+      // sometimes you have to leave require statements
+      // unconverted. Pass an array containing the IDs
+      // or a `id => boolean` function. Only use this
+      // option if you know what you're doing!
+      ignore: [ 'conditional-runtime-dependency' ]
+      */
+    })
+  ]
+};
