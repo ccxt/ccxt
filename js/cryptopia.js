87f2b475dbb48ef1172a5e92518adc870934017f
Hasip Timurtaş <hasip.timurtas@gmail.com>
2018-03-09 20:01:03 +0300

    Update fetchTickers in cryptopia.js for sysmbols
    symbols parameter is not used in fetchTickers function. if I call fetchTickers with my symbols it returns all the markets not only my symbols. So I edited it.

--- js/cryptopia.js
+++ js/cryptopia.js
@@ -272,7 +272,13 @@ module.exports = class cryptopia extends Exchange {
                 throw new ExchangeError (this.id + ' fetchTickers() returned unrecognized pair id ' + id.toString ());
             let market = this.markets_by_id[id];
             let symbol = market['symbol'];
-            result[symbol] = this.parseTicker (ticker, market);
+            if(symbols){
+                if(symbols.includes(symbol)){
+                    result[symbol] = this.parseTicker (ticker, market);
+                }
+            }else{
+                result[symbol] = this.parseTicker (ticker, market);
+            }
         }
         return result;
     }
