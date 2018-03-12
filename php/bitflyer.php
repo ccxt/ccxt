db16b9e7c868049fc834db556c3d030a5ffd1d3f
Travis CI <travis@travis-ci.org>
2018-03-12 05:52:46 +0000

    1.11.78
    [ci skip]

--- php/bitflyer.php
+++ php/bitflyer.php
@@ -19,6 +19,8 @@ class bitflyer extends Exchange {
                 'withdraw' => true,
                 'fetchOrders' => true,
                 'fetchOrder' => true,
+                'fetchOpenOrders' => 'emulated',
+                'fetchClosedOrders' => 'emulated',
             ),
             'urls' => array (
                 'logo' => 'https://user-images.githubusercontent.com/1294454/28051642-56154182-660e-11e7-9b0d-6042d1e6edd8.jpg',
@@ -313,6 +315,11 @@ class bitflyer extends Exchange {
 
     public function fetch_open_orders ($symbol = null, $since = null, $limit = 100, $params = array ()) {
         $params['child_order_state'] = 'ACTIVE';
+        return $this->fetch_orders($symbol, $since, $limit, $params);
+    }
+
+    public function fetch_closed_orders ($symbol = null, $since = null, $limit = 100, $params = array ()) {
+        $params['child_order_state'] = 'COMPLETED';
         return $this->fetch_orders($symbol, $since, $limit, $params);
     }
 
