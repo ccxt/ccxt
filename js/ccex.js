c5998bb10a8a0c60e855d6ecbf238a22b3f890f4
Igor Kroitor <igor.kroitor@gmail.com>
2018-03-14 02:47:09 +0300

    ccex fetchMarkets → pairs.json fix #2246

--- js/ccex.js
+++ js/ccex.js
@@ -22,7 +22,7 @@ module.exports = class ccex extends Exchange {
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/27766433-16881f90-5ed8-11e7-92f8-3d92cc747a6c.jpg',
                 'api': {
-                    'tickers': 'https://c-cex.com/t',
+                    'web': 'https://c-cex.com/t',
                     'public': 'https://c-cex.com/t/api_pub.html',
                     'private': 'https://c-cex.com/t/api.html',
                 },
@@ -30,7 +30,7 @@ module.exports = class ccex extends Exchange {
                 'doc': 'https://c-cex.com/?id=api',
             },
             'api': {
-                'tickers': {
+                'web': {
                     'get': [
                         'coinnames',
                         '{market}',
@@ -83,24 +83,45 @@ module.exports = class ccex extends Exchange {
     }
 
     async fetchMarkets () {
-        let markets = await this.publicGetMarkets ();
-        let result = [];
-        for (let p = 0; p < markets['result'].length; p++) {
-            let market = markets['result'][p];
-            let id = market['MarketName'];
-            let base = market['MarketCurrency'];
-            let quote = market['BaseCurrency'];
+        let result = {};
+        let response = await this.webGetPairs ();
+        let markets = response['pairs'];
+        for (let i = 0; i < markets.length; i++) {
+            let id = markets[i];
+            let [ baseId, quoteId ] = id.split ('-');
+            let base = baseId.toUpperCase ();
+            let quote = quoteId.toUpperCase ();
             base = this.commonCurrencyCode (base);
             quote = this.commonCurrencyCode (quote);
             let symbol = base + '/' + quote;
-            result.push ({
+            result[symbol] = {
                 'id': id,
                 'symbol': symbol,
                 'base': base,
                 'quote': quote,
-                'info': market,
-            });
+                'baseId': baseId,
+                'quoteId': quoteId,
+                'info': id,
+            };
         }
+        // an alternative documented parser
+        //     let markets = await this.publicGetMarkets ();
+        //     for (let p = 0; p < markets['result'].length; p++) {
+        //         let market = markets['result'][p];
+        //         let id = market['MarketName'];
+        //         let base = market['MarketCurrency'];
+        //         let quote = market['BaseCurrency'];
+        //         base = this.commonCurrencyCode (base);
+        //         quote = this.commonCurrencyCode (quote);
+        //         let symbol = base + '/' + quote;
+        //         result.push ({
+        //             'id': id,
+        //             'symbol': symbol,
+        //             'base': base,
+        //             'quote': quote,
+        //             'info': market,
+        //         });
+        //     }
         return result;
     }
 
@@ -204,7 +225,7 @@ module.exports = class ccex extends Exchange {
 
     async fetchTickers (symbols = undefined, params = {}) {
         await this.loadMarkets ();
-        let tickers = await this.tickersGetPrices (params);
+        let tickers = await this.webGetPrices (params);
         let result = { 'info': tickers };
         let ids = Object.keys (tickers);
         for (let i = 0; i < ids.length; i++) {
@@ -230,7 +251,7 @@ module.exports = class ccex extends Exchange {
     async fetchTicker (symbol, params = {}) {
         await this.loadMarkets ();
         let market = this.market (symbol);
-        let response = await this.tickersGetMarket (this.extend ({
+        let response = await this.webGetMarket (this.extend ({
             'market': market['id'].toLowerCase (),
         }, params));
         let ticker = response['ticker'];
@@ -307,7 +328,7 @@ module.exports = class ccex extends Exchange {
 
     async request (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
         let response = await this.fetch2 (path, api, method, params, headers, body);
-        if (api === 'tickers')
+        if (api === 'web')
             return response;
         if ('success' in response)
             if (response['success'])
