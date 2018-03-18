e44e62cd94da4c44403014ff221ec85a0ae3f05b
Igor Kroitor <igor.kroitor@gmail.com>
2018-03-18 16:54:34 +0300

    bibox fetchTicker consistency

--- js/bibox.js
+++ js/bibox.js
@@ -132,6 +132,7 @@ module.exports = class bibox extends Exchange {
         } else {
             symbol = ticker['coin_symbol'] + '/' + ticker['currency_symbol'];
         }
+        let last = this.safeFloat (ticker, 'last');
         return {
             'symbol': symbol,
             'timestamp': timestamp,
@@ -139,12 +140,14 @@ module.exports = class bibox extends Exchange {
             'high': this.safeFloat (ticker, 'high'),
             'low': this.safeFloat (ticker, 'low'),
             'bid': this.safeFloat (ticker, 'buy'),
+            'bidVolume': undefined,
             'ask': this.safeFloat (ticker, 'sell'),
+            'askVolume': undefined,
             'vwap': undefined,
             'open': undefined,
-            'close': undefined,
-            'first': undefined,
-            'last': this.safeFloat (ticker, 'last'),
+            'close': last,
+            'last': last,
+            'previousClose': undefined,
             'change': undefined,
             'percentage': this.safeString (ticker, 'percent'),
             'average': undefined,
