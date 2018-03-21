f5a333817aaf2fa654d2d8dae857745aca2bf1d7
Igor Kroitor <igor.kroitor@gmail.com>
2018-03-21 18:01:31 +0300

    added a check for the entry point to examples/py/async-basic.py fix #2324 #2173

--- examples/py/async-basic.py
+++ examples/py/async-basic.py
@@ -16,5 +16,5 @@ async def test_gdax():
     await gdax.close()
     return markets
 
-
-print(asyncio.get_event_loop().run_until_complete(test_gdax()))
+if __name__ == '__main__':
+    print(asyncio.get_event_loop().run_until_complete(test_gdax()))
