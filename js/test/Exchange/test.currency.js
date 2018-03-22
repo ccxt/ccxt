20ecfe0ffe06cc0c83da470e99ad2be63a87fcd8
Igor Kroitor <igor.kroitor@gmail.com>
2018-03-22 19:06:43 +0300

    test.currency.js, test.markets.js relaxed temporarily

--- js/test/Exchange/test.currency.js
+++ js/test/Exchange/test.currency.js
@@ -15,27 +15,32 @@ module.exports = (exchange, currency, method) => {
     const format = {
         'id':     'btc',      // string literal for referencing within an exchange
         'code':   'BTC',      // uppercase string literal of a pair of currencies
-        'name':   'Bitcoin',  // uppercase string, base currency, 3 or more letters
-        'quote':  'USD',      // uppercase string, quote currency, 3 or more letters
-        'withdraw': true,     // can withdraw
-        'deposit': true,      // can deposit
-        'active': true,       // can both withdraw & deposit
-        'status': status,     //
-        'precision': 8,       // number of decimal digits "after the dot"
-        'limits': {           // value limits when placing orders on this market
-            'amount': {
-                'min': 0.01,  // order amount should be > min
-                'max': 1000,  // order amount should be < max
-            },
-            'price': {
-                'min': 0.01,  // order price should be > min
-                'max': 1000,  // order price should be < max
-            },
-            'cost':  {        // order cost = price * amount
-                'min': 0.01,  // order cost should be > min
-                'max': 1000,  // order cost should be < max
-            },
-        },
+
+        //----------------------------------------------------------------------
+        // commented temporarily to bring currencies to consistency first
+        // 'name':   'Bitcoin',  // uppercase string, base currency, 3 or more letters
+        // 'quote':  'USD',      // uppercase string, quote currency, 3 or more letters
+        // 'withdraw': true,     // can withdraw
+        // 'deposit': true,      // can deposit
+        // 'active': true,       // can both withdraw & deposit
+        // 'status': 'ok',       // ?
+        // 'precision': 8,       // number of decimal digits "after the dot"
+        // 'limits': {           // value limits when placing orders on this market
+        //     'amount': {
+        //         'min': 0.01,  // order amount should be > min
+        //         'max': 1000,  // order amount should be < max
+        //     },
+        //     'price': {
+        //         'min': 0.01,  // order price should be > min
+        //         'max': 1000,  // order price should be < max
+        //     },
+        //     'cost':  {        // order cost = price * amount
+        //         'min': 0.01,  // order cost should be > min
+        //         'max': 1000,  // order cost should be < max
+        //     },
+        // },
+        //---------------------------------------------------------------------
+
         'info': {}, // the original unparsed market info from the exchange
     }
 
