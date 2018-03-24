acf53bd96c29bfed492ce3c34da771d9eac1eb48
Igor Kroitor <igor.kroitor@gmail.com>
2018-03-24 23:07:47 +0300

    kraken limits urls

--- js/kraken.js
+++ js/kraken.js
@@ -45,7 +45,7 @@ module.exports = class kraken extends Exchange {
                 'api': {
                     'public': 'https://api.kraken.com',
                     'private': 'https://api.kraken.com',
-                    'zendesk': 'https://kraken.zendesk.com/hc/en-us/articles',
+                    'zendesk': 'https://support.kraken.com/hc/en-us/articles',
                 },
                 'www': 'https://www.kraken.com',
                 'doc': [
