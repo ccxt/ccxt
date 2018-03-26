7bde25e9d6d3056d8e7d503a849605de31a6b65a
Igor Kroitor <igor.kroitor@gmail.com>
2018-03-26 18:35:28 +0300

    livecoin XBT → Bricktox fix #2359

--- js/livecoin.js
+++ js/livecoin.js
@@ -84,6 +84,7 @@ module.exports = class livecoin extends Exchange {
             },
             'commonCurrencies': {
                 'CRC': 'CryCash',
+                'XBT': 'Bricktox',
             },
         });
     }
