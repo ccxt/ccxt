1aef6396103147c9c0bca907d5474f0998fc7411
redindian <red_indian@hotmail.com>
2018-04-03 21:50:13 +0200

    Update zaif.js

--- js/zaif.js
+++ js/zaif.js
@@ -33,6 +33,14 @@ module.exports = class zaif extends Exchange {
                     'https://www.npmjs.com/package/zaif.jp',
                     'https://github.com/you21979/node-zaif',
                 ],
+                'fees': 'https://zaif.jp/fee?lang=en',
+            },
+            'fees': {
+                'trading': {
+                    'percentage': true,
+                    'taker': -0.0001,
+                    'maker': -0.0005,
+                },
             },
             'api': {
                 'public': {
