236fd4b445d43aaf0289008419c8f1e351a78268
Igor Kroitor <igor.kroitor@gmail.com>
2018-04-11 13:52:06 +0300

    bitz fetchBalance update & fix #2537

--- js/bitz.js
+++ js/bitz.js
@@ -15,6 +15,7 @@ module.exports = class bitz extends Exchange {
             'countries': 'HK',
             'rateLimit': 1000,
             'version': 'v1',
+            'userAgent': this.userAgents['chrome'],
             'has': {
                 'fetchTickers': true,
                 'fetchOHLCV': true,
@@ -163,17 +164,18 @@ module.exports = class bitz extends Exchange {
         let result = { 'info': response };
         let keys = Object.keys (balances);
         for (let i = 0; i < keys.length; i++) {
-            let currency = keys[i];
-            let balance = parseFloat (balances[currency]);
-            if (currency in this.currencies_by_id)
-                currency = this.currencies_by_id[currency]['code'];
-            else
-                currency = currency.toUpperCase ();
-            let account = this.account ();
-            account['free'] = balance;
-            account['used'] = undefined;
-            account['total'] = balance;
-            result[currency] = account;
+            let id = keys[i];
+            if (!(id.indexOf ('_') >= 0)) {
+                let code = id.toUpperCase ();
+                if (id in this.currencies_by_id) {
+                    code = this.currencies_by_id[id]['code'];
+                }
+                let account = this.account ();
+                account['used'] = parseFloat (balances[id + '_lock']);
+                account['total'] = parseFloat (balances[id]);
+                account['free'] = account['total'] - account['used'];
+                result[code] = account;
+            }
         }
         return this.parseBalance (result);
     }
