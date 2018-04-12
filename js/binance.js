21a74e46f87ee4014cb3e75b931b4c921c106e0f
Max Kutny <mkutny@gmail.com>
2018-04-12 12:47:57 +0300

    binance: parseOrder: amount/cost: stick to precision

--- js/binance.js
+++ js/binance.js
@@ -585,10 +585,11 @@ module.exports = class binance extends Exchange {
         let amount = parseFloat (order['origQty']);
         let filled = this.safeFloat (order, 'executedQty', 0.0);
         let remaining = Math.max (amount - filled, 0.0);
+        remaining = this.amountToPrecision (symbol, remaining);
         let cost = undefined;
         if (typeof price !== 'undefined')
             if (typeof filled !== 'undefined')
-                cost = price * filled;
+                cost = this.costToPrecision (symbol, price * filled);
         let result = {
             'info': order,
             'id': order['orderId'].toString (),
