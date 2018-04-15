02331bef86ebd013d227cdc771e3558c69f00f8c
Igor Kroitor <igor.kroitor@gmail.com>
2018-04-15 04:50:28 +0300

    removed bittrex fetchOrders (obsolete)

--- js/bittrex.js
+++ js/bittrex.js
@@ -26,7 +26,6 @@ module.exports = class bittrex extends Exchange {
                 'fetchMyTrades': false,
                 'fetchOHLCV': true,
                 'fetchOrder': true,
-                'fetchOrders': true,
                 'fetchOpenOrders': true,
                 'fetchTickers': true,
                 'withdraw': true,
@@ -594,7 +593,7 @@ module.exports = class bittrex extends Exchange {
         return this.parseOrder (response['result']);
     }
 
-    async fetchOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
+    async fetchClosedOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
         await this.loadMarkets ();
         let request = {};
         let market = undefined;
@@ -607,11 +606,6 @@ module.exports = class bittrex extends Exchange {
         if (symbol)
             return this.filterBySymbol (orders, symbol);
         return orders;
-    }
-
-    async fetchClosedOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
-        let orders = await this.fetchOrders (symbol, since, limit, params);
-        return this.filterBy (orders, 'status', 'closed');
     }
 
     async fetchDepositAddress (code, params = {}) {
