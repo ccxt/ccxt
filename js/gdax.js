0ea25bc55378356291053a0fe94722ccbf186b28
amarvin <alex.marvin@basf.com>
2018-04-24 23:33:43 -0400

    Fixed JavaScript in-line comment

--- js/gdax.js
+++ js/gdax.js
@@ -104,7 +104,7 @@ module.exports = class gdax extends Exchange {
                     'tierBased': true, // complicated tier system per coin
                     'percentage': true,
                     'maker': 0.0,
-                    'taker': 0.3 / 100,  # tiered fee starts at 0.3%
+                    'taker': 0.3 / 100, // tiered fee starts at 0.3%
                 },
                 'funding': {
                     'tierBased': false,
