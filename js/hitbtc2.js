7f1d243f73244f6f73e654088d63cd3d1aafffdf
frosty00 <carlo.revelli@yahoo.co.uk>
2018-04-30 20:12:13 +0100

    hitbtc2 remove redundant request

--- js/hitbtc2.js
+++ js/hitbtc2.js
@@ -1134,11 +1134,4 @@ module.exports = class hitbtc2 extends hitbtc {
             throw new ExchangeError (this.id + ' ' + body);
         }
     }
-
-    async request (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
-        let response = await this.fetch2 (path, api, method, params, headers, body);
-        if ('error' in response)
-            throw new ExchangeError (this.id + ' ' + this.json (response));
-        return response;
-    }
 };
