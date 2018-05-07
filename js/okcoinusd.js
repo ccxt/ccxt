82ee6b5ae0a4250855b0d1918556ffb8b607b63e
Igor Kroitor <igor.kroitor@gmail.com>
2018-05-08 00:59:56 +0300

    okcoinusd handling for code 1019 order already closed = OrderNotFound

--- js/okcoinusd.js
+++ js/okcoinusd.js
@@ -131,6 +131,7 @@ module.exports = class okcoinusd extends Exchange {
             'exceptions': {
                 '1009': OrderNotFound, // for spot markets, cancelling closed order
                 '1051': OrderNotFound, // for spot markets, cancelling "just closed" order
+                '1019': OrderNotFound, // order closed?
                 '20015': OrderNotFound, // for future markets
                 '1013': InvalidOrder, // no contract type (PR-1101)
                 '1027': InvalidOrder, // createLimitBuyOrder(symbol, 0, 0): Incorrect parameter may exceeded limits
