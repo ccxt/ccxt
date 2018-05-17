6183856bed92df72c2b9a0d96a8e73573f448e31
Igor Kroitor <igor.kroitor@gmail.com>
2018-05-18 00:58:53 +0300

    cryptopia LBTC → LiteBitcoin fix #2871

--- js/cryptopia.js
+++ js/cryptopia.js
@@ -84,6 +84,7 @@ module.exports = class cryptopia extends Exchange {
                 'FCN': 'Facilecoin',
                 'FUEL': 'FC2', // FuelCoin != FUEL
                 'HAV': 'Havecoin',
+                'LBTC': 'LiteBitcoin',
                 'LDC': 'LADACoin',
                 'MARKS': 'Bitmark',
                 'NET': 'NetCoin',
