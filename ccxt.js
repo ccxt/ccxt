0a5d54b5b579a887cc4937bd2e1f15d77ebeed1d
Travis CI <travis@travis-ci.org>
2018-05-21 09:40:20 +0000

    1.14.40
    [ci skip]

--- ccxt.js
+++ ccxt.js
@@ -37,7 +37,7 @@ const Exchange  = require ('./js/base/Exchange')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.14.39'
+const version = '1.14.40'
 
 Exchange.ccxtVersion = version
 
