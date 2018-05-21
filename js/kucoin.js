e4df36988b78803db30af8d3f20343c347422ca8
Aloysius Pendergast <aloysius.pgast@gmail.com>
2018-05-21 16:25:17 +0200

    Pass 'limit' parameter to Kucoin when retrieving order book

--- js/kucoin.js
+++ js/kucoin.js
@@ -329,6 +329,7 @@ module.exports = class kucoin extends Exchange {
         let market = this.market (symbol);
         let response = await this.publicGetOpenOrders (this.extend ({
             'symbol': market['id'],
+            'limit': limit,
         }, params));
         let dataInResponse = ('data' in response);
         let orderbook = undefined;
