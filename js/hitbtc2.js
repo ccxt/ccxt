52ddaecb5f5b6930785c46b037e203e831c01daa
Igor Kroitor <igor.kroitor@gmail.com>
2018-05-21 16:41:21 +0300

    hitbtc2 handling for InvalidOrder: Quantity too low

--- js/hitbtc2.js
+++ js/hitbtc2.js
@@ -532,6 +532,7 @@ module.exports = class hitbtc2 extends hitbtc {
             'options': {
                 'defaultTimeInForce': 'FOK',
             },
+            'exceptions': {},
         });
     }
 
@@ -1128,9 +1129,16 @@ module.exports = class hitbtc2 extends hitbtc {
                 if ('error' in response) {
                     if ('message' in response['error']) {
                         let message = response['error']['message'];
+                        let code = this.safeString (response['error'], 'code');
+                        let exceptions = this.exceptions;
+                        if (code in exceptions) {
+                            throw new exceptions[code] (feedback);
+                        }
                         if (message === 'Order not found') {
                             throw new OrderNotFound (this.id + ' order not found in active orders');
                         } else if (message === 'Quantity not a valid number') {
+                            throw new InvalidOrder (feedback);
+                        } else if (message === 'Quantity too low') {
                             throw new InvalidOrder (feedback);
                         } else if (message === 'Insufficient funds') {
                             throw new InsufficientFunds (feedback);
