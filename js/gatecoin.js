2c6ba20b458c32c2ce6d83360b04df78f8c5ec04
Egor Egorov <me@egorfine.com>
2018-05-23 16:28:26 +0300

    gatecoin: added more statuses to parse

--- js/gatecoin.js
+++ js/gatecoin.js
@@ -460,6 +460,9 @@ module.exports = class gatecoin extends Exchange {
 
     parseOrderStatus (status) {
         const statuses = {
+            '1': 'open', // New
+            '2': 'open', // Filling
+            '4': 'canceled',
             '6': 'closed',
         };
         if (status in statuses)
