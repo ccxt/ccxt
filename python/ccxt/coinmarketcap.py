23700fa632c6dd0564e2b10154ee7245f620d95c
Travis CI <travis@travis-ci.org>
2018-05-29 17:34:16 +0000

    1.14.84
    [ci skip]

--- python/ccxt/coinmarketcap.py
+++ python/ccxt/coinmarketcap.py
@@ -109,7 +109,7 @@ class coinmarketcap (Exchange):
             'GET Protocol': 'GET Protocol',
             'Global Tour Coin': 'Global Tour Coin',  # conflict with GTC(Game.com)
             'GuccioneCoin': 'GuccioneCoin',  # conflict with GCC(Global Cryptocurrency)
-            'Hi Mutual Society': 'Hi Mutual Society',  # conflict with HMC(HarmonyCoin)
+            'HarmonyCoin': 'HarmonyCoin',  # conflict with HMC(Hi Mutual Society)
             'Huncoin': 'Huncoin',  # conflict with HNC(Helleniccoin)
             'iCoin': 'iCoin',
             'Infinity Economics': 'Infinity Economics',  # conflict with XIN(Mixin)
