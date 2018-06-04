1b0c986cb6de294b644ff7256422c1522e3da241
Travis CI <travis@travis-ci.org>
2018-06-04 17:26:36 +0000

    1.14.124
    [ci skip]

--- ccxt.js
+++ ccxt.js
@@ -37,7 +37,7 @@ const Exchange  = require ('./js/base/Exchange')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.14.123'
+const version = '1.14.124'
 
 Exchange.ccxtVersion = version
 
--- php/Exchange.php
+++ php/Exchange.php
@@ -30,7 +30,7 @@ SOFTWARE.
 
 namespace ccxt;
 
-$version = '1.14.123';
+$version = '1.14.124';
 
 // rounding mode
 const TRUNCATE = 0;
