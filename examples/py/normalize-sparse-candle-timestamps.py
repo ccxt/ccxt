c9d78f0fe2b203a1a538f1565aa7da8e75df8c8d
Igor Kroitor <igor.kroitor@gmail.com>
2018-06-05 11:22:40 +0300

    added examples/py/normalize-sparse-candle-timestamp.py fix #3034

--- examples/py/normalize-sparse-candle-timestamps.py
+++ examples/py/normalize-sparse-candle-timestamps.py
@@ -25,9 +25,9 @@ duration *= 1000
 
 pprint([[
     exchange.iso8601 (int(round(candle[0] / duration)) * duration),
-    candle[1],
-    candle[2],
-    candle[3],
-    candle[4],
-    candle[5]
+    candle[1],  # o
+    candle[2],  # h
+    candle[3],  # l
+    candle[4],  # c
+    candle[5]   # v
 ] for candle in candles])
