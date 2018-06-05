e22a8548e4ed31c3c4f85df846ef222b728e45e5
meold <yura.yazupol.official@gmail.com>
2018-06-05 20:45:54 +0300

    Exchange.parse8601 tests

--- python/test/test_exchange_datetime_functions.py
+++ python/test/test_exchange_datetime_functions.py
@@ -31,7 +31,10 @@ class TestExchange(TestCase):
         self.assertEqual(exchange.iso8601([1]), None)
 
     def test_parse_date(self):
-        # self.assertEqual(exchange.parse_date(714862627000), '1992-08-26T20:57:07.000Z')
+        self.assertEqual(exchange.parse_date('1996-04-26 00:00:00'), 830476800000)
+        self.assertEqual(exchange.parse_date('1996-04-26T01:23:47.000Z'), 830481827000)
+        self.assertEqual(exchange.parse_date('1996-13-13 00:00:00'), None)
+
         self.assertEqual(exchange.parse_date('Sun, 18 Mar 2012 05:50:34 GMT'), 1332049834000)
 
         self.assertEqual(exchange.parse_date('GMT'), None)
@@ -43,3 +46,18 @@ class TestExchange(TestCase):
         self.assertEqual(exchange.parse_date({}), None)
         self.assertEqual(exchange.parse_date([]), None)
         self.assertEqual(exchange.parse_date([1]), None)
+
+    def test_parse8601(self):
+        self.assertEqual(exchange.parse8601('1986-04-26T01:23:47.000Z'), 514862627000)
+
+        self.assertEqual(exchange.parse8601('1986-14-26T23:01:47.000Z'), None)
+        self.assertEqual(exchange.parse8601('1986-04-26T25:71:47.000Z'), None)
+
+        self.assertEqual(exchange.parse8601(None), None)
+        self.assertEqual(exchange.parse8601(), None)
+        self.assertEqual(exchange.parse8601(''), None)
+        self.assertEqual(exchange.parse8601('1'), None)
+        self.assertEqual(exchange.parse8601(1), None)
+        self.assertEqual(exchange.parse8601({}), None)
+        self.assertEqual(exchange.parse8601([]), None)
+        self.assertEqual(exchange.parse8601([1]), None)
