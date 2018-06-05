5be41c9431efffee5083ee18b196a5dc79e3734c
meold <yura.yazupol.official@gmail.com>
2018-06-05 20:32:38 +0300

    Exchange.parse_date tests

--- python/test/test_exchange_datetime_functions.py
+++ python/test/test_exchange_datetime_functions.py
@@ -29,3 +29,17 @@ class TestExchange(TestCase):
         self.assertEqual(exchange.iso8601('a'), None)
         self.assertEqual(exchange.iso8601([]), None)
         self.assertEqual(exchange.iso8601([1]), None)
+
+    def test_parse_date(self):
+        # self.assertEqual(exchange.parse_date(714862627000), '1992-08-26T20:57:07.000Z')
+        self.assertEqual(exchange.parse_date('Sun, 18 Mar 2012 05:50:34 GMT'), 1332049834000)
+
+        self.assertEqual(exchange.parse_date('GMT'), None)
+        self.assertEqual(exchange.parse_date('42 GMT'), None)
+
+        self.assertEqual(exchange.parse_date(None), None)
+        self.assertEqual(exchange.parse_date(), None)
+        self.assertEqual(exchange.parse_date(1), None)
+        self.assertEqual(exchange.parse_date({}), None)
+        self.assertEqual(exchange.parse_date([]), None)
+        self.assertEqual(exchange.parse_date([1]), None)
