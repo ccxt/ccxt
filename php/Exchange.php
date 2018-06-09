887b7ee4058a4cb44d8b043b1164002f8cf11b67
Travis CI <travis@travis-ci.org>
2018-06-09 18:38:57 +0000

    1.14.166
    [ci skip]

--- php/Exchange.php
+++ php/Exchange.php
@@ -30,7 +30,7 @@ SOFTWARE.
 
 namespace ccxt;
 
-$version = '1.14.165';
+$version = '1.14.166';
 
 // rounding mode
 const TRUNCATE = 0;
--- php/bitstamp.php
+++ php/bitstamp.php
@@ -380,12 +380,12 @@ class bitstamp extends Exchange {
         $method = 'privatePost' . $this->capitalize ($side);
         $order = array (
             'pair' => $this->market_id($symbol),
-            'amount' => $amount,
+            'amount' => $this->amount_to_precision($symbol, $amount),
         );
         if ($type === 'market')
             $method .= 'Market';
         else
-            $order['price'] = $price;
+            $order['price'] = $this->price_to_precision($symbol, $price);
         $method .= 'Pair';
         $response = $this->$method (array_merge ($order, $params));
         return array (
