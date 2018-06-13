4d5f1b92ca64e3953e0ab397556fadc193b17cdc
Igor Kroitor <igor.kroitor@gmail.com>
2018-06-13 09:41:29 +0300

    removed unnecessary request override from crypton fix #3090

--- js/crypton.js
+++ js/crypton.js
@@ -404,9 +404,4 @@ module.exports = class crypton extends Exchange {
             }
         }
     }
-
-    async request (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
-        let response = await this.fetch2 (path, api, method, params, headers, body);
-        return response['result'];
-    }
 };
