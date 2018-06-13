acca8332835213dbcc2a5915510cde4c40a69552
Igor Kroitor <igor.kroitor@gmail.com>
2018-06-13 19:01:23 +0300

    minor edit for the transpiler

--- js/bibox.js
+++ js/bibox.js
@@ -452,7 +452,9 @@ module.exports = class bibox extends Exchange {
     }
 
     parseOrder (order, market = undefined) {
-        if (Object.keys (order).length < 1) {
+        let keys = Object.keys (order);
+        let numKeys = keys.length;
+        if (numKeys < 1) {
             throw new OrderNotFound ('Order ' + order + ' does not exist on ' + this.id + '.');
         }
         let symbol = undefined;
