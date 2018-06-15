a18e2fbfc8d581d2b00d6a25660a5dcec9694773
Jack Cook <jgacook@gmail.com>
2018-06-15 12:15:26 -0700

    added property to base class allowing for instant transaction exchanges like shapeshift, changelly, etc.

--- js/shapeshift.js
+++ js/shapeshift.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Exchange = require ('./base/Exchange');
+const { ExchangeError } = require ('./base/errors');
 
 module.exports = class shapeshift extends Exchange {
     describe () {
@@ -20,21 +21,38 @@ module.exports = class shapeshift extends Exchange {
                 ],
             },
             'has': {
+                'cancelInstantTransaction': true,
+                'createOrder': false,
+                'createMarketOrder': false,
+                'createLimitOrder': false,
+                'fetchBalance': true,
                 'fetchCurrencies': true,
+                'fetchL2OrderBook': false,
                 'fetchMarkets': true,
                 'fetchTicker': true,
+                'fetchTrades': true,
+                'privateAPI': false,
+                'startInstantTransaction': true,
             },
             'api': {
                 'public': {
                     'get': [
                         'getcoins',
                         'limit/{pair}',
-                        'marketinfo',
+                        'marketinfo/{pair}',
                         'rate/{pair}',
                         'recenttx/{max}',
                         'txStat/{address}',
                     ],
+                    'post': [
+                        'cancelpending',
+                        'shift',
+                    ],
                 },
+            },
+            'requiredCredentials': {
+                'apiKey': false,
+                'secret': false,
             },
         });
     }
@@ -43,6 +61,76 @@ module.exports = class shapeshift extends Exchange {
         const request = '/' + this.implodeParams (path, params);
         const url = this.urls['api'] + request;
         return { 'url': url, 'method': method, 'body': body, 'headers': headers };
+    }
+
+    async startInstantTransaction (symbol, side, withdrawalAddress, affiliateAPIKey, params = {}) {
+        const [base, quote] = symbol.split ('/');
+        const pair = side === 'buy' ?
+            `${quote.toLowerCase ()}_${base.toLowerCase ()}` :
+            `${base.toLowerCase ()}_${quote.toLowerCase ()}`;
+        const request = {
+            'withdrawal': withdrawalAddress,
+            'pair': pair,
+            'returnAddress': withdrawalAddress,
+            'apiKey': affiliateAPIKey,
+        };
+        const response = await this.publicPostShift (this.extend (request, params));
+        if (response.error) throw new ExchangeError (response.error);
+        return {
+            'transactionId': response.deposit,
+            'depositAddress': response.deposit,
+            'info': response,
+        };
+    }
+
+    async cancelInstantTransaction (transactionId, params = {}) {
+        const request = {
+            'address': transactionId,
+        };
+        const response = await this.publicPostCancelpending (this.extend (request, params));
+        if (response.error) throw new ExchangeError (response.error);
+        return {
+            'success': true,
+            'info': response,
+        };
+    }
+
+    async fetchOrderBook (symbol, limit = undefined, params = {}) {
+        const [base, quote] = symbol.split ('/');
+        const bidSymbol = `${base.toLowerCase ()}_${quote.toLowerCase ()}`;
+        const askSymbol = `${quote.toLowerCase ()}_${base.toLowerCase ()}`;
+        const [bidResponse, askResponse] = await Promise.all ([
+            this.publicGetMarketinfoPair ({ 'pair': bidSymbol }),
+            this.publicGetMarketinfoPair ({ 'pair': askSymbol }),
+        ]);
+        return {
+            'timestamp': new Date ().getTime (),
+            'bids': [
+                [bidResponse.rate, bidResponse.limit],
+            ],
+            'asks': [
+                [(1.0 / askResponse.rate), (askResponse.rate * askResponse.limit)],
+            ],
+        };
+    }
+
+    async calculateFee (symbol, type = undefined, side, amount = undefined, price = undefined, takerOrMaker = 'taker', params = {}) {
+        await this.loadMarkets ();
+        const [base, quote] = symbol.split ('/');
+        let fee = undefined;
+        if (side === 'sell') {
+            const { info } = this.markets[symbol];
+            fee = info.minerFee;
+        } else {
+            const { info } = this.markets[`${quote}/${base}`];
+            fee = info.minerFee / info.rate;
+        }
+        return {
+            'type': undefined,
+            'currency': quote,
+            'rate': undefined,
+            'cost': fee,
+        };
     }
 
     // TODO: figure out how to get the other side of the market in here
@@ -116,7 +204,7 @@ module.exports = class shapeshift extends Exchange {
     }
 
     async fetchMarkets () {
-        const marketsResponse = await this.publicGetMarketinfo ({ 'pair': '' });
+        const marketsResponse = await this.publicGetMarketinfoPair ({ 'pair': '' });
         const numMarkets = marketsResponse.length;
         const result = new Array (numMarkets);
         for (let i = 0; i < numMarkets; i++) {
