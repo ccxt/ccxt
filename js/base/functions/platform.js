0099044710ad54eae4857e811d6b5e7465f9dbaf
Igor Kroitor <igor.kroitor@gmail.com>
2018-06-18 15:25:35 +0300

    reverted isNode detection schema refix #955 #3171 #2852

--- js/base/functions/platform.js
+++ js/base/functions/platform.js
@@ -1,12 +1,28 @@
 "use strict";
 
-/*  ------------------------------------------------------------------------ */
+// ----------------------------------------------------------------------------
+// There's been a lot of messing with this code...
+// The problem is to satisfy the following requirements:
+
+const isBrowser = typeof window !== 'undefined'
+
+const isElectron = typeof process !== 'undefined' &&
+                   typeof process.versions !== 'undefined' &&
+                   typeof process.versions.electron !== 'undefined'
+
+const isWebWorker = typeof WorkerGlobalScope !== 'undefined' && (self instanceof WorkerGlobalScope)
+
+const isWindows = typeof process !== 'undefined' && process.platform === "win32"
+
+const isNode = !(isBrowser || isWebWorker)
+
+// ----------------------------------------------------------------------------
 
 module.exports = {
 
-    isNode: typeof module !== 'undefined' && typeof module.exports !== 'undefined'
-
-    , isWindows: (typeof process !== 'undefined') ? process.platform === "win32" : false
+    isBrowser,
+    isElectron,
+    isWebWorker,
+    isNode,
+    isWindows,
 }
-
-/*  ------------------------------------------------------------------------ */
