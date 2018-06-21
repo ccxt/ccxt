9a1106e58c97eadb4852cf8d019ec0786407ce64
Luis Fernando Pardo <lfern70@gmail.com>
2018-06-21 10:35:24 +0200

    add async-orderbook examples in php and py

---
+++ examples/php/async-orderbook.php
@@ -0,0 +1,66 @@
+<?php 
+$root = dirname (dirname (dirname (__FILE__)));
+
+include $root . '/ccxt.php';
+include $root . '/php/Exchange.php';
+include $root . '/php/cex.php';
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+if ($argc > 5) {
+    $id = $argv[1];
+    $apiKey = $argv[2];
+    $secret = $argv[3];
+    $limit = intval ($argv[4]);
+    $symbols = array();
+    for ($i = 5; $i < $argc; $i++) {
+        $symbols[] = $argv[$i];
+    }
+    
+} else {
+    echo 'php '. __FILE__ . ' exchange-id apikey secret limit symbol ...\n';
+    exit(-1);
+}
+
+$loop = React\EventLoop\Factory::create();
+
+$cl = 'ccxt\\' . $id;
+
+$exchange = new  $cl (array(
+    'apiKey' => $apiKey,
+    'secret' => $secret,
+    'enableRateLimit' => true,
+    'verbose' => false,
+    'timeout' => 5 * 1000,
+    'react_loop' => $loop
+));
+
+$exchange->on ('err', function ($err, $conxid) use ($exchange){
+    echo ($err);
+    $exchange->asyncClose();
+});
+
+$exchange->on ('ob', function ($symbol, $ob) {
+    echo ("ob received from: " . $symbol);
+    // print_r ($ob);
+    echo ("\n");
+});
+
+foreach ($symbols as $symbol) {
+    echo ("subscribe: " . $symbol . "\n");
+    $exchange->async_subscribe ('ob', $symbol);
+    echo ("subscribed: " . $symbol . "\n");
+    $exchange->async_fetch_order_book($symbol, $limit);
+    echo ("ob fetched: " . $symbol . "\n");
+    // print_r ($ob);
+    Clue\React\Block\sleep(5, $loop);
+}
+foreach ($symbols as $symbol) {
+    echo ("unsubscribe: " . $symbol . "\n");
+    $exchange->async_unsubscribe ('ob', $symbol);
+    echo ("unsubscribed: " . $symbol . "\n");
+    Clue\React\Block\sleep(5, $loop);
+}
+
+
+$loop->run();
---
+++ examples/py/async-orderbook.py
@@ -0,0 +1,82 @@
+import os
+import sys
+import pprint
+import traceback
+
+pp = pprint.PrettyPrinter(depth=6)
+
+root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root + '/python')
+# import ccxt  # noqa: E402
+import ccxt.async as ccxt  # noqa: E402
+import asyncio  # noqa: E402
+
+loop = asyncio.get_event_loop()
+
+
+async def main():
+    if len(sys.argv) <= 5:
+        print('python ' + __file__ + ' exchange apikey secret limit symbol ...')
+        sys.exit(-1)
+
+    exchange_id = sys.argv[1]
+    apiKey = sys.argv[2]
+    secret = sys.argv[3]
+    limit = int(sys.argv[4])
+    symbols = []
+    for i in range(5, len(sys.argv)):
+        symbols.append(sys.argv[i])
+
+    exchange = getattr(ccxt, exchange_id)({
+        "apiKey": apiKey,
+        "secret": secret,
+        "enableRateLimit": True,
+        'verbose': True,
+        'timeout': 5 * 1000
+    })
+
+    @exchange.on('err')
+    def async_error(err, conxid):  # pylint: disable=W0612
+        print(type(err).__name__ + ":" + str(err))
+        traceback.print_tb(err.__traceback__)
+        traceback.print_stack()
+        loop.stop()
+
+    @exchange.on('ob')
+    def async_ob(symbol, ob):  # pylint: disable=W0612
+        print("ob received from: " + symbol)
+        sys.stdout.flush()
+        # pp.pprint(ob)
+
+    sys.stdout.flush()
+
+    for j in range(2):
+        for i in range(len(symbols)):
+            symbol = symbols[i]
+            print("subscribe: " + symbol)
+            sys.stdout.flush()
+            await exchange.async_subscribe('ob', symbol)
+            print("subscribed: " + symbol)
+            sys.stdout.flush()
+            ob = await exchange.async_fetch_order_book(symbol, limit)  # noqa: F841 pylint: disable=W0612
+            print("ob fetched: " + symbol)
+            # print(ob)
+            sys.stdout.flush()
+            await asyncio.sleep(5)
+
+        for i in range(len(symbols)):
+            symbol = symbols[i]
+            print("unsubscribe: " + symbol)
+            sys.stdout.flush()
+            await exchange.async_unsubscribe('ob', symbol)
+            print("unsubscribed: " + symbol)
+            sys.stdout.flush()
+            await asyncio.sleep(5)
+
+    await exchange.close()
+
+loop.run_until_complete(main())
+# loop.run_forever()
+# loop.stop()
+# loop.close()
+print("after complete")
