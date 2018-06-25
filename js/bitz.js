186623beff7394424d54e6fe7cdfca0ac15d87dd
Igor Kroitor <igor.kroitor@gmail.com>
2018-06-25 22:12:19 +0300

    bitz rateLimit = 2000 fix #3248

--- js/bitz.js
+++ js/bitz.js
@@ -13,7 +13,7 @@ module.exports = class bitz extends Exchange {
             'id': 'bitz',
             'name': 'Bit-Z',
             'countries': 'HK',
-            'rateLimit': 1000,
+            'rateLimit': 2000,
             'version': 'v1',
             'userAgent': this.userAgents['chrome'],
             'has': {
