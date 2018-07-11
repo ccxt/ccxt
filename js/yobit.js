13306b11060a33abee0c8418da7fb31a2bb7fa4d
Igor Kroitor <igor.kroitor@gmail.com>
2018-07-11 22:36:38 +0300

    minor transpiler fix for yobit #3372

--- js/yobit.js
+++ js/yobit.js
@@ -240,7 +240,8 @@ module.exports = class yobit extends liqui {
                             throw new DDoSProtection (this.id + ' ' + this.json (response));
                         } else if ((response['error_log'] === 'not available') || (response['error_log'] === 'external service unavailable')) {
                             throw new DDoSProtection (this.id + ' ' + this.json (response));
-                        } else if (response['error_log'] === 'Total transaction amount') { // eg {"success":0,"error":"Total transaction amount is less than minimal total: 0.00010000"}
+                        } else if (response['error_log'] === 'Total transaction amount') {
+                            // eg {"success":0,"error":"Total transaction amount is less than minimal total: 0.00010000"}
                             throw new InvalidOrder (this.id + ' ' + this.json (response));
                         }
                     }
