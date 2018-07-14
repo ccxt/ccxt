d9fd8f22de9a08e2b2cf1dc70ccd3fe72e831553
Travis CI <travis@travis-ci.org>
2018-07-14 13:52:26 +0000

    1.16.20
    [ci skip]

--- php/Exchange.php
+++ php/Exchange.php
@@ -30,7 +30,7 @@ SOFTWARE.
 
 namespace ccxt;
 
-$version = '1.16.19';
+$version = '1.16.20';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -46,7 +46,7 @@ const PAD_WITH_ZERO = 1;
 
 class Exchange {
 
-    const VERSION = '1.16.19';
+    const VERSION = '1.16.20';
 
     public static $exchanges = array (
         '_1broker',
