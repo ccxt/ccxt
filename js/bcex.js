8e141a3e996b861e06d6cd764f53e33d91031277
Josh Varty <joshvarty@gmail.com>
2018-07-16 00:41:22 -0400

    Retrieve list of currencies for fetchMarkets()

--- js/bcex.js
+++ js/bcex.js
@@ -22,21 +22,21 @@ module.exports = class bcex extends Exchange {
                 'cancelOrder': true,
                 'fetchTicker': true,
                 'fetchTickers': false,
-                'fetchOrder':true,
-                'fetchOrders':true,
-                'fetchOpenOrders':true,
+                'fetchOrder': true,
+                'fetchOrders': true,
+                'fetchOpenOrders': true,
             },
             'urls': {
                 'logo': 'https://www.bcex.top/images/bcex_logo0.png',
                 'api': 'https://www.bcex.top',
                 'www': 'https://www.bcex.ca',
                 'doc': 'https://www.bcex.ca/api_market/market/',
-                'fees': 'http://bcex.udesk.cn/hc/articles/57085',//TODO
+                'fees': 'http://bcex.udesk.cn/hc/articles/57085',
             },
             'api': {
                 'public': {
                     'get': [
-                        'Api_Market/markets',
+                        'Api_Market/getPriceList'
                     ],
                 },
                 'private': {
@@ -51,40 +51,70 @@ module.exports = class bcex extends Exchange {
                 },
             },
             'fees': {
-                //TODO
             },
         });
     }
 
-    async fetchMarkets() {
-        //TODO
+    async fetchMarkets () {
+        let response = await this.publicGetApiMarketGetPriceList ();
+        return 0;
     }
 
-    async fetchBalance(params = {}) {
-        //TODO
+    async fetchBalance (params = {}) {
+        return 0;
     }
 
-    async fetchOrderBook(symbol, limit = undefined, params = {}) {
-        //TODO
+    async fetchOrderBook (symbol, limit = undefined, params = {}) {
+        return 0;
     }
 
     async fetchMyTrades (symbol = undefined, since = undefined, limit = undefined, params = {}) {
-        //TODO
+        return 0;
     }
 
     async fetchOrder (id, symbol = undefined, params = {}) {
-        //TODO
+        return 0;
     }
 
     async fetchOpenOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
-        //TODO
+        return 0;
     }
 
     async createOrder (symbol, type, side, amount, price = undefined, params = {}) {
-        //TODO
+        return 0;
     }
 
     async cancelOrder (id, symbol = undefined, params = {}) {
-        //TODO
+        return 0;
+    }
+
+    sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
+        let request = '/' + this.implodeParams(path, params);
+        let query = this.omit(params, this.extractParams(path));
+        if (method === 'GET') {
+            if (Object.keys(query).length)
+                request += '?' + this.urlencode(query);
+        }
+        let url = this.urls['api'] + request;
+        if (api === 'private') {
+            this.checkRequiredCredentials();
+            let timestamp = this.milliseconds().toString();
+            let payload = '';
+            if (method !== 'GET') {
+                if (Object.keys(query).length) {
+                    body = this.json(query);
+                    payload = body;
+                }
+            }
+            let what = timestamp + method + request + payload;
+            let signature = this.hmac(this.encode(what), this.encode(this.secret), 'sha256');
+            headers = {
+                'CRYPTON-APIKEY': this.apiKey,
+                'CRYPTON-SIGNATURE': signature,
+                'CRYPTON-TIMESTAMP': timestamp,
+                'Content-Type': 'application/json',
+            };
+        }
+        return { 'url': url, 'method': method, 'body': body, 'headers': headers };
     }
 };
