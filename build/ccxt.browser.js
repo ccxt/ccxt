3d339ac61fa8d13286d03af955bec38685ea4372
Travis CI <travis@travis-ci.org>
2018-07-20 09:21:43 +0000

    1.16.75
    [ci skip]

--- build/ccxt.browser.js
+++ build/ccxt.browser.js
@@ -45,7 +45,7 @@ const Exchange  = require ('./js/base/Exchange')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.16.74'
+const version = '1.16.75'
 
 Exchange.ccxtVersion = version
 
@@ -13344,7 +13344,9 @@ module.exports = class bittrex extends Exchange {
         let request = {};
         request[orderIdField] = id;
         let response = await this.marketGetCancel (this.extend (request, params));
-        return this.parseOrder (response);
+        return this.extend (this.parseOrder (response), {
+            'status': 'canceled',
+        });
     }
 
     parseSymbol (id) {
