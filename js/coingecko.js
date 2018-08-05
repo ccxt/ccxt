4efeec9584b2b73f501587076236106902577173
Dominic Wong <dominic.wong.617@gmail.com>
2018-08-05 19:37:05 +0800

    Use current_price returned from coingecko for "average"

--- js/coingecko.js
+++ js/coingecko.js
@@ -217,7 +217,7 @@ module.exports = class coingecko extends Exchange {
             'percentage': parseFloat (market['price_change_percentage_24h']),
             'baseVolume': undefined,
             'quoteVolume': market['total_volume'],
-            'average': undefined,
+            'average': market['current_price'],
         };
     }
 
