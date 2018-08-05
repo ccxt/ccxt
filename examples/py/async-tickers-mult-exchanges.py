5ceada9f120652720ec2926a555b7d4bbabd6d4a
Federico Caccia <federicoagustincaccia@gmail.com>
2018-08-05 15:11:22 -0300

    PEP8 guidelines compliant
    syntax changes

--- examples/py/async-tickers-mult-exchanges.py
+++ examples/py/async-tickers-mult-exchanges.py
@@ -10,33 +10,37 @@ import sys
 root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(root + '/python')
 
+
 def sync_client(exchange):
-  client = getattr(ccxt, exchange)()
-  tickers = client.fetch_tickers()
-  return tickers
+    client = getattr(ccxt, exchange)()
+    tickers = client.fetch_tickers()
+    return tickers
+
 
 async def async_client(exchange):
-  client = getattr(ccxta, exchange)()
-  tickers = await client.fetch_tickers()
-  await client.close()
-  return tickers
+    client = getattr(ccxta, exchange)()
+    tickers = await client.fetch_tickers()
+    await client.close()
+    return tickers
 
-async def fetch_multiple_tickers(exchanges):
-  input_coroutines = [async_client(exchange) for exchange in exchanges]
-  tickers = await asyncio.gather(*input_coroutines, return_exceptions=True)
-  return tickers
+
+async def multi_tickers(exchanges):
+    input_coroutines = [async_client(exchange) for exchange in exchanges]
+    tickers = await asyncio.gather(*input_coroutines, return_exceptions=True)
+    return tickers
+
 
 if __name__ == '__main__':
-  
-  # Consider review request rate limit in the methods you call
-  exchanges = ["coinex", "bittrex", "bitfinex", "poloniex", "hitbtc"]
-  
-  tic = time.time()
-  a = asyncio.get_event_loop().run_until_complete(fetch_multiple_tickers(exchanges))
-  print("async call spend:", time.time()-tic)
 
-  time.sleep(1)
-  
-  tic = time.time()
-  a = [sync_client(exchange) for exchange in exchanges]
-  print("sync call spend:", time.time()-tic)
+    # Consider review request rate limit in the methods you call
+    exchanges = ["coinex", "bittrex", "bitfinex", "poloniex", "hitbtc"]
+
+    tic = time.time()
+    a = asyncio.get_event_loop().run_until_complete(multi_tickers(exchanges))
+    print("async call spend:", time.time()-tic)
+
+    time.sleep(1)
+
+    tic = time.time()
+    a = [sync_client(exchange) for exchange in exchanges]
+    print("sync call spend:", time.time()-tic)
