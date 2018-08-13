f5d588b5a74e48a69db5983b745866148136ac14
Ka.Z <imhazige@gmail.com>
2018-08-13 10:43:34 +0800

    add huobipro websocket raw example

--- examples/js/websocket-okex.js
+++ examples/js/websocket-okex.js
@@ -1,24 +1,24 @@
 var url = require('url');
 var WebSocket = require('ws');
-var HttpsProxyAgent = require('https-proxy-agent');
+// var HttpsProxyAgent = require('https-proxy-agent');
 
 // HTTP/HTTPS proxy to connect to
-var proxy = 'http://localhost:7070';
-console.log('using proxy server %j', proxy);
+// var proxy = 'http://localhost:7070';
+// console.log('using proxy server %j', proxy);
 
 // WebSocket endpoint for the proxy to connect to
-var endpoint = 'wss://okexcomreal.bafang.com:10440/websocket';
-// var endpoint = 'wss://real.okex.com:10441/websocket';
-var parsed = url.parse(endpoint);
-console.log('attempting to connect to WebSocket %j', endpoint);
+// var endpoint = 'wss://okexcomreal.bafang.com:10440/websocket';
+var endpoint = 'wss://real.okex.com:10441/websocket';
+// var parsed = url.parse(endpoint);
+// console.log('attempting to connect to WebSocket %j', endpoint);
 
 // create an instance of the `HttpsProxyAgent` class with the proxy server information
-var options = url.parse(proxy);
+// var options = url.parse(proxy);
 
-var agent = new HttpsProxyAgent(options);
+// var agent = new HttpsProxyAgent(options);
 
 // finally, initiate the WebSocket connection
-var socket = new WebSocket(endpoint, { agent });
+var socket = new WebSocket(endpoint);
 
 socket.on('open', function() {
   console.log('"open" event!');
