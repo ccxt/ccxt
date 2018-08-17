2f830d8fda44a4bc7fe54f964cef5ed9f8a44cff
rkj <romainknezevic@gmail.com>
2018-08-17 10:18:28 +0200

    allow more formats for JSONTime unmarshaller + tests

--- go/util/time.go
+++ go/util/time.go
@@ -1,18 +1,21 @@
 package util
 
 import (
+	"encoding/json"
 	"fmt"
 	"strconv"
 	"strings"
 	"time"
 )
 
-// JSONTime allows JSON marshalling from time.Time to string unix epoch,
-// and unmarshalling the other way around.
+// JSONTime is a time.Time that is JSON marshalled to unix timestamp in ms.
+// For unmarshalling JSONTime is flexible, it allows for unix timestamps in ms
+// or seconds depending on int value, if that fails it tries to
+// unmarshal as a time.Time.
 type JSONTime time.Time
 
 func (t JSONTime) MarshalJSON() ([]byte, error) {
-	return []byte(strconv.FormatInt(time.Time(t).Unix()*1000, 10)), nil
+	return []byte(strconv.FormatInt(time.Time(t).UnixNano()/1e6, 10)), nil
 }
 
 func (t *JSONTime) UnmarshalJSON(s []byte) (err error) {
@@ -21,9 +24,23 @@ func (t *JSONTime) UnmarshalJSON(s []byte) (err error) {
 
 	q, err := strconv.ParseInt(r, 10, 64)
 	if err != nil {
-		return err
+		// attempt time.Time unmarshalling
+		var tt time.Time
+		err2 := json.Unmarshal(s, &tt)
+		if err2 == nil {
+			*(*time.Time)(t) = tt
+			return nil
+		}
+		return fmt.Errorf("can't unmarshal \"%s\" to JSONTime: %s", s, err)
 	}
-	*(*time.Time)(t) = time.Unix(q/1000, 0)
+
+	if q > 1e11 {
+		// 1e11 is year 5138, we assume we're dealing with millisecs
+		*(*time.Time)(t) = time.Unix(q/1000, (q%1000)*1e6)
+	} else {
+		// seconds timestamp
+		*(*time.Time)(t) = time.Unix(q, 0)
+	}
 	return
 }
 
