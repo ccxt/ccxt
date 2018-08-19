e76c4e17e39cae5badfedb10d48c7675722febc3
Igor Kroitor <igor.kroitor@gmail.com>
2018-08-19 10:00:47 +0300

    bibox proper refix parseOrder safeFloat2 for cost

--- js/bibox.js
+++ js/bibox.js
@@ -483,10 +483,7 @@ module.exports = class bibox extends Exchange {
         let average = this.safeFloat (order, 'deal_price');
         let filled = this.safeFloat (order, 'deal_amount');
         let amount = this.safeFloat (order, 'amount');
-        let cost = this.safeFloat (order, 'deal_money');
-        if (cost === 0) {
-            cost = this.safeFloat (order, 'money');
-        }
+        let cost = this.safeFloat2 (order, 'deal_money', 'money');
         let remaining = undefined;
         if (typeof filled !== 'undefined') {
             if (typeof amount !== 'undefined')
