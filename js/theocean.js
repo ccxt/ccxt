161e307cedc01203ff3f7f82409b3ae233fc018e
Igor Kroitor <igor.kroitor@gmail.com>
2018-08-19 07:10:30 +0300

    theocean metainfo

--- js/theocean.js
+++ js/theocean.js
@@ -27,6 +27,7 @@ module.exports = class theocean extends Exchange {
                 'fetchTickers': true,
                 'fetchOHLCV': false,
                 'fetchOrder': true,
+                'fetchOrders': true,
             },
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/43103756-d56613ce-8ed7-11e8-924e-68f9d4bcacab.jpg',
