5171e1b893536cd1e25bd2e68b4f16b6cd460a15
ZacharyATanenbaum <ZachTanenbaum@gmail.com>
2018-08-22 16:55:14 -0400

    Updated fee urls for Coinbase Pro

--- js/coinbasepro.js
+++ js/coinbasepro.js
@@ -18,8 +18,8 @@ module.exports = class coinbasepro extends gdax {
                 'www': 'https://pro.coinbase.com/',
                 'doc': 'https://docs.pro.coinbase.com/',
                 'fees': [
-                    'https://www.gdax.com/fees',
-                    'https://support.gdax.com/customer/en/portal/topics/939402-depositing-and-withdrawing-funds/articles',
+                    'https://docs.pro.coinbase.com/#fees',
+                    'https://support.pro.coinbase.com/customer/en/portal/articles/2945310-fees',
                 ],
             },
         });
