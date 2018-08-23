ac5453ca871813506ce46f91f2fbf0f5e91bd18f
ZacharyATanenbaum <ZachTanenbaum@gmail.com>
2018-08-23 10:27:12 -0400

    Added information to Coinbase Prime

--- js/coinbaseprime.js
+++ js/coinbaseprime.js
@@ -12,13 +12,14 @@ module.exports = class coinbaseprime extends gdax {
             'id': 'coinbaseprime',
             'name': 'Coinbase Prime',
             'urls': {
-                'logo': 'https://user-images.githubusercontent.com/1294454/41764625-63b7ffde-760a-11e8-996d-a6328fa9347a.jpg',
-                'api': 'https://api.pro.coinbase.com',
-                'www': 'https://pro.coinbase.com/',
-                'doc': 'https://docs.gdax.com',
+                'test': 'https://api-public.sandbox.prime.coinbase.com',
+                'logo': 'https://ph-files.imgix.net/58ceb872-d4a9-4cf2-99ea-def0fceb13d7?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop',
+                'api': 'https://api.prime.coinbase.com',
+                'www': 'https://prime.coinbase.com/',
+                'doc': 'https://docs.prime.coinbase.com/',
                 'fees': [
-                    'https://www.gdax.com/fees',
-                    'https://support.gdax.com/customer/en/portal/topics/939402-depositing-and-withdrawing-funds/articles',
+                    'https://docs.prime.coinbase.com/#fees',
+                    'https://support.prime.coinbase.com/customer/en/portal/articles/2945629-fees?b_id=17475',
                 ],
             },
         });
