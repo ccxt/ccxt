addf8f73c1bdd7d5eb379a328805e2faadc7904f
Travis CI <travis@travis-ci.org>
2018-08-25 01:21:41 +0000

    1.17.181
    [ci skip]

--- php/Exchange.php
+++ php/Exchange.php
@@ -34,7 +34,7 @@ use kornrunner\Eth;
 use kornrunner\Secp256k1;
 use kornrunner\Solidity;
 
-$version = '1.17.180';
+$version = '1.17.181';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -50,7 +50,7 @@ const PAD_WITH_ZERO = 1;
 
 class Exchange {
 
-    const VERSION = '1.17.180';
+    const VERSION = '1.17.181';
 
     public static $eth_units = array (
         'wei'        => '1',
