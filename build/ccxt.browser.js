5cfd8bc8145f4d26992b3657704ae6db521b5813
Travis CI <travis@travis-ci.org>
2018-08-27 19:06:40 +0000

    1.17.195
    [ci skip]

--- build/ccxt.browser.js
+++ build/ccxt.browser.js
@@ -45,7 +45,7 @@ const Exchange  = require ('./js/base/Exchange')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.17.194'
+const version = '1.17.195'
 
 Exchange.ccxtVersion = version
 
@@ -20119,6 +20119,7 @@ module.exports = class cobinhood extends Exchange {
                 'fetchWithdrawals': true,
                 'withdraw': false,
                 'fetchMyTrades': true,
+                'editOrder': true,
             },
             'requiredCredentials': {
                 'apiKey': true,
@@ -20210,6 +20211,9 @@ module.exports = class cobinhood extends Exchange {
                         'wallet/deposit_addresses',
                         'wallet/withdrawal_addresses',
                         'wallet/withdrawals',
+                    ],
+                    'put': [
+                        'trading/orders/{order_id}',
                     ],
                     'delete': [
                         'trading/orders/{order_id}',
@@ -20603,6 +20607,17 @@ module.exports = class cobinhood extends Exchange {
         let id = order['id'];
         this.orders[id] = order;
         return order;
+    }
+
+    async editOrder (id, symbol, type, side, amount, price, params = {}) {
+        let response = await this.privatePutTradingOrdersOrderId (this.extend ({
+            'order_id': id,
+            'price': this.priceToPrecision (symbol, price),
+            'size': this.amountToString (symbol, amount),
+        }, params));
+        return this.parseOrder (this.extend (response, {
+            'id': id,
+        }));
     }
 
     async cancelOrder (id, symbol = undefined, params = {}) {
