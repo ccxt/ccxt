b2509df4bf615ff901737fe2feae36a4b7299dc3
Igor Kroitor <igor.kroitor@gmail.com>
2018-09-03 05:40:54 +0300

    binance minor formatting edits

--- js/binance.js
+++ js/binance.js
@@ -275,7 +275,7 @@ module.exports = class binance extends Exchange {
                 '-1021': InvalidNonce, // 'your time is ahead of server'
                 '-1022': AuthenticationError, // {"code":-1022,"msg":"Signature for this request is not valid."}
                 '-1100': InvalidOrder, // createOrder(symbol, 1, asdf) -> 'Illegal characters found in parameter 'price'
-                '-1104': ExchangeError, // {"code":-1104,"msg":"Not all sent parameters were read; read '8' parameter(s) but was sent '9'."}
+                '-1104': ExchangeError, // Not all sent parameters were read, read 8 parameters but was sent 9
                 '-1128': ExchangeError, // {"code":-1128,"msg":"Combination of optional parameters invalid."}
                 '-2010': ExchangeError, // generic error code for createOrder -> 'Account has insufficient balance for requested action.', {"code":-2010,"msg":"Rest API trading is not enabled."}, etc...
                 '-2011': OrderNotFound, // cancelOrder(1, 'BTC/USDT') -> 'UNKNOWN_ORDER'
