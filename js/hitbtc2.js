f52e6fae990d58c0d2883a9ff3b7f2dc7c612865
Igor Kroitor <igor.kroitor@gmail.com>
2018-09-11 03:50:28 +0300

    hitbtc2 lots reverted to 0 until negative precision is supported everywhere

--- js/hitbtc2.js
+++ js/hitbtc2.js
@@ -564,9 +564,9 @@ module.exports = class hitbtc2 extends hitbtc {
             let step = this.safeFloat (market, 'tickSize');
             let precision = {
                 'price': this.precisionFromString (market['tickSize']),
+                'amount': this.precisionFromString (market['quantityIncrement']),
                 // FIXME: for lots > 1 the following line returns 0
-                // 'amount': this.precisionFromString (market['quantityIncrement']),
-                'amount': -1 * Math.log10 (lot),
+                // 'amount': -1 * parseInt (Math.log10 (lot)),
             };
             let taker = this.safeFloat (market, 'takeLiquidityRate');
             let maker = this.safeFloat (market, 'provideLiquidityRate');
