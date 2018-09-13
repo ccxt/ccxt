9a8a026a1a05e31ee1f347f0aee725558a26e9fa
Travis CI <travis@travis-ci.org>
2018-09-13 22:21:28 +0000

    1.17.289
    [ci skip]

--- build/ccxt.browser.js
+++ build/ccxt.browser.js
@@ -45,7 +45,7 @@ const Exchange  = require ('./js/base/Exchange')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.17.288'
+const version = '1.17.289'
 
 Exchange.ccxtVersion = version
 
@@ -1879,6 +1879,9 @@ module.exports = class Exchange {
         this.trades      = {}
         this.transactions = {}
 
+        this.enableLastJsonResponse = true
+        this.enableLastHttpResponse = true
+        this.enableLastResponseHeaders = true
         this.last_http_response    = undefined
         this.last_json_response    = undefined
         this.last_response_headers = undefined
@@ -2198,9 +2201,17 @@ module.exports = class Exchange {
 
             let responseHeaders = this.getResponseHeaders (response)
 
-            this.last_response_headers = responseHeaders
-            this.last_http_response = responseBody // FIXME: for those classes that haven't switched to handleErrors yet
-            this.last_json_response = json         // FIXME: for those classes that haven't switched to handleErrors yet
+            if (this.enableLastResponseHeaders) {
+                this.last_response_headers = responseHeaders
+            }
+
+            if (this.enableLastHttpResponse) {
+                this.last_http_response = responseBody // FIXME: for those classes that haven't switched to handleErrors yet
+            }
+
+            if (this.enableLastJsonResponse) {
+                this.last_json_response = json         // FIXME: for those classes that haven't switched to handleErrors yet
+            }
 
             if (this.verbose)
                 console.log ("handleRestResponse:\n", this.id, method, url, response.status, response.statusText, "\nResponse:\n", responseHeaders, "\n", responseBody, "\n")
