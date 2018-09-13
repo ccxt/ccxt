20dcd432d92048c077f5a034d2be6a40e877a964
Travis CI <travis@travis-ci.org>
2018-09-13 04:42:41 +0000

    1.17.282
    [ci skip]

--- ccxt.js
+++ ccxt.js
@@ -37,7 +37,7 @@ const Exchange  = require ('./js/base/Exchange')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.17.281'
+const version = '1.17.282'
 
 Exchange.ccxtVersion = version
 
--- python/ccxt/btcalpha.py
+++ python/ccxt/btcalpha.py
@@ -237,13 +237,17 @@ class btcalpha (Exchange):
         for i in range(0, len(balances)):
             balance = balances[i]
             currency = self.common_currency_code(balance['currency'])
-            account = {
-                'free': float(balance['balance']),
-                'used': float(balance['reserve']),
-                'total': 0.0,
+            used = self.safe_float(balance, 'reserve')
+            total = self.safe_float(balance, 'balance')
+            free = None
+            if used is not None:
+                if total is not None:
+                    free = total - used
+            result[currency] = {
+                'free': free,
+                'used': used,
+                'total': total,
             }
-            account['total'] = self.sum(account['free'], account['used'])
-            result[currency] = account
         return self.parse_balance(result)
 
     def parse_order(self, order, market=None):
