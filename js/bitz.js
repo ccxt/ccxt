80225bb6ac7c3a3611e94431a7e6a2677fd49daf
Igor Kroitor <igor.kroitor@gmail.com>
2018-09-21 16:34:10 +0300

    bitz BOX → BOX Token fix #3881

--- js/bitz.js
+++ js/bitz.js
@@ -152,6 +152,9 @@ module.exports = class bitz extends Exchange {
                 'lastNonceTimestamp': 0,
             },
             'commonCurrencies': {
+                // https://github.com/ccxt/ccxt/issues/3881
+                // https://support.bit-z.pro/hc/en-us/articles/360007500654-BOX-BOX-Token-
+                'BOX': 'BOX Token',
                 'XRB': 'NANO',
                 'PXC': 'Pixiecoin',
             },
