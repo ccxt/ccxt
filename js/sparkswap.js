3b064e69a02e5daef950d30d64ed14094e1bd2ae
Danny Paz <dannypaz@Dannys-MacBook-Pro.local>
2018-09-28 13:17:48 -0700

    added sparkswap to ccxt

---
+++ js/sparkswap.js
@@ -0,0 +1,140 @@
+'use strict';
+
+// ----------------------------------------------------------------------------
+
+const Exchange = require ('./base/Exchange');
+const { ExchangeError } = require ('./base/errors');
+
+// ----------------------------------------------------------------------------
+
+module.exports = class sparkswap extends Exchange {
+    describe () {
+        return this.deepExtend (super.describe (), {
+            'id': 'sparkswap',
+            'name': 'sparkswap',
+            'countries': [ 'US' ],
+            // 10k calls per hour
+            'rateLimit': 400,
+            'enableRateLimit': true,
+            'version': 'v1',
+            'userAgent': this.userAgents['chrome'],
+            'headers': {},
+            'timeout': 10000, // number in milliseconds
+            'verbose': false, // boolean, output error details
+            'requiredCredentials': {
+                'uid': true,
+                'password': true,
+            },
+            'has': {
+                // We do not support CORS w/ headers
+                'CORS': false,
+                // Sparkswap exchange has both private and public APIs
+                'private': false,
+                'public': false,
+                // Methods that we support for sparkswap
+                'cancelOrder': 'emulated',
+                'createDepositAddress': 'emulated',
+                'createOrder': 'emulated',
+                'deposit': 'emulated',
+                'fetchBalance': 'emulated',
+                'fetchMarkets': 'emulated',
+                'fetchOrder': 'emulated',
+                'fetchOrderBook': 'emulated',
+                'fetchOrders': 'emulated',
+                'fetchTicker': 'emulated',
+                'fetchTrades': 'emulated',
+                'withdraw': 'emulated',
+                'commit': 'emulated',
+                'release': 'emulated',
+                // Unsupported methods for sparkswap
+                'fetchClosedOrders': false,
+                'fetchCurrencies': false,
+                'fetchDepositAddress': false,
+                'fetchMyTrades': false,
+                'fetchOHLCV': false,
+                'fetchOpenOrders': false,
+                'fetchTickers': false,
+                'fetchBidsAsks': false,
+                'fetchTransactions': false,
+                'fetchDeposits': false,
+                'fetchWithdrawals': false,
+            },
+            'urls': {
+                'logo': 'https://user-images.githubusercontent.com/1294454/40811661-b6eceae2-653a-11e8-829e-10bfadb078cf.jpg',
+                'www': 'https://www.sparkswap.com',
+                'doc': 'https://docs.sparkswap.com',
+            },
+            'api': {
+                'public': {
+                    'get': [],
+                },
+                'private': {
+                    'get': [],
+                    'post': [],
+                    'put': [],
+                    'delete': [],
+                },
+            },
+            'exceptions': {},
+            'markets': {},
+            'options': {},
+        });
+    }
+
+    async cancelOrder (orderId, symbol = undefined, params = {}) {
+        throw new ExchangeError ('Not Implemented');
+    }
+
+    async createDepositAddress () {
+        throw new ExchangeError ('Not Implemented');
+    }
+
+    async createOrder (symbol, type, side, amount, price = undefined, params = {}) {
+        throw new ExchangeError ('Not Implemented');
+    }
+
+    async deposit () {
+        throw new ExchangeError ('Not Implemented');
+    }
+
+    async fetchBalance (params = {}) {
+        throw new ExchangeError ('Not Implemented');
+    }
+
+    async fetchMarkets () {
+        throw new ExchangeError ('Not Implemented');
+    }
+
+    async fetchOrder (orderId, symbol = undefined, params = {}) {
+        throw new ExchangeError ('Not Implemented');
+    }
+
+    async fetchOrderBook () {
+        throw new ExchangeError ('Not Implemented');
+    }
+
+    async fetchOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
+        throw new ExchangeError ('Not Implemented');
+    }
+
+    async fetchTicker () {
+        throw new ExchangeError ('Not Implemented');
+    }
+
+    async fetchTrades () {
+        throw new ExchangeError ('Not Implemented');
+    }
+
+    async withdraw () {
+        throw new ExchangeError ('Not Implemented');
+    }
+
+    async commit () {
+        throw new ExchangeError ('Not Implemented');
+    }
+
+    async release () {
+        throw new ExchangeError ('Not Implemented');
+    }
+};
+
