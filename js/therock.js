c71237fd11d865b9245c027ddb8e5a60210e062f
ShoresOfNowhere <subscrg@atipica.it>
2018-10-04 01:37:36 +0200

    the cancelOrder call doesn't have the required parameter fund_id
    The cancelOrder call to privateDeleteFundsFundIdOrdersId needs to pass the required fund_id parameter, which is missing...

--- js/therock.js
+++ js/therock.js
@@ -310,6 +310,7 @@ module.exports = class therock extends Exchange {
         await this.loadMarkets ();
         return await this.privateDeleteFundsFundIdOrdersId (this.extend ({
             'id': id,
+            'fund_id': this.marketId (symbol),
         }, params));
     }
 
