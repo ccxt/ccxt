8b688ec08a6a9df39bb15a6201a5453834afac67
Igor Kroitor <igor.kroitor@gmail.com>
2018-10-27 11:43:07 +0300

    btimarket withdraw() signature unified

--- js/bitmarket.js
+++ js/bitmarket.js
@@ -310,15 +310,16 @@ module.exports = class bitmarket extends Exchange {
         return false;
     }
 
-    async withdraw (currency, amount, address, tag = undefined, params = {}) {
+    async withdraw (code, amount, address, tag = undefined, params = {}) {
         this.checkAddress (address);
         await this.loadMarkets ();
+        let currency = this.currency (code);
         let method = undefined;
         let request = {
-            'currency': currency,
+            'currency': currency['id'],
             'quantity': amount,
         };
-        if (this.isFiat (currency)) {
+        if (this.isFiat (code)) {
             method = 'privatePostWithdrawFiat';
             if ('account' in params) {
                 request['account'] = params['account']; // bank account code for withdrawal
