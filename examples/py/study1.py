ce2e2e83408e683bccfc76640d65503d09a3fe0e
jiaojinglong <jiaojinglong@dprating.com>
2018-10-28 23:33:20 +0800

    optimize code

--- examples/py/study1.py
+++ examples/py/study1.py
@@ -2,8 +2,10 @@ import ccxt
 import time
 import os
 good_coin = ['BTC', 'ETH', 'XRP', 'BCH', 'EOS', 'XLM', 'LTC', 'ADA', 'XMR', 'TRX', 'BNB', 'ONT', 'NEO', 'DCR']
-# good_coin = ['BTC', 'ETH', 'EOS']
-good_exchange_name = ['binance', 'fcoin', 'gateio', 'huobi', 'kucoin', 'okex']
+# good_coin = ['BTC', 'ETH', 'XRP']
+good_exchange_name = ['binance', 'fcoin', 'gateio', 'huobipro', 'kucoin', 'okex']
+# good_exchange_name = ['huobipro', 'kucoin', 'okex']
+
 def_quote = 'USDT'
 # delay 2 seconds
 delay = 2
@@ -58,13 +60,13 @@ def get_exchange_list(good_list):
     return exchange_list
 
 
-# 获取指定交易对的潜在套利最大化的2个交易所，bid1买一最低，ask1卖一最高
+# 低买高卖，获取指定交易对的潜在套利最大化的2个交易所，理论上先去ask1卖一最低（卖的最便宜的bid1）的买入，立刻去bid1买一最高（买起来最贵的bid1）卖出
 def find_trade_object(in_symbol,exchange_list):
     pass
-    #初始化为一个较大值
-    min_bid1 = 10000
-    #初始化为一个较小值
-    max_ask1 = 0
+    #最贵的买一 初始化为一个较小值
+    max_bid1 = 0
+    #最便宜的卖一 初始化为一个较大值
+    min_ask1 = 10000
     bid_exchange = None
     ask_exchange = None
     bid_time = None
@@ -74,49 +76,60 @@ def find_trade_object(in_symbol,exchange_list):
     print ('current symbol is {}'.format(in_symbol))
     for exchange in exchange_list:
         #获取市场交易对数据
-        exchange.load_markets()
+        try:
+            exchange.load_markets()
+        except Exception as e:
+            print('-------XXXXXX11111 load_markets exception is {},exchange is {},symbol is {}'.format(e.args[0],exchange.name,symbol))
+            continue
         if exchange.markets is None:
             print('exchange markets {} is None'.format(exchange))
             continue
-        for symbol in exchange.markets:
-            if symbol == in_symbol:
-                #获取订单数据
-                try:
-                    orderbook = exchange.fetch_order_book(symbol)
-                except Exception as e:
-                    print('-------XXXXXX exception is {},exchange is {},symbol is {}'.e.args[0],exchange.name,symbol)
-                    continue
-                date_time = exchange.last_response_headers['Date']
-                bid1 = orderbook['bids'][0][0] if len(orderbook['bids']) > 0 else None
-                bid1_amount = orderbook['bids'][0][1] if len(orderbook['bids']) > 0 else None
-                ask1 = orderbook['asks'][0][0] if len(orderbook['asks']) > 0 else None
-                ask1_amount = orderbook['asks'][0][1] if len(orderbook['asks']) > 0 else None
-                print('exchange {} bid1 ask1 is \n---- {},{},   {},{},date_time is {}'.format(exchange.name,bid1,bid1_amount,ask1,ask1_amount,date_time))
-                # spread = (ask1 - bid1) if (bid1 and ask1) else None
-                # 比较获取并保存 最低买一价和最高卖一价
-                if bid1 and (bid1 < min_bid1) :
-                    min_bid1 = bid1
-                    bid_exchange = exchange
-                    bid_time = date_time
-                    bid_amount = bid1_amount
-                    print('get new min_bid1 is {},exchange is {},date_time is {}'.format(min_bid1,exchange.name,date_time))
-                if ask1 and (ask1 > max_ask1) :
-                    max_ask1 = ask1
-                    ask_exchange = exchange
-                    ask_time = date_time
-                    ask_amount = ask1_amount
-                    print('get new max_ask1 is {},exchange is {},date_time is {}'.format(max_ask1,exchange.name,date_time))
-                time.sleep(delay)
-        # print('exchange {} over'.format(exchange))
+        
+        try:
+            orderbook = exchange.fetch_order_book(symbol)
+        except Exception as e:
+            print('-------XXXXXX22222 fetch_order_book exception is {},exchange is {},symbol is {}'.format(e.args[0], exchange.name, symbol))
+            continue
+        date_time = exchange.last_response_headers['Date']
+        bid1 = orderbook['bids'][0][0] if len(orderbook['bids']) > 0 else None
+        bid1_amount = orderbook['bids'][0][1] if len(orderbook['bids']) > 0 else None
+        ask1 = orderbook['asks'][0][0] if len(orderbook['asks']) > 0 else None
+        ask1_amount = orderbook['asks'][0][1] if len(orderbook['asks']) > 0 else None
+        print('exchange {} bid1 ask1 is \n---- {},{},   {},{},date_time is {}'.format(exchange.name, bid1, bid1_amount,
+                                                                                      ask1, ask1_amount, date_time))
+        #比较价格并保存 最低卖一价和最高买一价
+        #找到最贵的买一，进行卖操作
+        if bid1 and (bid1 > max_bid1):
+            max_bid1 = bid1
+            bid_exchange = exchange
+            bid_time = date_time
+            bid_amount = bid1_amount
+            print('get new max_bid1 is {},exchange is {},date_time is {}'.format(max_bid1, exchange.name, date_time))
+        #找到最便宜的卖一，进行买操作
+        if ask1 and (ask1 < min_ask1):
+            min_ask1 = ask1
+            ask_exchange = exchange
+            ask_time = date_time
+            ask_amount = ask1_amount
+            print('get new min_ask1 is {},exchange is {},date_time is {}'.format(min_ask1, exchange.name, date_time))
+        time.sleep(delay)
+                
     if bid_exchange and ask_exchange:
-        diff = max_ask1 - min_bid1
-        percent = diff / (max_ask1+min_bid1) * 2 * 100
-        print('\n\n****** symbol {} find good exchange,\n percent {}%,diff {} buy {},{},{},{}, sell {},{},{},{}'.
-              format(in_symbol,percent,diff, min_bid1,bid_amount,min_bid1*bid_amount,bid_exchange.name, max_ask1,ask_amount,max_ask1*ask_amount,ask_exchange.name))
-        return percent,diff,min_bid1,bid_amount,min_bid1*bid_amount,bid_exchange.name,bid_time,max_ask1,ask_amount,max_ask1*ask_amount,ask_exchange.name,ask_time
+        price_diff = max_bid1 - min_ask1
+        percent = price_diff / min_ask1 * 100
+        trade_volume = min(ask_amount,bid_amount)
+        profits = min_ask1 * trade_volume * percent/100
+        print('\n\n****** symbol {} find good exchange,\n percent {}%,price_diff {},trade_volume {},profits {},'
+              '\nbuy at {},{},{},{},\nsell at {},{},{},{}'.
+              format(in_symbol,percent,price_diff,trade_volume,profits, min_ask1,ask_amount,min_ask1*ask_amount,ask_exchange.name,
+                     max_bid1,bid_amount,max_bid1*bid_amount,bid_exchange.name))
+        if price_diff < 0:
+            print('oooooooo unlucky symbol {},no pair to make money'.format(symbol))
+        return percent,price_diff,trade_volume,profits, min_ask1,ask_amount,min_ask1*ask_amount, ask_exchange.name,ask_time,\
+               max_bid1,bid_amount,max_bid1*bid_amount,bid_exchange.name,bid_time
     else :
         print('\n\n******------ symbol {} not find good exchange'.format(in_symbol))
-        return min_bid1,None,max_ask1,None
+        return min_ask1,None,max_bid1,None
 
 if __name__ == '__main__':
 
