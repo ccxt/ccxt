ae8d155f8f5076e3436f32cf2e9a4db21c2a1413
Travis CI <travis@travis-ci.org>
2018-11-05 01:12:04 +0000

    1.17.469
    [ci skip]

--- python/ccxt/async_support/quadrigacx.py
+++ python/ccxt/async_support/quadrigacx.py
@@ -28,6 +28,8 @@ class quadrigacx (Exchange):
             'has': {
                 'fetchDepositAddress': True,
                 'fetchTickers': True,
+                'fetchOrder': True,
+                'fetchMyTrades': True,
                 'CORS': True,
                 'withdraw': True,
             },
@@ -74,16 +76,16 @@ class quadrigacx (Exchange):
                 },
             },
             'markets': {
-                'BTC/CAD': {'id': 'btc_cad', 'symbol': 'BTC/CAD', 'base': 'BTC', 'quote': 'CAD', 'maker': 0.005, 'taker': 0.005},
-                'BTC/USD': {'id': 'btc_usd', 'symbol': 'BTC/USD', 'base': 'BTC', 'quote': 'USD', 'maker': 0.005, 'taker': 0.005},
-                'ETH/BTC': {'id': 'eth_btc', 'symbol': 'ETH/BTC', 'base': 'ETH', 'quote': 'BTC', 'maker': 0.002, 'taker': 0.002},
-                'ETH/CAD': {'id': 'eth_cad', 'symbol': 'ETH/CAD', 'base': 'ETH', 'quote': 'CAD', 'maker': 0.005, 'taker': 0.005},
-                'LTC/CAD': {'id': 'ltc_cad', 'symbol': 'LTC/CAD', 'base': 'LTC', 'quote': 'CAD', 'maker': 0.005, 'taker': 0.005},
-                'LTC/BTC': {'id': 'ltc_btc', 'symbol': 'LTC/BTC', 'base': 'LTC', 'quote': 'BTC', 'maker': 0.005, 'taker': 0.005},
-                'BCH/CAD': {'id': 'bch_cad', 'symbol': 'BCH/CAD', 'base': 'BCH', 'quote': 'CAD', 'maker': 0.005, 'taker': 0.005},
-                'BCH/BTC': {'id': 'bch_btc', 'symbol': 'BCH/BTC', 'base': 'BCH', 'quote': 'BTC', 'maker': 0.005, 'taker': 0.005},
-                'BTG/CAD': {'id': 'btg_cad', 'symbol': 'BTG/CAD', 'base': 'BTG', 'quote': 'CAD', 'maker': 0.005, 'taker': 0.005},
-                'BTG/BTC': {'id': 'btg_btc', 'symbol': 'BTG/BTC', 'base': 'BTG', 'quote': 'BTC', 'maker': 0.005, 'taker': 0.005},
+                'BTC/CAD': {'id': 'btc_cad', 'symbol': 'BTC/CAD', 'base': 'BTC', 'quote': 'CAD', 'baseId': 'btc', 'quoteId': 'cad', 'maker': 0.005, 'taker': 0.005},
+                'BTC/USD': {'id': 'btc_usd', 'symbol': 'BTC/USD', 'base': 'BTC', 'quote': 'USD', 'baseId': 'btc', 'quoteId': 'usd', 'maker': 0.005, 'taker': 0.005},
+                'ETH/BTC': {'id': 'eth_btc', 'symbol': 'ETH/BTC', 'base': 'ETH', 'quote': 'BTC', 'baseId': 'eth', 'quoteId': 'btc', 'maker': 0.002, 'taker': 0.002},
+                'ETH/CAD': {'id': 'eth_cad', 'symbol': 'ETH/CAD', 'base': 'ETH', 'quote': 'CAD', 'baseId': 'eth', 'quoteId': 'cad', 'maker': 0.005, 'taker': 0.005},
+                'LTC/CAD': {'id': 'ltc_cad', 'symbol': 'LTC/CAD', 'base': 'LTC', 'quote': 'CAD', 'baseId': 'ltc', 'quoteId': 'cad', 'maker': 0.005, 'taker': 0.005},
+                'LTC/BTC': {'id': 'ltc_btc', 'symbol': 'LTC/BTC', 'base': 'LTC', 'quote': 'BTC', 'baseId': 'ltc', 'quoteId': 'btc', 'maker': 0.005, 'taker': 0.005},
+                'BCH/CAD': {'id': 'bch_cad', 'symbol': 'BCH/CAD', 'base': 'BCH', 'quote': 'CAD', 'baseId': 'bch', 'quoteId': 'cad', 'maker': 0.005, 'taker': 0.005},
+                'BCH/BTC': {'id': 'bch_btc', 'symbol': 'BCH/BTC', 'base': 'BCH', 'quote': 'BTC', 'baseId': 'bch', 'quoteId': 'btc', 'maker': 0.005, 'taker': 0.005},
+                'BTG/CAD': {'id': 'btg_cad', 'symbol': 'BTG/CAD', 'base': 'BTG', 'quote': 'CAD', 'baseId': 'btg', 'quoteId': 'cad', 'maker': 0.005, 'taker': 0.005},
+                'BTG/BTC': {'id': 'btg_btc', 'symbol': 'BTG/BTC', 'base': 'BTG', 'quote': 'BTC', 'baseId': 'btg', 'quoteId': 'btc', 'maker': 0.005, 'taker': 0.005},
             },
             'exceptions': {
                 '101': AuthenticationError,
@@ -93,17 +95,101 @@ class quadrigacx (Exchange):
     async def fetch_balance(self, params={}):
         balances = await self.privatePostBalance()
         result = {'info': balances}
-        currencies = list(self.currencies.keys())
-        for i in range(0, len(currencies)):
-            currency = currencies[i]
-            lowercase = currency.lower()
-            account = {
-                'free': float(balances[lowercase + '_available']),
-                'used': float(balances[lowercase + '_reserved']),
-                'total': float(balances[lowercase + '_balance']),
+        currencyIds = list(self.currencies_by_id.keys())
+        for i in range(0, len(currencyIds)):
+            currencyId = currencyIds[i]
+            currency = self.currencies_by_id[currencyId]
+            code = currency['code']
+            result[code] = {
+                'free': self.safe_float(balances, currencyId + '_available'),
+                'used': self.safe_float(balances, currencyId + '_reserved'),
+                'total': self.safe_float(balances, currencyId + '_balance'),
             }
-            result[currency] = account
         return self.parse_balance(result)
+
+    async def fetch_my_trades(self, symbol=None, since=None, limit=None, params={}):
+        market = None
+        request = {}
+        if symbol is not None:
+            market = self.market(symbol)
+            request['book'] = market['id']
+        if limit is not None:
+            request['limit'] = limit
+        response = await self.privatePostUserTransactions(self.extend(request, params))
+        trades = self.filter_by(response, 'type', 2)
+        return self.parse_trades(trades, market, since, limit)
+
+    async def fetch_order(self, id, symbol=None, params={}):
+        request = {
+            'id': id,
+        }
+        response = await self.privatePostLookupOrder(self.extend(request, params))
+        return self.parse_orders(response)
+
+    def parse_order_status(self, status):
+        statuses = {
+            '-1': 'canceled',
+            '0': 'open',
+            '1': 'open',
+            '2': 'closed',
+        }
+        return self.safe_string(statuses, status, status)
+
+    def parse_order(self, order, market=None):
+        id = self.safe_string(order, 'id')
+        price = self.safe_float(order, 'price')
+        amount = None
+        filled = None
+        remaining = self.safe_float(order, 'amount')
+        cost = None
+        symbol = None
+        marketId = self.safe_string(order, 'book')
+        if marketId in self.markets_by_id:
+            market = self.markets_by_id[marketId]
+        else:
+            baseId, quoteId = marketId.split('_')
+            base = baseId.upper()
+            quote = quoteId.upper()
+            base = self.common_currency_code(base)
+            quote = self.common_currency_code(quote)
+            symbol = base + '/' + quote
+        side = self.safe_string(order, 'type')
+        if side == '0':
+            side = 'buy'
+        else:
+            side = 'sell'
+        status = self.parse_order_status(self.safe_string(order, 'status'))
+        timestamp = self.parse8601(self.safe_string(order, 'created'))
+        lastTradeTimestamp = self.parse8601(self.safe_string(order, 'updated'))
+        type = 'market' if (price == 0.0) else 'limit'
+        if market is not None:
+            symbol = market['symbol']
+        if status == 'closed':
+            amount = remaining
+            filled = remaining
+            remaining = 0
+        if (type == 'limit') and(price is not None):
+            if filled is not None:
+                cost = price * filled
+        result = {
+            'info': order,
+            'id': id,
+            'timestamp': timestamp,
+            'datetime': self.iso8601(timestamp),
+            'lastTradeTimestamp': lastTradeTimestamp,
+            'symbol': symbol,
+            'type': type,
+            'side': side,
+            'price': price,
+            'cost': cost,
+            'average': None,
+            'amount': amount,
+            'filled': filled,
+            'remaining': remaining,
+            'status': status,
+            'fee': None,
+        }
+        return result
 
     async def fetch_order_book(self, symbol, limit=None, params={}):
         orderbook = await self.publicGetOrderBook(self.extend({
@@ -178,19 +264,101 @@ class quadrigacx (Exchange):
             'info': ticker,
         }
 
-    def parse_trade(self, trade, market):
-        timestamp = int(trade['date']) * 1000
+    def parse_trade(self, trade, market=None):
+        #
+        # fetchTrades(public)
+        #
+        #     {"amount":"2.26252009","date":"1541355778","price":"0.03300000","tid":3701722,"side":"sell"}
+        #
+        # fetchMyTrades(private)
+        #
+        #     {
+        #         "datetime": "2018-01-01T00:00:00",  # date and time
+        #         "id": 123,  # unique identifier(only for trades)
+        #         "type": 2,  # transaction type(0 - deposit 1 - withdrawal 2 - trade)
+        #         "method": "...",  # deposit or withdrawal method
+        #         "(minor currency code)" – the minor currency amount
+        #         "(major currency code)" – the major currency amount
+        #         "order_id": "...",  # a 64 character long hexadecimal string representing the order that was fully or partially filled(only for trades)
+        #         "fee": 123.45,  # transaction fee
+        #         "rate": 54.321,  # rate per btc(only for trades)
+        #     }
+        #
+        id = self.safe_string_2(trade, 'tid', 'id')
+        timestamp = self.parse8601(self.safe_string(trade, 'datetime'))
+        if timestamp is None:
+            timestamp = self.safe_integer(trade, 'date')
+            if timestamp is not None:
+                timestamp *= 1000
+        symbol = None
+        omitted = self.omit(trade, ['datetime', 'id', 'type', 'method', 'order_id', 'fee', 'rate'])
+        keys = list(omitted.keys())
+        rate = self.safe_float(trade, 'rate')
+        for i in range(0, len(keys)):
+            marketId = keys[i]
+            if trade[marketId] == rate:
+                if marketId in self.markets_by_id:
+                    market = self.markets_by_id[marketId]
+                else:
+                    currencyIds = marketId.split('_')
+                    numCurrencyIds = len(currencyIds)
+                    if numCurrencyIds == 2:
+                        baseId = currencyIds[0]
+                        quoteId = currencyIds[1]
+                        base = baseId.upper()
+                        quote = quoteId.upper()
+                        base = self.common_currency_code(base)
+                        quote = self.common_currency_code(base)
+                        symbol = base + '/' + quote
+        orderId = self.safe_string(trade, 'order_id')
+        side = self.safe_string(trade, 'side')
+        price = self.safe_float(trade, 'price', rate)
+        amount = self.safe_float(trade, 'amount')
+        cost = None
+        if market is not None:
+            symbol = market['symbol']
+            baseId = market['baseId']
+            quoteId = market['quoteId']
+            if amount is None:
+                amount = self.safe_float(trade, baseId)
+                if amount is not None:
+                    amount = abs(amount)
+            cost = self.safe_float(trade, quoteId)
+            if cost is not None:
+                cost = abs(cost)
+            if side is None:
+                if self.safe_float(trade, market['base']) > 0:
+                    side = 'buy'
+                else:
+                    side = 'sell'
+        if cost is None:
+            if price is not None:
+                if amount is not None:
+                    cost = amount * price
+        fee = None
+        feeCost = self.safe_float(trade, 'fee')
+        if feeCost is not None:
+            feeCurrency = None
+            if market is not None:
+                feeCurrency = market['base'] if (side == 'buy') else market['quote']
+            fee = {
+                'cost': feeCost,
+                'currency': feeCurrency,
+            }
         return {
             'info': trade,
+            'id': id,
             'timestamp': timestamp,
             'datetime': self.iso8601(timestamp),
-            'symbol': market['symbol'],
-            'id': str(trade['tid']),
-            'order': None,
+            'symbol': symbol,
+            'order': orderId,
             'type': None,
-            'side': trade['side'],
-            'price': self.safe_float(trade, 'price'),
-            'amount': self.safe_float(trade, 'amount'),
+            'side': side,
+            'takerOrMaker': None,
+            'price': price,
+            'amount': amount,
+            'cost': cost,
+            'fee': fee,
         }
 
     async def fetch_trades(self, symbol, since=None, limit=None, params={}):
