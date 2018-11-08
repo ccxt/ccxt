9f1a7d66372b5f937e23dd6ec43227bce23cee8c
Igor Kroitor <igor.kroitor@gmail.com>
2018-11-09 02:08:47 +0300

    no need to list blockbid in ccxt.js (added automatically on Travis)

--- ccxt.js
+++ ccxt.js
@@ -70,7 +70,6 @@ const exchanges = {
     'bitz':                    require ('./js/bitz.js'),
     'bl3p':                    require ('./js/bl3p.js'),
     'bleutrade':               require ('./js/bleutrade.js'),
-    'blockbid':                require ('./js/blockbid.js'),
     'braziliex':               require ('./js/braziliex.js'),
     'btcalpha':                require ('./js/btcalpha.js'),
     'btcbox':                  require ('./js/btcbox.js'),
