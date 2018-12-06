863c1f3cfc8e193654cabc5b59ec6ceb9c32470e
Igor Kroitor <igor.kroitor@gmail.com>
2018-12-06 06:38:59 +0300

    bitfinex removed USD → USDT mapping fix #2851

--- js/bitfinex.js
+++ js/bitfinex.js
@@ -279,7 +279,6 @@ module.exports = class bitfinex extends Exchange {
                 'SPK': 'SPANK',
                 'STJ': 'STORJ',
                 'YYW': 'YOYOW',
-                'USD': 'USDT',
                 'UTN': 'UTNP',
             },
             'exceptions': {
