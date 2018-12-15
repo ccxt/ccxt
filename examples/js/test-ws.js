efefaad137ef1207c09ac8f59470cd04ff71945d
Luis Fernando Pardo <lfern70@gmail.com>
2018-12-15 10:58:47 +0100

    add ws-test to start testing exchange websocket before coding

---
+++ examples/js/test-ws.js
@@ -0,0 +1,54 @@
+const WebSocket = require('ws');
+
+//const url = "wss://api.upbit.com/websocket/v1";
+const url = "wss://crix-ws.upbit.com/websocket";
+const url = "wss://crix-websocket-sg.upbit.com/sockjs/websocket";
+const ws = new WebSocket(url);
+
+let payload = [
+    {
+        "ticket":"ram macbook"
+    },{
+        //"format":"PRTBUF"
+        "format":"DEFAULT"
+    },{
+        "type":"crixOrderbook",
+        "codes":[
+            "CRIX.UPBIT.BTC-ETH"
+        ]
+    }
+];
+/*
+payload = [
+    {
+        "ticket":"UNIQUE_TICKET"
+    },{
+        
+        "format":"DEFAULT"
+    },{
+        "type":"orderbook",
+        "codes":["BTC-ETH"]
+    }
+];
+*/
+ws.on('open', async () => {
+    console.log("opened");
+    ws.send(JSON.stringify(payload));
+});
+
+ws.on('error', (error) => {
+    console.log(error);
+});
+
+ws.on('close', () => {
+    console.log('closed');
+});
+
+ws.on('message', async (data) => {
+    console.log(data);
+    console.log(data.toString('hex'));
+    console.log(data.toString('utf8'));
+});
+
+
+
