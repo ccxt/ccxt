7c3d3558f61dbe84d34b35108c1a18259407bc5c
frosty00 <carlo.revelli@yahoo.co.uk>
2018-12-17 10:20:28 +0000

    first three methods

--- js/coss.js
+++ js/coss.js
@@ -24,6 +24,11 @@ module.exports = class coss extends Exchange {
                     'https://api.coss.io/v1/spec',
                 ],
             },
+            'has': {
+                'fetchMarkets': true,
+                'fetchCurrencies': true,
+                'fetchBalance': true,
+            },
             'api': {
                 'engine': {
                     'get': [
@@ -33,23 +38,102 @@ module.exports = class coss extends Exchange {
                 },
                 'trade': {
                     'get': [
-                        'exchange-info',
                         'account/balances',
                         'account/details',
                         'getmarketsummaries',
                         'market-price',
+                        'exchange-info',
                     ],
                 },
             },
         });
     }
 
-    async fetchMarkets () {
-        return '';
+    async fetchMarkets (params = {}) {
+        let response = await this.tradeGetExchangeInfo (params);
+        let markets = response['symbols'];
+        let result = [];
+        for (let i = 0; i < markets.length; i++) {
+            let entry = markets[i];
+            let marketId = entry['symbol'];
+            let [ baseId, quoteId ] = marketId.split ('_');
+            let base = this.commonCurrencyCode (baseId);
+            let quote = this.commonCurrencyCode (quoteId);
+            let symbol = base + '/' + quote;
+            let precision = {
+                'amount': this.safeInteger (entry, 'amount_limit_decimal'),  // decimal places (i.e. 2 = 5.03)
+                'price': this.safeInteger (entry, 'price_limit_decimal'),
+            };
+            let active = entry['allow_trading'];
+            let value = {
+                'symbol': symbol,
+                'id': marketId,
+                'baseId': baseId,
+                'quoteId': quoteId,
+                'base': base,
+                'quote': quote,
+                'active': active,
+                'precision': precision,
+                'info': entry,
+            };
+            result.push (value);
+        }
+        return result;
+    }
+
+    async fetchCurrencies (params = {}) {
+        let response = await this.tradeGetExchangeInfo (params);
+        let result = {};
+        let currencies = response['coins'];
+        for (let i = 0; i < currencies.length; i++) {
+            let info = currencies[i];
+            let currencyId = info['currency_code'];
+            let name = info['name'];
+            let limits = {
+                'amount': {
+                    'min': this.safeFloat (info, 'minimum_order_amount'),
+                    'max': undefined,
+                },
+            };
+            let code = this.commonCurrencyCode (currencyId);
+            result[code] = {
+                'code': code,
+                'id': currencyId,
+                'limits': limits,
+                'name': name,
+                'info': info,
+            };
+        }
+        return result;
+    }
+
+    async fetchBalance (params = {}) {
+        await this.loadMarkets ();
+        let response = await this.tradeGetAccountBalances ();
+        let result = {};
+        for (let i = 0; i < response.length; i++) {
+            let info = response[i];
+            let currencyId = info['currency_code'];
+            let code = this.currencies_by_id[currencyId]['code'];
+            let total = this.safeFloat (info, 'total');
+            let used = this.safeFloat (info, 'in_order');
+            let free = this.safeFloat (info, 'available');
+            result[code] = {
+                'total': total,
+                'used': used,
+                'free': free,
+            };
+        }
+        return this.parseBalance (result);
+    }
+
+    nonce () {
+        return this.milliseconds ();
     }
 
     sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
-        let pathArray = (this.urls['api'] + '/' + this.version + '/' + path).split ('/');
+        let url = this.urls['api'] + '/' + this.version + '/' + path;
+        let pathArray = url.split ('/');
         if (api === 'trade') {
             pathArray[2] = 'trade.' + pathArray[2];
             pathArray.splice (3, 0, 'c');
@@ -57,11 +141,11 @@ module.exports = class coss extends Exchange {
             pathArray[2] = 'engine.' + pathArray[2];
         }
         path = pathArray.join ('/');
-        let url = undefined;
         if (method === 'GET' && path.indexOf ('account') < 0) {
             url = path + '?' + this.urlencode (params);
         } else {
-            let request = this.urlencode ({ 'recvWindow': '5000', 'timestamp': this.nonce () });
+            let requestParams = { 'recvWindow': '10000', 'timestamp': this.nonce () };
+            let request = this.implodeParams ('recvWindow={recvWindow}&timestamp={timestamp}', requestParams);
             url = path + '?' + request;
             headers = {
                 'Signature': this.hmac (request, this.secret, 'sha256', 'hex'),
