bfe0f254202b1a82f5b7d507d4eaabf5e3ffcca9
Igor Kroitor <igor.kroitor@gmail.com>
2018-12-19 21:29:50 +0300

    cryptopia fetchDepositAddress python transpilation fix #4359

--- js/cryptopia.js
+++ js/cryptopia.js
@@ -806,7 +806,8 @@ module.exports = class cryptopia extends Exchange {
         }, params));
         let address = this.safeString (response['Data'], 'BaseAddress');
         let tag = this.safeString (response['Data'], 'Address');
-        if ((address === undefined) || (address.length < 1)) {
+        const addressEmpty = address.length < 1;
+        if ((address === undefined) || addressEmpty) {
             address = tag;
             tag = undefined;
         }
