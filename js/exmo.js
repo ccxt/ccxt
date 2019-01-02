8e483f11df176b313ea49813c5dec76169594a6c
Igor Kroitor <igor.kroitor@gmail.com>
2019-01-02 14:31:27 +0300

    exmo fiat code to common currency code conversion minor fix #4422 #4421

--- js/exmo.js
+++ js/exmo.js
@@ -248,7 +248,7 @@ module.exports = class exmo extends Exchange {
         // sets fiat fees to undefined
         const fiatGroups = this.toArray (this.omit (groupsByGroup, 'crypto'));
         for (let i = 0; i < fiatGroups.length; i++) {
-            const code = this.safeString (fiatGroups[i], 'title');
+            const code = this.commonCurrencyCode (this.safeString (fiatGroups[i], 'title'));
             withdraw[code] = undefined;
             deposit[code] = undefined;
         }
