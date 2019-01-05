b4c7c01a46aac5f0630563da60c712fdfde4d8ff
Kuri0 <zkqri0@gmail.com>
2019-01-05 11:25:13 +0530

    Fix Python transpiler

--- transpile.js
+++ transpile.js
@@ -241,6 +241,7 @@ const pythonRegexes = [
     [ /([^\s]+) \}/g, '$1}' ],    // PEP8 E202 remove whitespaces before right } bracket
     [ /([^a-z])(elif|if|or|else)\(/g, '$1$2 \(' ], // a correction for PEP8 E225 side-effect for compound and ternary conditionals
     [ /\=\=\sTrue/g, 'is True' ], // a correction for PEP8 E712, it likes "is True", not "== True"
+    [ /delete\s/g, 'del ' ],
 ])
 
 // ----------------------------------------------------------------------------
