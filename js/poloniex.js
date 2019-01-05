795498d235237fd8d7001c9f36ac64e741658b55
Igor Kroitor <igor.kroitor@gmail.com>
2019-01-05 06:19:24 +0300

    poloniex fetchTransactions amount includes the fee

--- js/poloniex.js
+++ js/poloniex.js
@@ -917,7 +917,7 @@ module.exports = class poloniex extends Exchange {
         await this.loadMarkets ();
         const year = 31104000; // 60 * 60 * 24 * 30 * 12 = one year of history, why not
         const now = this.seconds ();
-        let start = (since !== undefined) ? parseInt (since / 1000) : now - year;
+        let start = (since !== undefined) ? parseInt (since / 1000) : now - 10 * year;
         let request = {
             'start': start, // UNIX timestamp, required
             'end': now, // UNIX timestamp, required
@@ -1072,14 +1072,14 @@ module.exports = class poloniex extends Exchange {
         let amount = this.safeFloat (transaction, 'amount');
         const address = this.safeString (transaction, 'address');
         let feeCost = this.safeFloat (transaction, 'fee');
-        if (feeCost === undefined) {
-            if (type === 'deposit') {
+        if (type === 'deposit') {
+            if (feeCost === undefined) {
                 // according to https://poloniex.com/fees/
                 feeCost = 0; // FIXME: remove hardcoded value that may change any time
-            } else {
-                // poloniex withdrawal amount includes the fee
-                amount = amount - feeCost;
             }
+        } else {
+            // poloniex withdrawal amount includes the fee
+            amount = amount - feeCost;
         }
         return {
             'info': transaction,
