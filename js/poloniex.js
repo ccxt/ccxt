e024c286f4f2c715da96fed84fed1f2e466d70d8
Igor Kroitor <igor.kroitor@gmail.com>
2019-01-13 16:41:27 +0300

    poloniex parseTransaction null fee fix #4489

--- js/poloniex.js
+++ js/poloniex.js
@@ -1072,16 +1072,13 @@ module.exports = class poloniex extends Exchange {
         let amount = this.safeFloat (transaction, 'amount');
         const address = this.safeString (transaction, 'address');
         let feeCost = this.safeFloat (transaction, 'fee');
-        if (type === 'deposit') {
-            if (feeCost === undefined) {
-                // according to https://poloniex.com/fees/
-                feeCost = 0; // FIXME: remove hardcoded value that may change any time
-            }
-        } else {
-            if (feeCost !== undefined) {
-                // poloniex withdrawal amount includes the fee
-                amount = amount - feeCost;
-            }
+        if (feeCost === undefined) {
+            // according to https://poloniex.com/fees/
+            feeCost = 0; // FIXME: remove hardcoded value that may change any time
+        }
+        if (type === 'withdrawal') {
+            // poloniex withdrawal amount includes the fee
+            amount = amount - feeCost;
         }
         return {
             'info': transaction,
