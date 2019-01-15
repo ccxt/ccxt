0624040595be6b01f75ef378f35a0a3e95cc79f9
Travis CI <travis@travis-ci.org>
2019-01-15 05:48:10 +0000

    1.18.129
    [ci skip]

--- build/ccxt.browser.js
+++ build/ccxt.browser.js
@@ -45,7 +45,7 @@ const Exchange  = require ('./js/base/Exchange')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.18.128'
+const version = '1.18.129'
 
 Exchange.ccxtVersion = version
 
@@ -33463,7 +33463,7 @@ module.exports = class deribit extends Exchange {
             },
             'timeframes': {},
             'urls': {
-                // 'test': 'https://test.deribit.com',
+                'test': 'https://test.deribit.com',
                 'logo': 'https://user-images.githubusercontent.com/1294454/41933112-9e2dd65a-798b-11e8-8440-5bab2959fcb8.jpg',
                 'api': 'https://www.deribit.com',
                 'www': 'https://www.deribit.com',
