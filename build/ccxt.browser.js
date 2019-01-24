18e101639bdfc542747a8b6b7414b9685b003951
Travis CI <travis@travis-ci.org>
2019-01-24 21:32:13 +0000

    1.18.150
    [ci skip]

--- build/ccxt.browser.js
+++ build/ccxt.browser.js
@@ -45,7 +45,7 @@ const Exchange  = require ('./js/base/Exchange')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.18.149'
+const version = '1.18.150'
 
 Exchange.ccxtVersion = version
 
@@ -10208,6 +10208,7 @@ module.exports = class bitflyer extends Exchange {
                     'get': [
                         'getpermissions',
                         'getbalance',
+                        'getbalancehistory',
                         'getcollateral',
                         'getcollateralaccounts',
                         'getaddresses',
--- ccxt.js
+++ ccxt.js
@@ -37,7 +37,7 @@ const Exchange  = require ('./js/base/Exchange')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.18.149'
+const version = '1.18.150'
 
 Exchange.ccxtVersion = version
 
