6e9bac16187a4ec4b4636b517a026574b4f52dec
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-01-28 04:59:28 +0100

    ci trigger

--- examples/js/walutomat-examples.js
+++ examples/js/walutomat-examples.js
@@ -8,29 +8,23 @@ const walutomat = new ccxt.walutomat({
 (async () => {
     
     const orderBookBest = await walutomat.fetchOrderBook("EUR/PLN");
-
     console.log(orderBookBest);
 
     const balances = await walutomat.fetchBalance();
-
     console.log(balances);
 
     const trades = await walutomat.fetchTrades();
-
     console.log(trades);
 
     const orders = await walutomat.fetchOrders();
-
     console.log(orders);
 
     const createOrder = await walutomat.createOrder("EUR/PLN", null, "buy", 1, 5, {
         submitId: "GUID"
     });
-
     console.log(createOrder);
 
     const cancelOrder = await walutomat.cancelOrder("GUID");
-
     console.log(cancelOrder);
     
 })();
