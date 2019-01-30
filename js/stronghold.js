404c3c2002a1c914b3e0e104e0ad72992c7cf169
Carlo Revelli <carlorevelli@ocf.berkeley.edu>
2019-01-30 10:55:38 -0800

    k

--- js/stronghold.js
+++ js/stronghold.js
@@ -198,7 +198,7 @@ module.exports = class Stronghold extends Exchange {
 
     async fetchMyTrades (symbol = undefined, since = undefined, limit = undefined, params = {}) {
         await this.loadMarkets ();
-        const response = this.privateGetVenuesVenueIdAccountsAccountIdTransactions ({ "accountId": "f72b9fb5-9607-4dd3-b31f-6ded21337056" })git
+        const response = this.privateGetVenuesVenueIdAccountsAccountIdTransactions ({ "accountId": "f72b9fb5-9607-4dd3-b31f-6ded21337056" })
 
 
     }
