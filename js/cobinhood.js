b71cb668631a40a8188754a9979b9fa7d1ba7246
Luis Fernando Pardo <lfern70@gmail.com>
2019-02-18 22:07:37 +0100

    add RequestTimeout to cobinhood.js when pong not received after 30 seconds

--- js/cobinhood.js
+++ js/cobinhood.js
@@ -3,7 +3,7 @@
 //  ---------------------------------------------------------------------------
 
 const Exchange = require ('./base/Exchange');
-const { ExchangeError, InvalidAddress, InsufficientFunds, InvalidNonce, InvalidOrder, PermissionDenied, NotSupported } = require ('./base/errors');
+const { ExchangeError, InvalidAddress, InsufficientFunds, InvalidNonce, InvalidOrder, PermissionDenied, NotSupported, RequestTimeout } = require ('./base/errors');
 
 //  ---------------------------------------------------------------------------
 
@@ -196,6 +196,7 @@ module.exports = class cobinhood extends Exchange {
                 'methodmap': {
                     '_websocketSendHeartbeat': '_websocketSendHeartbeat',
                     '_websocketTimeoutRemoveNonce': '_websocketTimeoutRemoveNonce',
+                    '_websocketPongTimeout': '_websocketPongTimeout',
                 },
                 'events': {
                     'ob': {
@@ -903,6 +904,8 @@ module.exports = class cobinhood extends Exchange {
         if (type === 'error') {
             this.emit ('err', new ExchangeError (this.id + ' error ' + h[3] + ':' + h[4]));
         } else if (type === 'pong') {
+            let pongTimeout = this._contextGet(contextId, 'pongtimeout');
+            this._cancelTimeout (pongTimeout);
             this.emit ('pong');
         } else if (channel.indexOf ('order-book.') >= 0) {
             if (type === 'subscribed') {
@@ -967,7 +970,14 @@ module.exports = class cobinhood extends Exchange {
         this._contextSet (contextId, 'heartbeattimer', heartbeatTimer);
     }
 
+    _websocketPongTimeout (contextId) {
+        let ex = new RequestTimeout (this.id + " does not received pong message after 30 seconds");
+        this.emit('err', ex, contextId);
+    }
+
     _websocketSendHeartbeat (contextId) {
+        let pongTimeout = this._setTimeout (contextId, 30000, this._websocketMethodMap ('_websocketPongTimeout'), [contextId]);
+        this._contextSet(contextId, 'pongtimeout', pongTimeout);
         this.websocketSendJson ({
             'action': 'ping',
         }, contextId);
