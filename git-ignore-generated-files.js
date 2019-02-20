4872954c7c14b46525377ea0a82fa9da01a7278e
Igor Kroitor <igor.kroitor@gmail.com>
2019-02-20 06:27:24 +0300

    git-ignore-generated-files split commands for each file individually

--- git-ignore-generated-files.js
+++ git-ignore-generated-files.js
@@ -55,11 +55,13 @@ for (const id of require ('./exchanges.json').ids) {
 if (process.argv.includes ('--unignore')) {
 
     log.bright.green (`Re-enabling the git changes tracking for ${files.length} generated files...`)
-    execSync ('git update-index --no-assume-unchanged ' + files.join (' '))
+        
+    files.forEach (file => execSync ('git update-index --no-assume-unchanged ' + file))
 
 } else {
     
     log.bright.cyan (`Disabling the git changes tracking for ${files.length} generated files...`)
-    execSync ('git update-index --assume-unchanged ' + files.join (' '))
+        
+    files.forEach (file => execSync ('git update-index --assume-unchanged ' + file))
 }
 
