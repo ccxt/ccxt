63f8f0612139354db0e78c1768cc8f651feeabe5
xpl <rocket.mind@gmail.com>
2019-03-02 15:14:26 +0300

    dist/ccxt.browser.js

--- dist/ccxt.browser.js
+++ dist/ccxt.browser.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*  A entry point for the browser bundle version. This gets compiled by:
         
-        browserify --debug ./ccxt.browser.js > ./build/ccxt.browser.js
+        browserify --debug ./ccxt.browser.js > ./dist/ccxt.browser.js
  */
 
 window.ccxt = require ('./ccxt')
