7e12ac1ce4575d407d312c6d38579d889de7df28
Igor Kroitor <igor.kroitor@gmail.com>
2019-03-16 19:59:11 +0300

    braziliex fetchCurrenciesFromCache is now redundant

--- js/braziliex.js
+++ js/braziliex.js
@@ -75,6 +75,8 @@ module.exports = class braziliex extends Exchange {
     }
 
     async fetchCurrenciesFromCache (params = {}) {
+        // this method is now redundant
+        // currencies are now fetched before markets
         const options = this.safeValue (this.options, 'fetchCurrencies', {});
         const timestamp = this.safeInteger (options, 'timestamp');
         const expires = this.safeInteger (options, 'expires', 1000);
