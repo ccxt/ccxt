c2e3fec0edc539f3cc82444ec94a0ca9973ae559
xpl <rocket.mind@gmail.com>
2019-03-18 06:09:25 +0300

    removed redundant git config --global user.email/user.name from push.sh

--- build/push.sh
+++ build/push.sh
@@ -17,8 +17,6 @@ echo "Pushing to ccxt.wiki"
 
 cd build/ccxt.wiki
 cp ../wiki/* .
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
 git commit -a -m ${COMMIT_MESSAGE}
 git remote remove origin
 git remote add origin https://${GITHUB_TOKEN}@github.com/ccxt/ccxt.wiki.git
