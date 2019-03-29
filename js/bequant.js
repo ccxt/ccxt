c1b385f4d75553e14f2b595b65b665a88c71170e
Igor Kroitor <igor.kroitor@gmail.com>
2019-03-29 20:00:43 +0300

    bequant country HK → MT
    https://bequant.io/legal-information
    
    > 1) Post address for correspondence:
    > Bequant Exchange Limited
    > 5B, Office 2, Spinola Residence, Spinola Road,
    > St. Julians. STJ 3012, Malta

--- js/bequant.js
+++ js/bequant.js
@@ -10,7 +10,7 @@ module.exports = class bequant extends hitbtc2 {
         return this.deepExtend (super.describe (), {
             'id': 'bequant',
             'name': 'Bequant',
-            'countries': [ 'HK' ],
+            'countries': [ 'MT' ], // Malta
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/55248342-a75dfe00-525a-11e9-8aa2-05e9dca943c6.jpg',
                 'api': 'https://api.bequant.io',
