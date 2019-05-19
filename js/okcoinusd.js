03786e645a98227a6de7bd55341adc9daaeac935
Igor Kroitor <igor.kroitor@gmail.com>
2019-05-19 13:08:19 +0300

    okcoinusd/okex referral link

--- js/okcoinusd.js
+++ js/okcoinusd.js
@@ -131,6 +131,7 @@ module.exports = class okcoinusd extends Exchange {
                     'https://www.okcoin.com/docs/en/',
                     'https://www.npmjs.com/package/okcoin.com',
                 ],
+                'referral': 'https://www.okcoin.com/account/register?flag=activity&channelId=600001513',
             },
             // these are okcoin.com fees, okex fees are in okex.js
             'fees': {
--- js/okex.js
+++ js/okex.js
@@ -30,6 +30,7 @@ module.exports = class okex extends okcoinusd {
                     'https://www.okex.com/docs/en/',
                 ],
                 'fees': 'https://www.okex.com/pages/products/fees.html',
+                'referral': 'https://www.okex.com',
             },
             'fees': {
                 'trading': {
