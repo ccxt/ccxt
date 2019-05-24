15a316541866ec499b17de0ba52c4d843125e329
Matt Smith <matt.joseph.smith@gmail.com>
2019-05-24 15:44:02 -0400

    Fix syntax lint issue

--- js/boaexchange.js
+++ js/boaexchange.js
@@ -776,7 +776,7 @@ module.exports = class boaexchange extends Exchange {
     parseMarkets (markets) {
         let results = [];
         for (let i = 0; i < markets.length; i++) {
-            results.push(this.parseMarket(markets[i]));
+            results.push (this.parseMarket (markets[i]));
         }
         return results;
     }
