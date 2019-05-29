8765091e39fc1bf8cd45cc1bc83b4bd94295d575
Carlo Revelli <carlo.revelli@berkeley.edu>
2019-05-29 12:28:39 +0100

    k

--- python/ccxt/static_dependencies/asn1crypto/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-from __future__ import unicode_literals, division, absolute_import, print_function
-
-from .version import __version__, __version_info__
-
-__all__ = [
-    '__version__',
-    '__version_info__',
-]
