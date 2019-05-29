37a4bd2284d6292e2be46284ff4da1a99c5d4a19
Carlo Revelli <carlo.revelli@berkeley.edu>
2019-05-29 16:58:02 +0100

    fix php bug

--- php/static_dependencies/adhocore/jwt/src/JWT.php
+++ php/static_dependencies/adhocore/jwt/src/JWT.php
@@ -116,11 +116,6 @@ class JWT
         $header = ['typ' => 'JWT', 'alg' => $this->algo] + $header;
 
         $this->validateKid($header);
-
-        if (!isset($payload['iat']) && !isset($payload['exp'])) {
-            $payload['exp'] = ($this->timestamp ?: \time()) + $this->maxAge;
-        }
-
         $header    = $this->urlSafeEncode($header);
         $payload   = $this->urlSafeEncode($payload);
         $signature = $this->urlSafeEncode($this->sign($header . '.' . $payload));
