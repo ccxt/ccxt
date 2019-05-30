cd04ccc424ac3fa2b48a5ea99c41355fdacf7d93
Travis CI <travis@travis-ci.org>
2019-05-30 11:59:33 +0000

    1.18.594
    [ci skip]

--- php/Exchange.php
+++ php/Exchange.php
@@ -34,7 +34,7 @@ use kornrunner\Eth;
 use kornrunner\Secp256k1;
 use kornrunner\Solidity;
 
-$version = '1.18.593';
+$version = '1.18.594';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -50,7 +50,7 @@ const PAD_WITH_ZERO = 1;
 
 class Exchange {
 
-    const VERSION = '1.18.593';
+    const VERSION = '1.18.594';
 
     public static $eth_units = array (
         'wei'        => '1',
--- python/ccxt/bitbay.py
+++ python/ccxt/bitbay.py
@@ -30,6 +30,7 @@ class bitbay (Exchange):
             'has': {
                 'CORS': True,
                 'withdraw': True,
+                'fetchMyTrades': True,
             },
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/27766132-978a7bd8-5ece-11e7-9540-bc96d1e9bbb8.jpg',
@@ -37,11 +38,15 @@ class bitbay (Exchange):
                 'api': {
                     'public': 'https://bitbay.net/API/Public',
                     'private': 'https://bitbay.net/API/Trading/tradingApi.php',
+                    'v1_01Public': 'https://api.bitbay.net/rest',
+                    'v1_01Private': 'https://api.bitbay.net/rest',
                 },
                 'doc': [
                     'https://bitbay.net/public-api',
+                    'https://bitbay.net/en/private-api',
                     'https://bitbay.net/account/tab-api',
                     'https://github.com/BitBayNet/API',
+                    'https://docs.bitbay.net/v1.0.1-en/reference',
                 ],
                 'fees': 'https://bitbay.net/en/fees',
             },
@@ -68,47 +73,42 @@ class bitbay (Exchange):
                         'transactions',
                     ],
                 },
-            },
-            'markets': {
-                'BTC/USD': {'id': 'BTCUSD', 'symbol': 'BTC/USD', 'base': 'BTC', 'quote': 'USD', 'baseId': 'BTC', 'quoteId': 'USD'},
-                'BTC/EUR': {'id': 'BTCEUR', 'symbol': 'BTC/EUR', 'base': 'BTC', 'quote': 'EUR', 'baseId': 'BTC', 'quoteId': 'EUR'},
-                'BTC/PLN': {'id': 'BTCPLN', 'symbol': 'BTC/PLN', 'base': 'BTC', 'quote': 'PLN', 'baseId': 'BTC', 'quoteId': 'PLN'},
-                'LTC/USD': {'id': 'LTCUSD', 'symbol': 'LTC/USD', 'base': 'LTC', 'quote': 'USD', 'baseId': 'LTC', 'quoteId': 'USD'},
-                'LTC/EUR': {'id': 'LTCEUR', 'symbol': 'LTC/EUR', 'base': 'LTC', 'quote': 'EUR', 'baseId': 'LTC', 'quoteId': 'EUR'},
-                'LTC/PLN': {'id': 'LTCPLN', 'symbol': 'LTC/PLN', 'base': 'LTC', 'quote': 'PLN', 'baseId': 'LTC', 'quoteId': 'PLN'},
-                'LTC/BTC': {'id': 'LTCBTC', 'symbol': 'LTC/BTC', 'base': 'LTC', 'quote': 'BTC', 'baseId': 'LTC', 'quoteId': 'BTC'},
-                'ETH/USD': {'id': 'ETHUSD', 'symbol': 'ETH/USD', 'base': 'ETH', 'quote': 'USD', 'baseId': 'ETH', 'quoteId': 'USD'},
-                'ETH/EUR': {'id': 'ETHEUR', 'symbol': 'ETH/EUR', 'base': 'ETH', 'quote': 'EUR', 'baseId': 'ETH', 'quoteId': 'EUR'},
-                'ETH/PLN': {'id': 'ETHPLN', 'symbol': 'ETH/PLN', 'base': 'ETH', 'quote': 'PLN', 'baseId': 'ETH', 'quoteId': 'PLN'},
-                'ETH/BTC': {'id': 'ETHBTC', 'symbol': 'ETH/BTC', 'base': 'ETH', 'quote': 'BTC', 'baseId': 'ETH', 'quoteId': 'BTC'},
-                'LSK/USD': {'id': 'LSKUSD', 'symbol': 'LSK/USD', 'base': 'LSK', 'quote': 'USD', 'baseId': 'LSK', 'quoteId': 'USD'},
-                'LSK/EUR': {'id': 'LSKEUR', 'symbol': 'LSK/EUR', 'base': 'LSK', 'quote': 'EUR', 'baseId': 'LSK', 'quoteId': 'EUR'},
-                'LSK/PLN': {'id': 'LSKPLN', 'symbol': 'LSK/PLN', 'base': 'LSK', 'quote': 'PLN', 'baseId': 'LSK', 'quoteId': 'PLN'},
-                'LSK/BTC': {'id': 'LSKBTC', 'symbol': 'LSK/BTC', 'base': 'LSK', 'quote': 'BTC', 'baseId': 'LSK', 'quoteId': 'BTC'},
-                'BCH/USD': {'id': 'BCCUSD', 'symbol': 'BCH/USD', 'base': 'BCH', 'quote': 'USD', 'baseId': 'BCC', 'quoteId': 'USD'},
-                'BCH/EUR': {'id': 'BCCEUR', 'symbol': 'BCH/EUR', 'base': 'BCH', 'quote': 'EUR', 'baseId': 'BCC', 'quoteId': 'EUR'},
-                'BCH/PLN': {'id': 'BCCPLN', 'symbol': 'BCH/PLN', 'base': 'BCH', 'quote': 'PLN', 'baseId': 'BCC', 'quoteId': 'PLN'},
-                'BCH/BTC': {'id': 'BCCBTC', 'symbol': 'BCH/BTC', 'base': 'BCH', 'quote': 'BTC', 'baseId': 'BCC', 'quoteId': 'BTC'},
-                'BTG/USD': {'id': 'BTGUSD', 'symbol': 'BTG/USD', 'base': 'BTG', 'quote': 'USD', 'baseId': 'BTG', 'quoteId': 'USD'},
-                'BTG/EUR': {'id': 'BTGEUR', 'symbol': 'BTG/EUR', 'base': 'BTG', 'quote': 'EUR', 'baseId': 'BTG', 'quoteId': 'EUR'},
-                'BTG/PLN': {'id': 'BTGPLN', 'symbol': 'BTG/PLN', 'base': 'BTG', 'quote': 'PLN', 'baseId': 'BTG', 'quoteId': 'PLN'},
-                'BTG/BTC': {'id': 'BTGBTC', 'symbol': 'BTG/BTC', 'base': 'BTG', 'quote': 'BTC', 'baseId': 'BTG', 'quoteId': 'BTC'},
-                'DASH/USD': {'id': 'DASHUSD', 'symbol': 'DASH/USD', 'base': 'DASH', 'quote': 'USD', 'baseId': 'DASH', 'quoteId': 'USD'},
-                'DASH/EUR': {'id': 'DASHEUR', 'symbol': 'DASH/EUR', 'base': 'DASH', 'quote': 'EUR', 'baseId': 'DASH', 'quoteId': 'EUR'},
-                'DASH/PLN': {'id': 'DASHPLN', 'symbol': 'DASH/PLN', 'base': 'DASH', 'quote': 'PLN', 'baseId': 'DASH', 'quoteId': 'PLN'},
-                'DASH/BTC': {'id': 'DASHBTC', 'symbol': 'DASH/BTC', 'base': 'DASH', 'quote': 'BTC', 'baseId': 'DASH', 'quoteId': 'BTC'},
-                'GAME/USD': {'id': 'GAMEUSD', 'symbol': 'GAME/USD', 'base': 'GAME', 'quote': 'USD', 'baseId': 'GAME', 'quoteId': 'USD'},
-                'GAME/EUR': {'id': 'GAMEEUR', 'symbol': 'GAME/EUR', 'base': 'GAME', 'quote': 'EUR', 'baseId': 'GAME', 'quoteId': 'EUR'},
-                'GAME/PLN': {'id': 'GAMEPLN', 'symbol': 'GAME/PLN', 'base': 'GAME', 'quote': 'PLN', 'baseId': 'GAME', 'quoteId': 'PLN'},
-                'GAME/BTC': {'id': 'GAMEBTC', 'symbol': 'GAME/BTC', 'base': 'GAME', 'quote': 'BTC', 'baseId': 'GAME', 'quoteId': 'BTC'},
-                'XRP/USD': {'id': 'XRPUSD', 'symbol': 'XRP/USD', 'base': 'XRP', 'quote': 'USD', 'baseId': 'XRP', 'quoteId': 'USD'},
-                'XRP/EUR': {'id': 'XRPEUR', 'symbol': 'XRP/EUR', 'base': 'XRP', 'quote': 'EUR', 'baseId': 'XRP', 'quoteId': 'EUR'},
-                'XRP/PLN': {'id': 'XRPPLN', 'symbol': 'XRP/PLN', 'base': 'XRP', 'quote': 'PLN', 'baseId': 'XRP', 'quoteId': 'PLN'},
-                'XRP/BTC': {'id': 'XRPBTC', 'symbol': 'XRP/BTC', 'base': 'XRP', 'quote': 'BTC', 'baseId': 'XRP', 'quoteId': 'BTC'},
-                # 'XIN/USD': {'id': 'XINUSD', 'symbol': 'XIN/USD', 'base': 'XIN', 'quote': 'USD', 'baseId': 'XIN', 'quoteId': 'USD'},
-                # 'XIN/EUR': {'id': 'XINEUR', 'symbol': 'XIN/EUR', 'base': 'XIN', 'quote': 'EUR', 'baseId': 'XIN', 'quoteId': 'EUR'},
-                # 'XIN/PLN': {'id': 'XINPLN', 'symbol': 'XIN/PLN', 'base': 'XIN', 'quote': 'PLN', 'baseId': 'XIN', 'quoteId': 'PLN'},
-                'XIN/BTC': {'id': 'XINBTC', 'symbol': 'XIN/BTC', 'base': 'XIN', 'quote': 'BTC', 'baseId': 'XIN', 'quoteId': 'BTC'},
+                'v1_01Public': {
+                    'get': [
+                        'trading/ticker',
+                        'trading/ticker/{symbol}',
+                        'trading/stats',
+                        'trading/orderbook/{symbol}',
+                        'trading/transactions/{symbol}',
+                        'trading/candle/history/{symbol}/{resolution}',
+                    ],
+                },
+                'v1_01Private': {
+                    'get': [
+                        'payments/withdrawal/{detailId}',
+                        'payments/deposit/{detailId}',
+                        'trading/offer',
+                        'trading/config/{symbol}',
+                        'trading/history/transactions',
+                        'balances/BITBAY/history',
+                        'balances/BITBAY/balance',
+                        'fiat_cantor/rate/{baseId}/{quoteId}',
+                        'fiat_cantor/history',
+                    ],
+                    'post': [
+                        'trading/offer/{symbol}',
+                        'trading/config/{symbol}',
+                        'balances/BITBAY/balance',
+                        'balances/BITBAY/balance/transfer/{source}/{destination}',
+                        'fiat_cantor/exchange',
+                    ],
+                    'delete': [
+                        'trading/offer/{symbol}/{id}/{side}/{price}',
+                    ],
+                    'put': [
+                        'balances/BITBAY/balance/{id}',
+                    ],
+                },
             },
             'fees': {
                 'trading': {
@@ -151,6 +151,86 @@ class bitbay (Exchange):
                 '510': ExchangeError,  # Invalid market name
             },
         })
+
+    def fetch_markets(self, params={}):
+        #   {status: 'Ok',
+        #     items:
+        #     {'BSV-USD':
+        #      {market:
+        #        {code: 'BSV-USD',
+        #          first: {currency: 'BSV', minOffer: '0.00035', scale: 8},
+        #          second: {currency: 'USD', minOffer: '5', scale: 2} },
+        #       time: '1557569762154',
+        #           highestBid: '52.31',
+        #       lowestAsk: '62.99',
+        #       rate: '63',
+        #       previousRate: '51.21'},
+        #      ...
+        response = self.v1_01PublicGetTradingTicker({})
+        if response['status'] != 'Ok':
+            raise ExchangeError(self.id + ' tickers query failed ' + self.json(response))
+        result = []
+        symbols = list(response['items'].keys())
+        for i in range(0, len(symbols)):
+            symbol = symbols[i]
+            item = response['items'][symbol]
+            nativeMarket = item['market']
+            baseId = self.safe_string(nativeMarket['first'], 'currency')
+            quoteId = self.safe_string(nativeMarket['second'], 'currency')
+            id = baseId + quoteId
+            base = self.common_currency_code(baseId)
+            quote = self.common_currency_code(quoteId)
+            precision = self.safe_integer(nativeMarket['second'], 'scale')
+            # todo: check that the limits have ben interpreted correctly
+            # todo: parse the fees page
+            result.append({
+                'id': id,
+                'symbol': base + '/' + quote,
+                'base': base,
+                'quote': quote,
+                'baseId': baseId,
+                'quoteId': quoteId,
+                'precision': precision,
+                'active': None,
+                'fee': None,
+                'limits': {
+                    'amount': {
+                        'min': self.safe_float(nativeMarket['first'], 'minOffer'),
+                    },
+                    'cost': {
+                        'min': self.safe_float(nativeMarket['second'], 'minOffer'),
+                    },
+                },
+                'info': item,
+            })
+        return result
+
+    def fetch_my_trades(self, symbol=None, since=None, limit=None, params={}):
+        self.load_markets()
+        request = self.extend({
+            'markets': [self.market_id(symbol)] if symbol else [],
+        }, params)
+        response = self.v1_01PrivateGetTradingHistoryTransactions({'query': self.json(request)})
+        if response['status'] != 'Ok':
+            raise ExchangeError(self.id + ' balances query failed ' + self.json(response))
+        #   {status: 'Ok',
+        #     totalRows: '67',
+        #     items:
+        #     [{id: 'b54659a0-51b5-42a0-80eb-2ac5357ccee2',
+        #         market: 'BTC-EUR',
+        #         time: '1541697096247',
+        #         amount: '0.00003',
+        #         rate: '4341.44',
+        #         initializedBy: 'Sell',
+        #         wasTaker: False,
+        #         userAction: 'Buy',
+        #         offerId: 'bd19804a-6f89-4a69-adb8-eb078900d006',
+        #         commissionValue: null}, ...
+        items = response['items']
+        result = self.parse_trades(items, None, since, limit)
+        if symbol is None:
+            return result
+        return self.filter_by_symbol(result, symbol)
 
     def fetch_balance(self, params={}):
         response = self.privatePostInfo()
@@ -212,6 +292,56 @@ class bitbay (Exchange):
         }
 
     def parse_trade(self, trade, market):
+        if 'tid' in trade:
+            return self.parse_public_trade(trade, market)
+        else:
+            return self.parse_my_trade(trade, market)
+
+    def parse_my_trade(self, trade, market):
+        #   {id: '5b6780e2-5bac-4ac7-88f4-b49b5957d33a',
+        #     market: 'BTC-EUR',
+        #     time: '1520719374684',
+        #     amount: '0.3',
+        #     rate: '7502',
+        #     initializedBy: 'Sell',
+        #     wasTaker: True,
+        #     userAction: 'Sell',
+        #     offerId: 'd093b0aa-b9c9-4a52-b3e2-673443a6188b',
+        #     commissionValue: null},
+        timestamp = self.safe_integer(trade, 'time')
+        userAction = self.safe_string(trade, 'userAction')
+        takerOrMaker = self.safe_string(trade, 'wasTaker') == 'taker' if 'true' else 'maker'
+        price = self.safe_float(trade, 'rate')
+        amount = self.safe_float(trade, 'amount')
+        commissionValue = self.safe_float(trade, 'commissionValue')
+        fee = None
+        if commissionValue is not None:
+            # it always seems to be null so don't know what currency to use
+            fee = {
+                'currency': None,
+                'cost': commissionValue,
+            }
+        marketId = self.safe_string(trade, 'market')
+        order = self.safe_string(trade, 'offerId')
+        # todo: check self logic
+        type = 'limit' if order else 'market'
+        return {
+            'id': self.safe_string(trade, 'id'),
+            'order': order,
+            'timestamp': timestamp,
+            'datetime': self.iso8601(timestamp),
+            'symbol': self.find_symbol(marketId.replace('-', '')),
+            'type': type,
+            'side': userAction == 'buy' if 'Buy' else 'sell',
+            'price': price,
+            'amount': amount,
+            'cost': price * amount,
+            'takerOrMaker': takerOrMaker,
+            'fee': fee,
+            'info': trade,
+        }
+
+    def parse_public_trade(self, trade, market):
         timestamp = trade['date'] * 1000
         return {
             'id': trade['tid'],
@@ -287,7 +417,30 @@ class bitbay (Exchange):
         if api == 'public':
             query = self.omit(params, self.extract_params(path))
             url += '/' + self.implode_params(path, params) + '.json'
-            url += '?' + self.urlencode(query)
+            if query:
+                url += '?' + self.urlencode(query)
+        elif api == 'v1_01Public':
+            query = self.omit(params, self.extract_params(path))
+            url += '/' + self.implode_params(path, params)
+            if query:
+                url += '?' + self.urlencode(query)
+        elif api == 'v1_01Private':
+            self.check_required_credentials()
+            query = self.omit(params, self.extract_params(path))
+            url += '/' + self.implode_params(path, params)
+            if query:
+                url += '?' + self.urlencode(query)
+            nonce = self.now()
+            payload = self.apiKey + nonce
+            if body is not None:
+                body = self.json(body)
+            headers = {
+                'Request-Timestamp': nonce,
+                'Operation-Id': self.uuid(),
+                'API-Key': self.apiKey,
+                'API-Hash': self.hmac(self.encode(payload), self.encode(self.secret), hashlib.sha512),
+                'Content-Type': 'application/json',
+            }
         else:
             self.check_required_credentials()
             body = self.urlencode(self.extend({
