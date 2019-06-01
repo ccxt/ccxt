02f3137cc9cc3d680bafeb4b721c5e5c25dea71e
Igor Kroitor <igor.kroitor@gmail.com>
2019-06-01 06:12:07 +0300

    cut down encryptEngines

--- js/static_dependencies/node-rsa/encryptEngines/encryptEngines.js
+++ js/static_dependencies/node-rsa/encryptEngines/encryptEngines.js
@@ -1,17 +1,8 @@
-var crypt = require('crypto');
+'use strict';
 
 module.exports = {
-    getEngine: function (keyPair, options) {
-        var engine = require('./js.js');
-        if (options.environment === 'node') {
-            if (typeof crypt.publicEncrypt === 'function' && typeof crypt.privateDecrypt === 'function') {
-                if (typeof crypt.privateEncrypt === 'function' && typeof crypt.publicDecrypt === 'function') {
-                    engine = require('./io.js');
-                } else {
-                    engine = require('./node12.js');
-                }
-            }
-        }
-        return engine(keyPair, options);
+    getEngine (keyPair, options) {
+        const engine = require ('./js.js')
+        return engine (keyPair, options)
     }
-};
+}
