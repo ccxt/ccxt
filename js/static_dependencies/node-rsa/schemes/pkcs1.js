dbc66b607729aa0581db4d08a99490d13320a166
Igor Kroitor <igor.kroitor@gmail.com>
2019-06-03 08:16:02 +0300

    Update pkcs1.js

--- js/static_dependencies/node-rsa/schemes/pkcs1.js
+++ js/static_dependencies/node-rsa/schemes/pkcs1.js
@@ -3,7 +3,7 @@
  */
 
 var BigInteger = require('../libs/jsbn');
-var CryptoJS = require('crypto-js');
+var CryptoJS = require('../../crypto-js/crypto-js');
 var constants = require('constants');
 var SIGN_INFO_HEAD = {
     md2: Buffer.from('3020300c06082a864886f70d020205000410', 'hex'),
