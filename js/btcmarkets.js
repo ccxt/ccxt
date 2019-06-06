bef38370ce555db8f6a8d3730f6c4a64737a1426
Igor Kroitor <igor.kroitor@gmail.com>
2019-06-07 02:16:57 +0300

    Update btcmarkets.js

--- js/btcmarkets.js
+++ js/btcmarkets.js
@@ -596,7 +596,7 @@ module.exports = class btcmarkets extends Exchange {
             };
             if (method === 'post') {
                 headers['Content-Type'] = 'application/json';
-                auth = uri + "\n" + nonce + "\n"; // eslint-disable quotes
+                auth = uri + "\n" + nonce + "\n"; // eslint-disable-line quotes
                 body = this.json (params);
                 auth += body;
             } else {
@@ -605,9 +605,9 @@ module.exports = class btcmarkets extends Exchange {
                 if (Object.keys (query).length) {
                     queryString = this.urlencode (query);
                     url += '?' + queryString;
-                    queryString += "\n"; // eslint-disable quotes
+                    queryString += "\n"; // eslint-disable-line quotes
                 }
-                auth = uri + "\n" + queryString + nonce + "\n"; // eslint-disable quotes
+                auth = uri + "\n" + queryString + nonce + "\n"; // eslint-disable-line quotes
             }
             let secret = this.base64ToBinary (this.secret);
             let signature = this.hmac (this.encode (auth), secret, 'sha512', 'base64');
