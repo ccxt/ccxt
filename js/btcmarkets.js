88d8c992e553c1ef89862d53d39a265579443f6e
Nick Pomfret <nick-github@snowmonkey.co.uk>
2019-06-06 13:10:30 +0100

    [btcmarkets] added fetchTransactions

--- js/btcmarkets.js
+++ js/btcmarkets.js
@@ -49,9 +49,10 @@ module.exports = class btcmarkets extends Exchange {
                     'get': [
                         'account/balance',
                         'account/{id}/tradingfee',
+                        'fundtransfer/history',
                         'v2/order/open',
                         'v2/order/open/{id}',
-                        'v2/order/history/{id}',
+                        'v2/order/history/{instrument}/{currency}/',
                         'v2/order/trade/history/{id}',
                         'v2/transaction/history/{currency}',
                     ],
@@ -85,6 +86,104 @@ module.exports = class btcmarkets extends Exchange {
         });
     }
 
+    async fetchTransactions (code = undefined, since = undefined, limit = undefined, params = {}) {
+        await this.loadMarkets ();
+        const request = {
+            'limit': limit,
+            'since': since,
+        };
+        const response = await this.privateGetFundtransferHistory (this.extend (request, params));
+        if (!response['success'])
+            throw new ExchangeError (this.id + ' fetch transactions failed ' + this.json (response));
+        const results = [];
+        for (let i = 0; i < response['fundTransfers'].length; i++) {
+            //   { status: 'Complete',
+            //     fundTransferId: 1904311906,
+            //     description: 'ETH withdraw from [me@email.com] to Address: 0xF123aa44FadEa913a7da99cc2eE202Db684Ce0e3 amount: 8.28965701 fee: 0.00000000',
+            //     creationTime: 1529418358525,
+            //     currency: 'ETH',
+            //     amount: 828965701,
+            //     fee: 0,
+            //     transferType: 'WITHDRAW',
+            //     errorMessage: null,
+            //     lastUpdate: 1529418376754,
+            //     cryptoPaymentDetail:
+            //     { address: '0xF123aa44FadEa913a7da99cc2eE202Db684Ce0e3',
+            //       txId: '0x8fe483b6f9523559b9ebffb29624f98e86227d2660d4a1fd4785d45e51c662c2' } }
+            //
+            //   { status: 'Complete',
+            //     fundTransferId: 494077500,
+            //     description: 'BITCOIN Deposit, B 0.1000',
+            //     creationTime: 1501077601015,
+            //     currency: 'BTC',
+            //     amount: 10000000,
+            //     fee: 0,
+            //     transferType: 'DEPOSIT',
+            //     errorMessage: null,
+            //     lastUpdate: 1501077601133,
+            //     cryptoPaymentDetail: null }
+            //
+            //   { "fee": 0,
+            //     "amount": 56,
+            //     "status": "Complete",
+            //     "currency": "BCHABC",
+            //     "lastUpdate": 1542339164044,
+            //     "description": "BitcoinCashABC Deposit, P 0.00000056",
+            //     "creationTime": 1542339164003,
+            //     "errorMessage": null,
+            //     "transferType": "DEPOSIT",
+            //     "fundTransferId": 2527326972,
+            //     "cryptoPaymentDetail": null}
+            const item = response['fundTransfers'][i];
+            const timestamp = this.safeInteger (item, 'creationTime');
+            const lastUpdate = this.safeInteger (item, 'lastUpdate');
+            const transferType = this.safeString (item, 'transferType');
+            let txid = undefined;
+            let address = undefined;
+            if (item['cryptoPaymentDetail']) {
+                address = this.safeString (item['cryptoPaymentDetail'], 'address');
+                txid = this.safeString (item['cryptoPaymentDetail'], 'txId');
+            }
+            let type = undefined;
+            if (transferType === 'DEPOSIT')
+                type = 'deposit';
+            else if (transferType === 'WITHDRAW') {
+                type = 'withdrawal';
+            } else {
+                type = transferType;
+            }
+            const fee = this.safeFloat (item, 'fee');
+            const rawStatus = this.safeString (item, 'status');
+            let status = undefined;
+            if (rawStatus === 'Complete') {
+                status = 'ok';
+            } else {
+                status = rawStatus;
+            }
+            const ccy = this.safeString (item, 'currency');
+            const code = this.commonCurrencyCode (ccy);
+            results.push ({
+                'id': this.safeString (item, 'fundTransferId'),
+                'txid': txid,
+                'timestamp': timestamp,
+                'datetime': this.iso8601 (timestamp),
+                'address': address,
+                'tag': undefined,
+                'type': type,
+                'amount': this.safeFloat (item, 'amount') / 100000000,
+                'currency': code,
+                'status': status,
+                'updated': lastUpdate,
+                'fee': {
+                    'currency': code,
+                    'cost': fee,
+                },
+                'info': item,
+            });
+        }
+        return results;
+    }
+
     async fetchMarkets (params = {}) {
         let response = await this.publicGetV2MarketActive ();
         let result = [];
@@ -97,6 +196,7 @@ module.exports = class btcmarkets extends Exchange {
             let base = this.commonCurrencyCode (baseId);
             let quote = this.commonCurrencyCode (quoteId);
             let symbol = base + '/' + quote;
+            // todo: refactor this
             let fee = (quote === 'AUD') ? 0.0085 : 0.0022;
             let pricePrecision = 2;
             let amountPrecision = 4;
@@ -444,9 +544,11 @@ module.exports = class btcmarkets extends Exchange {
             throw new ArgumentsRequired (this.id + ': fetchOrders requires a `symbol` argument.');
         }
         await this.loadMarkets ();
-        let market = this.market (symbol);
-        let request = this.createPaginatedRequest (market, since, limit);
-        let response = await this.privatePostOrderHistory (this.extend (request, params));
+        const market = this.market (symbol);
+        const request = this.createPaginatedRequest (market, since, limit);
+        const response = await this.privatePostOrderHistory (this.extend (request, params));
+        if (!response['success'])
+            throw new ExchangeError (this.id + ' fetch orders failed ' + this.json (response));
         return this.parseOrders (response['orders'], market);
     }
 
@@ -487,16 +589,26 @@ module.exports = class btcmarkets extends Exchange {
         if (api === 'private') {
             this.checkRequiredCredentials ();
             let nonce = this.nonce ().toString ();
-            // eslint-disable-next-line quotes
-            let auth = uri + "\n" + nonce + "\n";
+            let auth = undefined;
             headers = {
-                'Content-Type': 'application/json',
                 'apikey': this.apiKey,
                 'timestamp': nonce,
             };
-            if (method === 'POST') {
+            if (method === 'post') {
+                // eslint-disable-next-line quotes
+                headers['Content-Type'] = 'application/json';
+                auth = uri + '\n' + nonce + '\n';
                 body = this.json (params);
                 auth += body;
+            } else {
+                let queryStringParams = this.omit (params, this.extractParams (path));
+                let queryString = '';
+                if (Object.keys (queryStringParams).length) {
+                    let query = this.ordered (queryStringParams);
+                    queryString = this.urlencode (query);
+                    url += '?' + queryString;
+                }
+                auth = uri + '\n' + (queryString ? queryString + '\n' : '') + nonce + '\n';
             }
             let secret = this.base64ToBinary (this.secret);
             let signature = this.hmac (this.encode (auth), secret, 'sha512', 'base64');
