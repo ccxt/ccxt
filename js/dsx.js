a54c0f8105fbd79aaaebf1fb8637d8f522d1edb6
Igor Kroitor <igor.kroitor@gmail.com>
2019-06-08 03:22:25 +0300

    dsx doc url

--- js/dsx.js
+++ js/dsx.js
@@ -35,6 +35,7 @@ module.exports = class dsx extends liqui {
                 },
                 'www': 'https://dsx.uk',
                 'doc': [
+                    'https://dsx.uk/developers/publicApiV2',
                     'https://api.dsx.uk',
                     'https://dsx.uk/api_docs/public',
                     'https://dsx.uk/api_docs/private',
