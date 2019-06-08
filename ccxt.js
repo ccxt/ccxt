e5950b1e7771a0005a01b48cf0af609a6642e4b6
Igor Kroitor <igor.kroitor@gmail.com>
2019-06-08 10:46:49 +0300

    quadrigacx delisted

--- ccxt.js
+++ ccxt.js
@@ -162,7 +162,6 @@ const exchanges = {
     'okex3':                   require ('./js/okex3.js'),
     'paymium':                 require ('./js/paymium.js'),
     'poloniex':                require ('./js/poloniex.js'),
-    'quadrigacx':              require ('./js/quadrigacx.js'),
     'rightbtc':                require ('./js/rightbtc.js'),
     'southxchange':            require ('./js/southxchange.js'),
     'stronghold':              require ('./js/stronghold.js'),
