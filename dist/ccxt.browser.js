f8fd0ca3c3b8b9b9046efb4d584d8fd1191a8f0b
Travis CI <travis@travis-ci.org>
2019-06-08 22:01:51 +0000

    1.18.655
    [ci skip]

--- dist/ccxt.browser.js
+++ dist/ccxt.browser.js
@@ -45,7 +45,7 @@ const Exchange  = require ('./js/base/Exchange')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.18.654'
+const version = '1.18.655'
 
 Exchange.ccxtVersion = version
 
@@ -40932,10 +40932,11 @@ module.exports = class fcoin extends Exchange {
 
     async fetchTicker (symbol, params = {}) {
         await this.loadMarkets ();
-        let market = this.market (symbol);
-        let ticker = await this.marketGetTickerSymbol (this.extend ({
+        const market = this.market (symbol);
+        const request = {
             'symbol': market['id'],
-        }, params));
+        };
+        const ticker = await this.marketGetTickerSymbol (this.extend (request, params));
         return this.parseTicker (ticker['data'], market);
     }
 
