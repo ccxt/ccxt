52687d560e57da9fbd710d75026a466b053adc59
Timofey Polozov <timofey.polozov@latoken.com>
2019-06-11 14:38:27 +0300

    fix probable error

--- js/latoken.js
+++ js/latoken.js
@@ -490,7 +490,7 @@ module.exports = class latoken extends Exchange {
         let executedAmount = this.safeFloat (response, 'executedAmount');
         let reaminingAmount = this.safeFloat (response, 'reaminingAmount');
         let timeCreated = this.safeValue (response, 'timeCreated');
-        let timeFilled = this.safeValue (response, 'timeFilled'); //(this.safeValue (response, 'timeFilled') === undefined) ? null : this.safeValue (response, 'timeFilled');
+        let timeFilled = this.safeValue (response, 'timeFilled');
         return {
             'orderId': orderId,
             'cliOrdId': cliOrdId,
