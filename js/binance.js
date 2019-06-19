1c9c10c00155b0e0c5843bc48e57ccf2c7c5fbc0
Ahmed Bodiwala <ahmedbodi@crypto-expert.com>
2019-06-19 04:21:42 +0100

    [Binance] Add FetchStatus Call

--- js/binance.js
+++ js/binance.js
@@ -366,6 +366,15 @@ module.exports = class binance extends Exchange {
         };
     }
 
+    async fetchStatus (params = {}) {
+        const systemStatus = await this.wapiGetSystemStatus()
+        const status = systemStatus.status === 0 ? 'ok' : 'maintenance'
+        return this.status = this.extend (this.status, {
+            'status': status,
+            'updated': this.milliseconds (),
+        })
+    }
+
     async fetchTicker (symbol, params = {}) {
         await this.loadMarkets ();
         let market = this.market (symbol);
