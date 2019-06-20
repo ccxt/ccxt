329681346ce5a06b72bd5389208bc339d92c0400
Igor Kroitor <igor.kroitor@gmail.com>
2019-06-20 23:33:49 +0300

    deribit empty timeframes removed

--- js/deribit.js
+++ js/deribit.js
@@ -26,7 +26,6 @@ module.exports = class deribit extends Exchange {
                 'fetchMyTrades': true,
                 'fetchTickers': false,
             },
-            'timeframes': {},
             'urls': {
                 'test': 'https://test.deribit.com',
                 'logo': 'https://user-images.githubusercontent.com/1294454/41933112-9e2dd65a-798b-11e8-8440-5bab2959fcb8.jpg',
