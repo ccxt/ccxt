37b8c10b1d27c7ca2adeb7504b4e79b719b5f799
Igor Kroitor <igor.kroitor@gmail.com>
2019-06-20 23:38:01 +0300

    coinegg referral url

--- js/coinegg.js
+++ js/coinegg.js
@@ -29,7 +29,7 @@ module.exports = class coinegg extends Exchange {
                 'www': 'https://www.coinegg.com',
                 'doc': 'https://www.coinegg.com/explain.api.html',
                 'fees': 'https://www.coinegg.com/fee.html',
-                'referral': 'http://www.coinegg.com/user/register?invite=523218',
+                'referral': 'https://www.coinegg.com/user/register?invite=523218',
             },
             'api': {
                 'web': {
