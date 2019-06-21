02eba6ed728d0cd36eb1e2b1a840d4f00b054335
Igor Kroitor <igor.kroitor@gmail.com>
2019-06-22 01:05:28 +0300

    added southxchange to test.ticker.js exceptions

--- js/test/Exchange/test.ticker.js
+++ js/test/Exchange/test.ticker.js
@@ -77,12 +77,14 @@ module.exports = (exchange, ticker, method, symbol) => {
         'coss',
         'mandala',
         'okex',
+        'southxchange', // https://user-images.githubusercontent.com/1294454/59953532-314bea80-9489-11e9-85b3-2a711ca49aa7.png
 
     ].includes (exchange.id)) {
 
         if (ticker['baseVolume'] || ticker['quoteVolume']) {
-            if (ticker['bid'] && ticker['ask'])
-                assert (ticker['bid'] <= ticker['ask'])
+            if (ticker['bid'] && ticker['ask']) {
+                assert (ticker['bid'] <= ticker['ask'], 'ticker bid is less than ticker ask!')
+            }
         }
 
     }
