222d92c00ddd5e9832abd2d8fdc869d4ed9b6f62
Igor Kroitor <igor.kroitor@gmail.com>
2019-06-24 06:00:40 +0300

    pep8 typo fix

--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -313,7 +313,7 @@ class Exchange(object):
         #     'User-Agent': 'ccxt/' + __version__ + ' (+https://github.com/ccxt/ccxt) Python/' + version
         # }
 
-        self.origin = self.uuid ()
+        self.origin = self.uuid()
         self.userAgent = default_user_agent()
 
         settings = self.deep_extend(self.describe(), config)
