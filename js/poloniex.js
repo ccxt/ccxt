05bcb61a87b354ee3ea2ad68489e1036ab6cd82e
Nick Pomfret <nick-github@snowmonkey.co.uk>
2019-06-25 13:18:57 +0100

    [poloniex] updated fees (https://github.com/ccxt/ccxt/issues/5356)

--- js/poloniex.js
+++ js/poloniex.js
@@ -107,12 +107,10 @@ module.exports = class poloniex extends Exchange {
                     ],
                 },
             },
-            // Fees are tier-based. More info: https://poloniex.com/fees/
-            // Rates below are highest possible.
             'fees': {
                 'trading': {
-                    'maker': 0.001,
-                    'taker': 0.002,
+                    'maker': 0.15 / 100,
+                    'taker': 0.25 / 100,
                 },
                 'funding': {},
             },
