95ae4c459073c5d408fe87ef2cd72141262df048
Igor Kroitor <igor.kroitor@gmail.com>
2019-07-06 19:22:38 +0000

    1.18.902
    [ci skip]

--- ccxt.js
+++ ccxt.js
@@ -35,7 +35,7 @@ const Exchange  = require ('./js/base/Exchange')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.18.901'
+const version = '1.18.902'
 
 Exchange.ccxtVersion = version
 
