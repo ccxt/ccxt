65c7f5436f9cc8233403071ec9602d0970e456c9
Igor Kroitor <igor.kroitor@gmail.com>
2019-07-08 19:21:52 +0300

    exchange.py reverted to merge the php fix

--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -766,8 +766,6 @@ class Exchange(object):
 
     @staticmethod
     def implode_params(string, params):
-        if not isinstance(params, dict):
-            return string
         for key in params:
             string = string.replace('{' + key + '}', str(params[key]))
         return string
