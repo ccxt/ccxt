5e7948680e0134cad164b1b4ff3c3a2d978c0413
Igor Kroitor <igor.kroitor@gmail.com>
2019-07-08 14:46:44 +0300

    common.js regexAll

--- build/common.js
+++ build/common.js
@@ -40,10 +40,21 @@ function createFolderRecursively (folder) {
     }
 }
 
+function regexAll (text, array) {
+
+    for (let i in array) {
+        let regex = array[i][0]
+        regex = typeof regex === 'string' ? new RegExp (regex, 'g') : new RegExp (regex)
+        text = text.replace (regex, array[i][1])
+    }
+    return text
+}
+
 module.exports = {
     replaceInFile,
     copyFile,
     overwriteFile,
     createFolder,
-    createFolderRecursively
+    createFolderRecursively,
+    regexAll
 }
