871df666b42abc891f49a46fa2674f1ac1785be6
Igor Kroitor <igor.kroitor@gmail.com>
2019-07-08 14:27:02 +0300

    Update common.js

--- build/common.js
+++ build/common.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const fs = require ('fs')
+    , path = require ('path')
 
 function replaceInFile (filename, regex, replacement) {
     let contents = fs.readFileSync (filename, 'utf8')
@@ -9,6 +10,40 @@ function replaceInFile (filename, regex, replacement) {
     fs.writeFileSync (filename, newContents)
 }
 
+function copyFile (oldName, newName) {
+    let contents = fs.readFileSync (oldName, 'utf8')
+    fs.truncateSync (newName)
+    fs.writeFileSync (newName, contents)
+}
+
+function overwriteFile (filename, contents) {
+    // log.cyan ('Overwriting → ' + filename.yellow)
+    fs.closeSync (fs.openSync (filename, 'a'));
+    fs.truncateSync (filename)
+    fs.writeFileSync (filename, contents)
+}
+
+function createFolder (folder) {
+    try {
+        fs.mkdirSync (folder)
+    } catch (err) {
+        if (err.code !== 'EEXIST') {
+            throw err
+        }
+    }
+}
+
+function createFolderRecursively (folder) {
+    const parts = folder.split (path.sep)
+    for (let i = 1; i <= parts.length; i++) {
+        createFolder (path.join.apply (null, parts.slice (0, i)))
+    }
+}
+
 module.exports = {
     replaceInFile,
+    copyFile,
+    overwriteFile,
+    createFolder,
+    createFolderRecursively
 }
