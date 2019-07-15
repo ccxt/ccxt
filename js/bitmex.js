8a2b0318ab77213f91e1731659a7c031762061a5
Carlo Revelli <carlo.revelli@berkeley.edu>
2019-07-15 17:15:27 +0100

    k transpiler

--- js/bitmex.js
+++ js/bitmex.js
@@ -871,7 +871,10 @@ module.exports = class bitmex extends Exchange {
             // 'endTime': '',    // ending date filter for results
         };
         if (limit !== undefined) {
-            request['count'] = limit + (this.options['fetchOHLCVOpenTimestamp'] ? 1 : 0); // default 100, max 500
+            if (this.options['fetchOHLCVOpenTimestamp']) {
+                limit = limit + 1;
+            }
+            request['count'] = limit; // default 100, max 500
         }
         // if since is not set, they will return candles starting from 2017-01-01
         if (since !== undefined) {
