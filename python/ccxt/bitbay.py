35b6227c0aa6e4c2b838fbc6c384ca08e77b4ab6
Travis CI <travis@travis-ci.org>
2019-08-04 14:40:50 +0000

    1.18.1007
    [ci skip]

--- python/ccxt/bitbay.py
+++ python/ccxt/bitbay.py
@@ -313,6 +313,347 @@ class bitbay (Exchange):
             'info': ticker,
         }
 
+    def fetch_ledger(self, code=None, since=None, limit=None, params={}):
+        balanceCurrencies = []
+        if code is not None:
+            currency = self.currency(code)
+            balanceCurrencies.append(currency['id'])
+        request = {
+            'balanceCurrencies': balanceCurrencies,
+        }
+        if since is not None:
+            request['fromTime'] = since
+        if limit is not None:
+            request['limit'] = limit
+        request = self.extend(request, params)
+        response = self.v1_01PrivateGetBalancesBITBAYHistory({'query': self.json(request)})
+        items = response['items']
+        return self.parse_ledger(items, None, since, limit)
+
+    def parse_ledger_entry(self, item, currency=None):
+        #
+        #    FUNDS_MIGRATION
+        #    {
+        #      "historyId": "84ea7a29-7da5-4de5-b0c0-871e83cad765",
+        #      "balance": {
+        #        "id": "821ec166-cb88-4521-916c-f4eb44db98df",
+        #        "currency": "LTC",
+        #        "type": "CRYPTO",
+        #        "userId": "a34d361d-7bad-49c1-888e-62473b75d877",
+        #        "name": "LTC"
+        #      },
+        #      "detailId": null,
+        #      "time": 1506128252968,
+        #      "type": "FUNDS_MIGRATION",
+        #      "value": 0.0009957,
+        #      "fundsBefore": {"total": 0, "available": 0, "locked": 0},
+        #      "fundsAfter": {"total": 0.0009957, "available": 0.0009957, "locked": 0},
+        #      "change": {"total": 0.0009957, "available": 0.0009957, "locked": 0}
+        #    }
+        #
+        #    CREATE_BALANCE
+        #    {
+        #      "historyId": "d0fabd8d-9107-4b5e-b9a6-3cab8af70d49",
+        #      "balance": {
+        #        "id": "653ffcf2-3037-4ebe-8e13-d5ea1a01d60d",
+        #        "currency": "BTG",
+        #        "type": "CRYPTO",
+        #        "userId": "a34d361d-7bad-49c1-888e-62473b75d877",
+        #        "name": "BTG"
+        #      },
+        #      "detailId": null,
+        #      "time": 1508895244751,
+        #      "type": "CREATE_BALANCE",
+        #      "value": 0,
+        #      "fundsBefore": {"total": null, "available": null, "locked": null},
+        #      "fundsAfter": {"total": 0, "available": 0, "locked": 0},
+        #      "change": {"total": 0, "available": 0, "locked": 0}
+        #    }
+        #
+        #    BITCOIN_GOLD_FORK
+        #    {
+        #      "historyId": "2b4d52d3-611c-473d-b92c-8a8d87a24e41",
+        #      "balance": {
+        #        "id": "653ffcf2-3037-4ebe-8e13-d5ea1a01d60d",
+        #        "currency": "BTG",
+        #        "type": "CRYPTO",
+        #        "userId": "a34d361d-7bad-49c1-888e-62473b75d877",
+        #        "name": "BTG"
+        #      },
+        #      "detailId": null,
+        #      "time": 1508895244778,
+        #      "type": "BITCOIN_GOLD_FORK",
+        #      "value": 0.00453512,
+        #      "fundsBefore": {"total": 0, "available": 0, "locked": 0},
+        #      "fundsAfter": {"total": 0.00453512, "available": 0.00453512, "locked": 0},
+        #      "change": {"total": 0.00453512, "available": 0.00453512, "locked": 0}
+        #    }
+        #
+        #    ADD_FUNDS
+        #    {
+        #      "historyId": "3158236d-dae5-4a5d-81af-c1fa4af340fb",
+        #      "balance": {
+        #        "id": "3a7e7a1e-0324-49d5-8f59-298505ebd6c7",
+        #        "currency": "BTC",
+        #        "type": "CRYPTO",
+        #        "userId": "a34d361d-7bad-49c1-888e-62473b75d877",
+        #        "name": "BTC"
+        #      },
+        #      "detailId": "8e83a960-e737-4380-b8bb-259d6e236faa",
+        #      "time": 1520631178816,
+        #      "type": "ADD_FUNDS",
+        #      "value": 0.628405,
+        #      "fundsBefore": {"total": 0.00453512, "available": 0.00453512, "locked": 0},
+        #      "fundsAfter": {"total": 0.63294012, "available": 0.63294012, "locked": 0},
+        #      "change": {"total": 0.628405, "available": 0.628405, "locked": 0}
+        #    }
+        #
+        #    TRANSACTION_PRE_LOCKING
+        #    {
+        #      "historyId": "e7d19e0f-03b3-46a8-bc72-dde72cc24ead",
+        #      "balance": {
+        #        "id": "3a7e7a1e-0324-49d5-8f59-298505ebd6c7",
+        #        "currency": "BTC",
+        #        "type": "CRYPTO",
+        #        "userId": "a34d361d-7bad-49c1-888e-62473b75d877",
+        #        "name": "BTC"
+        #      },
+        #      "detailId": null,
+        #      "time": 1520706403868,
+        #      "type": "TRANSACTION_PRE_LOCKING",
+        #      "value": -0.1,
+        #      "fundsBefore": {"total": 0.63294012, "available": 0.63294012, "locked": 0},
+        #      "fundsAfter": {"total": 0.63294012, "available": 0.53294012, "locked": 0.1},
+        #      "change": {"total": 0, "available": -0.1, "locked": 0.1}
+        #    }
+        #
+        #    TRANSACTION_POST_OUTCOME
+        #    {
+        #      "historyId": "c4010825-231d-4a9c-8e46-37cde1f7b63c",
+        #      "balance": {
+        #        "id": "3a7e7a1e-0324-49d5-8f59-298505ebd6c7",
+        #        "currency": "BTC",
+        #        "type": "CRYPTO",
+        #        "userId": "a34d361d-7bad-49c1-888e-62473b75d877",
+        #        "name": "BTC"
+        #      },
+        #      "detailId": "bf2876bc-b545-4503-96c8-ef4de8233876",
+        #      "time": 1520706404032,
+        #      "type": "TRANSACTION_POST_OUTCOME",
+        #      "value": -0.01771415,
+        #      "fundsBefore": {"total": 0.63294012, "available": 0.53294012, "locked": 0.1},
+        #      "fundsAfter": {"total": 0.61522597, "available": 0.53294012, "locked": 0.08228585},
+        #      "change": {"total": -0.01771415, "available": 0, "locked": -0.01771415}
+        #    }
+        #
+        #    TRANSACTION_POST_INCOME
+        #    {
+        #      "historyId": "7f18b7af-b676-4125-84fd-042e683046f6",
+        #      "balance": {
+        #        "id": "ab43023b-4079-414c-b340-056e3430a3af",
+        #        "currency": "EUR",
+        #        "type": "FIAT",
+        #        "userId": "a34d361d-7bad-49c1-888e-62473b75d877",
+        #        "name": "EUR"
+        #      },
+        #      "detailId": "f5fcb274-0cc7-4385-b2d3-bae2756e701f",
+        #      "time": 1520706404035,
+        #      "type": "TRANSACTION_POST_INCOME",
+        #      "value": 628.78,
+        #      "fundsBefore": {"total": 0, "available": 0, "locked": 0},
+        #      "fundsAfter": {"total": 628.78, "available": 628.78, "locked": 0},
+        #      "change": {"total": 628.78, "available": 628.78, "locked": 0}
+        #    }
+        #
+        #    TRANSACTION_COMMISSION_OUTCOME
+        #    {
+        #      "historyId": "843177fa-61bc-4cbf-8be5-b029d856c93b",
+        #      "balance": {
+        #        "id": "ab43023b-4079-414c-b340-056e3430a3af",
+        #        "currency": "EUR",
+        #        "type": "FIAT",
+        #        "userId": "a34d361d-7bad-49c1-888e-62473b75d877",
+        #        "name": "EUR"
+        #      },
+        #      "detailId": "f5fcb274-0cc7-4385-b2d3-bae2756e701f",
+        #      "time": 1520706404050,
+        #      "type": "TRANSACTION_COMMISSION_OUTCOME",
+        #      "value": -2.71,
+        #      "fundsBefore": {"total": 766.06, "available": 766.06, "locked": 0},
+        #      "fundsAfter": {"total": 763.35,"available": 763.35, "locked": 0},
+        #      "change": {"total": -2.71, "available": -2.71, "locked": 0}
+        #    }
+        #
+        #    TRANSACTION_OFFER_COMPLETED_RETURN
+        #    {
+        #      "historyId": "cac69b04-c518-4dc5-9d86-e76e91f2e1d2",
+        #      "balance": {
+        #        "id": "3a7e7a1e-0324-49d5-8f59-298505ebd6c7",
+        #        "currency": "BTC",
+        #        "type": "CRYPTO",
+        #        "userId": "a34d361d-7bad-49c1-888e-62473b75d877",
+        #        "name": "BTC"
+        #      },
+        #      "detailId": null,
+        #      "time": 1520714886425,
+        #      "type": "TRANSACTION_OFFER_COMPLETED_RETURN",
+        #      "value": 0.00000196,
+        #      "fundsBefore": {"total": 0.00941208, "available": 0.00941012, "locked": 0.00000196},
+        #      "fundsAfter": {"total": 0.00941208, "available": 0.00941208, "locked": 0},
+        #      "change": {"total": 0, "available": 0.00000196, "locked": -0.00000196}
+        #    }
+        #
+        #    WITHDRAWAL_LOCK_FUNDS
+        #    {
+        #      "historyId": "03de2271-66ab-4960-a786-87ab9551fc14",
+        #      "balance": {
+        #        "id": "3a7e7a1e-0324-49d5-8f59-298505ebd6c7",
+        #        "currency": "BTC",
+        #        "type": "CRYPTO",
+        #        "userId": "a34d361d-7bad-49c1-888e-62473b75d877",
+        #        "name": "BTC"
+        #      },
+        #      "detailId": "6ad3dc72-1d6d-4ec2-8436-ca43f85a38a6",
+        #      "time": 1522245654481,
+        #      "type": "WITHDRAWAL_LOCK_FUNDS",
+        #      "value": -0.8,
+        #      "fundsBefore": {"total": 0.8, "available": 0.8, "locked": 0},
+        #      "fundsAfter": {"total": 0.8, "available": 0, "locked": 0.8},
+        #      "change": {"total": 0, "available": -0.8, "locked": 0.8}
+        #    }
+        #
+        #    WITHDRAWAL_SUBTRACT_FUNDS
+        #    {
+        #      "historyId": "b0308c89-5288-438d-a306-c6448b1a266d",
+        #      "balance": {
+        #        "id": "3a7e7a1e-0324-49d5-8f59-298505ebd6c7",
+        #        "currency": "BTC",
+        #        "type": "CRYPTO",
+        #        "userId": "a34d361d-7bad-49c1-888e-62473b75d877",
+        #        "name": "BTC"
+        #      },
+        #      "detailId": "6ad3dc72-1d6d-4ec2-8436-ca43f85a38a6",
+        #      "time": 1522246526186,
+        #      "type": "WITHDRAWAL_SUBTRACT_FUNDS",
+        #      "value": -0.8,
+        #      "fundsBefore": {"total": 0.8, "available": 0, "locked": 0.8},
+        #      "fundsAfter": {"total": 0, "available": 0, "locked": 0},
+        #      "change": {"total": -0.8, "available": 0, "locked": -0.8}
+        #    }
+        #
+        #    TRANSACTION_OFFER_ABORTED_RETURN
+        #    {
+        #      "historyId": "b1a3c075-d403-4e05-8f32-40512cdd88c0",
+        #      "balance": {
+        #        "id": "3a7e7a1e-0324-49d5-8f59-298505ebd6c7",
+        #        "currency": "BTC",
+        #        "type": "CRYPTO",
+        #        "userId": "a34d361d-7bad-49c1-888e-62473b75d877",
+        #        "name": "BTC"
+        #      },
+        #      "detailId": null,
+        #      "time": 1522512298662,
+        #      "type": "TRANSACTION_OFFER_ABORTED_RETURN",
+        #      "value": 0.0564931,
+        #      "fundsBefore": {"total": 0.44951311, "available": 0.39302001, "locked": 0.0564931},
+        #      "fundsAfter": {"total": 0.44951311, "available": 0.44951311, "locked": 0},
+        #      "change": {"total": 0, "available": 0.0564931, "locked": -0.0564931}
+        #    }
+        #
+        #    WITHDRAWAL_UNLOCK_FUNDS
+        #    {
+        #      "historyId": "0ed569a2-c330-482e-bb89-4cb553fb5b11",
+        #      "balance": {
+        #        "id": "3a7e7a1e-0324-49d5-8f59-298505ebd6c7",
+        #        "currency": "BTC",
+        #        "type": "CRYPTO",
+        #        "userId": "a34d361d-7bad-49c1-888e-62473b75d877",
+        #        "name": "BTC"
+        #      },
+        #      "detailId": "0c7be256-c336-4111-bee7-4eb22e339700",
+        #      "time": 1527866360785,
+        #      "type": "WITHDRAWAL_UNLOCK_FUNDS",
+        #      "value": 0.05045,
+        #      "fundsBefore": {"total": 0.86001578, "available": 0.80956578, "locked": 0.05045},
+        #      "fundsAfter": {"total": 0.86001578, "available": 0.86001578, "locked": 0},
+        #      "change": {"total": 0, "available": 0.05045, "locked": -0.05045}
+        #    }
+        #
+        #    TRANSACTION_COMMISSION_RETURN
+        #    {
+        #      "historyId": "07c89c27-46f1-4d7a-8518-b73798bf168a",
+        #      "balance": {
+        #        "id": "ab43023b-4079-414c-b340-056e3430a3af",
+        #        "currency": "EUR",
+        #        "type": "FIAT",
+        #        "userId": "a34d361d-7bad-49c1-888e-62473b75d877",
+        #        "name": "EUR"
+        #      },
+        #      "detailId": null,
+        #      "time": 1528304043063,
+        #      "type": "TRANSACTION_COMMISSION_RETURN",
+        #      "value": 0.6,
+        #      "fundsBefore": {"total": 0, "available": 0, "locked": 0},
+        #      "fundsAfter": {"total": 0.6, "available": 0.6, "locked": 0},
+        #      "change": {"total": 0.6, "available": 0.6, "locked": 0}
+        #    }
+        #
+        timestamp = self.safe_integer(item, 'time')
+        balance = self.safe_value(item, 'balance', {})
+        currencyId = self.safe_string(balance, 'currency')
+        code = self.safe_currency_code(currencyId)
+        change = self.safe_value(item, 'change', {})
+        amount = self.safe_float(change, 'total')
+        direction = 'in'
+        if amount < 0:
+            direction = 'out'
+            amount = -amount
+        id = self.safe_string(item, 'historyId')
+        # there are 2 undocumented api calls: (v1_01PrivateGetPaymentsDepositDetailId and v1_01PrivateGetPaymentsWithdrawalDetailId)
+        # that can be used to enrich the transfers with txid, address etc(you need to use info.detailId as a parameter)
+        referenceId = self.safe_string(item, 'detailId')
+        type = self.parse_ledger_entry_type(self.safe_string(item, 'type'))
+        fundsBefore = self.safe_value(item, 'fundsBefore', {})
+        before = self.safe_float(fundsBefore, 'total')
+        fundsAfter = self.safe_value(item, 'fundsAfter', {})
+        after = self.safe_float(fundsAfter, 'total')
+        return {
+            'info': item,
+            'id': id,
+            'direction': direction,
+            'account': None,
+            'referenceId': referenceId,
+            'referenceAccount': None,
+            'type': type,
+            'currency': code,
+            'amount': amount,
+            'before': before,
+            'after': after,
+            'status': 'ok',
+            'timestamp': timestamp,
+            'datetime': self.iso8601(timestamp),
+            'fee': None,
+        }
+
+    def parse_ledger_entry_type(self, type):
+        types = {
+            'ADD_FUNDS': 'transaction',
+            'BITCOIN_GOLD_FORK': 'transaction',
+            'CREATE_BALANCE': 'transaction',
+            'FUNDS_MIGRATION': 'transaction',
+            'WITHDRAWAL_LOCK_FUNDS': 'transaction',
+            'WITHDRAWAL_SUBTRACT_FUNDS': 'transaction',
+            'WITHDRAWAL_UNLOCK_FUNDS': 'transaction',
+            'TRANSACTION_COMMISSION_OUTCOME': 'fee',
+            'TRANSACTION_COMMISSION_RETURN': 'fee',
+            'TRANSACTION_OFFER_ABORTED_RETURN': 'trade',
+            'TRANSACTION_OFFER_COMPLETED_RETURN': 'trade',
+            'TRANSACTION_POST_INCOME': 'trade',
+            'TRANSACTION_POST_OUTCOME': 'trade',
+            'TRANSACTION_PRE_LOCKING': 'trade',
+        }
+        return self.safe_string(types, type, type)
+
     def parse_trade(self, trade, market):
         if 'tid' in trade:
             return self.parse_public_trade(trade, market)
