3134d2cb229ac60e009100e7ab4a60576f9b0f14
Igor Kroitor <igor.kroitor@gmail.com>
2019-08-08 04:53:26 +0300

    added safe_integer_product, safe_integer_product_2 to Exchange.php

--- php/Exchange.php
+++ php/Exchange.php
@@ -236,6 +236,10 @@ class Exchange {
         return (isset($object[$key]) && is_numeric($object[$key])) ? intval($object[$key]) : $default_value;
     }
 
+    public static function safe_integer_product($object, $key, $factor, $default_value = null) {
+        return (isset($object[$key]) && is_numeric($object[$key])) ? intval($object[$key] * $factor) : $default_value;
+    }
+
     public static function safe_value($object, $key, $default_value = null) {
         return (is_array($object) && array_key_exists($key, $object)) ? $object[$key] : $default_value;
     }
@@ -266,6 +270,11 @@ class Exchange {
     public static function safe_integer_2($object, $key1, $key2, $default_value = null) {
         $value = static::safe_integer($object, $key1);
         return isset($value) ? $value : static::safe_integer($object, $key2, $default_value);
+    }
+
+    public static function safe_integer_product_2($object, $key1, $key2, $factor, $default_value = null) {
+        $value = static::safe_integer_product($object, $key1, $factor);
+        return isset($value) ? $value : static::safe_integer_product($object, $key2, $factor, $default_value);
     }
 
     public static function safe_value_2($object, $key1, $key2, $default_value = null) {
