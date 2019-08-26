36510fe8ab9eb34b0842c2fcc44705d4046a5df4
Igor Kroitor <igor.kroitor@gmail.com>
2019-08-26 16:48:37 +0300

    idex certified=true

--- js/idex.js
+++ js/idex.js
@@ -15,6 +15,7 @@ module.exports = class idex extends Exchange {
             'countries': [ 'US' ],
             'rateLimit': 1500,
             'version': '0',
+            'certified': true,
             'requiresWeb3': true,
             'has': {
                 'fetchOrderBook': true,
