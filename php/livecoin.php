6a97b6350ee5ff1f08d6d33e21eaa033f5e8da72
Travis CI <travis@travis-ci.org>
2019-08-28 11:35:34 +0000

    1.18.1099
    [ci skip]

--- php/livecoin.php
+++ php/livecoin.php
@@ -92,7 +92,7 @@ class livecoin extends Exchange {
             ),
             'commonCurrencies' => array (
                 'BTCH' => 'Bithash',
-                'CPC' => 'CapriCoin',
+                'CPC' => 'Capricoin',
                 'EDR' => 'E-Dinar Coin', // conflicts with EDR for Endor Protocol and EDRCoin
                 'eETT' => 'EETT',
                 'FirstBlood' => '1ST',
--- python/ccxt/async_support/livecoin.py
+++ python/ccxt/async_support/livecoin.py
@@ -104,7 +104,7 @@ class livecoin (Exchange):
             },
             'commonCurrencies': {
                 'BTCH': 'Bithash',
-                'CPC': 'CapriCoin',
+                'CPC': 'Capricoin',
                 'EDR': 'E-Dinar Coin',  # conflicts with EDR for Endor Protocol and EDRCoin
                 'eETT': 'EETT',
                 'FirstBlood': '1ST',
