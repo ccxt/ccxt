15cd08441732295a8c0c21c9496214163d719461
Travis CI <travis@travis-ci.org>
2019-08-28 10:58:33 +0000

    1.18.1096
    [ci skip]

--- php/Exchange.php
+++ php/Exchange.php
@@ -34,7 +34,7 @@ use kornrunner\Eth;
 use kornrunner\Solidity;
 use Elliptic\EC;
 
-$version = '1.18.1095';
+$version = '1.18.1096';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -53,7 +53,7 @@ const PAD_WITH_ZERO = 1;
 
 class Exchange {
 
-    const VERSION = '1.18.1095';
+    const VERSION = '1.18.1096';
 
     public static $eth_units = array (
         'wei'        => '1',
