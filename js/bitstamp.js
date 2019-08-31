5a40c8aadaf46409934bf91d587f42e8dd860988
Igor Kroitor <igor.kroitor@gmail.com>
2019-08-31 14:43:59 +0300

    bitstamp safeIntegerProduct → safeTimestamp #5758 #5761

--- js/bitstamp.js
+++ js/bitstamp.js
@@ -228,7 +228,7 @@ module.exports = class bitstamp extends Exchange {
             'pair': this.marketId (symbol),
         };
         const response = await this.publicGetOrderBookPair (this.extend (request, params));
-        const timestamp = this.safeIntegerProduct (response, 'timestamp', 1000);
+        const timestamp = this.safeTimestamp (response, 'timestamp', 1000);
         return this.parseOrderBook (response, timestamp);
     }
 
@@ -238,7 +238,7 @@ module.exports = class bitstamp extends Exchange {
             'pair': this.marketId (symbol),
         };
         const ticker = await this.publicGetTickerPair (this.extend (request, params));
-        const timestamp = this.safeIntegerProduct (ticker, 'timestamp', 1000);
+        const timestamp = this.safeTimestamp (ticker, 'timestamp', 1000);
         const vwap = this.safeFloat (ticker, 'vwap');
         const baseVolume = this.safeFloat (ticker, 'volume');
         let quoteVolume = undefined;
