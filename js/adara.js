bac720f0439d4a9e6aa22e5841d3abaa6c9afac8
Igor Kroitor <igor.kroitor@gmail.com>
2019-09-11 07:22:29 +0300

    adara doc url

--- js/adara.js
+++ js/adara.js
@@ -43,7 +43,7 @@ module.exports = class adara extends Exchange {
                 'logo': 'https://user-images.githubusercontent.com/1294454/49189583-0466a780-f380-11e8-9248-57a631aad2d6.jpg',
                 'api': 'https://api.adara.io',
                 'www': 'https://adara.io',
-                'doc': 'https://api.adara.io/v2.0',
+                'doc': 'https://api.adara.io/v1',
                 'fees': 'https://adara.io/fees',
             },
             'api': {
