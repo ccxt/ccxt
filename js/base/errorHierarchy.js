3047212e0cbb932cf09777c275067d7741a5db37
brandsimon <simon.brand@postadigitale.de>
2019-09-23 20:41:54 +0200

    Add MaintananceError

--- js/base/errorHierarchy.js
+++ js/base/errorHierarchy.js
@@ -26,6 +26,7 @@ const errorHierarchy = {
                 'OrderNotFillable': {},
                 'DuplicateOrderId': {},
             },
+            'MaintananceError': {},
             'NotSupported': {},
         },
         'NetworkError': {
