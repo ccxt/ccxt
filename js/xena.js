9e9adf1fc32a29ac37302f7981138aea796454e6
Igor Kroitor <igor.kroitor@gmail.com>
2019-09-25 02:34:23 +0300

    xena handleErrors

--- js/xena.js
+++ js/xena.js
@@ -1025,7 +1025,7 @@ module.exports = class xena extends Exchange {
         return { 'url': url, 'method': method, 'body': body, 'headers': headers };
     }
 
-    handleErrors (code, reason, url, method, headers, body, response) {
+    handleErrors (code, reason, url, method, headers, body, response, requestHeaders, requestBody) {
         if (response === undefined) {
             return;
         }
