b2b3c9c2a9d424cfaf0c05076614918874ef8a65
Igor Kroitor <igor.kroitor@gmail.com>
2019-10-12 04:50:44 +0300

    bytetrade fetchMarkets uppercase fix

--- js/bytetrade.js
+++ js/bytetrade.js
@@ -231,8 +231,8 @@ module.exports = class bytetrade extends Exchange {
             const market = markets[i];
             const id = this.safeString (market, 'symbol');
             // there may be duplicate codes
-            let base = this.safeCurrencyCode (this.safeString (market, 'baseName'));
-            let quote = this.safeCurrencyCode (this.safeString (market, 'quoteName'));
+            let base = this.safeString (market, 'baseName');
+            let quote = this.safeString (market, 'quoteName');
             const baseId = this.safeString (market, 'base');
             const quoteId = this.safeString (market, 'quote');
             if (baseId in this.commonCurrencies) {
