d348cff9133f51e1ed8d9c3a2b58d6d8531b4b9b
Travis CI <travis@travis-ci.org>
2019-10-13 14:57:51 +0000

    1.18.1254
    [ci skip]

--- php/Exchange.php
+++ php/Exchange.php
@@ -34,7 +34,7 @@ use kornrunner\Keccak;
 use kornrunner\Solidity;
 use Elliptic\EC;
 
-$version = '1.18.1253';
+$version = '1.18.1254';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -53,7 +53,7 @@ const PAD_WITH_ZERO = 1;
 
 class Exchange {
 
-    const VERSION = '1.18.1253';
+    const VERSION = '1.18.1254';
 
     public static $eth_units = array (
         'wei'        => '1',
