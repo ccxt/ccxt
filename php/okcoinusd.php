f989ced93893939213255bba5b08bfc01992a3c8
Travis CI <travis@travis-ci.org>
2019-10-15 23:56:38 +0000

    1.18.1272
    [ci skip]

--- php/okcoinusd.php
+++ php/okcoinusd.php
@@ -133,12 +133,13 @@ class okcoinusd extends Exchange {
                     'https://www.npmjs.com/package/okcoin.com',
                 ),
                 'referral' => 'https://www.okcoin.com/account/register?flag=activity&channelId=600001513',
+                'fees' => 'https://support.okcoin.com/hc/en-us/articles/360015261532-OKCoin-Fee-Rates',
             ),
             // these are okcoin.com fees, okex fees are in okex.js
             'fees' => array (
                 'trading' => array (
-                    'taker' => 0.001,
-                    'maker' => 0.0005,
+                    'taker' => 0.002,
+                    'maker' => 0.001,
                 ),
             ),
             'exceptions' => array (
