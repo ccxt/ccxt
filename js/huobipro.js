c81b616aa6316f75b6ee09e3494bd77a6c06039e
Igor Kroitor <igor.kroitor@gmail.com>
2019-10-16 06:33:02 +0300

    huobipro parseTrade fee currency fix linting #5971

--- js/huobipro.js
+++ js/huobipro.js
@@ -445,7 +445,7 @@ module.exports = class huobipro extends Exchange {
         if (filledPoints !== undefined) {
             if ((feeCost === undefined) || (feeCost === 0.0)) {
                 feeCost = filledPoints;
-                const feeCurrency = this.safeCurrencyCode (this.safeString (trade, 'fee-deduct-currency'));
+                feeCurrency = this.safeCurrencyCode (this.safeString (trade, 'fee-deduct-currency'));
             }
         }
         if (feeCost !== undefined) {
