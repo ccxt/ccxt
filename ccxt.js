76da515c750fa38020c62648a0df164d2692e530
Travis CI <travis@travis-ci.org>
2019-10-18 07:41:17 +0000

    1.18.1294
    [ci skip]

--- ccxt.js
+++ ccxt.js
@@ -35,7 +35,7 @@ const Exchange  = require ('./js/base/Exchange')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.18.1293'
+const version = '1.18.1294'
 
 Exchange.ccxtVersion = version
 
--- php/Exchange.php
+++ php/Exchange.php
@@ -34,7 +34,7 @@ use kornrunner\Keccak;
 use kornrunner\Solidity;
 use Elliptic\EC;
 
-$version = '1.18.1293';
+$version = '1.18.1294';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -53,7 +53,7 @@ const PAD_WITH_ZERO = 1;
 
 class Exchange {
 
-    const VERSION = '1.18.1293';
+    const VERSION = '1.18.1294';
 
     public static $eth_units = array (
         'wei'        => '1',
