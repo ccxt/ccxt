7e95ed4ded665d311c1744c045ac2cbc9441872f
Abhishek Ranjan <abhishekr700@gmail.com>
2019-10-22 03:12:34 +0530

    Add all reqd methods
    Signed-off-by: Abhishek Ranjan <abhishekr700@gmail.com>

--- js/bitbns.js
+++ js/bitbns.js
@@ -19,22 +19,22 @@ module.exports = class bitbnsexchange extends Exchange {
             'has': {
                 // 'CORS': false,
                 'fetchCurrencies': false,
-                'fetchTicker': false, // Can be emulated on fetchTickers if necessary
-                'fetchTickers': false,
+                'fetchTicker': true, // Can be emulated on fetchTickers if necessary
+                'fetchTickers': true,
                 'fetchTrades': true,
                 'fetchTradingFees': false,
                 'fetchBalance': false,
-                'createOrder': false,
-                'cancelOrder': false,
-                'fetchOpenOrders': false,
+                'createOrder': true,
+                'cancelOrder': true,
+                'fetchOpenOrders': true,
                 'fetchMyTrades': false,
                 'fetchDepositAddress': false,
                 'fetchWithdrawals': false,
                 'fetchDeposits': false,
-                'fetchClosedOrders': false,
+                'fetchClosedOrders': 'emulated',
                 'fetchL2OrderBook': false,
                 'fetchOHLCV': 'emulated',
-                'fetchOrder': false,
+                'fetchOrder': true,
                 'editOrder': false,
                 'fetchTransactions': false,
                 'fetchLedger': false,
@@ -47,6 +47,7 @@ module.exports = class bitbnsexchange extends Exchange {
                 'api': {
                     'public': 'https://c086dmj6f2.execute-api.ap-south-1.amazonaws.com/dev/',
                     'private': 'https://api.bitbns.com/api/trade/v2',
+                    'private1': 'https://api.bitbns.com/api/trade/v1',
                 },
                 'www': 'https://bitbns.com/',
                 'doc': 'https://github.com/bitbns-official/node-bitbns-api',
@@ -63,6 +64,10 @@ module.exports = class bitbnsexchange extends Exchange {
                 'private': { 'post': [
                     'orders',
                     'cancel',
+                ] },
+                'private1': { 'post': [
+                    'orderStatus',
+                    'listOpenOrders',
                 ] },
             },
             'fees': {
@@ -81,40 +86,80 @@ module.exports = class bitbnsexchange extends Exchange {
     }
 
     sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
-        console.log ('Body: ', body);
+        // console.log("PAth:", path);
+        // console.log ('Params: ', params);
         let url = '';
-        if (api === 'private') {
-            console.log ('Signing Privately !!!');
-            // Generate payload
-            const timeStamp_nonce = Date.now ().toString ();
-            const data = {
-                'symbol': this.implodeParams (path, params),
-                'timeStamp_nonce': timeStamp_nonce,
-                'body': body,
-            };
-            const payload = this.stringToBase64 (JSON.stringify (data));
-            // Generate signature from payload
-            const signature = this.hmac (payload, this.secret, 'sha512', 'hex');
+        if (api === 'private1') {
             // Generate complete url
-            url = this.urls['api'][api] + '/' + this.implodeParams (path, params);
+            url = this.urls['api'][api] + '/' + path + '/' + this.safeString (params, 'symbol');
+            // console.log (method, url);
+            // console.log ('Signing Privately !!!');
             if (method === 'POST') {
                 body = this.json (params);
             }
-            console.log (method, url);
+            // Generate payload
+            const timeStamp_nonce = Date.now ().toString ();
+            // const timeStamp_nonce = '1571663667098';
+            const data = {
+                'symbol': '/' + path + '/' + this.safeString (params, 'symbol'),
+                'timeStamp_nonce': timeStamp_nonce,
+                'body': body,
+            };
+            // console.log("ccxt data:", data);
+            const payload = this.stringToBase64 (JSON.stringify (data));
+            // console.log("ccxt payload:", payload);
+            // Generate signature from payload
+            const signature = this.hmac (payload, this.secret, 'sha512', 'hex');
+            // console.log("ccxt sign:", signature);
             // Init headers
             headers = {};
             // Attach headers
             headers['X-BITBNS-APIKEY'] = this.apiKey;
             headers['X-BITBNS-PAYLOAD'] = payload;
             headers['X-BITBNS-SIGNATURE'] = signature;
+            headers['Accept'] = 'application/json';
+            headers['Accept-Charset'] = 'utf-8';
+            headers['content-type'] = 'application/x-www-form-urlencoded';
+        } else if (api === 'private') {
+            // Generate complete url
+            url = this.urls['api'][api] + '/' + this.implodeParams (path, params);
+            // console.log (method, url);
+            // console.log ('Signing Privately !!!');
+            if (method === 'POST') {
+                body = this.json (params);
+            }
+            // Generate payload
+            const timeStamp_nonce = Date.now ().toString ();
+            // const timeStamp_nonce = '1571663667098';
+            const data = {
+                'symbol': '/' + path + '/' + this.safeString (params, 'symbol'),
+                'timeStamp_nonce': timeStamp_nonce,
+                'body': body,
+            };
+            // console.log("ccxt data:", data);
+            const payload = this.stringToBase64 (JSON.stringify (data));
+            // console.log("ccxt payload:", payload);
+            // Generate signature from payload
+            const signature = this.hmac (payload, this.secret, 'sha512', 'hex');
+            // console.log("ccxt sign:", signature);
+            // Init headers
+            headers = {};
+            // Attach headers
+            headers['X-BITBNS-APIKEY'] = this.apiKey;
+            headers['X-BITBNS-PAYLOAD'] = payload;
+            headers['X-BITBNS-SIGNATURE'] = signature;
+            headers['Accept'] = 'application/json';
+            headers['Accept-Charset'] = 'utf-8';
+            headers['content-type'] = 'application/x-www-form-urlencoded';
         } else {
             url = this.urls['api'][api] + path;
             if (Object.keys (params).length) {
                 url += '?' + this.urlencode (params);
             }
-            console.log (url);
+            // console.log (method, url);
         }
-        console.log ({ 'url': url, 'method': method, 'body': body, 'headers': headers });
+        // if (api === 'private')
+        // console.log ("ccxt obj:",{ 'url': url, 'method': method, 'body': body, 'headers': headers });
         return { 'url': url, 'method': method, 'body': body, 'headers': headers };
     }
 
@@ -129,10 +174,9 @@ module.exports = class bitbnsexchange extends Exchange {
         return tickers;
     }
 
-    // Emulated using fetchTickers
     async fetchTicker (symbol = undefined, params = {}) {
         const tickers = await this.fetchTickers ();
-        return this.safeString (tickers, symbol);
+        return this.safeValue (tickers, symbol);
     }
 
     async fetchTrades (symbol, since = undefined, limit = undefined, params = {}) {
@@ -146,8 +190,14 @@ module.exports = class bitbnsexchange extends Exchange {
             'limit': limit,
         };
         const trades = await this.publicGetFetchTrades (this.extend (request, params));
+        // console.log (trades.length);
         for (let i = 0; i < trades.length; i++) {
             trades[i].symbol = symbol;
+            for (const key of Object.keys (trades[i])) {
+                if (trades[i][key] === null) {
+                    trades[i][key] = undefined;
+                }
+            }
         }
         return trades;
     }
@@ -164,10 +214,6 @@ module.exports = class bitbnsexchange extends Exchange {
         return ob;
     }
 
-    // =================
-    // PRIVATE ENDPOINTS
-    // =================
-
     async createOrder (symbol, type, side, amount, price = undefined, params = {}) {
         await this.loadMarkets ();
         const market = this.market (symbol);
@@ -182,7 +228,7 @@ module.exports = class bitbnsexchange extends Exchange {
         if (splitSymbol[1] === 'USDT') {
             request['symbol'] = splitSymbol[0] + '_' + splitSymbol[1];
         }
-        console.log (request);
+        // console.log (request);
         // return request;
         const resp = await this.privatePostOrders (this.extend (request, params));
         return {
@@ -210,5 +256,88 @@ module.exports = class bitbnsexchange extends Exchange {
         }
         const resp = await this.privatePostCancel (this.extend (request, params));
         return resp;
+    }
+
+    async fetchOrder (id, symbol = undefined, params = {}) {
+        await this.loadMarkets ();
+        const market = this.market (symbol);
+        const tradingSymbol = market['id'];
+        const request = {
+            'symbol': tradingSymbol,
+            'entry_id': id,
+        };
+        const resp = await this.private1PostOrderStatus (this.extend (request, params));
+        const order = this.safeValue (resp, 'data')[0];
+        // console.log(order);
+        const orderObj = {
+            'id': this.safeString (order, 'entry_id'), // string
+            'datetime': this.safeString (order, 'time'), // ISO8601 datetime of 'timestamp' with milliseconds
+            'timestamp': (new Date (this.safeString (order, 'time'))).getTime (),
+            'lastTradeTimestamp': undefined, // Unix timestamp of the most recent trade on this order
+            'symbol': symbol,      // symbol
+            'type': 'limit',        // 'market', 'limit'
+            'side': undefined,          // 'buy', 'sell'
+            'price': this.safeString (order, 'rate'),    // float price in quote currency
+            'amount': undefined,           // ordered amount of base currency
+            'filled': undefined,           // filled amount of base currency
+            'remaining': this.safeString (order, 'btc'), // remaining amount to fill
+            'cost': undefined,   // 'filled' * 'price' (filling price used where available)
+            'trades': undefined,         // a list of order trades/executions
+            'fee': undefined,
+            'info': resp,              // the original unparsed order structure as is
+        };
+        const status = this.safeInteger (order, 'status');
+        // console.log(status);
+        if (status === 0) {
+            orderObj['status'] = 'open';
+        } else if (status === -1) {
+            orderObj['status'] = 'canceled';
+        } else if (status === 2) {
+            orderObj['status'] = 'closed';
+        }
+        return orderObj;
+    }
+
+    async fetchOpenOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
+        await this.loadMarkets ();
+        const market = this.market (symbol);
+        const tradingSymbol = market['id'];
+        const request = {
+            'symbol': tradingSymbol,
+            'page': 0,
+        };
+        const resp = await this.private1PostListOpenOrders (this.extend (request, params));
+        const orders = this.safeValue (resp, 'data');
+        const openOrders = [];
+        for (let i = 0; i < orders.length; i += 1) {
+            const orderObj = {
+                'id': this.safeString (orders[i], 'entry_id'), // string
+                'datetime': this.safeString (orders[i], 'time'), // ISO8601 datetime of 'timestamp' with milliseconds
+                'timestamp': (new Date (this.safeString (orders[i], 'time'))).getTime (),
+                'lastTradeTimestamp': undefined, // Unix timestamp of the most recent trade on this order
+                'symbol': symbol,      // symbol
+                'type': 'limit',        // 'market', 'limit'
+                'side': undefined,          // 'buy', 'sell'
+                'price': this.safeString (orders[i], 'rate'),    // float price in quote currency
+                'amount': undefined,           // ordered amount of base currency
+                'filled': undefined,           // filled amount of base currency
+                'remaining': this.safeString (orders[i], 'btc'), // remaining amount to fill
+                'cost': undefined,   // 'filled' * 'price' (filling price used where available)
+                'trades': undefined,         // a list of order trades/executions
+                'fee': undefined,
+                'info': resp,              // the original unparsed order structure as is
+            };
+            const status = this.safeInteger (orders[i], 'status');
+            // console.log(status);
+            if (status === 0) {
+                orderObj['status'] = 'open';
+            } else if (status === -1) {
+                orderObj['status'] = 'canceled';
+            } else if (status === 2) {
+                orderObj['status'] = 'closed';
+            }
+            openOrders.push (orderObj);
+        }
+        return openOrders;
     }
 };
--- test2.js
+++ test2.js
@@ -1,5 +1,10 @@
 const ccxt = require("./ccxt")
-console.log(ccxt.version);
+const bitbnsApi = require('bitbns');
+const bitbns2 = new bitbnsApi({
+      apiKey :  '***REMOVED***',
+      apiSecretKey : '***REMOVED***'
+}); 
+// console.log(ccxt.version);
 
 // console.log(ccxt.exchanges);
 // KEY: DC6A865DC713A9D99A9C9B1374C35771
@@ -8,7 +13,7 @@ console.log(ccxt.version);
 let bitbns = new ccxt.bitbns();
 
 (async () => {
-    await bitbns.loadMarkets();
+    // await bitbns.loadMarkets();
 
     // let cs = await bitbns.fetchOHLCV('BTC/INR', '1d');
     // console.log(cs);
@@ -16,9 +21,12 @@ let bitbns = new ccxt.bitbns();
 
     // console.log(bitbns.symbols);
     // console.log(bitbns.markets["BTC/USDT"]);
-    
-    // let tickers = await bitbns.fetchTicker("BTC/INR");
+
+    // let tickers = await bitbns.fetchTickers();
     // console.log(tickers);
+    
+    // let tickerone = await bitbns.fetchTicker("BTC/INR");
+    // console.log(tickerone);
     
     // let trades = await bitbns.fetchTrades("BTC/INR", Date.now()-360000000);
     // console.log(trades);
@@ -26,9 +34,54 @@ let bitbns = new ccxt.bitbns();
     
     // let ob = await bitbns.fetchOrderBook("BTC/INR", 100);
     // console.log(ob);
+
+    // let data1 = bitbns.sign('orders', 'private', 'POST', params = {
+    //     'symbol': 'BTC',
+    //     'side': 'BUY',
+    //     'quantity': '0.001',
+    //     'rate': '766000',
+    // }, undefined,undefined)
+
+    // let data2 = bitbns2.populateHeadersForPost('BTC', 'orders',{
+    //     symbol: 'BTC',
+    //     side: 'BUY',
+    //     quantity: '0.001',
+    //     rate: '766000'
+    // })
+    // bitbns2.getPayload('/orders/BTC',{
+    //         symbol: 'BTC',
+    //         side: 'BUY',
+    //         quantity: '0.001',
+    //         rate: '766000'
+    // })
+    // let order2 = bitbns2.placeOrders({
+    //     symbol: 'BTC',
+    //     side: 'BUY',
+    //     quantity: '0.001',
+    //     rate: '766000'
+    // }, (e,d)=>{
+    //     if(e) console.log(e);
+        
+    //     console.log(d);
+        
+    // })
+
+    // let order = await bitbns.createOrder("BTC/INR", "", 'buy', '0.001', '766000');
+    // console.log(order);
+    // let cancel = await bitbns.cancelOrder('asd', 'BTC/INR');
+    // console.log(cancel);
     
-    let order = await bitbns.createOrder("BTC/INR", "", 'buy', '0.0001', '766000');
-    console.log(order);
+    // console.log(order2);
+    // closed: '2583833'
+    // canceled: 2583798
+    // let status = await bitbns.fetchOrder('2583833', 'BTC/INR');
+    // console.log(status);
+
+    let openOrder = await bitbns.fetchOpenOrders ('BTC/INR');
+    console.log(openOrder);
+    
+    
+    
     
     // let cancelOrder = await bitbns.cancelOrder('1234', 'BTC/INR');
     // console.log(cancelOrder);
