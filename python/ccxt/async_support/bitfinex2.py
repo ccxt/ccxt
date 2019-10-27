c2062b67fda57475835ed57176278bf9e2705fb9
Travis CI <travis@travis-ci.org>
2019-10-27 16:35:50 +0000

    1.18.1338
    [ci skip]

--- python/ccxt/async_support/bitfinex2.py
+++ python/ccxt/async_support/bitfinex2.py
@@ -111,6 +111,7 @@ class bitfinex2 (bitfinex):
                         'auth/r/orders/{symbol}/new',
                         'auth/r/orders/{symbol}/hist',
                         'auth/r/order/{symbol}:{id}/trades',
+                        'auth/w/order/submit',
                         'auth/r/trades/hist',
                         'auth/r/trades/{symbol}/hist',
                         'auth/r/positions',
