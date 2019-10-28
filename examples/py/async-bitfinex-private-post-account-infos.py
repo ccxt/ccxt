9abfff9cd555dfe25b916de913b325cffc375d8e
Carlo Revelli <carlo.revelli@berkeley.edu>
2019-10-27 17:27:19 -0700

    fix async python examples

--- examples/py/async-bitfinex-private-post-account-infos.py
+++ examples/py/async-bitfinex-private-post-account-infos.py
@@ -10,17 +10,14 @@ sys.path.append(root + '/python')
 import ccxt.async_support as ccxt  # noqa: E402
 
 
-async def test(exchange):
-    print(await exchange.private_post_account_infos())
+async def test():
+    bitfinex = ccxt.bitfinex({
+        'apiKey': "4FlEDtxDl35gdEiobnfZ72vJeZteE4Bb7JdvqzjIjHq",
+        'secret': "D4DXM8DZdHuAq9YptUsb42aWT1XBnGlIJgLi8a7tzFH",
+        'verbose': True,  # switch it to False if you don't want the HTTP log
+    })
+    print(await bitfinex.public_get_symbols())
+    await bitfinex.close()
 
-
-bitfinex = ccxt.bitfinex({
-    'apiKey': "4FlEDtxDl35gdEiobnfZ72vJeZteE4Bb7JdvqzjIjHq",
-    'secret': "D4DXM8DZdHuAq9YptUsb42aWT1XBnGlIJgLi8a7tzFH",
-    'verbose': True,  # switch it to False if you don't want the HTTP log
-})
-
-[asyncio.ensure_future(test(exchange)) for exchange in [bitfinex]]
-pending = asyncio.Task.all_tasks()
 loop = asyncio.get_event_loop()
-loop.run_until_complete(asyncio.gather(*pending))
+loop.run_until_complete(test())
