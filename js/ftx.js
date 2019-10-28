9e8a4bf097201dc0b4f40e504265335ae8b7d854
Igor Kroitor <igor.kroitor@gmail.com>
2019-10-28 08:03:46 +0300

    ftx certified

--- js/ftx.js
+++ js/ftx.js
@@ -14,7 +14,7 @@ module.exports = class ftx extends Exchange {
             'name': 'FTX',
             'countries': [ 'HK' ],
             'rateLimit': 100,
-            'certified': false,
+            'certified': true,
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/67149189-df896480-f2b0-11e9-8816-41593e17f9ec.jpg',
                 'www': 'https://ftx.com',
