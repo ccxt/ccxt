20359866c37b36579d781451b46a24e863a602e0
Carlo Revelli <carlo.revelli@berkeley.edu>
2019-11-06 16:01:22 -0800

    restore uncamelcase js

--- js/base/errors.js
+++ js/base/errors.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const errorHierarchy = require ('./errorHierarchy')
+const { unCamelCase } = require ('./functions/string')
 
 /*  ------------------------------------------------------------------------ */
 
