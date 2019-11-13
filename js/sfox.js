bff8712cb02532bc03d208f932beb421e326d789
Matt Rasband <matt.rasband@gmail.com>
2019-11-13 11:07:14 -0700

    Linting fixes

--- js/sfox.js
+++ js/sfox.js
@@ -3,13 +3,12 @@
 // ----------------------------------------------------------------------------
 
 const Exchange = require ('./base/Exchange');
-const { ExchangeError, ArgumentsRequired, AuthenticationError, DDoSProtection } = require ('./base/errors');
 
 // ----------------------------------------------------------------------------
 
 module.exports = class sfox extends Exchange {
-    describe() {
-        return this.deepExtend (super.describe(), {
+    describe () {
+        return this.deepExtend (super.describe (), {
             'id': 'sfox',
             'name': 'SFOX',
             'countries': [ 'US' ],
@@ -73,118 +72,116 @@ module.exports = class sfox extends Exchange {
                     'delete': [
                         'orders/{order_id}',
                     ],
-                }
+                },
             },
-        })
+        });
     }
 
     sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
-      let fullPath = '/' + this.version + '/' + this.implodeParams (path, params);
-      const query = this.omit (params, this.extractParams (path));
-      if (method == 'GET') {
-        if (Object.keys (query).length) {
-          fullPath += '?' + this.urlencode (query);
+        let fullPath = '/' + this.version + '/' + this.implodeParams (path, params);
+        const query = this.omit (params, this.extractParams (path));
+        if (method === 'GET') {
+            if (Object.keys (query).length) {
+                fullPath += '?' + this.urlencode (query);
+            }
         }
-      }
-      const url = this.urls['api'] + fullPath;
-      if (api === 'private') {
-        this.checkRequiredCredentials ();
-        headers = {
-          'Authorization': 'Bearer ' + this.apiKey,
-        };
-      }
-      return { 'url': url, 'method': method, 'body': body, 'headers': headers };
+        const url = this.urls['api'] + fullPath;
+        if (api === 'private') {
+            this.checkRequiredCredentials ();
+            headers = {
+                'Authorization': 'Bearer ' + this.apiKey,
+            };
+        }
+        return { 'url': url, 'method': method, 'body': body, 'headers': headers };
     }
 
-    async fetchMarkets() {
-      return [
-        {
-          'id': 'bchbtc',
-          'symbol': 'bchbtc',
-          'base': 'BCH',
-          'quote': 'BTC',
-        },
-        {
-          'id': 'bchusd',
-          'symbol': 'bchusd',
-          'base': 'BCH',
-          'quote': 'USD',
-        },
-        {
-          'id': 'bsvbtc',
-          'symbol': 'bsvbtc',
-          'base': 'BSV',
-          'quote': 'BTC',
-        },
-        {
-          'id': 'btcusd',
-          'symbol': 'btcusd',
-          'base': 'BTC',
-          'quote': 'USD',
-        },
-        {
-          'id': 'etcbtc',
-          'symbol': 'etcbtc',
-          'base': 'ETC',
-          'quote': 'BTC'
-        },
-        {
-          'id': 'etcusd',
-          'symbol': 'etcusd',
-          'base': 'ETC',
-          'quote': 'USD'
-        },
-        {
-          'id': 'ethbtc',
-          'symbol': 'ethbtc',
-          'base': 'ETH',
-          'quote': 'BTC'
-        },
-        {
-          'id': 'ethusd',
-          'symbol': 'ethusd',
-          'base': 'ETH',
-          'quote': 'USD'
-        },
-        {
-          'id': 'ltcbtc',
-          'symbol': 'ltcbtc',
-          'base': 'LTC',
-          'quote': 'BTC'
-        },
-        {
-          'id': 'ltcusd',
-          'symbol': 'ltcusd',
-          'base': 'LTC',
-          'quote': 'USD'
-        },
-      ];
+    async fetchMarkets () {
+        return [
+            {
+                'id': 'bchbtc',
+                'symbol': 'bchbtc',
+                'base': 'BCH',
+                'quote': 'BTC',
+            },
+            {
+                'id': 'bchusd',
+                'symbol': 'bchusd',
+                'base': 'BCH',
+                'quote': 'USD',
+            },
+            {
+                'id': 'bsvbtc',
+                'symbol': 'bsvbtc',
+                'base': 'BSV',
+                'quote': 'BTC',
+            },
+            {
+                'id': 'btcusd',
+                'symbol': 'btcusd',
+                'base': 'BTC',
+                'quote': 'USD',
+            },
+            {
+                'id': 'etcbtc',
+                'symbol': 'etcbtc',
+                'base': 'ETC',
+                'quote': 'BTC',
+            },
+            {
+                'id': 'etcusd',
+                'symbol': 'etcusd',
+                'base': 'ETC',
+                'quote': 'USD',
+            },
+            {
+                'id': 'ethbtc',
+                'symbol': 'ethbtc',
+                'base': 'ETH',
+                'quote': 'BTC',
+            },
+            {
+                'id': 'ethusd',
+                'symbol': 'ethusd',
+                'base': 'ETH',
+                'quote': 'USD',
+            },
+            {
+                'id': 'ltcbtc',
+                'symbol': 'ltcbtc',
+                'base': 'LTC',
+                'quote': 'BTC',
+            },
+            {
+                'id': 'ltcusd',
+                'symbol': 'ltcusd',
+                'base': 'LTC',
+                'quote': 'USD',
+            },
+        ];
     }
 
     async fetchOrderBook (symbol, limit = undefined, params = {}) {
-        const response = await this.publicGetMarketsOrderbook({pair: symbol});
+        const response = await this.publicGetMarketsOrderbook ({ 'pair': symbol });
         const bids = [];
-        for (let i = 0; i < response["bids"].length; i++) {
-            const bid = response["bids"][i];
-            bids.push([bid[0], bid[1]]);
+        for (let i = 0; i < response['bids'].length; i++) {
+            const bid = response['bids'][i];
+            bids.push ([bid[0], bid[1]]);
         }
         const asks = [];
-        for (let i = 0; i < response["asks"].length; i++) {
-            const ask = response["asks"][i];
-            asks.push([ask[0], ask[1]]);
+        for (let i = 0; i < response['asks'].length; i++) {
+            const ask = response['asks'][i];
+            asks.push ([ask[0], ask[1]]);
         }
         return {
             'bids': bids,
             'asks': asks,
-            'timestamp': response["lastupdated"],
-            'datetime': this.iso8601(response["lastupdated"]),
+            'timestamp': response['lastupdated'],
+            'datetime': this.iso8601 (response['lastupdated']),
         };
     }
 
-    // fetchTrades (symbol, since, limi, params = {}) {}
-
     async fetchBalance (params = {}) {
-        const response = await this.privateGetUserBalance();
+        const response = await this.privateGetUserBalance ();
         const result = { 'info': response };
         for (let i = 0; i < response.length; i++) {
             const balance = response[i];
@@ -200,54 +197,55 @@ module.exports = class sfox extends Exchange {
     }
 
     async createOrder (symbol, type, side, amount, price = undefined, params = {}) {
-      await this.loadMarkets();
-      const request = {
-        'quantity': amount,
-        'currency_pair': this.marketId (symbol),
-      };
-      if (type === 'limit') {
-        request['algo_id'] = 200;  // smart routing
-        request['price'] = price;
-      }
-      if (side === 'sell') {
-        const order = await this.privatePostOrdersSell (this.extend (request, params));
-      } else {
-        const order = await this.privatePostOrdersBuy (this.extend (request, params));
-      }
-      return this.parseOrder (order);
+        await this.loadMarkets ();
+        const request = {
+            'quantity': amount,
+            'currency_pair': this.marketId (symbol),
+        };
+        if (type === 'limit') {
+            request['algo_id'] = 200;  // smart routing
+            request['price'] = price;
+        }
+        let order = undefined;
+        if (side === 'sell') {
+            order = await this.privatePostOrdersSell (this.extend (request, params));
+        } else {
+            order = await this.privatePostOrdersBuy (this.extend (request, params));
+        }
+        return this.parseOrder (order);
     }
 
     async cancelOrder (id, symbol = undefined, params = {}) {
-      const order = await this.privateDeleteOrdersOrderId({ 'order_id': id });
-      return this.parseOrder (order);
+        const order = await this.privateDeleteOrdersOrderId ({ 'order_id': id });
+        return this.parseOrder (order);
     }
 
     async fetchOrder (id, symbol = undefined, params = {}) {
-      const order = await this.privateGetOrdersOrderId({ 'order_id': id });
-      return this.parseOrder (order);
+        const order = await this.privateGetOrdersOrderId ({ 'order_id': id });
+        return this.parseOrder (order);
     }
 
     async fetchOpenOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
-      const orders = await this.privateGetOrders();
-      return this.parseOrders (orders);
+        const orders = await this.privateGetOrders ();
+        return this.parseOrders (orders);
     }
 
-    async deposit(code, amount, address, params = {}) {
-      if (code === "usd") {
-        const request = {
-          'amount': amount,
-        };
-        const result = await this.privatePostUserBankDeposit(request);
-        return {
-            'info': result,
-            'currency': 'usd',
-            'id': result['tx_status'],
-        };
-      }
+    async deposit (code, amount, address, params = {}) {
+        if (code === 'usd') {
+            const request = {
+                'amount': amount,
+            };
+            const result = await this.privatePostUserBankDeposit (request);
+            return {
+                'info': result,
+                'currency': 'usd',
+                'id': result['tx_status'],
+            };
+        }
     }
 
     async createDepositAddress (code, params = {}) {
-        const response = await this.privatePostUserDepositAddressCurrency({ 'currency': currency });
+        const response = await this.privatePostUserDepositAddressCurrency ({ 'currency': code });
         const address = this.safeString (response, 'address');
         return {
             'currency': code,
@@ -256,56 +254,56 @@ module.exports = class sfox extends Exchange {
         };
     }
 
-  async fetchDepositAddress(code, params = {}) {
-      const addresses = await this.privateGetUserDepositAddressCurrency({ 'currency': code });
-      const address = addresses[code][addresses[code].length - 1];
-      return {
-          'currency': this.safeString (address, 'currency'),
-          'address': this.safeString (address, 'address'),
-          'info': addresses,
-      };
-  }
+    async fetchDepositAddress (code, params = {}) {
+        const addresses = await this.privateGetUserDepositAddressCurrency ({ 'currency': code });
+        const address = addresses[code][addresses[code].length - 1];
+        return {
+            'currency': this.safeString (address, 'currency'),
+            'address': this.safeString (address, 'address'),
+            'info': addresses,
+        };
+    }
 
     async withdraw (code, amount, address, tag = undefined, params = {}) {
         const request = {
-          'amount': amount,
-          'address': address,
-          'currency': code,
+            'amount': amount,
+            'address': address,
+            'currency': code,
         };
-        return this.privatePostUserWithdraw(request);
+        return this.privatePostUserWithdraw (request);
     }
 
     parseOrder (order, market = undefined) {
-      const status = this.parseOrderStatus (this.safeString (order, 'status'))
-      const timestamp = this.safeString (order, 'dateupdated')
-      const filled = this.safeFloat (order, 'filled');
-      const quantity = this.safeFloat (order, 'quantity');
-      return {
-          'id': order['id'],
-          'info': order,
-          'timestamp': this.parse8601 (timestamp),
-          'datetime': timestamp,
-          'lastTradeTimestamp': undefined,
-          'status': status,
-          'symbol': this.safeString (order, 'pair'),
-          'type': this.safeString (order, 'action'),
-          'side': this.safeString (order, 'action'),
-          'price': this.safeFloat (order, 'price'),
-          'cost': this.safeFloat (order, 'net_proceeds'),
-          'filled': filled,
-          'remaining': quantity - filled,
-          'fee': this.safeFloat (order, 'fees'),
-      };
+        const status = this.parseOrderStatus (this.safeString (order, 'status'));
+        const timestamp = this.safeString (order, 'dateupdated');
+        const filled = this.safeFloat (order, 'filled');
+        const quantity = this.safeFloat (order, 'quantity');
+        return {
+            'id': order['id'],
+            'info': order,
+            'timestamp': this.parse8601 (timestamp),
+            'datetime': timestamp,
+            'lastTradeTimestamp': undefined,
+            'status': status,
+            'symbol': this.safeString (order, 'pair'),
+            'type': this.safeString (order, 'action'),
+            'side': this.safeString (order, 'action'),
+            'price': this.safeFloat (order, 'price'),
+            'cost': this.safeFloat (order, 'net_proceeds'),
+            'filled': filled,
+            'remaining': quantity - filled,
+            'fee': this.safeFloat (order, 'fees'),
+        };
     }
 
-  parseOrderStatus (status) {
-    const statuses = {
-      'Done': 'closed',
-      'Started': 'open',
-      'Pending': 'open',
-      'Moving': 'open',
-      'Canceled': 'canceled',
-    };
-    return this.safeString (statuses, status, status);
-  }
+    parseOrderStatus (status) {
+        const statuses = {
+            'Done': 'closed',
+            'Started': 'open',
+            'Pending': 'open',
+            'Moving': 'open',
+            'Canceled': 'canceled',
+        };
+        return this.safeString (statuses, status, status);
+    }
 };
