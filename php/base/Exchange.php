117cfe9fec9c44ce65e72abc391c56854ef652e7
Travis CI <travis@travis-ci.org>
2019-11-20 13:24:38 +0000

    1.19.59
    [ci skip]

--- php/base/Exchange.php
+++ php/base/Exchange.php
@@ -35,7 +35,7 @@ use kornrunner\Solidity;
 use Elliptic\EC;
 use BN\BN;
 
-$version = '1.19.58';
+$version = '1.19.59';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -54,7 +54,7 @@ const PAD_WITH_ZERO = 1;
 
 class Exchange {
 
-    const VERSION = '1.19.58';
+    const VERSION = '1.19.59';
 
     public static $eth_units = array (
         'wei'        => '1',
