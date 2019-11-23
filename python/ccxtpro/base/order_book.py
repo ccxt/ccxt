885857e0a5f655398d7afc8186f32536dcf845e6
Carlo Revelli <carlo.revelli@berkeley.edu>
2019-11-23 14:42:09 -0800

    add the correct file

--- python/ccxtpro/base/order_book.py
+++ python/ccxtpro/base/order_book.py
@@ -1,6 +1,7 @@
 from ccxtpro.base import order_book_side
 from ccxt import Exchange
 
+
 class OrderBook(dict):
     def __init__(self, snapshot={}):
         super(OrderBook, self).__init__()
@@ -34,9 +35,76 @@ class OrderBook(dict):
         self['datetime'] = Exchange.iso8601 (timestamp)
 
 
-x = OrderBook()
+# -----------------------------------------------------------------------------
+# some exchanges limit the number of bids/asks in the aggregated orderbook
+# orders beyond the limit threshold are not updated with new ws deltas
+# those orders should not be returned to the user, they are outdated quickly
 
-x.update(10, 1000, [[4, 5], [6, 7]], [[8, 9], [10, 11]])
-x.limit()
+class LimitedOrderBook(OrderBook):
+    def __init__(self, snapshot={}, depth=None):
+        snapshot.update({
+            'asks': order_book_side.LimitedAsks(snapshot['asks'] or []),
+            'bids': order_book_side.LimitedBids(snapshot['bids'] or []),
+        })
+        super(LimitedOrderBook, self).__init__(snapshot)
 
-print(x)
+# -----------------------------------------------------------------------------
+# overwrites absolute volumes at price levels
+# or deletes price levels based on order counts (3rd value in a bidask delta)
+
+
+class CountedOrderBook(OrderBook):
+    def __init__(self, snapshot={}, depth=None):
+        snapshot.update({
+            'asks': order_book_side.CountedAsks(snapshot['asks'] or []),
+            'bids': order_book_side.CountedBids(snapshot['bids'] or []),
+        })
+        super(CountedOrderBook, self).__init__(snapshot)
+
+# -----------------------------------------------------------------------------
+# indexed by order ids (3rd value in a bidask delta)
+
+
+class IndexedOrderBook(OrderBook):
+    def __init__(self, snapshot={}, depth=None):
+        snapshot.update({
+            'asks': order_book_side.IndexedAsks(snapshot['asks'] or []),
+            'bids': order_book_side.IndexedBids(snapshot['bids'] or []),
+        })
+        super(IndexedOrderBook, self).__init__(snapshot)
+
+# -----------------------------------------------------------------------------
+# adjusts the volumes by positive or negative relative changes or differences
+
+
+class IncrementalOrderBook(OrderBook):
+    def __init__(self, snapshot={}, depth=None):
+        snapshot.update({
+            'asks': order_book_side.IncrementalAsks(snapshot['asks'] or []),
+            'bids': order_book_side.IncrementalBids(snapshot['bids'] or []),
+        })
+        super(IncrementalOrderBook, self).__init__(snapshot)
+
+# -----------------------------------------------------------------------------
+# limited and indexed (2 in 1)
+
+
+class LimitedIndexedOrderBook(OrderBook):
+    def __init__(self, snapshot={}, depth=None):
+        snapshot.update({
+            'asks': order_book_side.LimitedIndexedAsks(snapshot['asks'] or []),
+            'bids': order_book_side.LimitedIndexedBids(snapshot['bids'] or []),
+        })
+        super(LimitedIndexedOrderBook, self).__init__(snapshot)
+
+# -----------------------------------------------------------------------------
+# incremental and indexed (2 in 1)
+
+
+class IncrementalIndexedOrderBook(OrderBook):
+    def __init__(self, snapshot={}, depth=None):
+        snapshot.update({
+            'asks': order_book_side.IncrementalIndexedAsks(snapshot['asks'] or []),
+            'bids': order_book_side.IncrementalIndexedBids(snapshot['bids'] or []),
+        })
+        super(IncrementalIndexedOrderBook, self).__init__(snapshot)
