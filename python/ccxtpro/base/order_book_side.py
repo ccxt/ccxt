70d48deb628f1aac4698982bbcc792f81b81122e
Igor Kroitor <igor.kroitor@gmail.com>
2019-11-24 23:40:31 +0300

    order_book_side.py pep8

--- python/ccxtpro/base/order_book_side.py
+++ python/ccxtpro/base/order_book_side.py
@@ -77,7 +77,7 @@ class OrderBookSide(list):
     def extend(self, iterable):
         evaluated = list(iterable)
         length = len(evaluated)
-        self[self._len:self._len+length] = evaluated
+        self[self._len:self._len + length] = evaluated
         self._len += length
 
     def append(self, object):
