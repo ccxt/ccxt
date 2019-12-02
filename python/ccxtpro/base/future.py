4d1accbcde3960b376dba1387afeb5508adc4fee
Igor Kroitor <igor.kroitor@gmail.com>
2019-12-03 02:45:51 +0300

    future.py

--- python/ccxtpro/base/future.py
+++ python/ccxtpro/base/future.py
@@ -1,15 +1,10 @@
 import asyncio
 
 
-class Future:
-    def __init__(self):
-        self._future = asyncio.Future()
+class Future(asyncio.Future):
 
     def resolve(self, result):
-        self._future.set_result(result)
+        self.set_result(result)
 
     def reject(self, error):
-        self._future.set_exception(error)
-
-    def promise(self):
-        return self._future
+        self.set_exception(error)
