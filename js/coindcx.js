ef5b3561cfe46d08f50c11f94dffb84adf4aeb36
- <maximambrosevich@gmail.com>
2019-12-03 19:07:01 +0300

    start working on tickers

--- js/coindcx.js
+++ js/coindcx.js
@@ -14,8 +14,9 @@ module.exports = class coindcx extends Exchange {
             'countries': ['IN'], // india
             'urls': {
                 'api': {
-                    'api': 'https://api.coindcx.com',
+                    'general': 'https://api.coindcx.com',
                     'public': 'https://public.coindcx.com',
+                    'private': 'https://api.coindcx.com',
                 },
                 'www': 'https://coindcx.com/',
                 'doc': 'https://coindcx-official.github.io/rest-api/',
@@ -23,7 +24,7 @@ module.exports = class coindcx extends Exchange {
             },
             'version': 'v1',
             'api': {
-                'api': {
+                'general': {
                     'get': [
                         'exchange/ticker',
                         'exchange/v1/markets',
@@ -39,7 +40,21 @@ module.exports = class coindcx extends Exchange {
                 },
             },
             'has': {},
-            'timeframes': {},
+            'timeframes': {
+                '1m': '1m',
+                '5m': '5m',
+                '15m': '15m',
+                '30m': '30m',
+                '1h': '1h',
+                '2h': '2h',
+                '4h': '4h',
+                '6h': '6h',
+                '8h': '8h',
+                '1d': '1d',
+                '3d': '3d',
+                '1w': '1w',
+                '1M': '1M',
+            },
             'timeout': 10000,
             'rateLimit': 2000,
         });
@@ -47,7 +62,7 @@ module.exports = class coindcx extends Exchange {
 
     async fetchMarkets (params = {}) {
         // answer example https://coindcx-official.github.io/rest-api/?javascript#markets-details
-        const details = await this.apiGetExchangeV1MarketsDetails (params);
+        const details = await this.generalGetExchangeV1MarketsDetails (params);
         const result = [];
         for (let i = 0; i < details.length; i++) {
             const market = details[i];
@@ -89,6 +104,38 @@ module.exports = class coindcx extends Exchange {
             });
         }
         return result;
+    }
+
+    async fetchTickers (symbols = undefined, params = {}) {
+        await this.loadMarkets ();
+        console.log (this.markets)
+        const response = await this.generalGetExchangeTicker (params);
+        const result = {};
+        for (let i = 0; i < response.length; i++) {
+            const ticker = this.parseTicker (response[i]);
+            const symbol = ticker['symbol'];
+            result[symbol] = ticker;
+        }
+        return result;
+    }
+
+    async fetchTicker (symbol, params = {}) {
+        await this.loadMarkets ();
+        const response = await this.generalGetExchangeTicker (params);
+        const result = {};
+        for (let i = 0; i < response.length; i++) {
+            if (response[i].market !== this.markets[symbol]['id']) {
+                continue;
+            }
+            const ticker = this.parseTicker (response[i]);
+            const parsedSymbol = ticker['symbol'];
+            result[parsedSymbol] = ticker;
+        }
+        return result;
+    }
+
+    parseTicker (ticker) {
+        return ticker;
     }
 
     sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
