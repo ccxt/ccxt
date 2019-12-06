bf152afd6b7faeb0a2d0a8405308ea04b039861b
Igor Kroitor <igor.kroitor@gmail.com>
2019-12-06 14:57:33 +0300

    anxpro handle { "status": "Not available" }

--- js/anxpro.js
+++ js/anxpro.js
@@ -1195,11 +1195,13 @@ module.exports = class anxpro extends Exchange {
         }
         const result = this.safeString (response, 'result');
         const code = this.safeString (response, 'resultCode');
-        if (((result !== undefined) && (result !== 'success')) || ((code !== undefined) && (code !== 'OK'))) {
-            const message = this.safeString2 (response, 'error', 'status');
+        const status = this.safeString (response, 'status');
+        if (((result !== undefined) && (result !== 'success')) || ((code !== undefined) && (code !== 'OK')) || (status !== undefined)) {
+            const message = this.safeString (response, 'error');
             const feedback = this.id + ' ' + body;
             this.throwExactlyMatchedException (this.exceptions['exact'], code, feedback);
             this.throwExactlyMatchedException (this.exceptions['exact'], message, feedback);
+            this.throwExactlyMatchedException (this.exceptions['exact'], status, feedback);
             this.throwBroadlyMatchedException (this.exceptions['broad'], message, feedback);
             throw new ExchangeError (feedback); // unknown message
         }
