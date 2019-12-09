02f7685f1700b49df147a35b0cf4cabfc19e8c13
Igor Kroitor <igor.kroitor@gmail.com>
2019-12-09 22:23:13 +0300

    bw www url fix #6243

--- js/bw.js
+++ js/bw.js
@@ -67,7 +67,7 @@ module.exports = class bw extends Exchange {
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/69436317-31128c80-0d52-11ea-91d1-eb7bb5818812.jpg',
                 'api': 'https://www.{hostname}',
-                'www': 'https://www.{hostname}',
+                'www': 'https://www.bw.com',
                 'doc': 'https://github.com/bw-exchange/api_docs_en/wiki',
                 'fees': 'https://www.bw.com/feesRate',
             },
