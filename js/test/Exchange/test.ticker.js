9d7af9b0c9c269d4ae29cf0e5f91dc4a2ced82f8
Vladislav Gabov <vgabov@tab-trader.com>
2019-12-12 15:19:43 +0300

    added coinsbit to the exceptions.
    Coinsbit has been added to the exceptions, as it has overlaps for some symbols.

--- js/test/Exchange/test.ticker.js
+++ js/test/Exchange/test.ticker.js
@@ -81,6 +81,7 @@ module.exports = (exchange, ticker, method, symbol) => {
         'okex',
         'southxchange', // https://user-images.githubusercontent.com/1294454/59953532-314bea80-9489-11e9-85b3-2a711ca49aa7.png
         'bitmart',
+        'coinsbit', // The exchange has an overlaps for some symbols
 
     ].includes (exchange.id)) {
 
