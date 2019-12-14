de819d94dca4c1a2a17704a87cbb272b65a67feb
Igor Kroitor <igor.kroitor@gmail.com>
2019-12-14 21:27:02 +0300

    asyncio.base_futures.InvalidStateError: invalid state guard

--- python/ccxtpro/base/future.py
+++ python/ccxtpro/base/future.py
@@ -4,7 +4,9 @@ import asyncio
 class Future(asyncio.Future):
 
     def resolve(self, result=None):
-        self.set_result(result)
+        if not self.done():
+            self.set_result(result)
 
     def reject(self, error=None):
-        self.set_exception(error)
+        if not self.done():
+            self.set_exception(error)
