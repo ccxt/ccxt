9e0d029bcf78dd4a2d1978b5c0e6ac757c0981cc
Igor Kroitor <igor.kroitor@gmail.com>
2019-12-18 19:09:53 +0300

    Future.php

--- php/base/Future.php
+++ php/base/Future.php
@@ -3,23 +3,4 @@
 namespace ccxtpro;
 require_once __DIR__ . '/../vendor/autoload.php';
 
-
-class Future {
-    private $deferred;
-    public function __construct() {
-        $this->deferred = new \React\Promise\Deferred();
-    }
-
-    public function resolve($result) {
-        $this->deferred->resolve($result);
-    }
-
-    public function reject($result) {
-        $this->deferred->reject($result);
-
-    }
-
-    public function promise() {
-        return $this->deferred->promise();
-    }
-}
+class Future extends \React\Promise\Deferred {};
