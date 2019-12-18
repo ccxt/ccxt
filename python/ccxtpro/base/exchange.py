3990580eefab663e4d7e9d8f4e670af2e218f7b1
Igor Kroitor <igor.kroitor@gmail.com>
2019-12-18 09:39:08 +0300

    exchange.py cleanup

--- python/ccxtpro/base/exchange.py
+++ python/ccxtpro/base/exchange.py
@@ -115,32 +115,3 @@ class Exchange(BaseExchange):
             await self.clients[key].close()
             del self.clients[key]
         return await super(Exchange, self).close()
-
-    # -------------------------------------------------------------------------
-
-    # def handle_message(self, client, response):
-    #     message_hash = self.get_ws_message_hash(client, response)
-    #     if message_hash not in client.futures:
-    #         self.handle_dropped(client, response, message_hash)
-    #         return
-    #     future = client.futures[message_hash]
-    #     future.resolve(response)
-
-    # def handle_dropped(self, client, response, message_hash):
-    #     pass
-
-    # @classmethod
-    # def define_ws_api(cls, has):
-    #     methods = [name[name.index('Ws'):] for name, has in has.items() if has and 'Ws' in name]
-
-    #     def key_closure(k):
-    #         async def ws_x_message(_self, url, message_hash, subscribe=None):
-    #             result = await WebSocketClient.registerFuture(url, message_hash, _self.handle_message, _self.apiKey, subscribe, _self.asyncio_loop)
-    #             return getattr(_self, 'handle_' + k)(result)
-    #         return ws_x_message
-    #     for key in methods:
-    #         setattr(cls, key + 'Message', key_closure(cls.underscore(key)))
-
-    # def __repr__(self):
-    #     return 'ccxtpro.' + self.id + '()'
-    #
