29ff37a48ec692deba3bb661fb60f511473ff215
Igor Kroitor <igor.kroitor@gmail.com>
2019-12-19 02:15:24 +0300

    Future.php then()

--- php/base/Future.php
+++ php/base/Future.php
@@ -3,4 +3,10 @@
 namespace ccxtpro;
 require_once __DIR__ . '/../vendor/autoload.php';
 
-class Future extends \React\Promise\Deferred {};
+class Future extends \React\Promise\Deferred {
+
+    public function then(callable $onFulfilled = null, callable $onRejected = null): PromiseInterface {
+        return $this->promise()->then($onFulfilled, $onRejected);
+    }
+
+};
