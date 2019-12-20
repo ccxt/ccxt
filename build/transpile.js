91e491b3bd190710b81701773bd5ed5b52c522c6
Igor Kroitor <igor.kroitor@gmail.com>
2019-12-20 03:04:00 +0300

    build/transpile support php callables

--- build/transpile.js
+++ build/transpile.js
@@ -543,9 +543,13 @@ class Transpiler {
         let bodyAsString = body.join ("\n")
 
         for (let method of methods) {
-            const regex = new RegExp ('this->(' + method + ')([^a-zA-Z0-9])', 'g')
+            const regex = new RegExp ('\\$this->(' + method + ')(\\s\\(|[^a-zA-Z0-9])', 'g')
             bodyAsString = bodyAsString.replace (regex,
-                (match, p1, p2) => ('this->' + unCamelCase (p1) + p2))
+                (match, p1, p2) => {
+                    return ((p2 === ' (') ?
+                        ('$this->' + unCamelCase (p1) + p2) : // support direct php calls
+                        ("array($this, '" + unCamelCase (p1) + "')" + p2)) // as well as passing instance methods as callables
+                })
         }
 
         const footer =[
