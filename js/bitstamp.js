f03d429fa71e1a56908f97a0e48263fa8f8408c6
Igor Kroitor <igor.kroitor@gmail.com>
2020-01-08 05:30:57 +0300

    bitstamp API URLs minor edits

--- js/bitstamp.js
+++ js/bitstamp.js
@@ -30,6 +30,7 @@ module.exports = class bitstamp extends Exchange {
                 'logo': 'https://user-images.githubusercontent.com/1294454/27786377-8c8ab57e-5fe9-11e7-8ea4-2b05b6bcceec.jpg',
                 'api': {
                     'public': 'https://www.bitstamp.net/api',
+                    'private': 'https://www.bitstamp.net/api',
                 },
                 'www': 'https://www.bitstamp.net',
                 'doc': 'https://www.bitstamp.net/api',
