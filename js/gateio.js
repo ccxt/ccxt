2b67ff7d39cf5abc7de40fe6edd3e32c46be27fa
Carlo Revelli <carlo.revelli@berkeley.edu>
2020-01-11 00:02:07 +0300

    add support for gateio

--- js/gateio.js
+++ js/gateio.js
@@ -12,6 +12,9 @@ module.exports = class gateio extends ccxt.gateio {
         return this.deepExtend (super.describe (), {
             'has': {
                 'watchOrderBook': true,
+                'watchTicker': true,
+                'watchTrades': true,
+                'watchOHLCV': true,
             },
             'urls': {
                 'api': {
@@ -32,18 +35,17 @@ module.exports = class gateio extends ccxt.gateio {
         } else if (limit !== 1 && limit !== 5 && limit !== 10 && limit !== 20 && limit !== 30) {
             throw new ExchangeError (this.id + ' watchOrderBook limit argument must be undefined, 1, 5, 10, 20, or 30');
         }
-        const interval = this.safeString (params, 'interval', '0.1');
+        const interval = this.safeString (params, 'interval', '0.00000001');
         const precision = -1 * Math.log10 (interval);
         if (precision < 0 || precision > 8 || precision % 1 !== 0) {
             throw new ExchangeError (this.id + ' invalid interval');
         }
-        const messageHash = precision.toString () + ':' + marketId;
+        const messageHash = 'depth.update' + ':' + marketId;
         const subscribeMessage = {
             'id': requestId,
             'method': 'depth.subscribe',
             'params': [marketId, limit, interval],
         };
-        console.log (messageHash);
         const future = this.watch (url, messageHash, subscribeMessage);
         return future;
     }
@@ -62,63 +64,134 @@ module.exports = class gateio extends ccxt.gateio {
         const clean = params[0];
         const book = params[1];
         const marketId = params[2];
+        const methodType = message['method'];
+        const messageHash = methodType + ':' + marketId;
         let orderBook = undefined;
-        if (marketId in this.orderbooks) {
-            orderBook = this.orderbooks[marketId];
-        } else {
+        if (clean) {
             orderBook = this.orderBook ({});
-        }
-        const parsed = {};
-        let price = undefined;
-        let side = undefined;
-        if ('asks' in book) {
-            parsed['asks'] = this.parseOrderBookSide (book['asks']);
-            side = book['asks'];
+            this.orderbooks[marketId] = orderBook;
         } else {
-            parsed['asks'] = [];
+            orderBook = this.orderbooks[marketId];
         }
-        if ('bids' in book) {
-            parsed['bids'] = this.parseOrderBookSide (book['bids']);
-            side = book['bids'];
-        } else {
-            parsed['bids'] = [];
-        }
-        for (let i = 0; i < side.length; i++) {
-            const order = side[i];
-            if (order[0].indexOf ('.') > -1) {
-                price = order[0];
-                break;
+        const sides = ['bids', 'asks'];
+        for (let j = 0; j < 2; j++) {
+            const side = sides[j];
+            if (side in book) {
+                const bookSide = book[side];
+                for (let i = 0; i < bookSide.length; i++) {
+                    const order = bookSide[i];
+                    orderBook[side].store (parseFloat (order[0]), parseFloat (order[1]));
+                }
             }
         }
-        const afterDecimal = price.split ('.')[1];
-        const precision = afterDecimal.length;
-        const messageHash = precision.toString () + ':' + marketId;
-        if (clean) {
-            orderBook.reset (parsed);
-        } else {
-            orderBook.update (parsed);
-        }
-        client.resolve (messageHash, orderBook);
+        client.resolve (orderBook, messageHash);
     }
 
-    parseOrderBookSide (bookSide) {
-        const result = [];
-        for (let i = 0; i < bookSide.length; i++) {
-            const order = bookSide[i];
-            result.push ([parseFloat (order[0]), parseFloat (order[1])]);
+    async watchTicker (symbol, params = {}) {
+        await this.loadMarkets ();
+        const market = this.market (symbol);
+        const marketId = market['id'].toUpperCase ();
+        const requestId = this.nonce ();
+        const url = this.urls['api']['ws'];
+        const subscribeMessage = {
+            'id': requestId,
+            'method': 'ticker.subscribe',
+            'params': [marketId],
+        };
+        const messageHash = 'ticker.update' + ':' + marketId;
+        return await this.watch (url, messageHash, subscribeMessage);
+    }
+
+    handleTicker (client, message) {
+        const result = message['params'];
+        const marketId = result[0];
+        const normalMarketId = marketId.toLowerCase ();
+        let market = undefined;
+        if (normalMarketId in this.markets_by_id) {
+            market = this.markets_by_id[normalMarketId];
         }
-        return result;
+        const ticker = result[1];
+        const parsed = this.parseTicker (ticker, market);
+        const methodType = message['method'];
+        const messageHash = methodType + ':' + marketId;
+        client.resolve (parsed, messageHash);
+    }
+
+    async watchTrades (symbol, params = {}) {
+        await this.loadMarkets ();
+        const market = this.market (symbol);
+        const marketId = market['id'].toUpperCase ();
+        const requestId = this.nonce ();
+        const url = this.urls['api']['ws'];
+        const subscribeMessage = {
+            'id': requestId,
+            'method': 'trades.subscribe',
+            'params': [marketId],
+        };
+        const messageHash = 'trades.update' + ':' + marketId;
+        return await this.watch (url, messageHash, subscribeMessage);
+    }
+
+    handleTrades (client, messsage) {
+        const result = messsage['params'];
+        const marketId = result[0];
+        const normalMarketId = marketId.toLowerCase ();
+        let market = undefined;
+        if (normalMarketId in this.markets_by_id) {
+            market = this.markets_by_id[normalMarketId];
+        }
+        const trades = result[1];
+        for (let i = 0; i < trades.length; i++) {
+            const trade = trades[i];
+            const parsed = this.parseTrade (trade, market);
+            this.trades[marketId].push (parsed);
+        }
+        const methodType = messsage['method'];
+        const messageHash = methodType + ':' + marketId;
+        client.resolve (this.trades[marketId], messageHash);
+    }
+
+    async watchOHLCV (symbol, timeframe = '1m', since = undefined, limit = undefined, params = {}) {
+        await this.loadMarkets ();
+        const market = this.market (symbol);
+        const marketId = market['id'].toUpperCase ();
+        const requestId = this.nonce ();
+        const url = this.urls['api']['ws'];
+        const interval = parseInt (this.timeframes['10mins']);
+        const subscribeMessage = {
+            'id': requestId,
+            'method': 'kline.subscribe',
+            'params': [marketId, 1800],
+        };
+        const messageHash = 'kline.update' + ':' + marketId;
+        return await this.watch (url, messageHash, subscribeMessage);
+    }
+
+    handleOHLCV (client, message) {
+        const ohlcv = message['params'][0];
+        const marketId = ohlcv[7];
+        const normalMarketId = marketId.toLowerCase ();
+        let market = undefined;
+        if (normalMarketId in this.markets_by_id) {
+            market = this.markets_by_id[normalMarketId];
+        }
+        const parsed = this.parseOHLCV (ohlcv, market);
+        const methodType = message['method'];
+        const messageHash = methodType + ':' + marketId;
+        client.resolve (parsed, messageHash);
     }
 
     handleMessage (client, message) {
-        console.log (message);
         const methods = {
-            'depth.update': this.handleOrderBook.bind (this),
+            'depth.update': this.handleOrderBook,
+            'ticker.update': this.handleTicker,
+            'trades.update': this.handleTrades,
+            'kline.update': this.handleOHLCV,
         };
         const methodType = this.safeString (message, 'method');
         const method = this.safeValue (methods, methodType);
         if (method) {
-            method (client, message);
+            method.apply (this, [client, message]);
         }
     }
 };
