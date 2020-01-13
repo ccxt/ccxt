f17314e8d8a5d85d1686f2719f5dcf510254d75e
Alexey Marunin <a.v.marunin@yandex.ru>
2020-01-13 19:56:24 +0300

    bitfinex2: Fix transpile errors

--- js/bitfinex2.js
+++ js/bitfinex2.js
@@ -709,8 +709,10 @@ module.exports = class bitfinex2 extends bitfinex {
             throw new ExchangeError (this.id + ' ' + response[6] + ': ' + errorText + ' (#' + errorCode + ')');
         }
         const order = this.parseOrder (response[4][0]);
-        if (this.options['fetchOrderOnCreate'] === false) return order;
-        return await this.fetchOrder (order['id'], order['symbol']);
+        if (this.options['fetchOrderOnCreate']) {
+            return await this.fetchOrder (order['id'], order['symbol']);
+        }
+        return order;
     }
 
     async cancelAllOrders (params = {}) {
