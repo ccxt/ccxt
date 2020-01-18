2bd8f852ad674612aeff183ce34ba02668b268c7
Igor Kroitor <igor.kroitor@gmail.com>
2020-01-18 23:44:45 +0300

    bittrex wip

--- js/bittrex.js
+++ js/bittrex.js
@@ -46,11 +46,12 @@ module.exports = class bittrex extends ccxt.bittrex {
     }
 
     async negotiate (params = {}) {
-        let future = this.safeValue (this.options, 'negotiated');
+        const client = this.client (this.urls['api']['ws']);
+        const messageHash = 'negotiate';
+        let future = this.safeValue (client.subscriptions, messageHash);
         if (future === undefined) {
-            const client = this.client (this.urls['api']['ws']);
-            const messageHash = 'negotiate';
             future = client.future (messageHash);
+            client.subscriptions[messageHash] = future;
             const request = this.createSignalRQuery (params);
             const response = await this.signalrGetNegotiate (this.extend (request, params));
             //
@@ -67,8 +68,11 @@ module.exports = class bittrex extends ccxt.bittrex {
             //         LongPollDelay: 0
             //     }
             //
-            this.options['negotiate'] = response;
-            client.resolve (response, messageHash);
+            const result = {
+                'request': request,
+                'response': response,
+            };
+            client.resolve (result, messageHash);
         }
         return future;
     }
@@ -92,13 +96,11 @@ module.exports = class bittrex extends ccxt.bittrex {
         const future = client.future ('authenticated');
         const authenticate = this.safeValue (client.subscriptions, method);
         if (authenticate === undefined) {
-            const requestId = this.milliseconds ();
+            const requestId = this.milliseconds ().toString ();
             const request = {
                 // 'H': hub,
                 'M': method, // request method
-                'A': [
-                    this.apiKey,
-                ], // arguments
+                'A': [ this.apiKey ], // arguments
                 'I': requestId, // invocation request id
             };
             const subscription = {
@@ -109,40 +111,45 @@ module.exports = class bittrex extends ccxt.bittrex {
         return await future;
     }
 
-    async subscribeToOrderBook (negotiation, symbol, limit = undefined, params = {}) {
+    async subscribeToExchangeDeltas (negotiation, symbol, limit = undefined, params = {}) {
+        await this.loadMarkets ();
         const market = this.market (symbol);
-        const connectionToken = this.safeString (negotiation, 'ConnectionToken');
-        const query = this.createSignalRQuery ({
+        const connectionToken = this.safeString (negotiation['response'], 'ConnectionToken');
+        const query = this.extend (negotiation['request'], {
             'transport': 'webSockets',
             'connectionToken': connectionToken,
             // 'tid': this.milliseconds () % 10,
         });
         // wss://socket.bittrex.com/signalr/connect?transport=webSockets&clientProtocol=1.5&connectionToken=jm2IAcLJJHlW%2Bs%2FCJ8fGrs6ovAvWLczCyznWTnamXI5d2M73KcBuYXE9onggFPH5aPfK7Uh0lx115eVKZpNEng%2FCTJa3eMVp3HGzN9vKaYV86viH&connectionData=%5B%7B%22name%22%3A%22c2%22%7D%5D&tid=8
         const url = this.urls['api']['ws'] + '?' + this.urlencode (query);
-        const requestId = this.milliseconds ();
+        const requestId = this.milliseconds ().toString ();
         const method = 'SubscribeToExchangeDeltas';
         const messageHash = 'orderbook' + ':' + symbol;
         const subscribeHash = method + ':' + symbol;
+        const marketId = market['id'];
+        const hub = this.safeString (this.options, 'hub', 'c2');
         const request = {
-            'H': 'c2', // hub
+            'H': hub,
             'M': method,
-            'A': [
-                market['id'],
-            ],
+            'A': [ marketId ], // arguments
             'I': requestId, // invocation request id
         };
-        // const subscribeMessage = {
-        //     'id': requestId,
-        //     'method': 'balance.subscribe',
-        //     'params': [],
-        // };
-        return await this.watch (url, messageHash, request, subscribeHash);
+        const subscription = {
+            'id': requestId,
+            'symbol': symbol,
+            'limit': limit,
+            'params': params,
+            'method': this.handleSubscribeToExchangeDeltas,
+            'negotiation': negotiation,
+        };
+        const future = this.watch (url, messageHash, request, subscribeHash, subscription);
+        return await this.after (future, this.limitOrderBook, symbol, limit, params);
     }
 
     async watchOrderBook (symbol, limit = undefined, params = {}) {
         await this.loadMarkets ();
         const future = this.negotiate ();
-        return await this.afterAsync (future, this.subscribeToOrderBook, symbol, limit, params);
+        return await this.afterAsync (future, this.subscribeToExchangeDeltas, symbol, limit, params);
     }
 
     limitOrderBook (orderbook, symbol, limit = undefined, params = {}) {
@@ -161,30 +168,21 @@ module.exports = class bittrex extends ccxt.bittrex {
         }
     }
 
-    handleOrderBook (client, message) {
-        //
-        // first message (snapshot)
-        //
-        //     {
-        //         "type": "snapshot",
-        //         "product_id": "BTC-USD",
-        //         "bids": [
-        //             ["10101.10", "0.45054140"]
-        //         ],
-        //         "asks": [
-        //             ["10102.55", "0.57753524"]
-        //         ]
-        //     }
+    handleExchangeDelta (client, message) {
+        console.log (new Date (), 'handleExchangeDelta');
+        return message;
         //
         // subsequent updates
         //
         //     {
-        //         "type": "l2update",
-        //         "product_id": "BTC-USD",
-        //         "time": "2019-08-14T20:42:27.265Z",
-        //         "changes": [
-        //             [ "buy", "10101.80000000", "0.162567" ]
-        //         ]
+        //         M: 'BTC-ETH',
+        //         N: 2322248,
+        //         Z: [],
+        //         S: [
+        //             { TY: 0, R: 0.01938852, Q: 29.32758526 },
+        //             { TY: 1, R: 0.02322822, Q: 0 }
+        //         ],
+        //         f: []
         //     }
         //
         const type = this.safeString (message, 'type');
@@ -241,29 +239,132 @@ module.exports = class bittrex extends ccxt.bittrex {
         return message;
     }
 
+    async fetchExchangeState (symbol, limit = undefined, params = {}) {
+        await this.loadMarkets ();
+        const future = this.negotiate ();
+        return await this.afterAsync (future, this.queryExchangeState, symbol, limit, params);
+    }
+
+    async queryExchangeState (negotiation, symbol, limit = undefined, params = {}) {
+        // process.exit ();
+        await this.loadMarkets ();
+        // const symbol = this.safeString (subscription, 'symbol');
+        const market = this.market (symbol);
+        // const negotiation = this.safeValue (subscription, 'negotiation');
+        const connectionToken = this.safeString (negotiation['response'], 'ConnectionToken');
+        const query = this.extend (negotiation['request'], {
+            'transport': 'webSockets',
+            'connectionToken': connectionToken,
+            // 'tid': this.milliseconds () % 10,
+        });
+        // wss://socket.bittrex.com/signalr/connect?transport=webSockets&clientProtocol=1.5&connectionToken=jm2IAcLJJHlW%2Bs%2FCJ8fGrs6ovAvWLczCyznWTnamXI5d2M73KcBuYXE9onggFPH5aPfK7Uh0lx115eVKZpNEng%2FCTJa3eMVp3HGzN9vKaYV86viH&connectionData=%5B%7B%22name%22%3A%22c2%22%7D%5D&tid=8
+        const url = this.urls['api']['ws'] + '?' + this.urlencode (query);
+        const method = 'QueryExchangeState';
+        const requestId = this.milliseconds ().toString ();
+        const marketId = market['id'];
+        const hub = this.safeString (this.options, 'hub', 'c2');
+        const request = {
+            'H': hub,
+            'M': method,
+            'A': [ marketId ], // arguments
+            'I': requestId, // invocation request id
+        };
+        const subscription = {
+            'id': requestId,
+            'method': this.handleExchangeState,
+        };
+        const future = this.watch (url, requestId, request, requestId, subscription);
+        return await this.after (future, this.limitOrderBook, symbol, limit, params);
+
+        // // 3. Get a depth snapshot from https://www.binance.com/api/v1/depth?symbol=BNBBTC&limit=1000 .
+        // // todo: this is a synch blocking call in ccxt.php - make it async
+        // const snapshot = await this.fetchOrderBook (symbol);
+        // const orderbook = this.orderbooks[symbol];
+        // orderbook.reset (snapshot);
+        // // unroll the accumulated deltas
+        // const messages = orderbook.cache;
+        // for (let i = 0; i < messages.length; i++) {
+        //     const message = messages[i];
+        //     this.handleOrderBookMessage (client, message, orderbook);
+        // }
+        // this.orderbooks[symbol] = orderbook;
+        // client.resolve (orderbook, messageHash);
+    }
+
+    handleExchangeState (client, message, subscription) {
+        const R = JSON.parse (this.inflate (this.safeValue (message, 'R')));
+        console.log ('--------------------------------------------------------');
+        console.log (message);
+        console.log ('--------------------------------------------------------');
+        console.log (R);
+        console.log ('--------------------------------------------------------');
+        console.log (subscription);
+        console.log ('--------------------------------------------------------');
+        process.exit ();
+    }
+
+    handleSubscribeToExchangeDeltas (client, message, subscription) {
+        const symbol = this.safeString (subscription, 'symbol');
+        const limit = this.safeString (subscription, 'limit');
+        const params = this.safeString (subscription, 'params');
+        if (symbol in this.orderbooks) {
+            delete this.orderbooks[symbol];
+        }
+        this.orderbooks[symbol] = this.orderBook ({}, limit);
+        // fetch the snapshot in a separate async call
+        this.spawn (this.fetchExchangeState, symbol, limit, params);
+    }
+
     handleSubscriptionStatus (client, message) {
         //
-        //     {
-        //         type: 'subscriptions',
-        //         channels: [
-        //             {
-        //                 name: 'level2',
-        //                 product_ids: [ 'ETH-BTC' ]
-        //             }
-        //         ]
-        //     }
+        //     { R: true, I: '1579299273251' }
         //
+        const I = this.safeString (message, 'I');
+        let subscription = this.safeValue (client.subscriptions, I);
+        if (subscription === undefined) {
+            const subscriptionsById = this.indexBy (client.subscriptions, 'id');
+            subscription = this.safeValue (subscriptionsById, I, {});
+        } else {
+            // clear if subscriptionHash === requestId (one-time request)
+            delete client.subscriptions[I];
+        }
+        const method = this.safeValue (subscription, 'method');
+        if (method === undefined) {
+            client.resolve (message, I);
+        } else {
+            method.call (this, client, message, subscription);
+        }
+        return message;
+    }
+
+    handleSystemStatus (client, message) {
+        // send signalR protocol start() call
+        console.log (message);
         return message;
     }
 
     handleMessage (client, message) {
+        const methods = {
+            'uE': this.handleExchangeDelta,
+        };
         const M = this.safeValue (message, 'M', []);
-        const M0 = this.safeValue (M, 0, {});
-        const A = this.safeValue (M0, 'A', []);
-        const A0 = this.safeString (A, 0);
-        if (A0 !== undefined) {
-            console.log (A);
-            console.log (this.inflate (A0));
+        for (let i = 0; i < M.length; i++) {
+            const methodType = this.safeValue (M[i], 'M');
+            const method = this.safeValue (methods, methodType);
+            if (method !== undefined) {
+                const A = this.safeValue (M[i], 'A', []);
+                for (let k = 0; k < A.length; k++) {
+                    const message = JSON.parse (this.inflate (A[k]));
+                    method.call (this, client, message);
+                }
+            }
+        }
+        // resolve invocations by request id
+        if ('I' in message) {
+            this.handleSubscriptionStatus (client, message);
+        }
+        if ('S' in message) {
+            this.handleSystemStatus (client, message);
         }
         // const type = this.safeString (message, 'type');
         // const methods = {
@@ -280,11 +381,10 @@ module.exports = class bittrex extends ccxt.bittrex {
     }
 
     sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
-        let url = this.implodeParams (this.urls['api'][api], {
-            'hostname': this.hostname,
-        }) + '/';
         if (api === 'signalr') {
-            url += path;
+            let url = this.implodeParams (this.urls['api'][api], {
+                'hostname': this.hostname,
+            }) + '/' + path;
             if (Object.keys (params).length) {
                 url += '?' + this.urlencode (params);
             }
