b72807b1e221903c2e2bf928b20b7435266f2ce5
Igor Kroitor <igor.kroitor@gmail.com>
2020-01-25 14:01:43 +0300

    dsx fetchOrderBooks limit

--- js/dsx.js
+++ js/dsx.js
@@ -444,7 +444,7 @@ module.exports = class dsx extends Exchange {
         return this.parseOrderBook (orderbook);
     }
 
-    async fetchOrderBooks (symbols = undefined, params = {}) {
+    async fetchOrderBooks (symbols = undefined, limit = undefined, params = {}) {
         await this.loadMarkets ();
         let ids = undefined;
         if (symbols === undefined) {
@@ -461,6 +461,9 @@ module.exports = class dsx extends Exchange {
         const request = {
             'pair': ids,
         };
+        if (limit !== undefined) {
+            request['limit'] = limit; // default = 150, max = 2000
+        }
         const response = await this.publicGetDepthPair (this.extend (request, params));
         const result = {};
         ids = Object.keys (response);
