b112f7f6ad867ee674794b54d02ff0bf5c08bae6
Igor Kroitor <igor.kroitor@gmail.com>
2020-01-28 15:25:31 +0300

    poloniex watchTicker

--- js/poloniex.js
+++ js/poloniex.js
@@ -12,7 +12,7 @@ module.exports = class poloniex extends ccxt.poloniex {
         return this.deepExtend (super.describe (), {
             'has': {
                 'ws': true,
-                'watchTicker': false,
+                'watchTicker': true,
                 'watchOrderBook': true,
                 'watchBalance': false, // not implemented yet
             },
@@ -24,23 +24,75 @@ module.exports = class poloniex extends ccxt.poloniex {
         });
     }
 
-    handleTickers (client, response) {
-        const data = response[2];
-        const market = this.safeValue (this.options['marketsByNumericId'], data[0].toString ());
+    handleTickers (client, message) {
+        //
+        //     [
+        //         1002,
+        //         null,
+        //         [
+        //             50,               // currency pair id
+        //             '0.00663930',     // last trade price
+        //             '0.00663924',     // lowest ask
+        //             '0.00663009',     // highest bid
+        //             '0.01591824',     // percent change in last 24 hours
+        //             '176.03923205',   // 24h base volume
+        //             '26490.59208176', // 24h quote volume
+        //             0,                // is frozen
+        //             '0.00678580',     // highest price
+        //             '0.00648216'      // lowest price
+        //         ]
+        //     ]
+        //
+        const channelId = this.safeString (message, 0);
+        const subscribed = this.safeValue (message, 1);
+        if (subscribed) {
+            // skip subscription confirmation
+            return;
+        }
+        const ticker = this.safeValue (message, 2);
+        const numericId = this.safeString (ticker, 0);
+        const market = this.safeValue (this.options['marketsByNumericId'], numericId);
+        if (market === undefined) {
+            // todo handle market not found, reject corresponging futures
+            return;
+        }
         const symbol = this.safeString (market, 'symbol');
-        return {
-            'info': response,
+        const timestamp = this.milliseconds ();
+        let open = undefined;
+        let change = undefined;
+        let average = undefined;
+        const last = this.safeFloat (ticker, 1);
+        const relativeChange = this.safeFloat (ticker, 4);
+        if (relativeChange !== -1) {
+            open = last / this.sum (1, relativeChange);
+            change = last - open;
+            average = this.sum (last, open) / 2;
+        }
+        const result = {
             'symbol': symbol,
-            'last': parseFloat (data[1]),
-            'ask': parseFloat (data[2]),
-            'bid': parseFloat (data[3]),
-            'change': parseFloat (data[4]),
-            'baseVolume': parseFloat (data[5]),
-            'quoteVolume': parseFloat (data[6]),
-            'active': data[7] ? false : true,
-            'high': parseFloat (data[8]),
-            'low': parseFloat (data[9]),
+            'timestamp': timestamp,
+            'datetime': this.iso8601 (timestamp),
+            'high': this.safeFloat (ticker, 8),
+            'low': this.safeFloat (ticker, 9),
+            'bid': this.safeFloat (ticker, 3),
+            'bidVolume': undefined,
+            'ask': this.safeFloat (ticker, 2),
+            'askVolume': undefined,
+            'vwap': undefined,
+            'open': open,
+            'close': last,
+            'last': last,
+            'previousClose': undefined,
+            'change': change,
+            'percentage': relativeChange * 100,
+            'average': average,
+            'baseVolume': this.safeFloat (ticker, 6),
+            'quoteVolume': this.safeFloat (ticker, 5),
+            'info': ticker,
         };
+        this.tickers[symbol] = result;
+        const messageHash = channelId + ':' + numericId;
+        client.resolve (result, messageHash);
     }
 
     async watchBalance (params = {}) {
@@ -56,17 +108,31 @@ module.exports = class poloniex extends ccxt.poloniex {
         return await this.watch (url, messageHash, subscribe, channelId);
     }
 
+    async watchTicker (symbol, params = {}) {
+        await this.loadMarkets ();
+        const market = this.market (symbol);
+        const numericId = this.safeString (market, 'numericId');
+        const channelId = '1002';
+        const messageHash = channelId + ':' + numericId;
+        const url = this.urls['api']['ws'];
+        const subscribe = {
+            'command': 'subscribe',
+            'channel': channelId,
+        };
+        return await this.watch (url, messageHash, subscribe, channelId);
+    }
+
     async watchTickers (symbols = undefined, params = {}) {
         await this.loadMarkets ();
-        // rewrite
-        throw new NotImplemented (this.id + 'watchTickers not implemented yet');
-        // const market = this.market (symbol);
-        // const numericId = market['info']['id'].toString ();
-        // const url = this.urls['api']['websocket']['public'];
-        // return await this.WsTickerMessage (url, '1002' + numericId, {
-        //     'command': 'subscribe',
-        //     'channel': 1002,
-        // });
+        const channelId = '1002';
+        const messageHash = channelId;
+        const url = this.urls['api']['ws'];
+        const subscribe = {
+            'command': 'subscribe',
+            'channel': channelId,
+        };
+        const future = this.watch (url, messageHash, subscribe, channelId);
+        return await this.after (future, this.filterByArray, 'symbol', symbols);
     }
 
     async loadMarkets (reload = false, params = {}) {
@@ -301,7 +367,7 @@ module.exports = class poloniex extends ccxt.poloniex {
             if (method === undefined) {
                 return message;
             } else {
-                method.apply (this, client, message);
+                method.call (this, client, message);
             }
         } else {
             return this.handleOrderBookAndTrades (client, message);
