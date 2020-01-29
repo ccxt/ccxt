66052e21b136027f57bf5bd644cd1bb3d9251636
Igor Kroitor <igor.kroitor@gmail.com>
2020-01-29 16:47:45 +0300

    test_watch_ticker.php

--- php/test/Exchange/test_watch_ticker.php
+++ php/test/Exchange/test_watch_ticker.php
@@ -4,18 +4,26 @@ function test_watch_ticker($exchange, $symbol) {
 
     $future = new \ccxtpro\Future();
 
-    function tick($future, $iteration, $maxIterations, $exchange, $symbol) {
-        $exchange->watch_ticker($symbol)->then(function($result) use ($future, $iteration, $maxIterations, $exchange, $symbol) {
-            echo $result['datetime'], ' ', $exchange->id, ' ', $symbol, ' watch_ticker ', $result['last'] . "\n";
-            if (++$iteration < $maxIterations) {
-                tick($future, $iteration, $maxIterations, $exchange, $symbol);
-            } else {
-                $future->resolve($result);
-            }
-        });
-    };
+    if (array_key_exists('watchTicker', $exchange->has) && $exchange->has['watchTicker']) {
 
-    tick($future, 0, 5, $exchange, $symbol);
+        function tick_ticker($future, $iteration, $maxIterations, $exchange, $symbol) {
+            $exchange->watch_ticker($symbol)->then(function($result) use ($future, $iteration, $maxIterations, $exchange, $symbol) {
+                echo $result['datetime'], ' ', $exchange->id, ' ', $symbol, ' watch_ticker ', $result['last'] . "\n";
+                if (++$iteration < $maxIterations) {
+                    tick_ticker($future, $iteration, $maxIterations, $exchange, $symbol);
+                } else {
+                    $future->resolve($result);
+                }
+            });
+        };
+
+        tick_ticker($future, 0, 3, $exchange, $symbol);
+
+    } else {
+
+        echo $exchange->id, "watchTicker() is not supported or not implemented yet\n";
+        $future->resolve(true);
+    }
 
     return $future;
 
