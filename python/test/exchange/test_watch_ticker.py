1d232f142fb572e7e8ba63d2b2bf8cb80fdd39d9
Igor Kroitor <igor.kroitor@gmail.com>
2020-01-29 16:48:00 +0300

    test_watch_ticker.py

--- python/test/exchange/test_watch_ticker.py
+++ python/test/exchange/test_watch_ticker.py
@@ -7,14 +7,14 @@ async def test_watch_ticker(exchange, symbol):
     # todo add real ticker tests here
     # log (symbol.green, 'watching order book...')
     method = 'watchTicker'
-    if exchange.has[method]:
+    if (method in exchange.has) and exchange.has[method]:
         response = None
-        for i in range(0, 5):
+        for i in range(0, 3):
             response = await getattr(exchange, method)(symbol)
             test_ticker(exchange, response, method, symbol)
         return response
     else:
-        print(method, 'not supported')
+        print(exchange.id, method, 'is not supported or not implemented yet')
 
 
 __all__ = ['test_watch_ticker']
