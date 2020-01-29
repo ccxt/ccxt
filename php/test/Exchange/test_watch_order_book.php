1d1c169ad240fe78d34c5ff6279078a153ce0b04
Igor Kroitor <igor.kroitor@gmail.com>
2020-01-29 16:47:14 +0300

    test_watch_order_book.php

--- php/test/Exchange/test_watch_order_book.php
+++ php/test/Exchange/test_watch_order_book.php
@@ -1,7 +1,9 @@
 <?php
 
 function print_orderbook($orderbook) {
-    echo (isset($orderbook['nonce']) ? $orderbook['nonce'] : date('c ', (int) $orderbook['timestamp'] / 1000)), ' ',
+    $id = isset($orderbook['nonce']) ? $orderbook['nonce'] :
+        (isset($orderbook['timestamp']) ? date('c', (int) $orderbook['timestamp'] / 1000) : '');
+    echo $id, ' ',
         count($orderbook['asks']), ' asks ', json_encode($orderbook['asks'][0]), ' ',
         count($orderbook['bids']), ' bids ', json_encode($orderbook['bids'][0]), "\n";
 }
@@ -10,18 +12,26 @@ function test_watch_order_book($exchange, $symbol) {
 
     $future = new \ccxtpro\Future();
 
-    function tick($iteration, $maxIterations, $future, $method, ... $args) {
-        $method(... $args)->then(function($result) use ($iteration, $maxIterations, $future, $method, $args) {
-            print_orderbook($result);
-            if ($iteration < $maxIterations) {
-                tick(++$iteration, $maxIterations, $future, $method, ...$args);
-            } else {
-                $future->resolve($result);
-            }
-        });
-    };
+    if (array_key_exists('watchOrderBook', $exchange->has) && $exchange->has['watchOrderBook']) {
 
-    tick(0, 10, $future, array($exchange, 'watch_order_book'), $symbol);
+        function tick_order_book($iteration, $maxIterations, $future, $method, ... $args) {
+            $method(... $args)->then(function($result) use ($iteration, $maxIterations, $future, $method, $args) {
+                print_orderbook($result);
+                if ($iteration < $maxIterations) {
+                    tick_order_book(++$iteration, $maxIterations, $future, $method, ...$args);
+                } else {
+                    $future->resolve($result);
+                }
+            });
+        };
+
+        tick_order_book(0, 10, $future, array($exchange, 'watch_order_book'), $symbol);
+
+    } else {
+
+        echo $exchange->id, "watchOrderBook() is not supported or not implemented yet\n";
+        $future->resolve(true);
+    }
 
     return $future;
 }
