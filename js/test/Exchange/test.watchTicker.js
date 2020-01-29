c8d7cd0dbafce07533b6f1447c95b0d8d6f00328
Igor Kroitor <igor.kroitor@gmail.com>
2020-01-29 16:48:19 +0300

    test.watchTicker.js

--- js/test/Exchange/test.watchTicker.js
+++ js/test/Exchange/test.watchTicker.js
@@ -14,13 +14,13 @@ module.exports = async (exchange, symbol) => {
     const method = 'watchTicker'
 
     if (!exchange.has[method]) {
-        log (exchange.id, 'does not support', method + '() method')
+        log (exchange.id, method, 'is not supported or not implemented yet')
         return
     }
 
     let response = undefined
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 3; i++) {
 
         response = await exchange[method] (symbol)
         testTicker (exchange, response, method, symbol)
