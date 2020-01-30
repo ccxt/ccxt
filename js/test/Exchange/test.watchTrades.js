8ff0382a48e42be22a9292267a6ab75ffd0ed4dd
Igor Kroitor <igor.kroitor@gmail.com>
2020-01-30 14:12:29 +0300

    test.watchTrades.js

---
+++ js/test/Exchange/test.watchTrades.js
@@ -0,0 +1,46 @@
+'use strict'
+
+// ----------------------------------------------------------------------------
+
+const log = require ('ololog')
+    , chai = require ('chai')
+    , asTable = require ('as-table')
+    , assert = chai.assert
+    , testTrade = require ('ccxt/js/test/Exchange/test.trade.js')
+
+/*  ------------------------------------------------------------------------ */
+
+module.exports = async (exchange, symbol) => {
+
+    // log (symbol.green, 'watching order book...')
+
+    const method = 'watchTrades'
+
+    if (!exchange.has[method]) {
+        log (exchange.id, 'does not support', method + '() method')
+        return
+    }
+
+    let response = undefined
+
+    for (let i = 0; i < 3; i++) {
+
+        response = await exchange[method] (symbol)
+
+        log (asTable (response))
+
+        assert (response instanceof Array)
+        log (symbol.green, 'received', Object.values (response).length.toString ().green, 'trades')
+        let now = Date.now ()
+        for (let i = 0; i < response.length; i++) {
+            testTrade (exchange, response[i], symbol, now)
+            if (i > 0) {
+                if (response[i].timestamp && response[i - 1].timestamp) {
+                    assert (response[i].timestamp >= response[i - 1].timestamp)
+                }
+            }
+        }
+    }
+
+    return response
+}
