f73193e8b1da94a48d4e561359f9932318d7234e
Igor Kroitor <igor.kroitor@gmail.com>
2020-02-01 22:19:00 +0300

    ccxt 1.22.27 kraken integer timeframes

--- python/setup.py
+++ python/setup.py
@@ -78,7 +78,7 @@ setup(
         ':python_version>="3.5.2"': [
             'aiohttp>=3.0.1',
             'aiodns==1.1.1',
-            'ccxt>=1.22.26',
+            'ccxt>=1.22.27',
             'yarl==1.1.0',
         ],
         'qa': [
