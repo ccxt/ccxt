38e0bdda01c1cc331ecdf7eda8e72c1f4e2a57fb
Rohit <rohitchangediya@gmail.com>
2020-02-05 05:48:26 +0000

    levidge update

--- js/levidge.js
+++ js/levidge.js
@@ -15,42 +15,12 @@ module.exports = class levidge extends Exchange {
             'rateLimit': 500,
             'certified': false,
             'has': {
-                'fetchDepositAddress': false,
                 'CORS': false,
                 'fetchBidsAsks': true,
                 'fetchTickers': true,
-                'fetchTime': false,
                 'fetchOHLCV': true,
-                'fetchMyTrades': false,
                 'fetchOrder': true,
-                'fetchOrders': false,
-                'fetchOpenOrders': false,
-                'fetchClosedOrders': 'emulated',
-                'withdraw': false,
-                'fetchFundingFees': false,
-                'fetchDeposits': false,
-                'fetchWithdrawals': false,
-                'fetchTransactions': false,
-                'fetchTradingFee': false,
-                'fetchTradingFees': false,
-            },
-            'timeframes': {
-                '1m': '1m',
-                '3m': '3m',
-                '5m': '5m',
-                '15m': '15m',
-                '30m': '30m',
-                '1h': '1h',
-                '2h': '2h',
-                '4h': '4h',
-                '6h': '6h',
-                '8h': '8h',
-                '12h': '12h',
-                '1d': '1d',
-                '3d': '3d',
-                '1w': '1w',
-                '1M': '1M',
-            },
+                },
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/29604020-d5483cdc-87ee-11e7-94c7-d1a8d9169293.jpg',
                 'api': {
@@ -81,29 +51,10 @@ module.exports = class levidge extends Exchange {
                     ],
                 },
             },
-            // exchange-specific options
-            'exceptions': {
-                'API key does not exist': AuthenticationError,
-                'Order would trigger immediately.': InvalidOrder,
-                'Account has insufficient balance for requested action.': InsufficientFunds,
-                'Rest API trading is not enabled.': ExchangeNotAvailable,
-                '-1000': ExchangeNotAvailable, // {"code":-1000,"msg":"An unknown error occured while processing the request."}
-                '-1013': InvalidOrder, // createOrder -> 'invalid quantity'/'invalid price'/MIN_NOTIONAL
-                '-1021': InvalidNonce, // 'your time is ahead of server'
-                '-1022': AuthenticationError, // {"code":-1022,"msg":"Signature for this request is not valid."}
-                '-1100': InvalidOrder, // createOrder(symbol, 1, asdf) -> 'Illegal characters found in parameter 'price'
-                '-1104': ExchangeError, // Not all sent parameters were read, read 8 parameters but was sent 9
-                '-1128': ExchangeError, // {"code":-1128,"msg":"Combination of optional parameters invalid."}
-                '-2010': ExchangeError, // generic error code for createOrder -> 'Account has insufficient balance for requested action.', {"code":-2010,"msg":"Rest API trading is not enabled."}, etc...
-                '-2011': OrderNotFound, // cancelOrder(1, 'BTC/USDT') -> 'UNKNOWN_ORDER'
-                '-2013': OrderNotFound, // fetchOrder (1, 'BTC/USDT') -> 'Order does not exist'
-                '-2014': AuthenticationError, // { "code":-2014, "msg": "API-key format invalid." }
-                '-2015': AuthenticationError, // "Invalid API-key, IP, or permissions for action."
-            },
-        });
+            });
     }
 
-    async fetchInstruments (params = {}) {
+    async fetchTicker (params = {}) {
         const defaultType = this.safeString2 (this.options, 'fetchTickers', 'defaultType', 'spot');
         const type = this.safeString (params, 'type', defaultType);
         const method = (type === 'spot') ? 'publicGetInstruments' : 'fpublicGetInstruments';
@@ -111,28 +62,26 @@ module.exports = class levidge extends Exchange {
         return response;
     }
 
-    async fetchInstrumentPairs (params = {}) {
-        const defaultType = this.safeString2 (this.options, 'fetchTickers', 'defaultType', 'spot');
-        const type = this.safeString (params, 'type', defaultType);
-        const method = (type === 'spot') ? 'publicGetInstrumentPairs' : 'fpublicGetInstrumentPairs';
-        const response = await this[method] (params);
-        return response;
-    }
+    // async fetchInstrumentPairs (params = {}) {
+    //     const defaultType = this.safeString2 (this.options, 'fetchTickers', 'defaultType', 'spot');
+    //     const type = this.safeString (params, 'type', defaultType);
+    //     const method = (type === 'spot') ? 'publicGetInstrumentPairs' : 'fpublicGetInstrumentPairs';
+    //     const response = await this[method] (params);
+    //     return response;
+    // }
 
-    async fetchOrderBookSnap (id, params = {}) {
+    async fetchOrderBook (id, params = {}) {
         // console.log (id);
         const market = this.market (id);
         const request = {
             'pairId': market['id'],
         };
-        const defaultType = this.safeString2 (this.options, 'fetchOrder', 'defaultType', 'spot');
-        const type = this.safeString (params, 'type', defaultType);
-        const method = (type === 'spot') ? 'publicGetOrderBook' : 'fpublicGetOrderBook';
+        const method = market['spot'] ? 'publicGetOrderBook' : 'fpublicGetOrderBook';
         const response = await this[method] (this.extend (request, params));
         return response;
     }
 
-    async fetchTradeHistory (id, params = {}) {
+    async fetchBidsAsks (id, params = {}) {
         // console.log (id);
         const market = this.market (id);
         const request = {
