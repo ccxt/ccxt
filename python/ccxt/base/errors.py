f6d104c2181fd964c000eb61acd40f8928d11285
Carlo Revelli <carlo.revelli@berkeley.edu>
2020-02-21 15:15:26 -0800

    flake

--- python/ccxt/base/errors.py
+++ python/ccxt/base/errors.py
@@ -42,86 +42,114 @@ error_hierarchy = {
 class BaseError(Exception):
     pass
 
+
 class ExchangeError(BaseError):
     pass
+
 
 class AuthenticationError(ExchangeError):
     pass
 
+
 class PermissionDenied(AuthenticationError):
     pass
+
 
 class AccountSuspended(AuthenticationError):
     pass
 
+
 class ArgumentsRequired(ExchangeError):
     pass
+
 
 class BadRequest(ExchangeError):
     pass
 
+
 class BadSymbol(BadRequest):
     pass
+
 
 class BadResponse(ExchangeError):
     pass
 
+
 class NullResponse(BadResponse):
     pass
+
 
 class InsufficientFunds(ExchangeError):
     pass
 
+
 class InvalidAddress(ExchangeError):
     pass
+
 
 class AddressPending(InvalidAddress):
     pass
 
+
 class InvalidOrder(ExchangeError):
     pass
+
 
 class OrderNotFound(InvalidOrder):
     pass
 
+
 class OrderNotCached(InvalidOrder):
     pass
+
 
 class CancelPending(InvalidOrder):
     pass
 
+
 class OrderImmediatelyFillable(InvalidOrder):
     pass
+
 
 class OrderNotFillable(InvalidOrder):
     pass
 
+
 class DuplicateOrderId(InvalidOrder):
     pass
+
 
 class NotSupported(ExchangeError):
     pass
 
+
 class NetworkError(BaseError):
     pass
+
 
 class DDoSProtection(NetworkError):
     pass
 
+
 class RateLimitExceeded(DDoSProtection):
     pass
+
 
 class ExchangeNotAvailable(NetworkError):
     pass
 
+
 class OnMaintenance(ExchangeNotAvailable):
     pass
+
 
 class InvalidNonce(NetworkError):
     pass
 
+
 class RequestTimeout(NetworkError):
     pass
+
 
 __all__ = [
     error_hierarchy,
