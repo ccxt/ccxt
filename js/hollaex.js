344341e13f7ac9cadc4501b857fa0bf0213f4ed1
Igor Kroitor <igor.kroitor@gmail.com>
2020-03-04 06:04:29 +0300

    hollaex logo

--- js/hollaex.js
+++ js/hollaex.js
@@ -51,7 +51,7 @@ module.exports = class hollaex extends Exchange {
                 '1d': '1d',
             },
             'urls': {
-                'logo': 'https://user-images.githubusercontent.com/10441291/59487066-8058b500-8eb6-11e9-82fd-c9157b18c2d8.jpg',
+                'logo': 'https://user-images.githubusercontent.com/1294454/75841031-ca375180-5ddd-11ea-8417-b975674c23cb.jpg',
                 'api': 'https://api.hollaex.com',
                 'www': 'https://hollaex.com',
                 'doc': 'https://apidocs.hollaex.com',
