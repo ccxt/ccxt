3576342e3988c5703b0f63b69b3d6cd2d0709d7e
Travis CI <travis@travis-ci.org>
2020-03-05 06:34:17 +0000

    1.23.63
    [ci skip]

--- php/huobipro.php
+++ php/huobipro.php
@@ -156,6 +156,7 @@ class huobipro extends Exchange {
             'exceptions' => array(
                 'exact' => array(
                     // err-code
+                    'api-not-support-temp-addr' => '\\ccxt\\PermissionDenied', // array("status":"error","err-code":"api-not-support-temp-addr","err-msg":"API withdrawal does not support temporary addresses","data":null)
                     'timeout' => '\\ccxt\\RequestTimeout', // array("ts":1571653730865,"status":"error","err-code":"timeout","err-msg":"Request Timeout")
                     'gateway-internal-error' => '\\ccxt\\ExchangeNotAvailable', // array("status":"error","err-code":"gateway-internal-error","err-msg":"Failed to load data. Try again later.","data":null)
                     'account-frozen-balance-insufficient-error' => '\\ccxt\\InsufficientFunds', // array("status":"error","err-code":"account-frozen-balance-insufficient-error","err-msg":"trade account balance is not enough, left => `0.0027`","data":null)
