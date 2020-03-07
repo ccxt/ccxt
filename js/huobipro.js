0af42f84450ab5e32baa92ca621f51f0f935a532
Igor Kroitor <igor.kroitor@gmail.com>
2020-03-07 08:35:54 +0300

    huobipro logo

--- js/huobipro.js
+++ js/huobipro.js
@@ -49,7 +49,7 @@ module.exports = class huobipro extends Exchange {
                 '1y': '1year',
             },
             'urls': {
-                'logo': 'https://user-images.githubusercontent.com/1294454/27766569-15aa7b9a-5edd-11e7-9e7f-44791f4ee49c.jpg',
+                'logo': 'https://user-images.githubusercontent.com/1294454/76137448-22748a80-604e-11ea-8069-6e389271911d.jpg',
                 'api': {
                     'market': 'https://{hostname}',
                     'public': 'https://{hostname}',
