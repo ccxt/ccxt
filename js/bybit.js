c9dac71453b1d90295b91421657575d733050c75
Igor Kroitor <igor.kroitor@gmail.com>
2020-03-12 21:04:04 +0300

    bybit referral url

--- js/bybit.js
+++ js/bybit.js
@@ -66,7 +66,7 @@ module.exports = class bybit extends Exchange {
                     'https://github.com/bybit-exchange',
                 ],
                 'fees': 'https://help.bybit.com/hc/en-us/articles/360039261154',
-                'referral': 'https://www.deribit.com/reg-1189.4038',
+                'referral': 'https://www.bybit.com/app/register?ref=X7Prm',
             },
             'api': {
                 'public': {
