306fb5dab0d241802ff7d9d564a117cf99fc4cba
Igor Kroitor <igor.kroitor@gmail.com>
2020-03-15 02:27:02 +0300

    okcoincny hostname/urls

--- js/okcoincny.js
+++ js/okcoincny.js
@@ -12,9 +12,13 @@ module.exports = class okcoincny extends okcoinusd {
             'id': 'okcoincny',
             'name': 'OKCoin CNY',
             'countries': [ 'CN' ],
-            'hostname': 'okcoin.cn',
+            'hostname': 'www.okcoin.cn',
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/27766792-8be9157a-5ee5-11e7-926c-6d69b8d3378d.jpg',
+                'www': 'https://www.okcoin.cn',
+                'doc': 'https://www.okcoin.cn/docs/en/',
+                'fees': 'https://www.okcoin.cn/coin-fees',
+                'referral': 'https://www.okcoin.cn',
             },
         });
     }
