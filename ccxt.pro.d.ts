64d68b8c86fdbf3c4bffc156f948869384ae0513
Omid K. Rad <omidkrad@users.noreply.github.com>
2020-03-14 22:23:05 -0700

    Extend ccxt.Exchange

--- ccxt.pro.d.ts
+++ ccxt.pro.d.ts
@@ -1,5 +1,17 @@
-declare module 'ccxt' {
-    export class Exchange {
+import {
+    Balances,
+    Dictionary,
+    Exchange as BaseExchange,
+    OHLCV,
+    Order,
+    OrderBook,
+    Params,
+    Ticker,
+    Trade,
+} from 'ccxt';
+
+declare module 'ccxt.pro' {
+    export class Exchange extends BaseExchange {
         watchTicker (symbol: string, params?: Params): Promise<Ticker>;
         watchTickers (symbols?: string[], params?: Params): Promise<Dictionary<Ticker>>;
         watchOrderBook (symbol: string, limit?: number, params?: Params): Promise<OrderBook>;
