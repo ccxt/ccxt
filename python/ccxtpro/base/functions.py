be8f026957889ee701e140326e1a94b8a93253f1
Igor Kroitor <igor.kroitor@gmail.com>
2020-03-17 12:19:34 +0300

    functions.py pep8

--- python/ccxtpro/base/functions.py
+++ python/ccxtpro/base/functions.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from zlib import decompress, MAX_WBITS, decompressobj
+from zlib import decompress, MAX_WBITS
 from base64 import b64decode
 from gzip import GzipFile
 from io import BytesIO
 
 
 def inflate(data):
-    return decompress(data, -MAX_WBITS).decode ('utf-8')
+    return decompress(data, -MAX_WBITS).decode('utf-8')
 
 
 def inflate64(data):
