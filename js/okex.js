f4f1a56b97683c0b7438a15e0cf373b8aafc4443
Igor Kroitor <igor.kroitor@gmail.com>
2020-03-18 11:27:30 +0300

    okex filterBySinceLimit wrapper

--- js/okex.js
+++ js/okex.js
@@ -57,7 +57,11 @@ module.exports = class okex extends ccxt.okex {
 
     async watchTrades (symbol, since = undefined, limit = undefined, params = {}) {
         const future = this.subscribe ('trade', symbol, params);
-        return await this.after (future, this.filterBySinceLimit, since, limit);
+        return await this.after (future, this.filterArrayBySinceLimit, since, limit);
+    }
+
+    filterArrayBySinceLimit (array, since = undefined, limit = undefined, key = 'timestamp', tail = false) {
+        return this.filterBySinceLimit (array, since, limit, key, tail);
     }
 
     async watchTicker (symbol, params = {}) {
@@ -140,7 +144,7 @@ module.exports = class okex extends ccxt.okex {
         const interval = this.timeframes[timeframe];
         const name = 'candle' + interval + 's';
         const future = this.subscribe (name, symbol, params);
-        return await this.after (future, this.filterBySinceLimit, since, limit, 0);
+        return await this.after (future, this.filterArrayBySinceLimit, since, limit, 0, true);
     }
 
     findTimeframe (timeframe) {
