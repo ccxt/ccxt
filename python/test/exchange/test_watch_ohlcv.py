04b655ce89f2b64120fbe1d8e124b35f8b1c7ca4
Igor Kroitor <igor.kroitor@gmail.com>
2020-03-20 22:04:48 +0300

    test_watch_ohlcv enhancement

--- python/test/exchange/test_watch_ohlcv.py
+++ python/test/exchange/test_watch_ohlcv.py
@@ -3,6 +3,12 @@
 from exchange.test_ohlcv import test_ohlcv
 
 
+def as_table(values):
+    widths = [max([len(str(v[i])) for v in values]) for i in range(0, len(values[0]))]
+    string = ' | '.join(['{:<' + str(w) + '}' for w in widths])
+    return "\n".join([string.format(*v) for v in values])
+
+
 async def test_watch_ohlcv(exchange, symbol):
     # todo add real ohlcv tests here
     # log (symbol.green, 'watching ohlcvs...')
@@ -21,9 +27,11 @@ async def test_watch_ohlcv(exchange, symbol):
         while now < end:
             print('-----------------------------------------------------------')
             ohlcvs = await getattr(exchange, method)(symbol, timeframe, since, limit)
+            now = exchange.milliseconds()
+            print(exchange.iso8601(now), symbol, timeframe)
             for ohlcv in ohlcvs:
                 test_ohlcv(exchange, ohlcv, method, symbol)
-            now = exchange.milliseconds()
+            print(as_table([[exchange.iso8601(o[0])] + o[1:] for o in ohlcvs]))
         return response
     else:
         print(exchange.id, method, 'is not supported or not implemented yet')
