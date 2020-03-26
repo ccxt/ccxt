ae863dd920503eea6eb24f524910eb565ba05353
Igor Kroitor <igor.kroitor@gmail.com>
2020-03-26 11:29:35 +0300

    python/deploy.sh minor edit

--- python/deploy.sh
+++ python/deploy.sh
@@ -1,2 +1,2 @@
 python setup.py sdist bdist_wheel
-twine upload dist/* -u x84 -p ${PYPI_PASSWORD} --repository-url=https://pypi.python.org/pypi
+twine upload dist/* -u x84 -p ${PYPI_PASSWORD} --repository-url https://pypi.python.org/pypi
