51d7a66a0571731d3b2c7b1ed5e66304bad8ce36
Igor Kroitor <igor.kroitor@gmail.com>
2020-03-28 19:50:00 +0300

    bequant API urls structure inherited from hitbtc2

--- js/bequant.js
+++ js/bequant.js
@@ -13,7 +13,10 @@ module.exports = class bequant extends hitbtc2 {
             'countries': [ 'MT' ], // Malta
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/55248342-a75dfe00-525a-11e9-8aa2-05e9dca943c6.jpg',
-                'api': 'https://api.bequant.io',
+                'api': {
+                    'public': 'https://api.bequant.io',
+                    'private': 'https://api.bequant.io',
+                },
                 'www': 'https://bequant.io',
                 'doc': [
                     'https://api.bequant.io/',
