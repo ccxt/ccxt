da4c802e4d3094d3a740baa70ef6268c174b2e16
Igor Kroitor <igor.kroitor@gmail.com>
2020-04-02 09:17:13 +0300

    js/test/Exchange stability edits

--- js/test/Exchange/test.watchTicker.js
+++ js/test/Exchange/test.watchTicker.js
@@ -4,6 +4,7 @@
 
 const log = require ('ololog')
     , testTicker = require ('ccxt/js/test/Exchange/test.ticker.js')
+    , errors = require ('ccxt/js/base/errors.js')
 
 /*  ------------------------------------------------------------------------ */
 
@@ -25,11 +26,21 @@ module.exports = async (exchange, symbol) => {
 
     while (now < ends) {
 
-        response = await exchange[method] (symbol)
+        try {
+
+            response = await exchange[method] (symbol)
+
+            testTicker (exchange, response, method, symbol)
+
+        } catch (e) {
+
+            if (!(e instanceof errors.NetworkError)) {
+                throw e
+            }
+        }
 
         now = Date.now ()
 
-        testTicker (exchange, response, method, symbol)
     }
 
     return response
