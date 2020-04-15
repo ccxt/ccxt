3da5bd88bf7240e72a0330457a4ca2f8f8c5d6a7
Travis CI <travis@travis-ci.org>
2020-04-15 11:12:59 +0000

    1.26.28
    [ci skip]

--- python/ccxt/bitz.py
+++ python/ccxt/bitz.py
@@ -633,7 +633,7 @@ class bitz(Exchange):
                 request['to'] = self.sum(since, limit * duration * 1000)
         else:
             if since is not None:
-                raise ExchangeError(self.id + ' fetchOHLCV requires a limit argument if the since argument is specified')
+                raise ArgumentsRequired(self.id + ' fetchOHLCV requires a limit argument if the since argument is specified')
         response = self.marketGetKline(self.extend(request, params))
         #
         #     {   status:    200,
