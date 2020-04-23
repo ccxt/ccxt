827c0227d6e1de398df54557094f380509957c98
galvanizze <marek.galvanek@gmail.com>
2020-04-23 10:43:38 +0200

    [okex] add testnet support

--- js/okex.js
+++ js/okex.js
@@ -61,6 +61,9 @@ module.exports = class okex extends Exchange {
                 'doc': 'https://www.okex.com/docs/en/',
                 'fees': 'https://www.okex.com/pages/products/fees.html',
                 'referral': 'https://www.okex.com/join/1888677',
+                'test: {
+                    'rest': 'https://testnet.okex.com',
+                },
             },
             'api': {
                 'general': {
