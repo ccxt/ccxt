019028c1cfc4bbbff42448dc285142ca2cb32d85
Igor Kroitor <igor.kroitor@gmail.com>
2020-04-28 10:09:57 +0300

    binance defaultLimitOrderType deprecated #6861

--- js/binance.js
+++ js/binance.js
@@ -301,7 +301,6 @@ module.exports = class binance extends Exchange {
                 'fetchTradesMethod': 'publicGetAggTrades', // publicGetTrades, publicGetHistoricalTrades
                 'fetchTickersMethod': 'publicGetTicker24hr',
                 'defaultTimeInForce': 'GTC', // 'GTC' = Good To Cancel (default), 'IOC' = Immediate Or Cancel
-                'defaultLimitOrderType': 'limit', // or 'limit_maker'
                 'defaultType': 'spot', // 'spot', 'future'
                 'hasAlreadyAuthenticatedSuccessfully': false,
                 'warnOnFetchOpenOrdersWithoutSymbol': true,
