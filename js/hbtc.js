70f8b59c308b3cd7a6600bf30e28e0a3e4b57744
Igor Kroitor <igor.kroitor@gmail.com>
2020-05-01 22:29:38 +0300

    hbtc linting

--- js/hbtc.js
+++ js/hbtc.js
@@ -795,7 +795,6 @@ module.exports = class hbtc extends Exchange {
     }
 
     async fetchMyTrades (symbol = undefined, since = undefined, limit = undefined, params = {}) {
-
         if (symbol === undefined) {
             throw new ArgumentsRequired (this.id + ' fetchMyTrades requires a `symbol` argument');
         }
