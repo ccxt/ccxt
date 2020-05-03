001113fccebcc62cf32b637f56512e66078f248a
Travis CI <travis@travis-ci.org>
2020-05-03 17:30:21 +0000

    1.27.20
    [ci skip]

--- php/binance.php
+++ php/binance.php
@@ -334,6 +334,7 @@ class binance extends Exchange {
                 '-1022' => '\\ccxt\\AuthenticationError', // array("code":-1022,"msg":"Signature for this request is not valid.")
                 '-1100' => '\\ccxt\\InvalidOrder', // createOrder(symbol, 1, asdf) -> 'Illegal characters found in parameter 'price'
                 '-1104' => '\\ccxt\\ExchangeError', // Not all sent parameters were read, read 8 parameters but was sent 9
+                '-1116' => '\\ccxt\\InvalidOrder', // array("code":-1116,"msg":"Invalid orderType.")
                 '-1128' => '\\ccxt\\ExchangeError', // array("code":-1128,"msg":"Combination of optional parameters invalid.")
                 '-2010' => '\\ccxt\\ExchangeError', // generic error code for createOrder -> 'Account has insufficient balance for requested action.', array("code":-2010,"msg":"Rest API trading is not enabled."), etc...
                 '-2011' => '\\ccxt\\OrderNotFound', // cancelOrder(1, 'BTC/USDT') -> 'UNKNOWN_ORDER'
@@ -1170,13 +1171,17 @@ class binance extends Exchange {
             'side' => strtoupper($side),
         );
         if ($uppercaseType === 'MARKET') {
-            $quoteOrderQty = $this->safe_float($params, 'quoteOrderQty');
             $precision = $market['precision']['price'];
-            if ($quoteOrderQty !== null) {
-                $request['quoteOrderQty'] = $this->decimal_to_precision($quoteOrderQty, TRUNCATE, $precision, $this->precisionMode);
-                $params = $this->omit($params, 'quoteOrderQty');
-            } else if ($price !== null) {
-                $request['quoteOrderQty'] = $this->decimal_to_precision($amount * $price, TRUNCATE, $precision, $this->precisionMode);
+            if ($market['spot']) {
+                $quoteOrderQty = $this->safe_float($params, 'quoteOrderQty');
+                if ($quoteOrderQty !== null) {
+                    $request['quoteOrderQty'] = $this->decimal_to_precision($quoteOrderQty, TRUNCATE, $precision, $this->precisionMode);
+                    $params = $this->omit($params, 'quoteOrderQty');
+                } else if ($price !== null) {
+                    $request['quoteOrderQty'] = $this->decimal_to_precision($amount * $price, TRUNCATE, $precision, $this->precisionMode);
+                } else {
+                    $request['quantity'] = $this->amount_to_precision($symbol, $amount);
+                }
             } else {
                 $request['quantity'] = $this->amount_to_precision($symbol, $amount);
             }
--- python/ccxt/binance.py
+++ python/ccxt/binance.py
@@ -343,6 +343,7 @@ class binance(Exchange):
                 '-1022': AuthenticationError,  # {"code":-1022,"msg":"Signature for self request is not valid."}
                 '-1100': InvalidOrder,  # createOrder(symbol, 1, asdf) -> 'Illegal characters found in parameter 'price'
                 '-1104': ExchangeError,  # Not all sent parameters were read, read 8 parameters but was sent 9
+                '-1116': InvalidOrder,  # {"code":-1116,"msg":"Invalid orderType."}
                 '-1128': ExchangeError,  # {"code":-1128,"msg":"Combination of optional parameters invalid."}
                 '-2010': ExchangeError,  # generic error code for createOrder -> 'Account has insufficient balance for requested action.', {"code":-2010,"msg":"Rest API trading is not enabled."}, etc...
                 '-2011': OrderNotFound,  # cancelOrder(1, 'BTC/USDT') -> 'UNKNOWN_ORDER'
@@ -1105,13 +1106,16 @@ class binance(Exchange):
             'side': side.upper(),
         }
         if uppercaseType == 'MARKET':
-            quoteOrderQty = self.safe_float(params, 'quoteOrderQty')
             precision = market['precision']['price']
-            if quoteOrderQty is not None:
-                request['quoteOrderQty'] = self.decimal_to_precision(quoteOrderQty, TRUNCATE, precision, self.precisionMode)
-                params = self.omit(params, 'quoteOrderQty')
-            elif price is not None:
-                request['quoteOrderQty'] = self.decimal_to_precision(amount * price, TRUNCATE, precision, self.precisionMode)
+            if market['spot']:
+                quoteOrderQty = self.safe_float(params, 'quoteOrderQty')
+                if quoteOrderQty is not None:
+                    request['quoteOrderQty'] = self.decimal_to_precision(quoteOrderQty, TRUNCATE, precision, self.precisionMode)
+                    params = self.omit(params, 'quoteOrderQty')
+                elif price is not None:
+                    request['quoteOrderQty'] = self.decimal_to_precision(amount * price, TRUNCATE, precision, self.precisionMode)
+                else:
+                    request['quantity'] = self.amount_to_precision(symbol, amount)
             else:
                 request['quantity'] = self.amount_to_precision(symbol, amount)
         else:
