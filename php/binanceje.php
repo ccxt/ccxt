3e4be1dc4be90a000f34de8c3776fca7fdc7788f
Travis CI <travis@travis-ci.org>
2020-05-15 00:37:33 +0000

    1.27.75
    [ci skip]

--- php/binanceje.php
+++ php/binanceje.php
@@ -49,6 +49,9 @@ class binanceje extends binance {
                     'deposit' => array(),
                 ),
             ),
+            'options' => array(
+                'quoteOrderQty' => false, // whether market orders support amounts in quote currency
+            ),
         ));
     }
 }
