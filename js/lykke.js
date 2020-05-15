f70876cf5a977e19e76f245eeff39586782203f8
ndubel <38309641+ndubel@users.noreply.github.com>
2020-05-16 00:37:02 +0300

    Lykke new endpoints, timeframes
    https://public-api.lykke.com/swagger/ui/

--- js/lykke.js
+++ js/lykke.js
@@ -23,6 +23,19 @@ module.exports = class lykke extends Exchange {
                 'fetchOrder': true,
                 'fetchOrders': true,
             },
+            'timeframes': {
+                '1m': 'Minute',
+                '5m': 'Min5',
+                '15m': 'Min15',
+                '30m': 'Min30',
+                '1h': 'Hour',
+                '4h': 'Hour4',
+                '6h': 'Hour6',
+                '12h': 'Hour12',
+                '1d': 'Day',
+                '1w': 'Week',
+                '1M': 'Month',
+            },
             'requiredCredentials': {
                 'apiKey': true,
                 'secret': false,
@@ -49,8 +62,26 @@ module.exports = class lykke extends Exchange {
             'api': {
                 'mobile': {
                     'get': [
+                        'AssetPairs/rate',
+                        'AssetPairs/rate/{assetPairId}',
+                        'AssetPairs/dictionary/{market}',
+                        'Assets/dictionary',
+                        'Candles/history/{market}/available',
+                        'Candles/history/{market}/{assetPair}/{period}/{type}/{from}/{to}',
+                        'Company/ownershipStructure',
+                        'Company/registrationsCount',
+                        'IsAlive',
+                        'Market',
                         'Market/{market}',
+                        'Market/capitalization/{market}',
+                        'OrderBook',
+                        'OrderBook/{assetPairId}',
                         'Trades/{AssetPairId}',
+                        'Trades/Last/{assetPair}/{n}',
+                    ],
+                    'post': [
+                        'AssetPairs/rate/history',
+                        'AssetPairs/rate/history/{assetPairId}',
                     ],
                 },
                 'public': {
