8213c83fabdf85b6c352686329c9ed1a9acbfb01
Carlo Revelli <carlo.revelli@berkeley.edu>
2020-05-18 22:05:49 -0400

    defer creating object

--- js/binance.js
+++ js/binance.js
@@ -420,8 +420,11 @@ module.exports = class binance extends ccxt.binance {
         const event = this.safeString (message, 'e');
         const messageHash = lowerCaseId + '@' + event;
         const trade = this.parseTrade (message, market);
-        const limit = this.safeInteger (this.options, 'tradesLimit', 1000);
-        const array = this.safeValue (this.trades, symbol, new ArrayCache (limit));
+        let array = this.safeValue (this.trades, symbol);
+        if (array === undefined) {
+            const limit = this.safeInteger (this.options, 'tradesLimit', 1000);
+            array = new ArrayCache (limit);
+        }
         array.append (trade);
         this.trades[symbol] = array;
         client.resolve (array, messageHash);
