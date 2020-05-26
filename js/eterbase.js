33695a2246e5b2be9dd1fe731577f547213f0868
Igor Kroitor <igor.kroitor@gmail.com>
2020-05-26 23:15:47 +0300

    eterbase has['withdraw'] = true

--- js/eterbase.js
+++ js/eterbase.js
@@ -39,7 +39,7 @@ module.exports = class eterbase extends Exchange {
                 'fetchTickers': true,
                 'fetchTime': true,
                 'fetchTrades': true,
-                'withdraw': false,
+                'withdraw': true,
             },
             'timeframes': {
                 '1m': '1',
