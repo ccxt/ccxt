a268d4639e54798125b99669756debe8d875463e
Travis CI <travis@travis-ci.org>
2020-05-31 03:33:54 +0000

    1.28.90
    [ci skip]

--- php/base/Exchange.php
+++ php/base/Exchange.php
@@ -35,7 +35,7 @@ use kornrunner\Solidity;
 use Elliptic\EC;
 use BN\BN;
 
-$version = '1.28.89';
+$version = '1.28.90';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -54,7 +54,7 @@ const PAD_WITH_ZERO = 1;
 
 class Exchange {
 
-    const VERSION = '1.28.89';
+    const VERSION = '1.28.90';
 
     public static $exchanges = array(
         '_1btcxe',
