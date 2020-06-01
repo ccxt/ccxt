a4237f0bed36a8dc37e35401abac2e507eaedca2
Igor Kroitor <igor.kroitor@gmail.com>
2020-06-01 09:30:03 +0300

    binance loadTimeDifference propagate params

--- js/binance.js
+++ js/binance.js
@@ -357,8 +357,8 @@ module.exports = class binance extends Exchange {
         return this.safeInteger (response, 'serverTime');
     }
 
-    async loadTimeDifference () {
-        const serverTime = await this.fetchTime ();
+    async loadTimeDifference (params = {}) {
+        const serverTime = await this.fetchTime (params);
         const after = this.milliseconds ();
         this.options['timeDifference'] = after - serverTime;
         return this.options['timeDifference'];
