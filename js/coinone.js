542c1041d4a70aef9308edb65f232015f8fc9c08
Igor Kroitor <igor.kroitor@gmail.com>
2020-06-10 14:48:06 +0300

    coinone linting

--- js/coinone.js
+++ js/coinone.js
@@ -293,7 +293,7 @@ module.exports = class coinone extends Exchange {
         };
         const method = 'privatePostOrder' + this.capitalize (type) + this.capitalize (side);
         const response = await this[method] (this.extend (request, params));
-        let id = this.safeStringUpper (response, 'orderId');
+        const id = this.safeStringUpper (response, 'orderId');
         const timestamp = this.milliseconds ();
         const cost = price * amount;
         const order = {
