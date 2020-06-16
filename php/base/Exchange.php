0cd1f4085a6068eb1d601e23cd4c806554681e6c
Carlo Revelli <carlo.revelli@berkeley.edu>
2020-06-16 13:56:13 -0400

    retrigger

--- php/base/Exchange.php
+++ php/base/Exchange.php
@@ -2676,7 +2676,7 @@ class Exchange {
 
     public static function has_web3() {
         // PHP version of this function does nothing, as most of its
-        // dependencies are very lightweight
+        // dependencies are extremely lightweight
         return true;
     }
 
