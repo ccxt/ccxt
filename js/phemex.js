0ff6d212c1303b9e344726bad74cceb417205f7d
Igor Kroitor <igor.kroitor@gmail.com>
2020-06-17 08:48:10 +0300

    phemex fetchMarkets

--- js/phemex.js
+++ js/phemex.js
@@ -96,8 +96,212 @@ module.exports = class phemex extends Exchange {
         return this.safeFloat (parts, 0);
     }
 
+    parseSwapMarket (market) {
+        //
+        //     {
+        //         "symbol":"BTCUSD",
+        //         "displaySymbol":"BTC / USD",
+        //         "indexSymbol":".BTC",
+        //         "markSymbol":".MBTC",
+        //         "fundingRateSymbol":".BTCFR",
+        //         "fundingRate8hSymbol":".BTCFR8H",
+        //         "contractUnderlyingAssets":"USD",
+        //         "settleCurrency":"BTC",
+        //         "quoteCurrency":"USD",
+        //         "contractSize":"1 USD",
+        //         "lotSize":1,
+        //         "tickSize":0.5,
+        //         "priceScale":4,
+        //         "ratioScale":8,
+        //         "pricePrecision":1,
+        //         "minPriceEp":5000,
+        //         "maxPriceEp":10000000000,
+        //         "maxOrderQty":1000000,
+        //         "type":"Perpetual"
+        //         "steps":"50",
+        //         "riskLimits":[
+        //             {"limit":100,"initialMargin":"1.0%","initialMarginEr":1000000,"maintenanceMargin":"0.5%","maintenanceMarginEr":500000},
+        //             {"limit":150,"initialMargin":"1.5%","initialMarginEr":1500000,"maintenanceMargin":"1.0%","maintenanceMarginEr":1000000},
+        //             {"limit":200,"initialMargin":"2.0%","initialMarginEr":2000000,"maintenanceMargin":"1.5%","maintenanceMarginEr":1500000},
+        //         ],
+        //         "underlyingSymbol":".BTC",
+        //         "baseCurrency":"BTC",
+        //         "settlementCurrency":"BTC",
+        //         "valueScale":8,
+        //         "defaultLeverage":0,
+        //         "maxLeverage":100,
+        //         "initMarginEr":"1000000",
+        //         "maintMarginEr":"500000",
+        //         "defaultRiskLimitEv":10000000000,
+        //         "deleverage":true,
+        //         "makerFeeRateEr":-250000,
+        //         "takerFeeRateEr":750000,
+        //         "fundingInterval":8,
+        //         "marketUrl":"https://phemex.com/trade/BTCUSD",
+        //         "description":"BTCUSD is a BTC/USD perpetual contract priced on the .BTC Index. Each contract is worth 1 USD of Bitcoin. Funding is paid and received every 8 hours. At UTC time: 00:00, 08:00, 16:00.",
+        //     }
+        //
+        const id = this.safeString (market, 'symbol');
+        const quoteId = this.safeString (market, 'quoteCurrency');
+        const baseId = this.safeString (market, 'baseCurrency');
+        const type = this.safeStringLower (market, 'type');
+        let taker = undefined;
+        let maker = undefined;
+        let inverse = false;
+        const spot = false;
+        const swap = true;
+        const settleCurrency = this.safeString (market, 'settleCurrency');
+        if (settleCurrency !== quoteId) {
+            inverse = true;
+        }
+        const linear = !inverse;
+        const precision = {
+            'amount': this.safeFloat (market, 'lotSize'),
+            'price': this.safeFloat (market, 'tickSize'),
+        };
+        const priceScale = this.safeInteger (market, 'priceScale');
+        const ratioScale = this.safeInteger (market, 'ratioScale');
+        const priceScalePower = Math.pow (10, -priceScale);
+        const ratioScalePower = Math.pow (10, -ratioScale);
+        const minPriceEp = this.safeFloat (market, 'minPriceEp');
+        const maxPriceEp = this.safeFloat (market, 'maxPriceEp');
+        const makerFeeRateEr = this.safeFloat (market, 'makerFeeRateEr');
+        const takerFeeRateEr = this.safeFloat (market, 'takerFeeRateEr');
+        let minPrice = undefined;
+        let maxPrice = undefined;
+        if ((minPriceEp !== undefined) && (precision['price'] !== undefined)) {
+            minPrice = parseFloat (this.decimalToPrecision (minPriceEp * priceScalePower, ROUND, precision['price'], this.precisionMode));
+        }
+        if ((maxPriceEp !== undefined) && (precision['price'] !== undefined)) {
+            maxPrice = parseFloat (this.decimalToPrecision (maxPriceEp * priceScalePower, ROUND, precision['price'], this.precisionMode));
+        }
+        if (makerFeeRateEr !== undefined) {
+            maker = parseFloat (this.decimalToPrecision (makerFeeRateEr * ratioScalePower, ROUND, 0.00000001, this.precisionMode));
+        }
+        if (takerFeeRateEr !== undefined) {
+            taker = parseFloat (this.decimalToPrecision (takerFeeRateEr * ratioScalePower, ROUND, 0.00000001, this.precisionMode));
+        }
+        const limits = {
+            'amount': {
+                'min': precision['amount'],
+                'max': undefined,
+            },
+            'price': {
+                'min': minPrice,
+                'max': maxPrice,
+            },
+            'cost': {
+                'min': undefined,
+                'max': this.parseSafeFloat (this.safeString (market, 'maxOrderQty')),
+            },
+        };
+        const base = this.safeCurrencyCode (baseId);
+        const quote = this.safeCurrencyCode (quoteId);
+        const symbol = base + '/' + quote;
+        const active = undefined;
+        return {
+            'id': id,
+            'symbol': symbol,
+            'base': base,
+            'quote': quote,
+            'baseId': baseId,
+            'quoteId': quoteId,
+            'info': market,
+            'type': type,
+            'spot': spot,
+            'swap': swap,
+            'linear': linear,
+            'inverse': inverse,
+            'active': active,
+            'taker': taker,
+            'maker': maker,
+            'precision': precision,
+            'limits': limits,
+        };
+    }
+
+    parseSpotMarket (market) {
+        //
+        //     {
+        //         "symbol":"sBTCUSDT",
+        //         "displaySymbol":"BTC / USDT",
+        //         "quoteCurrency":"USDT",
+        //         "pricePrecision":2,
+        //         "type":"Spot",
+        //         "baseCurrency":"BTC",
+        //         "baseTickSize":"0.000001 BTC",
+        //         "baseTickSizeEv":100,
+        //         "quoteTickSize":"0.01 USDT",
+        //         "quoteTickSizeEv":1000000,
+        //         "minOrderValue":"10 USDT",
+        //         "minOrderValueEv":1000000000,
+        //         "maxBaseOrderSize":"1000 BTC",
+        //         "maxBaseOrderSizeEv":100000000000,
+        //         "maxOrderValue":"5,000,000 USDT",
+        //         "maxOrderValueEv":500000000000000,
+        //         "defaultTakerFee":"0.001",
+        //         "defaultTakerFeeEr":100000,
+        //         "defaultMakerFee":"0.001",
+        //         "defaultMakerFeeEr":100000,
+        //         "baseQtyPrecision":6,
+        //         "quoteQtyPrecision":2
+        //     }
+        //
+        const type = this.safeStringLower (market, 'type');
+        const id = this.safeString (market, 'symbol');
+        const quoteId = this.safeString (market, 'quoteCurrency');
+        const baseId = this.safeString (market, 'baseCurrency');
+        const linear = undefined;
+        const inverse = undefined;
+        const spot = true;
+        const swap = false;
+        const taker = this.safeFloat (market, 'defaultTakerFee');
+        const maker = this.safeFloat (market, 'defaultMakerFee');
+        const precision = {
+            'amount': this.parseSafeFloat (this.safeString (market, 'baseTickSize')),
+            'price': this.parseSafeFloat (this.safeString (market, 'quoteTickSize')),
+        };
+        const limits = {
+            'amount': {
+                'min': precision['amount'],
+                'max': this.parseSafeFloat (this.safeString (market, 'maxBaseOrderSize')),
+            },
+            'price': {
+                'min': precision['price'],
+                'max': undefined,
+            },
+            'cost': {
+                'min': this.parseSafeFloat (this.safeString (market, 'minOrderValue')),
+                'max': this.parseSafeFloat (this.safeString (market, 'maxOrderValue')),
+            },
+        };
+        const base = this.safeCurrencyCode (baseId);
+        const quote = this.safeCurrencyCode (quoteId);
+        const symbol = base + '/' + quote;
+        const active = undefined;
+        return {
+            'id': id,
+            'symbol': symbol,
+            'base': base,
+            'quote': quote,
+            'baseId': baseId,
+            'quoteId': quoteId,
+            'info': market,
+            'type': type,
+            'spot': spot,
+            'swap': swap,
+            'linear': linear,
+            'inverse': inverse,
+            'active': active,
+            'taker': taker,
+            'maker': maker,
+            'precision': precision,
+            'limits': limits,
+        };
+    }
+
     async fetchMarkets (params = {}) {
-        const response = await this.publicGetCfgV2Products (params);
+        const v2Products = await this.publicGetCfgV2Products (params);
         //
         //     {
         //         "code":0,
@@ -175,131 +379,63 @@ module.exports = class phemex extends Exchange {
         //         }
         //     }
         //
-        const data = this.safeValue (response, 'data', {});
-        const products = this.safeValue (data, 'products', []);
-        const riskLimits = this.safeValue (data, 'riskLimits', []);
+        const v1Products = await this.v1GetExchangePublicProducts (params);
+        const v1ProductsData = this.safeValue (v1Products, 'data', []);
+        //
+        //     {
+        //         "code":0,
+        //         "msg":"OK",
+        //         "data":[
+        //             {
+        //                 "symbol":"BTCUSD",
+        //                 "underlyingSymbol":".BTC",
+        //                 "quoteCurrency":"USD",
+        //                 "baseCurrency":"BTC",
+        //                 "settlementCurrency":"BTC",
+        //                 "maxOrderQty":1000000,
+        //                 "maxPriceEp":100000000000000,
+        //                 "lotSize":1,
+        //                 "tickSize":"0.5",
+        //                 "contractSize":"1 USD",
+        //                 "priceScale":4,
+        //                 "ratioScale":8,
+        //                 "valueScale":8,
+        //                 "defaultLeverage":0,
+        //                 "maxLeverage":100,
+        //                 "initMarginEr":"1000000",
+        //                 "maintMarginEr":"500000",
+        //                 "defaultRiskLimitEv":10000000000,
+        //                 "deleverage":true,
+        //                 "makerFeeRateEr":-250000,
+        //                 "takerFeeRateEr":750000,
+        //                 "fundingInterval":8,
+        //                 "marketUrl":"https://phemex.com/trade/BTCUSD",
+        //                 "description":"BTCUSD is a BTC/USD perpetual contract priced on the .BTC Index. Each contract is worth 1 USD of Bitcoin. Funding is paid and received every 8 hours. At UTC time: 00:00, 08:00, 16:00.",
+        //                 "type":"Perpetual"
+        //             },
+        //         ]
+        //     }
+        //
+        const v2ProductsData = this.safeValue (v2Products, 'data', {});
+        const products = this.safeValue (v2ProductsData, 'products', []);
+        const riskLimits = this.safeValue (v2ProductsData, 'riskLimits', []);
         const riskLimitsById = this.indexBy (riskLimits, 'symbol');
+        const v1ProductsById = this.indexBy (v1ProductsData, 'symbol');
         const result = [];
         for (let i = 0; i < products.length; i++) {
             let market = products[i];
-            const id = this.safeString (market, 'symbol');
-            const quoteId = this.safeString (market, 'quoteCurrency');
-            let type = this.safeStringLower (market, 'type');
-            let baseId = undefined;
-            let precision = undefined;
-            let limits = undefined;
-            let spot = undefined;
-            let swap = undefined;
-            let taker = undefined;
-            let maker = undefined;
-            let linear = undefined;
-            let inverse = undefined;
+            const type = this.safeStringLower (market, 'type');
             if (type === 'perpetual') {
-                type = 'swap';
-                spot = false;
-                swap = true;
+                const id = this.safeString (market, 'symbol');
                 const riskLimitValues = this.safeValue (riskLimitsById, id, {});
                 market = this.extend (market, riskLimitValues);
-                const settleCurrency = this.safeString (market, 'settleCurrency');
-                const contractUnderlyingAssets = this.safeString (market, 'contractUnderlyingAssets');
-                if (settleCurrency === quoteId) {
-                    baseId = contractUnderlyingAssets;
-                    inverse = false;
-                } else {
-                    baseId = settleCurrency;
-                    inverse = true;
-                }
-                linear = !inverse;
-                precision = {
-                    'amount': this.safeFloat (market, 'lotSize'),
-                    'price': this.safeFloat (market, 'tickSize'),
-                };
-                const priceScale = this.safeInteger (market, 'priceScale');
-                const tenToPriceScale = Math.pow (10, -priceScale);
-                const minPriceEp = this.safeFloat (market, 'minPriceEp');
-                const maxPriceEp = this.safeFloat (market, 'maxPriceEp');
-                let minPrice = undefined;
-                let maxPrice = undefined;
-                if (minPriceEp !== undefined) {
-                    minPrice = parseFloat (this.decimalToPrecision (minPriceEp * tenToPriceScale, ROUND, precision['price'], this.precisionMode));
-                }
-                if (maxPriceEp !== undefined) {
-                    maxPrice = parseFloat (this.decimalToPrecision (maxPriceEp * tenToPriceScale, ROUND, precision['price'], this.precisionMode));
-                }
-                limits = {
-                    'amount': {
-                        'min': precision['amount'],
-                        'max': undefined,
-                    },
-                    'price': {
-                        'min': minPrice,
-                        'max': maxPrice,
-                    },
-                    'cost': {
-                        'min': this.parseSafeFloat (this.safeString (market, 'minOrderValue')),
-                        'max': undefined,
-                    },
-                };
-                // console.log (market);
-                // process.exit ();
-                // continue;
-            } else if (type === 'spot') {
-                baseId = this.safeString (market, 'baseCurrency');
-                spot = true;
-                swap = false;
-                taker = this.safeFloat (market, 'defaultTakerFee');
-                maker = this.safeFloat (market, 'defaultMakerFee');
-                precision = {
-                    'amount': this.parseSafeFloat (this.safeString (market, 'baseTickSize')),
-                    'price': this.parseSafeFloat (this.safeString (market, 'quoteTickSize')),
-                };
-                limits = {
-                    'amount': {
-                        'min': precision['amount'],
-                        'max': this.parseSafeFloat (this.safeString (market, 'maxBaseOrderSize')),
-                    },
-                    'price': {
-                        'min': precision['price'],
-                        'max': undefined,
-                    },
-                    'cost': {
-                        'min': this.parseSafeFloat (this.safeString (market, 'minOrderValue')),
-                        'max': this.parseSafeFloat (this.safeString (market, 'maxOrderValue')),
-                    },
-                };
+                const v1ProductsValues = this.safeValue (v1ProductsById, id, {});
+                market = this.extend (market, v1ProductsValues);
+                market = this.parseSwapMarket (market);
+            } else {
+                market = this.parseSpotMarket (market);
             }
-            const base = this.safeCurrencyCode (baseId);
-            const quote = this.safeCurrencyCode (quoteId);
-            const symbol = base + '/' + quote;
-            const active = undefined;
-            // const baseCurrency = this.safeValue (currenciesById, baseId);
-            // let amountPrecision = undefined;
-            // if (baseCurrency !== undefined) {
-            //     amountPrecision = this.safeInteger (baseCurrency, 'decimals', 8);
-            // }
-            // const precision = {
-            //     'price': this.safeInteger (market, 'pricePrecision'),
-            //     'amount': amountPrecision,
-            // };
-            result.push ({
-                'id': id,
-                'symbol': symbol,
-                'base': base,
-                'quote': quote,
-                'baseId': baseId,
-                'quoteId': quoteId,
-                'info': market,
-                'type': type,
-                'spot': spot,
-                'swap': swap,
-                'linear': linear,
-                'inverse': inverse,
-                'active': active,
-                'taker': taker,
-                'maker': maker,
-                'precision': precision,
-                'limits': limits,
-            });
+            result.push (market);
         }
         return result;
     }
