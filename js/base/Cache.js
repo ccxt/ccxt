e022967aa62a12c6e7a9e9dd9fc258af112d3163
Igor Kroitor <igor.kroitor@gmail.com>
2020-06-24 20:27:15 +0300

    Cache.js maxSize fix

--- js/base/Cache.js
+++ js/base/Cache.js
@@ -1,12 +1,9 @@
 'use strict';
 
 class ArrayCache extends Array {
-    constructor (maxSize) {
+    constructor (maxSize = undefined) {
         super ()
-        if (maxSize <= 0) {
-            throw new Error ('Cache size must be larger than 0')
-        }
-        Object.defineProperty(this, 'maxSize', {
+        Object.defineProperty (this, 'maxSize', {
             __proto__: null, // make it invisible
             value: maxSize,
             writable: true,
@@ -19,7 +16,8 @@ class ArrayCache extends Array {
 
     append (item) {
         super.push (item)
-        if (this.length > this.maxSize) {
+        // maxSize may be 0 when initialized by a .filter() copy-construction
+        if (this.maxSize && (this.length > this.maxSize)) {
             this.shift ()
         }
     }
