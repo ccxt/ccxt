584b2da40d3ef0a847d67f081fc17f66e26edc7a
Igor Kroitor <igor.kroitor@gmail.com>
2020-06-25 18:35:07 +0300

    huobijp.js

---
+++ js/huobijp.js
@@ -0,0 +1,45 @@
+'use strict';
+
+//  ---------------------------------------------------------------------------
+
+const huobipro = require ('./huobipro.js');
+const { NotSupported } = require ('ccxt/js/base/errors');
+
+// ---------------------------------------------------------------------------
+
+module.exports = class huobijp extends huobipro {
+    describe () {
+        return this.deepExtend (super.describe (), {
+            'id': 'huobijp',
+            'name': 'Huobi Japan',
+            'countries': [ 'JP' ],
+            'hostname': 'api-cloud.huobi.co.jp',
+            'pro': true,
+            'has': {
+                'fetchDepositAddress': false,
+            },
+            'urls': {
+                'logo': 'https://user-images.githubusercontent.com/1294454/85734211-85755480-b705-11ea-8b35-0b7f1db33a2f.jpg',
+                'api': {
+                    'ws': {
+                        'api': {
+                            'public': 'wss://{hostname}/ws',
+                            'private': 'wss://{hostname}/ws',
+                        },
+                    },
+                    'market': 'https://{hostname}/api',
+                    'public': 'https://{hostname}/api',
+                    'private': 'https://{hostname}/api',
+                },
+                'www': 'https://www.huobi.co.jp',
+                'referral': 'https://www.huobi.co.jp/register/?invite_code=znnq3',
+                'doc': 'https://api-doc.huobi.co.jp',
+                'fees': 'https://www.huobi.co.jp/support/fee',
+            },
+        });
+    }
+
+    async fetchDepositAddress (code, params = {}) {
+        throw new NotSupported (this.id + ' fetchDepositAddress not supported yet');
+    }
+};
