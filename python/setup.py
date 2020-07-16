21da13d6d8da21cac94b3068a335dfa13482035c
Igor Kroitor <igor.kroitor@gmail.com>
2020-07-16 04:42:27 +0300

    ccxt 1.31.35 coolcoin delisted

--- python/setup.py
+++ python/setup.py
@@ -77,7 +77,7 @@ setup(
         ':python_version>="3.5.2"': [
             'aiohttp>=3.0.1',
             'aiodns==1.1.1',
-            'ccxt>=1.31.32',
+            'ccxt>=1.31.35',
             'yarl==1.1.0',
         ],
         'qa': [
