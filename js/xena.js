e70a793af4584cae378dc3ad587f3fc92182be7f
Igor Kroitor <igor.kroitor@gmail.com>
2020-07-19 10:43:05 +0300

    xena certified

--- js/xena.js
+++ js/xena.js
@@ -10,6 +10,7 @@ module.exports = class xena extends Exchange {
             'name': 'Xena Exchange',
             'countries': [ 'VC', 'UK' ],
             'rateLimit': 100,
+            'certified': true,
             'has': {
                 'CORS': false,
                 'cancelAllOrders': true,
