f97a2bb9ae1ab18b50962331b07910e2b91156d6
Travis CI <travis@travis-ci.org>
2020-07-24 18:24:15 +0000

    1.31.93
    [ci skip]

--- python/ccxt/bleutrade.py
+++ python/ccxt/bleutrade.py
@@ -23,23 +23,29 @@ class bleutrade(Exchange):
             'rateLimit': 1000,
             'certified': False,
             'has': {
+                'cancelOrder': True,
                 'CORS': True,
-                'cancelOrder': False,  # todo
-                'createLimitOrder': False,  # todo
-                'createMarketOrder': False,  # todo
-                'createOrder': False,  # todo
-                'editOrder': False,  # todo
-                'withdraw': False,  # todo
-                'fetchTrades': False,
-                'fetchTickers': True,
-                'fetchTicker': True,
-                'fetchOrders': False,
+                'createLimitOrder': False,
+                'createMarketOrder': False,
+                'createOrder': True,
+                'editOrder': False,
+                'fetchBalance': True,
                 'fetchClosedOrders': True,
-                'fetchOpenOrders': True,
-                'fetchWithdrawals': True,
-                'fetchOrderTrades': False,
-                'fetchLedger': True,
+                'fetchCurrencies': True,
                 'fetchDepositAddress': True,
+                'fetchDeposits': True,
+                'fetchLedger': True,
+                'fetchMarkets': True,
+                'fetchOHLCV': True,
+                'fetchOpenOrders': True,
+                'fetchOrderBook': True,
+                'fetchOrders': False,
+                'fetchOrderTrades': False,
+                'fetchTicker': True,
+                'fetchTickers': True,
+                'fetchTrades': False,
+                'fetchWithdrawals': True,
+                'withdraw': False,
             },
             'timeframes': {
                 '1h': '1h',
