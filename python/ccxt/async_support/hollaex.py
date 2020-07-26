1b9a42578746829bd3c50ebbb44fbb7f8063e5c6
Travis CI <travis@travis-ci.org>
2020-07-26 12:49:52 +0000

    1.32.4
    [ci skip]

--- python/ccxt/async_support/hollaex.py
+++ python/ccxt/async_support/hollaex.py
@@ -463,7 +463,7 @@ class hollaex(Exchange):
         #         ]
         #     }
         #
-        trades = await self.safe_value(response, market['id'], [])
+        trades = self.safe_value(response, market['id'], [])
         return self.parse_trades(trades, market, since, limit)
 
     def parse_trade(self, trade, market=None):
