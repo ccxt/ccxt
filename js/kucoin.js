23c336ca63b446fe9bf48c28437147126ef19d08
Zach Holmes <shortkeys@gmail.com>
2020-07-27 17:30:10 -0400

    add optional partnerId/signature to authenticated kucoin requests

--- js/kucoin.js
+++ js/kucoin.js
@@ -1801,6 +1801,14 @@ module.exports = class kucoin extends Exchange {
             const payload = timestamp + method + endpoint + endpart;
             const signature = this.hmac (this.encode (payload), this.encode (this.secret), 'sha256', 'base64');
             headers['KC-API-SIGN'] = this.decode (signature);
+            const partnerId = this.options['partnerId'];
+            if (partnerId) {
+                const partnerSecret = this.options['partnerSecret'];
+                const partnerPayload = timestamp + partnerId + this.apiKey;
+                const partnerSignature = this.hmac (this.encode (partnerPayload), this.encode (partnerSecret), 'sha256', 'base64');
+                headers['KC-API-PARTNER-SIGN'] = this.decode (partnerSignature);
+                headers['KC-API-PARTNER'] = partnerId;
+            }
         }
         return { 'url': url, 'method': method, 'body': body, 'headers': headers };
     }
