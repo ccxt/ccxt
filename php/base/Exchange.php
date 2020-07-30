0b4aeceff0669247dac10bd6ce0a8174b9d59028
Travis CI <travis@travis-ci.org>
2020-07-30 12:27:10 +0000

    1.32.29
    [ci skip]

--- php/base/Exchange.php
+++ php/base/Exchange.php
@@ -36,7 +36,7 @@ use Elliptic\EC;
 use Elliptic\EdDSA;
 use BN\BN;
 
-$version = '1.32.28';
+$version = '1.32.29';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -55,7 +55,7 @@ const PAD_WITH_ZERO = 1;
 
 class Exchange {
 
-    const VERSION = '1.32.28';
+    const VERSION = '1.32.29';
 
     private static $base58_alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
     private static $base58_encoder = null;
