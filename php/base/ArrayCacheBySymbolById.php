9257e2ab83ba3d8eaf69d5ed534fe506528cd548
Igor Kroitor <igor.kroitor@gmail.com>
2020-08-03 09:45:34 +0300

    ArrayCacheBySymbolById.php edits

--- php/base/ArrayCacheBySymbolById.php
+++ php/base/ArrayCacheBySymbolById.php
@@ -4,20 +4,20 @@ namespace ccxtpro;
 
 class ArrayCacheBySymbolById extends ArrayCache {
 
+    public function match($a, $b) {
+        return ($a['id'] === $b['id']) && ($a['symbol'] === $b['symbol']);
+    }
+
     public function append($item) {
         $first_match_index = null;
         for ($k = 0; $k < $this->deque->count(); $k++) {
-            $stored = $this->deque[$k];
-            if (($stored['id'] === $item['id']) && ($stored['symbol'] === $item['symbol'])) {
+            if ($this->match($this->deque[$k], $item)) {
                 $first_match_index = $k;
                 break;
             }
         }
         if (is_null($first_match_index)) {
-            $this->deque->push($item);
-            if ($this->max_size && ($this->deque->count() > $this->max_size)) {
-                $this->deque->shift();
-            }
+            parent::append($item);
         } else {
             $this->deque->set($first_match_index, $item);
         }
