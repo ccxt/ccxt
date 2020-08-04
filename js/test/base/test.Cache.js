a874e86564ae0d2401539f6754a902c362078327
Igor Kroitor <igor.kroitor@gmail.com>
2020-08-04 03:46:02 +0300

    test.Cache.js

--- js/test/base/test.Cache.js
+++ js/test/base/test.Cache.js
@@ -88,3 +88,19 @@ assert (equals (cache, [
     { 'symbol': 'BTC/USDT', 'id': '9', 'i': 9 },
     { 'symbol': 'BTC/USDT', 'id': '10', 'i': 10 },
 ]));
+
+for (let i = 1; i <= 10; i++) {
+    cache.append ({
+        'symbol': 'BTC/USDT',
+        'id': i.toString (),
+        'i': i + 10,
+    });
+}
+
+assert (equals (cache, [
+    { 'symbol': 'BTC/USDT', 'id': '6', 'i': 16 },
+    { 'symbol': 'BTC/USDT', 'id': '7', 'i': 17 },
+    { 'symbol': 'BTC/USDT', 'id': '8', 'i': 18 },
+    { 'symbol': 'BTC/USDT', 'id': '9', 'i': 19 },
+    { 'symbol': 'BTC/USDT', 'id': '10', 'i': 20 },
+]));
