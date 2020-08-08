c0e33d67db9483e77c464ca8ed11871237dd8f83
Carlo Revelli <carlo.revelli@berkeley.edu>
2020-08-09 00:14:02 +0200

    save

--- python/test/exchange/test_ws_client.py
+++ python/test/exchange/test_ws_client.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-import ccxtpro
+import ccxt
+from ccxtpro.base.exchange import Exchange
 import asyncio
 import subprocess
 import datetime
@@ -15,12 +16,12 @@ subprocess.Popen(['node', './js/test/base/fastMessageServer.js'])
 async def main():
     output = []
     await asyncio.sleep(1)  # let the ws server start up
-    ws = ccxtpro.Exchange({'verbose': 0, 'id': 'id', 'handle_message': resolver})
+    ws = Exchange({'verbose': 0, 'id': 'id', 'handle_message': resolver})
     while True:
         try:
             output.append(await ws.watch('ws://localhost:8080', 'lol', None, None, None))
             # time.sleep(0.02)  # represent some processing on the user end
-        except ccxtpro.NetworkError as e:
+        except ccxt.NetworkError as e:
             print(datetime.datetime.now().isoformat(timespec='milliseconds') + 'Z', 'python error', type(e), e)
             output.append(e)
             if 'Connect call' in str(e):
