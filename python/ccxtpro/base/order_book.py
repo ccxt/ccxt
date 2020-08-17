eb5c9fe0d7bd292f354daa0fb850221d82cdb1d4
Carlo Revelli <carlo.revelli@berkeley.edu>
2020-08-17 16:14:49 +0200

    undo commit

--- python/ccxtpro/base/order_book.py
+++ python/ccxtpro/base/order_book.py
@@ -25,7 +25,7 @@ class OrderBook(dict):
         # merge to self
         super(OrderBook, self).__init__(defaults)
 
-    def limit(self, n=None):
+    def limit(self, n=float('inf')):
         self['asks'].limit(n)
         self['bids'].limit(n)
         return self
