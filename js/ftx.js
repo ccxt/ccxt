314b22a619fe57448b3a5f4bcf0dad7a4efa54d2
Igor Kroitor <igor.kroitor@gmail.com>
2020-08-17 08:08:09 +0300

    ftx parseTransaction fixup #7473

--- js/ftx.js
+++ js/ftx.js
@@ -1410,7 +1410,7 @@ module.exports = class ftx extends Exchange {
         const address = this.safeString (transaction, 'address');
         const tag = this.safeString (transaction, 'tag');
         const fee = this.safeFloat (transaction, 'fee');
-        const type = ('destinationName' in transaction) ? 'deposit' : 'withdrawal';
+        const type = ('destinationName' in transaction) ? 'withdrawal' : 'deposit';
         return {
             'info': transaction,
             'id': id,
