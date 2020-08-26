ccd5c7175294cb9943e218684dab7d0b9230dddc
Igor Kroitor <igor.kroitor@gmail.com>
2020-08-26 21:32:54 +0300

    coinmate parseOrder remaining amount fix #7514

--- js/coinmate.js
+++ js/coinmate.js
@@ -626,8 +626,11 @@ module.exports = class coinmate extends Exchange {
         const timestamp = this.safeInteger (order, 'timestamp');
         const side = this.safeStringLower (order, 'type');
         const price = this.safeFloat (order, 'price');
-        const amount = this.safeFloat2 (order, 'originalAmount', 'amount');
-        const remaining = this.safeFloat (order, 'remainingAmount', amount);
+        let amount = this.safeFloat (order, 'originalAmount');
+        const remaining = this.safeFloat (order, 'remainingAmount');
+        if (remaining === undefined) {
+            amount = this.safeFloat (order, 'amount');
+        }
         let status = this.parseOrderStatus (this.safeString (order, 'status'));
         const type = this.parseOrderType (this.safeString (order, 'orderTradeType'));
         let filled = undefined;
