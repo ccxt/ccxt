caa5a08fdeb505a23492a9c3d928827d050d4544
Travis CI <travis@travis-ci.org>
2020-08-31 14:26:59 +0000

    1.33.73
    [ci skip]

--- python/ccxt/bitpanda.py
+++ python/ccxt/bitpanda.py
@@ -516,7 +516,7 @@ class bitpanda(Exchange):
         baseVolume = self.safe_float(ticker, 'base_volume')
         quoteVolume = self.safe_float(ticker, 'quote_volume')
         vwap = None
-        if (quoteVolume is not None) and (baseVolume is not None) and (baseVolume != 0):
+        if (quoteVolume is not None) and (baseVolume is not None) and (baseVolume > 0):
             vwap = quoteVolume / baseVolume
         return {
             'symbol': symbol,
