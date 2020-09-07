4d40e6351d32ad52f0c2342ec7bcdd1d8cf0d842
Igor Kroitor <igor.kroitor@gmail.com>
2020-09-07 07:56:18 +0300

    test.ohlcvs.js string concat

--- js/test/Exchange/test.ohlcv.js
+++ js/test/Exchange/test.ohlcv.js
@@ -18,9 +18,9 @@ function testOHLCV (exchange, ohlcv, symbol, now) {
     assert (ohlcv[0] > 1230940800000); // 03 Jan 2009 - first block
     assert (ohlcv[0] < 2147483648000); // 19 Jan 2038 - int32 overflows
 
-    assert ((ohlcv[1] === undefined) || (ohlcv[2] === undefined) || (ohlcv[1] <= ohlcv[2]), 'open > high, ' + ohlcv[1] + ' > ' + ohlcv[2]); // open <= high
-    assert ((ohlcv[3] === undefined) || (ohlcv[2] === undefined) || (ohlcv[3] <= ohlcv[2]), 'low > high, ' + ohlcv[3] + ' > ' + ohlcv[2]); // low <= high
-    assert ((ohlcv[3] === undefined) || (ohlcv[4] === undefined) || (ohlcv[3] <= ohlcv[4]), 'low > close, ' + ohlcv[3] + ' > ' + ohlcv[4]); // low <= close
+    assert ((ohlcv[1] === undefined) || (ohlcv[2] === undefined) || (ohlcv[1] <= ohlcv[2]), 'open > high, ' + ohlcv[1].toString () + ' > ' + ohlcv[2].toString ()); // open <= high
+    assert ((ohlcv[3] === undefined) || (ohlcv[2] === undefined) || (ohlcv[3] <= ohlcv[2]), 'low > high, ' + ohlcv[3].toString () + ' > ' + ohlcv[2].toString ()); // low <= high
+    assert ((ohlcv[3] === undefined) || (ohlcv[4] === undefined) || (ohlcv[3] <= ohlcv[4]), 'low > close, ' + ohlcv[3].toString () + ' > ' + ohlcv[4].toString ()); // low <= close
 }
 
 module.exports = testOHLCV;
