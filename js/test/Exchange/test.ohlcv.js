e46afc6a5e50b07c05fc0141997d9100a91164ff
Carlo Revelli <carlo.revelli@berkeley.edu>
2020-09-11 23:01:51 +0200

    more verbose test output

--- js/test/Exchange/test.ohlcv.js
+++ js/test/Exchange/test.ohlcv.js
@@ -7,16 +7,17 @@ const assert = require ('assert');
 //  ---------------------------------------------------------------------------
 
 function testOHLCV (exchange, ohlcv, symbol, now) {
+    const json = exchange.json (ohlcv);
     assert (ohlcv);
-    assert (Array.isArray (ohlcv));
+    assert (Array.isArray (ohlcv), json);
     const length = ohlcv.length;
     assert (length >= 6);
     for (let i = 0; i < ohlcv.length; i++) {
-        assert ((ohlcv[i] === undefined) || (typeof ohlcv[i] === 'number'));
+        assert ((ohlcv[i] === undefined) || (typeof ohlcv[i] === 'number'), json);
     }
 
-    assert (ohlcv[0] > 1230940800000); // 03 Jan 2009 - first block
-    assert (ohlcv[0] < 2147483648000); // 19 Jan 2038 - int32 overflows
+    assert (ohlcv[0] > 1230940800000, json); // 03 Jan 2009 - first block
+    assert (ohlcv[0] < 2147483648000, json); // 19 Jan 2038 - int32 overflows
 
     assert ((ohlcv[1] === undefined) || (ohlcv[2] === undefined) || (ohlcv[1] <= ohlcv[2]), 'open > high! ' + ohlcv[1] + ' > ' + ohlcv[2]); // open <= high
     assert ((ohlcv[3] === undefined) || (ohlcv[2] === undefined) || (ohlcv[3] <= ohlcv[2]), 'low > high! ' + ohlcv[3] + ' > ' + ohlcv[2]); // low <= high
