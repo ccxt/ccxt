b981fb313c35a3d258f365337304332548cb8bbc
Travis CI <travis@travis-ci.org>
2020-10-09 18:08:58 +0000

    1.35.79
    [ci skip]

--- python/ccxt/async_support/yobit.py
+++ python/ccxt/async_support/yobit.py
@@ -40,7 +40,6 @@ class yobit(Exchange):
                 'createMarketOrder': False,
                 'createOrder': True,
                 'fetchBalance': True,
-                'fetchClosedOrders': 'emulated',
                 'fetchDepositAddress': True,
                 'fetchDeposits': False,
                 'fetchMarkets': True,
@@ -49,7 +48,6 @@ class yobit(Exchange):
                 'fetchOrder': True,
                 'fetchOrderBook': True,
                 'fetchOrderBooks': True,
-                'fetchOrders': 'emulated',
                 'fetchTicker': True,
                 'fetchTickers': True,
                 'fetchTrades': True,
@@ -506,7 +504,7 @@ class yobit(Exchange):
             filled = self.safe_float(response['return'], 'received', 0.0)
             remaining = self.safe_float(response['return'], 'remains', amount)
         timestamp = self.milliseconds()
-        order = {
+        return {
             'id': id,
             'timestamp': timestamp,
             'datetime': self.iso8601(timestamp),
@@ -527,18 +525,13 @@ class yobit(Exchange):
             'average': None,
             'trades': None,
         }
-        self.orders[id] = order
-        return order
 
     async def cancel_order(self, id, symbol=None, params={}):
         await self.load_markets()
         request = {
             'order_id': int(id),
         }
-        response = await self.privatePostCancelOrder(self.extend(request, params))
-        if id in self.orders:
-            self.orders[id]['status'] = 'canceled'
-        return response
+        return await self.privatePostCancelOrder(self.extend(request, params))
 
     def parse_order_status(self, status):
         statuses = {
@@ -556,18 +549,13 @@ class yobit(Exchange):
         marketId = self.safe_string(order, 'pair')
         symbol = self.safe_symbol(marketId, market)
         remaining = self.safe_float(order, 'amount')
-        amount = None
+        amount = self.safe_float(order, 'start_amount')
         price = self.safe_float(order, 'rate')
         filled = None
         cost = None
-        if 'start_amount' in order:
-            amount = self.safe_float(order, 'start_amount')
-        else:
-            if id in self.orders:
-                amount = self.orders[id]['amount']
         if amount is not None:
             if remaining is not None:
-                filled = amount - remaining
+                filled = max(0, amount - remaining)
                 cost = price * filled
         fee = None
         type = 'limit'
@@ -613,48 +601,12 @@ class yobit(Exchange):
         }
         response = await self.privatePostOrderInfo(self.extend(request, params))
         id = str(id)
-        newOrder = self.parse_order(self.extend({'id': id}, response['return'][id]))
-        oldOrder = self.orders[id] if (id in self.orders) else {}
-        self.orders[id] = self.extend(oldOrder, newOrder)
-        return self.orders[id]
+        orders = self.safe_value(response, 'return', {})
+        return self.parse_order(self.extend({'id': id}, orders[id]))
 
-    def update_cached_orders(self, openOrders, symbol):
-        # update local cache with open orders
-        # self will add unseen orders and overwrite existing ones
-        for j in range(0, len(openOrders)):
-            id = openOrders[j]['id']
-            self.orders[id] = openOrders[j]
-        openOrdersIndexedById = self.index_by(openOrders, 'id')
-        cachedOrderIds = list(self.orders.keys())
-        for k in range(0, len(cachedOrderIds)):
-            # match each cached order to an order in the open orders array
-            # possible reasons why a cached order may be missing in the open orders array:
-            # - order was closed or canceled -> update cache
-            # - symbol mismatch(e.g. cached BTC/USDT, fetched ETH/USDT) -> skip
-            cachedOrderId = cachedOrderIds[k]
-            cachedOrder = self.orders[cachedOrderId]
-            if not (cachedOrderId in openOrdersIndexedById):
-                # cached order is not in open orders array
-                # if we fetched orders by symbol and it doesn't match the cached order -> won't update the cached order
-                if symbol is not None and symbol != cachedOrder['symbol']:
-                    continue
-                # cached order is absent from the list of open orders -> mark the cached order as closed
-                if cachedOrder['status'] == 'open':
-                    cachedOrder = self.extend(cachedOrder, {
-                        'status': 'closed',  # likewise it might have been canceled externally(unnoticed by "us")
-                        'cost': None,
-                        'filled': cachedOrder['amount'],
-                        'remaining': 0.0,
-                    })
-                    if cachedOrder['cost'] is None:
-                        if cachedOrder['filled'] is not None:
-                            cachedOrder['cost'] = cachedOrder['filled'] * cachedOrder['price']
-                    self.orders[cachedOrderId] = cachedOrder
-        return self.to_array(self.orders)
-
-    async def fetch_orders(self, symbol=None, since=None, limit=None, params={}):
+    async def fetch_open_orders(self, symbol=None, since=None, limit=None, params={}):
         if symbol is None:
-            raise ArgumentsRequired(self.id + ' fetchOrders requires a symbol argument')
+            raise ArgumentsRequired(self.id + ' fetchOpenOrders() requires a symbol argument')
         await self.load_markets()
         request = {}
         market = None
@@ -662,21 +614,8 @@ class yobit(Exchange):
             market = self.market(symbol)
             request['pair'] = market['id']
         response = await self.privatePostActiveOrders(self.extend(request, params))
-        # can only return 'open' orders(i.e. no way to fetch 'closed' orders)
-        openOrders = []
-        if 'return' in response:
-            openOrders = self.parse_orders(response['return'], market)
-        allOrders = self.update_cached_orders(openOrders, symbol)
-        result = self.filter_by_symbol(allOrders, symbol)
-        return self.filter_by_since_limit(result, since, limit)
-
-    async def fetch_open_orders(self, symbol=None, since=None, limit=None, params={}):
-        orders = await self.fetch_orders(symbol, since, limit, params)
-        return self.filter_by(orders, 'status', 'open')
-
-    async def fetch_closed_orders(self, symbol=None, since=None, limit=None, params={}):
-        orders = await self.fetch_orders(symbol, since, limit, params)
-        return self.filter_by(orders, 'status', 'closed')
+        orders = self.safe_value(response, 'return', [])
+        return self.parse_orders(orders, market, since, limit)
 
     async def fetch_my_trades(self, symbol=None, since=None, limit=None, params={}):
         if symbol is None:
