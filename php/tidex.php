46e679f1f37d219d62fdc5a839b6f762af3049ce
Travis CI <travis@travis-ci.org>
2020-10-11 00:11:36 +0000

    1.35.97
    [ci skip]

--- php/tidex.php
+++ php/tidex.php
@@ -620,21 +620,6 @@ class tidex extends Exchange {
         );
     }
 
-    public function parse_orders($orders, $market = null, $since = null, $limit = null, $params = array ()) {
-        $result = array();
-        $ids = is_array($orders) ? array_keys($orders) : array();
-        $symbol = null;
-        if ($market !== null) {
-            $symbol = $market['symbol'];
-        }
-        for ($i = 0; $i < count($ids); $i++) {
-            $id = $ids[$i];
-            $order = array_merge(array( 'id' => $id ), $orders[$id]);
-            $result[] = array_merge($this->parse_order($order, $market), $params);
-        }
-        return $this->filter_by_symbol_since_limit($result, $symbol, $since, $limit);
-    }
-
     public function fetch_order($id, $symbol = null, $params = array ()) {
         $this->load_markets();
         $request = array(
