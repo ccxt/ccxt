5d5d003c62471831bf6cf50574b0307e8c1170a8
Igor Kroitor <igor.kroitor@gmail.com>
2020-10-12 22:24:10 +0300

    binance ws defaultType delivery coin-margined futures

--- js/binance.js
+++ js/binance.js
@@ -27,6 +27,7 @@ module.exports = class binance extends ccxt.binance {
                         'spot': 'wss://testnet.binance.vision/ws',
                         'margin': 'wss://testnet.binance.vision/ws',
                         'future': 'wss://stream.binancefuture.com/ws',
+                        'delivery': 'wss://dstream.binancefuture.com/ws',
                     },
                 },
                 'api': {
@@ -34,6 +35,7 @@ module.exports = class binance extends ccxt.binance {
                         'spot': 'wss://stream.binance.com:9443/ws',
                         'margin': 'wss://stream.binance.com:9443/ws',
                         'future': 'wss://fstream.binance.com/ws',
+                        'delivery': 'wss://dstream.binance.com/ws',
                     },
                 },
             },
