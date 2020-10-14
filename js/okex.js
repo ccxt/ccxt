c304fbf3c268cb107cdf0f264f8a794d48c7642d
akatyal <45573007+akatyal@users.noreply.github.com>
2020-10-14 12:52:32 +0530

    Update okex.js
    Issue with None Value in clientOrderId in case it is not there

--- js/okex.js
+++ js/okex.js
@@ -2148,7 +2148,7 @@ module.exports = class okex extends Exchange {
                 'currency': feeCurrency,
             };
         }
-        let clientOrderId = this.safeString (order, 'client_oid');
+        let clientOrderId = this.safeString (order, 'client_oid', '');
         if (clientOrderId.length < 1) {
             clientOrderId = undefined; // fix empty clientOrderId string
         }
