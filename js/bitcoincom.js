a40f9ed3b04ad0b97b51f400c80749384d8584a8
Igor Kroitor <igor.kroitor@gmail.com>
2020-10-27 14:34:34 +0300

    bitcoincom logo

--- js/bitcoincom.js
+++ js/bitcoincom.js
@@ -13,7 +13,7 @@ module.exports = class bitcoincom extends hitbtc {
             'name': 'bitcoin.com',
             'countries': [ 'KN' ],
             'urls': {
-                'logo': '',
+                'logo': 'https://user-images.githubusercontent.com/1294454/97296144-514fa300-1861-11eb-952b-3d55d492200b.jpg',
                 'api': {
                     'public': 'https://api.exchange.bitcoin.com',
                     'private': 'https://api.exchange.bitcoin.com',
@@ -21,6 +21,7 @@ module.exports = class bitcoincom extends hitbtc {
                 'www': 'https://exchange.bitcoin.com',
                 'doc': 'https://api.exchange.bitcoin.com/api/2/explore',
                 'fees': 'https://exchange.bitcoin.com/fees-and-limits',
+                'referral': 'https://exchange.bitcoin.com/referral/da948b21d6c92d69',
             },
             'fees': {
                 'trading': {
