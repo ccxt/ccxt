c3424f1814be41ee322ea2d5a01a5ba2d8c396c9
Igor Kroitor <igor.kroitor@gmail.com>
2020-10-31 20:22:50 +0300

    examples/py/binance-futures-positions.py close #7900

--- examples/py/binance-futures-positions.py
+++ examples/py/binance-futures-positions.py
@@ -24,17 +24,6 @@ exchange.set_sandbox_mode(True)  # comment if you're not using the testnet
 markets = exchange.load_markets()
 exchange.verbose = True  # debug output
 
-def table(values):
-    first = values[0]
-    keys = list(first.keys()) if isinstance(first, dict) else range(0, len(first))
-    widths = [max([len(str(v[k])) for v in values]) for k in keys]
-    string = ' | '.join(['{:<' + str(w) + '}' for w in widths])
-    return "\n".join([string.format(*[str(v[k]) for k in keys]) for v in values])
-
-
 balance = exchange.fetch_balance()
-
-
 positions = balance['info']['positions']
 pprint(positions)
-print(table(positions))
