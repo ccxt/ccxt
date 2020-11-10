bc0fdd8347ac2c5fb3ba68c8fc4fd30e5bd32ae8
Travis CI <travis@travis-ci.org>
2020-11-10 05:06:03 +0000

    1.37.44
    [ci skip]

--- python/ccxt/async_support/bybit.py
+++ python/ccxt/async_support/bybit.py
@@ -154,11 +154,11 @@ class bybit(Exchange):
                     'post': [
                         'order/create',
                         'order/cancel',
-                        'order/cancelAll',
+                        'order/cancel-all',
                         'order/replace',
                         'stop-order/create',
                         'stop-order/cancel',
-                        'stop-order/cancelAll',
+                        'stop-order/cancel-all',
                         'stop-order/replace',
                         'position/switch-isolated',
                         'position/set-auto-add-margin',
