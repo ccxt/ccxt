6a0b83214096af761601f16b754f2c51c0155943
Igor Kroitor <igor.kroitor@gmail.com>
2020-11-14 17:21:57 +0300

    delta initial commit

---
+++ js/delta.js
@@ -0,0 +1,126 @@
+'use strict';
+
+//  ---------------------------------------------------------------------------
+
+const Exchange = require ('./base/Exchange');
+const { ExchangeError } = require ('./base/errors');
+
+//  ---------------------------------------------------------------------------
+
+module.exports = class delta extends Exchange {
+    describe () {
+        return this.deepExtend (super.describe (), {
+            'id': 'delta',
+            'name': 'Delta Exchange',
+            'countries': [ 'VC' ], // Saint Vincent and the Grenadines
+            'rateLimit': 300,
+            'version': 'v2',
+            // new metainfo interface
+            'has': {
+            },
+            'timeframes': {
+            },
+            'urls': {
+                'logo': 'https://user-images.githubusercontent.com/51840849/87591171-9a377d80-c6f0-11ea-94ac-97a126eac3bc.jpg',
+                'test': {
+                    'public': 'https://testnet-api.delta.exchange',
+                    'private': 'https://testnet-api.delta.exchange',
+                },
+                'api': {
+                    'public': 'https://api.delta.exchange',
+                    'private': 'https://api.delta.exchange',
+                },
+                'www': 'https://www.delta.exchange',
+                'doc': [
+                    'https://docs.delta.exchange',
+                ],
+                'fees': 'https://www.delta.exchange/fees',
+            },
+            'api': {
+                'public': {
+                    'get': [
+                        'assets',
+                        'indices',
+                        'products',
+                        'tickers',
+                        'tickers/{symbol}',
+                        'l2orderbook/{symbol}',
+                        'trades/{symbol}',
+                        'history/candles',
+                        'history/sparklines',
+                    ],
+                },
+                'private': {
+                    'get': [
+                        'orders',
+                        'orders/leverage',
+                        'positions',
+                        'positions/margined',
+                        'orders/history',
+                        'fills',
+                        'fills/history/download/csv',
+                        'wallet/balances',
+                        'wallet/transactions',
+                        'wallet/transactions/download',
+                    ],
+                    'post': [
+                        'orders',
+                        'orders/batch',
+                        'orders/leverage',
+                        'positions/change_margin',
+                    ],
+                    'put': [
+                        'orders',
+                        'orders/batch',
+                    ],
+                    'delete': [
+                        'orders',
+                        'orders/all',
+                        'orders/batch',
+                    ],
+                },
+            },
+            'fees': {
+                'trading': {
+                    'tierBased': true,
+                    'percentage': true,
+                    'taker': 0.15 / 100,
+                    'maker': 0.10 / 100,
+                    'tiers': [
+                        // volume in BTC
+                        {
+                            'taker': [
+                                [0, 0.15 / 100],
+                                [100, 0.13 / 100],
+                                [250, 0.13 / 100],
+                                [1000, 0.1 / 100],
+                                [5000, 0.09 / 100],
+                                [10000, 0.075 / 100],
+                                [20000, 0.065 / 100],
+                            ],
+                            'maker': [
+                                [0, 0.1 / 100],
+                                [100, 0.1 / 100],
+                                [250, 0.09 / 100],
+                                [1000, 0.075 / 100],
+                                [5000, 0.06 / 100],
+                                [10000, 0.05 / 100],
+                                [20000, 0.05 / 100],
+                            ],
+                        },
+                    ],
+                },
+            },
+            'requiredCredentials': {
+                'apiKey': true,
+                'secret': false,
+            },
+            'exceptions': {
+                'exact': {
+                },
+                'broad': {
+                },
+            },
+        });
+    }
+};
