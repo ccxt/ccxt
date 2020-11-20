8b0691f5bde8f20e95a72758e9f22fb46b3098aa
Igor Kroitor <igor.kroitor@gmail.com>
2020-11-20 03:16:57 +0300

    therock timeInForce

--- js/therock.js
+++ js/therock.js
@@ -963,6 +963,7 @@ module.exports = class therock extends Exchange {
             'status': status,
             'symbol': symbol,
             'type': type,
+            'timeInForce': undefined,
             'side': side,
             'price': price,
             'cost': cost,
