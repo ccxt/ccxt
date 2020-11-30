0440bdeb2a7eed32465ef6eaeccc9bb2574424ea
Igor Kroitor <igor.kroitor@gmail.com>
2020-11-30 13:37:00 +0300

    hitbtc stopPrice

--- js/hitbtc.js
+++ js/hitbtc.js
@@ -888,6 +888,7 @@ module.exports = class hitbtc extends Exchange {
             'timeInForce': timeInForce,
             'side': side,
             'price': price,
+            'stopPrice': undefined,
             'average': average,
             'amount': amount,
             'cost': cost,
