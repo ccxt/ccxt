0d807fda5cf9dd45464c2cdf6ff570379325f173
Travis CI <travis@travis-ci.org>
2020-11-30 05:57:52 +0000

    1.38.56
    [ci skip]

--- python/ccxt/hitbtc.py
+++ python/ccxt/hitbtc.py
@@ -162,6 +162,7 @@ class hitbtc(Exchange):
                 'defaultTimeInForce': 'FOK',
             },
             'commonCurrencies': {
+                'BCC': 'BCC',  # initial symbol for Bitcoin Cash, now inactive
                 'BET': 'DAO.Casino',
                 'BOX': 'BOX Token',
                 'CPT': 'Cryptaur',  # conflict with CPT = Contents Protocol https://github.com/ccxt/ccxt/issues/4920 and https://github.com/ccxt/ccxt/issues/6081
