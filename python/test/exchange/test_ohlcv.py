aab3320c58ce693b70f1743fa6aeeb8894a98d57
Igor Kroitor <igor.kroitor@gmail.com>
2020-12-07 23:47:16 +0300

    python/test/exchange/test_ohlcv.py int/float numbers

--- python/test/exchange/test_ohlcv.py
+++ python/test/exchange/test_ohlcv.py
@@ -13,11 +13,11 @@ def test_ohlcv(exchange, ohlcv, method, symbol):
     assert ohlcv[0] > 1230940800000  # 03 Jan 2009 - first block
     assert ohlcv[0] < 2147483648000  # 19 Jan 2038 - int32 overflows
 
-    assert (ohlcv[1] is None) or isinstance(ohlcv[1], float)  # open
-    assert (ohlcv[2] is None) or isinstance(ohlcv[2], float)  # high
-    assert (ohlcv[3] is None) or isinstance(ohlcv[3], float)  # low
-    assert (ohlcv[4] is None) or isinstance(ohlcv[4], float)  # close
-    assert (ohlcv[5] is None) or isinstance(ohlcv[5], float)  # volume
+    assert (ohlcv[1] is None) or isinstance(ohlcv[1], (int, float))  # open
+    assert (ohlcv[2] is None) or isinstance(ohlcv[2], (int, float))  # high
+    assert (ohlcv[3] is None) or isinstance(ohlcv[3], (int, float))  # low
+    assert (ohlcv[4] is None) or isinstance(ohlcv[4], (int, float))  # close
+    assert (ohlcv[5] is None) or isinstance(ohlcv[5], (int, float))  # volume
 
     assert (ohlcv[1] is None) or (ohlcv[2] is None) or (ohlcv[1] <= ohlcv[2]), \
         'open (' + str(ohlcv[1]) + ') > high (' + str(ohlcv[2]) + ')'
