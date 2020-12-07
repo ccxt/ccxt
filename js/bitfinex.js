eb325f8e32389cfb9ec55a833bcbc99a9640269f
Igor Kroitor <igor.kroitor@gmail.com>
2020-12-08 00:47:03 +0300

    bitfinex v1 vs v2 symbol conflict

--- js/bitfinex.js
+++ js/bitfinex.js
@@ -157,8 +157,11 @@ module.exports = class bitfinex extends ccxt.bitfinex {
         }
         const seq = this.safeString (trade, 2);
         const parts = seq.split ('-');
-        const marketId = this.safeString (parts, 1);
-        const symbol = this.safeSymbol (marketId);
+        let marketId = this.safeString (parts, 1);
+        if (marketId !== undefined) {
+            marketId = marketId.replace ('t', '');
+        }
+        const symbol = this.safeSymbol (marketId, market);
         const takerOrMaker = undefined;
         const orderId = undefined;
         return {
