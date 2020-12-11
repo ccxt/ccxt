409e7cb92ca776c1a509b79c857b5b89ef9c8aef
Igor Kroitor <igor.kroitor@gmail.com>
2020-12-11 06:25:41 +0300

    bithumb bitmart postOnly

--- js/bithumb.js
+++ js/bithumb.js
@@ -708,6 +708,7 @@ module.exports = class bithumb extends Exchange {
             'symbol': symbol,
             'type': type,
             'timeInForce': undefined,
+            'postOnly': undefined,
             'side': side,
             'price': price,
             'stopPrice': undefined,
