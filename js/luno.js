cfdaae974632eb6b6e07826fe64552dea75da477
Igor Kroitor <igor.kroitor@gmail.com>
2020-12-12 09:19:18 +0300

    lykke luno livecoin postOnly

--- js/luno.js
+++ js/luno.js
@@ -254,6 +254,7 @@ module.exports = class luno extends Exchange {
             'symbol': symbol,
             'type': undefined,
             'timeInForce': undefined,
+            'postOnly': undefined,
             'side': side,
             'price': price,
             'stopPrice': undefined,
