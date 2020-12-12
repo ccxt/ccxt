d4bd024951520c6d09e45b6560e8aa9b81fda1dc
Igor Kroitor <igor.kroitor@gmail.com>
2020-12-12 03:21:31 +0300

    bleutrade braziliex btcalpha postOnly

--- js/braziliex.js
+++ js/braziliex.js
@@ -460,6 +460,7 @@ module.exports = class braziliex extends Exchange {
             'symbol': symbol,
             'type': 'limit',
             'timeInForce': undefined,
+            'postOnly': undefined,
             'side': side,
             'price': price,
             'stopPrice': undefined,
