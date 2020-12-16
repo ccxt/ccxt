7912afd84fe6a7499d565cf992bb9299194040bb
Igor Kroitor <igor.kroitor@gmail.com>
2020-12-16 09:51:40 +0300

    coinbasepro fetchOrders, fetchClosedOrders deduplication

--- js/coinbasepro.js
+++ js/coinbasepro.js
@@ -757,20 +757,10 @@ module.exports = class coinbasepro extends Exchange {
     }
 
     async fetchOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
-        await this.loadMarkets ();
         const request = {
             'status': 'all',
         };
-        let market = undefined;
-        if (symbol !== undefined) {
-            market = this.market (symbol);
-            request['product_id'] = market['id'];
-        }
-        if (limit !== undefined) {
-            request['limit'] = limit; // default 100
-        }
-        const response = await this.privateGetOrders (this.extend (request, params));
-        return this.parseOrders (response, market, since, limit);
+        return await this.fetchOpenOrders (symbol, since, limit, this.extend (request, params));
     }
 
     async fetchOpenOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
@@ -789,20 +779,10 @@ module.exports = class coinbasepro extends Exchange {
     }
 
     async fetchClosedOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
-        await this.loadMarkets ();
         const request = {
             'status': 'done',
         };
-        let market = undefined;
-        if (symbol !== undefined) {
-            market = this.market (symbol);
-            request['product_id'] = market['id'];
-        }
-        if (limit !== undefined) {
-            request['limit'] = limit; // default 100
-        }
-        const response = await this.privateGetOrders (this.extend (request, params));
-        return this.parseOrders (response, market, since, limit);
+        return await this.fetchOpenOrders (symbol, since, limit, this.extend (request, params));
     }
 
     async createOrder (symbol, type, side, amount, price = undefined, params = {}) {
