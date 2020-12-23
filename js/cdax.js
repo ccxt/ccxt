95fc76331d7d816042dcdcb5e922a83cc72923a2
Igor Kroitor <igor.kroitor@gmail.com>
2020-12-24 02:33:29 +0300

    cdax fixes

--- js/cdax.js
+++ js/cdax.js
@@ -16,6 +16,13 @@ module.exports = class cdax extends huobipro {
             'pro': false,
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/102157692-fd406280-3e90-11eb-8d46-4511b617cd17.jpg',
+                'api': {
+                    'market': 'https://{hostname}/api',
+                    'public': 'https://{hostname}/api',
+                    'private': 'https://{hostname}/api',
+                    'v2Public': 'https://{hostname}/api',
+                    'v2Private': 'https://{hostname}/api',
+                },
                 'www': 'https://cdax.io',
                 'referral': 'https://cdax.io/invite?invite_code=esc74',
                 'doc': 'https://github.com/cloudapidoc/API_Docs',
