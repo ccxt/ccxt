af169a95366fb53412f8afebd5d3df6325a3151a
Travis CI <travis@travis-ci.org>
2020-12-23 23:46:33 +0000

    1.39.60
    [ci skip]

--- php/cdax.php
+++ php/cdax.php
@@ -18,6 +18,13 @@ class cdax extends huobipro {
             'pro' => false,
             'urls' => array(
                 'logo' => 'https://user-images.githubusercontent.com/1294454/102157692-fd406280-3e90-11eb-8d46-4511b617cd17.jpg',
+                'api' => array(
+                    'market' => 'https://{hostname}/api',
+                    'public' => 'https://{hostname}/api',
+                    'private' => 'https://{hostname}/api',
+                    'v2Public' => 'https://{hostname}/api',
+                    'v2Private' => 'https://{hostname}/api',
+                ),
                 'www' => 'https://cdax.io',
                 'referral' => 'https://cdax.io/invite?invite_code=esc74',
                 'doc' => 'https://github.com/cloudapidoc/API_Docs',
