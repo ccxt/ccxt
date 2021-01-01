1e3512e7d83a46bf61c4dcfeb2ba198630a3aecd
Igor Kroitor <igor.kroitor@gmail.com>
2021-01-01 22:15:00 +0300

    gopax cleanup

--- js/gopax.js
+++ js/gopax.js
@@ -3,7 +3,6 @@
 //  ---------------------------------------------------------------------------
 
 const ccxt = require ('ccxt');
-const { AuthenticationError, ArgumentsRequired } = require ('ccxt/js/base/errors');
 const { ArrayCache, ArrayCacheBySymbolById } = require ('./base/Cache');
 
 //  ---------------------------------------------------------------------------
