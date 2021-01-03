e2fd8e04c81a573c3c94bb64f6017da028ddbee8
Igor Kroitor <igor.kroitor@gmail.com>
2021-01-03 03:32:11 +0300

    cache.py ArrayCacheById

--- python/ccxtpro/base/cache.py
+++ python/ccxtpro/base/cache.py
@@ -47,23 +47,38 @@ class ArrayCache(list):
             return deque[item]
 
 
-class ArrayCacheBySymbolById(ArrayCache):
+class ArrayCacheById(ArrayCache):
     def __init__(self, max_size=None):
-        super(ArrayCacheBySymbolById, self).__init__(max_size)
-        self._hashmap = {}
+        super(ArrayCacheById, self).__init__(max_size)
+        self._index = {}
 
     def __getattribute__(self, item):
-        methods = ArrayCacheBySymbolById.__dict__
+        methods = ArrayCacheById.__dict__
         if item in methods and hasattr(methods[item], '__get__'):
             # method calls
-            return methods[item].__get__(self, ArrayCacheBySymbolById)
+            return methods[item].__get__(self, ArrayCacheById)
         variables = object.__getattribute__(self, '__dict__')
         if item in variables:
             return variables[item]
-        return super(ArrayCacheBySymbolById, self).__getattribute__(item)
+        return super(ArrayCacheById, self).__getattribute__(item)
 
     def append(self, item):
-        by_id = self._hashmap.setdefault(item['symbol'], {})
+        if item['id'] in self._index:
+            reference = self._index[item['id']]
+            reference.clear()
+            reference.update(item)
+        else:
+            self._index[item['id']] = item
+            if len(self._deque) == self._deque.maxlen:
+                delete_reference = self._deque.popleft()
+                del self._index[delete_reference['id']]
+            self._deque.append(item)
+
+
+class ArrayCacheBySymbolById(ArrayCacheById):
+
+    def append(self, item):
+        by_id = self._index.setdefault(item['symbol'], {})
         if item['id'] in by_id:
             reference = by_id[item['id']]
             reference.clear()
