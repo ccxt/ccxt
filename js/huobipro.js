512840898e243a722c64d6e692666ad17e906220
ndubel <38309641+ndubel@users.noreply.github.com>
2021-01-06 15:06:02 +0300

    huobi linting

--- js/huobipro.js
+++ js/huobipro.js
@@ -209,7 +209,7 @@ module.exports = class huobipro extends Exchange {
                     'order-limitorder-price-min-error': InvalidOrder, // limit order price error
                     'order-limitorder-price-max-error': InvalidOrder, // limit order price error
                     'order-holding-limit-failed': InvalidOrder, // {"status":"error","err-code":"order-holding-limit-failed","err-msg":"Order failed, exceeded the holding limit of this currency","data":null}
-                    'order-orderprice-precision-error': InvalidOrder, //  {"status":"error","err-code":"order-orderprice-precision-error","err-msg":"order price precision error, scale: `4`","data":null}
+                    'order-orderprice-precision-error': InvalidOrder, // {"status":"error","err-code":"order-orderprice-precision-error","err-msg":"order price precision error, scale: `4`","data":null}
                     'order-orderstate-error': OrderNotFound, // canceling an already canceled order
                     'order-queryorder-invalid': OrderNotFound, // querying a non-existent order
                     'order-update-error': ExchangeNotAvailable, // undocumented error
