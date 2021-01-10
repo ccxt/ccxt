766865251eedf83eef29e7afa4203228f0278245
Travis CI <travis@travis-ci.org>
2021-01-10 19:01:09 +0000

    1.40.30
    [ci skip]

--- python/ccxt/ftx.py
+++ python/ccxt/ftx.py
@@ -221,6 +221,7 @@ class ftx(Exchange):
                     'Order already closed': InvalidOrder,  # {"error":"Order already closed","success":false}
                 },
                 'broad': {
+                    'Account does not have enough margin for order': InsufficientFunds,
                     'Invalid parameter': BadRequest,  # {"error":"Invalid parameter start_time","success":false}
                     'The requested URL was not found on the server': BadRequest,
                     'No such coin': BadRequest,
