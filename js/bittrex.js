a7d50415e269993b49d26f61320d85697f854ebf
Igor Kroitor <igor.kroitor@gmail.com>
2021-01-11 18:27:35 +0300

    bittrex watch_balance api secret fixup ccxt/ccxt#8242

--- js/bittrex.js
+++ js/bittrex.js
@@ -72,7 +72,7 @@ module.exports = class bittrex extends ccxt.bittrex {
         const timestamp = this.milliseconds ();
         const uuid = this.uuid ();
         const auth = timestamp.toString () + uuid;
-        const signature = this.hmac (this.encode (auth), this.secret, 'sha512');
+        const signature = this.hmac (this.encode (auth), this.encode (this.secret), 'sha512');
         const args = [ this.apiKey, timestamp, uuid, signature ];
         const method = 'Authenticate';
         return this.makeRequest (requestId, method, args);
