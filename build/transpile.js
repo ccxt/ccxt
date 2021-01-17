e29dcd706438dca2f758b64e0cd63e216a663ae6
Igor Kroitor <igor.kroitor@gmail.com>
2021-01-17 10:30:40 +0300

    build/transpile.js revert #8291

--- build/transpile.js
+++ build/transpile.js
@@ -806,17 +806,16 @@ class Transpiler {
 
     // ------------------------------------------------------------------------
 
-    getExchangeClassDeclarationMatches (contents) {
-        return contents.match (/^module\.exports\s*=\s*class\s+([\S]+)\s+extends\s+([\S]+)\s+{([\s\S]+?)^};*/m)
-    }
-
-    // ------------------------------------------------------------------------
-
     transpileDerivedExchangeClass (contents, methodNames = undefined) {
 
-        const [ _, className, baseClass, methodMatches ] = this.getExchangeClassDeclarationMatches (contents)
+        let exchangeClassDeclarationMatches = contents.match (/^module\.exports\s*=\s*class\s+([\S]+)\s+extends\s+([\S]+)\s+{([\s\S]+?)^};*/m)
 
-        const methods = methodMatches.trim ().split (/\n\s*\n/)
+        // log.green (file, exchangeClassDeclarationMatches[3])
+
+        let className = exchangeClassDeclarationMatches[1]
+        let baseClass = exchangeClassDeclarationMatches[2]
+
+        let methods = exchangeClassDeclarationMatches[3].trim ().split (/\n\s*\n/)
 
         let python2 = []
         let python3 = []
@@ -840,6 +839,14 @@ class Transpiler {
 
             // async or not
             let keyword = matches[1]
+            // try {
+            //     keyword = matches[1]
+            // } catch (e) {
+            //     log.red (e)
+            //     log.green (methods[i])
+            //     log.yellow (exchangeClassDeclarationMatches[3].trim ().split (/\n\s*\n/))
+            //     process.exit ()
+            // }
 
             // method name
             let method = matches[2]
@@ -917,37 +924,39 @@ class Transpiler {
         try {
 
             const { python2Folder, python3Folder, phpFolder } = options
-            const pythonFilename = filename.replace ('.js', '.py')
-            const phpFilename = filename.replace ('.js', '.php')
-            const jsmtime = fs.statSync (jsFolder + filename).mtime.getTime ()
-            const python2mtime = fs.statSync (python2Folder + pythonFilename).mtime.getTime ()
-            const python3mtime = fs.statSync (python3Folder + pythonFilename).mtime.getTime ()
-            const phpmtime = fs.statSync (phpFolder + phpFilename).mtime.getTime ()
-            const contents = fs.readFileSync (jsFolder + filename, 'utf8')
+            const path = jsFolder + filename
+            const contents = fs.readFileSync (path, 'utf8')
 
-            if ((jsmtime > python2mtime) || (jsmtime > python3mtime) || (jsmtime > phpmtime)) {
-                const { python2, python3, php, className, baseClass } = this.transpileDerivedExchangeClass (contents)
-                log.cyan ('Transpiling from', filename.yellow)
+            // function getMethodNames (object) {
+            //     let functions = []
+            //     let o = object
+            //     do {
+            //         functions = functions.concat (Object.getOwnPropertyNames (o))
+            //     } while (o = Object.getPrototypeOf (o))
+            //     return unique (functions.filter (f => (typeof object[f] === 'function')))
+            // }
+            // const exchangeClass = require ('.' + path)
+            // const exchange = new exchangeClass ()
+            // const methodNames = getMethodNames (exchange)
+            // const { python2, python3, php, className, baseClass } =
+            //     this.transpileDerivedExchangeClass (contents, methodNames)
 
-                ;[
-                    [ python2Folder, pythonFilename, python2 ],
-                    [ python3Folder, pythonFilename, python3 ],
-                    [ phpFolder, phpFilename, php ],
-                ].forEach (([ folder, filename, code ]) => {
-                    if (folder) {
-                        overwriteFile (folder + filename, code)
-                        fs.utimesSync (folder + filename, new Date (), new Date (jsmtime))
-                    }
-                })
+            const { python2, python3, php, className, baseClass } =
+                this.transpileDerivedExchangeClass (contents)
 
-                return { className, baseClass }
+            log.cyan ('Transpiling from', filename.yellow)
 
-            } else {
+            ;[
+                [ python2Folder, filename.replace ('.js', '.py'), python2 ],
+                [ python3Folder, filename.replace ('.js', '.py'), python3 ],
+                [ phpFolder, filename.replace ('.js', '.php'), php ],
+            ].forEach (([ folder, filename, code ]) => {
+                if (folder) {
+                    overwriteFile (folder + filename, code)
+                }
+            })
 
-                const [ _, className, baseClass ] = this.getExchangeClassDeclarationMatches (contents)
-                log.green ('Already transpiled', filename.yellow)
-                return { className, baseClass }
-            }
+            return { className, baseClass }
 
         } catch (e) {
 
