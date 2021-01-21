9cdd0d13270f4e4b6f5317edba1ad9c95458922f
Carlo Revelli <carlo.revelli@berkeley.edu>
2021-01-21 11:12:27 +0000

    add asyncPhpmtime

--- build/transpile.js
+++ build/transpile.js
@@ -996,9 +996,11 @@ class Transpiler {
                 const python2Path = python2Folder ? (python2Folder + pythonFilename) : undefined
                 const python3Path = python3Folder + pythonFilename
                 const phpPath = phpFolder + phpFilename
+                const asyncPhpPath = asyncPhpFolder + phpFilename
                 python2mtime = python2Folder ? (fs.existsSync (python2Path) ? fs.statSync (python2Path).mtime.getTime () : 0) : undefined
                 python3mtime = fs.existsSync (python3Path) ? fs.statSync (python3Path).mtime.getTime () : 0
                 phpmtime = fs.existsSync (python3Path) ? fs.statSync (phpPath).mtime.getTime () : 0
+                asyncPhpmtime = fs.existsSync (asyncPhpPath) ? fs.statSync (phpPath).mtime.getTime () : 0
             }
             if (force || (jsmtime > python3mtime) || (jsmtime > phpmtime) || (jsmtime > asyncPhpmtime) || (python2Folder && (jsmtime > python2mtime))) {
                 const { python2, python3, php, asyncPhp, className, baseClass } = this.transpileDerivedExchangeClass (contents)
