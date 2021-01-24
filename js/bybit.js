5e824fa0679af3f8fccbb04a6c02b23df85da2b7
Igor Kroitor <igor.kroitor@gmail.com>
2021-01-25 00:26:31 +0300

    bybit stop_px sign decimal digits fix #8340

--- js/bybit.js
+++ js/bybit.js
@@ -2199,12 +2199,6 @@ module.exports = class bybit extends Exchange {
                 'timestamp': timestamp,
             });
             let auth = this.rawencode (this.keysort (query));
-            // https://github.com/ccxt/ccxt/issues/7377
-            // https://github.com/ccxt/ccxt/issues/7515
-            // bybit encodes whole floats as integers without .0 for conditional stop-orders only
-            if (path.indexOf ('stop-order') >= 0) {
-                auth = auth.replace ('.0&', '&');
-            }
             const signature = this.hmac (this.encode (auth), this.encode (this.secret));
             if (method === 'POST') {
                 body = this.json (this.extend (query, {
