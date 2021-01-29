3a0c96a99e54cdbe92afbf2aba543ac8d3536c2a
Igor Kroitor <igor.kroitor@gmail.com>
2021-01-29 15:46:16 +0300

    bithumb linting

--- js/bithumb.js
+++ js/bithumb.js
@@ -115,7 +115,7 @@ module.exports = class bithumb extends Exchange {
                         },
                     },
                     'KRW': {},
-                }
+                },
             },
         });
     }
