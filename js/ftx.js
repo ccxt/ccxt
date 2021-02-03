e98dc026b4221e3a1e131f610d8a796179042bb1
Nick Pomfret <npomfret@users.noreply.github.com>
2021-02-03 11:19:52 +0000

    [ftx] error mapping
    https://github.com/ccxt/ccxt/issues/8380

--- js/ftx.js
+++ js/ftx.js
@@ -231,6 +231,7 @@ module.exports = class ftx extends Exchange {
                     'Order already queued for cancellation': CancelPending, // {"error":"Order already queued for cancellation","success":false}
                 },
                 'broad': {
+                    'Please slow down': RateLimitExceeded,
                     'Account does not have enough margin for order': InsufficientFunds,
                     'Invalid parameter': BadRequest, // {"error":"Invalid parameter start_time","success":false}
                     'The requested URL was not found on the server': BadRequest,
