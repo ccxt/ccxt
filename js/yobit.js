fdb1afb02833254f5d9ef77c572ebf291ced1e9e
ndubel <38309641+ndubel@users.noreply.github.com>
2021-02-24 19:13:58 +0300

    yobit MASK mapping
    I can't find any info about this coin but it is not https://coinmarketcap.com/currencies/mask-network/markets/
    price is too different https://yobit.net/ru/trade/MASK/USD

--- js/yobit.js
+++ js/yobit.js
@@ -133,6 +133,7 @@ module.exports = class yobit extends Exchange {
                 'LOCX': 'LOC',
                 'LUNYR': 'LUN',
                 'LUN': 'LunarCoin',  // they just change the ticker if it is already taken
+                'MASK': 'Yobit MASK',
                 'MDT': 'Midnight',
                 'NAV': 'NavajoCoin',
                 'NBT': 'NiceBytes',
