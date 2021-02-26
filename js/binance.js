1420e1253ceeeb91709da2a23286903211b2411b
Carlo Revelli <carlo.revelli@berkeley.edu>
2021-02-26 18:06:45 +0000

    binance fix watchOrders

--- js/binance.js
+++ js/binance.js
@@ -827,10 +827,10 @@ module.exports = class binance extends ccxt.binance {
         const defaultType = this.safeString2 (this.options, 'watchOrders', 'defaultType', 'spot');
         const type = this.safeString (params, 'type', defaultType);
         const url = this.urls['api']['ws'][type] + '/' + this.options[type]['listenKey'];
-        const messageHash = 'orders';
-        let subscriptionHash = messageHash;
+        let messageHash = 'orders';
+        const subscriptionHash = messageHash;
         if (symbol !== undefined) {
-            subscriptionHash += ':' + symbol;
+            messageHash += ':' + symbol;
         }
         const message = undefined;
         const orders = await this.watch (url, messageHash, message, subscriptionHash);
@@ -1087,10 +1087,10 @@ module.exports = class binance extends ccxt.binance {
         const defaultType = this.safeString2 (this.options, 'watchMyTrades', 'defaultType', 'spot');
         const type = this.safeString (params, 'type', defaultType);
         const url = this.urls['api']['ws'][type] + '/' + this.options[type]['listenKey'];
-        const messageHash = 'myTrades';
-        let subscriptionHash = messageHash;
+        let messageHash = 'myTrades';
+        const subscriptionHash = messageHash;
         if (symbol !== undefined) {
-            subscriptionHash += ':' + symbol;
+            messageHash += ':' + symbol;
         }
         const message = undefined;
         const trades = await this.watch (url, messageHash, message, subscriptionHash);
