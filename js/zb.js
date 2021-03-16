8c78de1393dce4ea4111075edb3b4383d474319d
Igor Kroitor <igor.kroitor@gmail.com>
2021-03-16 04:53:46 +0300

    zb fetchMarkets comments

--- js/zb.js
+++ js/zb.js
@@ -205,6 +205,16 @@ module.exports = class zb extends Exchange {
 
     async fetchMarkets (params = {}) {
         const markets = await this.publicGetMarkets (params);
+        //
+        //     {
+        //         "zb_qc":{
+        //             "amountScale":2,
+        //             "minAmount":0.01,
+        //             "minSize":5,
+        //             "priceScale":4,
+        //         },
+        //     }
+        //
         const keys = Object.keys (markets);
         const result = [];
         for (let i = 0; i < keys.length; i++) {
