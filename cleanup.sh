269e6bc58ced4105538a2115d6f410192972b172
quechi199900 <71445529+quechi199900@users.noreply.github.com>
2021-03-20 10:46:15 +0700

    Update cleanup.sh
    git checkout HEAD 1588200054 .json @CHI_Adelaide

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git checkout HEAD package.json README.md ccxt.d.ts ccxt.js wiki python php dist doc
+git checkout HEAD 1588200054.json @CHI_Adelaide.md ccxt.d.ts ccxt.js wiki python php dist doc
