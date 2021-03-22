d2ccba27020c10f01b12d4cad7640ac2cf2f498a
Travis CI <travis@travis-ci.org>
2021-03-22 17:06:27 +0000

    1.43.95
    [ci skip]

--- php/async/base/Exchange.php
+++ php/async/base/Exchange.php
@@ -28,11 +28,11 @@ use Exception;
 
 include 'throttle.php';
 
-$version = '1.43.94';
+$version = '1.43.95';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '1.43.94';
+    const VERSION = '1.43.95';
 
     public static $loop;
     public static $kernel;
--- python/ccxt/async_support/__init__.py
+++ python/ccxt/async_support/__init__.py
@@ -4,7 +4,7 @@
 
 # -----------------------------------------------------------------------------
 
-__version__ = '1.43.94'
+__version__ = '1.43.95'
 
 # -----------------------------------------------------------------------------
 
@@ -54,6 +54,7 @@ from ccxt.base.errors import error_hierarchy                # noqa: F401
 from ccxt.async_support.aax import aax                                    # noqa: F401
 from ccxt.async_support.acx import acx                                    # noqa: F401
 from ccxt.async_support.aofex import aofex                                # noqa: F401
+from ccxt.async_support.ascendex import ascendex                          # noqa: F401
 from ccxt.async_support.bequant import bequant                            # noqa: F401
 from ccxt.async_support.bibox import bibox                                # noqa: F401
 from ccxt.async_support.bigone import bigone                              # noqa: F401
@@ -179,6 +180,7 @@ exchanges = [
     'aax',
     'acx',
     'aofex',
+    'ascendex',
     'bequant',
     'bibox',
     'bigone',
