2c47e82bfe27d3264bc3fade5d741f175afb3a49
Travis CI <travis@travis-ci.com>
2021-03-26 12:27:34 +0000

    0.6.22
    [ci skip]

--- python/ccxtpro/__init__.py
+++ python/ccxtpro/__init__.py
@@ -8,7 +8,7 @@ import ccxt.async_support as ccxt
 
 # -----------------------------------------------------------------------------
 
-__version__ = '0.6.21'
+__version__ = '0.6.22'
 
 # -----------------------------------------------------------------------------
 
@@ -59,7 +59,6 @@ from ccxt import OrderNotFillable                   # noqa: F401
 # CCXT exchanges
 
 from ccxt.async_support.aax import aax                                    # noqa: F401
-from ccxt.async_support.acx import acx                                    # noqa: F401
 from ccxt.async_support.aofex import aofex                                # noqa: F401
 from ccxt.async_support.ascendex import ascendex                          # noqa: F401
 from ccxt.async_support.bibox import bibox                                # noqa: F401
