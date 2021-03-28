a3983f18820eda374189c20c36c2a484d9ca4550
Travis CI <travis@travis-ci.org>
2021-03-28 13:46:30 +0000

    1.45.5
    [ci skip]

--- php/coingi.php
+++ php/coingi.php
@@ -167,10 +167,10 @@ class coingi extends Exchange {
             $currencyId = $this->safe_string($balance['currency'], 'name');
             $code = $this->safe_currency_code($currencyId);
             $account = $this->account();
-            $account['free'] = $this->safe_float($balance, 'available');
-            $blocked = $this->safe_float($balance, 'blocked');
-            $inOrders = $this->safe_float($balance, 'inOrders');
-            $withdrawing = $this->safe_float($balance, 'withdrawing');
+            $account['free'] = $this->safe_number($balance, 'available');
+            $blocked = $this->safe_number($balance, 'blocked');
+            $inOrders = $this->safe_number($balance, 'inOrders');
+            $withdrawing = $this->safe_number($balance, 'withdrawing');
             $account['used'] = $this->sum($blocked, $inOrders, $withdrawing);
             $result[$code] = $account;
         }
@@ -200,11 +200,11 @@ class coingi extends Exchange {
             'symbol' => $symbol,
             'timestamp' => $timestamp,
             'datetime' => $this->iso8601($timestamp),
-            'high' => $this->safe_float($ticker, 'high'),
-            'low' => $this->safe_float($ticker, 'low'),
-            'bid' => $this->safe_float($ticker, 'highestBid'),
+            'high' => $this->safe_number($ticker, 'high'),
+            'low' => $this->safe_number($ticker, 'low'),
+            'bid' => $this->safe_number($ticker, 'highestBid'),
             'bidVolume' => null,
-            'ask' => $this->safe_float($ticker, 'lowestAsk'),
+            'ask' => $this->safe_number($ticker, 'lowestAsk'),
             'askVolume' => null,
             'vwap' => null,
             'open' => null,
@@ -214,8 +214,8 @@ class coingi extends Exchange {
             'change' => null,
             'percentage' => null,
             'average' => null,
-            'baseVolume' => $this->safe_float($ticker, 'baseVolume'),
-            'quoteVolume' => $this->safe_float($ticker, 'counterVolume'),
+            'baseVolume' => $this->safe_number($ticker, 'baseVolume'),
+            'quoteVolume' => $this->safe_number($ticker, 'counterVolume'),
             'info' => $ticker,
         );
     }
@@ -248,8 +248,8 @@ class coingi extends Exchange {
     }
 
     public function parse_trade($trade, $market = null) {
-        $price = $this->safe_float($trade, 'price');
-        $amount = $this->safe_float($trade, 'amount');
+        $price = $this->safe_number($trade, 'price');
+        $amount = $this->safe_number($trade, 'amount');
         $cost = null;
         if ($price !== null) {
             if ($amount !== null) {
