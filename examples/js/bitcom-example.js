a7aaf304407edf8933aed5c8b60830520cb3e2f7
wafeishushu <a153255005@163.com>
2021-03-31 15:09:45 +0800

    add fetchOrder(s)

--- examples/js/bitcom-example.js
+++ examples/js/bitcom-example.js
@@ -37,49 +37,22 @@ function prettyJSONLog(str) {
     const klines = await exchange.fetchOHLCV (symbol, '5m', undefined, 10)
     prettyJSONLog (klines)
 
-
     // fetch ticker
     symbol = 'BTC-PERPETUAL'
     const ticker = await exchange.fetchTicker (symbol)
     prettyJSONLog (ticker)
 
+    // fetch order
+    params = {
+        'currency': 'BTC',
+    }
+    const order = await exchange.fetchOrder ('17508028', undefined, params )
+    prettyJSONLog (order)
 
-    //
-    // //get orderbook
-    // const orderbook = await exchange.fetchOrderBook ('BTC/USDT')
-    // log (orderbook)
-    // //get public trades
-    // const publicTrades = await exchange.fetchTrades ('BTC/USDT', 1518983548636 - 2 * 24 * 60 * 60 * 1000)
-    // log (asTable (publicTrades))
-    //
-    // //get tickers
-    // const tickers = await exchange.fetchTickers ()
-    // log (tickers)
-
-    //
-    // //get private trades
-    // let myTrades = await exchange.fetchMyTrades('BTC/USDT', undefined, 5)
-    // log('myTrades'.green, asTable(myTrades))
-    //
-    // //create and delete order
-    // {
-    //     const orderId = await exchange.createOrder('BTC/USDT', 'market', 'sell', 0.0001)
-    //     const order = await exchange.fetchOrder(orderId['id'])
-    //     log(order)
-    //     const cancelResponse = await exchange.cancelOrder(orderId['id'])
-    //     log(cancelResponse)
-    // }
-    //
-
-    //
-    // //get open orders
-    // const openOrders = await exchange.fetchOpenOrders ()
-    // log (asTable (openOrders.map (order => ccxt.omit (order, [ 'timestamp', 'info' ]))))
-    //
-    // //get closed orders
-    // const orders = await exchange.fetchClosedOrders ()
-    // log (asTable (orders.map (order => ccxt.omit (order, [ 'timestamp', 'info' ]))))
-    // const order = await exchange.fetchOrder (orders[0]['id'])
-    // log (order)
-
+    // fetch orders
+    params = {
+        'currency': 'BTC',
+    }
+    const orders = await exchange.fetchOrders (undefined, undefined, params )
+    prettyJSONLog (orders)
 })()
