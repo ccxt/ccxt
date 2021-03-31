f563c4608ef1ec5d93ca06f5f2f86673667aaa9b
Carlo Revelli <carlo.revelli@berkeley.edu>
2021-03-31 02:05:22 +0100

    edit comment

--- js/binance.js
+++ js/binance.js
@@ -1768,7 +1768,7 @@ module.exports = class binance extends Exchange {
         //         "updateTime": 1499827319559
         //     }
         //
-        // market orders with { "newOrderRespType": "FULL" }
+        //  createOrder with { "newOrderRespType": "FULL" }
         //
         //     {
         //       "symbol": "BTCUSDT",
