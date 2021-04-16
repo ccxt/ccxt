cc3359bc7e24d7e741bbaf7773416df70d1d554b
Igor Kroitor <igor.kroitor@gmail.com>
2021-04-16 05:06:55 +0300

    okex fetchOrderBook swap markets timestamp fix #8935

--- js/okex.js
+++ js/okex.js
@@ -1025,6 +1025,8 @@ module.exports = class okex extends Exchange {
         }
         const response = await this[method] (this.extend (request, params));
         //
+        // spot
+        //
         //     {      asks: [ ["0.02685268", "0.242571", "1"],
         //                    ["0.02685493", "0.164085", "1"],
         //                    ...
@@ -1037,7 +1039,19 @@ module.exports = class okex extends Exchange {
         //                    ["0.02634962", "0.264838", "2"]    ],
         //       timestamp:   "2018-12-17T20:24:16.159Z"            }
         //
-        const timestamp = this.parse8601 (this.safeString (response, 'timestamp'));
+        // swap
+        //
+        //     {
+        //         "asks":[
+        //             ["916.21","94","0","1"]
+        //         ],
+        //         "bids":[
+        //             ["916.1","15","0","1"]
+        //         ],
+        //         "time":"2021-04-16T02:04:48.282Z"
+        //     }
+        //
+        const timestamp = this.parse8601 (this.safeString2 (response, 'timestamp', 'time'));
         return this.parseOrderBook (response, timestamp);
     }
 
