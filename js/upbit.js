e598fca15265e471adda48fb72f94bda1ae65b7f
Igor Kroitor <igor.kroitor@gmail.com>
2021-04-26 13:58:34 +0300

    upbit fetchBalance timestamp

--- js/upbit.js
+++ js/upbit.js
@@ -428,7 +428,11 @@ module.exports = class upbit extends Exchange {
         //         avg_krw_buy_price: "250000",
         //                  modified:  false    }   ]
         //
-        const result = { 'info': response };
+        const result = {
+            'info': response,
+            'timestamp': undefined,
+            'datetime': undefined,
+        };
         for (let i = 0; i < response.length; i++) {
             const balance = response[i];
             const currencyId = this.safeString (balance, 'currency');
