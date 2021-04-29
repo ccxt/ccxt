45b2d0aa0a7787b368f2a962946eb3da66993ed1
Munny Kumar <munny@lcx.com>
2021-04-29 10:21:59 +0530

    test

--- js/lcx.js
+++ js/lcx.js
@@ -306,7 +306,7 @@ module.exports = class lcx extends Exchange {
     }
 
     parseTrade (trade, market = undefined) {
-        const timestamp = this.parseNumber (this.nonce ());
+        const timestamp = this.parseNumber (trade[3]);
         const id = trade[3].toString ();
         const symbol = this.safeString (market, 'symbol');
         let side = this.safeString (trade[2], 'side');
