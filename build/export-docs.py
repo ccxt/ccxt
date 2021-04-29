a8ad8a9beabacbedff5a22d2e4ea933d55234c44
Carlo Revelli <carlo.revelli@berkeley.edu>
2021-04-29 15:04:15 +0100

    save progress

---
+++ build/export-docs.py
@@ -0,0 +1,19 @@
+import re
+import m2r2
+
+files = [
+    'Manual',
+    'Exchange-Markets-By-Country',
+    'Exchange-Markets',
+]
+
+for file in files:
+    rst = m2r2.parse_from_file('./wiki/' + file + '.md', 'utf-8', parse_relative_links=True, anonymous_references=True)
+    blank_lines = re.sub('- \n {5}.. image', '- .. image', rst)
+    indent_level = re.sub(':(target|alt):', r'  :\1:', blank_lines)
+    reference_links = re.sub(r'<(\w+)-(\w+)-(\w+)-(\w+)>`', r'<\1 \2 \3 \4>`', indent_level)
+    reference_links2 = re.sub(r'<(\w+)-(\w+)-(\w+)>`', r'<\1 \2 \3>`', reference_links)
+    reference_links3 = re.sub(r'<(\w+)-(\w+)>`', r'<\1 \2>`', reference_links2)
+
+    with open('./doc/' + file + '.rst', 'w') as f:
+        f.write(reference_links3)
--- doc/conf.py
+++ doc/conf.py
@@ -39,7 +39,7 @@ with open(os.path.join(root_path, 'package.json')) as f:
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.intersphinx']
+extensions = ['sphinx.ext.intersphinx', 'sphinx.ext.autosectionlabel', 'm2r2']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,7 +47,7 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst']
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
@@ -87,13 +87,20 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'collapse_navagation': False,
+    'titles_only': False,
+}
+
+html_context = {
+    'display_github': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
