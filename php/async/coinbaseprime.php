19a7aba3b065f8aed62c391761c280b3d30d9023
Travis CI <travis@travis-ci.org>
2021-05-05 16:47:05 +0000

    1.49.53
    [ci skip]

--- php/async/coinbaseprime.php
+++ php/async/coinbaseprime.php
@@ -14,20 +14,19 @@ class coinbaseprime extends coinbasepro {
             'id' => 'coinbaseprime',
             'name' => 'Coinbase Prime',
             'pro' => true,
-            'hostname' => 'prime.coinbase.com',
+            'hostname' => 'exchange.coinbase.com',
             'urls' => array(
                 'test' => array(
-                    'public' => 'https://api-public.sandbox.prime.coinbase.com',
-                    'private' => 'https://api-public.sandbox.prime.coinbase.com',
+                    'public' => 'https://public.sandbox.exchange.coinbase.com',
+                    'private' => 'https://public.sandbox.exchange.coinbase.com',
                 ),
                 'logo' => 'https://user-images.githubusercontent.com/1294454/44539184-29f26e00-a70c-11e8-868f-e907fc236a7c.jpg',
                 'api' => array(
                     'public' => 'https://api.{hostname}',
                     'private' => 'https://api.{hostname}',
                 ),
-                'www' => 'https://prime.coinbase.com',
-                'doc' => 'https://docs.prime.coinbase.com',
-                'fees' => 'https://support.prime.coinbase.com/customer/en/portal/articles/2945629-fees?b_id=17475',
+                'www' => 'https://exchange.coinbase.com',
+                'doc' => 'https://docs.exchange.coinbase.com',
             ),
         ));
     }
