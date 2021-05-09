a734174c702931a05a038f1f7d3ed4eb7051922a
ndubel <38309641+ndubel@users.noreply.github.com>
2021-05-09 21:53:52 +0300

    crex24 FUND mapping
    https://www.coingecko.com/en/coins/fundchains
    conflict with https://www.coingecko.com/en/coins/unification

--- js/crex24.js
+++ js/crex24.js
@@ -134,6 +134,7 @@ module.exports = class crex24 extends Exchange {
                 'BULL': 'BuySell',
                 'CREDIT': 'TerraCredit',
                 'EPS': 'Epanus',  // conflict with EPS Ellipsis https://github.com/ccxt/ccxt/issues/8909
+                'FUND': 'FUNDChains',
                 'GHOST': 'GHOSTPRISM',
                 'IQ': 'IQ.Cash',
                 'PUT': 'PutinCoin',
