572eb782bb089db5fa49b53bd340d783260a6331
Igor Kroitor <igor.kroitor@gmail.com>
2021-05-11 03:04:39 +0300

    binance usdⓈ-m logo

--- js/binanceusdm.js
+++ js/binanceusdm.js
@@ -11,6 +11,9 @@ module.exports = class binanceusdm extends binance {
         return this.deepExtend (super.describe (), {
             'id': 'binanceusdm',
             'name': 'Binance USDⓈ-M',
+            'urls': {
+                'logo': 'https://user-images.githubusercontent.com/1294454/117738721-668c8d80-b205-11eb-8c49-3fad84c4a07f.jpg',
+            },
             'options': {
                 'defaultType': 'future',
             },
