792d0521ec34922b40668d5c34e23174ddd23d52
Igor Kroitor <igor.kroitor@gmail.com>
2021-05-11 08:08:51 +0300

    binance coin-m, usdcⓈ-m

---
+++ js/binancecoinm.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//  ---------------------------------------------------------------------------
+
+const { NotSupported } = require ('ccxt/js/base/errors');
+const binance = require ('./binance.js');
+
+// ---------------------------------------------------------------------------
+
+module.exports = class binancecoinm extends binance {
+    describe () {
+        return this.deepExtend (super.describe (), {
+            'id': 'binancecoinm',
+            'id': 'binancecoinm',
+            'name': 'Binance COIN-M',
+            'urls': {
+                'logo': 'https://user-images.githubusercontent.com/1294454/117738721-668c8d80-b205-11eb-8c49-3fad84c4a07f.jpg',
+            },
+            'options': {
+                'defaultType': 'delivery',
+            },
+        });
+    }
+};
