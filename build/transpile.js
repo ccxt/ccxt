8c038eb7d2d279eda7d6f861f514283cc5542ad0
Carlo Revelli <carlo.revelli@berkeley.edu>
2021-05-12 06:30:34 +0100

    use classes instead

--- build/transpile.js
+++ build/transpile.js
@@ -1077,13 +1077,11 @@ class Transpiler {
 
         if (classNames.length > 1) {
 
-            const errors = require ('../js/base/errors')
-
             function deleteOldTranspiledFiles (folder, pattern) {
                 fs.readdirSync (folder)
                     .filter (file =>
                         !fs.lstatSync (folder + file).isDirectory () &&
-                        !(file.replace (pattern, '') in errors) &&
+                        (file.replace (pattern, '') in classes) &&
                         !(file.replace (/\.[a-z]+$/, '') in classes) &&
                         !file.match (/^Exchange|errors|__init__|\\./))
                     .map (file => folder + file)
