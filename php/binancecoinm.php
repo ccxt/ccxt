ca114a92605ef62186d0005df53c7efe79bcec34
Travis CI <travis@travis-ci.org>
2021-05-12 16:21:07 +0000

    1.49.91
    [ci skip]

--- php/binancecoinm.php
+++ php/binancecoinm.php
@@ -19,6 +19,74 @@ class binancecoinm extends binance {
             'options' => array(
                 'defaultType' => 'delivery',
             ),
+            // https://www.binance.com/en/fee/deliveryFee
+            'fees' => array(
+                'trading' => array(
+                    'tierBased' => true,
+                    'percentage' => true,
+                    'taker' => $this->parse_number('0.000500'),
+                    'maker' => $this->parse_number('0.000100'),
+                    'tiers' => array(
+                        'taker' => array(
+                            array( $this->parse_number('0'), $this->parse_number('0.000500') ),
+                            array( $this->parse_number('250'), $this->parse_number('0.000450') ),
+                            array( $this->parse_number('2500'), $this->parse_number('0.000400') ),
+                            array( $this->parse_number('7500'), $this->parse_number('0.000300') ),
+                            array( $this->parse_number('22500'), $this->parse_number('0.000250') ),
+                            array( $this->parse_number('50000'), $this->parse_number('0.000240') ),
+                            array( $this->parse_number('100000'), $this->parse_number('0.000240') ),
+                            array( $this->parse_number('200000'), $this->parse_number('0.000240') ),
+                            array( $this->parse_number('400000'), $this->parse_number('0.000240') ),
+                            array( $this->parse_number('750000'), $this->parse_number('0.000240') ),
+                        ),
+                        'maker' => array(
+                            array( $this->parse_number('0'), $this->parse_number('0.000100') ),
+                            array( $this->parse_number('250'), $this->parse_number('0.000080') ),
+                            array( $this->parse_number('2500'), $this->parse_number('0.000050') ),
+                            array( $this->parse_number('7500'), $this->parse_number('0.0000030') ),
+                            array( $this->parse_number('22500'), $this->parse_number('0') ),
+                            array( $this->parse_number('50000'), $this->parse_number('-0.000050') ),
+                            array( $this->parse_number('100000'), $this->parse_number('-0.000060') ),
+                            array( $this->parse_number('200000'), $this->parse_number('-0.000070') ),
+                            array( $this->parse_number('400000'), $this->parse_number('-0.000080') ),
+                            array( $this->parse_number('750000'), $this->parse_number('-0.000090') ),
+                        ),
+                    ),
+                ),
+            ),
         ));
+    }
+
+    public function fetch_trading_fees($params = array ()) {
+        $this->load_markets();
+        $marketSymbols = is_array($this->markets) ? array_keys($this->markets) : array();
+        $fees = array();
+        $accountInfo = $this->dapiPrivateGetAccount ($params);
+        //
+        // {
+        //      "canDeposit" => true,
+        //      "canTrade" => true,
+        //      "canWithdraw" => true,
+        //      "$feeTier" => 2,
+        //      "updateTime" => 0
+        //      ...
+        //  }
+        //
+        $feeTier = $this->safe_integer($accountInfo, 'feeTier');
+        $feeTiers = $this->fees['trading']['tiers'];
+        $maker = $feeTiers['maker'][$feeTier][1];
+        $taker = $feeTiers['taker'][$feeTier][1];
+        for ($i = 0; $i < count($marketSymbols); $i++) {
+            $symbol = $marketSymbols[$i];
+            $fees[$symbol] = array(
+                'info' => array(
+                    'feeTier' => $feeTier,
+                ),
+                'symbol' => $symbol,
+                'maker' => $maker,
+                'taker' => $taker,
+            );
+        }
+        return $fees;
     }
 }
--- python/ccxt/async_support/binancecoinm.py
+++ python/ccxt/async_support/binancecoinm.py
@@ -18,4 +18,70 @@ class binancecoinm(binance):
             'options': {
                 'defaultType': 'delivery',
             },
+            # https://www.binance.com/en/fee/deliveryFee
+            'fees': {
+                'trading': {
+                    'tierBased': True,
+                    'percentage': True,
+                    'taker': self.parse_number('0.000500'),
+                    'maker': self.parse_number('0.000100'),
+                    'tiers': {
+                        'taker': [
+                            [self.parse_number('0'), self.parse_number('0.000500')],
+                            [self.parse_number('250'), self.parse_number('0.000450')],
+                            [self.parse_number('2500'), self.parse_number('0.000400')],
+                            [self.parse_number('7500'), self.parse_number('0.000300')],
+                            [self.parse_number('22500'), self.parse_number('0.000250')],
+                            [self.parse_number('50000'), self.parse_number('0.000240')],
+                            [self.parse_number('100000'), self.parse_number('0.000240')],
+                            [self.parse_number('200000'), self.parse_number('0.000240')],
+                            [self.parse_number('400000'), self.parse_number('0.000240')],
+                            [self.parse_number('750000'), self.parse_number('0.000240')],
+                        ],
+                        'maker': [
+                            [self.parse_number('0'), self.parse_number('0.000100')],
+                            [self.parse_number('250'), self.parse_number('0.000080')],
+                            [self.parse_number('2500'), self.parse_number('0.000050')],
+                            [self.parse_number('7500'), self.parse_number('0.0000030')],
+                            [self.parse_number('22500'), self.parse_number('0')],
+                            [self.parse_number('50000'), self.parse_number('-0.000050')],
+                            [self.parse_number('100000'), self.parse_number('-0.000060')],
+                            [self.parse_number('200000'), self.parse_number('-0.000070')],
+                            [self.parse_number('400000'), self.parse_number('-0.000080')],
+                            [self.parse_number('750000'), self.parse_number('-0.000090')],
+                        ],
+                    },
+                },
+            },
         })
+
+    async def fetch_trading_fees(self, params={}):
+        await self.load_markets()
+        marketSymbols = list(self.markets.keys())
+        fees = {}
+        accountInfo = await self.dapiPrivateGetAccount(params)
+        #
+        # {
+        #      "canDeposit": True,
+        #      "canTrade": True,
+        #      "canWithdraw": True,
+        #      "feeTier": 2,
+        #      "updateTime": 0
+        #      ...
+        #  }
+        #
+        feeTier = self.safe_integer(accountInfo, 'feeTier')
+        feeTiers = self.fees['trading']['tiers']
+        maker = feeTiers['maker'][feeTier][1]
+        taker = feeTiers['taker'][feeTier][1]
+        for i in range(0, len(marketSymbols)):
+            symbol = marketSymbols[i]
+            fees[symbol] = {
+                'info': {
+                    'feeTier': feeTier,
+                },
+                'symbol': symbol,
+                'maker': maker,
+                'taker': taker,
+            }
+        return fees
