34b6d8009498291f57725ef79978b464251b52e8
Nick Pomfret <npomfret@users.noreply.github.com>
2021-05-23 11:14:26 +0100

    Update cex.js

--- js/cex.js
+++ js/cex.js
@@ -89,7 +89,7 @@ module.exports = class cex extends Exchange {
                         'open_position/{pair}/',
                         'open_positions/{pair}/',
                         'place_order/{pair}/',
-                        'raw_tx_history'
+                        'raw_tx_history',
                     ],
                 },
             },
