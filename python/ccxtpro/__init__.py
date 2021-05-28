62956dae342bd32517cc28edb9dcda84b61ed124
Travis CI <travis@travis-ci.com>
2021-05-28 01:57:57 +0000

    0.7.4
    [ci skip]

--- python/ccxtpro/__init__.py
+++ python/ccxtpro/__init__.py
@@ -8,7 +8,7 @@ import ccxt.async_support as ccxt
 
 # -----------------------------------------------------------------------------
 
-__version__ = '0.7.3'
+__version__ = '0.7.4'
 
 # -----------------------------------------------------------------------------
 
@@ -95,7 +95,6 @@ from ccxt.async_support.coinegg import coinegg                            # noqa
 from ccxt.async_support.coinex import coinex                              # noqa: F401
 from ccxt.async_support.coinfalcon import coinfalcon                      # noqa: F401
 from ccxt.async_support.coinfloor import coinfloor                        # noqa: F401
-from ccxt.async_support.coingi import coingi                              # noqa: F401
 from ccxt.async_support.coinmarketcap import coinmarketcap                # noqa: F401
 from ccxt.async_support.coinmate import coinmate                          # noqa: F401
 from ccxt.async_support.coinone import coinone                            # noqa: F401
