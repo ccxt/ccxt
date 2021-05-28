f4f0bc7b497ec060a41763b88f3f57703ee6400d
Igor Kroitor <igor.kroitor@gmail.com>
2021-05-28 07:04:30 +0300

    okex country fix #9260

--- js/okex.js
+++ js/okex.js
@@ -14,7 +14,7 @@ module.exports = class okex extends Exchange {
         return this.deepExtend (super.describe (), {
             'id': 'okex',
             'name': 'OKEX',
-            'countries': [ 'CN', 'US' ],
+            'countries': [ 'CN' ],
             'version': 'v3',
             'rateLimit': 1000, // up to 3000 requests per 5 minutes ≈ 600 requests per minute ≈ 10 requests per second ≈ 100 ms
             'pro': true,
