266b4ce84bb9f9dcede2cb87e36eb039cdba62bd
Travis CI <travis@travis-ci.org>
2021-06-03 16:05:50 +0000

    1.50.81
    [ci skip]

--- php/binanceusdm.php
+++ php/binanceusdm.php
@@ -187,11 +187,7 @@ class binanceusdm extends binance {
         $this->load_leverage_brackets();
         $account = $this->fapiPrivateGetAccount ($params);
         $result = $this->parse_account_positions ($account);
-        if ($symbols === null) {
-            return $result;
-        } else {
-            return $this->filter_by_array($result, 'symbol', $symbols, false);
-        }
+        return $this->filter_by_array($result, 'symbol', $symbols, false);
     }
 
     public function fetch_isolated_positions($symbol = null, $params = array ()) {
--- python/ccxt/async_support/binanceusdm.py
+++ python/ccxt/async_support/binanceusdm.py
@@ -174,10 +174,7 @@ class binanceusdm(binance):
         await self.load_leverage_brackets()
         account = await self.fapiPrivateGetAccount(params)
         result = self.parse_account_positions(account)
-        if symbols is None:
-            return result
-        else:
-            return self.filter_by_array(result, 'symbol', symbols, False)
+        return self.filter_by_array(result, 'symbol', symbols, False)
 
     async def fetch_isolated_positions(self, symbol=None, params={}):
         # only supported in usdm futures
