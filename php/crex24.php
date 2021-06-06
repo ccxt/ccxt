7a5ad943ae5049759a07299de48a8e5bd563ff71
Travis CI <travis@travis-ci.org>
2021-06-06 09:14:28 +0000

    1.50.93
    [ci skip]

--- php/crex24.php
+++ php/crex24.php
@@ -143,6 +143,7 @@ class crex24 extends Exchange {
                 'EPS' => 'Epanus',  // conflict with EPS Ellipsis https://github.com/ccxt/ccxt/issues/8909
                 'FUND' => 'FUNDChains',
                 'GHOST' => 'GHOSTPRISM',
+                'GTC' => 'Gastrocoin', // conflict with Gitcoin and Game.com
                 'IQ' => 'IQ.Cash',
                 'PUT' => 'PutinCoin',
                 'SBTC' => 'SBTCT', // SiamBitcoin
