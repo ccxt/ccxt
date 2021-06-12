a101b07bc92bf755fc4ba40434c16a9b51bc6d26
Carlo Revelli <carlo.revelli@berkeley.edu>
2021-06-12 13:24:30 +0100

    working links

--- doc/_static/javascript/index.js
+++ doc/_static/javascript/index.js
@@ -1,9 +1,35 @@
 window.addEventListener ("load", () => {
-    const $links = $('.toctree-l1');
+    const $links = $('ul > .toctree-l1');
     const $sublinks = $('.toctree-l2')
+    const $allLinks = $('ul > .toctree-l1,.toctree-l2')
     const $sections = $('.section')
-    const $sidebar = $('.wy-side-scroll')
-
+    const $menu = $('.wy-menu')
+    // change the structure of the captions so the sliding looks better
+    let lastP = null
+    for (const child of $menu.children ()) {
+        console.log (child, child.nodeName)
+        if (child.nodeName === 'P') {
+            lastP = child
+        } else if (lastP !== null) {
+            const li = document.createElement ('li')
+            li.classList.add ('toctree-l1')
+            li.append (lastP)
+            child.prepend (li)
+            lastP = null
+        }
+    }
+    // set the height values for the sticky css property
+    const $linkGroups = $links.parents ('ul')
+    const heights = {}
+    $linkGroups.each (function () {
+        const $sublinks = $(this).find ('li.toctree-l1')
+        let height = 1
+        for (const link of $sublinks) {
+            const $link = $(link)
+            heights[$link.children ().first ().attr ('href')] = -Math.ceil (height)
+            height += $link.innerHeight ()
+        }
+    })
     // link the sidebar links and the sections
     const $topLinks = $links.find ('a.reference.internal[href="#"]')
     $topLinks.each (function () {
@@ -12,21 +38,22 @@ window.addEventListener ("load", () => {
     })
     const linksBySectionId = {}
     $sections.each (function () {
-        linksBySectionId[this.id] = $links.find ('a.reference.internal[href="#' + this.id + '"]').parent ().filter ('li')
+        linksBySectionId[this.id] = $allLinks.find ('a.reference.internal[href="#' + this.id + '"]').parent ().filter ('li')
     })
-    console.log (linksBySectionId)
     let lock = null
     let prevLock = null
     let last = null
     let lastLock = null
+    let $current = null
     function open () {
         if (lock === null && prevLock !== this) {
-            if (this.classList.contains ('toctree-l1')) {
+            $current = $(this)
+            if ($current.hasClass ('toctree-l1')) {
                 lock = this
                 prevLock = lock
                 $links.removeClass ('current')
-                this.classList.remove ('hidden')
-                $(this).addClass ('current', 400, 'linear', () => {
+                $current.removeClass ('hidden')
+                $current.addClass ('current', 400, 'linear', () => {
                     lock = null
                     if (last !== null && lastLock === null) {
                         lastLock = last
@@ -36,56 +63,27 @@ window.addEventListener ("load", () => {
                         }, 400)
                     }
                 })
+                // console.log ('setting height to ', heights[$current.children ().first ().attr ('href')])
+                $current.parent ().css ('top', heights[$current.children ().first ().attr ('href')])
             } else {
                 $sublinks.removeClass ('current')
-                this.classList.add ('current')
+                $current.addClass ('current')
             }
         } else {
             last = this
         }
     }
-    //$links.on ('mouseover', open)
+    // $links.on ('mouseover', open)
     window.addEventListener ('scroll', () => {
-        const fromTop = window.scrollY + window.innerHeight *
+        const fromTop = window.scrollY + window.innerHeight * 0.5
         $sections.each (function () {
             if (this.offsetTop <= fromTop && this.offsetTop + this.offsetHeight > fromTop) {
                 const sidelink = linksBySectionId[this.id]
                 if (sidelink.length) {
                     const element = sidelink[0]
                     open.call (element)
-                    const $current = $('.toctree-l1.current')
-                    if ($current.length === 0) {
-                        return
-                    }
-                    const boundingBox = $current[0].getBoundingClientRect ();
-                    const $siblings = $current.siblings ()
-                    const $outerbox = $('ul.current')
-                    console.log ('outside', boundingBox.y)
-                    if (boundingBox.y <= -1) {
-                        console.log ('here', boundingBox.y)
-                        $siblings.addClass ('hidden')
-                        $outerbox.addClass ('sticker')
-                        if ($current[0].getBoundingClientRect ().top !== -1) {
-                            console.log ('scrolling', $current[0].getBoundingClientRect ().top)
-                            $current[0].scrollIntoView (true)
-                            $sidebar[0].scrollBy (0, $current[0].getBoundingClientRect ().top + 5)
-                            console.log ('scrolled', $current[0].getBoundingClientRect ().top)
-                        }
-                    } else if ($siblings.hasClass ('hidden')) {
-                        console.log ('there', boundingBox.y)
-                        $outerbox.removeClass ('sticker')
-                        $siblings.removeClass ('hidden')
-                        $current[0].scrollIntoView (true)
-                    } else if ($current.hasClass ('hidden')) {
-                        $current.removeClass ('hidden')
-                    }
                 }
            }
         })
     })
 });
-
-var $outerbox = $('ul.current')
-var $current = $('.toctree-l1.current')
-var $sidebar = $('.wy-side-scroll')
-var $siblings = $current.siblings ()
