1e86e851b0a18a37df9c6e3f978ff78626509da7
Travis CI <travis@travis-ci.org>
2021-06-14 14:36:33 +0000

    1.51.43
    [ci skip]

--- python/ccxt/bitget.py
+++ python/ccxt/bitget.py
@@ -164,6 +164,11 @@ class bitget(Exchange):
                         'trace/closeTrack',
                         'trace/currentTrack',
                         'trace/historyTrack',
+                        'trace/summary',
+                        'trace/profitSettleTokenIdGroup',
+                        'trace/profitDateGroupList',
+                        'trace/profitDateList',
+                        'trace/waitProfitDateList',
                     ],
                     'post': [
                         'account/leverage',
