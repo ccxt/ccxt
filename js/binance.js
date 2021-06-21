2cc78338071eb9849778581cf03c5e9f69d41e70
Igor Kroitor <igor.kroitor@gmail.com>
2021-06-21 07:06:59 +0300

    binance 20% trading fee discount for ccxt users

--- js/binance.js
+++ js/binance.js
@@ -106,8 +106,8 @@ module.exports = class binance extends Exchange {
                 },
                 'www': 'https://www.binance.com',
                 'referral': {
-                    'url': 'https://www.binance.com/en/register?ref=D7YA7CLY',
-                    'discount': 0.1,
+                    'url': 'https://www.binance.com/en/register?ref=BLEJC98C',
+                    'discount': 0.2,
                 },
                 'doc': [
                     'https://binance-docs.github.io/apidocs/spot/en',
