390384691a026927198f98036e2043abaa8167b6
Igor Kroitor <igor.kroitor@gmail.com>
2021-06-21 05:19:43 +0300

    zb linting

--- js/zb.js
+++ js/zb.js
@@ -146,7 +146,7 @@ module.exports = class zb extends Exchange {
                 'referral': {
                     'url': 'https://www.zbex.club/en/register?ref=1625gesp',
                     'discount': 0.06,
-                }
+                },
             },
             'api': {
                 'trade': {
