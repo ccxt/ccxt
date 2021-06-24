bc3b821e9541dc91717663e4a029ed03ba50b99b
Carlo Revelli <carlo.revelli@berkeley.edu>
2021-06-24 18:24:31 +0100

    edit number_to_string

--- python/ccxt/base/decimal_to_precision.py
+++ python/ccxt/base/decimal_to_precision.py
@@ -164,6 +164,4 @@ def decimal_to_precision(n, rounding_mode=ROUND, precision=None, counting_mode=D
 
 def number_to_string(x):
     # avoids scientific notation for too large and too small numbers
-    d = decimal.Decimal(str(x))
-    formatted = '{:f}'.format(d)
-    return formatted.rstrip('0').rstrip('.') if '.' in formatted else formatted
+    return '{:.16f}'.format(x).rstrip('0')
