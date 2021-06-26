fa2a4e9c7a5f88c71593e675ab842593f4745f4a
Igor Kroitor <igor.kroitor@gmail.com>
2021-06-26 13:52:47 +0300

    wavesexchange edit

--- js/wavesexchange.js
+++ js/wavesexchange.js
@@ -15,7 +15,6 @@ module.exports = class wavesexchange extends Exchange {
             'name': 'Waves.Exchange',
             'countries': ['CH'], // Switzerland
             'rateLimit': 500,
-            'certified': true,
             'pro': false,
             'has': {
                 'cancelOrder': true,
