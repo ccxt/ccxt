0cfaeff0ff674efa030fb6850c81790980b69dfd
Igor Kroitor <igor.kroitor@gmail.com>
2021-07-06 22:17:53 +0300

    bigone implodeHostname

--- js/bigone.js
+++ js/bigone.js
@@ -960,7 +960,7 @@ module.exports = class bigone extends Exchange {
 
     sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
         const query = this.omit (params, this.extractParams (path));
-        const baseUrl = this.implodeParams (this.urls['api'][api], { 'hostname': this.hostname });
+        const baseUrl = this.implodeHostname (this.urls['api'][api]);
         let url = baseUrl + '/' + this.implodeParams (path, params);
         if (api === 'public') {
             if (Object.keys (query).length) {
