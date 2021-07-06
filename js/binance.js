2bbf5b144698cb2a384bd90b9242a455c1087035
Lester Kim <lester.c.kim@gmail.com>
2021-07-06 14:39:58 -0400

    feat(binance.js): Add dust log and sub-account SAPI endpoints

--- js/binance.js
+++ js/binance.js
@@ -128,6 +128,7 @@ module.exports = class binance extends Exchange {
                         'margin/priceIndex',
                         // these endpoints require this.apiKey + this.secret
                         'asset/assetDividend',
+                        'asset/dribblet',
                         'asset/transfer',
                         'asset/assetDetail',
                         'asset/tradeFee',
@@ -172,10 +173,12 @@ module.exports = class binance extends Exchange {
                         'sub-account/futures/accountSummary',
                         'sub-account/futures/positionRisk',
                         'sub-account/futures/internalTransfer',
+                        'sub-account/list',
                         'sub-account/margin/account',
                         'sub-account/margin/accountSummary',
                         'sub-account/spotSummary',
                         'sub-account/status',
+                        'sub-account/sub/transfer/history',
                         'sub-account/transfer/subUserHistory',
                         'sub-account/universalTransfer',
                         // lending endpoints
