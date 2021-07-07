582a58f0e3dbe0ed13c726b413cd1248cf30c5c2
Igor Kroitor <igor.kroitor@gmail.com>
2021-07-07 03:04:43 +0300

    bw implodeHostname

--- js/bw.js
+++ js/bw.js
@@ -935,7 +935,7 @@ module.exports = class bw extends Exchange {
     }
 
     sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
-        let url = this.implodeParams (this.urls['api'], { 'hostname': this.hostname }) + '/' + path;
+        let url = this.implodeHostname (this.urls['api']) + '/' + path;
         if (method === 'GET') {
             if (Object.keys (params).length) {
                 url += '?' + this.urlencode (params);
