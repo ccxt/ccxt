5147682266597a29fdb2086a679d2ad7baa48a17
Stolen <stolenqwerty@gmail.com>
2021-07-21 17:18:17 +0300

    export coinflex

--- ccxt.js
+++ ccxt.js
@@ -27,9 +27,6 @@ SOFTWARE.
 */
 
 //-----------------------------------------------------------------------------
-
-import {coinflex} from "ccxt";
-
 const Exchange  = require ('./js/base/Exchange')
     , functions = require ('./js/base/functions')
     , errors    = require ('./js/base/errors')
@@ -97,7 +94,7 @@ const exchanges = {
     'coinegg':                 require ('./js/coinegg.js'),
     'coinex':                  require ('./js/coinex.js'),
     'coinfalcon':              require ('./js/coinfalcon.js'),
-    'coinflex':              require ('./js/coinflex.js'),
+    'coinflex':                require ('./js/coinflex.js'),
     'coinfloor':               require ('./js/coinfloor.js'),
     'coingi':                  require ('./js/coingi.js'),
     'coinmarketcap':           require ('./js/coinmarketcap.js'),
