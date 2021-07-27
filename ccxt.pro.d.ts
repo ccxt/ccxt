c4edc9b5ddba275a93b8e4908240d18b3b86efa4
Travis CI <travis@travis-ci.com>
2021-07-27 17:12:58 +0000

    0.7.85
    [ci skip]

--- ccxt.pro.d.ts
+++ ccxt.pro.d.ts
@@ -59,7 +59,7 @@ declare module 'ccxt.pro' {
     export class ndax extends Exchange {}
     export class okcoin extends okex3 {}
     export class okex extends Exchange {}
-    export class okex3 extends Exchange {}
+    export class okex3 extends Exchange3 {}
     export class phemex extends Exchange {}
     export class poloniex extends Exchange {}
     export class ripio extends Exchange {}
