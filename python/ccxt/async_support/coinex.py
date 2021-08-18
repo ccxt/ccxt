d796cf305bc6303d425b372d20d463703edc0eb1
Travis CI <travis@travis-ci.org>
2021-08-18 13:22:26 +0000

    1.55.15
    [ci skip]

--- python/ccxt/async_support/coinex.py
+++ python/ccxt/async_support/coinex.py
@@ -198,6 +198,9 @@ class coinex(Exchange):
             'options': {
                 'createMarketBuyOrderRequiresPrice': True,
             },
+            'commonCurrencies': {
+                'ACM': 'Actinium',
+            },
         })
 
     async def fetch_markets(self, params={}):
