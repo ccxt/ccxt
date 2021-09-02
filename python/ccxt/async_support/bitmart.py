ea4bf62b9aa42c8a0d29dfa0e91040b06ddd81e7
Travis CI <travis@travis-ci.org>
2021-09-02 15:43:07 +0000

    1.55.65
    [ci skip]

--- python/ccxt/async_support/bitmart.py
+++ python/ccxt/async_support/bitmart.py
@@ -76,10 +76,7 @@ class bitmart(Exchange):
                 'api': 'https://api-cloud.{hostname}',  # bitmart.info for Hong Kong users
                 'www': 'https://www.bitmart.com/',
                 'doc': 'https://developer-pro.bitmart.com/',
-                'referral': {
-                    'url': 'http://www.bitmart.com/?r=rQCFLh',
-                    'discount': 0.25,
-                },
+                'referral': 'http://www.bitmart.com/?r=rQCFLh',
                 'fees': 'https://www.bitmart.com/fee/en',
             },
             'requiredCredentials': {
