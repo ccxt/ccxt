3d5108c202855c833034a5862f9f561c07a9bbc8
Igor Kroitor <igor.kroitor@gmail.com>
2021-09-04 22:16:45 +0300

    examples/py/phemex-cancel-all-orders.py

---
+++ examples/py/phemex-cancel-all-orders.py
@@ -0,0 +1,24 @@
+import ccxtpro
+from asyncio import get_event_loop
+from pprint import pprint
+
+print('CCXT Version:', ccxt.__version__)
+
+async def main():
+    exchange = ccxtpro.phemex({
+        'apiKey': 'YOUR_API_KEY',
+        'secret': 'YOUR_SECRET',
+    })
+    markets = await exchange.load_markets()
+    # exchange.verbose = True  # uncomment for debugging purposes if necessary
+    try:
+        order_id = 'YOUR_ORDER_ID_HERE'
+        symbol = 'UNI/USDT'
+        response = await exchange.cancel_all_orders(order_id, symbol)
+        pprint(response)
+    except Exception as e:
+        print(type(e).__name__, str(e))
+    await exchange.close()
+
+loop = get_event_loop()
+loop.run_until_complete(main())
