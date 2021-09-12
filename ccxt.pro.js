b82e1c1c35e31085890c094d1004c0240ad5c25d
Travis CI <travis@travis-ci.com>
2021-09-12 07:52:25 +0000

    0.8.54
    [ci skip]

--- ccxt.pro.js
+++ ccxt.pro.js
@@ -11,7 +11,7 @@ const ccxt = require ('ccxt')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '0.8.53'
+const version = '0.8.54'
 
 // Exchange.ccxtVersion = version
 
