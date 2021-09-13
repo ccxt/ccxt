22cf4742d616aed54168f8fc3eda54dfa318e08b
Igor Kroitor <igor.kroitor@gmail.com>
2021-09-13 10:38:59 +0300

    examples/js/binance-margin-stop-order.js #9971

---
+++ examples/js/binance-margin-stop-order.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const ccxt = require ('../../ccxt.js')
+
+console.log ('CCXT Version:', ccxt.version)
+
+async function main () {
+
+    const exchange = new ccxt.binance({
+            'apiKey': 'YOUR_API_KEY',
+            'secret': 'YOUR_SECRET',
+            'options': {
+                'defaultType': 'margin',
+            },
+        })
+
+        , markets = await exchange.loadMarkets ()
+
+    // exchange.verbose = true // uncomment for debugging purposes if necessary
+
+        , symbol = 'BTC/USDT'
+        , type = 'limit'
+        , side = 'buy'
+        , amount = YOUR_AMOUNT_HERE
+        , price = YOUR_LIMIT_PRICE_HERE
+        , params = {
+            'stopPrice': YOUR_STOP_PRICE_HERE,
+        }
+        , order = await exchange.createOrder (symbol, type, side, amount, price, params)
+
+    console.log (order)
+}
+
+main ()
