1f4801f3f1d70cb6c5a133cd4679e87138f0a6bc
Igor Kroitor <igor.kroitor@gmail.com>
2021-09-13 10:45:04 +0300

    examples/js/binance-margin-stop-order.js #9971

--- examples/js/binance-margin-stop-order.js
+++ examples/js/binance-margin-stop-order.js
@@ -7,32 +7,32 @@ console.log ('CCXT Version:', ccxt.version)
 async function main () {
 
     const exchange = new ccxt.binance({
-            'apiKey': 'YOUR_API_KEY',
-            'secret': 'YOUR_SECRET',
-            'options': {
-                'defaultType': 'margin',
-            },
-        })
+        'apiKey': 'YOUR_API_KEY',
+        'secret': 'YOUR_API_SECRET',
+        'options': {
+            'defaultType': 'margin',
+        },
+    })
 
-        , markets = await exchange.loadMarkets ()
+    const markets = await exchange.loadMarkets ()
 
-        // exchange.verbose = true // uncomment for debugging purposes if necessary
+    exchange.verbose = true // uncomment for debugging purposes if necessary
 
-        , symbol = 'BTC/USDT'
-        , type = 'limit'
-        , side = 'buy'
-        , amount = YOUR_AMOUNT_HERE
-        , price = YOUR_LIMIT_PRICE_HERE
-        , params = {
-            'stopPrice': YOUR_STOP_PRICE_HERE,
-        }
+    const symbol = 'BTC/USDT'
+    const type = 'limit'
+    const side = 'buy'
+    const amount = YOUR_AMOUNT_HERE
+    const price = YOUR_PRICE_HERE
+    const params = {
+        'stopPrice': YOUR_STOP_PRICE_HERE,
+    }
 
-        try {
-            const order = await exchange.createOrder (symbol, type, side, amount, price, params)
-            console.log (order)
-        } catch (e) {
-            console.log (e.constructor.name, e.message)
-        }
+    try {
+        const order = await exchange.createOrder (symbol, type, side, amount, price, params)
+        console.log (order)
+    } catch (e) {
+        console.log (e.constructor.name, e.message)
+    }
 }
 
 main ()
