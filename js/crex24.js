1f3f10626e1dc73f378a475cf0090fb812f16c17
ndubel <38309641+ndubel@users.noreply.github.com>
2021-09-17 17:13:10 +0300

    crex24 CLC -> CaluraCoin
    https://www.coingecko.com/en/coins/caluracoin#markets
    conflict with https://www.coingecko.com/en/coins/cifculation#markets

--- js/crex24.js
+++ js/crex24.js
@@ -135,6 +135,7 @@ module.exports = class crex24 extends Exchange {
                 'BCC': 'BCH',
                 'BIT': 'BitMoney',
                 'BULL': 'BuySell',
+                'CLC': 'CaluraCoin',
                 'CREDIT': 'TerraCredit',
                 'EPS': 'Epanus',  // conflict with EPS Ellipsis https://github.com/ccxt/ccxt/issues/8909
                 'FUND': 'FUNDChains',
