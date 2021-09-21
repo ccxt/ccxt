a7cc029b921bb6bd119a678e9cf2a47b4aa38917
Guilherme camacho <guilherme.camacho@financialhub.dev>
2021-09-21 17:21:00 -0300

    Revert "bitmart implement withdraw with multiple networks"
    This reverts commit ebd552025d1dd0e376f5f535efe573725530bd9c.

--- js/bitmart.js
+++ js/bitmart.js
@@ -283,13 +283,6 @@ module.exports = class bitmart extends Exchange {
                 'TCT': 'TacoCat Token',
             },
             'options': {
-                'networks': {
-                    'TRC20': 'TRC20',
-                    'ERC20': 'ERC20',
-                    'BEP20': 'BEP20',
-                    'BEP2': 'BEP2',
-                    'SOL': 'SOL',
-                },
                 'defaultType': 'spot', // 'spot', 'swap'
                 'fetchBalance': {
                     'type': 'spot', // 'spot', 'swap', 'contract', 'account'
@@ -2104,13 +2097,6 @@ module.exports = class bitmart extends Exchange {
         };
         if (tag !== undefined) {
             request['address_memo'] = tag;
-        }
-        const networks = this.safeValue (this.options, 'networks', {});
-        let network = this.safeString (params, 'network'); // this line allows the user to specify either ERC20 or ETH
-        network = this.safeString (networks, network, network); // handle ERC20>ETH alias
-        if (network !== undefined) {
-            request['currency'] += '-' + network; // when network the currency need to be changed to currency + '-' + network https://developer-pro.bitmart.com/en/account/withdraw_apply.html on the end of page
-            params = this.omit (params, 'network');
         }
         const response = await this.privateAccountPostWithdrawApply (this.extend (request, params));
         //
