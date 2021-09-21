16b9d11e469366c0c3fc0c2e363a45ba1ad8d4a0
Carlo Revelli <carlo.revelli@berkeley.edu>
2021-09-21 12:59:14 -0700

    fix lint

--- js/bibox.js
+++ js/bibox.js
@@ -994,7 +994,7 @@ module.exports = class bibox extends Exchange {
     }
 
     async withdraw (code, amount, address, tag = undefined, params = {}) {
-        [ tag, params ] = this.handleTagAndParams (tag, params);        
+        [ tag, params ] = this.handleTagAndParams (tag, params);
         this.checkAddress (address);
         await this.loadMarkets ();
         const currency = this.currency (code);
