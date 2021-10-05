26fd8629e951ad7f10569f6bc81c4914006c533f
ndubel <38309641+ndubel@users.noreply.github.com>
2021-10-05 14:49:22 +0300

    probit GOL, GOGOL mapping
    https://support.probit.com/hc/en-us/articles/360035318892-Goldofir-GOL-Trading-Competition-
    conflict with https://www.coingecko.com/en/coins/gogolcoin#markets

--- js/probit.js
+++ js/probit.js
@@ -156,6 +156,8 @@ module.exports = class probit extends Exchange {
                 'BTCBULL': 'BULL',
                 'CBC': 'CryptoBharatCoin',
                 'EPS': 'Epanus',  // conflict with EPS Ellipsis https://github.com/ccxt/ccxt/issues/8909
+                'GOGOL': 'GOL',
+                'GOL': 'Goldofir',
                 'GRB': 'Global Reward Bank',
                 'HBC': 'Hybrid Bank Cash',
                 'ORC': 'Oracle System',
