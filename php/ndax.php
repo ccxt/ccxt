c6c0a5e5e677472cb9b92774ec749e0e8d04541d
Travis CI <travis@travis-ci.org>
2021-10-10 13:56:03 +0000

    1.57.85
    [ci skip]

--- php/ndax.php
+++ php/ndax.php
@@ -193,9 +193,9 @@ class ndax extends Exchange {
                 'secret' => true,
                 'uid' => true,
                 // these credentials are required for signIn() and withdraw()
-                // 'login' => true,
-                // 'password' => true,
-                // 'twofa' => true,
+                'login' => true,
+                'password' => true,
+                'twofa' => true,
             ),
             'precisionMode' => TICK_SIZE,
             'exceptions' => array(
