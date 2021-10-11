562c9c2910f32fce96b6b1ee5fc09b7441daf126
Igor Kroitor <igor.kroitor@gmail.com>
2021-10-11 06:15:36 +0300

    idex, gateio indexOf fix

--- js/gateio.js
+++ js/gateio.js
@@ -608,7 +608,7 @@ module.exports = class gateio extends Exchange {
         const addressField = this.safeString (response, 'address');
         let tag = undefined;
         let address = undefined;
-        if (addressField.indexOf (' ') > -1) {
+        if (addressField.indexOf (' ') >= 0) {
             const splitted = addressField.split (' ');
             address = splitted[0];
             tag = splitted[1];
