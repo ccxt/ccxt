f29faca7823214c0f56bfd565df2ba53a37f860d
Pietro De Nicolao <pd@bendingspoons.com>
2021-10-16 00:06:24 +0200

    fix: drop useless variable

--- js/okex.js
+++ js/okex.js
@@ -1636,8 +1636,7 @@ module.exports = class okex extends Exchange {
         let cost = undefined;
         // spot market buy: "sz" can refer either to base currency units or to quote currency units
         // see documentation: https://www.okex.com/docs-v5/en/#rest-api-trade-place-order
-        const defaultTgtCcy = this.safeString (this.options, 'tgtCcy', 'base_ccy');
-        const tgtCcy = this.safeString (order, 'tgtCcy', defaultTgtCcy);
+        const tgtCcy = this.safeString (order, 'tgtCcy', 'base_ccy');
         if (side === 'buy' && type === 'market' && market['type'] === 'spot' && tgtCcy === 'quote_ccy') {
             // "sz" refers to the cost
             cost = this.safeNumber (order, 'sz');
