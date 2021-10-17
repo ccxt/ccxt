c99c6b443b9f130a57015592c0272adb3414502b
Igor Kroitor <igor.kroitor@gmail.com>
2021-10-17 06:49:34 +0300

    coinfalcon market type

--- js/coinfalcon.js
+++ js/coinfalcon.js
@@ -108,6 +108,8 @@ module.exports = class coinfalcon extends Exchange {
                 'quote': quote,
                 'baseId': baseId,
                 'quoteId': quoteId,
+                'type': 'spot',
+                'spot': true,
                 'active': true,
                 'precision': precision,
                 'limits': {
