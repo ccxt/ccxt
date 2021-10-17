1b371892517b2f853b21a0a193b3f1c72065ab10
Igor Kroitor <igor.kroitor@gmail.com>
2021-10-17 05:41:46 +0300

    bitforex market type

--- js/bitforex.js
+++ js/bitforex.js
@@ -154,6 +154,8 @@ module.exports = class bitforex extends Exchange {
                 'quote': quote,
                 'baseId': baseId,
                 'quoteId': quoteId,
+                'type': 'spot',
+                'spot': true,
                 'active': active,
                 'precision': precision,
                 'limits': limits,
