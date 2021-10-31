79d2a0dabcd8194995b416826f33e6eda4f0cff8
Igor Kroitor <igor.kroitor@gmail.com>
2021-10-31 08:57:32 +0300

    ccxt 1.59.72

--- python/setup.py
+++ python/setup.py
@@ -77,7 +77,7 @@ setup(
         ':python_version>="3.5.2"': [
             'aiohttp>=3.0.1',
             'aiodns>=1.1.1',
-            'ccxt>=1.59.61',
+            'ccxt>=1.59.72',
             'yarl==1.6.3',
         ],
         'qa': [
