ac5f739a23eebca9108768a7c30e6ace03e47662
Igor Kroitor <igor.kroitor@gmail.com>
2021-11-01 14:03:22 +0300

    okex reflink fix

--- js/okex.js
+++ js/okex.js
@@ -77,7 +77,7 @@ module.exports = class okex extends Exchange {
                 'www': 'https://www.okex.com',
                 'doc': 'https://www.okex.com/docs-v5/en/',
                 'fees': 'https://www.okex.com/pages/products/fees.html',
-                'referral': 'https://www.okex.com/join/1888677',
+                // 'referral': 'https://www.okex.com/join/1888677',
                 'test': {
                     'rest': 'https://testnet.okex.com',
                 },
