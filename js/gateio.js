86339e3aa7f66cd55433f16325c242f61fd9db70
Sam Germain <sam.germain@usask.ca>
2021-11-05 03:06:15 -0600

    Fixed parse order gateio

--- js/gateio.js
+++ js/gateio.js
@@ -2099,7 +2099,7 @@ module.exports = class gateio extends Exchange {
         const remaining = this.safeString (order, 'left');
         const cost = this.safeString2 (order, 'filled_total'); // same as filled_price
         let side = this.safeString (order, 'side');
-        if (market['contract']) {
+        if (market && 'contract' in market) {
             side = amount > 0 ? 'buy' : 'sell';
         }
         const type = this.safeString (order, 'type');
