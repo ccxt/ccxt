8e47fbdde5caedef182a4bcb8dc507a1c50497b7
Igor Kroitor <igor.kroitor@gmail.com>
2021-11-08 10:53:24 +0300

    lykke typo

--- js/lykke.js
+++ js/lykke.js
@@ -492,7 +492,7 @@ module.exports = class lykke extends Exchange {
         }
         const remaining = Precise.stringAbs (this.safeString (order, 'RemainingVolume'));
         const id = this.safeString (order, 'Id');
-        return this., marketsafeOrder2 ({
+        return this.safeOrder2 ({
             'info': order,
             'id': id,
             'clientOrderId': undefined,
