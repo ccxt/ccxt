2962c21ea5d333a24f3a6f3902a510e953c69402
Igor Kroitor <igor.kroitor@gmail.com>
2021-11-12 19:12:47 +0300

    ftxus added to ccxt.pro

---
+++ js/ftxus.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//  ---------------------------------------------------------------------------
+
+const ftx = require ('./ftx.js');
+
+// ---------------------------------------------------------------------------
+
+module.exports = class ftxus extends ftx {
+    describe () {
+        return this.deepExtend (super.describe (), {
+            'id': 'ftxus',
+            'name': 'FTXUS',
+            'countries': ['US'],
+            'hostname': 'ftx.us',
+            'urls': {
+                'www': 'https://ftx.us/',
+                'docs': 'https://docs.ftx.us/',
+                'fees': 'https://help.ftx.us/hc/en-us/articles/360043579273-Fees',
+            },
+        });
+    }
+};
