74dc3ccbc4ca1967661be4186278263f99beb24f
Travis CI <travis@travis-ci.org>
2021-11-12 18:53:52 +0000

    1.61.8
    [ci skip]

--- php/async/ftxus.php
+++ php/async/ftxus.php
@@ -14,6 +14,7 @@ class ftxus extends ftx {
             'id' => 'ftxus',
             'name' => 'FTX US',
             'countries' => ['US'],
+            'certified' => false,
             'hostname' => 'ftx.us',
             'urls' => array(
                 'logo' => 'https://user-images.githubusercontent.com/1294454/141506670-12f6115f-f425-4cd8-b892-b51d157ca01f.jpg',
--- python/ccxt/ftxus.py
+++ python/ccxt/ftxus.py
@@ -13,6 +13,7 @@ class ftxus(ftx):
             'id': 'ftxus',
             'name': 'FTX US',
             'countries': ['US'],
+            'certified': False,
             'hostname': 'ftx.us',
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/141506670-12f6115f-f425-4cd8-b892-b51d157ca01f.jpg',
