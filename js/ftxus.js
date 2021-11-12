1162772fa9bd04453b77137582b4158d391bca40
Carlo Revelli <carlo.revelli@berkeley.edu>
2021-11-12 10:30:59 -0800

    uncertify-ftxus

--- js/ftxus.js
+++ js/ftxus.js
@@ -10,6 +10,7 @@ module.exports = class ftxus extends ftx {
             'id': 'ftxus',
             'name': 'FTX US',
             'countries': ['US'],
+            'certified': false,
             'hostname': 'ftx.us',
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/141506670-12f6115f-f425-4cd8-b892-b51d157ca01f.jpg',
