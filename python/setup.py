5f7eee45bdb11ee43f4818c6d595e2b6f6063ba3
Igor Kroitor <igor.kroitor@gmail.com>
2021-11-12 04:58:41 +0300

    python/setup.py fixup

--- python/setup.py
+++ python/setup.py
@@ -77,7 +77,7 @@ setup(
         ':python_version>="3.5.2"': [
             'aiohttp>=3.0.1',
             'aiodns>=1.1.1',
-            'ccxt>=1.64.1',
+            'ccxt>=1.61.4',
             'yarl==1.6.3',
         ],
         'qa': [
