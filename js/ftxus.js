7bb0d4842efdf3dcf63ef326f07c0e050f36d301
Igor Kroitor <igor.kroitor@gmail.com>
2021-11-12 20:55:22 +0300

    ftxus minor edits

--- js/ftxus.js
+++ js/ftxus.js
@@ -10,10 +10,11 @@ module.exports = class ftxus extends ftx {
     describe () {
         return this.deepExtend (super.describe (), {
             'id': 'ftxus',
-            'name': 'FTXUS',
+            'name': 'FTX.us',
             'countries': ['US'],
             'hostname': 'ftx.us',
             'urls': {
+                'logo': 'https://user-images.githubusercontent.com/1294454/141506670-12f6115f-f425-4cd8-b892-b51d157ca01f.jpg',
                 'www': 'https://ftx.us/',
                 'docs': 'https://docs.ftx.us/',
                 'fees': 'https://help.ftx.us/hc/en-us/articles/360043579273-Fees',
