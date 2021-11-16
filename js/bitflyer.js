34b4447373b72b90de969cadf66f4c3a3a1a1181
Doctor Slimm <l.korganas@gmail.com>
2021-11-16 10:25:56 +0000

    save

--- js/bitflyer.js
+++ js/bitflyer.js
@@ -4,7 +4,6 @@
 
 const Exchange = require ('./base/Exchange');
 const { ExchangeError, ArgumentsRequired, OrderNotFound } = require ('./base/errors');
-const Precise = require ('./base/Precise');
 
 //  ---------------------------------------------------------------------------
 
