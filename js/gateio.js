869c89b51f16596641aeabc7d4b7bbce34c32b6b
ndubel <38309641+ndubel@users.noreply.github.com>
2021-11-22 17:07:27 +0300

    gateio STX remove mapping
    https://www.coingecko.com/en/coins/stacks#markets was listed 
    and old STX renamed to STOX https://www.coingecko.com/en/coins/stox#markets

--- js/gateio.js
+++ js/gateio.js
@@ -279,7 +279,6 @@ module.exports = class gateio extends Exchange {
                 'MPH': 'Morpher', // conflict with 88MPH
                 'RAI': 'Rai Reflex Index', // conflict with RAI Finance
                 'SBTC': 'Super Bitcoin',
-                'STX': 'Stox',
                 'TNC': 'Trinity Network Credit',
                 'TON': 'TONToken',
                 'VAI': 'VAIOT',
