512caffc9116a48111fac799099b39db84131c64
Igor Kroitor <igor.kroitor@gmail.com>
2021-11-30 19:48:16 +0300

    test.ticker.js btcturk bid greater than ask

--- js/test/Exchange/test.ticker.js
+++ js/test/Exchange/test.ticker.js
@@ -72,21 +72,22 @@ module.exports = (exchange, ticker, method, symbol) => {
 
     if (![
 
+        'bigone',
+        'bitmart',
+        'btcturk',
         'bybit',
         'coinmarketcap',
-        'xbtce',
         'coss',
+        'ftx',
+        'gateio', // some ticker bids are greaters than asks
         'idex',
         'mercado',
         'okex',
-        'southxchange', // https://user-images.githubusercontent.com/1294454/59953532-314bea80-9489-11e9-85b3-2a711ca49aa7.png
-        'bitmart',
-        'ftx',
-        'gateio', // some ticker bids are greaters than asks
-        'timex',
         'poloniex',
         'qtrade',
-        'bigone',
+        'southxchange', // https://user-images.githubusercontent.com/1294454/59953532-314bea80-9489-11e9-85b3-2a711ca49aa7.png
+        'timex',
+        'xbtce',
 
     ].includes (exchange.id)) {
 
