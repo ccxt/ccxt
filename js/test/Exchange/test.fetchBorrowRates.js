e2b8b90e5abdb26041a6a3a03f7419caaba12129
Igor Kroitor <igor.kroitor@gmail.com>
2021-12-01 07:12:51 +0300

    test.fetchBorrowRates assert/chai/log cleanup

--- js/test/Exchange/test.fetchBorrowRates.js
+++ js/test/Exchange/test.fetchBorrowRates.js
@@ -2,42 +2,27 @@
 
 // ----------------------------------------------------------------------------
 
-const log       = require ('ololog')
-    , ansi      = require ('ansicolor').nice
-    , chai      = require ('chai')
-    , expect    = chai.expect
-    , assert    = chai.assert
-    , testBorrowRate = require ('./test.borrowRate.js')
+const testBorrowRate = require ('./test.borrowRate.js')
 
-/*  ------------------------------------------------------------------------ */
+// ----------------------------------------------------------------------------
 
 module.exports = async (exchange) => {
 
     if (exchange.has.fetchBorrowRates) {
 
-        // log ('fetching all BorrowRates at once...')
-
         const method = 'fetchBorrowRates'
-        let borrowRates = undefined
+        const borrowRates = await exchange[method] ()
+        console.log ('fetched all', Object.keys (borrowRates).length, 'borrow rates')
 
-        try {
-
-            borrowRates = await exchange[method] ()
-            log ('fetched all', Object.keys (borrowRates).length.toString ().green, 'borrow rates')
-
-        } catch (e) {
-
-            log ('failed to fetch all borrow rates, fetching multiple borrow rates at once...')
-            borrowRates = await exchange[method] ()
-            log ('fetched', Object.keys (borrowRates).length.toString ().green, 'borrow rates')
+        const values = Object.values (borrowRates)
+        for (let i = 0; i < values.length; i++) {
+            const borrowRate = values[i]
+            testBorrowRate (exchange, borrowRate, method)
         }
-
-        Object.values (borrowRates).forEach ((borrowRate) => testBorrowRate (exchange, borrowRate, method))
         return borrowRates
 
     } else {
 
-        log ('fetching all borrow rates at once not supported')
+        console.log ('fetching all borrow rates at once not supported')
     }
 }
-
