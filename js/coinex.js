fcd7f9cac827b110bb403baa5e08b703f58b028f
Doctor Slimm <l.korganas@gmail.com>
2021-12-10 15:32:00 +0000

    coinex add fetchBalance wrapper for fetchSpotBalance and fetchMarginBalance

--- js/coinex.js
+++ js/coinex.js
@@ -450,80 +450,77 @@ module.exports = class coinex extends Exchange {
         return this.parseOHLCVs (data, market, timeframe, since, limit);
     }
 
-    async fetchBalance (params = {}) {
-        await this.loadMarkets ();
-        const accountType = this.safeString (params, 'type', 'main');
-        params = this.omit (params, 'type');
-        if (accountType === 'margin') {
-            const symbol = this.safeString (params, 'market');
-            params = this.omit (params, 'market');
-            if (symbol === undefined) {
-                throw new ArgumentsRequired (this.id + 'fetching a margin account requires a market parameter');
-            }
-            const market = this.market (symbol);
-            const marketId = market['id'];
-            const request = {
-                'market': marketId,
-            };
-            const response = await this.privateGetMarginAccount (this.extend (request, params));
-            //
-            //      {
-            //          "code":    0,
-            //           "data": {
-            //              "account_id":    126,
-            //              "leverage":    3,
-            //              "market_type":   "AAVEUSDT",
-            //              "sell_asset_type":   "AAVE",
-            //              "buy_asset_type":   "USDT",
-            //              "balance": {
-            //                  "sell_type": "0.3",     // borrowed
-            //                  "buy_type": "30"
-            //                  },
-            //              "frozen": {
-            //                  "sell_type": "0",
-            //                  "buy_type": "0"
-            //                  },
-            //              "loan": {
-            //                  "sell_type": "0.3", // loan
-            //                  "buy_type": "0"
-            //                  },
-            //              "interest": {
-            //                  "sell_type": "0.0000125",
-            //                  "buy_type": "0"
-            //                  },
-            //              "can_transfer": {
-            //                  "sell_type": "0.02500646",
-            //                  "buy_type": "4.28635738"
-            //                  },
-            //              "warn_rate":   "",
-            //              "liquidation_price":   ""
-            //              },
-            //          "message": "Success"
-            //      }
-            //
-            const result = { 'info': response };
-            const data = this.safeValue (response, 'data', {});
-            const free = data['can_transfer'];
-            const total = data['balance'];
-            //
-            const sellAccount = this.account ();
-            const sellCurrencyId = this.safeString (data, 'sell_asset_type');
-            const sellCurrencyCode = this.safeCurrencyCode (sellCurrencyId);
-            sellAccount['free'] = free['sell_type'];
-            sellAccount['total'] = total['sell_type'];
-            sellAccount['used'] = undefined;
-            result[sellCurrencyCode] = sellAccount;
-            //
-            const buyAccount = this.account ();
-            const buyCurrencyId = this.safeString (data, 'buy_asset_type');
-            const buyCurrencyCode = this.safeCurrencyCode (buyCurrencyId);
-            buyAccount['free'] = free['buy_type'];
-            buyAccount['total'] = total['buy_type'];
-            buyAccount['used'] = undefined;
-            result[buyCurrencyCode] = buyAccount;
-            //
-            return this.parseBalance (result);
+    async fetchMarginBalance (params = {}) {
+        const symbol = this.safeString (params, 'market');
+        params = this.omit (params, 'market');
+        if (symbol === undefined) {
+            throw new ArgumentsRequired (this.id + ' fetching a margin account requires a market parameter');
         }
+        const market = this.market (symbol);
+        const marketId = market['id'];
+        const request = {
+            'market': marketId,
+        };
+        const response = await this.privateGetMarginAccount (this.extend (request, params));
+        //
+        //      {
+        //          "code":    0,
+        //           "data": {
+        //              "account_id":    126,
+        //              "leverage":    3,
+        //              "market_type":   "AAVEUSDT",
+        //              "sell_asset_type":   "AAVE",
+        //              "buy_asset_type":   "USDT",
+        //              "balance": {
+        //                  "sell_type": "0.3",     // borrowed
+        //                  "buy_type": "30"
+        //                  },
+        //              "frozen": {
+        //                  "sell_type": "0",
+        //                  "buy_type": "0"
+        //                  },
+        //              "loan": {
+        //                  "sell_type": "0.3", // loan
+        //                  "buy_type": "0"
+        //                  },
+        //              "interest": {
+        //                  "sell_type": "0.0000125",
+        //                  "buy_type": "0"
+        //                  },
+        //              "can_transfer": {
+        //                  "sell_type": "0.02500646",
+        //                  "buy_type": "4.28635738"
+        //                  },
+        //              "warn_rate":   "",
+        //              "liquidation_price":   ""
+        //              },
+        //          "message": "Success"
+        //      }
+        //
+        const result = { 'info': response };
+        const data = this.safeValue (response, 'data', {});
+        const free = data['can_transfer'];
+        const total = data['balance'];
+        //
+        const sellAccount = this.account ();
+        const sellCurrencyId = this.safeString (data, 'sell_asset_type');
+        const sellCurrencyCode = this.safeCurrencyCode (sellCurrencyId);
+        sellAccount['free'] = this.safeString (free, 'sell_type');
+        sellAccount['total'] = this.safeString (total, 'sell_type');
+        result[sellCurrencyCode] = sellAccount;
+        //
+        const buyAccount = this.account ();
+        const buyCurrencyId = this.safeString (data, 'buy_asset_type');
+        const buyCurrencyCode = this.safeCurrencyCode (buyCurrencyId);
+        buyAccount['free'] = this.safeString (free, 'buy_type');
+        buyAccount['total'] = this.safeString (total, 'buy_type');
+        result[buyCurrencyCode] = buyAccount;
+        //
+        return this.parseBalance (result);
+    }
+
+    async fetchSpotBalance (params = {}) {
+        await this.loadMarkets ();
         const response = await this.privateGetBalanceInfo (params);
         //
         //     {
@@ -558,6 +555,16 @@ module.exports = class coinex extends Exchange {
             result[code] = account;
         }
         return this.parseBalance (result);
+    }
+
+    async fetchBalance (params = {}) {
+        const accountType = this.safeString (params, 'type', 'main');
+        params = this.omit (params, 'type');
+        if (accountType === 'margin') {
+            return await this.fetchMarginBalance (params);
+        } else {
+            return await this.fetchSpotBalance (params);
+        }
     }
 
     parseOrderStatus (status) {
