b6e2b6d92d99fe3af99bc07554cfe3f021433c13
Doctor Slimm <l.korganas@gmail.com>
2021-12-10 16:21:07 +0000

    remove Precise import

--- js/crex24.js
+++ js/crex24.js
@@ -5,7 +5,6 @@
 const Exchange = require ('./base/Exchange');
 const { ExchangeError, BadRequest, InvalidNonce, RequestTimeout, ExchangeNotAvailable, InsufficientFunds, OrderNotFound, InvalidOrder, DDoSProtection, AuthenticationError, BadSymbol, AccountSuspended } = require ('./base/errors');
 const { TICK_SIZE } = require ('./base/functions/number');
-const Precise = require ('./base/Precise');
 
 //  ---------------------------------------------------------------------------
 
