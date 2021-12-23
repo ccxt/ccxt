5983acd36fb8d20cde8f0b8382d95fa8727fc362
Igor Kroitor <igor.kroitor@gmail.com>
2021-12-23 23:57:56 +0300

    gate MM > Million conflict with MilliMeter

--- js/gateio.js
+++ js/gateio.js
@@ -292,6 +292,7 @@ module.exports = class gateio extends Exchange {
                 'GTC_HT': 'Game.com HT',
                 'GTC_BSC': 'Game.com BSC',
                 'HIT': 'HitChain',
+                'MM': 'Million', // conflict with MilliMeter
                 'MPH': 'Morpher', // conflict with 88MPH
                 'RAI': 'Rai Reflex Index', // conflict with RAI Finance
                 'SBTC': 'Super Bitcoin',
