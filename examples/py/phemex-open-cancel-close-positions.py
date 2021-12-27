8399191ea09c278fba6a8f528bf6f03b4ec9e168
cjg <carlos.goncalves@outsystems.com>
2021-12-27 22:16:36 +0000

    add more examples

--- examples/py/phemex-open-cancel-close-positions.py
+++ examples/py/phemex-open-cancel-close-positions.py
@@ -21,22 +21,30 @@ exchange = ccxt.phemex({
     },
 })
 
-# exchange.set_sandbox_mode(True)  # uncomment to use the testnet sandbox
+exchange.set_sandbox_mode(True)  # uncomment to use the testnet sandbox
 
 markets = exchange.load_markets()
 
 amount = 10
 symbol = 'BTC/USD:USD'
 
-# Opening and Canceling a pending contract order (unrealistic price)
+# Opening and Canceling a pending contract (limit) order (unrealistic price)
 order = exchange.create_order(symbol, 'limit', 'buy', amount, '20000')
-exchange.cancel_order(order['id'], symbol)
+response = exchange.cancel_order(order['id'], symbol)
+pprint(response)
+
+# Opening and Canceling a pending contract (stop-market) order (unrealistic price)
+stopMarketOrder = exchange.create_order(symbol, 'MarketIfTouched', 'buy', amount, None, {'stopPx': 20000})
+stopMarketResponse = exchange.cancel_order(stopMarketOrder['id'], symbol)
+pprint(stopMarketResponse)
+
+# Opening and Canceling a pending contract (stop-limit) order (unrealistic price)
+stopLimitOrder = exchange.create_order(symbol, 'LimitIfTouched', 'buy', amount, 20000, {'stopPx': 20000})
+stopLimitResponse = exchange.cancel_order(stopLimitOrder['id'], symbol)
+pprint(stopLimitResponse)
 
 # Opening and exiting a filled contract position by issuing the exact same order but in the opposite direction
-
-# Opening a long position
 order = exchange.create_order(symbol, 'market', 'buy', amount)
-
 # closing the previous position by issuing the exact same order but in the opposite direction
 # with reduceOnly option to prevent an unwanted exposure increase
 orderClose = exchange.create_order(symbol, 'market', 'sell', amount, None, {'reduceOnly': True})
