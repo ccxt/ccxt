4f7cd10fa6517c105af457dc2c8750c01b812c23
Igor Kroitor <igor.kroitor@gmail.com>
2021-12-28 03:50:50 +0300

    huobi cancelAllOrders for futures and swaps

--- js/huobi.js
+++ js/huobi.js
@@ -3206,19 +3206,55 @@ module.exports = class huobi extends Exchange {
 
     async cancelAllOrders (symbol = undefined, params = {}) {
         await this.loadMarkets ();
+        let methodType = undefined;
+        [ methodType, params ] = this.handleMarketTypeAndParams ('cancelOrder', undefined, params);
         const request = {
-            // 'account-id' string false NA The account id used for this cancel Refer to GET /v1/account/accounts
+            // spot -----------------------------------------------------------
+            // 'account-id': account['id'],
             // 'symbol': market['id'], // a list of comma-separated symbols, all symbols by default
             // 'types' 'string', buy-market, sell-market, buy-limit, sell-limit, buy-ioc, sell-ioc, buy-stop-limit, sell-stop-limit, buy-limit-fok, sell-limit-fok, buy-stop-limit-fok, sell-stop-limit-fok
             // 'side': 'buy', // or 'sell'
             // 'size': 100, // the number of orders to cancel 1-100
+            // contract -------------------------------------------------------
+            // 'symbol': market['settleId'], // required
+            // 'contract_code': market['id'],
+            // 'contract_type': 'this_week', // swap, this_week, next_week, quarter, next_ quarter
+            // 'direction': 'buy': // buy, sell
+            // 'offset': 'open', // open, close
         };
         let market = undefined;
-        if (symbol !== undefined) {
-            market = this.market (symbol);
-            request['symbol'] = market['id'];
+        let method = undefined;
+        if (methodType === 'spot') {
+            if (symbol !== undefined) {
+                market = this.market (symbol);
+                request['symbol'] = market['id'];
+            }
+            method = 'spotPrivatePostV1OrderOrdersBatchCancelOpenOrders';
+        } else {
+            if (symbol === undefined) {
+                throw new ArgumentsRequired (this.id + ' cancelOrders() requires a symbol for ' + methodType + ' orders');
+            }
+            const market = this.market (symbol);
+            request['contract_code'] = market['id'];
+            if (methodType === 'future') {
+                method = 'contractPrivatePostApiV1ContractCancelall';
+                request['symbol'] = market['settleId'];
+            } else if (methodType === 'swap') {
+                if (market['linear']) {
+                    const marginType = this.safeString2 (this.options, 'defaultMarginType', 'marginType', 'isolated');
+                    if (marginType === 'isolated') {
+                        method = 'contractPrivatePostLinearSwapApiV1SwapCancelallall';
+                    } else if (marginType === 'cross') {
+                        method = 'contractPrivatePostLinearSwapApiV1SwapCrossCancelall';
+                    }
+                } else if (market['inverse']) {
+                    method = 'contractPrivatePostSwapApiV1SwapCancelall';
+                }
+            } else {
+                throw new NotSupported (this.id + ' cancelOrders() does not support ' + methodType + ' markets');
+            }
         }
-        const response = await this.spotPrivatePostV1OrderOrdersBatchCancelOpenOrders (this.extend (request, params));
+        const response = await this[method] (this.extend (request, params));
         //
         //     {
         //         code: 200,
