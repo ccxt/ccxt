ddf5564284acb833fe76ae8eabd558c315b2024f
cjg <carlos.goncalves@outsystems.com>
2021-12-30 14:25:40 +0000

    complement first example

--- examples/py/bitmex-cancel-orders.py
+++ examples/py/bitmex-cancel-orders.py
@@ -11,11 +11,11 @@ import ccxt  # noqa: E402
 
 
 
-exchange = ccxt.bitmex({
-    'apiKey': 'YOUR_API_KEY',
-    'secret': 'YOUR_SECRET',
-    'enableRateLimit': True,
-})
+# exchange = ccxt.bitmex({
+#     'apiKey': 'YOUR_API_KEY',
+#     'secret': 'YOUR_SECRET',
+#     'enableRateLimit': True,
+# })
 
 exchange.set_sandbox_mode(True)  # uncomment to use the testnet sandbox
 
@@ -29,19 +29,19 @@ price = 15000
 ## https://github.com/ccxt/ccxt/issues/10112
 
 # create first order
-order1 = exchange.create_order(symbol, type, side, amount, price, {'clientOrderId': 'order001'})
-print(order1)
-
+order1 = exchange.create_order(symbol, type, side, amount, price, {'clientOrderId': 'order0001'})
+print(order1['id'])
 # create second order
 price = 10000
-order2 = exchange.create_order(symbol, type, side, amount, price, {'clientOrderId': 'order002'})
-print(order2)
-
+order2 = exchange.create_order(symbol, type, side, amount, price, {'clientOrderId': 'order0002'})
+print(order2['id'])
 # create third order
-price = 10000
-order2 = exchange.create_order(symbol, type, side, amount, price, {'clientOrderId': 'order002'})
-print(order2)
-
+price = 9000
+order3 = exchange.create_order(symbol, type, side, amount, price, {'clientOrderId': 'order0003'})
+print(order3['id'])
 # canceling first order 
-cancelResponse = exchange.cancel_order(None, None, {'clientOrderId': 'order001'})
-# print(cancelResponse)
+cancelResponse = exchange.cancel_order(None, None, {'clientOrderId': 'order0001'})
+print(cancelResponse)
+# cancel second and third order at the same time
+cancelboth = exchange.cancel_orders(None, None, {'clientOrderId': ['order0002','order0003']})
+print(cancelboth)
