7857d17101f41da74ae1b298478e33816e950b7e
cjg <carlos.goncalves@outsystems.com>
2021-12-30 14:32:56 +0000

    second example

--- examples/py/bitmex-cancel-orders.py
+++ examples/py/bitmex-cancel-orders.py
@@ -9,15 +9,13 @@ sys.path.append(root + '/python')
 import ccxt  # noqa: E402
 
 
+exchange = ccxt.bitmex({
+    'apiKey': 'YOUR_API_KEY',
+    'secret': 'YOUR_SECRET',
+    'enableRateLimit': True,
+})
 
-
-# exchange = ccxt.bitmex({
-#     'apiKey': 'YOUR_API_KEY',
-#     'secret': 'YOUR_SECRET',
-#     'enableRateLimit': True,
-# })
-
-exchange.set_sandbox_mode(True)  # uncomment to use the testnet sandbox
+# exchange.set_sandbox_mode(True)  # uncomment to use the testnet sandbox
 
 symbol = 'BTC/USD'  
 type = 'limit'
@@ -25,8 +23,11 @@ side = 'buy'
 amount = 100
 price = 15000
 
-## Creating multiple orders and cancel them
-## https://github.com/ccxt/ccxt/issues/10112
+### Creating multiple orders and cancel them
+### https://github.com/ccxt/ccxt/issues/10112
+
+## Example 1
+## Canceling orders using ClientOrderId
 
 # create first order
 order1 = exchange.create_order(symbol, type, side, amount, price, {'clientOrderId': 'order0001'})
@@ -44,4 +45,26 @@ cancelResponse = exchange.cancel_order(None, None, {'clientOrderId': 'order0001'
 print(cancelResponse)
 # cancel second and third order at the same time
 cancelboth = exchange.cancel_orders(None, None, {'clientOrderId': ['order0002','order0003']})
+print(cancelboth)
+
+## Example 2
+## Canceling orders using OrderId
+
+# create first order
+newOrder1 = exchange.create_order(symbol, type, side, amount, price)
+print(newOrder1['id'])
+# create second order
+price = 10000
+newOrder2 = exchange.create_order(symbol, type, side, amount, price)
+print(newOrder2['id'])
+# create third order
+price = 9000
+newOrder3 = exchange.create_order(symbol, type, side, amount, price)
+print(newOrder3['id'])
+# canceling first order 
+cancelResponse = exchange.cancel_order(newOrder1['id'])
+print(cancelResponse)
+# cancel second and third order at the same time
+ids = [newOrder2['id'], newOrder3['id']]
+cancelboth = exchange.cancel_orders(ids)
 print(cancelboth)
