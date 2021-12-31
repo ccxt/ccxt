93dfc029e4e310416c267e73ad300607b6d49a9e
Travis CI <travis@travis-ci.com>
2021-12-31 01:21:33 +0000

    0.9.73
    [ci skip]

--- python/ccxtpro/__init__.py
+++ python/ccxtpro/__init__.py
@@ -8,7 +8,7 @@ import ccxt.async_support as ccxt
 
 # -----------------------------------------------------------------------------
 
-__version__ = '0.9.72'
+__version__ = '0.9.73'
 
 # -----------------------------------------------------------------------------
 
@@ -94,6 +94,7 @@ from ccxt.async_support.coinmate import coinmate                          # noqa
 from ccxt.async_support.coinone import coinone                            # noqa: F401
 from ccxt.async_support.coinspot import coinspot                          # noqa: F401
 from ccxt.async_support.crex24 import crex24                              # noqa: F401
+from ccxt.async_support.cryptocom import cryptocom                        # noqa: F401
 from ccxt.async_support.delta import delta                                # noqa: F401
 from ccxt.async_support.deribit import deribit                            # noqa: F401
 from ccxt.async_support.digifinex import digifinex                        # noqa: F401
