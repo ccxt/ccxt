4320981dd96c9c1550ea978ca8abe5f3d61f64db
Sam Germain <sam.germain@usask.ca>
2022-01-01 05:20:35 -0600

    test.fetchBorrowInterestHistory is async, styling

--- js/test/Exchange/test.fetchBorrowInterestHistory.js
+++ js/test/Exchange/test.fetchBorrowInterestHistory.js
@@ -1,17 +1,17 @@
-'use strict';
+'use strict'
 
 // ----------------------------------------------------------------------------
 
-const assert = require ('assert');
+const assert = require ('assert')
 
 // ----------------------------------------------------------------------------
 
-module.exports = (exchange, code, symbol) => {
+module.exports = async (exchange, code, symbol) => {
 
-    const method = 'fetchBorrowInterestHistory';
+    const method = 'fetchBorrowInterestHistory'
 
     if (exchange.has[method]) {
-        
+
         const format = {
             'account': 'BTC/USDT',
             'currency': 'USDT',
@@ -21,39 +21,39 @@ module.exports = (exchange, code, symbol) => {
             'timestamp': 1638230400000,
             'datetime': '2021-11-30T00:00:00.000Z',
             'info': {},
-        };
-        
-        const interest = await exchange.fetchBorrowInterestHistory (code, symbol);
-        
-        console.log (code, method, interest['datetime'], 'symbol: ', symbol,  'interest: ', interest['interest'], 'interestRate: ', interest['interestRate'], 'amountBorrowed: ', interest['amountBorrowed']);
+        }
+
+        const interest = await exchange.fetchBorrowInterestHistory (code, symbol)
+
+        console.log (code, method, interest['datetime'], 'symbol: ', symbol,  'interest: ', interest['interest'], 'interestRate: ', interest['interestRate'], 'amountBorrowed: ', interest['amountBorrowed'])
 
         if (code) {
-            assert (interest['currency'] === code);
+            assert (interest['currency'] === code)
         }
-        assert (interest['account'] === symbol || interest['account'] === undefined);
+        assert (interest['account'] === symbol || interest['account'] === undefined)
 
         const keys = Object.keys (format)
         for (let i = 0; i < keys.length; i++) {
-            const key = keys[i];
-            assert (key in interest);
+            const key = keys[i]
+            assert (key in interest)
         }
-    
+
         if (interest['amountBorrowed'] !== undefined) {
-            assert (interest['amountBorrowed'] >= 0);
+            assert (interest['amountBorrowed'] >= 0)
         }
         if (interest['interestRate'] !== undefined) {
-            assert (interest['interestRate'] > 0);
+            assert (interest['interestRate'] > 0)
         }
         if (interest['interest'] !== undefined) {
-            assert (interest['interest'] >= 0);
+            assert (interest['interest'] >= 0)
         }
         if (interest['timestamp'] !== undefined) {
-            assert (interest['timestamp'] > 1640933203000);
+            assert (interest['timestamp'] > 1640933203000)
         }
 
-        return borrowRate;
+        return interest
 
     } else {
-        console.log (code, 'fetchBorrowInterestHistory () not supported');
+        console.log (code, 'fetchBorrowInterestHistory () not supported')
     }
 }
