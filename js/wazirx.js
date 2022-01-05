782b8dd3ab3354e72996522f28962e3dc1fe1c58
cjg <carlos.goncalves@outsystems.com>
2022-01-05 11:37:10 +0000

    fetchTicker and parseTicker

--- js/wazirx.js
+++ js/wazirx.js
@@ -2,6 +2,7 @@
 
 const Exchange = require ('./base/Exchange');
 const { ExchangeError } = require ('./base/errors');
+const time = require ('./base/functions/time');
 
 module.exports = class wazirx extends Exchange {
     describe () {
@@ -14,8 +15,8 @@ module.exports = class wazirx extends Exchange {
                 'CORS': true,
                 'fetchMarkets': true,
                 'fetchCurrencies': false,
-                'fetchTickers': false,
-                'fetchTicker': false,
+                'fetchTickers': true,
+                'fetchTicker': true,
                 'fetchOHLCV': false,
                 'fetchOrderBook': true,
                 'fetchTrades': false,
@@ -183,6 +184,64 @@ module.exports = class wazirx extends Exchange {
         return this.parseOrderBook (response, symbol, timestamp);
     }
 
+    async fetchTicker (symbol, params = {}) {
+        await this.loadMarkets ();
+        const market = this.market (symbol);
+        const request = {
+            'market': market['id'],
+        };
+        const response = await this.v1PublicGetTicker (this.extend (request, params));
+        //
+        // {
+        //     "symbol":"wrxinr",
+        //     "baseAsset":"wrx",
+        //     "quoteAsset":"inr",
+        //     "openPrice":"94.77",
+        //     "lowPrice":"92.7",
+        //     "highPrice":"95.17",
+        //     "lastPrice":"94.03",
+        //     "volume":"1118700.0",
+        //     "bidPrice":"94.02",
+        //     "askPrice":"94.03",
+        //     "at":1641382455000
+        // }
+        //
+        const ticker = this.safeValue (response, undefined, {});
+        return this.parseTicker (ticker, market);
+    }
+
+    async fetchTickers (symbols = undefined, params = {}) {
+        await this.loadMarkets ();
+        const response = await this.spotV1PublicGetTickers24hr ();
+        //
+        // [
+        //     {
+        //        "symbol":"btcinr",
+        //        "baseAsset":"btc",
+        //        "quoteAsset":"inr",
+        //        "openPrice":"3698486",
+        //        "lowPrice":"3641155.0",
+        //        "highPrice":"3767999.0",
+        //        "lastPrice":"3713212.0",
+        //        "volume":"254.11582",
+        //        "bidPrice":"3715021.0",
+        //        "askPrice":"3715022.0",
+        //     }
+        //     ...
+        // ]
+        //
+        const tickers = this.safeValue (response, undefined, []);
+        const result = {};
+        for (let i = 0; i < tickers.length; i++) {
+            const ticker = tickers[i];
+            const marketId = this.safeString (ticker, 'symbol');
+            const market = this.safeMarket (marketId, undefined);
+            const symbol = market['symbol'];
+            result[symbol] = this.parseTicker (ticker, market);
+        }
+        return result;
+    }
+
     async fetchStatus (params = {}) {
         const response = await this.spotV1PublicGetSystemStatus (params);
         //
@@ -205,6 +264,57 @@ module.exports = class wazirx extends Exchange {
         //     }
         //
         return this.safeInteger (response, 'serverTime');
+    }
+
+    parseTicker (ticker, market = undefined) {
+        //
+        //     {
+        //        "symbol":"btcinr",
+        //        "baseAsset":"btc",
+        //        "quoteAsset":"inr",
+        //        "openPrice":"3698486",
+        //        "lowPrice":"3641155.0",
+        //        "highPrice":"3767999.0",
+        //        "lastPrice":"3713212.0",
+        //        "volume":"254.11582", // base volume
+        //        "bidPrice":"3715021.0",
+        //        "askPrice":"3715022.0",
+        //        "at":1641382455000 // only on fetchTicker
+        //     }
+        //
+        const marketId = this.safeString (ticker, 'symbol');
+        market = this.safeMarket (marketId, market);
+        const symbol = market['symbol'];
+        const last = this.safeNumber (ticker, 'lastPrice');
+        const open = this.safeNumber (ticker, 'openPrice');
+        const high = this.safeNumber (ticker, 'highPrice');
+        const low = this.safeNumber (ticker, 'lowPrice');
+        const baseVolume = this.safeNumber (ticker, 'volume');
+        const bid = this.safeNumber (ticker, 'bid');
+        const ask = this.safeNumber (ticker, 'ask');
+        const timestamp = this.safeString (ticker, 'at');
+        return this.safeTicker ({
+            'symbol': symbol,
+            'timestamp': timestamp,
+            'datetime': this.iso8601 (timestamp),
+            'high': high,
+            'low': low,
+            'bid': bid,
+            'bidVolume': undefined,
+            'ask': ask,
+            'askVolume': undefined,
+            'vwap': undefined,
+            'open': open,
+            'close': last,
+            'last': last,
+            'previousClose': undefined,
+            'change': undefined,
+            'percentage': undefined,
+            'average': undefined,
+            'baseVolume': baseVolume,
+            'quoteVolume': undefined,
+            'info': ticker,
+        }, market);
     }
 
     sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
