2d04828d6aace43ac1fecc5e7eb14d3a6528ecb5
Igor Kroitor <igor.kroitor@gmail.com>
2022-01-07 16:50:31 +0300

    transpile.js missing comma

--- build/transpile.js
+++ build/transpile.js
@@ -163,7 +163,7 @@ class Transpiler {
             [ /\.base16ToBinary/g, '.base16_to_binary'],
             [ /\'use strict\';?\s+/g, '' ],
             [ /\.urlencodeWithArrayRepeat\s/g, '.urlencode_with_array_repeat' ],
-            [ /\.call\s*\(this, /g, '(' ]
+            [ /\.call\s*\(this, /g, '(' ],
             [ /\.getSupportedMapping\s/g, '.get_supported_mapping'],
             [ /\.fetchBorrowRate\s/g, '.fetch_borrow_rate'],
             [ /\.handleMarketTypeAndParams\s/g, '.handle_market_type_and_params'],
