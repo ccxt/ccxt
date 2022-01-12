ba5d7ca56cd530d07789d075c2e7e75589f57170
Travis CI <travis@travis-ci.org>
2022-01-12 01:10:27 +0000

    1.67.14
    [ci skip]

--- php/Exchange.php
+++ php/Exchange.php
@@ -36,7 +36,7 @@ use Elliptic\EdDSA;
 use BN\BN;
 use Exception;
 
-$version = '1.67.13';
+$version = '1.67.14';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -55,7 +55,7 @@ const PAD_WITH_ZERO = 1;
 
 class Exchange {
 
-    const VERSION = '1.67.13';
+    const VERSION = '1.67.14';
 
     private static $base58_alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
     private static $base58_encoder = null;
--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -28,11 +28,11 @@ use Exception;
 
 include 'Throttle.php';
 
-$version = '1.67.13';
+$version = '1.67.14';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '1.67.13';
+    const VERSION = '1.67.14';
 
     public static $loop;
     public static $kernel;
