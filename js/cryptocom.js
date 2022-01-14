cca89fe0e213dc7beaecb6c9bde03ebbf3aff5fd
Igor Kroitor <igor.kroitor@gmail.com>
2022-01-14 13:24:17 +0300

    cryptocom fetchMyTrades since pagination fix #11330

--- js/cryptocom.js
+++ js/cryptocom.js
@@ -1041,6 +1041,9 @@ module.exports = class cryptocom extends Exchange {
         if (since !== undefined) {
             // maximum date range is one day
             request['start_ts'] = since;
+            const now = this.milliseconds ();
+            const endTimestamp = this.sim (since, 24 * 60 * 60 * 1000);
+            request['end_ts'] = Math.min (now, endTimestamp);
         }
         if (limit !== undefined) {
             request['page_size'] = limit;
