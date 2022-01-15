b679c9554676ba6f9cee5e91976834452a3388c3
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-01-15 11:37:50 -0400

    add market tests to unify the confederacy

--- js/test/Exchange/test.market.js
+++ js/test/Exchange/test.market.js
@@ -17,48 +17,58 @@ module.exports = (exchange, market, method) => {
         'maker':   0.0009,     // maker fee, for example, 0.0009 = 0.09%
         //---------------------------------------------------------------------
         // commented temporarily to bring markets to consistency first
-        // 'baseId':  'btc',      // exchange-specific base currency id
-        // 'quoteId': 'usd',      // exchange-specific quote currency id
-        // 'active': true,       // boolean, market status
-        // 'precision': {        // number of decimal digits "after the dot"
-        //     'price': 8,       // integer
-        //     'amount': 8,      // integer
-        //     'cost': 8,        // integer
-        // },
-        // 'limits': {           // value limits when placing orders on this market
-        //     'amount': {
-        //         'min': 0.01,  // order amount should be > min
-        //         'max': 1000,  // order amount should be < max
-        //     },
-        //     'price': {
-        //         'min': 0.01,  // order price should be > min
-        //         'max': 1000,  // order price should be < max
-        //     },
-        //     'cost':  { // order cost = price * amount
-        //         'min': 0.01,  // order cost should be > min
-        //         'max': 1000,  // order cost should be < max
-        //     },
-        // },
-        // 'info': {}, // the original unparsed market info from the exchange
+        'baseId':  'btc',     // exchange-specific base currency id
+        'quoteId': 'usd',     // exchange-specific quote currency id
+        'active': true,       // boolean, market status
+        // uncomment these lines to test the futures keys
+        'type': 'spot',
+        'linear': undefined,
+        'inverse': undefined,
+        'spot': true,
+        'swap': false,     // doesn't exist on binance...
+        'future': false,   // doesn't exist on binance...
+        'margin': false,
+        'contract': false,
+        'precision': {        // number of decimal digits "after the dot"
+             'price': 8,       // integer
+             'amount': 8,      // integer
+             'cost': 8,        // integer
+         },
+        'limits': {           // value limits when placing orders on this market
+             'amount': {
+                 'min': 0.01,  // order amount should be > min
+                 'max': 1000,  // order amount should be < max
+             },
+             'price': {
+                 'min': 0.01,  // order price should be > min
+                 'max': 1000,  // order price should be < max
+             },
+             'cost':  { // order cost = price * amount
+                 'min': 0.01,  // order cost should be > min
+                 'max': 1000,  // order cost should be < max
+             },
+        },
+        'info': {}, // the original unparsed market info from the exchange
         //---------------------------------------------------------------------
     }
 
-    const keys = Object.keys (format)
+    const keys = Object.keys (format);
     for (let i = 0; i < keys.length; i++) {
         const key = keys[i]
-        assert (key in market)
+        assert (key in market, key + JSON.stringify (market));
     }
-
-    // assert ((market['baseId'] === undefined) || (typeof market['baseId'] === 'string'))
-    // assert ((market['quoteId'] === undefined) || (typeof market['quoteId'] === 'string'))
-
-    assert ((market['taker'] === undefined) || (typeof market['taker'] === 'number'))
-    assert ((market['maker'] === undefined) || (typeof market['maker'] === 'number'))
-
-    // expect (market['precision']['amount']).to.not.be.undefined
-    // expect (market['precision']['price']).to.not.be.undefined
-
-    // expect (market['limits']['amount']['min']).to.not.be.undefined
-    // expect (market['limits']['price']['min']).to.not.be.undefined
-    // expect (market['limits']['cost']['min']).to.not.be.undefined
+    assert ((market['taker'] === undefined) || (typeof market['taker'] === 'number'));
+    assert ((market['maker'] === undefined) || (typeof market['maker'] === 'number'));
+    if (market['contract'] === false) {
+        assert ((market['linear'] === undefined) && (market['inverse'] === undefined))
+    }
+    const validTypes = {
+        'spot': true,
+        'margin': true,
+        'swap': true,
+        'future': true,
+        'option': true,
+    };
+    const type = market['type'];
+    assert (type in validTypes);
 }
