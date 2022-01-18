6d3d1c76c49a6d056d358a451f68bbf54bdf4fd5
cjg <carlos.goncalves@outsystems.com>
2022-01-18 23:18:06 +0000

    php futures example

---
+++ examples/php/huobi-futures.php
@@ -0,0 +1,51 @@
+<?php
+
+$root = dirname (dirname (dirname (__FILE__)));
+
+include $root . '/ccxt.php';
+
+date_default_timezone_set ('UTC');
+
+$exchange = new \ccxt\huobipro (array (
+    'apiKey' => 'ez2xc4vb6n-da8c4c7d-76cbde5b-7cc77', // ←------------ replace with your keys
+    'secret' => '03454cfd-cf15e71b-fe87eadf-16a79',
+    'options' => array(
+        'defaultType' => 'future',
+    )
+    // 'verbose' => true, // uncomment if debug output is needed
+));
+
+try {
+
+    $markets = $exchange->load_markets ();
+
+    // Example: Creating/cancelling a linear swap (limit) order 
+    $symbol = 'ADA/USD:ADA-220121'; //The last segment it's the date of expiration (can bee next week, next quarter,...) adjust it accordingly
+    $order_type = 'limit';
+    $side = 'buy';
+    $offset = 'open';
+    $cli_order_id = random_int (0,100);
+    $leverage = 1;
+    $amount = 1;
+    $price = 1;
+
+    $params = array (
+        'offset' => $offset,
+        'lever_rate' => $leverage,
+        'client_order_id' => $cli_order_id
+    );
+
+    $order = $exchange->create_order($symbol, $order_type, $side, $amount, $price, $params);
+    var_dump($order);
+    $cancel = $exchange->cancel_order($order['id'], $symbol);
+    var_dump($cancel);
+
+} catch (\ccxt\NetworkError $e) {
+    echo '[Network Error] ' . $e->getMessage () . "\n";
+} catch (\ccxt\ExchangeError $e) {
+    echo '[Exchange Error] ' . $e->getMessage () . "\n";
+} catch (Exception $e) {
+    echo '[Error] ' . $e->getMessage () . "\n";
+}
+
+?>
