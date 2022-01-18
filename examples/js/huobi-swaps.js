9d652044bdbea13db95889e28560438b9d7e0612
cjg <carlos.goncalves@outsystems.com>
2022-01-18 21:05:27 +0000

    js swaps

--- examples/js/huobi-swaps.js
+++ examples/js/huobi-swaps.js
@@ -3,28 +3,60 @@ const ccxt = require ('../../ccxt');
 const exchange = new ccxt.huobi ({
     'apiKey': 'ez2xc4vb6n-da8c4c7d-76cbde5b-7cc77',
     'secret': '03454cfd-cf15e71b-fe87eadf-16a79',
+    'options': {
+        'defaultType': 'swap',
+    },
 })
 
 ;(async () => {
-    console.log (await exchange.fetchBalance ())
-
     const markets = await exchange.loadMarkets ()
 
-    exchange.verbose = true // uncomment for debugging purposes if necessary
+    // exchange.verbose = true // uncomment for debugging purposes if necessary
 
-    const symbol = 'ADA/USD'
-    const type = 'STOP_LOSS_LIMIT'
-    const side = 'buy'
-    const amount = 1
-    const price = YOUR_PRICE_HERE
-    const params = {
-        'stopPrice': YOUR_STOP_PRICE_HERE,
-        'timeInForce': 'GTC',
+    // Example 1: Creating/cancelling a linear swap (limit) order 
+    let symbol = 'ADA/USDT:USDT'
+    let type = 'limit'
+    let side = 'buy'
+    let offset= 'open'
+    let clientOrderId = Math.floor (Math.random () * 10);
+    let leverage = 1
+    let amount = 1
+    let price = 1 // 1 contract = 10 ADA = 10 usd in this case
+    let params = {
+        'offset': offset,
+        'lever_rate': leverage,
+        'client_order_id': clientOrderId
     }
 
     try {
         const order = await exchange.createOrder (symbol, type, side, amount, price, params)
         console.log (order)
+        const cancel = await exchange.cancelOrder (order['id'], symbol)
+        console.log (cancel)
+    } catch (e) {
+        console.log (e.constructor.name, e.message)
+    }
+
+    // Example 2: Creating/cancelling inverse swap (limit) order
+    symbol = 'ADA/USD:ADA'
+    type = 'limit'
+    side = 'buy'
+    offset = 'open'
+    clientOrderId = Math.floor (Math.random () * 10);
+    leverage = 1
+    amount = 1
+    price = 1 // 1 contract = 10 ADA = 10 usd in this case
+    params = {
+        'offset': offset,
+        'lever_rate': leverage,
+        'client_order_id': clientOrderId
+    }
+
+    try {
+        const order = await exchange.createOrder (symbol, type, side, amount, price, params)
+        console.log (order)
+        const cancel = await exchange.cancelOrder (order['id'], symbol)
+        console.log (cancel)
     } catch (e) {
         console.log (e.constructor.name, e.message)
     }
