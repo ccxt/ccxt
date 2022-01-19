f1e7749869ece77cfdeb54b0bd2bc1a16095f2b3
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-01-19 02:11:43 -0400

    edit

--- js/exmo.js
+++ js/exmo.js
@@ -443,7 +443,7 @@ module.exports = class exmo extends Exchange {
                 'margin': false,
                 'future': false,
                 'swap': false,
-                'option': undefined,
+                'option': false,
                 'optionType': undefined,
                 'strike': undefined,
                 'linear': undefined,
