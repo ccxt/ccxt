5ce5c71ae9ff82014ca9e3d182d5a055a73bc29a
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-01-19 02:07:32 -0400

    edit

--- js/btcalpha.js
+++ js/btcalpha.js
@@ -122,7 +122,7 @@ module.exports = class btcalpha extends Exchange {
                 'margin': false,
                 'future': false,
                 'swap': false,
-                'option': undefined,
+                'option': false,
                 'optionType': undefined,
                 'strike': undefined,
                 'linear': undefined,
