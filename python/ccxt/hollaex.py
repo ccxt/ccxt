5b91018248adcc1ebd78435d59e6a1add0f4cd14
Travis CI <travis@travis-ci.org>
2022-01-20 06:18:23 +0000

    1.68.73
    [ci skip]

--- python/ccxt/hollaex.py
+++ python/ccxt/hollaex.py
@@ -74,8 +74,13 @@ class hollaex(Exchange):
                 'withdraw': True,
             },
             'timeframes': {
+                '1m': '1m',
+                '5m': '5m',
+                '15m': '15m',
                 '1h': '1h',
+                '4h': '4h',
                 '1d': '1d',
+                '1w': '1w',
             },
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/75841031-ca375180-5ddd-11ea-8417-b975674c23cb.jpg',
