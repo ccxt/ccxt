cad3d93db456791d6bc7bb5aebb23fbb505baf4a
ndubel <38309641+ndubel@users.noreply.github.com>
2022-01-21 15:27:33 +0300

    probit GM -> GM Holding
    https://www.coingecko.com/en/coins/gm-holding#markets
    conclict with https://www.coingecko.com/en/coins/gm#markets

--- js/probit.js
+++ js/probit.js
@@ -163,6 +163,7 @@ module.exports = class probit extends Exchange {
                 'EPS': 'Epanus',  // conflict with EPS Ellipsis https://github.com/ccxt/ccxt/issues/8909
                 'FX': 'Fanzy',
                 'GDT': 'Gorilla Diamond',
+                'GM': 'GM Holding',
                 'GOGOL': 'GOL',
                 'GOL': 'Goldofir',
                 'GRB': 'Global Reward Bank',
