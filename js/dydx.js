d494e31f988b3af9204b8f9b100222d4221d86ea
T.T <7117978+ttodua@users.noreply.github.com>
2022-01-26 00:37:02 +0400

    linting

--- js/dydx.js
+++ js/dydx.js
@@ -5,7 +5,7 @@
 const Exchange = require ('./base/Exchange');
 const { ExchangeNotAvailable, AuthenticationError, BadSymbol, ExchangeError, InvalidOrder, InsufficientFunds } = require ('./base/errors');
 const { TICK_SIZE } = require ('./base/functions/number');
-const Precise = require ('./base/Precise');
+// const Precise = require ('./base/Precise');
 
 // ----------------------------------------------------------------------------
 
