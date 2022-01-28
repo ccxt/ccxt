7db4b93c9e29e4559c2a34307468005139de58cd
Dan <dan.krm@outlook.com>
2022-01-28 04:44:20 -0600

    Update mexc.js

--- js/mexc.js
+++ js/mexc.js
@@ -40,6 +40,7 @@ module.exports = class mexc extends Exchange {
                 'fetchFundingRate': true,
                 'fetchFundingRateHistory': true,
                 'fetchMarkets': true,
+                'fetchMarkOHLCV': true,
                 'fetchMyTrades': true,
                 'fetchOHLCV': true,
                 'fetchOpenOrders': true,
