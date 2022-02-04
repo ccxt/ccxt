f190449d7243d0800083defa159c43447fb6eb34
Sam Germain <sam.germain@usask.ca>
2022-02-04 17:50:49 -0600

    bybit.fetchLeverageTiers

--- js/bybit.js
+++ js/bybit.js
@@ -39,6 +39,7 @@ module.exports = class bybit extends Exchange {
                 'fetchFundingRateHistory': false,
                 'fetchIndexOHLCV': true,
                 'fetchLedger': true,
+                'fetchLeverageTiers': true,
                 'fetchMarkets': true,
                 'fetchMarkOHLCV': true,
                 'fetchMyTrades': true,
@@ -3093,5 +3094,111 @@ module.exports = class bybit extends Exchange {
             this.throwBroadlyMatchedException (this.exceptions['broad'], body, feedback);
             throw new ExchangeError (feedback); // unknown message
         }
+    }
+
+    async fetchLeverageTiers (symbol = undefined, params = {}) {
+        await this.loadMarkets ();
+        const request = {};
+        let market = undefined;
+        if (symbol !== undefined) {
+            market = this.market (symbol);
+            if (market['spot']) {
+                throw new BadRequest (this.id + '.fetchLeverageTiers symbol supports contract markets only');
+            }
+            request['symbol'] = market['id'];
+        }
+        const [ type, query ] = this.handleMarketTypeAndParams ('fetchLeverageTiers', market, params);
+        if (type === 'linear' && symbol === undefined) {
+            throw new ArgumentsRequired (this.id + '.fetchLeverageTiers requires argument symbol for ' + type + ' markets');
+        }
+        const method = this.getSupportedMapping (type, {
+            'linear': 'publicLinearGetRiskLimit',
+            'swap': 'publicLinearGetRiskLimit',
+            'inverse': 'v2PublicGetRiskLimitList',
+            'future': 'v2PublicGetRiskLimitList',
+        });
+        const response = await this[method] (this.extend (request, query));
+        //
+        //  publicLinearGetRiskLimit
+        //    {
+        //        ret_code: '0',
+        //        ret_msg: 'OK',
+        //        ext_code: '',
+        //        ext_info: '',
+        //        result: [
+        //            {
+        //                id: '11',
+        //                symbol: 'ETHUSDT',
+        //                limit: '800000',
+        //                maintain_margin: '0.01',
+        //                starting_margin: '0.02',
+        //                section: [
+        //                    '1',  '2',  '3',
+        //                    '5',  '10', '15',
+        //                    '25'
+        //                ],
+        //                is_lowest_risk: '1',
+        //                created_at: '2022-02-04 23:30:33.555252',
+        //                updated_at: '2022-02-04 23:30:33.555254',
+        //                max_leverage: '50'
+        //            },
+        //            ...
+        //        ]
+        //    }
+        //
+        //  v2PublicGetRiskLimitList
+        //    {
+        //        ret_code: '0',
+        //        ret_msg: 'OK',
+        //        ext_code: '',
+        //        ext_info: '',
+        //        result: [
+        //            {
+        //                id: '180',
+        //                is_lowest_risk: '0',
+        //                section: [
+        //                  '1', '2', '3',
+        //                  '4', '5', '7',
+        //                  '8', '9'
+        //                ],
+        //                symbol: 'ETHUSDH22',
+        //                limit: '30000',
+        //                max_leverage: '9',
+        //                starting_margin: '11',
+        //                maintain_margin: '5.5',
+        //                coin: 'ETH',
+        //                created_at: '2021-04-22T15:00:00Z',
+        //                updated_at: '2021-04-22T15:00:00Z'
+        //            },
+        //        ],
+        //        time_now: '1644017569.683191'
+        //    }
+        //
+        const result = this.safeValue (response, 'result');
+        const tiers = {};
+        let tier = 1;
+        let notionalFloor = 0;
+        for (let i = 0; i < result.length; i++) {
+            const item = result[i];
+            const symbol = this.safeSymbol (this.safeString (item, 'symbol'));
+            const notionalCap = this.safeNumber (item, 'limit');
+            if (!(symbol in result)) {
+                notionalFloor = 0;
+                tier = 1;
+                tiers[symbol] = [];
+            }
+            tiers[symbol].push ({
+                'tier': tier,
+                'notionalFloor': notionalFloor,
+                'notionalCap': notionalCap,
+                'maintenanceMarginRatio': this.safeNumber (item, 'maintain_margin'),
+                'maxLeverage': this.safeNumber (item, 'max_leverage'),
+                'maintenanceAmount': undefined,
+                'info': item,
+            });
+            tier += 1;
+            notionalFloor = notionalCap;
+        }
+        return tiers;
     }
 };
