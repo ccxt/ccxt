52f591127282ccb5510bc03ba463d5168146cdc0
Dan <dan.krm@outlook.com>
2022-02-05 00:33:01 -0600

    Okx fetchCanceledOrders
    Added fetchCanceledOrders to Okx and fixed fetchClosedOrders: Endpoint was temporarily not working properly while making adjustments.
    ```
    okx.fetchCanceledOrders ()
    380 ms
                    id | clientOrderId |     timestamp |                 datetime | lastTradeTimestamp |   symbol |  type | timeInForce | postOnly | side | price | stopPrice | average | cost |     amount | filled |  remaining |   status |                         fee | trades |                          fees
    ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
    410057407937818630 |               | 1644037304550 | 2022-02-05T05:01:44.550Z |                    | BTC/USDT | limit |             |          |  buy | 30000 |         0 |         |    0 | 0.00060483 |      0 | 0.00060483 | canceled | {"cost":0,"currency":"BTC"} |     [] | [{"cost":0,"currency":"BTC"}]
    410059580352409602 |               | 1644037822494 | 2022-02-05T05:10:22.494Z |                    | BTC/USDT | limit |             |          |  buy | 30000 |         0 |         |    0 |  0.0005452 |      0 |  0.0005452 | canceled | {"cost":0,"currency":"BTC"} |     [] | [{"cost":0,"currency":"BTC"}]
    2 objects
    ```
    ```
    okx.fetchClosedOrders ()
    374 ms
                    id | clientOrderId |     timestamp |                 datetime | lastTradeTimestamp |   symbol |   type | timeInForce | postOnly | side |   price | stopPrice | average |         cost |     amount |     filled | remaining | status |                                       fee | trades |
                             fees
    -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
    408536984288931843 |               | 1643674807304 | 2022-02-01T00:20:07.304Z |      1643674807311 | BTC/USDT | market |             |          |  buy | 38340.2 |         0 | 38340.2 |         16.3 | 0.00042514 | 0.00042514 |         0 | closed |       {"cost":4.2514e-7,"currency":"BTC"} |     [] |       [{"cost":4.2514e-7,"currency":"BTC"}]
    408581807058366470 |               | 1643685493885 | 2022-02-01T03:18:13.885Z |      1643685493889 | BTC/USDT |  limit |             |          | sell |   38480 |         0 | 38489.7 | 16.384680393 | 0.00042569 | 0.00042569 |         0 | closed | {"cost":0.016384680393,"currency":"USDT"} |     [] | [{"cost":0.016384680393,"currency":"USDT"}]
    2 objects
    ```

--- js/okx.js
+++ js/okx.js
@@ -43,7 +43,7 @@ module.exports = class okx extends Exchange {
                 'fetchBorrowRateHistory': undefined,
                 'fetchBorrowRates': true,
                 'fetchBorrowRatesPerSymbol': false,
-                'fetchCanceledOrders': undefined,
+                'fetchCanceledOrders': true,
                 'fetchClosedOrder': undefined,
                 'fetchClosedOrders': true,
                 'fetchCurrencies': true,
@@ -2227,6 +2227,84 @@ module.exports = class okx extends Exchange {
         return this.parseOrders (data, market, since, limit);
     }
 
+    async fetchCanceledOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
+        await this.loadMarkets ();
+        const defaultType = this.safeString (this.options, 'defaultType');
+        const options = this.safeValue (this.options, 'fetchCanceledOrders', {});
+        let type = this.safeString (options, 'type', defaultType);
+        type = this.safeString (params, 'type', type);
+        params = this.omit (params, 'type');
+        const request = {
+            // 'instType': type.toUpperCase (), // SPOT, MARGIN, SWAP, FUTURES, OPTION
+            // 'uly': currency['id'],
+            // 'instId': market['id'],
+            // 'ordType': 'limit', // market, limit, post_only, fok, ioc, comma-separated
+            // 'state': 'filled', // filled, canceled
+            // 'after': orderId,
+            // 'before': orderId,
+            // 'limit': limit, // default 100, max 100
+        };
+        let market = undefined;
+        if (symbol !== undefined) {
+            market = this.market (symbol);
+            if (market['futures'] || market['swap']) {
+                type = market['type'];
+            }
+            request['instId'] = market['id'];
+        }
+        request['instType'] = type.toUpperCase ();
+        if (limit !== undefined) {
+            request['limit'] = limit; // default 100, max 100
+        }
+        request['state'] = 'canceled';
+        const method = this.safeString (options, 'method', 'privateGetTradeOrdersHistory');
+        const response = await this[method] (this.extend (request, params));
+        //
+        //     {
+        //         "code":"0",
+        //         "data":[
+        //             {
+        //                 "accFillSz":"0",
+        //                 "avgPx":"",
+        //                 "cTime":"1621910749815",
+        //                 "category":"normal",
+        //                 "ccy":"",
+        //                 "clOrdId":"",
+        //                 "fee":"0",
+        //                 "feeCcy":"ETH",
+        //                 "fillPx":"",
+        //                 "fillSz":"0",
+        //                 "fillTime":"",
+        //                 "instId":"ETH-USDT",
+        //                 "instType":"SPOT",
+        //                 "lever":"",
+        //                 "ordId":"317251910906576896",
+        //                 "ordType":"limit",
+        //                 "pnl":"0",
+        //                 "posSide":"net",
+        //                 "px":"2000",
+        //                 "rebate":"0",
+        //                 "rebateCcy":"USDT",
+        //                 "side":"buy",
+        //                 "slOrdPx":"",
+        //                 "slTriggerPx":"",
+        //                 "state":"live",
+        //                 "sz":"0.001",
+        //                 "tag":"",
+        //                 "tdMode":"cash",
+        //                 "tpOrdPx":"",
+        //                 "tpTriggerPx":"",
+        //                 "tradeId":"",
+        //                 "uTime":"1621910749815"
+        //             }
+        //         ],
+        //         "msg":""
+        //     }
+        //
+        const data = this.safeValue (response, 'data', []);
+        return this.parseOrders (data, market, since, limit);
+    }
+
     async fetchClosedOrders (symbol = undefined, since = undefined, limit = undefined, params = {}) {
         await this.loadMarkets ();
         const defaultType = this.safeString (this.options, 'defaultType');
@@ -2256,6 +2334,7 @@ module.exports = class okx extends Exchange {
         if (limit !== undefined) {
             request['limit'] = limit; // default 100, max 100
         }
+        request['state'] = 'filled';
         const method = this.safeString (options, 'method', 'privateGetTradeOrdersHistory');
         const response = await this[method] (this.extend (request, params));
         //
