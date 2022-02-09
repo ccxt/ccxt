b16a78a2a22bf071a5aa0a35a03781899bfbd401
Sam Germain <sam.germain@usask.ca>
2022-02-09 01:12:00 -0600

    test.Exchange.test.fetchLeverageTiers.js

---
+++ js/test/Exchange/test.fetchLeverageTiers.js
@@ -0,0 +1,53 @@
+'use strict'
+
+// ----------------------------------------------------------------------------
+
+const assert = require ('assert')
+
+// ----------------------------------------------------------------------------
+
+module.exports = async (exchange, symbol) => {
+
+    const format = {
+        // 'RAY/USDT': [
+        //   {
+            'tier': 1,
+            'notionalFloor': 0,
+            'notionalCap': 5000,
+            'maintenanceMarginRatio': 0.01,
+            'maxLeverage': 25,
+            'info': {}
+        //   },
+        // ],
+    }
+
+    if (exchange.has.fetchLeverageTiers) {
+
+        const method = 'fetchLeverageTiers'
+        const tiers = await exchange[method] (symbol)
+        const tierKeys = Object.keys(tiers)
+        console.log ('fetched leverage tiers for ', tierKeys.length, ' markets')
+
+        for (let i = 0; i < tierKeys.length; i++) {
+            const tiersForSymbol = tiers[tierKeys[i]]
+            for (let j=0; j < tiersForSymbol.length; j++) {
+                const tier = tiersForSymbol[j]
+                const keys = Object.keys (format)
+                for (let k = 0; k < keys.length; k++) {
+                    const key = keys[i]
+                    assert (key in tier)
+                }
+                assert (tier['tier'] >= 0)
+                assert (tier['notionalFloor'] >= 0)
+                assert (tier['notionalCap'] >= 0)
+                assert (tier['maintenanceMarginRatio'] >= 0)
+                assert (tier['maintenanceMarginRatio'] <= 1)
+                assert (tier['maxLeverage'] >= 1)
+            }
+        }
+        return tiers
+
+    } else {
+        console.log ('fetchLeverageTiers not supported')
+    }
+}
