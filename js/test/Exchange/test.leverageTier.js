cd369f18771b77946908a79a17d3bb5087079cf8
Igor Kroitor <igor.kroitor@gmail.com>
2022-02-14 01:28:48 +0300

    test.leverageTier.js made transpilable

--- js/test/Exchange/test.leverageTier.js
+++ js/test/Exchange/test.leverageTier.js
@@ -1,42 +1,28 @@
-'use strict'
+'use strict';
 
-// ----------------------------------------------------------------------------
+const assert = require ('assert');
 
-const assert = require ('assert')
-
-// ----------------------------------------------------------------------------
-
-module.exports = async (exchange, tier, method) => {
-    
+function testLeverageTier (exchange, method, tier) {
     const format = {
         'tier': 1,
         'notionalFloor': 0,
         'notionalCap': 5000,
         'maintenanceMarginRatio': 0.01,
         'maxLeverage': 25,
-        'info': {}
-    }
-
-    const keys = Object.keys (format)
+        'info': {},
+    };
+    const keys = Object.keys (format);
     for (let k = 0; k < keys.length; k++) {
-        const key = keys[i]
-        assert (key in tier)
+        const key = keys[i];
+        assert (key in tier);
     }
-    assert (tier['tier'] >= 0)
-    assert (tier['notionalFloor'] >= 0)
-    assert (tier['notionalCap'] >= 0)
-    assert (tier['maintenanceMarginRate'] <= 1)
-    assert (tier['maxLeverage'] >= 1)
-
-    console.log (
-        exchange.id,
-        method,
-        tier['tier'],
-        tier['notionalFloor'],
-        tier['notionalCap'],
-        tier['maintenanceMarginRate'],
-        tier['maxLeverage']
-    )
-
-    return tier
+    assert (tier['tier'] >= 0);
+    assert (tier['notionalFloor'] >= 0);
+    assert (tier['notionalCap'] >= 0);
+    assert (tier['maintenanceMarginRate'] <= 1);
+    assert (tier['maxLeverage'] >= 1);
+    console.log (exchange.id, method, tier['tier'], tier['notionalFloor'], tier['notionalCap'], tier['maintenanceMarginRate'], tier['maxLeverage']);
+    return tier;
 }
+
+module.exports = testLeverageTier;
