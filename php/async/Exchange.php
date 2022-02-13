bb1bed78c6b618831e12d1eae6ad79d43f233db9
Travis CI <travis@travis-ci.org>
2022-02-13 16:02:45 +0000

    1.72.93
    [ci skip]

--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -28,11 +28,11 @@ use Exception;
 
 include 'Throttle.php';
 
-$version = '1.72.92';
+$version = '1.72.93';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '1.72.92';
+    const VERSION = '1.72.93';
 
     public static $loop;
     public static $kernel;
--- python/ccxt/async_support/kucoinfutures.py
+++ python/ccxt/async_support/kucoinfutures.py
@@ -62,6 +62,7 @@ class kucoinfutures(kucoin):
                 'fetchIndexOHLCV': False,
                 'fetchL3OrderBook': True,
                 'fetchLedger': True,
+                'fetchLeverageTiers': True,
                 'fetchMarkets': True,
                 'fetchMarkOHLCV': False,
                 'fetchMyTrades': True,
@@ -112,6 +113,7 @@ class kucoinfutures(kucoin):
                     'get': {
                         'contracts/active': 1,
                         'contracts/{symbol}': 1,
+                        'contracts/risk-limit/{symbol}': 1,
                         'ticker': 1,
                         'level2/snapshot': 1.33,
                         'level2/depth{limit}': 1,
@@ -1522,3 +1524,50 @@ class kucoinfutures(kucoin):
 
     async def fetch_ledger(self, code=None, since=None, limit=None, params={}):
         raise BadRequest(self.id + ' has no method fetchLedger')
+
+    async def fetch_leverage_tiers(self, symbol=None, params={}):
+        await self.load_markets()
+        if symbol is None:
+            raise ArgumentsRequired(self.id + ' fetchLeverageTiers() requires a symbol argument')
+        market = self.market(symbol)
+        if not market['contract']:
+            raise BadRequest(self.id + ' fetchLeverageTiers() supports contract markets only')
+        request = {
+            'symbol': market['id'],
+        }
+        response = await self.futuresPublicGetContractsRiskLimitSymbol(self.extend(request, params))
+        #
+        #    {
+        #        "code": "200000",
+        #        "data": [
+        #            {
+        #                "symbol": "ETHUSDTM",
+        #                "level": 1,
+        #                "maxRiskLimit": 300000,
+        #                "minRiskLimit": 0,
+        #                "maxLeverage": 100,
+        #                "initialMargin": 0.0100000000,
+        #                "maintainMargin": 0.0050000000
+        #            },
+        #            ...
+        #        ]
+        #    }
+        #
+        data = self.safe_value(response, 'data')
+        tiers = {}
+        for i in range(0, len(data)):
+            tier = data[i]
+            symbol = self.safe_symbol(self.safe_string(tier, 'symbol'))
+            if not (symbol in tiers):
+                tiers[symbol] = []
+            market = self.market(symbol)
+            tiers[symbol].append({
+                'tier': self.safe_number(tier, 'level'),
+                'notionalCurrency': market['base'],
+                'notionalFloor': self.safe_number(tier, 'minRiskLimit'),
+                'notionalCap': self.safe_number(tier, 'maxRiskLimit'),
+                'maintenanceMarginRate': self.safe_number(tier, 'maintainMargin'),
+                'maxLeverage': self.safe_number(tier, 'maxLeverage'),
+                'info': tier,
+            })
+        return tiers
