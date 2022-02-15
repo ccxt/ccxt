7b13f1dd04543c354c4348e6b60112d7c7aa0fab
Travis CI <travis@travis-ci.org>
2022-02-15 18:59:11 +0000

    1.73.12
    [ci skip]

--- python/ccxt/test/test_position.py
+++ python/ccxt/test/test_position.py
@@ -8,14 +8,14 @@ except NameError:
 def test_position(exchange, position, symbol, now):
     assert position
     assert 'id' in position
-    assert isinstance(position['id'], basestring)
+    assert position['id'] is None or isinstance(position['id'], basestring)
     assert 'timestamp' in position
     assert isinstance(position['timestamp'], numbers.Real)
-    assert position['timestamp'] > 1230940800000  # 03 Jan 2009 - first block
+    assert position['timestamp'] > 1230940800000  # 03 Jan 2009 - first cryptocurrency block creation time
     assert position['timestamp'] < now
     assert 'datetime' in position
     assert position['datetime'] == exchange.iso8601(position['timestamp'])
     assert 'symbol' in position
-    assert position['symbol'] == symbol
+    assert symbol is None or position['symbol'] == symbol
     assert 'info' in position
     assert position['info']
