c06df994144c8af671c9cb47853f4a98168e9084
Travis CI <travis@travis-ci.org>
2022-02-19 17:22:52 +0000

    1.73.51
    [ci skip]

--- python/ccxt/async_support/liquid.py
+++ python/ccxt/async_support/liquid.py
@@ -220,11 +220,11 @@ class liquid(Exchange):
                 'product_disabled': BadSymbol,  # {"errors":{"order":["product_disabled"]}}
             },
             'commonCurrencies': {
+                'BIFI': 'BIFIF',
                 'HOT': 'HOT Token',
                 'MIOTA': 'IOTA',  # https://github.com/ccxt/ccxt/issues/7487
-                'TON': 'Tokamak Network',
-                'BIFI': 'Bifrost Finance',
                 'P-BTC': 'BTC',
+                'TON': 'Tokamak Network',
             },
             'options': {
                 'cancelOrderException': True,
