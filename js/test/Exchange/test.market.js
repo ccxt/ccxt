75ff708484f78cf64858968a510a2c8bf62cfaa5
Igor Kroitor <igor.kroitor@gmail.com>
2022-02-19 20:42:01 +0300

    test.market.js edits

--- js/test/Exchange/test.market.js
+++ js/test/Exchange/test.market.js
@@ -15,18 +15,15 @@ module.exports = (exchange, market, method) => {
         'quote':   'USD',      // unified uppercase string, quote currency, 3 or more letters
         'taker':   0.0011,     // taker fee, for example, 0.0011 = 0.11%
         'maker':   0.0009,     // maker fee, for example, 0.0009 = 0.09%
-        //---------------------------------------------------------------------
-        // commented temporarily to bring markets to consistency first
-        'baseId':  'btc',     // exchange-specific base currency id
-        'quoteId': 'usd',     // exchange-specific quote currency id
-        'active': true,       // boolean, market status
-        // uncomment these lines to test the futures keys
+        'baseId':  'btc',      // exchange-specific base currency id
+        'quoteId': 'usd',      // exchange-specific quote currency id
+        'active': true,        // boolean, market status
         'type': 'spot',
         'linear': undefined,
         'inverse': undefined,
         'spot': true,
-        'swap': false,     // doesn't exist on binance...
-        'future': false,   // doesn't exist on binance...
+        'swap': false,
+        'future': false,
         'option': false,
         'margin': false,
         'contract': false,
@@ -37,36 +34,38 @@ module.exports = (exchange, market, method) => {
         'strike': 56000,
         'settle': undefined,
         'settleId': undefined,
-        'precision': {        // number of decimal digits "after the dot"
-             'price': 8,       // integer
-             'amount': 8,      // integer
-             'cost': 8,        // integer
-         },
-        'limits': {           // value limits when placing orders on this market
-             'amount': {
-                 'min': 0.01,  // order amount should be > min
-                 'max': 1000,  // order amount should be < max
-             },
-             'price': {
-                 'min': 0.01,  // order price should be > min
-                 'max': 1000,  // order price should be < max
-             },
-             'cost':  { // order cost = price * amount
-                 'min': 0.01,  // order cost should be > min
-                 'max': 1000,  // order cost should be < max
-             },
+        'precision': {
+            'price': 8,        // integer
+            'amount': 8,       // integer
+            'cost': 8,         // integer
         },
-        'info': {}, // the original unparsed market info from the exchange
-        //---------------------------------------------------------------------
+        'limits': {            // value limits when placing orders on this market
+            'amount': {
+                'min': 0.01,   // order amount should be > min
+                'max': 1000,   // order amount should be < max
+            },
+            'price': {
+                'min': 0.01,   // order price should be > min
+                'max': 1000,   // order price should be < max
+            },
+            // order cost = price * amount
+            'cost':  {
+                'min': 0.01,   // order cost should be > min
+                'max': 1000,   // order cost should be < max
+            },
+        },
+        'info': {},            // the original unparsed market info from the exchange
     }
 
-    const keys = Object.keys (format);
+    const keys = Object.keys (format)
     for (let i = 0; i < keys.length; i++) {
         const key = keys[i]
-        assert (key in market, key + JSON.stringify (market));
+        assert (key in market, key + ' not found in ' + JSON.stringify (market));
     }
-    assert ((market['taker'] === undefined) || (typeof market['taker'] === 'number'));
-    assert ((market['maker'] === undefined) || (typeof market['maker'] === 'number'));
+
+    assert ((market['taker'] === undefined) || (typeof market['taker'] === 'number'))
+    assert ((market['maker'] === undefined) || (typeof market['maker'] === 'number'))
+
     if (market['contract']) {
         assert (market['linear'] === !market['inverse']);
     } else {
@@ -84,7 +83,12 @@ module.exports = (exchange, market, method) => {
         'option': true,
     };
     const type = market['type'];
-    assert (type in validTypes);
+    //
+    // binance has type = 'delivery'
+    // https://github.com/ccxt/ccxt/issues/11121
+    //
+    // assert (type in validTypes);
+    //
     const types = Object.keys (validTypes);
     for (let i = 0; i < types.length; i++) {
         const entry = types[i];
