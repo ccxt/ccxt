3082718877c625de54ff0de9c5c542ad4a085182
T.T <7117978+ttodua@users.noreply.github.com>
2022-02-21 23:22:23 +0400

    Leverage methods

--- js/currencycom.js
+++ js/currencycom.js
@@ -3,7 +3,7 @@
 //  ---------------------------------------------------------------------------
 
 const Exchange = require ('./base/Exchange');
-const { ExchangeError, ArgumentsRequired, ExchangeNotAvailable, InsufficientFunds, OrderNotFound, InvalidOrder, DDoSProtection, InvalidNonce, AuthenticationError, BadRequest } = require ('./base/errors');
+const { BadSymbol, ExchangeError, ArgumentsRequired, ExchangeNotAvailable, InsufficientFunds, OrderNotFound, InvalidOrder, DDoSProtection, InvalidNonce, AuthenticationError, BadRequest } = require ('./base/errors');
 const { TICK_SIZE } = require ('./base/functions/number');
 const Precise = require ('./base/Precise');
 
@@ -63,7 +63,8 @@ module.exports = class currencycom extends Exchange {
                 'fetchL2OrderBook': true,
                 'fetchLedger': undefined,
                 'fetchLedgerEntry': undefined,
-                'fetchLeverageTiers': undefined,
+                'fetchLeverage': true,
+                'fetchLeverageTiers': true,
                 'fetchMarkets': true,
                 'fetchMarkOHLCV': false,
                 'fetchMyTrades': true,
@@ -217,6 +218,7 @@ module.exports = class currencycom extends Exchange {
                     'Order would trigger immediately.': InvalidOrder,
                     'Account has insufficient balance for requested action.': InsufficientFunds,
                     'Rest API trading is not enabled.': ExchangeNotAvailable,
+                    'Only leverage symbol allowed here:': BadSymbol, // when you fetchLeverage for non-leverage symbols, like 'BTC/USDT' instead of 'BTC/USDT_LEVERAGE': {"code":"-1128","msg":"Only leverage symbol allowed here: BTC/USDT"}
                 },
                 'exact': {
                     '-1000': ExchangeNotAvailable, // {"code":-1000,"msg":"An unknown error occured while processing the request."}
@@ -1163,6 +1165,56 @@ module.exports = class currencycom extends Exchange {
         //     ]
         //
         return this.parseTrades (response, market, since, limit);
+    }
+
+    async fetchLeverage (symbol, params = {}) {
+        await this.loadMarkets ();
+        const market = this.market (symbol);
+        const request = {
+            'symbol': market['id'],
+        };
+        const response = await this.privateGetV2LeverageSettings (this.extend (request, params));
+        //
+        // {
+        //     "values": [ 1, 2, 5, 10, ],
+        //     "value": "10",
+        // }
+        //
+        return this.safeNumber (response, 'value');
+    }
+
+    async fetchLeverageTiers (symbol = undefined, params = {}) {
+        await this.loadMarkets ();
+        if (symbol === undefined) {
+            throw new ArgumentsRequired (this.id + ' fetchLeverageTiers() requires a symbol argument');
+        }
+        const market = this.market (symbol);
+        const request = {
+            'symbol': market['id'],
+        };
+        const response = await this.privateGetV2LeverageSettings (this.extend (request, params));
+        //
+        // {
+        //     "values": [ 1, 2, 5, 10, ],
+        //     "value": "10",
+        // }
+        //
+        const data = this.safeValue (response, 'values');
+        const result = {};
+        result[symbol] = [];
+        for (let i = 0; i < data.length; i++) {
+            const value = data[i];
+            result[symbol].push ({
+                'tier': undefined,
+                'notionalCurrency': undefined,
+                'notionalFloor': undefined,
+                'notionalCap': undefined,
+                'maintenanceMarginRate': undefined,
+                'maxLeverage': value,
+                'info': value,
+            });
+        }
+        return result;
     }
 
     sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
