7f71f6c7c0ce81b8b227a103c5b1e09636509ec7
Travis CI <travis@travis-ci.org>
2022-02-21 12:12:55 +0000

    1.73.78
    [ci skip]

--- php/wavesexchange.php
+++ php/wavesexchange.php
@@ -137,6 +137,7 @@ class wavesexchange extends Exchange {
                         'matcher/orderbook/market',
                         'matcher/orderbook/cancel',
                         'matcher/orderbook/{baseId}/{quoteId}/cancel',
+                        'matcher/orderbook/{amountAsset}/{priceAsset}/calculateFee',
                         'matcher/debug/saveSnapshots',
                         'matcher/orders/{address}/cancel',
                         'matcher/orders/cancel/{orderId}',
@@ -289,6 +290,9 @@ class wavesexchange extends Exchange {
                     ),
                 ),
             ),
+            'currencies' => array(
+                'WX' => array( 'id' => 'EMAMLxDnv3xiz8RXg8Btj33jcEw3wLczL3JKYYmuubpc', 'numericId' => null, 'code' => 'WX', 'precision' => 8 ),
+            ),
             'options' => array(
                 'allowedCandles' => 1440,
                 'accessToken' => null,
@@ -348,19 +352,51 @@ class wavesexchange extends Exchange {
         return parent::set_sandbox_mode($enabled);
     }
 
+    public function get_fees_for_asset($symbol, $side, $amount, $price, $params = array ()) {
+        $this->load_markets();
+        $market = $this->market($symbol);
+        $amount = $this->amount_to_precision($symbol, $amount);
+        $price = $this->price_to_precision($symbol, $price);
+        $request = array_merge(array(
+            'amountAsset' => $market['baseId'],
+            'priceAsset' => $market['quoteId'],
+            'orderType' => $side,
+            'amount' => $amount,
+            'price' => $price,
+        ), $params);
+        return $this->matcherPostMatcherOrderbookAmountAssetPriceAssetCalculateFee ($request);
+    }
+
     public function calculate_fee($symbol, $type, $side, $amount, $price, $takerOrMaker = 'taker', $params = array ()) {
-        $settings = $this->matcherGetMatcherSettings ();
-        $dynamic = $this->safe_get_dynamic($settings);
-        $baseMatcherFee = $this->safe_string($dynamic, 'baseFee');
+        $response = $this->get_fees_for_asset($symbol, $side, $amount, $price);
+        // {
+        //     "base":array(
+        //        "feeAssetId":"WAVES",
+        //        "matcherFee":"1000000"
+        //     ),
+        //     "discount":{
+        //        "feeAssetId":"EMAMLxDnv3xiz8RXg8Btj33jcEw3wLczL3JKYYmuubpc",
+        //        "matcherFee":"4077612"
+        //     }
+        //  }
+        $isDiscountFee = $this->safe_value($params, 'isDiscountFee', false);
+        $mode = null;
+        if ($isDiscountFee) {
+            $mode = $this->safe_value($response, 'discount');
+        } else {
+            $mode = $this->safe_value($response, 'base');
+        }
+        $matcherFee = $this->safe_string($mode, 'matcherFee');
+        $feeAssetId = $this->safe_string($mode, 'feeAssetId');
+        $feeAsset = $this->safe_currency_code($feeAssetId);
+        $adjustedMatcherFee = $this->currency_from_precision($feeAsset, $matcherFee);
         $amountAsString = $this->number_to_string($amount);
         $priceAsString = $this->number_to_string($price);
-        $wavesMatcherFee = $this->currency_from_precision('WAVES', $baseMatcherFee);
-        $feeCost = $this->fee_to_precision($symbol, $this->parse_number($wavesMatcherFee));
-        $feeRate = Precise::string_div($wavesMatcherFee, Precise::string_mul($amountAsString, $priceAsString));
+        $feeCost = $this->fee_to_precision($symbol, $this->parse_number($adjustedMatcherFee));
+        $feeRate = Precise::string_div($adjustedMatcherFee, Precise::string_mul($amountAsString, $priceAsString));
         return array(
             'type' => $takerOrMaker,
-            'currency' => 'WAVES',
-            // To calculate the rate since Waves has a fixed fee.
+            'currency' => $feeAsset,
             'rate' => $this->parse_number($feeRate),
             'cost' => $this->parse_number($feeCost),
         );
@@ -623,7 +659,8 @@ class wavesexchange extends Exchange {
         } else if ($api === 'matcher') {
             if ($method === 'POST') {
                 $headers = array(
-                    'content-type' => 'application/json',
+                    'Accept' => 'application/json',
+                    'Content-Type' => 'application/json',
                 );
                 $body = $this->json($query);
             } else {
@@ -1108,109 +1145,74 @@ class wavesexchange extends Exchange {
         $matcherPublicKey = $this->get_matcher_public_key();
         $amountAsset = $this->get_asset_id($market['baseId']);
         $priceAsset = $this->get_asset_id($market['quoteId']);
-        $amount = $this->amount_to_precision($symbol, $amount);
         $isMarketOrder = ($type === 'market');
         if (($isMarketOrder) && ($price === null)) {
             throw new InvalidOrder($this->id . ' createOrder() requires a $price argument for ' . $type . ' orders to determine the max $price for buy and the min $price for sell');
         }
-        $price = $this->price_to_precision($symbol, $price);
         $orderType = ($side === 'buy') ? 0 : 1;
         $timestamp = $this->milliseconds();
         $defaultExpiryDelta = $this->safe_integer($this->options, 'createOrderDefaultExpiry', 2419200000);
         $expiration = $this->sum($timestamp, $defaultExpiryDelta);
-        $settings = $this->matcherGetMatcherSettings ();
+        $matcherFees = $this->get_fees_for_asset($symbol, $side, $amount, $price);
         // {
-        //   "orderVersions" => array(
-        //     1,
-        //     2,
-        //     3
-        //   ),
-        //   "success" => true,
-        //   "matcherPublicKey" => "9cpfKN9suPNvfeUNphzxXMjcnn974eme8ZhWUjaktzU5",
-        //   "orderFee" => {
-        //     "dynamic" => {
-        //       "baseFee" => 300000,
-        //       "rates" => array(
-        //         "34N9YcEETLWn93qYQ64EsP1x89tSruJU44RrEMSXXEPJ" => 1.0257813,
-        //         "62LyMjcr2DtiyF5yVXFhoQ2q414VPPJXjsNYp72SuDCH" => 0.01268146,
-        //         "HZk1mbfuJpmxU1Fs4AX5MWLVYtctsNcg6e2C6VKqK8zk" => 0.05232404,
-        //         "8LQW8f7P5d5PZM7GtZEBgaqRPGSzS3DfPuiXrURJ4AJS" => 0.00023985,
-        //         "4LHHvYGNKJUg5hj65aGD5vgScvCBmLpdRFtjokvCjSL8" => 19.5967716,
-        //         "474jTeYx2r2Va35794tCScAXWJG9hU2HcgxzMowaZUnu" => 0.00937073,
-        //         "DG2xFkPdDwKUoBkzGAhQtLpSGzfXLiCYPEzeKH2Ad24p" => 2.19825,
-        //         "B3uGHFRpSUuGEDWjqB9LWWxafQj8VTvpMucEyoxzws5H" => 0.03180264,
-        //         "zMFqXuoyrn5w17PFurTqxB7GsS71fp9dfk6XFwxbPCy" => 0.00996631,
-        //         "5WvPKSJXzVE2orvbkJ8wsQmmQKqTv9sGBPksV4adViw3" => 0.03254476,
-        //         "WAVES" => 1,
-        //         "BrjUWjndUanm5VsJkbUip8VRYy6LWJePtxya3FNv4TQa" => 0.03703704
-        //       }
+        //     "base":array(
+        //        "feeAssetId":"WAVES", // varies depending on the trading pair
+        //        "matcherFee":"1000000"
+        //     ),
+        //     "discount":{
+        //        "feeAssetId":"EMAMLxDnv3xiz8RXg8Btj33jcEw3wLczL3JKYYmuubpc",
+        //        "matcherFee":"4077612"
         //     }
-        //   ),
-        //   "networkByte" => 87,
-        //   "matcherVersion" => "2.1.4.8",
-        //   "status" => "SimpleResponse",
-        //   "priceAssets" => array(
-        //     "Ft8X1v1LTa1ABafufpaCWyVj8KkaxUWE6xBhW6sNFJck",
-        //     "DG2xFkPdDwKUoBkzGAhQtLpSGzfXLiCYPEzeKH2Ad24p",
-        //     "34N9YcEETLWn93qYQ64EsP1x89tSruJU44RrEMSXXEPJ",
-        //     "Gtb1WRznfchDnTh37ezoDTJ4wcoKaRsKqKjJjy7nm2zU",
-        //     "2mX5DzVKWrAJw8iwdJnV2qtoeVG9h5nTDpTqC1wb1WEN",
-        //     "8LQW8f7P5d5PZM7GtZEBgaqRPGSzS3DfPuiXrURJ4AJS",
-        //     "WAVES",
-        //     "474jTeYx2r2Va35794tCScAXWJG9hU2HcgxzMowaZUnu",
-        //     "zMFqXuoyrn5w17PFurTqxB7GsS71fp9dfk6XFwxbPCy",
-        //     "62LyMjcr2DtiyF5yVXFhoQ2q414VPPJXjsNYp72SuDCH",
-        //     "HZk1mbfuJpmxU1Fs4AX5MWLVYtctsNcg6e2C6VKqK8zk",
-        //     "B3uGHFRpSUuGEDWjqB9LWWxafQj8VTvpMucEyoxzws5H",
-        //     "5WvPKSJXzVE2orvbkJ8wsQmmQKqTv9sGBPksV4adViw3",
-        //     "BrjUWjndUanm5VsJkbUip8VRYy6LWJePtxya3FNv4TQa",
-        //     "4LHHvYGNKJUg5hj65aGD5vgScvCBmLpdRFtjokvCjSL8"
-        //   )
-        // }
-        $dynamic = $this->safe_get_dynamic($settings);
-        $baseMatcherFee = $this->safe_string($dynamic, 'baseFee');
-        $wavesMatcherFee = $this->currency_from_precision('WAVES', $baseMatcherFee);
-        $rates = $this->safe_get_rates($dynamic);
-        // choose sponsored assets from the list of $priceAssets above
-        $priceAssets = is_array($rates) ? array_keys($rates) : array();
+        //  }
+        $base = $this->safe_value($matcherFees, 'base');
+        $baseFeeAssetId = $this->safe_string($base, 'feeAssetId');
+        $baseFeeAsset = $this->safe_currency_code($baseFeeAssetId);
+        $baseMatcherFee = $this->safe_string($base, 'matcherFee');
+        $discount = $this->safe_value($matcherFees, 'discount');
+        $discountFeeAssetId = $this->safe_string($discount, 'feeAssetId');
+        $discountFeeAsset = $this->safe_currency_code($discountFeeAssetId);
+        $discountMatcherFee = $this->safe_string($discount, 'matcherFee');
         $matcherFeeAssetId = null;
         $matcherFee = null;
-        if (is_array($params) && array_key_exists('feeAssetId', $params)) {
-            $matcherFeeAssetId = $params['feeAssetId'];
-        } else if (is_array($this->options) && array_key_exists('feeAssetId', $this->options)) {
-            $matcherFeeAssetId = $this->options['feeAssetId'];
-        } else {
-            $balances = $this->fetch_balance();
-            $floatWavesMatcherFee = floatval($wavesMatcherFee);
-            if ($balances['WAVES']['free'] > $floatWavesMatcherFee) {
-                $matcherFeeAssetId = 'WAVES';
-                $matcherFee = $baseMatcherFee;
+        // check first if user supplied asset fee is valid
+        if ((is_array($params) && array_key_exists('feeAsset', $params)) || (is_array($this->options) && array_key_exists('feeAsset', $this->options))) {
+            $feeAsset = $this->safe_string($params, 'feeAsset', $this->safe_string($this->options, 'feeAsset'));
+            $feeCurrency = $this->currency($feeAsset);
+            $matcherFeeAssetId = $this->safe_string($feeCurrency, 'id');
+        }
+        $balances = $this->fetch_balance();
+        if ($matcherFeeAssetId !== null) {
+            if ($baseFeeAssetId !== $matcherFeeAssetId && $discountFeeAssetId !== $matcherFeeAssetId) {
+                throw new InvalidOrder($this->id . ' asset fee must be ' . $baseFeeAsset . ' or ' . $discountFeeAsset);
+            }
+            $matcherFeeAsset = $this->safe_currency_code($matcherFeeAssetId);
+            $rawMatcherFee = ($matcherFeeAssetId === $baseFeeAssetId) ? $baseMatcherFee : $discountMatcherFee;
+            $floatMatcherFee = floatval($this->currency_from_precision($matcherFeeAsset, $rawMatcherFee));
+            if ((is_array($balances) && array_key_exists($matcherFeeAsset, $balances)) && ($balances[$matcherFeeAsset]['free'] >= $floatMatcherFee)) {
+                $matcherFee = intval($rawMatcherFee);
             } else {
-                for ($i = 0; $i < count($priceAssets); $i++) {
-                    $assetId = $priceAssets[$i];
-                    $code = $this->safe_currency_code($assetId);
-                    $balance = $this->safe_string($this->safe_value($balances, $code, array()), 'free');
-                    $assetFee = Precise::string_mul($rates[$assetId], $wavesMatcherFee);
-                    if (($balance !== null) && Precise::string_gt($balance, $assetFee)) {
-                        $matcherFeeAssetId = $assetId;
-                        break;
-                    }
+                throw new InsufficientFunds($this->id . ' not enough funds of the selected asset fee');
+            }
+        }
+        if ($matcherFeeAssetId === null) {
+            // try to the pay the fee using the $base first then $discount asset
+            $floatBaseMatcherFee = floatval($this->currency_from_precision($baseFeeAsset, $baseMatcherFee));
+            if ((is_array($balances) && array_key_exists($baseFeeAsset, $balances)) && ($balances[$baseFeeAsset]['free'] >= $floatBaseMatcherFee)) {
+                $matcherFeeAssetId = $baseFeeAssetId;
+                $matcherFee = intval($baseMatcherFee);
+            } else {
+                $floatDiscountMatcherFee = floatval($this->currency_from_precision($discountFeeAsset, $discountMatcherFee));
+                if ((is_array($balances) && array_key_exists($discountFeeAsset, $balances)) && ($balances[$discountFeeAsset]['free'] >= $floatDiscountMatcherFee)) {
+                    $matcherFeeAssetId = $discountFeeAssetId;
+                    $matcherFee = intval($discountMatcherFee);
                 }
             }
         }
         if ($matcherFeeAssetId === null) {
-            throw InsufficientFunds ($this->id . ' not enough funds to cover the fee, specify feeAssetId in $params or options, or buy some WAVES');
+            throw new InsufficientFunds($this->id . ' not enough funds on none of the eligible asset fees');
         }
-        if ($matcherFee === null) {
-            $wavesPrecision = $this->safe_integer($this->options, 'wavesPrecision', 8);
-            $rate = $this->safe_string($rates, $matcherFeeAssetId);
-            $code = $this->safe_currency_code($matcherFeeAssetId);
-            $currency = $this->currency($code);
-            $newPrecison = $wavesPrecision - $currency['precision'];
-            $matcherFee = $this->from_precision(Precise::string_mul($rate, $baseMatcherFee), $newPrecison);
-            // ceil the fee
-            $matcherFee = Precise::string_div(Precise::string_add($matcherFee, '1'), '1', 0);
-        }
+        $amount = $this->amount_to_precision($symbol, $amount);
+        $price = $this->price_to_precision($symbol, $price);
         $byteArray = [
             $this->number_to_be(3, 1),
             $this->base58_to_binary($this->apiKey),
