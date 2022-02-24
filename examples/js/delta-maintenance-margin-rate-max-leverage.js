4bf45413b9e594d3c583dc71afcdb9b94ff3c61a
Sam Germain <sam.germain@usask.ca>
2022-02-24 07:42:00 -0600

    removed price param from examples/js/delta-maintenance-margin-rate-max-leverage.js

--- examples/js/delta-maintenance-margin-rate-max-leverage.js
+++ examples/js/delta-maintenance-margin-rate-max-leverage.js
@@ -2,52 +2,44 @@ const ccxt = require('../../ccxt');
 
 console.log ('CCXT Version:', ccxt.version);
 
-function getMaxLeverage (market, positionSize, price) {
+function getMaxLeverage (market, positionSize) {
     /**
      * Equation taken from https://www.delta.exchange/contracts/
      * @param market: CCXT market
      * @param positionSize: The value of the position in quote currency
-     * @param price: The latest market price
      * @return: The maximum leverage available for the market for the given position size
      */
     const info = market['info'];
     const maxLeverageNotional = Number (info['max_leverage_notional']);
     const initialMarginScalingFactor = Number (info['initial_margin_scaling_factor']);
     let initialMargin = Number (info['initial_margin']);
-    const positionSizeLimit = Number (info['position_size_limit']);
 
     if (positionSize <= maxLeverageNotional) {
         const initialMarginRatio = initialMargin / 100;
         return 1 / initialMarginRatio;
-    } else if (positionSize <= (positionSizeLimit * market['contractSize'] * price)) {
+    } else {
         initialMargin = initialMargin + (initialMarginScalingFactor * (positionSize - maxLeverageNotional));
         const initialMarginRatio = initialMargin / 100;
         return 1 / initialMarginRatio;
-    } else {
-        throw new Error ('positionSize is too high for ' + market['symbol']);
     }
 }
 
-function getMaintenanceMarginRate (market, positionSize, price) {
+function getMaintenanceMarginRate (market, positionSize) {
     /**
      * Equation taken from https://www.delta.exchange/contracts/
      * @param market: CCXT market
      * @param positionSize: The value of the position in quote currency
-     * @param price: The latest market price
      * @return: The maintenance margin rate as a percentage for the market with the given position size
      */
     const info = market['info'];
     const maxLeverageNotional = Number (info['max_leverage_notional']);
     const maintenanceMarginScalingFactor = Number (info['maintenance_margin_scaling_factor']);
     const maintenanceMargin = Number (info['maintenance_margin']);
-    const positionSizeLimit = Number (info['position_size_limit']);
 
     if (positionSize <= maxLeverageNotional) {
         return maintenanceMargin;
-    } else if (positionSize <= (positionSizeLimit * market['contractSize'] * price)) {
-        return maintenanceMargin + (maintenanceMarginScalingFactor * (positionSize - maxLeverageNotional));
     } else {
-        throw new Error ('positionSize is too high for ' + market['symbol']);
+        return maintenanceMargin + (maintenanceMarginScalingFactor * (positionSize - maxLeverageNotional));
     }
 }
 
@@ -58,11 +50,10 @@ async function main () {
     
     const symbol = 'ADA/USDT:USDT';
     const market = exchange.market (symbol);
-    const ticker = exchange.fetchTicker (symbol);
 
     // Gets the maximum leverage and maintenance margin rate for a position worth 100,000 USDT on the ADA/USDT:USDT market
-    const maxLeverage = getMaxLeverage(market, 100000, ticker['close']);
-    const maintenanceMarginRate = getMaintenanceMarginRate(market, 100000, ticker['close']);
+    const maxLeverage = getMaxLeverage(market, 100000);
+    const maintenanceMarginRate = getMaintenanceMarginRate(market, 100000);
     console.log(maxLeverage, maintenanceMarginRate);
 }
 
