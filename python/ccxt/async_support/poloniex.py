990f8069ee46b460dca2cf201feb70b1649511e4
Travis CI <travis@travis-ci.org>
2022-02-27 08:10:58 +0000

    1.74.40
    [ci skip]

--- python/ccxt/async_support/poloniex.py
+++ python/ccxt/async_support/poloniex.py
@@ -69,11 +69,9 @@ class poloniex(Exchange):
                 'withdraw': True,
             },
             'timeframes': {
-                '1m': 60,
                 '5m': 300,
                 '15m': 900,
                 '30m': 1800,
-                '1h': 3600,
                 '2h': 7200,
                 '4h': 14400,
                 '1d': 86400,
