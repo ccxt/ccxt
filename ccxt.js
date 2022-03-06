949c138b265906d898962b35824afe471cfc14b3
Travis CI <travis@travis-ci.org>
2022-03-06 22:44:06 +0000

    1.75.10
    [ci skip]

--- ccxt.js
+++ ccxt.js
@@ -36,7 +36,7 @@ const Exchange  = require ('./js/base/Exchange')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '1.75.9'
+const version = '1.75.10'
 
 Exchange.ccxtVersion = version
 
--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -28,11 +28,11 @@ use Exception;
 
 include 'Throttle.php';
 
-$version = '1.75.9';
+$version = '1.75.10';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '1.75.9';
+    const VERSION = '1.75.10';
 
     public static $loop;
     public static $kernel;
