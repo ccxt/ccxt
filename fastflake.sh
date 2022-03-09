34846339c8650cc90d99951fb3847c018cd0b8fe
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-03-09 13:06:57 -0800

    derp

--- fastflake.sh
+++ fastflake.sh
@@ -7,7 +7,7 @@ command="flake8 --ignore=F722,F841,F821,W504,E402,E501"
 execute="${command} ${files}"
 printf "\n${execute}\n\n"
 
-if [[ -n ${files] ]]; then
+if [[ -n ${files} ]]; then
   # only one variable expansion happens here so it is safe
   ${execute}
 fi
