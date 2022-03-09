acb227a8e967ca6c8894f80b1bd7ef1dbfb122c7
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-03-09 03:53:35 -0800

    a better wrapper i guess

--- fastflake.sh
+++ fastflake.sh
@@ -3,4 +3,7 @@
 # saves time over the other running flake8 on all the files
 
 files=$(git diff --name-only origin/master | sed -n '/py$/p' | xargs)
-flake8 --ignore=F722,F841,F821,W504,E402,E501 ${files}
+command="flake8 --ignore=F722,F841,F821,W504,E402,E501"
+exe="${command} ${files}"
+printf "\n${exe}\n\n"
+${exe}
