685ca6ace4f17080cbbda13c2ba6d67346c48919
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-03-09 05:12:02 -0800

    propogate exit code

--- qa.py
+++ qa.py
@@ -1,7 +1,8 @@
 import os
 
 if os.name == 'posix':
-    os.system('./fastflake.sh')
+    code = os.WEXITSTATUS(os.system('./fastflake.sh'))
+    exit(code)
 else:
     command = 'flake8 --ignore=F722,F841,F821,W504,E402,E501 --exclude static_dependencies,node_modules,.tox python'
     print(f'\n{command}\n')
