d9e94b73cc4b7e843c20a05122d4f78e776f956a
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-03-09 04:17:55 -0800

    fix bug in windows

--- qa.py
+++ qa.py
@@ -3,4 +3,5 @@ import os
 if os.name == 'posix':
     os.system('./fastflake.sh')
 else:
-    os.system('cd python && tox -e qa && cd ..')
+    command = 'flake8 --ignore=F722,F841,F821,W504,E402,E501 --exclude static_dependencies,node_modules,.tox'
+    os.system(f'cd python && {command}')
