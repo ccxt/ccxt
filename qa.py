dc8f8dad0d4e9a555da3a07b59ecbecb7b13372f
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-03-09 04:38:33 -0800

    minor formatting

--- qa.py
+++ qa.py
@@ -3,5 +3,6 @@ import os
 if os.name == 'posix':
     os.system('./fastflake.sh')
 else:
-    command = 'flake8 --ignore=F722,F841,F821,W504,E402,E501 --exclude static_dependencies,node_modules,.tox'
-    os.system(f'cd python && {command}')
+    command = 'flake8 --ignore=F722,F841,F821,W504,E402,E501 --exclude static_dependencies,node_modules,.tox python'
+    print(f'\n{command}\n')
+    os.system(command)
