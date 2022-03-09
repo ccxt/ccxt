5379fd5c6e8d041759c68a0c50a6c276d46faa65
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-03-09 03:59:14 -0800

    final

--- fastflake.sh
+++ fastflake.sh
@@ -4,6 +4,8 @@
 
 files=$(git diff --name-only origin/master | sed -n '/py$/p' | xargs)
 command="flake8 --ignore=F722,F841,F821,W504,E402,E501"
-exe="${command} ${files}"
+execute="${command} ${files}"
 printf "\n${exe}\n\n"
-${exe}
+
+# only one variable expansion happens here so it is safe
+${execute}
