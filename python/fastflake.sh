f6b9f2c1e725a106f612b88b29801d86815af568
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-03-09 17:57:43 -0800

    qa-stuff

--- python/fastflake.sh
+++ python/fastflake.sh
@@ -2,7 +2,7 @@
 
 # saves time over the other running flake8 on all the files
 
-files=$(git diff --name-only origin/master | sed -n '/py$/p' | xargs)
+files=$(git diff --name-only --relative origin/master | sed -n '/py$/p' | xargs)
 command="flake8 --ignore=F722,F841,F821,W504,E402,E501"
 execute="${command} ${files}"
 printf "\n${execute}\n\n"
