91d941735176d050f3d276d9b92dce8cd18164c7
Travis CI <travis@travis-ci.org>
2022-03-11 12:35:39 +0000

    1.75.81
    [ci skip]

--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -28,11 +28,11 @@ use Exception;
 
 include 'Throttle.php';
 
-$version = '1.75.80';
+$version = '1.75.81';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '1.75.80';
+    const VERSION = '1.75.81';
 
     public static $loop;
     public static $kernel;
--- python/ccxt/async_support/binance.py
+++ python/ccxt/async_support/binance.py
@@ -4458,7 +4458,7 @@ class binance(Exchange):
                     onePlusMaintenanceMarginPercentageString = Precise.string_add('-1', maintenanceMarginPercentageString)
                 inner = Precise.string_mul(liquidationPriceString, onePlusMaintenanceMarginPercentageString)
                 leftSide = Precise.string_add(inner, entryPriceSignString)
-                quotePrecision = self.safe_string(precision, 'quote')
+                quotePrecision = self.safe_integer(precision, 'quote')
                 collateralString = Precise.string_div(Precise.string_mul(leftSide, contractsAbs), '1', quotePrecision)
             else:
                 # walletBalance = (contracts * contractSize) * (±1/entryPrice - (±1 - mmp) / liquidationPrice)
@@ -4471,7 +4471,7 @@ class binance(Exchange):
                     entryPriceSignString = Precise.string_mul('-1', entryPriceSignString)
                 leftSide = Precise.string_mul(contractsAbs, contractSizeString)
                 rightSide = Precise.string_sub(Precise.string_div('1', entryPriceSignString), Precise.string_div(onePlusMaintenanceMarginPercentageString, liquidationPriceString))
-                basePrecision = self.safe_string(precision, 'base')
+                basePrecision = self.safe_integer(precision, 'base')
                 collateralString = Precise.string_div(Precise.string_mul(leftSide, rightSide), '1', basePrecision)
         else:
             collateralString = self.safe_string(position, 'isolatedMargin')
