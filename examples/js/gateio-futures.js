1c32898ae8e2870b5598631b29fdd92aa62416d6
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-03-11 17:58:00 +0000

    js example

--- examples/js/gateio-futures.js
+++ examples/js/gateio-futures.js
@@ -1,18 +1,8 @@
 const ccxt = require ('../../ccxt');
 
-//testnet
-// const exchange = new ccxt.gateio ({
-//     'apiKey': '39c98db8245cb3e7c2dc804fd4e3cfad',
-//     'secret': '995bbde3d3ec4bc5bff51f45c14c1df5942e560e1ee3a14201edff258a488cdf',
-//     'options': {
-//         'defaultType': 'future',
-//     },
-// })
-
 const exchange = new ccxt.gateio ({
-    'apiKey': '9c0e5229f3e078b32d472094393977cc',
-    'secret': 'a770943b223f514a4ea61c50fe3a09f25d543637b07dfc8631a714327ce20267',
-    'password': '670667',
+    'apiKey': 'YOUR_API_KEY',
+    'secret': 'YOUR_SECRET_KEY',
     'options': {
         'defaultType': 'future',
     },
@@ -20,6 +10,7 @@ const exchange = new ccxt.gateio ({
 
 ;(async () => {
     // exchange.setSandboxMode (true)
+
     const markets = await exchange.loadMarkets ()
 
     exchange.verbose = true // uncomment for debugging purposes if necessary
@@ -55,17 +46,27 @@ const exchange = new ccxt.gateio ({
         const amount = 1
         const price = 55
 
-        const stopPrice = 40
-        params = {
+        const stopPrice = 130
+        const params = {
             'stopPrice': stopPrice,
         }
+        //set leverage
+        const leverage = await exchange.setLeverage(3, symbol);
+        console.log(leverage)
+
         // placing an order
         const order = await exchange.createOrder (symbol, type, side, amount, price, params)
         console.log (order)
 
         // canceling an order
-        const cancel = await exchange.cancelOrder (order['id'], symbol)
+        const cancelParams = {
+            isStop: true,
+        };
+        const cancel = await exchange.cancelOrder (order['id'], symbol, cancelParams)
         console.log (cancel)
+
+        //reset leverage
+        exchange.setLeverage(1, symbol);
     } catch (e) {
         console.log (e.constructor.name, e.message)
     }
