fbc73a6dbc9e64cfe89c2ad261281c5fcdbfe2d8
T.T <7117978+ttodua@users.noreply.github.com>
2022-03-15 19:40:03 +0400

    IBKR updates - endpoints - research - testing ...

--- js/interactivebrokers.js
+++ js/interactivebrokers.js
@@ -118,6 +118,7 @@ module.exports = class interactivebrokers extends Exchange {
                 'www': 'https://www.interactivebrokers.com/',
                 'referral': '',
                 'doc': [
+                    'https://ibkr.info/article/3059', // cryptos offered by IBKR (BTC,ETH,LTC,BCH)
                     'https://www.interactivebrokers.com/en/index.php?f=5041', // main
                     'https://interactivebrokers.github.io/cpwebapi/', // Client-Portal web-api
                     'https://interactivebrokers.github.io/cpwebapi/swagger-ui.html', // Swagger
@@ -137,52 +138,53 @@ module.exports = class interactivebrokers extends Exchange {
                 },
                 'private': {
                     'get': {
-                        // ISERVER
-                        'iserver/account/trades': 1, // -
+                        // informational
+                        'trsrv/secdef/schedule': 1,                     // trading schedule up to a month for the requested contract (sample: pastebin(dot)com/BTQW7Pxc)
+                        'trsrv/futures': 1,                             // a list of non-expired future contracts (conid) for given symbol(s)
+                        'trsrv/stocks': 1,                              // a list of all stock contracts (conid) for given symbol(s)
+                        // iserver
+                        'iserver/account/trades': 1,                    // -
                         'iserver/account/{accountId}/alerts': 1,
                         'iserver/account/alert/{id}': 1,
                         'iserver/account/mta': 1,
-                        'iserver/account/orders': 1, // -
-                        'iserver/account/order/status/{orderId}': 1, // -
-                        'iserver/account/pnl/partitioned': 1, // unrealized pln (?)
+                        'iserver/account/orders': 1,                    // -
+                        'iserver/account/order/status/{orderId}': 1,    // -
+                        'iserver/account/pnl/partitioned': 1,           // [implicit] unrealized pln (?)
                         //     The below endpoint returns a list of accounts the user has trading access to,
                         //     their respective aliases and the currently selected account.
                         //     Note 1: this endpoint must be called before modifying an order or querying open orders.
                         //     Note 2: 'iserver' endpoints doesn't seem to work on free-trial accounts.
-                        'iserver/accounts': 1, // +
-                        'iserver/marketdata/snapshot': 1, // needs conid !!!!
-                        'iserver/marketdata/{conid}/unsubscribe': 1, // needs conid !!!!
+                        'iserver/accounts': 1,                          // +
+                        'iserver/marketdata/snapshot': 1,               // needs conid !!!!
+                        'iserver/marketdata/{conid}/unsubscribe': 1,    // needs conid !!!!
                         'iserver/marketdata/unsubscribeall': 1,
-                        'iserver/marketdata/history': 1, // needs conid !!!!
-                        'iserver/contract/{conid}/info': 1, // needs conid !!!!
-                        'iserver/secdef/strikes': 1, // needs conid !!!!
-                        'iserver/secdef/info': 1, // needs conid !!!!
-                        'iserver/contract/{conid}/algos': 1, // needs conid !!!!
-                        'iserver/contract/{conid}/info-and-rules': 1, // needs conid !!!!
-                        'iserver/scanner/params': 1, // scannable params (sample: pastebin(dot)com/bQh3nr35)
-                        'trsrv/secdef/schedule': 1, // trading schedule up to a month for the requested contract (sample: pastebin(dot)com/BTQW7Pxc)
-                        'trsrv/futures': 1, // a list of non-expired future contracts (conid) for given symbol(s)
-                        'trsrv/stocks': 1, // an object contains all stock contracts (conid) for given symbol(s)
-                        // PORTFOLIO
+                        'iserver/marketdata/history': 1,                // needs conid !!!!
+                        'iserver/contract/{conid}/info': 1,             // needs conid !!!!
+                        'iserver/secdef/strikes': 1,                    // needs conid !!!!
+                        'iserver/secdef/info': 1,                       // needs conid !!!!
+                        'iserver/contract/{conid}/algos': 1,            // needs conid !!!!
+                        'iserver/contract/{conid}/info-and-rules': 1,   // needs conid !!!!
+                        'iserver/scanner/params': 1,                    // scannable params (sample: pastebin(dot)com/bQh3nr35)
+                        // portfolio
                         //      In non-tiered account structures, the below endpoint returns a list of accounts for which
                         //      the user can view position and account information. This endpoint must be called prior to
                         //      calling other /portfolio endpoints for those accounts.
-                        'portfolio/accounts': 1,
-                        'portfolio/subaccounts': 1,
-                        'portfolio/{accountId}/meta': 1,
-                        'portfolio/{accountId}/allocation': 1,
-                        'portfolio/{accountId}/positions/{pageId}': 1,
-                        'portfolio/{accountId}/position/{conid}': 1,
-                        'portfolio/{accountId}/summary': 1,
-                        'portfolio/{accountId}/ledger': 1,
-                        'portfolio/positions/{conid}': 1,
+                        'portfolio/accounts': 1,                        // +
+                        'portfolio/subaccounts': 1,                     // +
+                        'portfolio/{accountId}/meta': 1,                // +
+                        'portfolio/{accountId}/allocation': 1,          // - available balance in account (sample: pastebin(dot)com/1CzqtqAZ)
+                        'portfolio/{accountId}/positions/{pageId}': 1,  // -
+                        'portfolio/{accountId}/position/{conid}': 1,    // -
+                        'portfolio/{accountId}/summary': 1,             // -
+                        'portfolio/{accountId}/ledger': 1,              // -
+                        'portfolio/positions/{conid}': 1,               // -
                         // CCP (Beta)
                         'ccp/status': 1,
                         'ccp/account': 1,
                         'ccp/positions': 1,
                         'ccp/orders': 1,
                         'ccp/trades': 1,
-                        // FYI
+                        // fyi
                         'fyi/unreadnumber': 1,
                         'fyi/settings': 1,
                         'fyi/disclaimer/{typecode}': 1,
@@ -195,39 +197,39 @@ module.exports = class interactivebrokers extends Exchange {
                         'sso/Dispatcher': 1, // uncodumented: validates the login
                     },
                     'post': {
-                        // ISERVER
-                        'iserver/auth/status': 1,
-                        'iserver/reauthenticate': 1,
-                        'iserver/account': 1,
+                        // iserver
+                        'iserver/auth/status': 1,                          // [implicit] similar to tickle (sample: pastebin(dot)com/TD5EU3z6)
+                        'iserver/reauthenticate': 1,                       // [implicit] just returns "{message: 'triggered'}"
+                        'iserver/account': 1,                              // +
                         'iserver/account/{accountId}/alert': 1,
                         'iserver/account/{accountId}/alert/activate': 1,
-                        'iserver/account/{accountId}/order': 1,
-                        'iserver/account/{accountId}/orders': 1,
-                        'iserver/account/orders/{faGroup}': 1,
-                        'iserver/reply/{replyid}': 1,
-                        'iserver/account/{accountId}/order/whatif': 1,
-                        'iserver/account/{accountId}/orders/whatif': 1,
-                        'iserver/account/{accountId}/order/{orderId}': 1,
-                        'iserver/secdef/search': 1,
-                        'iserver/scanner/run': 1,
+                        // 'iserver/account/{accountId}/order': 1,         // is being deprecated
+                        'iserver/account/{accountId}/orders': 1,           // -
+                        'iserver/account/orders/{faGroup}': 1,             // [implicit] financial advisor orders, ignoring for ccxt
+                        'iserver/reply/{replyid}': 1,                      // order place status
+                        // 'iserver/account/{accountId}/order/whatif': 1,  // is being depreciated
+                        'iserver/account/{accountId}/orders/whatif': 1,    // [implicit] allows you to preview order without actually submitting the order and you can get commission information in the response. Also supports bracket orders.
+                        'iserver/account/{accountId}/order/{orderId}': 1,  // - Modify order
+                        'iserver/secdef/search': 1,                        // - Search by underlying symbol or company name. Relays back what derivative contract(s) it has. This endpoint must be called before using /secdef/info. If company name is specified will only receive limited response: conid, companyName, companyHeader and symbol.
+                        'iserver/scanner/run': 1,                          // - run scanner to get a list of contracts
                         // portfolio
-                        'portfolio/allocation': 1,
-                        'portfolio/{accountId}/positions/invalidate': 1,
+                        'portfolio/allocation': 1,                         // - similar to 'portfolio/{accountId}/allocation' , but a consolidated view of of all the accounts returned by /portfolio/accounts
+                        'portfolio/{accountId}/positions/invalidate': 1,   // [implicit]
                         // CCP (Beta)
                         'ccp/auth/init': 1,
                         'ccp/auth/response': 1,
                         'ccp/order': 1,
-                        // FYI
+                        // fyi
                         'fyi/settings/{typecode}': 1,
                         'fyi/deliveryoptions/device': 1,
                         // others
-                        'pa/performance': 1,
-                        'pa/summary': 1,
-                        'pa/transactions': 1,
+                        'pa/performance': 1,                               // - Returns a performance (MTM) for the given accounts, if more than one account is passed, the result is consolidated.
+                        'pa/summary': 1,                                   // - Returns a summary of all account balances for the given accounts, if more than one account is passed, the result is consolidated.
+                        'pa/transactions': 1,                              // - Returns a transaction history for a given number of conids and accounts. Types of transactions include dividend payments, buy and sell transactions, transfers.
                         'ws': 1,
-                        'tickle': 1, // validates the login
-                        'logout': 1,
-                        'trsrv/secdef': 1,
+                        'tickle': 1,                                       // validates the login
+                        'logout': 1,                                       // [implicit]
+                        'trsrv/secdef': 1,                                 // needs conid !!!!
                     },
                     'put': {
                         'fyi/disclaimer/{typecode}': 1,
@@ -238,8 +240,8 @@ module.exports = class interactivebrokers extends Exchange {
                     'delete': {
                         'fyi/deliveryoptions/{deviceId}': 1,
                         'ccp/order': 1,
-                        'iserver/account/{accountId}/alert/{alertId}': 1,
-                        'iserver/account/{accountId}/order/{orderId}': 1,
+                        'iserver/account/{accountId}/alert/{alertId}': 1,  // [implicit]
+                        'iserver/account/{accountId}/order/{orderId}': 1,  // - delete order
                     },
                 },
             },
@@ -255,7 +257,7 @@ module.exports = class interactivebrokers extends Exchange {
             'precisionMode': TICK_SIZE,
             // exchange-specific options
             'options': {
-                'fetchAccountsMethod': 'privateGetIserverAccounts', // privateGetIserverAccounts, privateGetPortfolioAccounts
+                'accounts': [],
             },
             'exceptions': {
                 'broad': {
@@ -301,11 +303,19 @@ module.exports = class interactivebrokers extends Exchange {
     }
 
     async fetchAccounts (params = {}) {
-        await this.loadMarkets ();
-        let method = this.safeString (this.options, 'fetchAccountsMethod');
-        method = this.safeString (params, 'method', method);
-        params = this.omit (params, 'method');
-        const response = await this[method] (params);
+        // As a requirement by IBKR, before any other trade endpoint, this endpoint should be called at least once.
+        const responseServiceAccounts = await this.fetchServiceAccounts (params);
+        const responsePortfolioAccounts = await this.fetchPortfolioAccounts (params);
+        this.options['accounts'] = this.safeValue ();
+        return {
+            'serviceAccounts': responseServiceAccounts,
+            'portfolioAccounts': responsePortfolioAccounts,
+        };
+    }
+
+    async fetchServiceAccounts (params = {}) {
+        // await this.loadMarkets ();
+        const response = await this.privateGetIserverAccounts (params);
         //
         // iserver/accounts
         //
@@ -360,8 +370,16 @@ module.exports = class interactivebrokers extends Exchange {
         //         "sessionId": "613de523.0000000b"
         //     }
         //
+        return response;
+    }
+
+    async fetchPortfolioAccounts (params) {
+        let method = this.safeString (this.options, 'fetchPortfolioAccountsMethod', 'privateGetPortfolioAccounts');
+        method = this.safeString (params, 'method', method);
+        params = this.omit (params, 'method');
+        const response = await this[method] (params);
         //
-        // portfolio/accounts
+        // portfolio/accounts, portfolio/subaccounts (also object from portfolio/{accountId}/meta)
         //
         //     [
         //         {
@@ -391,6 +409,63 @@ module.exports = class interactivebrokers extends Exchange {
         //     ]
         //
         return response;
+    }
+
+    async swithToAccount (params = {}) {
+        const accountId = this.safeString (params, 'acctId');
+        if (accountId === undefined) {
+            throw new ArgumentsRequired (this.id + 'switchToAccount() requires "accId" parameter');
+        }
+        const response = await this.privatePostIserverAccount (params);
+        //
+        //     {
+        //         "set": true,
+        //         "acctId": "U3449298"
+        //     }
+        //
+        // Note, if account is already switched to the requested acctId , then instead of the above response, 501 http error status is thrown from api
+        //
+        return response;
+    }
+
+    async fetchBalance (params = {}) {
+        await this.loadMarkets ();
+        await this.fetchAccounts ();
+        let accountId = this.safeString (params, 'accountId');
+        if (accountId === undefined) {
+            accountId = this.options['accounts'][0]; // default main account
+        }
+        const request = {
+            'accountId': accountId,
+        };
+        const response = await this.privateGetPortfolioAccountIdAllocation (this.extend (request, params));
+        //
+        //     {
+        //         "assetClass": {
+        //             "long": {
+        //                 "CASH": "1234.567"
+        //             },
+        //             "short": {}
+        //         },
+        //         "sector": {
+        //             "long": {},
+        //             "short": {}
+        //         },
+        //         "group": {
+        //             "long": {},
+        //             "short": {}
+        //         }
+        //     }
+        //
+        const result = { 'info': response };
+        const balance = response[i];
+        const type = this.safeString (balance, 'type');
+        const currencyId = this.safeStringLower (balance, 'currency', '');
+        const account = this.account ();
+        account['free'] = this.safeString (balance, 'available');
+        account['total'] = this.safeString (balance, 'amount');
+        result['USD'] = account;
+        return this.safeBalance (result);
     }
 
     sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
