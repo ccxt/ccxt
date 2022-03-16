84d064e5ef84f5f93236c3c0f483e7dd5c4e6178
Travis CI <travis@travis-ci.org>
2022-03-16 13:57:09 +0000

    1.76.27
    [ci skip]

--- php/okx.php
+++ php/okx.php
@@ -1161,7 +1161,7 @@ class okx extends Exchange {
         $symbol = $market['symbol'];
         $last = $this->safe_string($ticker, 'last');
         $open = $this->safe_string($ticker, 'open24h');
-        $quoteVolume = $this->safe_string($ticker, 'volCcy24h');
+        $quoteVolume = $market['spot'] ? $this->safe_string($ticker, 'volCcy24h') : null;
         $baseVolume = $this->safe_string($ticker, 'vol24h');
         return $this->safe_ticker(array(
             'symbol' => $symbol,
--- python/ccxt/async_support/okx.py
+++ python/ccxt/async_support/okx.py
@@ -1148,7 +1148,7 @@ class okx(Exchange):
         symbol = market['symbol']
         last = self.safe_string(ticker, 'last')
         open = self.safe_string(ticker, 'open24h')
-        quoteVolume = self.safe_string(ticker, 'volCcy24h')
+        quoteVolume = self.safe_string(ticker, 'volCcy24h') if market['spot'] else None
         baseVolume = self.safe_string(ticker, 'vol24h')
         return self.safe_ticker({
             'symbol': symbol,
