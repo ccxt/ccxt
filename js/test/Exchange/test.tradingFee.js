8b22427274b4df0d9d94ebae00c8143323251547
Pablo Criado-Perez <pablo@criadoperez.com>
2022-03-17 23:31:53 +0100

    testTradingFee

--- js/test/Exchange/test.tradingFee.js
+++ js/test/Exchange/test.tradingFee.js
@@ -8,19 +8,25 @@ const assert = require ('assert')
 
 function testTradingFee (exchange = undefined, symbol, fee) {
     assert (fee, 'fee is undefined')
-    assert (fee['info'], 'fee info is empty')
+    const sampleFee = {
+        'info': { 'a': 1, 'b': 2, 'c': 3 },
+        'symbol': 'ETH/BTC',
+        'maker': 0.002,
+        'taker': 0.003,
+    }
+    const keys = Object.keys (sampleFee)
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i]
+        assert (key in fee, 'fee key ' + key + ' not found')
+    }
     assert (fee['symbol'] === symbol, 'trade symbol is not equal to requested symbol: trade: ' + fee['symbol'] + ' requested: ' + symbol)
-    if (fee['maker']) {
-        assert (typeof fee['maker'] === 'number', 'maker fee is not a number')
-    }
-    if (fee['taker']) {
-        assert (typeof fee['taker'] === 'number', 'taker fee is not a number')
-    }
-    if (fee['percentage']) {
+    assert (typeof fee['maker'] === 'number', 'maker fee is not a number')
+    assert (typeof fee['taker'] === 'number', 'taker fee is not a number')
+    if ('percentage' in fee) {
         assert (typeof fee['percentage'] === 'boolean', 'percentage is not a boolean')
     }
-    if (fee['tierBased']) {
-        assert (typeof fee['tierBased'] === 'boolean', 'tierBased is not a boolean')
+    if ('tierBased' in fee) {
+        assert (typeof fee['tierBased'] === 'boolean', 'percentage is not a boolean')
     }
     return fee
 }
