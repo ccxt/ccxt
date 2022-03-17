52fd72a94fd9964d41e3f3bf683a61447757e71a
T.T <7117978+ttodua@users.noreply.github.com>
2022-03-17 19:29:33 +0400

    skeleton

---
+++ js/mexc3.js
@@ -0,0 +1,383 @@
+'use strict';
+
+// ---------------------------------------------------------------------------
+
+const Exchange = require ('./base/Exchange');
+
+// ---------------------------------------------------------------------------
+
+module.exports = class mexc3 extends Exchange {
+    describe () {
+        return this.deepExtend (super.describe (), {
+            'id': 'mexc',
+            'name': 'MEXC Global',
+            'countries': [ 'SC' ], // Seychelles
+            'rateLimit': 50, // default rate limit is 20 times per second
+            'version': 'v3',
+            'certified': true,
+            'has': {
+                'publicAPI': true,
+                'privateAPI': true,
+                'CORS': undefined,
+                'spot': undefined,
+                'margin': undefined,
+                'swap': undefined,
+                'future': undefined,
+                'option': undefined,
+                'addMargin': undefined,
+                'cancelAllOrders': undefined,
+                'cancelOrder': undefined,
+                'cancelOrders': undefined,
+                'createDepositAddress': undefined,
+                'createLimitOrder': undefined,
+                'createMarketOrder': undefined,
+                'createOrder': undefined,
+                'deposit': undefined,
+                'editOrder': 'emulated',
+                'fetchAccounts': undefined,
+                'fetchBalance': undefined,
+                'fetchBidsAsks': undefined,
+                'fetchBorrowRate': undefined,
+                'fetchBorrowRateHistory': undefined,
+                'fetchBorrowRatesPerSymbol': undefined,
+                'fetchBorrowRates': undefined,
+                'fetchCanceledOrders': undefined,
+                'fetchClosedOrder': undefined,
+                'fetchClosedOrders': undefined,
+                'fetchCurrencies': 'emulated',
+                'fetchDeposit': undefined,
+                'fetchDepositAddress': undefined,
+                'fetchDepositAddresses': undefined,
+                'fetchDepositAddressesByNetwork': undefined,
+                'fetchDeposits': undefined,
+                'fetchFundingFee': undefined,
+                'fetchFundingFees': undefined,
+                'fetchFundingHistory': undefined,
+                'fetchFundingRate': undefined,
+                'fetchFundingRateHistory': undefined,
+                'fetchFundingRates': undefined,
+                'fetchIndexOHLCV': undefined,
+                'fetchL2OrderBook': undefined,
+                'fetchLedger': undefined,
+                'fetchLedgerEntry': undefined,
+                'fetchLeverageTiers': undefined,
+                'fetchMarketLeverageTiers': undefined,
+                'fetchMarkets': undefined,
+                'fetchMarkOHLCV': undefined,
+                'fetchMyTrades': undefined,
+                'fetchOHLCV': 'emulated',
+                'fetchOpenOrder': undefined,
+                'fetchOpenOrders': undefined,
+                'fetchOrder': undefined,
+                'fetchOrderBook': undefined,
+                'fetchOrderBooks': undefined,
+                'fetchOrders': undefined,
+                'fetchOrderTrades': undefined,
+                'fetchPosition': undefined,
+                'fetchPositions': undefined,
+                'fetchPositionsRisk': undefined,
+                'fetchPremiumIndexOHLCV': undefined,
+                'fetchStatus': 'emulated',
+                'fetchTicker': undefined,
+                'fetchTickers': undefined,
+                'fetchTime': undefined,
+                'fetchTrades': undefined,
+                'fetchTradingFee': undefined,
+                'fetchTradingFees': undefined,
+                'fetchTradingLimits': undefined,
+                'fetchTransactions': undefined,
+                'fetchTransfers': undefined,
+                'fetchWithdrawal': undefined,
+                'fetchWithdrawals': undefined,
+                'loadMarkets': undefined,
+                'reduceMargin': undefined,
+                'setLeverage': undefined,
+                'setMarginMode': undefined,
+                'setPositionMode': undefined,
+                'signIn': undefined,
+                'transfer': undefined,
+                'withdraw': undefined,
+            },
+            'timeframes': {
+            },
+            'urls': {
+                'logo': 'https://user-images.githubusercontent.com/1294454/137283979-8b2a818d-8633-461b-bfca-de89e8c446b2.jpg',
+                'api': {
+                    'spot': {
+                        'public': 'https://api.mexc.com',
+                        'private': 'https://api.mexc.com',
+                    },
+                },
+                'www': 'https://www.mexc.com/',
+                'doc': [
+                    'https://mxcdevelop.github.io/apidocs/spot_v3_en/',
+                ],
+                'fees': [
+                    'https://www.mexc.com/fee',
+                ],
+                'referral': 'https://m.mexc.com/auth/signup?inviteCode=1FQ1G',
+            },
+            'api': {
+                'spot': {
+                    'public': {
+                        'get': {
+                            'ping': 1,
+                            'time': 1,
+                            'exchangeInfo': 1,
+                            'depth': 1,
+                            'trades': 1,
+                            'historicalTrades': 1,
+                            'aggTrades': 1,
+                            'klines': 1,
+                            'avgPrice': 1,
+                            'ticker/24hr': 1,
+                            'ticker/price': 1,
+                            'ticker/bookTicker': 1,
+                            'order': 1,
+                            'openOrders': 1,
+                            'allOrders': 1,
+                            'account': 1,
+                            'myTrades': 1,
+                            'etf/info': 1,
+                        },
+                    },
+                    'private': {
+                        'get': {
+                        },
+                        'post': {
+                            'order': 1,
+                            'order/test': 1,
+                        },
+                        'delete': {
+                            'order': 1,
+                            'openOrders': 1,
+                        },
+                    },
+                },
+            },
+            'fees': {
+                'trading': {
+                    'tierBased': false,
+                    'percentage': true,
+                    'maker': 0.2 / 100, // maker / taker
+                    'taker': 0.2 / 100,
+                },
+            },
+            'options': {
+                'fetchMarkets': {
+                    'types': {
+                        'spot': true,
+                        'future': {
+                            'linear': false,
+                            'inverse': false,
+                        },
+                        'swap': {
+                            'linear': false,
+                            'inverse': false,
+                        },
+                    },
+                },
+                'timeframes': {
+                    'spot': {
+                    },
+                },
+                'defaultType': 'spot', // spot, swap
+                'networks': {
+                    'TRX': 'TRC-20',
+                    'TRC20': 'TRC-20',
+                    'ETH': 'ERC-20',
+                    'ERC20': 'ERC-20',
+                    'BEP20': 'BEP20(BSC)',
+                },
+            },
+            'commonCurrencies': {
+                'BEYONDPROTOCOL': 'BEYOND',
+                'BIFI': 'BIFIF',
+                'BYN': 'BeyondFi',
+                'COFI': 'COFIX', // conflict with CoinFi
+                'DFI': 'DfiStarter',
+                'DFT': 'dFuture',
+                'DRK': 'DRK',
+                'EGC': 'Egoras Credit',
+                'FLUX1': 'FLUX', // switched places
+                'FLUX': 'FLUX1', // switched places
+                'FREE': 'FreeRossDAO', // conflict with FREE Coin
+                'HERO': 'Step Hero', // conflict with Metahero
+                'MIMO': 'Mimosa',
+                'PROS': 'Pros.Finance', // conflict with Prosper
+                'SIN': 'Sin City Token',
+            },
+            'exceptions': {
+                'exact': {
+                },
+                'broad': {
+                },
+            },
+        });
+    }
+
+    async fetchStatus (params = {}) {
+        const [ marketType, query ] = this.handleMarketTypeAndParams ('fetchStatus', undefined, params);
+        const method = this.getSupportedMapping (marketType, {
+            'spot': 'spotPublicGetPing',
+            'swap': '',
+        });
+        const response = await this[method] (this.extend (query));
+        //
+        // {}
+        //
+        const length = Object.keys (response).length;
+        const status = length === 0 ? 'ok' : 'maintenance';
+        this.status = this.extend (this.status, {
+            'status': status,
+            'updated': this.milliseconds (),
+        });
+        return this.status;
+    }
+
+    async fetchTime (params = {}) {
+        const [ marketType, query ] = this.handleMarketTypeAndParams ('fetchTime', undefined, params);
+        const method = this.getSupportedMapping (marketType, {
+            'spot': 'spotPublicGetTime',
+            'swap': '',
+        });
+        const response = await this[method] (this.extend (query));
+        //
+        // spot
+        //
+        //     {
+        //         "serverTime": "1647519277579"
+        //     }
+        //
+        return this.safeInteger (response, 'serverTime');
+    }
+
+    async fetchMarkets (params = {}) {
+        const response = await this.spotPublicGetExchangeInfo (params);
+        //
+        //     {
+        //         "timezone": "CST",
+        //         "serverTime": 1647521860402,
+        //         "rateLimits": [],
+        //         "exchangeFilters": [],
+        //         "symbols": [
+        //           {
+        //             "symbol": "BTCUSDT",
+        //             "status": "ENABLED",
+        //             "baseAsset": "BTC",
+        //             "baseAssetPrecision": 6,
+        //             "quoteAsset": "USDT",
+        //             "quotePrecision": 2,
+        //             "quoteAssetPrecision": 2,
+        //             "baseCommissionPrecision": 6,
+        //             "quoteCommissionPrecision": 2,
+        //             "orderTypes": [
+        //               "LIMIT",
+        //               "LIMIT_MAKER"
+        //             ],
+        //             "icebergAllowed": false,
+        //             "ocoAllowed": false,
+        //             "quoteOrderQtyMarketAllowed": false,
+        //             "isSpotTradingAllowed": true,
+        //             "isMarginTradingAllowed": false,
+        //             "permissions": [
+        //               "SPOT"
+        //             ],
+        //             "filters": []
+        //           },
+        //         ]
+        //     }
+        //
+        const data = this.safeValue (response, 'symbols', []);
+        const result = [];
+        for (let i = 0; i < data.length; i++) {
+            const market = data[i];
+            const id = this.safeString (market, 'symbol');
+            const baseId = this.safeString (market, 'baseAsset');
+            const quoteId = this.safeString (market, 'quoteAsset');
+            const base = this.safeCurrencyCode (baseId);
+            const quote = this.safeCurrencyCode (quoteId);
+            const status = this.safeString (market, 'status');
+            result.push ({
+                'id': id,
+                'symbol': base + '/' + quote,
+                'base': base,
+                'quote': quote,
+                'settle': undefined,
+                'baseId': baseId,
+                'quoteId': quoteId,
+                'settleId': undefined,
+                'type': 'spot',
+                'spot': true,
+                'margin': false,
+                'swap': false,
+                'future': false,
+                'option': false,
+                'active': (status === 'ENABLED'),
+                'contract': false,
+                'linear': undefined,
+                'inverse': undefined,
+                'taker': undefined,
+                'maker': undefined,
+                'contractSize': undefined,
+                'expiry': undefined,
+                'expiryDatetime': undefined,
+                'strike': undefined,
+                'optionType': undefined,
+                'precision': {
+                    'amount': undefined,
+                    'price': this.safeInteger (market, 'quotePrecision'),
+                    'base': this.safeInteger (market, 'baseAssetPrecision'),
+                    'quote': this.safeInteger (market, 'quoteAssetPrecision'),
+                },
+                'limits': {
+                    'leverage': {
+                        'min': undefined,
+                        'max': undefined,
+                    },
+                    'amount': {
+                        'min': undefined,
+                        'max': undefined,
+                    },
+                    'price': {
+                        'min': undefined,
+                        'max': undefined,
+                    },
+                    'cost': {
+                        'min': undefined,
+                        'max': undefined,
+                    },
+                },
+                'info': market,
+            });
+        }
+        return result;
+    }
+
+    sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
+        const [ section, access ] = api;
+        [ path, params ] = this.resolvePath (path, params);
+        let url = this.urls['api'][section][access] + '/api/' + this.version + '/' + path;
+        if (method === 'GET' || method === 'DELETE') {
+            if (Object.keys (params).length) {
+                url += '?' + this.urlencode (params);
+            }
+        }
+        return { 'url': url, 'method': method, 'body': body, 'headers': headers };
+    }
+
+    handleErrors (code, reason, url, method, headers, body, response, requestHeaders, requestBody) {
+        // if (response === undefined) {
+        //    return;
+        // }
+        //     {"code":10232,"msg":"The currency not exist"}
+        //     {"code":10216,"msg":"No available deposit address"}
+        //
+        //     {
+        //         "success":true,
+        //         "code":0,
+        //         "data":1634095541710
+        //     }
+        //
+    }
+};
