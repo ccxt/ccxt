6dd80ca0f43e9725f06215a457e824929874e1b8
docek <60971434+docek@users.noreply.github.com>
2022-03-21 12:24:56 +0100

    New public api end point to fetch funding rates

--- js/phemex.js
+++ js/phemex.js
@@ -98,6 +98,7 @@ module.exports = class phemex extends Exchange {
                 'public': {
                     'get': [
                         'cfg/v2/products', // spot + contracts
+                        'cfg/fundingRates', 
                         'products', // contracts only
                         'nomics/trades', // ?market=<symbol>&since=<since>
                         'md/kline', // ?from=1589811875&resolution=1800&symbol=sBTCUSDT&to=1592457935
