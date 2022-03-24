bd83be59dfd792b924a8e86c16d0cffa4bd9e160
T.T <7117978+ttodua@users.noreply.github.com>
2022-03-25 01:14:08 +0400

    Addition: contract endpoints

--- ccxt.js
+++ ccxt.js
@@ -131,6 +131,7 @@ const exchanges = {
     'lykke':                   require ('./js/lykke.js'),
     'mercado':                 require ('./js/mercado.js'),
     'mexc':                    require ('./js/mexc.js'),
+    'mexc3':                   require ('./js/mexc3.js'),
     'ndax':                    require ('./js/ndax.js'),
     'novadax':                 require ('./js/novadax.js'),
     'oceanex':                 require ('./js/oceanex.js'),
