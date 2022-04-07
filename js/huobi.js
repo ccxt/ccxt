2bc8056b0edaaf5184d68e8c8012922b93942dbb
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-04-07 12:00:20 +0100

    cr suggestions

--- js/huobi.js
+++ js/huobi.js
@@ -4621,6 +4621,8 @@ module.exports = class huobi extends Exchange {
 
     async fetchBorrowInterest (code = undefined, symbol = undefined, since = undefined, limit = undefined, params = {}) {
         await this.loadMarkets ();
+        const defaultMargin = this.safeString (params, 'marginType', 'cross'); // cross or isolated
+        const marginType = this.safeString2 (this.options, 'defaultMarginType', 'marginType', defaultMargin);
         const request = {};
         if (since !== undefined) {
             request['start-date'] = this.yyyymmdd (since);
@@ -4629,12 +4631,15 @@ module.exports = class huobi extends Exchange {
             request['size'] = limit;
         }
         let market = undefined;
-        let method = 'privateGetCrossMarginLoanOrders';
-        if (symbol !== undefined) {  // Isolated
-            market = this.market (symbol);
-            request['symbol'] = market['id'];
+        let method = undefined;
+        if (marginType === 'isolated') {
             method = 'privateGetMarginLoanOrders';
+            if (symbol !== undefined) {
+                market = this.market (symbol);
+                request['symbol'] = market['id'];
+            }
         } else {  // Cross
+            method = 'privateGetCrossMarginLoanOrders';
             if (code !== undefined) {
                 const currency = this.currency (code);
                 request['currency'] = currency['id'];
@@ -4664,20 +4669,20 @@ module.exports = class huobi extends Exchange {
         //    }
         //
         const data = this.safeValue (response, 'data');
-        const interest = this.parseBorrowInterests (data, market);
+        const interest = this.parseBorrowInterests (data, marginType, market);
         return this.filterByCurrencySinceLimit (interest, code, since, limit);
     }
 
-    parseBorrowInterests (response, market = undefined) {
+    parseBorrowInterests (response, marginType, market = undefined) {
         const interest = [];
         for (let i = 0; i < response.length; i++) {
             const row = response[i];
-            interest.push (this.parseBorrowInterest (row, market));
+            interest.push (this.parseBorrowInterest (row, marginType, market));
         }
         return interest;
     }
 
-    parseBorrowInterest (info, market = undefined) {
+    parseBorrowInterest (info, marginType, market = undefined) {
         // isolated
         //    {
         //        "interest-rate":"0.000040830000000000",
@@ -4719,8 +4724,10 @@ module.exports = class huobi extends Exchange {
         //       "filled-ht":"0"
         //   }
         //
-        const symbol = this.safeString (market, 'symbol');
-        const account = (symbol === undefined) ? 'CROSS' : symbol;
+        const marketId = this.safeString (info, 'symbol');
+        market = this.safeMarket (marketId, market);
+        const symbol = market['symbol'];
+        const account = (marginType === 'cross') ? 'CROSS' : symbol;
         const timestamp = this.safeNumber (info, 'accrued-at');
         return {
             'account': account,  // isolated symbol, will not be returned for crossed margin
