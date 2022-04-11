d0c6f47f759b2e468313ffb929604ee67acd3295
Travis CI <travis@travis-ci.org>
2022-04-11 18:36:52 +0000

    1.78.65
    [ci skip]

--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -28,11 +28,11 @@ use Exception;
 
 include 'Throttle.php';
 
-$version = '1.78.64';
+$version = '1.78.65';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '1.78.64';
+    const VERSION = '1.78.65';
 
     public static $loop;
     public static $kernel;
