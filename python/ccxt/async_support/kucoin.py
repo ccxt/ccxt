155962269428f0a0da84930f26677cd598a28b68
Travis CI <travis@travis-ci.org>
2022-04-11 13:08:29 +0000

    1.78.63
    [ci skip]

--- python/ccxt/async_support/kucoin.py
+++ python/ccxt/async_support/kucoin.py
@@ -321,6 +321,7 @@ class kucoin(Exchange):
                     '400008': NotSupported,
                     '400100': BadRequest,
                     '400350': InvalidOrder,  # {"code":"400350","msg":"Upper limit for holding: 10,000USDT, you can still buy 10,000USDT worth of coin."}
+                    '400370': InvalidOrder,  # {"code":"400370","msg":"Max. price: 0.02500000000000000000"}
                     '400500': InvalidOrder,  # {"code":"400500","msg":"Your located country/region is currently not supported for the trading of self token"}
                     '400600': BadSymbol,  # {"code":"400600","msg":"validation.createOrder.symbolNotAvailable"}
                     '401000': BadRequest,  # {"code":"401000","msg":"The interface has been deprecated"}
