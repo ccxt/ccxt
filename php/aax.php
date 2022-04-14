2be0c0fb637389bc4ace5f17300017695ec365a1
Travis CI <travis@travis-ci.org>
2022-04-14 05:31:44 +0000

    1.79.2
    [ci skip]

--- php/aax.php
+++ php/aax.php
@@ -82,8 +82,8 @@ class aax extends Exchange {
                 'fetchOrderBooks' => null,
                 'fetchOrders' => true,
                 'fetchOrderTrades' => null,
-                'fetchPosition' => null,
-                'fetchPositions' => null,
+                'fetchPosition' => true,
+                'fetchPositions' => true,
                 'fetchPositionsRisk' => false,
                 'fetchPremiumIndexOHLCV' => false,
                 'fetchStatus' => true,
@@ -2503,6 +2503,211 @@ class aax extends Exchange {
         }
         $transfer['status'] = $this->parse_transfer_status($this->safe_string($response, 'code'));
         return $transfer;
+    }
+
+    public function parse_position($position, $market = null) {
+        //
+        //    {
+        //        "autoMarginCall" => false,
+        //        "avgEntryPrice" => "3706.03",
+        //        "bankruptPrice" => "2963.3415880000",
+        //        "base" => "ETH",
+        //        "code" => "FP",
+        //        "commission" => "0.02964824",
+        //        "currentQty" => "2",
+        //        "funding" => "-0.04827355",
+        //        "fundingStatus" => null,
+        //        "id" => "385839395735639395",
+        //        "leverage" => "5",
+        //        "liquidationPrice" => "2983.07",
+        //        "marketPrice" => "3731.84",
+        //        "openTime" => "2021-12-31T18:57:25.930Z",
+        //        "posLeverage" => "5.00",
+        //        "posMargin" => "14.85376824",
+        //        "quote" => "USDT",
+        //        "realisedPnl" => "-0.07792179",
+        //        "riskLimit" => "10000000",
+        //        "riskyPrice" => "3272.25",
+        //        "settleType" => "VANILLA",
+        //        "stopLossPrice" => "0",
+        //        "stopLossSource" => 1,
+        //        "symbol" => "ETHUSDTFP",
+        //        "takeProfitPrice" => "0",
+        //        "takeProfitSource" => 1,
+        //        "unrealisedPnl" => "0.51620000",
+        //        "userID" => "3829384",
+        //        "ts" => 1641027194500
+        //    }
+        //
+        $contract = $this->safe_string($position, 'symbol');
+        $market = $this->safe_market($contract, $market);
+        $size = $this->safe_string($position, 'currentQty');
+        $side = null;
+        if (Precise::string_gt($size, '0')) {
+            $side = 'long';
+        } else if (Precise::string_lt($size, '0')) {
+            $side = 'short';
+        }
+        $leverage = $this->safe_string($position, 'leverage');
+        $unrealisedPnl = $this->safe_string($position, 'unrealisedPnl');
+        $currentQty = $this->safe_string($position, 'currentQty');
+        $contractSize = $this->safe_string($market, 'contractSize');
+        $initialQuote = Precise::string_mul($currentQty, $contractSize);
+        $marketPrice = $this->safe_string($position, 'marketPrice');
+        $notional = Precise::string_mul($initialQuote, $marketPrice);
+        $timestamp = $this->safe_integer($position, 'ts');
+        $liquidationPrice = $this->safe_string($position, 'liquidationPrice');
+        return array(
+            'info' => $position,
+            'symbol' => $this->safe_string($market, 'symbol'),
+            'timestamp' => $timestamp,
+            'datetime' => $this->iso8601($timestamp),
+            'initialMargin' => null,
+            'initialMarginPercentage' => null,
+            'maintenanceMargin' => null,
+            'maintenanceMarginPercentage' => null,
+            'entryPrice' => $this->safe_number($position, 'avgEntryPrice'),
+            'notional' => $this->parse_number($notional),
+            'leverage' => $this->parse_number($leverage),
+            'unrealizedPnl' => $this->parse_number($unrealisedPnl),
+            'contracts' => $this->parse_number($size),
+            'contractSize' => $this->parse_number($contractSize),
+            'marginRatio' => null,
+            'liquidationPrice' => $liquidationPrice,
+            'markPrice' => $this->safe_number($position, 'marketPrice'),
+            'collateral' => $this->safe_number($position, 'posMargin'),
+            'marginType' => $this->safe_string($position, 'settleType'),
+            'side' => $side,
+            'percentage' => null,
+        );
+    }
+
+    public function fetch_position($symbol = null, $params = array ()) {
+        $this->load_markets();
+        $market = $this->market($symbol);
+        $request = array(
+            'symbol' => $market['id'],
+        );
+        $response = $this->privateGetFuturesPosition (array_merge($request, $params));
+        //
+        //    {
+        //        "code" => 1,
+        //        "data" => array(
+        //            {
+        //                "autoMarginCall" => false,
+        //                "avgEntryPrice" => "3706.03",
+        //                "bankruptPrice" => "2963.3415880000",
+        //                "base" => "ETH",
+        //                "code" => "FP",
+        //                "commission" => "0.02964824",
+        //                "currentQty" => "2",
+        //                "funding" => "-0.04827355",
+        //                "fundingStatus" => null,
+        //                "id" => "385839395735639395",
+        //                "leverage" => "5",
+        //                "liquidationPrice" => "2983.07",
+        //                "marketPrice" => "3731.84",
+        //                "openTime" => "2021-12-31T18:57:25.930Z",
+        //                "posLeverage" => "5.00",
+        //                "posMargin" => "14.85376824",
+        //                "quote" => "USDT",
+        //                "realisedPnl" => "-0.07792179",
+        //                "riskLimit" => "10000000",
+        //                "riskyPrice" => "3272.25",
+        //                "settleType" => "VANILLA",
+        //                "stopLossPrice" => "0",
+        //                "stopLossSource" => 1,
+        //                "symbol" => "ETHUSDTFP",
+        //                "takeProfitPrice" => "0",
+        //                "takeProfitSource" => 1,
+        //                "unrealisedPnl" => "0.51620000",
+        //                "userID" => "3829384"
+        //            }
+        //            ...
+        //        ),
+        //        "message" => "success",
+        //        "ts" => 1641026778068
+        //    }
+        //
+        $positions = $this->safe_value($response, 'data', array());
+        $timestamp = $this->safe_integer($response, 'ts');
+        $first = $this->safe_value($positions, 0);
+        $position = $this->parse_position($first);
+        return array_merge($position, array(
+            'timestamp' => $timestamp,
+            'datetime' => $this->iso8601($timestamp),
+        ));
+    }
+
+    public function fetch_positions($symbols = null, $params = array ()) {
+        $this->load_markets();
+        $request = array();
+        if ($symbols !== null) {
+            $symbol = null;
+            if (gettype($symbols) === 'array' && count(array_filter(array_keys($symbols), 'is_string')) == 0) {
+                $symbolsLength = is_array($symbols) ? count($symbols) : 0;
+                if ($symbolsLength > 1) {
+                    throw new BadRequest($this->id . ' fetchPositions $symbols argument cannot contain more than 1 symbol');
+                }
+                $symbol = $symbols[0];
+            } else {
+                $symbol = $symbols;
+            }
+            $market = $this->market($symbol);
+            $request['symbol'] = $market['id'];
+        }
+        $response = $this->privateGetFuturesPosition (array_merge($request, $params));
+        //
+        //    {
+        //        "code" => 1,
+        //        "data" => array(
+        //            {
+        //                "autoMarginCall" => false,
+        //                "avgEntryPrice" => "3706.03",
+        //                "bankruptPrice" => "2963.3415880000",
+        //                "base" => "ETH",
+        //                "code" => "FP",
+        //                "commission" => "0.02964824",
+        //                "currentQty" => "2",
+        //                "funding" => "-0.04827355",
+        //                "fundingStatus" => null,
+        //                "id" => "385839395735639395",
+        //                "leverage" => "5",
+        //                "liquidationPrice" => "2983.07",
+        //                "marketPrice" => "3731.84",
+        //                "openTime" => "2021-12-31T18:57:25.930Z",
+        //                "posLeverage" => "5.00",
+        //                "posMargin" => "14.85376824",
+        //                "quote" => "USDT",
+        //                "realisedPnl" => "-0.07792179",
+        //                "riskLimit" => "10000000",
+        //                "riskyPrice" => "3272.25",
+        //                "settleType" => "VANILLA",
+        //                "stopLossPrice" => "0",
+        //                "stopLossSource" => 1,
+        //                "symbol" => "ETHUSDTFP",
+        //                "takeProfitPrice" => "0",
+        //                "takeProfitSource" => 1,
+        //                "unrealisedPnl" => "0.51620000",
+        //                "userID" => "3829384"
+        //            }
+        //            ...
+        //        ),
+        //        "message" => "success",
+        //        "ts" => 1641026778068
+        //    }
+        //
+        $result = array();
+        $positions = $this->safe_value($response, 'data', array());
+        $timestamp = $this->safe_integer($response, 'ts');
+        for ($i = 0; $i < count($positions); $i++) {
+            $position = $this->parse_position($positions[$i]);
+            $result[] = array_merge($position, array(
+                'timestamp' => $timestamp,
+                'datetime' => $this->iso8601($timestamp),
+            ));
+        }
+        return $this->filter_by_array($result, 'symbol', $symbols, false);
     }
 
     public function nonce() {
