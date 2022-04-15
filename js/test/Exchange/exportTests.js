2d58ffe275f0d19933045a25dd3159e2d30731da
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-04-15 11:51:17 +0100

    export tests

---
+++ js/test/Exchange/exportTests.js
@@ -0,0 +1,18 @@
+import testBalance from "./test.balance.js";
+import testOrderbook from "./test.orderbook.js";
+import testTrade from "./test.trade.js"
+import testOHLCV from "./test.ohlcv.js"
+import testOrder from "./test.order.js"
+import testTicker from "./test.ticker.js"
+import testOrder from "./test.order.js"
+
+
+export {
+    testBalance,
+    testOrderbook,
+    testTrade,
+    testOHLCV,
+    testOrder,
+    testTicker,
+    testOrder
+}
