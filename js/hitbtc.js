51a67c531c63912155fbbf84512d9c43923cab2c
T.Todua <7117978+ttodua@users.noreply.github.com>
2022-04-15 08:53:22 +0400

    withdraw parse

--- js/hitbtc.js
+++ js/hitbtc.js
@@ -769,7 +769,7 @@ module.exports = class hitbtc extends Exchange {
         //     {
         //         id: 'd53ee9df-89bf-4d09-886e-849f8be64647',
         //         index: 1044718371,
-        //         type: 'payout',
+        //         type: 'payout', // payout, payin
         //         status: 'success',
         //         currency: 'ETH',
         //         amount: '4.522683200000000000000000',
@@ -782,46 +782,20 @@ module.exports = class hitbtc extends Exchange {
         //     {
         //         id: 'e6c63331-467e-4922-9edc-019e75d20ba3',
         //         index: 1044714672,
-        //         type: 'exchangeToBank',
+        //         type: 'exchangeToBank', // exchangeToBank, bankToExchange, withdraw
         //         status: 'success',
         //         currency: 'ETH',
         //         amount: '4.532263200000000000',
         //         createdAt: '2018-06-07T00:42:39.543Z',
         //         updatedAt: '2018-06-07T00:42:39.683Z',
         //     },
-        //     {
-        //         id: '3b052faa-bf97-4636-a95c-3b5260015a10',
-        //         index: 1009280164,
-        //         type: 'bankToExchange',
-        //         status: 'success',
-        //         currency: 'CAS',
-        //         amount: '104797.875800000000000000',
-        //         createdAt: '2018-05-19T02:34:36.750Z',
-        //         updatedAt: '2018-05-19T02:34:36.857Z',
-        //     },
-        //     {
-        //         id: 'd525249f-7498-4c81-ba7b-b6ae2037dc08',
-        //         index: 1009279948,
-        //         type: 'payin',
-        //         status: 'success',
-        //         currency: 'CAS',
-        //         amount: '104797.875800000000000000',
-        //         createdAt: '2018-05-19T02:30:16.698Z',
-        //         updatedAt: '2018-05-19T02:34:28.159Z',
-        //         hash: '0xa6530e1231de409cf1f282196ed66533b103eac1df2aa4a7739d56b02c5f0388',
-        //         address: '0xd53ed559a6d963af7cb3f3fcd0e7ca499054db8b',
-        //     }
+        //
+        // withdraw
         //
         //     {
-        //         "id": "4f351f4f-a8ee-4984-a468-189ed590ddbd",
-        //         "index": 3112719565,
-        //         "type": "withdraw",
-        //         "status": "success",
-        //         "currency": "BCHOLD",
-        //         "amount": "0.02423133",
-        //         "createdAt": "2019-07-16T16:52:04.494Z",
-        //         "updatedAt": "2019-07-16T16:54:07.753Z"
+        //         "id": "d2ce578f-647d-4fa0-b1aa-4a27e5ee597b"
         //     }
+        //
         const id = this.safeString (transaction, 'id');
         const timestamp = this.parse8601 (this.safeString (transaction, 'createdAt'));
         const updated = this.parse8601 (this.safeString (transaction, 'updatedAt'));
@@ -1288,10 +1262,12 @@ module.exports = class hitbtc extends Exchange {
             params = this.omit (params, 'network');
         }
         const response = await this.privatePostAccountCryptoWithdraw (this.extend (request, params));
-        return {
-            'info': response,
-            'id': response['id'],
-        };
+        //
+        //     {
+        //         "id": "d2ce578f-647d-4fa0-b1aa-4a27e5ee597b"
+        //     }
+        //
+        return this.parseTransaction (response, currency);
     }
 
     nonce () {
