fc081eeb63a0a61af62e184758790213b28cca08
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-04-16 10:07:47 +0100

    fix exportTests

--- js/test/Exchange/exportTests.js
+++ js/test/Exchange/exportTests.js
@@ -1,5 +1,5 @@
 import testBalance from "./test.balance.js";
-import testOrderbook from "./test.orderbook.js";
+import testOrderBook from "./test.orderbook.js";
 import testTrade from "./test.trade.js"
 import testOHLCV from "./test.ohlcv.js"
 import testOrder from "./test.order.js"
@@ -8,7 +8,7 @@ import testTicker from "./test.ticker.js"
 
 export {
     testBalance,
-    testOrderbook,
+    testOrderBook,
     testTrade,
     testOHLCV,
     testOrder,
