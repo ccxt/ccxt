a717feb72452372fbfb0003c0dbc895f8ac61641
Igor Kroitor <igor.kroitor@gmail.com>
2022-04-17 20:03:29 +0300

    examples/ccxt.pro/js/gateio-watch-order-book.js

---
+++ examples/ccxt.pro/js/gateio-watch-order-book.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const ccxtpro = require ('./ccxt.pro')
+
+console.log ("CCXT Pro Version:", ccxtpro.version)
+
+async function watchOrderBook (exchange, symbol) {
+    while (true) {
+        try {
+            const orderbook = await exchange.watchOrderBook (symbol)
+            console.log (exchange.iso8601 (exchange.milliseconds ()), symbol, orderbook['nonce'], orderbook['asks'][0], orderbook['bids'][0])
+        } catch (e) {
+            console.log (e.constructor.name, e.message)
+        }
+    }
+}
+
+async function main () {
+    const exchange = new ccxtpro.gateio ({
+        'options': {
+            'defaultType': 'swap',
+        },
+    })
+    await exchange.loadMarkets ()
+    exchange.verbose = true
+    const symbols = [
+        'GMT/USDT:USDT',
+        'ASTR/USDT:USDT',
+    ]
+
+    await Promise.all (symbols.map (symbol => watchOrderBook (exchange, symbol)))
+}
+
+main ()
