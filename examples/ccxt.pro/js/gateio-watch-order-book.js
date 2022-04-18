78cc3718b8f6478f37f625ed33f4f45ea255705b
Igor Kroitor <igor.kroitor@gmail.com>
2022-04-19 00:49:09 +0300

    examples/ccxt.pro/js/gateio-watch-order-book.js sync demo

--- examples/ccxt.pro/js/gateio-watch-order-book.js
+++ examples/ccxt.pro/js/gateio-watch-order-book.js
@@ -4,11 +4,36 @@ const ccxtpro = require ('./ccxt.pro')
 
 console.log ("CCXT Pro Version:", ccxtpro.version)
 
+const orderbooks = {}
+
+async function watchAllSymbols (exchange, symbols) {
+    while (true) {
+        const keys = Object.keys (orderbooks);
+        if (symbols.length === keys.length) {
+            console.log ('\n\n\n\n\n')
+            console.log ('----------------------------------------------------')
+            console.log ('All orderbooks received at least one update:');
+            for (let i = 0; i < symbols.length; i++) {
+                const symbol = symbols[i]
+                const orderbook = orderbooks[symbol]
+                console.log (exchange.iso8601 (exchange.milliseconds ()), orderbook['datetime'], orderbook['nonce'], symbol, orderbook['asks'][0], orderbook['bids'][0])
+            }
+            console.log ('----------------------------------------------------')
+            console.log ('\n\n\n\n\n')
+            // process.exit () // stop here if you want
+            break
+        } else {
+            await exchange.sleep (1000);
+        }
+    }
+}
+
 async function watchOrderBook (exchange, symbol) {
     while (true) {
         try {
             const orderbook = await exchange.watchOrderBook (symbol)
-            console.log (exchange.iso8601 (exchange.milliseconds ()), symbol, orderbook['nonce'], orderbook['asks'][0], orderbook['bids'][0])
+            orderbooks[symbol] = orderbook
+            console.log (exchange.iso8601 (exchange.milliseconds ()), orderbook['datetime'], orderbook['nonce'], symbol, orderbook['asks'][0], orderbook['bids'][0])
         } catch (e) {
             console.log (e.constructor.name, e.message)
         }
@@ -19,21 +44,28 @@ async function main () {
     const exchange = new ccxtpro.gateio ({
         'options': {
             'defaultType': 'swap',
-            'maxOrderBookSyncAttempts': 100,
         },
     })
     await exchange.loadMarkets ()
-    // exchange.verbose = true
+    // exchange.verbose = true // uncomment for debugging purposes if necessary
     const symbols = [
-        'GMT/USDT:USDT',
-        'ASTR/USDT:USDT',
+        // 'SOS/USDT:USDT',
+        // 'JASMY/USDT:USDT',
+        // 'SLP/USDT:USDT',
+        'ACH/USDT:USDT',
+        'MKISHU/USDT:USDT',
+        // 'GMT/USDT:USDT',
+        // 'ASTR/USDT:USDT',
         'RAMP/USDT:USDT',
         'RSR/USDT:USDT',
-        'RACA/USDT:USDT',
-        'ROOK/USDT:USDT',
-        'ROSE/USDT:USDT',
+        // 'RACA/USDT:USDT',
+        // 'ROOK/USDT:USDT',
+        // 'ROSE/USDT:USDT',
     ]
-    await Promise.all (symbols.map (symbol => watchOrderBook (exchange, symbol)))
+    await Promise.all ([
+        watchAllSymbols (exchange, symbols),
+        ... symbols.map (symbol => watchOrderBook (exchange, symbol))
+    ])
 }
 
 main ()
