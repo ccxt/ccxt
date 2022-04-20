e0c33f8a98a1c483138eeb40cc07b2f0c7b38305
Igor Kroitor <igor.kroitor@gmail.com>
2022-04-20 03:03:21 +0300

    exchange.py safe_ledger_entry edits

--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -2027,7 +2027,8 @@ class Exchange(object):
         tail = since is None
         return self.filter_by_currency_since_limit(result, code, since, limit, tail)
 
-    def safe_ledger_entry(self, ledger_entry):
+    def safe_ledger_entry(self, entry, currency=None):
+        currency = self.safe_currency(None, currency)
         return self.extend({
             'id': None,
             'timestamp': None,
@@ -2037,14 +2038,14 @@ class Exchange(object):
             'referenceId': None,
             'referenceAccount': None,
             'type': None,
-            'currency': None,
+            'currency': currency['code'],
             'amount': None,
             'before': None,
             'after': None,
             'status': None,
             'fee': None,
-            'info': {},
-        }, ledger_entry)
+            'info': None,
+        }, entry)
 
     def parse_orders(self, orders, market=None, since=None, limit=None, params={}):
         if isinstance(orders, list):
