e35f1541441f2bc43ef0cb7e44373044efd313f4
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2022-04-21 10:56:30 +0530

    session headers fix for async

--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -573,7 +573,8 @@ class Exchange(object):
 
     def prepare_request_headers(self, headers=None):
         headers = headers or {}
-        headers.update(self.session.headers)
+        if self.session:
+            headers.update(self.session.headers)
         headers.update(self.headers)
         if self.userAgent:
             if type(self.userAgent) is str:
