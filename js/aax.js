bdb6b62723b4b936af75918c8849c1bbb01d7ad2
Doctor Slimm <L.Korganas@gmail.com>
2022-04-25 12:40:42 +0100

    update ratelimits

--- js/aax.js
+++ js/aax.js
@@ -16,7 +16,12 @@ module.exports = class aax extends Exchange {
             'name': 'AAX',
             'countries': [ 'MT' ], // Malta
             'enableRateLimit': true,
-            'rateLimit': 500,
+            // 6000 /  hour => 100 per minute => 1.66 requests per second => rateLimit = 600
+            // market endpoints ratelimits arent mentioned in docs so they are also set to "all other authenticated endpoints"
+            // 5000 / hour => weight = 1.2 ("all other authenticated endpoints")
+            // 600 / hour => weight = 10
+            // 200 / hour => weight = 30
+            'rateLimit': 600,
             'version': 'v2',
             'hostname': 'aaxpro.com', // aax.com
             'pro': true,
@@ -150,65 +155,65 @@ module.exports = class aax extends Exchange {
                     //     'tickers', // Get ticker of all markets
                     //     'tickers/{market}', // Get ticker of specific market
                     // ],
-                    'get': [
-                        'currencies',
-                        'announcement/maintenance', // System Maintenance Notice
-                        'time',
-                        'instruments', // Retrieve all trading pairs information
-                        'market/orderbook', // Order Book
-                        'futures/position/openInterest', // Open Interest
-                        'market/tickers', // Get the Last 24h Market Summary
-                        'market/candles', // Get Current Candlestick
-                        'market/history/candles', // Get Current Candlestick
-                        'market/trades', // Get the Most Recent Trades
-                        'market/markPrice', // Get Current Mark Price
-                        'futures/funding/predictedFunding/{symbol}', // Get Predicted Funding Rate
-                        'futures/funding/prevFundingRate/{symbol}', // Get Last Funding Rate
-                        'futures/funding/fundingRate',
-                        'market/candles/index', // * Deprecated
-                        'market/index/candles',
-                    ],
+                    'get': {
+                        'currencies': 1.2,
+                        'announcement/maintenance': 1.2, // System Maintenance Notice
+                        'time': 1.2,
+                        'instruments': 1.2, // Retrieve all trading pairs information
+                        'market/orderbook': 1.2, // Order Book
+                        'futures/position/openInterest': 1.2, // Open Interest
+                        'market/tickers': 1.2, // Get the Last 24h Market Summary
+                        'market/candles': 1.2, // Get Current Candlestick
+                        'market/history/candles': 1.2, // Get Current Candlestick
+                        'market/trades': 1.2, // Get the Most Recent Trades
+                        'market/markPrice': 1.2, // Get Current Mark Price
+                        'futures/funding/predictedFunding/{symbol}': 1.2, // Get Predicted Funding Rate
+                        'futures/funding/prevFundingRate/{symbol}': 1.2, // Get Last Funding Rate
+                        'futures/funding/fundingRate': 1.2,
+                        'market/candles/index': 1.2, // * Deprecated
+                        'market/index/candles': 1.2,
+                    },
                 },
                 'private': {
-                    'get': [
-                        'user/info', // Retrieve user information
-                        'account/balances', // Get Account Balances
-                        'account/deposit/address', // undocumented
-                        'account/deposits', // Get account deposits history
-                        'account/transfer',
-                        'account/withdraws', // Get account withdrawals history
-                        'spot/trades', // Retrieve trades details for a spot order
-                        'spot/openOrders', // Retrieve spot open orders
-                        'spot/orders', // Retrieve historical spot orders
-                        'futures/position', // Get positions for all contracts
-                        'futures/position/closed', // Get closed positions
-                        'futures/trades', // Retrieve trade details for a futures order
-                        'futures/openOrders', // Retrieve futures open orders
-                        'futures/orders', // Retrieve historical futures orders
-                        'futures/funding/fundingFee',
-                        'futures/funding/predictedFundingFee/{symbol}', // Get predicted funding fee
-                    ],
-                    'post': [
-                        'account/transfer', // Asset Transfer
-                        'spot/orders', // Create a new spot order
-                        'spot/orders/cancelAllOnTimeout', // Automatically cancel all your spot orders after a specified timeout.
-                        'futures/orders', // Create a new futures order
-                        'futures/orders/cancelAllOnTimeout', // Automatically cancel all your futures orders after a specified timeout.
-                        'futures/position/sltp', // Set take profit and stop loss orders for an opening position
-                        'futures/position/close', // Close position
-                        'futures/position/leverage', // Update leverage for position
-                        'futures/position/margin', // Modify Isolated Position Margin
-                    ],
-                    'put': [
-                        'spot/orders', // Amend spot order
-                        'futures/orders', // Amend the quantity of an open futures order
-                    ],
-                    'delete': [
-                        'spot/orders/cancel/{orderID}', // Cancel a spot order
-                        'spot/orders/cancel/all', // Batch cancel spot orders
-                        'futures/orders/cancel/{orderID}', // Cancel a futures order
-                        'futures/orders/cancel/all', // Batch cancel futures orders
-                    ],
+                    'get': {
+                        'user/info': 1.2, // Retrieve user information
+                        'account/balances': 1.2, // Get Account Balances
+                        'account/deposit/address': 1.2, // undocumented
+                        'account/deposits': 1.2, // Get account deposits history
+                        'account/transfer': 1.2,
+                        'account/withdraws': 1.2, // Get account withdrawals history
+                        'spot/trades': 1.2, // Retrieve trades details for a spot order
+                        'spot/openOrders': 1.2, // Retrieve spot open orders
+                        'spot/orders': 1.2, // Retrieve historical spot orders
+                        'futures/position': 1.2, // Get positions for all contracts
+                        'futures/position/closed': 1.2, // Get closed positions
+                        'futures/trades': 1.2, // Retrieve trade details for a futures order
+                        'futures/openOrders': 1.2, // Retrieve futures open orders
+                        'futures/orders': 1.2, // Retrieve historical futures orders
+                        'futures/funding/fundingFee': 1.2,
+                        'futures/funding/predictedFundingFee/{symbol}': 1.2, // Get predicted funding fee
+                    },
+                    'post': {
+                        'account/transfer': 1.2, // Asset Transfer
+                        'spot/orders': 1.2, // Create a new spot order
+                        'spot/orders/cancelAllOnTimeout': 10, // Automatically cancel all your spot orders after a specified timeout.
+                        'futures/orders': 1.2, // Create a new futures order
+                        'futures/orders/cancelAllOnTimeout': 10, // Automatically cancel all your futures orders after a specified timeout.
+                        'futures/position/sltp': 1.2, // Set take profit and stop loss orders for an opening position
+                        'futures/position/close': 1.2, // Close position
+                        'futures/position/leverage': 30, // Update leverage for position
+                        'futures/position/margin': 1.2, // Modify Isolated Position Margin
+                    },
+                    'put': {
+                        'spot/orders': 1.2, // Amend spot order
+                        'futures/orders': 1.2, // Amend the quantity of an open futures order
+                    },
+                    'delete': {
+                        'spot/orders/cancel/{orderID}': 1, // Cancel a spot order
+                        'spot/orders/cancel/all': 10, // Batch cancel spot orders
+                        'futures/orders/cancel/{orderID}': 1, // Cancel a futures order
+                        'futures/orders/cancel/all': 10, // Batch cancel futures orders
+                    },
                 },
             },
             'fees': {
