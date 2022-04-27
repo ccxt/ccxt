db2a0e62a55e1fd0a48c85e8fc5bff8cc4b6f3f4
Sam Germain <sam.germain@usask.ca>
2022-04-27 01:25:10 -0600

    examples/js/p2p-orders-from-previous-year.js

---
+++ examples/js/p2p-orders-from-previous-year.js
@@ -0,0 +1,52 @@
+const ccxt = require ('../../ccxt');
+const keys = require ('../../keys.local.json');
+
+async function main () {
+    const binance = new ccxt.binance (keys.binance);
+
+    const year = new Date ().getFullYear () - 1;
+    const dateStrings = [ 
+        `${year}-01-01T00:00:00`,
+        `${year}-01-15T00:00:00`,
+        `${year}-02-01T00:00:00`,
+        `${year}-02-15T00:00:00`,
+        `${year}-03-01T00:00:00`,
+        `${year}-03-15T00:00:00`,
+        `${year}-04-01T00:00:00`,
+        `${year}-04-15T00:00:00`,
+        `${year}-05-01T00:00:00`,
+        `${year}-05-15T00:00:00`,
+        `${year}-06-01T00:00:00`,
+        `${year}-06-15T00:00:00`,
+        `${year}-07-01T00:00:00`,
+        `${year}-07-15T00:00:00`,
+        `${year}-08-01T00:00:00`,
+        `${year}-08-15T00:00:00`,
+        `${year}-09-01T00:00:00`,
+        `${year}-09-15T00:00:00`,
+        `${year}-10-01T00:00:00`,
+        `${year}-10-15T00:00:00`,
+        `${year}-11-01T00:00:00`,
+        `${year}-11-15T00:00:00`,
+        `${year}-12-01T00:00:00`,
+        `${year}-12-15T00:00:00`,
+        `${year}-12-31T23:59:59`,
+    ]
+    const timestamps = dateStrings.map (dt => new Date (dt).getTime());
+
+    let history = [];
+
+    for (let i = 0; i < timestamps.length - 1; i++) {
+        const startTime = timestamps[i];
+        const endTime = timestamps[i+1] - 1;
+        const oneMonthHistory = await binance.sapiGetC2cOrderMatchListUserOrderHistory ({
+            'startTimestamp': startTime,
+            'endTimestamp': endTime,
+        });
+        const result = oneMonthHistory['data'];
+        history = history.concat (result);
+    }
+    console.log (JSON.stringify (history));
+}
+
+main ()
