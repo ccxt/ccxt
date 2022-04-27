4126129e4b362828c1fc2130617410361626af94
Pablo Criado-Perez <pablo@criadoperez.com>
2022-04-27 18:42:57 +0200

    transfer

--- js/latoken.js
+++ js/latoken.js
@@ -46,6 +46,9 @@ module.exports = class latoken extends Exchange {
                 'fetchTradingFee': true,
                 'fetchTradingFees': false,
                 'fetchTransactions': true,
+                'fetchTransfer': false,
+                'fetchTransfers': false,
+                'transfer': false,
             },
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/61511972-24c39f00-aa01-11e9-9f7c-471f1d6e5214.jpg',
