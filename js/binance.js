c0086e1128ae06e4e1d4444a46faf993cd6e2835
Igor Kroitor <igor.kroitor@gmail.com>
2022-04-29 19:58:56 +0300

    Update binance.js

--- js/binance.js
+++ js/binance.js
@@ -25,7 +25,7 @@ module.exports = class binance extends Exchange {
                 'margin': true,
                 'swap': true,
                 'future': true,
-                'option': true,
+                'option': undefined,
                 'addMargin': true,
                 'cancelAllOrders': true,
                 'cancelOrder': true,
