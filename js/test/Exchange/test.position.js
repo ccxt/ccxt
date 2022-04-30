e32e07c78d72f700b57a4a81e530b4c32c5eb205
T. Todua <7117978+ttodua@users.noreply.github.com>
2022-05-01 01:39:28 +0400

    Update test.position.js

--- js/test/Exchange/test.position.js
+++ js/test/Exchange/test.position.js
@@ -7,9 +7,9 @@ function testPosition (exchange, position, symbol, now) {
     assert ('id' in position);
     assert (position['id'] === undefined || typeof position['id'] === 'string');
     assert ('timestamp' in position);
-    assert (position['timestamp'] === undefiend || typeof position['timestamp'] === 'number');
-    assert (position['timestamp'] === undefiend || position['timestamp'] > 1230940800000); // 03 Jan 2009 - first cryptocurrency block creation time
-    assert (position['timestamp'] === undefiend || position['timestamp'] < now);
+    assert (position['timestamp'] === undefined || typeof position['timestamp'] === 'number');
+    assert (position['timestamp'] === undefined || position['timestamp'] > 1230940800000); // 03 Jan 2009 - first cryptocurrency block creation time
+    assert (position['timestamp'] === undefined || position['timestamp'] < now);
     assert ('datetime' in position);
     assert (position['datetime'] === exchange.iso8601 (position['timestamp']));
     assert ('symbol' in position);
