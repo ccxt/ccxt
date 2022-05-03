d85236e5b0fc816a44a93a46ca698d19135912d9
Sam Germain <sam.germain@usask.ca>
2022-05-03 16:43:59 -0600

    python fetchTransfer example string fix

--- examples/py/binance-universal-transfer.py
+++ examples/py/binance-universal-transfer.py
@@ -14,15 +14,15 @@ def main():
 
     # apiKey must have universal transfer permissions
     binance = ccxt.binance({
-        "apiKey": "",
-        "secret": "",
+        "apiKey": "...",
+        "secret": "...",
     })
     binance.load_markets()
 
-    pprint(binance.transfer('USDT', 1, 'spot', 'future'))
+    pprint(binance.transfer('USDT', 0.1, 'spot', 'future'))
     transfers = binance.fetchTransfers()
-    pprint('got ', len(transfers), ' transfers')
-    pprint(binance.transfer('USDT', 1, 'spot', 'margin'))
+    pprint('got ' + str(len(transfers)) + ' transfers')
+    pprint(binance.transfer('USDT', 0.1, 'spot', 'margin'))
 
     # binance requires from and to in the params
     pprint(binance.fetchTransfers(None, None, None, {'from': 'spot', 'to': 'margin'}))
