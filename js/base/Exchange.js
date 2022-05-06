5ef804bd176ccafd366d4eeb8a8c70ee1264d050
Sam Germain <sam.germain@usask.ca>
2022-05-05 18:15:10 -0600

    base.exchange.parseLeverageTiers symbols and marketIdKey default is undefined

--- js/base/Exchange.js
+++ js/base/Exchange.js
@@ -2261,7 +2261,8 @@ module.exports = class Exchange {
         return this.options['timeDifference'];
     }
 
-    parseLeverageTiers (response, symbols, marketIdKey) {
+    parseLeverageTiers (response, symbols = undefined, marketIdKey = undefined) {
+        // * marketIdKey should only be undefined when response is a dictionary
         const tiers = {};
         for (let i = 0; i < response.length; i++) {
             const item = response[i];
