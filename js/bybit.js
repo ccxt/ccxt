8e6cc7bfaed7b9fbe10345a79a59948f1d078b43
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-05-09 09:33:24 +0100

    fix signing

--- js/bybit.js
+++ js/bybit.js
@@ -3438,9 +3438,16 @@ module.exports = class bybit extends Exchange {
                 const isOpenapi = url.indexOf ('openapi') >= 0;
                 const timestamp = this.milliseconds ();
                 if (isOpenapi) {
-                    let query = '';
+                    let query = {};
                     if (Object.keys (params).length) {
-                        query = this.json (params);
+                        query = params;
+                    }
+                    // this is a PHP specific check
+                    const queryLength = query.length;
+                    if (Array.isArray (query) && queryLength === 0) {
+                        query = '';
+                    } else {
+                        query = this.json (query);
                     }
                     body = query;
                     const payload = timestamp.toString () + this.apiKey + query;
