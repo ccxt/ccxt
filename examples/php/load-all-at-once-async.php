150aed4c51da7378e50d6edc8e0da982fbb2b230
Igor Kroitor <igor.kroitor@gmail.com>
2022-05-11 04:43:44 +0300

    php/test enableRateLimit is on by default

--- examples/php/load-all-at-once-async.php
+++ examples/php/load-all-at-once-async.php
@@ -3,11 +3,11 @@
 use Recoil\React\ReactKernel;
 use Recoil\Recoil;
 
-$root = dirname (dirname (dirname (__FILE__)));
+$root = dirname(dirname(dirname(__FILE__)));
 
 include $root . '/ccxt.php';
 
-date_default_timezone_set ('UTC');
+date_default_timezone_set('UTC');
 
 function loadMarkets($exchange) {
     try {
@@ -16,21 +16,21 @@ function loadMarkets($exchange) {
         $msg = count (array_values ($markets)) . " markets: " .
             implode (', ', array_slice ($exchange->symbols, 0, 5)) . "...\n";
     } catch (\ccxt\RequestTimeout $e) {
-        $msg = '[Timeout Error] ' . $e->getMessage () . ' (ignoring)' . "\n";
+        $msg = '[Timeout Error] ' . $e->getMessage() . ' (ignoring)' . "\n";
     } catch (\ccxt\DDoSProtection $e) {
-        $msg = '[DDoS Protection Error] ' . $e->getMessage () . ' (ignoring)' . "\n";
+        $msg = '[DDoS Protection Error] ' . $e->getMessage() . ' (ignoring)' . "\n";
     } catch (\ccxt\AuthenticationError $e) {
-        $msg = '[Authentication Error] ' . $e->getMessage () . ' (ignoring)' . "\n";
+        $msg = '[Authentication Error] ' . $e->getMessage() . ' (ignoring)' . "\n";
     } catch (\ccxt\ExchangeNotAvailable $e) {
-        $msg = '[Exchange Not Available] ' . $e->getMessage () . ' (ignoring)' . "\n";
+        $msg = '[Exchange Not Available] ' . $e->getMessage() . ' (ignoring)' . "\n";
     } catch (\ccxt\NotSupported $e) {
-        $msg = '[Not Supported] ' . $e->getMessage () . ' (ignoring)' . "\n";
+        $msg = '[Not Supported] ' . $e->getMessage() . ' (ignoring)' . "\n";
     } catch (\ccxt\NetworkError $e) {
-        $msg = '[Network Error] ' . $e->getMessage () . ' (ignoring)' . "\n";
+        $msg = '[Network Error] ' . $e->getMessage() . ' (ignoring)' . "\n";
     } catch (\ccxt\ExchangeError $e) {
-        $msg = '[Exchange Error] ' . $e->getMessage () . ' (ignoring)' . "\n";
+        $msg = '[Exchange Error] ' . $e->getMessage() . ' (ignoring)' . "\n";
     } catch (Exception $e) {
-        $msg = '[Error] ' . $e->getMessage () . "\n";
+        $msg = '[Error] ' . $e->getMessage() . "\n";
     }
     echo "--------------------------------------------\n";
     echo $exchange->id . "\n";
