07ff18518ad188dfcff51393e8d8dc4bca26af32
T.Todua <7117978+ttodua@users.noreply.github.com>
2022-05-14 01:37:37 +0400

    fbi

--- js/test/Exchange/test.fetchBorrowInterest.js
+++ js/test/Exchange/test.fetchBorrowInterest.js
@@ -23,7 +23,7 @@ module.exports = async (exchange, code, symbol) => {
             'info': {},
         }
 
-        const interest = await exchange.fetchBorrowInterest (code, symbol)
+        const interest = await exchange[method] (code, symbol)
 
         console.log (code, method, interest['datetime'], 'symbol: ', symbol,  'interest: ', interest['interest'], 'interestRate: ', interest['interestRate'], 'amountBorrowed: ', interest['amountBorrowed'])
 
