00e7cf2c75b3adee544b0acca1b0afdc12e93a5c
Igor Kroitor <igor.kroitor@gmail.com>
2022-05-13 17:59:10 +0300

    gateio fix #13223 fix #13227

--- js/gateio.js
+++ js/gateio.js
@@ -3419,7 +3419,7 @@ module.exports = class gateio extends Exchange {
             result = [];
             for (let i = 0; i < response.length; i++) {
                 const orders = this.safeValue (response[i], 'orders');
-                result = result.concat (orders);
+                result = this.arrayConcat (orders);
             }
         }
         const orders = this.parseOrders (result, market, since, limit);
