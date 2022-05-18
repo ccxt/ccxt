8eaa8bfe603487812cadb32816705b1210283db6
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-05-18 11:39:14 +0100

    fix fetchTickers

--- js/cryptocom.js
+++ js/cryptocom.js
@@ -497,7 +497,7 @@ module.exports = class cryptocom extends Exchange {
             const symbol = market['symbol'];
             result[symbol] = this.parseTicker (ticker, market);
         }
-        return result;
+        return this.filterByArray (result, 'symbol', symbols);
     }
 
     async fetchTicker (symbol, params = {}) {
