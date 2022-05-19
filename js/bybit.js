0842b3afe6f735ce38f4542af42423a0113f2540
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-05-19 17:54:03 +0100

    remove market types

--- js/bybit.js
+++ js/bybit.js
@@ -2473,7 +2473,6 @@ module.exports = class bybit extends Exchange {
         const marketId = this.safeString (order, 'symbol');
         market = this.safeMarket (marketId, market);
         const symbol = market['symbol'];
-        let feeCurrency = undefined;
         let timestamp = this.parse8601 (this.safeString2 (order, 'created_at', 'created_time'));
         if (timestamp === undefined) {
             timestamp = this.safeNumber2 (order, 'time', 'transactTime');
@@ -2489,13 +2488,6 @@ module.exports = class bybit extends Exchange {
         const cost = this.safeString (order, 'cum_exec_value');
         const filled = this.safeString2 (order, 'cum_exec_qty', 'executedQty');
         const remaining = this.safeString (order, 'leaves_qty');
-        const marketTypes = this.safeValue (this.options, 'marketTypes', {});
-        const marketType = this.safeString (marketTypes, symbol);
-        if (marketType === 'linear') {
-            feeCurrency = market['quote'];
-        } else {
-            feeCurrency = market['base'];
-        }
         let lastTradeTimestamp = this.safeTimestamp (order, 'last_exec_time');
         if (lastTradeTimestamp === 0) {
             lastTradeTimestamp = undefined;
@@ -2511,6 +2503,7 @@ module.exports = class bybit extends Exchange {
         const feeCostString = this.safeString (order, 'cum_exec_fee');
         let fee = undefined;
         if (feeCostString !== undefined) {
+            const feeCurrency = market['linear'] ? market['quote'] : market['base'];
             fee = {
                 'cost': feeCostString,
                 'currency': feeCurrency,
