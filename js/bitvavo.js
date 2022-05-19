0553576ad9783c7121b30e6b1d47eb684e79f390
Igor Kroitor <igor.kroitor@gmail.com>
2022-05-19 07:11:51 +0300

    bitvavo new logo

--- js/bitvavo.js
+++ js/bitvavo.js
@@ -92,7 +92,7 @@ module.exports = class bitvavo extends Exchange {
                 '1d': '1d',
             },
             'urls': {
-                'logo': 'https://user-images.githubusercontent.com/1294454/83165440-2f1cf200-a116-11ea-9046-a255d09fb2ed.jpg',
+                'logo': 'https://user-images.githubusercontent.com/1294454/169202626-bd130fc5-fcf9-41bb-8d97-6093225c73cd.jpg',
                 'api': {
                     'public': 'https://api.bitvavo.com',
                     'private': 'https://api.bitvavo.com',
