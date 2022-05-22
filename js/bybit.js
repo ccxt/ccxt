12e5b1e4b3f4f1c9167f31c3abfc4c84cbecbd7b
Igor Kroitor <igor.kroitor@gmail.com>
2022-05-22 16:38:36 +0300

    bybit parseDepositAddress empty tag string fix

--- js/bybit.js
+++ js/bybit.js
@@ -3594,7 +3594,10 @@ module.exports = class bybit extends Exchange {
         //     }
         //
         const address = this.safeString (depositAddress, 'address_deposit');
-        const tag = this.safeString (depositAddress, 'tag_deposit');
+        let tag = this.safeString (depositAddress, 'tag_deposit');
+        if (tag === '') {
+            tag = undefined;
+        }
         const code = this.safeString (currency, 'code');
         const chain = this.safeString (depositAddress, 'chain');
         this.checkAddress (address);
