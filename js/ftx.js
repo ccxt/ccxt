55bf379227e93c055aa5e534ed253cff2d34fbb4
Igor Kroitor <igor.kroitor@gmail.com>
2022-05-23 17:54:25 +0300

    ftx fetchTickers ids vs symbols fix #13401

--- js/ftx.js
+++ js/ftx.js
@@ -799,6 +799,7 @@ module.exports = class ftx extends Exchange {
          * @returns {dict} an array of [ticker structures]{@link https://docs.ccxt.com/en/latest/manual.html#ticker-structure}
          */
         await this.loadMarkets ();
+        symbols = this.marketSymbols (symbols);
         const response = await this.publicGetMarkets (params);
         //
         //     {
