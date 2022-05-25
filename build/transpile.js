16108c280f902046d90f7cca954e5b4e38555da2
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-05-25 17:37:56 +0100

    init transpiling process

--- build/transpile.js
+++ build/transpile.js
@@ -1274,6 +1274,100 @@ class Transpiler {
 
     // ========================================================================
 
+    transpileExchangeCommonFiles(jsFolder, filename) {
+        const jsPath = jsFolder + '/' + filename
+        const className = 'Exchange'
+        let contents = fs.readFileSync (jsPath, 'utf8')
+
+        let methods = contents.trim ().split (/\n\s*\n/)
+        methods.pop(); // remove modules.exports = {...}
+
+        let python2 = []
+        let python3 = []
+        let php = []
+        let phpAsync = []
+
+        let methodNames = []
+
+        // run through all methods
+        for (let i = 0; i < methods.length; i++) {
+            // parse the method signature
+            let part = methods[i].trim ()
+            let lines = part.split ("\n")
+            let signature = lines[0].trim ()
+            let methodSignatureRegex = /(async |)([\S]+)\s\(([^)]*)\)\s*{/ // signature line
+            let matches = methodSignatureRegex.exec (signature)
+
+            if (!matches) {
+                log.red (methods[i])
+                log.yellow.bright ("\nMake sure your methods don't have empty lines!\n")
+            }
+
+            // async or not
+            let keyword = matches[1]
+
+            // method name
+            let method = matches[2]
+
+            methodNames.push (method)
+
+            method = unCamelCase (method)
+
+            // method arguments
+            let args = matches[3].trim ()
+
+            // extract argument names and local variables
+            args = args.length ? args.split (',').map (x => x.trim ()) : []
+
+            // get names of all method arguments for later substitutions
+            let variables = args.map (arg => arg.split ('=').map (x => x.trim ()) [0])
+
+            // add $ to each argument name in PHP method signature
+            let phpArgs = args.join (', $').trim ().replace (/undefined/g, 'null').replace (/\{\}/g, 'array ()')
+            phpArgs = phpArgs.length ? ('$' + phpArgs) : ''
+
+            // remove excessive spacing from argument defaults in Python method signature
+            let pythonArgs = args.map (x => x.replace (' = ', '='))
+                .join (', ')
+                .replace (/undefined/g, 'None')
+                .replace (/false/g, 'False')
+                .replace (/true/g, 'True')
+
+            // method body without the signature (first line)
+            // and without the closing bracket (last line)
+            let js = lines.slice (1, -1).join ("\n")
+
+            // transpile everything
+            let { python3Body, python2Body, phpBody, phpAsyncBody } = this.transpileJavaScriptToPythonAndPHP ({ js, className, variables, removeEmptyLines: true })
+
+            // compile the final Python code for the method signature
+            let pythonString = 'def ' + method + '(self' + (pythonArgs.length ? ', ' + pythonArgs : '') + '):'
+
+            // compile signature + body for Python sync
+            python2.push ('');
+            python2.push ('    ' + pythonString);
+            python2.push (python2Body);
+
+            // compile signature + body for Python async
+            python3.push ('');
+            python3.push ('    ' + keyword + pythonString);
+            python3.push (python3Body);
+
+            // compile signature + body for PHP
+            php.push ('');
+            php.push ('    public function ' + method + '(' + phpArgs + ') {');
+            php.push (phpBody);
+            php.push ('    }')
+
+            phpAsync.push ('');
+            phpAsync.push ('    public function ' + method + '(' + phpArgs + ') {');
+            phpAsync.push (phpAsyncBody);
+            phpAsync.push ('    }')
+        }
+    }
+
+    // ========================================================================
+
     exportTypeScriptClassNames (file, classes) {
 
         log.bright.cyan ('Exporting TypeScript class names →', file.yellow)
@@ -1751,6 +1845,8 @@ class Transpiler {
         createFolderRecursively (phpAsyncFolder)
 
         //*
+
+        this.transpileExchangeCommonFiles('./js/base', 'ExchangeCommon.js') 
 
         const classes = this.transpileDerivedExchangeFiles ('./js/', options, pattern, force)
 
