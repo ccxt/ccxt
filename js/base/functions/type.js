81baca8ce3eb49dfce3ef3e5eccb8a348ddf2f4a
Sam Germain <sam.germain@usask.ca>
2022-05-25 21:38:29 -0600

    safeString2 does not take empty string ""

--- js/base/functions/type.js
+++ js/base/functions/type.js
@@ -15,7 +15,7 @@ const isNumber          = Number.isFinite
 /*  .............................................   */
 
 const prop = (o, k) => (isObject (o) ? o[k] : undefined)
-    , prop2 = (o, k1, k2) => (!isObject (o) ? undefined : (((k1 in o) && (o[k1] !== null)) ? o[k1] : o[k2]))
+    , prop2 = (o, k1, k2) => (!isObject (o) ? undefined : (((k1 in o) && (o[k1] !== null) && o[k1] !== '') ? o[k1] : o[k2]))
     , getValueFromKeysInArray = (object, array) => object[array.find (k => prop (object,k) !== undefined)]
 
 /*  .............................................   */
