9c55331ed5c463a353f863aee215d45e0a2ea4d4
Igor Kroitor <igor.kroitor@gmail.com>
2022-05-26 18:44:57 +0300

    test_async.php test_accounts fixup

--- php/test/test_async.php
+++ php/test/test_async.php
@@ -302,7 +302,7 @@ function test_accounts($exchange) {
     $method = 'fetchAccounts';
     if ($exchange->has[$method]) {
         dump(green($exchange->id), 'executing ' . $method . '()');
-        $accounts = yield $exchange->{$method}($symbol);
+        $accounts = yield $exchange->{$method}();
         foreach ($accounts as $account) {
             test_account($exchange, $account);
         }
