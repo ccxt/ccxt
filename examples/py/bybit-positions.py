0967870b58887182124fa8a10f9b9d976f5f3fea
Igor Kroitor <igor.kroitor@gmail.com>
2022-05-31 00:02:11 +0300

    examples/py/bybit-positions.py upgrade and fix #13495

--- examples/py/bybit-positions.py
+++ examples/py/bybit-positions.py
@@ -13,32 +13,29 @@ from pprint import pprint
 print('CCXT Version:', ccxt.__version__)
 
 exchange = ccxt.bybit ({
-    'apiKey': 'YOUR_API_KEY',
-    'secret': 'YOUR_SECRET',
+    # 'apiKey': 'YOUR_API_KEY',
+    # 'secret': 'YOUR_SECRET',
+    "apiKey": "AOIOT8vKgfyQ4meCod",
+    "secret": "Tiyvoh5J3bFfwR1vwdFq2jBgXjykr6XPBHm5",
+
 })
 
-# https://github.com/ccxt/ccxt/wiki/Manual#loading-markets
+markets = exchange.load_markets() # https://github.com/ccxt/ccxt/wiki/Manual#loading-markets
 
-markets = exchange.load_markets()
-
-# exchange.verbose = True  # uncomment for debugging
-
-# https://github.com/ccxt/ccxt/wiki/Manual#implicit-api-methods
+exchange.verbose = True  # uncomment for debugging
 
 # -----------------------------------------------------------------------------
 
-symbol = 'BTC/USDT'
+symbol = 'BTC/USDT:USDT'
 market = exchange.market(symbol)
-
-response = exchange.private_linear_get_position_list({'symbol':market['id']})
-linear_positions = response['result']
+params = {'subType':'linear' if market['linear'] else 'inverse'}
+linear_positions = exchange.fetch_positions([ symbol ], params)
 pprint(linear_positions)
 
 # -----------------------------------------------------------------------------
 
-symbol = 'BTC/USD'
+symbol = 'BTC/USD:BTC'
 market = exchange.market(symbol)
-
-response = exchange.v2_private_get_position_list({'symbol':market['id']})
-inverse_positions = response['result']
+params = {'subType':'linear' if market['linear'] else 'inverse'}
+inverse_positions = exchange.fetch_positions([ symbol ], params)
 pprint(inverse_positions)
