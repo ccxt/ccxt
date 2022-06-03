2e7491b87a250af0a7fee8dadc0e1a4cb6767e2c
Sam Germain <sam.germain@usask.ca>
2022-06-02 21:35:16 -0600

    thottle.js comments

--- js/base/functions/throttle.js
+++ js/base/functions/throttle.js
@@ -15,31 +15,31 @@ class Throttle {
             maxCapacity: 2000,
             tokens: 0,
             cost: 1.0,
-        }
-        Object.assign (this.config, config)
-        this.queue = []
-        this.running = false
+        };
+        Object.assign (this.config, config);
+        this.queue = [];  // requests to be sent
+        this.running = false;
     }
 
     async loop () {
-        let lastTimestamp = now ()
-        while (this.running) {
-            const { resolver, cost } = this.queue[0]
-            if (this.config['tokens'] >= 0) {
-                this.config['tokens'] -= cost
-                resolver ()
-                this.queue.shift ()
+        let lastTimestamp = now ();
+        while (this.running) { // loops through method calls in the queue, executing them if tokens available, and waiting if tokens not available
+            const { resolver, cost } = this.queue[0];
+            if (this.config['tokens'] >= 0) { // if rate limit hasn't been reached
+                this.config['tokens'] -= cost;
+                resolver (); // method for api endpoint
+                this.queue.shift ();
                 // contextswitch
-                await Promise.resolve ()
+                await Promise.resolve (); // what does this do?
                 if (this.queue.length === 0) {
-                    this.running = false
+                    this.running = false;
                 }
             } else {
                 await sleep (this.config['delay'] * 1000);
-                const current = now ()
-                const elapsed = current - lastTimestamp
-                lastTimestamp = current
-                this.config['tokens'] = Math.min (this.config['tokens'] + (this.config['refillRate'] * elapsed), this.config['capacity'])
+                const current = now ();
+                const elapsed = current - lastTimestamp;
+                lastTimestamp = current;
+                this.config['tokens'] = Math.min (this.config['tokens'] + (this.config['refillRate'] * elapsed), this.config['capacity']);
             }
         }
     }
@@ -47,29 +47,28 @@ class Throttle {
 
 function throttle (config) {
     function inner (cost = undefined) {
-        let resolver
+        let resolver;
         const promise = new Promise ((resolve, reject) => {
-            resolver = resolve
+            resolver = resolve;
         })
         if (this.queue.length > this.config['maxCapacity']) {
-            throw new Error ('throttle queue is over maxCapacity')
+            throw new Error ('throttle queue is over maxCapacity');
         }
-        cost = (cost === undefined) ? this.config['cost'] : cost
-        this.queue.push ({
-            resolver,
-            cost,
-        })
-        if (!this.running) {
-            this.running = true
-            this.loop ()
+        if (cost === undefined) {
+            cost = this.config['cost'];
         }
-        return promise
+        this.queue.push ({ resolver, cost });
+        if (!this.running) { // start the throttle loop if it's not running
+            this.running = true;
+            this.loop ();
+        }
+        return promise;
     }
-    const instance = new Throttle (config)
-    const bound = inner.bind (instance)
+    const instance = new Throttle (config);
+    const bound = inner.bind (instance);
     // useful for inspecting the tokenBucket
-    bound.config = instance.config
-    return bound
+    bound.config = instance.config;
+    return bound;
 }
 
 module.exports = {
