46d6fe6541c673796041f1e5809a7fd8ab17d70a
Igor Kroitor <igor.kroitor@gmail.com>
2022-06-19 12:53:08 +0300

    huobi fetchCurrencies missing params fix #13953

--- js/huobi.js
+++ js/huobi.js
@@ -2653,7 +2653,7 @@ module.exports = class huobi extends Exchange {
          * @param {dict} params extra parameters specific to the huobi api endpoint
          * @returns {dict} an associative dictionary of currencies
          */
-        const response = await this.spotPublicGetV2ReferenceCurrencies ();
+        const response = await this.spotPublicGetV2ReferenceCurrencies (params);
         //     {
         //       "code": 200,
         //       "data": [
