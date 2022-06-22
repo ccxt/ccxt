9b34b5e72cae649edfca5013db6f1218e1ee8632
Dan <dan.krm@outlook.com>
2022-06-22 15:12:08 -0600

    Adjusted the error message to use the new method name

--- js/binance.js
+++ js/binance.js
@@ -6060,7 +6060,7 @@ module.exports = class binance extends Exchange {
         const marginMode = this.safeString (params, 'marginMode', defaultMarginMode); // cross or isolated
         if (marginMode === 'isolated') {
             if (symbol === undefined) {
-                throw new ArgumentsRequired (this.id + 'repayMarginLoan() requires a symbol argument for isolated margin');
+                throw new ArgumentsRequired (this.id + ' repayMargin() requires a symbol argument for isolated margin');
             }
             request['isIsolated'] = 'TRUE';
             request['symbol'] = market['id'];
