a572d8b3c4ea566d38844f989d4481ccfb932e95
Igor Kroitor <igor.kroitor@gmail.com>
2022-06-24 02:38:19 +0300

    mexc3 fetchFundingHistory timestamp safeInteger fix #12376

--- js/mexc3.js
+++ js/mexc3.js
@@ -2948,7 +2948,7 @@ module.exports = class mexc3 extends Exchange {
         const result = [];
         for (let i = 0; i < resultList.length; i++) {
             const entry = resultList[i];
-            const timestamp = this.safeString (entry, 'settleTime');
+            const timestamp = this.safeInteger (entry, 'settleTime');
             result.push ({
                 'info': entry,
                 'symbol': symbol,
