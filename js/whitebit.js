e605844ed88d4f4885683804e1cd96d8a880a107
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-06-27 15:01:03 +0100

    add missing decode

--- js/whitebit.js
+++ js/whitebit.js
@@ -1791,7 +1791,7 @@ module.exports = class whitebit extends Exchange {
             headers = {
                 'Content-Type': 'application/json',
                 'X-TXC-APIKEY': this.apiKey,
-                'X-TXC-PAYLOAD': payload,
+                'X-TXC-PAYLOAD': this.decode (payload),
                 'X-TXC-SIGNATURE': signature,
             };
         }
