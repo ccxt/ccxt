4bbee10d1215a6ff1a127fb69cb2c3b60d5d5271
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-06-29 13:48:23 +0100

    total used

--- js/base/Exchange.js
+++ js/base/Exchange.js
@@ -933,13 +933,13 @@ module.exports = class Exchange {
             let total = this.safeString (balance[code], 'total');
             let free = this.safeString (balance[code], 'free');
             let used = this.safeString (balance[code], 'used');
-            if (total === undefined) {
+            if (total === undefined && free !== undefined && used !== undefined) {
                 total = Precise.stringAdd (free, used);
             }
-            if (free === undefined) {
+            if (free === undefined && total !== undefined && used !== undefined) {
                 free = Precise.stringSub (total, used);
             }
-            if (used === undefined) {
+            if (used === undefined && total !== undefined && free !== undefined) {
                 used = Precise.stringSub (total, free);
             }
             balance[code]['free'] = this.parseNumber (free);
