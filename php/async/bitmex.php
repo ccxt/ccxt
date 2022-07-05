9f7ac595a54677f19412f7c332da8847d30a260d
Travis CI <travis@travis-ci.org>
2022-07-05 10:28:47 +0000

    1.90.3
    [ci skip]

--- php/async/bitmex.php
+++ php/async/bitmex.php
@@ -498,9 +498,12 @@ class bitmex extends Exchange {
             $account = $this->account();
             $free = $this->safe_string($balance, 'availableMargin');
             $total = $this->safe_string($balance, 'marginBalance');
-            if ($code === 'BTC') {
+            if ($code !== 'USDT') {
                 $free = Precise::string_div($free, '1e8');
                 $total = Precise::string_div($total, '1e8');
+            } else {
+                $free = Precise::string_div($free, '1e6');
+                $total = Precise::string_div($total, '1e6');
             }
             $account['free'] = $free;
             $account['total'] = $total;
