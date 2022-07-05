0bcbdd5d3b765c253a7a48fc95aa88f801e929d8
Sam Germain <sam.germain@usask.ca>
2022-07-05 16:22:11 -0600

    bibox.fetchOHLCV v4

--- js/bibox.js
+++ js/bibox.js
@@ -56,17 +56,19 @@ module.exports = class bibox extends Exchange {
                 'withdraw': true,
             },
             'timeframes': {
-                '1m': '1min',
-                '5m': '5min',
-                '15m': '15min',
-                '30m': '30min',
-                '1h': '1hour',
-                '2h': '2hour',
-                '4h': '4hour',
-                '6h': '6hour',
-                '12h': '12hour',
-                '1d': 'day',
-                '1w': 'week',
+                '1m': '1m',
+                '3m': '3m',
+                '5m': '5m',
+                '15m': '15m',
+                '30m': '30m',
+                '1h': '1h',
+                '2h': '2h',
+                '4h': '4h',
+                '6h': '6h',
+                '12h': '12h',
+                '1d': '1d',
+                '1w': '1w',
+                '1M': '1M',
             },
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/51840849/77257418-3262b000-6c85-11ea-8fb8-20bdf20b3592.jpg',
@@ -626,22 +628,25 @@ module.exports = class bibox extends Exchange {
 
     parseOHLCV (ohlcv, market = undefined) {
         //
-        //     {
-        //         "time":1591448220000,
-        //         "open":"0.02507029",
-        //         "high":"0.02507029",
-        //         "low":"0.02506349",
-        //         "close":"0.02506349",
-        //         "vol":"5.92000000"
-        //     }
+        //    [
+        //        '1656702000000',      // start time
+        //        '19449.4',            // opening price
+        //        '19451.7',            // maximum price
+        //        '19290.6',            // minimum price
+        //        '19401.5',            // closing price
+        //        '73.328833',          // transaction volume
+        //        '1419466.3805812',    // transaction value
+        //        '45740585',           // first transaction id
+        //        2899                  // The total number of transactions in the range
+        //    ]
         //
         return [
-            this.safeInteger (ohlcv, 'time'),
-            this.safeNumber (ohlcv, 'open'),
-            this.safeNumber (ohlcv, 'high'),
-            this.safeNumber (ohlcv, 'low'),
-            this.safeNumber (ohlcv, 'close'),
-            this.safeNumber (ohlcv, 'vol'),
+            this.safeInteger (ohlcv, 0),
+            this.safeNumber (ohlcv, 1),
+            this.safeNumber (ohlcv, 2),
+            this.safeNumber (ohlcv, 3),
+            this.safeNumber (ohlcv, 4),
+            this.safeNumber (ohlcv, 5),
         ];
     }
 
@@ -660,24 +665,30 @@ module.exports = class bibox extends Exchange {
         await this.loadMarkets ();
         const market = this.market (symbol);
         const request = {
-            'cmd': 'kline',
-            'pair': market['id'],
-            'period': this.timeframes[timeframe],
-            'size': limit,
+            'symbol': market['id'],
+            'time_frame': this.timeframes[timeframe],
+            'limit': limit,
         };
-        const response = await this.v1PublicGetMdata (this.extend (request, params));
+        const response = await this.v4PublicGetMarketdataCandles (this.extend (request, params));
         //
-        //     {
-        //         "result":[
-        //             {"time":1591448220000,"open":"0.02507029","high":"0.02507029","low":"0.02506349","close":"0.02506349","vol":"5.92000000"},
-        //             {"time":1591448280000,"open":"0.02506449","high":"0.02506975","low":"0.02506108","close":"0.02506843","vol":"5.72000000"},
-        //             {"time":1591448340000,"open":"0.02506698","high":"0.02506698","low":"0.02506452","close":"0.02506519","vol":"4.86000000"},
-        //         ],
-        //         "cmd":"kline",
-        //         "ver":"1.1"
-        //     }
+        //    {
+        //        t: '3600000',
+        //        e: [
+        //            [
+        //                '1656702000000',      // start time
+        //                '19449.4',            // opening price
+        //                '19451.7',            // maximum price
+        //                '19290.6',            // minimum price
+        //                '19401.5',            // closing price
+        //                '73.328833',          // transaction volume
+        //                '1419466.3805812',    // transaction value
+        //                '45740585',           // first transaction id
+        //                2899                  // The total number of transactions in the range
+        //            ],
+        //            ...
+        //    }
         //
-        const result = this.safeValue (response, 'result', []);
+        const result = this.safeValue (response, 'e', []);
         return this.parseOHLCVs (result, market, timeframe, since, limit);
     }
 
