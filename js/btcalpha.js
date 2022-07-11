e2d0f8cfe0c8d6e09bce2640db0e97650b94c6ee
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-07-11 17:27:55 +0100

    use safeTimestamp

--- js/btcalpha.js
+++ js/btcalpha.js
@@ -412,7 +412,7 @@ module.exports = class btcalpha extends Exchange {
         //          "status": 20
         //      }
         //
-        const timestamp = this.safeIntegerProduct (transaction, 'timestamp', 1000);
+        const timestamp = this.safeTimestamp (transaction, 'timestamp');
         const currencyId = this.safeString (transaction, 'currency');
         const statusId = this.safeString (transaction, 'status');
         return {
