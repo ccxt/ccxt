e4cc9d91702537b01b73155485b4714910bc5f86
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-07-11 15:29:34 +0100

    fix parseMarginModification

--- js/mexc.js
+++ js/mexc.js
@@ -2845,7 +2845,7 @@ module.exports = class mexc extends Exchange {
     }
 
     parseMarginModification (data, market = undefined) {
-        const statusRaw = this.safeString (data, 'success');
+        const statusRaw = this.safeValue (data, 'success');
         const status = (statusRaw === true) ? 'ok' : 'failed';
         return {
             'info': data,
