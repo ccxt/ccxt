f304055b17ed5768ddd11ea9aa2a1a79d23eda80
Travis CI <travis@travis-ci.org>
2022-07-14 13:45:11 +0000

    1.90.77
    [ci skip]

--- python/ccxt/async_support/bybit.py
+++ python/ccxt/async_support/bybit.py
@@ -4215,6 +4215,7 @@ class bybit(Exchange):
                 # futures only
                 rawPosition = self.safe_value(rawPosition, 'data')
             results.append(self.parse_position(rawPosition, market))
+        symbols = self.market_symbols(symbols)
         return self.filter_by_array(results, 'symbol', symbols, False)
 
     def parse_position(self, position, market=None):
