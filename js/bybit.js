53dd92baa96923c7238d71969373a58ea61bdb15
Dan <dan.krm@outlook.com>
2022-07-20 20:44:02 -0600

    Bybit: Margin support
    Set margin to true and set the remaining margin related methods to false.

--- js/bybit.js
+++ js/bybit.js
@@ -25,7 +25,7 @@ module.exports = class bybit extends Exchange {
             'has': {
                 'CORS': true,
                 'spot': true,
-                'margin': false,
+                'margin': true,
                 'swap': true,
                 'future': true,
                 'option': undefined,
@@ -38,6 +38,8 @@ module.exports = class bybit extends Exchange {
                 'editOrder': true,
                 'fetchBalance': true,
                 'fetchBorrowRate': false,
+                'fetchBorrowRateHistory': false,
+                'fetchBorrowRateHistories': false,
                 'fetchBorrowRates': false,
                 'fetchClosedOrders': true,
                 'fetchCurrencies': true,
