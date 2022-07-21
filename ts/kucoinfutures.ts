80248c37aeea9067dc51213f962b4b992076bdeb
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-07-21 15:59:41 +0100

    fix build

--- ts/kucoinfutures.ts
+++ ts/kucoinfutures.ts
@@ -304,17 +304,6 @@ export default class kucoinfutures extends kucoin {
         });
     }
 
-    // async fetchAccounts (params = {}) {
-    //     /**
-    //      * @method
-    //      * @name kucoinfutures#fetchAccounts
-    //      * @description fetch all the accounts associated with a profile
-    //      * @param {dict} params extra parameters specific to the kucoinfutures api endpoint
-    //      * @returns {dict} a dictionary of [account structures]{@link https://docs.ccxt.com/en/latest/manual.html#account-structure} indexed by the account type
-    //      */
-    //     throw new BadRequest (this.id + ' fetchAccounts() is not supported yet');
-    // }
-
     async fetchStatus (params = {}) {
         /**
          * @method
