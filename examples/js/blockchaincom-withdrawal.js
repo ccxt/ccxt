c22c24379dd754d11a9574d038eac69234acec6e
DoctorSlimm <87606888+DoctorSlimm@users.noreply.github.com>
2022-07-22 11:04:50 +0100

    remove anscicolor requirement

--- examples/js/blockchaincom-withdrawal.js
+++ examples/js/blockchaincom-withdrawal.js
@@ -6,17 +6,14 @@ const log = require('ololog').configure({ locate: false })
 
 
 const exchange = new ccxt.blockchaincom({
-    'secret': 'SECRET',
+    'secret': 'YOUR_API_SECRET',
 })
 
 // blockchaincom specific internal beneficiary id
-const withdrawal_beneficiary = 'WITHDRAWAL_BENEFICIARY'
+const withdrawal_beneficiary = 'WITHDRAWAL_BENEFICIARY';
 
 
-
-require('ansicolor').nice
-
-; (async () => {
+(async () => {
 
     const markets = await exchange.loadMarkets ()
 
@@ -26,25 +23,25 @@ require('ansicolor').nice
 
         // fetch withdrawal beneficiary ids
         const whiteList = await exchange.privateGetWhitelistCurrency({'currency': code})
-        log('Withdrawl Whitelist'.green, whiteList)
+        log('Withdrawl Whitelist', whiteList)
 
         // withdrawal
         let withdrawal = await exchange.withdraw(code, amount, undefined, undefined, { 'beneficiary': withdrawal_beneficiary })
-        log('Withdrawal'.green, withdrawal)
+        log('Withdrawal', withdrawal)
 
     } catch (e) {
         if (e instanceof ccxt.DDoSProtection || e.message.includes('ECONNRESET')) {
-            log.bright.yellow('[DDoS Protection] ' + e.message)
+            log('[DDoS Protection] ' + e.message)
         } else if (e instanceof ccxt.RequestTimeout) {
-            log.bright.yellow('[Request Timeout] ' + e.message)
+            log('[Request Timeout] ' + e.message)
         } else if (e instanceof ccxt.AuthenticationError) {
-            log.bright.yellow('[Authentication Error] ' + e.message)
+            log('[Authentication Error] ' + e.message)
         } else if (e instanceof ccxt.ExchangeNotAvailable) {
-            log.bright.yellow('[Exchange Not Available Error] ' + e.message)
+            log('[Exchange Not Available Error] ' + e.message)
         } else if (e instanceof ccxt.ExchangeError) {
-            log.bright.yellow('[Exchange Error] ' + e.message)
+            log('[Exchange Error] ' + e.message)
         } else if (e instanceof ccxt.NetworkError) {
-            log.bright.yellow('[Network Error] ' + e.message)
+            log('[Network Error] ' + e.message)
         } else {
             throw e;
         }
