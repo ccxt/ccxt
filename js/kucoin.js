19690b6021916f23ab94aba667678b90ee2e8662
leastchaos <64965782+leastchaos@users.noreply.github.com>
2022-07-24 14:29:03 +0800

    kucoin fetchBalance stuck at trade

--- js/kucoin.js
+++ js/kucoin.js
@@ -388,7 +388,6 @@ module.exports = class kucoin extends Exchange {
                 'version': 'v1',
                 'symbolSeparator': '-',
                 'fetchMyTradesMethod': 'private_get_fills',
-                'fetchBalance': 'trade',
                 'fetchMarkets': {
                     'fetchTickersFees': true,
                 },
