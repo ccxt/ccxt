3096ac35c51b8c2bd6e208025fcc5b0d3265bbcd
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-07-29 03:32:30 +0100

    restore

--- php/ClientTrait.php
+++ php/ClientTrait.php
@@ -76,14 +76,11 @@ trait ClientTrait {
         // todo: calculate the backoff delay in php
         $backoff_delay = 0; // milliseconds
         $future = $client->future($message_hash);
-        $subscribed = isset($client->subscriptions[$subscribe_hash]);
-        if (!$subscribed) {
-            $client->subscriptions[$subscribe_hash] = isset($subscription) ? $subscription : true;
-        }
         $connected = $client->connect($backoff_delay);
         $connected->then(
-            function($result) use ($client, $message_hash, $message, $subscribe_hash, $subscription, $subscribed) {
-                if (!$subscribed) {
+            function($result) use ($client, $message_hash, $message, $subscribe_hash, $subscription) {
+                if (!isset($client->subscriptions[$subscribe_hash])) {
+                    $client->subscriptions[$subscribe_hash] = isset($subscription) ? $subscription : true;
                     // todo: add PHP async rate-limiting
                     // todo: decouple signing from subscriptions
                     $options = $this->safe_value($this->options, 'ws');
