9692dfd4e11e7dd7ea78a7c57f921de023ca0ff3
Travis CI <travis@travis-ci.org>
2022-08-02 20:59:29 +0000

    1.91.63
    [ci skip]

--- php/async/zaif.php
+++ php/async/zaif.php
@@ -673,7 +673,7 @@ class zaif extends Exchange {
 
     public function nonce() {
         $nonce = floatval($this->milliseconds() / 1000);
-        return number_format($nonce, 8);
+        return sprintf('%.8f', $nonce);
     }
 
     public function sign($path, $api = 'public', $method = 'GET', $params = array (), $headers = null, $body = null) {
