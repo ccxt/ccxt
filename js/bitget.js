a6c1e61ac6283acae78f999424e7ea0546bc2066
Sam Germain <sam.germain@usask.ca>
2022-08-03 02:36:10 -0600

    bitget.parsePosition added fields stopLoss and takeProfit

--- js/bitget.js
+++ js/bitget.js
@@ -3167,6 +3167,8 @@ module.exports = class bitget extends Exchange {
             'initialMarginPercentage': undefined,
             'leverage': this.safeNumber (position, 'leverage'),
             'marginRatio': undefined,
+            'stopLoss': undefined,
+            'takeProfit': undefined,
         };
     }
 
