5cb119fd553ca9d298ea0162978b88d6734d3ce3
Guilherme Camacho <guilherme.camacho@financialhub.dev>
2022-08-08 15:20:15 -0300

    fix: novadax response for withdraw

--- js/novadax.js
+++ js/novadax.js
@@ -1344,6 +1344,7 @@ module.exports = class novadax extends Exchange {
             'Pending': 'pending',
             'confirming': 'pending',
             'SUCCESS': 'ok',
+            'Success': 'ok',
             'FAIL': 'failed',
         };
         return this.safeString (statuses, status, status);
@@ -1390,7 +1391,13 @@ module.exports = class novadax extends Exchange {
         const updated = this.safeInteger (transaction, 'updatedAt');
         const currencyId = this.safeString (transaction, 'currency');
         const code = this.safeCurrencyCode (currencyId, currency);
-        const status = this.parseTransactionStatus (this.safeString (transaction, 'state'));
+        let status = ''
+        if(type === 'withdraw'){
+            this.parseTransactionStatus (this.safeString (transaction, 'message'));
+        }else{
+            this.parseTransactionStatus (this.safeString (transaction, 'state'));
+        }
+
         const network = this.safeString (transaction, 'chain');
         return {
             'info': transaction,
