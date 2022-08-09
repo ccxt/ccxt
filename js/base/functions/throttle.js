e30f7e1acc5e2ca24c89008e346b10464307a8a6
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-08-09 22:22:05 +0100

    restore throttle

--- js/base/functions/throttle.js
+++ js/base/functions/throttle.js
@@ -3,56 +3,48 @@
 /*  ------------------------------------------------------------------------ */
 
 const { now, sleep } = require ('./time');
+
 /*  ------------------------------------------------------------------------ */
 
-const DEFAULT_CONFIG = {
-    refillRate: 1.0,
-    delay: 0.001,
-    capacity: 1.0,
-    maxCapacity: Number.MAX_SAFE_INTEGER,
-    tokens: 0,
-    cost: 1.0,
-    lastTimestamp: 0,
-};
-
 class Throttle {
-    constructor () {
+    constructor (config) {
+        this.config = {
+            refillRate: 1.0,
+            delay: 0.001,
+            capacity: 1.0,
+            maxCapacity: 2000,
+            tokens: 0,
+            cost: 1.0,
+        };
+        Object.assign (this.config, config);
         this.queue = [];
         this.running = false;
     }
 
-    instanceOf (config) {
-        return Object.assign ({}, DEFAULT_CONFIG, config);
-    }
-
     async loop () {
+        let lastTimestamp = now ();
         while (this.running) {
-            for (let i = 0; i < this.queue.length; i++) {
-                const { resolver, cost, config } = this.queue[i];
-                if (config['tokens'] >= 0) {
-                    config['tokens'] -= cost;
-                    resolver ();
-                    this.queue.splice (i, 1);
-                    i--;
-                    // contextswitch
-                    await Promise.resolve ();
-                    if (this.queue.length === 0) {
-                        this.running = false;
-                    }
-                } else {
-                    const current = now ();
-                    const elapsed = current - config['lastTimestamp'];
-                    config['lastTimestamp'] = current;
-                    const tokens = config['tokens'] + (config['refillRate'] * elapsed);
-                    config['tokens'] = Math.min (tokens, config['capacity']);
+            const { resolver, cost } = this.queue[0];
+            if (this.config['tokens'] >= 0) {
+                this.config['tokens'] -= cost;
+                resolver ();
+                this.queue.shift ();
+                // contextswitch
+                await Promise.resolve ();
+                if (this.queue.length === 0) {
+                    this.running = false;
                 }
+            } else {
+                await sleep (this.config['delay'] * 1000);
+                const current = now ();
+                const elapsed = current - lastTimestamp;
+                lastTimestamp = current;
+                const tokens = this.config['tokens'] + (this.config['refillRate'] * elapsed);
+                this.config['tokens'] = Math.min (tokens, this.config['capacity']);
             }
-            await sleep (DEFAULT_CONFIG['delay'] * 1000)
         }
     }
 }
-
-const globalThrottle = new Throttle ()
 
 function throttle (config) {
     function inner (cost = undefined) {
@@ -60,25 +52,21 @@ function throttle (config) {
         const promise = new Promise ((resolve, reject) => {
             resolver = resolve;
         });
-        if (globalThrottle.queue.length > this['maxCapacity']) {
+        if (this.queue.length > this.config['maxCapacity']) {
             throw new Error ('throttle queue is over maxCapacity');
         }
-        cost = (cost === undefined) ? this['cost'] : cost;
-        this['lastTimestamp'] = now ()
-        globalThrottle.queue.push ({
-            resolver,
-            cost,
-            'config': this,
-        });
-        if (!globalThrottle.running) {
-            globalThrottle.running = true;
-            globalThrottle.loop ()
+        cost = (cost === undefined) ? this.config['cost'] : cost;
+        this.queue.push ({ resolver, cost });
+        if (!this.running) {
+            this.running = true;
+            this.loop ();
         }
         return promise;
     }
-    const configInstance = globalThrottle.instanceOf (config);
-    const bound = inner.bind (configInstance);
+    const instance = new Throttle (config);
+    const bound = inner.bind (instance);
     // useful for inspecting the tokenBucket
+    bound.config = instance.config;
     return bound;
 }
 
