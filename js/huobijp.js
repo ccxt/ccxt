f842a482911312b1d8b0981b006f232b70459329
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-08-09 06:07:33 +0100

    safeValue-calcRateLimiterCost

--- js/huobijp.js
+++ js/huobijp.js
@@ -1893,10 +1893,6 @@ module.exports = class huobijp extends Exchange {
         return { 'url': url, 'method': method, 'body': body, 'headers': headers };
     }
 
-    calculateRateLimiterCost (api, method, path, params, config = {}, context = {}) {
-        return this.safeInteger (config, 'cost', 1);
-    }
-
     handleErrors (httpCode, reason, url, method, headers, body, response, requestHeaders, requestBody) {
         if (response === undefined) {
             return; // fallback to default error handler
