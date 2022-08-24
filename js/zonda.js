aeb16bb051dd4290388b37ed36d3a3feadad1174
T.Todua <7117978+ttodua@users.noreply.github.com>
2022-08-24 14:29:44 +0400

    ohlcv type

--- js/zonda.js
+++ js/zonda.js
@@ -694,7 +694,7 @@ module.exports = class zonda extends Exchange {
          * @param {int|undefined} since timestamp in ms of the earliest candle to fetch
          * @param {int|undefined} limit the maximum amount of candles to fetch
          * @param {object} params extra parameters specific to the zonda api endpoint
-         * @returns {[[int]]} A list of candles ordered as timestamp, open, high, low, close, volume
+         * @returns {[[number]]} A list of candles ordered as timestamp, open, high, low, close, volume
          */
         await this.loadMarkets ();
         const market = this.market (symbol);
