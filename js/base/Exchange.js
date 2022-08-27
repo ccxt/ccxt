8556e29b7c4667ce9665cc63f234059a62e8d409
T.Todua <7117978+ttodua@users.noreply.github.com>
2022-08-27 13:08:26 +0400

    remove

--- js/base/Exchange.js
+++ js/base/Exchange.js
@@ -797,10 +797,6 @@ module.exports = class Exchange {
     // ------------------------------------------------------------------------
     // METHODS BELOW THIS LINE ARE TRANSPILED FROM JAVASCRIPT TO PYTHON AND PHP
 
-    checkNull (response, defaultValue = undefined) {
-        return (this.isNull (response) ? defaultValue : response);
-    }
-
     safeLedgerEntry (entry, currency = undefined) {
         currency = this.safeCurrency (undefined, currency);
         let direction = this.safeString (entry, 'direction');
