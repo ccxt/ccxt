f7b0f8026be3871dc071cee1170f764678277d34
Dan <dan.krm@outlook.com>
2022-08-26 23:47:04 -0600

    Hitbtc3: editOrder, handleMarginModeAndParams
    ### editOrder:
    ```
    node examples/js/cli hitbtc3 editOrder cd3440f18fea48b29891bac8bcbc6963 BTC/USDT limit buy 0.002 18000 '{"margin":true}'
    
    hitbtc3.editOrder (cd3440f18fea48b29891bac8bcbc6963, BTC/USDT, limit, buy, 0.002, 18000, [object Object])
    2022-08-27T05:46:18.431Z iteration 0 passed in 198 ms
    
    {
      info: {
        id: '965419032797',
        client_order_id: 'zo2tGC4dq_CmX8yGH5ivefSgMe1Pz2zj',
        symbol: 'BTCUSDT',
        side: 'buy',
        status: 'new',
        type: 'limit',
        time_in_force: 'GTC',
        quantity: '0.00200',
        quantity_cumulative: '0',
        price: '18000.00',
        post_only: false,
        reduce_only: false,
        created_at: '2022-08-27T05:44:58.12Z',
        updated_at: '2022-08-27T05:46:19.754Z'
      },
      id: 'zo2tGC4dq_CmX8yGH5ivefSgMe1Pz2zj',
      clientOrderId: 'zo2tGC4dq_CmX8yGH5ivefSgMe1Pz2zj',
      timestamp: 1661579098120,
      datetime: '2022-08-27T05:44:58.120Z',
      lastTradeTimestamp: 1661579179754,
      symbol: 'BTC/USDT',
      price: 18000,
      amount: 0.002,
      type: 'limit',
      side: 'buy',
      timeInForce: 'GTC',
      postOnly: false,
      reduceOnly: false,
      filled: 0,
      remaining: 0.002,
      cost: 0,
      status: 'open',
      average: undefined,
      trades: [],
      fee: undefined,
      fees: []
    }
    2022-08-27T05:46:18.431Z iteration 1 passed in 198 ms
    ```

--- js/hitbtc3.js
+++ js/hitbtc3.js
@@ -1766,12 +1766,17 @@ module.exports = class hitbtc3 extends Exchange {
         if (symbol !== undefined) {
             market = this.market (symbol);
         }
-        const [ marketType, query ] = this.handleMarketTypeAndParams ('editOrder', market, params);
-        const method = this.getSupportedMapping (marketType, {
+        let marketType = undefined;
+        [ marketType, params ] = this.handleMarketTypeAndParams ('editOrder', market, params);
+        let method = this.getSupportedMapping (marketType, {
             'spot': 'privatePatchSpotOrderClientOrderId',
             'swap': 'privatePatchFuturesOrderClientOrderId',
             'margin': 'privatePatchMarginOrderClientOrderId',
         });
+        const [ marginMode, query ] = this.handleMarginModeAndParams ('editOrder', params);
+        if (marginMode !== undefined) {
+            method = 'privatePatchMarginOrderClientOrderId';
+        }
         const response = await this[method] (this.extend (request, query));
         return this.parseOrder (response, market);
     }
