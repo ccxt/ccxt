54c5ebca8d96034c27e838823312383d40f2f396
T.Todua <7117978+ttodua@users.noreply.github.com>
2022-08-29 00:14:03 +0400

    logs

--- js/test/Exchange/test.fetchBalance.js
+++ js/test/Exchange/test.fetchBalance.js
@@ -21,5 +21,7 @@ module.exports = async (exchange) => {
 
     testBalance (exchange, response)
 
+    console.log ('fetched balance:', Object.keys(response).length)
+
     return response
 }
--- js/test/Exchange/test.fetchTransactionFees.js
+++ js/test/Exchange/test.fetchTransactionFees.js
@@ -10,7 +10,17 @@ module.exports = async (exchange) => {
         return
     }
     if (exchange.has[method]) {
-        return await exchange[method] ()
+        const fees = await exchange[method] ()
+        let length = undefined
+        if (Array.isArray (fees['info'])) {
+            length = fees['info'].length
+        } else {
+            try {
+                length = Object.keys (fees['info']).length
+            } catch {}
+        }
+        console.log ('fetched items:', length)
+        return fees
     } else {
         console.log (method + '() is not supported')
     }
