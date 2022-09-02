f959619099505d3569b2dbd1e8388c092377be50
T.Todua <7117978+ttodua@users.noreply.github.com>
2022-09-02 17:04:19 +0400

    fix example

--- examples/js/margin-borrow-buy-sell-repay-example.js
+++ examples/js/margin-borrow-buy-sell-repay-example.js
@@ -15,10 +15,12 @@ async function example () {
     const borrow_coin = 'BUSD'; 
     // how many coins to sell
     const amount_to_trade = 20;
+    // at what limit-price you want to sell (set undefined/null/None if market-order)
+    const limit_price = 20;
     // what is the target margin. This might be obtainable from exchange automatically, using available endpoints, but for example purposes, we set here manually
     const margin_magnitude = 10; 
     // for example purposes, let's also check available balance at first
-    const balance_margin = await e.fetchBalance ({'defaultType': 'margin', 'marginMode': 'isolated'}); // 'isolated' or 'cross'.
+    const balance_margin = await exchange.fetchBalance ({'defaultType': 'margin', 'marginMode': 'isolated'}); // 'isolated' or 'cross'.
     // if we don't have enought coins, then we have to borrow at first
     if (balance_margin[symbol][borrow_coin]['free'] < amount_to_trade) {
         console.log ('hmm, I dont have enough margin balance, I should borrow at first.');
@@ -28,20 +30,20 @@ async function example () {
         if (balance_margin[symbol][collateral_coin]['free'] < needed_collateral_amount) {
             console.log ('hmm, at first I should transfer some collateral from spot');
             // let's check if we have spot balance at all
-            const balance_spot = await e.fetchBalance ({'type': 'spot'});
+            const balance_spot = await exchange.fetchBalance ({'type': 'spot'});
             if (balance_spot[collateral_coin]['free'] < needed_collateral_amount) {
                 console.log ('hmm, I neither do have enough balance on spot');
                 return;
             }  else {
                 console.log ('Transferring some ' + collateral_coin + ' to margin account');
-                await e.transfer (collateral_coin, needed_collateral_amount, 'spot', 'isolated', {'symbol': symbol});
+                await exchange.transfer (collateral_coin, needed_collateral_amount, 'spot', 'isolated', {'symbol': symbol});
             }
         }
         // now, as we have enough margin collateral, initiate borrow
         console.log ('Initiating margin borrow');
-        const borrowResult = await e.borrowMargin ('BUSD', amount_to_trade, 'BUSD/USDT', {'marginMode': 'isolated'});
+        const borrowResult = await exchange.borrowMargin ('BUSD', amount_to_trade, 'BUSD/USDT', {'marginMode': 'isolated'});
     }
-    const order = await e.createOrder (symbol, 'market', 'sell', amount_to_trade, 0.98, {'type': 'margin', 'marginMode': 'isolated'});
+    const order = await exchange.createOrder (symbol, 'market', 'sell', amount_to_trade, limit_price, {'type': 'margin', 'marginMode': 'isolated'});
 }
 
 example();
