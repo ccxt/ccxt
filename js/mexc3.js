f378d2050e93688bf5229159716ebea8a4501831
ndubel <38309641+ndubel@users.noreply.github.com>
2022-09-06 16:21:49 +0300

    mexc3 commonCurrencies

--- js/mexc3.js
+++ js/mexc3.js
@@ -414,10 +414,12 @@ module.exports = class mexc3 extends Exchange {
                 'FLUX1': 'FLUX', // switched places
                 'FLUX': 'FLUX1', // switched places
                 'FREE': 'FreeRossDAO', // conflict with FREE Coin
+                'GMT': 'GMT Token',
                 'HERO': 'Step Hero', // conflict with Metahero
                 'MIMO': 'Mimosa',
                 'PROS': 'Pros.Finance', // conflict with Prosper
                 'SIN': 'Sin City Token',
+                'STEPN': 'GMT',
             },
             'exceptions': {
                 'exact': {
