746535610a18b9bd1b31b67266104d6de23e09b8
T.Todua <7117978+ttodua@users.noreply.github.com>
2022-09-07 21:40:47 +0400

    OHLCVs range test

--- js/test/Exchange/test.fetchOHLCV.js
+++ js/test/Exchange/test.fetchOHLCV.js
@@ -2,6 +2,7 @@
 
 // ----------------------------------------------------------------------------
 
+const assert = require ('assert')
 const testOHLCV = require ('./test.ohlcv.js')
 
 // ----------------------------------------------------------------------------
@@ -28,7 +29,19 @@ module.exports = async (exchange, symbol) => {
         const duration = exchange.parseTimeframe (timeframe)
         const since = exchange.milliseconds () - duration * limit * 1000 - 1000
 
-        const ohlcvs = await exchange[method] (symbol, timeframe, since, limit)
+        const ohlcvs = await exchange[method] (symbol, timeframe, since, limit);
+        ohlcvs[ohlcvs.length-1][0] = ohlcvs[ohlcvs.length-1][0] + duration * 1000;
+        // ensure bars amount is less then limit
+        assert (ohlcvs.length <= limit, "Returned bars amount is more than requested");
+        if (ohlcvs.length > 0) {
+            // ensure that timestamps are greaterOrEqual than since
+            ohlcvs.forEach(ohlcv=>assert (ohlcv[0] >= since, "Returned bar is earlier than requested since"));
+            if (limit !== undefined) {
+                const maxTs = since + duration * 1000 * limit;
+                // ensure last timestamp is under than requested since + limit * duration
+                ohlcvs.forEach(ohlcv=>assert (ohlcv[0] <= maxTs, "Returned bar is greater than requested end boundary"));
+            }
+        }
 
         const now = Date.now ()
 
