e36b4133e9f2277d87a20abbe51e65a986c2d3f1
T. Todua <7117978+ttodua@users.noreply.github.com>
2022-09-13 21:49:11 +0400

    Update Exchange.php

--- php/Exchange.php
+++ php/Exchange.php
@@ -827,7 +827,7 @@ class Exchange {
         if (static::is_associative($params)) {
             foreach ($params as $key => $value) {
                 if (gettype($value) !== 'array') {
-                    $replaceWith = $this->is_null_or_undefined ($value) ? '' : $value;
+                    $replaceWith = ($value === null) ? '' : $value;
                     $string = implode($value, mb_split('{' . preg_quote($key) . '}', $replaceWith));
                 }
             }
