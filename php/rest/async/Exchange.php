76d09dfc6f8fd8ccf47a0002d66732190ba16420
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-09-20 20:21:04 +0100

    update version

--- php/rest/async/Exchange.php
+++ php/rest/async/Exchange.php
@@ -33,11 +33,11 @@ use Exception;
 
 include 'Throttle.php';
 
-$version = '1.91.43';
+$version = '1.93.70';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '1.91.43';
+    const VERSION = '1.93.70';
 
     public static $loop;
     public static $kernel;
