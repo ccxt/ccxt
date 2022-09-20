77d2de9f165e042ce8da4b31d63fadfd7185af94
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-09-20 12:55:14 +0100

    cleaning repo

--- ccxtpro/ccxt.pro.browser.js
+++
@@ -1,6 +0,0 @@
-/*  A entry point for the browser bundle version. This gets compiled by:
-        
-        browserify --debug ./ccxt.pro.browser.js > ./dist/ccxt.pro.browser.js
- */
-
-window.ccxt = require ('./ccxt.pro')
