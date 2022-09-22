9d4065ffe8bcdce7346b5312d6518abefa3f430a
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-09-22 19:06:41 +0100

    fix python import in test

--- build/transpile.js
+++ build/transpile.js
@@ -633,7 +633,7 @@ class Transpiler {
             "import os",
             "import sys",
             "",
-            "root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))",
+            "root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))",
             "sys.path.append(root)",
             "",
             "# ----------------------------------------------------------------------------",
