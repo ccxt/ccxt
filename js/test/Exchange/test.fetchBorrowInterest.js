54cf966a9666ab38e7006433be7c209092974db2
T.Todua <7117978+ttodua@users.noreply.github.com>
2022-09-22 17:38:59 +0400

    updates

--- js/test/Exchange/test.fetchBorrowInterest.js
+++ js/test/Exchange/test.fetchBorrowInterest.js
@@ -23,35 +23,41 @@ module.exports = async (exchange, code, symbol) => {
             'info': {},
         }
 
-        const interest = await exchange[method] (code, symbol)
+        const interestsArray = await exchange[method] (code, symbol)
 
-        console.log (code, method, interest['datetime'], 'symbol: ', symbol,  'interest: ', interest['interest'], 'interestRate: ', interest['interestRate'], 'amountBorrowed: ', interest['amountBorrowed'])
+        // atm, it is returning plural response (array of objects)
+        for (let i = 0; i < interestsArray.length; i++) {
 
-        if (code) {
-            assert (interest['currency'] === code)
-        }
-        assert (interest['account'] === symbol || interest['account'] === undefined)
+            const interest = interestsArray[i]
 
-        const keys = Object.keys (format)
-        for (let i = 0; i < keys.length; i++) {
-            const key = keys[i]
-            assert (key in interest)
+            console.log (code, method, interest['datetime'], 'symbol: ', symbol,  'interest: ', interest['interest'], 'interestRate: ', interest['interestRate'], 'amountBorrowed: ', interest['amountBorrowed'])
+
+            if (code) {
+                assert (interest['currency'] === code)
+            }
+            assert (interest['account'] === symbol || interest['account'] === undefined)
+    
+            const keys = Object.keys (format)
+            for (let i = 0; i < keys.length; i++) {
+                const key = keys[i]
+                assert (key in interest)
+            }
+    
+            if (interest['amountBorrowed'] !== undefined) {
+                assert (interest['amountBorrowed'] >= 0)
+            }
+            if (interest['interestRate'] !== undefined) {
+                assert (interest['interestRate'] > 0)
+            }
+            if (interest['interest'] !== undefined) {
+                assert (interest['interest'] >= 0)
+            }
+            if (interest['timestamp'] !== undefined) {
+                assert (interest['timestamp'] > 1640933203000)
+            }    
         }
 
-        if (interest['amountBorrowed'] !== undefined) {
-            assert (interest['amountBorrowed'] >= 0)
-        }
-        if (interest['interestRate'] !== undefined) {
-            assert (interest['interestRate'] > 0)
-        }
-        if (interest['interest'] !== undefined) {
-            assert (interest['interest'] >= 0)
-        }
-        if (interest['timestamp'] !== undefined) {
-            assert (interest['timestamp'] > 1640933203000)
-        }
-
-        return interest
+        return interestsArray
 
     } else {
         console.log (code, method + '() is not supported')
--- js/test/Exchange/test.fetchBorrowRate.js
+++ js/test/Exchange/test.fetchBorrowRate.js
@@ -13,8 +13,19 @@ module.exports = async (exchange, code) => {
 
     if (exchange.has[method]) {
 
-        const borrowRate = await exchange[method] (code)
-
+        let borrowRate = undefined;
+        try {
+            borrowRate = await exchange[method] (code)
+        } catch (ex) {
+            const message = ex.message;
+            // for exchanges, atm, we don't have the correct lists of currencies, which currency is borrowable and which not. So, because of our predetermined list of test-currencies, some of them might not be borrowable, and thus throws exception. However, we shouldn't break tests for that specific exceptions, and skip those occasions.
+            if (message.indexOf ('could not find the borrow rate for currency code') < 0) {
+                throw new Error (message);
+            }
+            // console.log (method + '() : ' + code + ' is not borrowable for this exchange. Skipping the test method.');
+            return;
+        }
+        
         testBorrowRate (exchange, borrowRate, method, code)
 
         console.log (code, method, borrowRate['datetime'], 'rate:', borrowRate['rate'], 'period:', borrowRate['period'])
