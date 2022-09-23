bd7a806cb9b5f58f889104cd95c52e18f36c4e9d
T.Todua <7117978+ttodua@users.noreply.github.com>
2022-09-23 13:50:14 +0400

    coinsbit - revival

---
+++ js/coinsbitin.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// ---------------------------------------------------------------------------
+
+const coinsbit = require ('./coinsbit.js');
+
+module.exports = class coinsbitin extends coinsbit {
+    describe () {
+        return this.deepExtend (super.describe (), {
+            'id': 'coinsbitin',
+            'name': 'Coinsbit India',
+            'countries': ['IN'],
+            'hostname': 'coinsbit.in',
+            'urls': {
+                'logo': '<<<TODO>>>',
+            },
+        });
+    }
+};
