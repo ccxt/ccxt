00ef07ff8cf475a7914d132f777c168f89c819ad
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-09-24 11:40:19 +0100

    Revert "update file content"
    This reverts commit fa82e1b36412d210c42cc8b3d6cb8d3fa642a2d1.

--- php/rest/async/binanceusdm.php
+++ php/rest/async/binanceusdm.php
@@ -41,16 +41,12 @@ class binanceusdm extends binance {
     }
 
     public function transfer_in($code, $amount, $params = array ()) {
-        return Async\async(function () use ($code, $amount, $params) {
-            // transfer from spot wallet to usdm futures wallet
-            return Async\await($this->futuresTransfer ($code, $amount, 1, $params));
-        }) ();
+        // transfer from spot wallet to usdm futures wallet
+        return yield $this->futuresTransfer ($code, $amount, 1, $params);
     }
 
     public function transfer_out($code, $amount, $params = array ()) {
-        return Async\async(function () use ($code, $amount, $params) {
-            // transfer from usdm futures wallet to spot wallet
-            return Async\await($this->futuresTransfer ($code, $amount, 2, $params));
-        }) ();
+        // transfer from usdm futures wallet to spot wallet
+        return yield $this->futuresTransfer ($code, $amount, 2, $params);
     }
 }
