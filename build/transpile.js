ab338104eb940f1f29930ea81caf5e8b6d71587f
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-09-25 14:07:54 -0700

    fix transpile

--- build/transpile.js
+++ build/transpile.js
@@ -634,7 +634,7 @@ class Transpiler {
             "import os",
             "import sys",
             "",
-            "root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))",
+            "root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))",
             "sys.path.append(root)",
             "",
             "# ----------------------------------------------------------------------------",
