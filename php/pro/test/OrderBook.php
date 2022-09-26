8b6d5ff60c69ab5ece865fec907ba0e708cb2253
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-09-26 09:48:17 +0100

    update php namespace

--- php/pro/test/OrderBook.php
+++ php/pro/test/OrderBook.php
@@ -1,5 +1,5 @@
 <?php
-namespace ccxtpro;
+namespace ccxt\pro;
 include_once __DIR__ . '/../../../vendor/autoload.php';
 // ----------------------------------------------------------------------------
 
