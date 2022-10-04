5d67f72975e834e6bb706f6a7885d0cb6db7fe6d
AlgoQ <kobejanssens@outlook.com>
2022-10-04 16:23:16 +0200

    Add/update method description

--- js/mexc3.js
+++ js/mexc3.js
@@ -3645,7 +3645,7 @@ module.exports = class mexc3 extends Exchange {
          * @method
          * @name mexc3#fetchDeposits
          * @description fetch all deposits made to an account
-         * @param {string|undefined} code unified currency code
+         * @param {string} code unified currency code
          * @param {int|undefined} since the earliest time in ms to fetch deposits for
          * @param {int|undefined} limit the maximum number of deposits structures to retrieve
          * @param {object} params extra parameters specific to the mexc3 api endpoint
@@ -3696,6 +3696,16 @@ module.exports = class mexc3 extends Exchange {
     }
 
     async fetchWithdrawals (code, since = undefined, limit = undefined, params = {}) {
+        /**
+         * @method
+         * @name mexc3#fetchWithdrawals
+         * @description fetch all withdrawals made from an account
+         * @param {string} code unified currency code
+         * @param {int|undefined} since the earliest time in ms to fetch withdrawals for
+         * @param {int|undefined} limit the maximum number of withdrawals structures to retrieve
+         * @param {object} params extra parameters specific to the mexc3 api endpoint
+         * @returns {[object]} a list of [transaction structures]{@link https://docs.ccxt.com/en/latest/manual.html#transaction-structure}
+         */
         await this.loadMarkets ();
         const request = {
             // 'coin': currency['id'],
