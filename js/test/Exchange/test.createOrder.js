05907d97755023722adbb1791eed13c43fb626d4
T.Todua <7117978+ttodua@users.noreply.github.com>
2022-10-10 23:57:04 +0400

    createorder tests rework

--- js/test/Exchange/test.createOrder.js
+++ js/test/Exchange/test.createOrder.js
@@ -8,10 +8,16 @@ const testOrder = require ('./test.order.js');
 const Precise = require ('../../base/Precise');
 // ----------------------------------------------------------------------------
 
-// temporary variables/functions for js version
-const warningPrefix = ' ! ';
+// before this will become transpiled, temporarily use some variables & verbose logs for successfull JS debugging
+const testName = 'testCreateOrder';
+const warningPrefix = ' !!! ' + testName;
+const debugPrefix = ' >>> ' + testName;
 const isVerbose = true // process.argv.includes ('--verbose');
-
+function verboseOutput (exchange, symbol, ...args) {
+    if (isVerbose) {
+        console.log (debugPrefix + ' [' + exchange['id'] + ' : ' + symbol + '] ', ...args);
+    }
+}
 // ----------------------------------------------------------------------------
 
 async function testCreateOrder(exchange, symbol) {
@@ -34,7 +40,7 @@ async function testCreateOrder(exchange, symbol) {
     }
     // ensure it has cancel (any) method, otherwise we should refrain from automatic test.
     if (!exchange.has['cancelOrder'] && !exchange.has['cancelOrders'] && !exchange.has['cancelAllOrders']) {
-        assert (true, warningPrefix + ' ' +  exchange.id + ' does not have cancelOrder|cancelOrders|canelAllOrders method, which is needed to make tests for `createOrder` method. Skipping the test...');
+        assert (false, warningPrefix + ' ' +  exchange.id + ' does not have cancelOrder|cancelOrders|canelAllOrders method, which is needed to make tests for `createOrder` method. Skipping the test...');
         return;
     }
 
@@ -43,26 +49,24 @@ async function testCreateOrder(exchange, symbol) {
 
     // we need fetchBalance method to test out orders correctly
     if (!exchange.has['fetchBalance']) {
-        assert (true, warningPrefix + ' ' +  exchange.id + ' does not have fetchBalance() method, which is needed to make tests for `createOrder` method. Skipping the test...');
+        assert (false, warningPrefix + ' ' +  exchange.id + ' does not have fetchBalance() method, which is needed to make tests for `createOrder` method. Skipping the test...');
     }
     // ensure there is enough balance of 'quote' asset, because at first we need to 'buy' the base asset
     const balance = await exchange.fetchBalance ();
-    if (balance[market['quote']]['free'] === undefined) {
-        assert (true, warningPrefix + ' ' +  exchange.id + ' does not have enough balance of' + market['quote'] + ' in fetchBalance() which is required to test ' + method);
+    const initialBaseBalance = balance[market['base']]['free'];
+    const initialQuoteBalance = balance[market['quote']]['free'];
+    if (initialQuoteBalance === undefined) {
+        assert (false, warningPrefix + ' ' +  exchange.id + ' does not have enough balance of' + market['quote'] + ' in fetchBalance() which is required to test ' + method);
     }
-    if (isVerbose) {
-        console.log ('fetched balance: ' + balance[market['quote']]['free']);
-    }
+    verboseOutput (exchange, symbol, 'fetched balance for', symbol,':', initialBaseBalance, market['base'], '/', initialQuoteBalance, market['quote']);
     // get best bid & ask
     const [bestBid, bestAsk] = await testCreateOrder_getBestBidAsk (exchange, symbol);
     // get minimum order amount & cost
     let [minimumAmountForBuy, minimumCostForBuy ] = getMinimumMarketCostAndAmountForBuy (exchange, market, bestAsk);
     if (minimumAmountForBuy === undefined || minimumCostForBuy === undefined) {
-        assert (true, warningPrefix + ' ' +  exchange.id + ' can not determine minimum amount/cost of order for ' + symbol + ' market');
+        assert (false, warningPrefix + ' ' +  exchange.id + ' can not determine minimum amount/cost of order for ' + symbol + ' market');
     }
-    if (isVerbose) {
-        console.log ('minimum amount should be: ' + minimumAmountForBuy + ', minimum cost should be: ' + minimumCostForBuy);
-    }
+    verboseOutput (exchange, symbol, 'minimum amount should be:', minimumAmountForBuy, ', minimum cost should be:', minimumCostForBuy);
 
     // ************************************ //
     // *********** [Scenario 1] *********** //
@@ -79,8 +83,9 @@ async function testCreateOrder(exchange, symbol) {
         assert (!isClosedFetched, warningPrefix + ' ' +  exchange.id + ' ' + symbol + ' order should not be filled, but it is. ' + JSON.stringify (buyOrder_nonFillable_fetched));
         // cancel the order
         await testCreateOrder_cancelOrder (exchange, symbol, buyOrder_nonFillable['id']);
+        verboseOutput (exchange, symbol, 'SCENARIO 1 PASSED !!!');
     } catch (e) {
-        assert (true, warningPrefix + ' ' +  exchange.id + ' ' + symbol + ' ' + method + ' failed for Scenario 1: ' + e.message);
+        assert (false, warningPrefix + ' ' +  exchange.id + ' ' + symbol + ' ' + method + ' failed for Scenario 1: ' + e.message);
     }
     // *********** [Scenario 1 - END ] *********** //
 
@@ -97,23 +102,23 @@ async function testCreateOrder(exchange, symbol) {
         const buyOrder_filled_fetched = await testCreateOrder_fetchOrder (exchange, symbol, buyOrder_fillable['id']);
         // we need to find out the amount of base asset that was bought
         let amountToSell = undefined;
-        if (buyOrder_filled_fetched['filled'] === undefined) {
+        if (buyOrder_filled_fetched['filled'] !== undefined) {
             amountToSell = buyOrder_filled_fetched['filled'];
         } else {
             // if it was not reported, then the last step is to re-fetch balance and sell all target tokens (don't worry, it will not sell much, as the test reached here, it means that even the minimum sell amount execution had failed because of insufficient coins, so, all this will sell is whatever small amount exists in wallet)
             const balance = await exchange.fetchBalance ();
-            amountToSell = balance[market['base']]['free'];
+            // subtract the initial balance from the current balance to find out the amount of base asset that was bought
+            amountToSell = balance[market['base']]['free'] - ((initialBaseBalance !== undefined) ? initialBaseBalance : 0);
         }
         const sellOrder = await testCreateOrder_submitSafeOrder (exchange, symbol, 'market', 'sell', amountToSell, undefined, {'reduceOnly': true}); // we use 'reduceOnly' to ensure we don't open a margin-ed position accidentally
         const sellOrder_fetched = await testCreateOrder_fetchOrder (exchange, symbol, sellOrder['id']);
         // try to test that order was fully filled
-        const isClosed = testCreateOrder_orderIs(exchange, sellOrder_fetched, 'closed');
         const isClosedFetched = testCreateOrder_orderIs(exchange, sellOrder_fetched, 'closed');
-        const isOpen = testCreateOrder_orderIs(exchange, sellOrder_fetched, 'open');
         const isOpenFetched = testCreateOrder_orderIs(exchange, sellOrder_fetched, 'open');
-        assert (isClosed || isClosedFetched || (isOpen === undefined && isOpenFetched === undefined), warningPrefix + ' ' +  exchange.id + ' ' + symbol + ' order should be filled, but it is not. ' + exchange.json (sellOrder_fetched));
+        assert (isClosedFetched || isOpenFetched === undefined, warningPrefix + ' ' +  exchange.id + ' ' + symbol + ' order should be filled, but it is not. ' + exchange.json (sellOrder_fetched));
+        verboseOutput (exchange, symbol, 'SCENARIO 2 PASSED !!!');
     } catch (e) {
-        assert (true, warningPrefix + ' ' +  exchange.id + ' ' + symbol + ' ' + method + ' failed for Scenario 2: ' + e.message);
+        assert (false, warningPrefix + ' ' +  exchange.id + ' ' + symbol + ' ' + method + ' failed for Scenario 2: ' + e.message);
     }
     // *********** [Scenario 2 - END ] *********** //
 
@@ -159,13 +164,11 @@ async function testCreateOrder_getBestBidAsk (exchange, symbol) {
     }
     //
     if (bestBid === undefined || bestAsk === undefined) {
-        assert (true, warningPrefix + ' ' +  exchange.id + ' could not get best bid/ask, skipping ' + method + ' test...');
+        assert (false, warningPrefix + ' ' +  exchange.id + ' could not get best bid/ask, skipping ' + method + ' test...');
     }
     // ensure values are correct
     assert ( (bestBid !== undefined) && (bestAsk !== undefined) && (bestBid > 0) || (bestAsk > 0) && (bestBid >= bestAsk), warningPrefix + ' ' +  exchange.id + ' best bid/ask seem incorrect. Bid:' + bestBid + ' Ask:' + bestAsk);
-    if (isVerbose) {
-        console.log ('fetched best bid/ask using ' + usedMethod + '; Bid:' + bestBid + ' Ask:' + bestAsk);
-    }
+    verboseOutput (exchange, symbol, 'fetched best bid/ask using', usedMethod, '- Bid:', bestBid, ' Ask:', bestAsk);
     return [ bestBid, bestAsk ];
 }
 
@@ -214,13 +217,10 @@ async function testCreateOrder_fetchOrder (exchange, symbol, orderId) {
     }
     // test fetched order object
     if (fetchedOrder !== undefined) {
-        testOrder (exchange, order, symbol, Date.now ());
+        testOrder (exchange, fetchedOrder, symbol, Date.now ());
     }
     if (usedMethod !== undefined) {
-        if (isVerbose) {
-            const jsoned = JSON.stringify (fetchedOrder);
-            console.log ('fetched order with ' + usedMethod + ' | ' + jsoned);
-        }
+        verboseOutput (exchange, symbol, 'fetched order using', usedMethod);
     }
     return fetchedOrder;
 }
@@ -228,7 +228,7 @@ async function testCreateOrder_fetchOrder (exchange, symbol, orderId) {
 // ----------------------------------------------------------------------------
 
 async function testCreateOrder_cancelOrder (exchange, symbol, orderId = undefined) {
-    // cancel the order (one of the below methods is guaranteed to be existent, as this was checked in the top of this test)
+    // cancel the order (one of the below methods is guaranteed to be existent, as this was checked in the start of this test)
     let usedMethod = undefined;
     let cancelResult = undefined;
     if (exchange.has['cancelOrder'] && orderId !== undefined) {
@@ -245,9 +245,7 @@ async function testCreateOrder_cancelOrder (exchange, symbol, orderId = undefine
             cancelResult = await exchange.cancelOrders ([orderId], symbol);
         }
     }
-    if (isVerbose) {
-        console.log ('canceled order using ' + usedMethod + ' : ' + exchange.json (cancelResult));
-    }
+    verboseOutput (exchange, symbol, 'canceled order using', usedMethod, ' : ', exchange.json (cancelResult));
 }
 
 // ----------------------------------------------------------------------------
@@ -271,9 +269,7 @@ function testCreateOrder_orderIs (exchange, order, statusSlug) {
 // ----------------------------------------------------------------------------
 
 async function testCreateOrder_submitSafeOrder (exchange, symbol, orderType, side, amount, price = undefined, params = {}) {
-    if (isVerbose) {
-        console.log ('Executing createOrder', symbol, orderType, side, amount, price, params);
-    }
+    verboseOutput (exchange, symbol, 'Executing createOrder', symbol, orderType, side, amount, price, params);
     let order = await exchange.createOrder (symbol, orderType, side, amount, price, params);
     try {
         // test through regular order object test
@@ -285,85 +281,62 @@ async function testCreateOrder_submitSafeOrder (exchange, symbol, orderType, sid
             testCreateOrder_tryCancelOrder (exchange, symbol, order);
         }
         // now, we can throw the initial error
-        assert (true, warningPrefix + ' ' +  exchange.id + ' failed to createOrder: ' + e.message);
+        assert (false, warningPrefix + ' ' +  exchange.id + ' failed to createOrder: ' + e.message);
     }
     return order;
 }
 
 function getMinimumMarketCostAndAmountForBuy (exchange, market, askPrice = undefined) {
     // pre-define some coefficients, which will be used down below
-    const orderCostMultiplier = 1.01;
-    const orderAmountMultiplier = 1.01;
+    const orderCostSafetyMultiplier = 1.01;
+    const orderAmountSafetyMultiplier = 1.01;
+    const orderPriceSafetyMultiplier = 1.2;
     // define how much to spend (it's enough to be around minimal required cost)
-    let minimumCostForBuy = undefined;
+    let minimumCostLimitForBuy = undefined;
+    let minimumAmountLimitForBuy = undefined;
     if (market['limits']['cost']['min']) {
-        minimumCostForBuy = market['limits']['cost']['min'];
-    } else if (market['limits']['amount']['min']) {
-        // as we know the minimal amount, we can calculate the approximate cost for purchase
-        minimumCostForBuy = market['limits']['amount']['min'] * askPrice;
-    } else {
-        // else, we take approximately 10 USD which seems to be the common borderline of "minimim cost of order" across various exchanges. For sure, the below numbers might need to change in a few years (it's not a problem) and if there is any exchange, that doesn't support any of the below currencies, we should add it manually here
-        const minimumOrderCosts = {
-            'USD': 10,
-            'USDT': 10,
-            'BUSD': 10,
-            'JPY': 1500,
-            'KRW': 20000,
-            'EUR': 10,
-            // define minimum amount for common cryptos (i.e. for sell orders, where we need to input amount, or even for buy orders where coin is the `base`)
-            'BTC': 0.001, // unless Bitcoin price falls below 10000$, this value is ok.
-            'ETH': 0.02, // unless Ethereum price falls below 500$, this value is ok.
-        };
-        const keys = Object.keys (minimumOrderCosts);
-        for (let i = 0; i < keys.length; i++) {
-            const currency = keys[i];
-            if (market['quote'] === currency) {
-                minimumCostForBuy = minimumOrderCosts[currency];
-            }
-        }
+        minimumCostLimitForBuy = market['limits']['cost']['min'];
     }
-    minimumCostForBuy = minimumCostForBuy * orderCostMultiplier; // add a tiny more amount than minimin required
-
-    // ensure the cost/amount is above minimum limits
-    let minimumAmountForBuy = undefined;
     if (market['limits']['amount']['min']) {
-        minimumAmountForBuy = market['limits']['amount']['min'] * orderAmountMultiplier; // add a small safety distance
-    } else {
-        minimumAmountForBuy = minimumCostForBuy / limitBuyPrice_nonFillable;
+        // as we know the minimal amount, we can calculate the approximate cost for purchase
+        minimumAmountLimitForBuy = market['limits']['amount']['min'];
     }
-    // intentionally add a tiny increment to the minimum amount/cost, to ensure that it will not cause precision issues (thus we ensure that implementation handles them)
-    if (minimumAmountForBuy) {
-        minimumAmountForBuy = minimumAmountForBuy + 0.000000000000000001;
+
+    let finalAmountForBuy = minimumAmountLimitForBuy;
+    let finalCostForBuy = minimumCostLimitForBuy;
+    // Some exchanges accept the "minimum amount" (independent of the overal cost), but other exchanges also need that order was also above "minimum cost". So, we choose the maximum of both values
+    if (finalCostForBuy !== undefined && finalAmountForBuy !== undefined) {
+        let approxAmountForCost = finalCostForBuy / (askPrice * orderPriceSafetyMultiplier);
+        finalAmountForBuy = Math.max (finalAmountForBuy, approxAmountForCost);
     }
-    if (minimumCostForBuy) {
-        minimumCostForBuy = minimumCostForBuy + 0.000000000000000001;
+
+    // Intentionally add a tiny increment to the minimum amount/cost, to test & ensure that it will not cause precision issues (thus we ensure that implementation handles them)
+    if (finalAmountForBuy) {
+        finalAmountForBuy = finalAmountForBuy + 0.000000000000000001;
     }
-    return [ minimumAmountForBuy, minimumCostForBuy ];
+    if (finalCostForBuy) {
+        finalCostForBuy = finalCostForBuy + 0.000000000000000001;
+    }
+    return [ finalAmountForBuy, finalCostForBuy ];
 }
 
 async function testCreateOrder_tryCancelOrder (exchange, symbol, order) {
     // fetch order for maximum accuracy
-    const order_fetched = await testCreateOrder_fetchOrder (exchange, symbol, order['id']);
+    const orderFetched = await testCreateOrder_fetchOrder (exchange, symbol, order['id']);
     // check their status
-    const isClosed = testCreateOrder_orderIs (exchange, order, 'closed');
-    const isClosedFetched = testCreateOrder_orderIs (exchange, order_fetched, 'closed');
-    const isOpen = testCreateOrder_orderIs (exchange, order, 'open');
-    const isOpenFetched = testCreateOrder_orderIs (exchange, order_fetched, 'open');
+    const isClosedFetched = testCreateOrder_orderIs (exchange, orderFetched, 'closed');
+    const isOpenFetched = testCreateOrder_orderIs (exchange, orderFetched, 'open');
     // if it was not reported as closed/filled, then try to cancel it
-    if ((isClosed === undefined && isClosedFetched === undefined) || (isOpen || isOpenFetched)) {
-        if (isVerbose) {
-            console.log ('trying to cancel the remaining amount of partially filled order...');
-        }
+    if (isClosedFetched === undefined || isOpenFetched) {
+        verboseOutput (exchange, symbol, 'trying to cancel the remaining amount of partially filled order...');
         try {
             await testCreateOrder_cancelOrder (exchange, symbol, order['id']);
         } catch (e) {
             // we don't throw exception here, because order might have been closed/filled already, before 'cancelOrder' call reaches server, so it is tolerable
-            console.log (warningPrefix + ' ' +  exchange['id'] + ' ' + symbol + ' order ' + order['id'] + ' a moment ago, order was reported as open, but could not be cancelled at this moment: ' + exchange.json (order) + '. Exception message: ' + e.message);
+            console.log (warningPrefix + ' ' +  exchange['id'] + ' ' + symbol + ' order ' + order['id'] + ' a moment ago order was reported as pending, but could not be cancelled at this moment: ' + exchange.json (order) + '. Exception message: ' + e.message);
         }
     } else {
-        if (isVerbose) {
-            console.log ('order is already closed/filled, no need to cancel it');
-        }
+        verboseOutput (exchange, symbol, 'order is already closed/filled, no need to cancel it');
     }
 }
 
