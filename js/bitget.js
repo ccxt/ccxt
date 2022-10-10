602196ac257e232da22fc70c67ddcfe7586cf52a
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-10-10 10:31:42 +0100

    fix withdraw

--- js/bitget.js
+++ js/bitget.js
@@ -742,6 +742,7 @@ module.exports = class bitget extends Exchange {
                     'invalid end time': BadRequest, // end time is a date 30 days ago; or end time is a date in the future
                     '20003': ExchangeError, // operation failed, {"status":"error","ts":1595730308979,"err_code":"bad-request","err_msg":"20003"}
                     '01001': ExchangeError, // order failed, {"status":"fail","err_code":"01001","err_msg":"系统异常，请稍后重试"}
+                    '43111': PermissionDenied, // {"code":"43111","msg":"参数错误 address not in address book","requestTime":1665394201164,"data":null}
                 },
                 'broad': {
                     'invalid size, valid range': ExchangeError,
@@ -1199,7 +1200,7 @@ module.exports = class bitget extends Exchange {
         await this.loadMarkets ();
         const currency = this.currency (code);
         const request = {
-            'coin': currency['id'],
+            'coin': currency['code'],
             'address': address,
             'chain': chain,
             'amount': amount,
