fa56635db3927e1db746fb3184f13d277313e517
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-10-12 09:59:26 +0100

    add default error

--- js/base/errors.js
+++ js/base/errors.js
@@ -1,5 +1,6 @@
-import { errorHierarchy } from './errorHierarchy.js'
+/* eslint-disable max-classes-per-file */
 
+import { errorHierarchy } from './errorHierarchy.js';
 // Commented out since I'm not sure this is mandatory anymore
 // and does not work out of the box with esm
 
@@ -45,197 +46,197 @@ import { errorHierarchy } from './errorHierarchy.js'
 
 //     return namespace
 // }
-
 class BaseError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = "BaseError";
+    constructor (message) {
+        super (message);
+        this.name = 'BaseError';
     }
 }
 // Exchange Error errors
 class ExchangeError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = "ExchangeError";
+    constructor (message) {
+        super (message);
+        this.name = 'ExchangeError';
     }
 }
 class AuthenticationError extends ExchangeError {
-    constructor(message) {
-        super(message);
-        this.name = "AuthenticationError";
+    constructor (message) {
+        super (message);
+        this.name = 'AuthenticationError';
     }
 }
 class PermissionDenied extends ExchangeError {
-    constructor(message) {
-        super(message);
-        this.name = "PermissionDenied";
+    constructor (message) {
+        super (message);
+        this.name = 'PermissionDenied';
     }
 }
 class AccountNotEnabled extends ExchangeError {
-    constructor(message) {
-        super(message);
-        this.name = "AccountNotEnabled";
+    constructor (message) {
+        super (message);
+        this.name = 'AccountNotEnabled';
     }
 }
 class AccountSuspended extends ExchangeError {
-    constructor(message) {
-        super(message);
-        this.name = "AccountSuspended";
+    constructor (message) {
+        super (message);
+        this.name = 'AccountSuspended';
     }
 }
 class ArgumentsRequired extends ExchangeError {
-    constructor(message) {
-        super(message);
-        this.name = "ArgumentsRequired";
+    constructor (message) {
+        super (message);
+        this.name = 'ArgumentsRequired';
     }
 }
 class BadRequest extends ExchangeError {
-    constructor(message) {
-        super(message);
-        this.name = "BadRequest";
+    constructor (message) {
+        super (message);
+        this.name = 'BadRequest';
     }
 }
 class BadSymbol extends BadRequest {
-    constructor(message) {
-        super(message);
-        this.name = "BadSymbol";
+    constructor (message) {
+        super (message);
+        this.name = 'BadSymbol';
     }
 }
 class MarginModeAlreadySet extends BadRequest {
-    constructor(message) {
-        super(message);
-        this.name = "MarginModeAlreadySet";
+    constructor (message) {
+        super (message);
+        this.name = 'MarginModeAlreadySet';
     }
 }
 class BadResponse extends ExchangeError {
-    constructor(message) {
-        super(message);
-        this.name = "BadResponse";
+    constructor (message) {
+        super (message);
+        this.name = 'BadResponse';
     }
 }
 class NullResponse extends ExchangeError {
-    constructor(message) {
-        super(message);
-        this.name = "NullResponse";
+    constructor (message) {
+        super (message);
+        this.name = 'NullResponse';
     }
 }
 class InsufficientFunds extends ExchangeError {
-    constructor(message) {
-        super(message);
-        this.name = "InsufficientFunds";
+    constructor (message) {
+        super (message);
+        this.name = 'InsufficientFunds';
     }
 }
 class InvalidAddress extends ExchangeError {
-    constructor(message) {
-        super(message);
-        this.name = "InvalidAddress";
+    constructor (message) {
+        super (message);
+        this.name = 'InvalidAddress';
     }
 }
 class AddressPending extends InvalidAddress {
-    constructor(message) {
-        super(message);
-        this.name = "AddressPending";
+    constructor (message) {
+        super (message);
+        this.name = 'AddressPending';
     }
 }
 class InvalidOrder extends ExchangeError {
-    constructor(message) {
-        super(message);
-        this.name = "InvalidOrder";
+    constructor (message) {
+        super (message);
+        this.name = 'InvalidOrder';
     }
 }
 class OrderNotFound extends InvalidOrder {
-    constructor(message) {
-        super(message);
-        this.name = "OrderNotFound";
+    constructor (message) {
+        super (message);
+        this.name = 'OrderNotFound';
     }
 }
 class OrderNotCached extends InvalidOrder {
-    constructor(message) {
-        super(message);
-        this.name = "OrderNotCached";
+    constructor (message) {
+        super (message);
+        this.name = 'OrderNotCached';
     }
 }
 class CancelPending extends InvalidOrder {
-    constructor(message) {
-        super(message);
-        this.name = "CancelPending";
+    constructor (message) {
+        super (message);
+        this.name = 'CancelPending';
     }
 }
 class OrderImmediatelyFillable extends InvalidOrder {
-    constructor(message) {
-        super(message);
-        this.name = "OrderImmediatelyFillable";
+    constructor (message) {
+        super (message);
+        this.name = 'OrderImmediatelyFillable';
     }
 }
 class OrderNotFillable extends InvalidOrder {
-    constructor(message) {
-        super(message);
-        this.name = "OrderNotFillable";
+    constructor (message) {
+        super (message);
+        this.name = 'OrderNotFillable';
     }
 }
 class DuplicateOrderId extends InvalidOrder {
-    constructor(message) {
-        super(message);
-        this.name = "DuplicateOrderId";
+    constructor (message) {
+        super (message);
+        this.name = 'DuplicateOrderId';
     }
 }
 class NotSupported extends ExchangeError {
-    constructor(message) {
-        super(message);
-        this.name = "NotSupported";
+    constructor (message) {
+        super (message);
+        this.name = 'NotSupported';
     }
 }
 // Network error
 class NetworkError extends BaseError {
-    constructor(message) {
-        super(message);
-        this.name = "NetworkError";
+    constructor (message) {
+        super (message);
+        this.name = 'NetworkError';
     }
 }
 class DDoSProtection extends NetworkError {
-    constructor(message) {
-        super(message);
-        this.name = "DDoSProtection";
+    constructor (message) {
+        super (message);
+        this.name = 'DDoSProtection';
     }
 }
 class RateLimitExceeded extends DDoSProtection {
-    constructor(message) {
-        super(message);
-        this.name = "RateLimitExceeded";
+    constructor (message) {
+        super (message);
+        this.name = 'RateLimitExceeded';
     }
 }
 class ExchangeNotAvailable extends NetworkError {
-    constructor(message) {
-        super(message);
-        this.name = "ExchangeNotAvailable";
+    constructor (message) {
+        super (message);
+        this.name = 'ExchangeNotAvailable';
     }
 }
 class OnMaintenance extends ExchangeNotAvailable {
-    constructor(message) {
-        super(message);
-        this.name = "OnMaintenance";
+    constructor (message) {
+        super (message);
+        this.name = 'OnMaintenance';
     }
 }
 class InvalidNonce extends NetworkError {
-    constructor(message) {
-        super(message);
-        this.name = "InvalidNonce";
+    constructor (message) {
+        super (message);
+        this.name = 'InvalidNonce';
     }
 }
 class RequestTimeout extends NetworkError {
-    constructor(message) {
-        super(message);
-        this.name = "RequestTimeout";
+    constructor (message) {
+        super (message);
+        this.name = 'RequestTimeout';
     }
 }
-
 /*  ------------------------------------------------------------------------ */
-
 // export default subclass (
 //     // Root class
 //     Error,
 //     // Derived class hierarchy
 //     errorHierarchy
 // )
+const errors = { BaseError, ExchangeError, PermissionDenied, AccountNotEnabled, AccountSuspended, ArgumentsRequired, BadRequest, BadSymbol, MarginModeAlreadySet, BadResponse, NullResponse, InsufficientFunds, InvalidAddress, InvalidOrder, OrderNotFound, OrderNotCached, CancelPending, OrderImmediatelyFillable, OrderNotFillable, DuplicateOrderId, NotSupported, NetworkError, DDoSProtection, RateLimitExceeded, ExchangeNotAvailable, OnMaintenance, InvalidNonce, RequestTimeout, AuthenticationError, AddressPending };
 
 export { BaseError, ExchangeError, PermissionDenied, AccountNotEnabled, AccountSuspended, ArgumentsRequired, BadRequest, BadSymbol, MarginModeAlreadySet, BadResponse, NullResponse, InsufficientFunds, InvalidAddress, InvalidOrder, OrderNotFound, OrderNotCached, CancelPending, OrderImmediatelyFillable, OrderNotFillable, DuplicateOrderId, NotSupported, NetworkError, DDoSProtection, RateLimitExceeded, ExchangeNotAvailable, OnMaintenance, InvalidNonce, RequestTimeout, AuthenticationError, AddressPending };
+
+export default errors;
