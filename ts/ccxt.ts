8636a281cf06ddb472992f889b4adc8fac9d6bea
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-10-18 17:10:46 +0100

    ccxt.ts update

--- ts/ccxt.ts
+++ ts/ccxt.ts
@@ -33,7 +33,7 @@ import { Exchange }  from './src/base/Exchange.js'
 import { Precise }   from './src/base/Precise.js'
 import * as functions from './src/base/functions.js'
 import * as errors   from './src/base/errors.js'
-import wsExchange from './src/pro/base/Exchange.js'
+import WsConnector from './src/pro/base/WsConnector.js'
 
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
@@ -412,6 +412,7 @@ export {
     Precise,
     functions,
     errors,
+    WsConnector,
     aax,
     alpaca,
     ascendex,
