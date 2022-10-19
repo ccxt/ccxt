52b24dc0d7d7a0305c5b64ab484ba82822862a57
Sam Germain <sam.germain@usask.ca>
2022-10-18 20:24:27 -0600

    bibox.fetchTickers docstring updated

--- js/bibox.js
+++ js/bibox.js
@@ -559,7 +559,7 @@ module.exports = class bibox extends Exchange {
         /**
          * @method
          * @name bibox#fetchTickers
-         * @description fetches price tickers for multiple markets, statistical calculations with the information calculated over the past 24 hours each market
+         * @description v1, fetches price tickers for multiple markets, statistical calculations with the information calculated over the past 24 hours each market
          * @param {[string]|undefined} symbols unified symbols of the markets to fetch the ticker for, all market tickers are returned if not assigned
          * @param {object} params extra parameters specific to the bibox api endpoint
          * @returns {object} an array of [ticker structures]{@link https://docs.ccxt.com/en/latest/manual.html#ticker-structure}
