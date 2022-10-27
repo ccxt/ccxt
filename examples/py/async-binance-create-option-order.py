7fe4c4ad0b43e552229564b9a774caf2c89dfbf2
Dan <dan.krm@outlook.com>
2022-10-26 22:17:37 -0600

    Binance: create option order example
    {
        'mmp': False,
        'orderId': '4612100534317743431',
        'postOnly': False,
        'price': '2.1',
        'quantity': '1',
        'reduceOnly': False,
        'side': 'BUY',
        'symbol': 'ETH-221028-1700-C',
        'type': 'LIMIT'
    }

---
+++ examples/py/async-binance-create-option-order.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import asyncio
+import os
+import sys
+from pprint import pprint
+
+root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root + '/python')
+
+import ccxt.async_support as ccxt  # noqa: E402
+
+
+async def main():
+    exchange = ccxt.binance({
+        'apiKey': 'YOUR_API_KEY',
+        'secret': 'YOUR_SECRET',
+        # 'verbose': True,  # for debug output
+    })
+    await exchange.load_markets()
+    try:
+        response = await exchange.eapiPrivatePostOrder({
+            # ETH/USDT call option strike 1700 USDT expiry on 2022-10-28
+            'symbol': 'ETH-221028-1700-C',
+            'side': 'BUY',
+            'type': 'LIMIT',
+            'quantity': 1,
+            'price': 2.1,
+        })
+        pprint(response)
+    except ccxt.InsufficientFunds as e:
+        print('eapiPrivatePostOrder() failed - not enough funds')
+        print(e)
+    except Exception as e:
+        print('eapiPrivatePostOrder() failed')
+        print(e)
+    await exchange.close()
+
+
+asyncio.run(main())
