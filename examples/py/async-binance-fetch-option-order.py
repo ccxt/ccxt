4d196f4f897ac6d2c590803d491192e340ec475e
Dan <dan.krm@outlook.com>
2022-10-26 22:59:16 -0600

    Binance: fetch option order example
    [
        {
            'avgPrice': '0.00000000',
            'clientOrderId': '',
            'createTime': '1666826857206',
            'executedQty': '0.00000000',
            'fee': '0.00000000',
            'mmp': False,
            'optionSide': 'CALL',
            'orderId': '4612098335294532880',
            'postOnly': False,
            'price': '2.10000000',
            'priceScale': '1',
            'quantity': '1.00000000',
            'quantityScale': '2',
            'quoteAsset': 'USDT',
            'reduceOnly': False,
            'side': 'BUY',
            'source': 'API',
            'status': 'ACCEPTED',
            'symbol': 'ETH-221028-1700-C',
            'timeInForce': 'GTC',
            'type': 'LIMIT',
            'updateTime': '1666826857206'
        }
    ]

---
+++ examples/py/async-binance-fetch-option-order.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import asyncio
+import os
+import sys
+from pprint import pprint
+
+root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root + '/python')
+
+import ccxt.async_support as ccxt  # noqa: E402
+
+
+async def main():
+    exchange = ccxt.binance({
+        'apiKey': 'YOUR_API_KEY',
+        'secret': 'YOUR_SECRET',
+        # 'verbose': True,  # for debug output
+    })
+    await exchange.load_markets()
+    market_id = 'ETH-221028-1700-C'
+    order_id = 4612098335294532880
+    try:
+        response = await exchange.eapiPrivateGetOpenOrders({
+            # 'symbol': market_id,  # optional
+            # 'orderId': order_id,  # optional
+        })
+        pprint(response)
+    except Exception as e:
+        print('eapiPrivateGetOpenOrders() failed')
+        print(e)
+    await exchange.close()
+
+
+asyncio.run(main())
