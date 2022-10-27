d265576453ab9011faad7441cd00df53c07fbc17
Dan <dan.krm@outlook.com>
2022-10-26 23:10:09 -0600

    Binance: fetch option greeks example
    [
        {
            'askIV': '0.96028566',
            'bidIV': '0.84410369',
            'delta': '0.08242491',
            'gamma': '0.00174896',
            'highPriceLimit': '291.50',
            'lowPriceLimit': '0.10',
            'markIV': '0.90219468',
            'markPrice': '3.1',
            'symbol': 'ETH-221028-1700-C',
            'theta': '-4.81838804',
            'vega': '0.14652545'
        },
        ...
    ]

---
+++ examples/py/async-binance-fetch-option-details.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import asyncio
+import os
+import sys
+from pprint import pprint
+
+root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root + '/python')
+
+import ccxt.async_support as ccxt  # noqa: E402
+
+
+async def main():
+    exchange = ccxt.binance({
+        'apiKey': 'YOUR_API_KEY',
+        'secret': 'YOUR_SECRET',
+        # 'verbose': True,  # for debug output
+    })
+    await exchange.load_markets()
+    market_id = 'ETH-221028-1700-C'
+    try:
+        response = await exchange.eapiPublicGetMark({
+            # 'symbol': market_id,  # optional
+        })
+        pprint(response)
+    except Exception as e:
+        print('eapiPublicGetMark() failed')
+        print(e)
+    await exchange.close()
+
+
+asyncio.run(main())
