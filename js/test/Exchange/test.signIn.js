0b0807cba84af41a24d41984d2af07bc59168ec0
T.Todua <7117978+ttodua@users.noreply.github.com>
2022-10-31 19:47:21 +0400

    fix

--- js/test/Exchange/test.signIn.js
+++ js/test/Exchange/test.signIn.js
@@ -10,7 +10,7 @@ const assert = require ('assert');
 async function testSignIn(exchange) {
     const method = 'signIn';
     if (exchange.has[method]) {
-        await exchange[method] (code)
+        await exchange[method] ();
         console.log (method + ' successful');
     } else {
         console.log (method + '() is not supported');
