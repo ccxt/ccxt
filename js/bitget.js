11dcf182bc03c1802b4f45577298c941577969a7
AlgoQ <kobejanssens@outlook.com>
2022-11-02 16:51:20 +0100

    Add futures see value

--- js/bitget.js
+++ js/bitget.js
@@ -1583,6 +1583,7 @@ module.exports = class bitget extends Exchange {
          * @name bitget#fetchTickers
          * @description fetches price tickers for multiple markets, statistical calculations with the information calculated over the past 24 hours each market
          * @see https://bitgetlimited.github.io/apidoc/en/spot/#get-all-tickers
+         * @see https://bitgetlimited.github.io/apidoc/en/mix/#get-all-symbol-ticker
          * @param {[string]|undefined} symbols unified symbols of the markets to fetch the ticker for, all market tickers are returned if not assigned
          * @param {object} params extra parameters specific to the bitget api endpoint
          * @returns {object} an array of [ticker structures]{@link https://docs.ccxt.com/en/latest/manual.html#ticker-structure}
