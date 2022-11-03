636ac85ec4e8ffa1bda95bf06a3105e78e678643
T.Todua <7117978+ttodua@users.noreply.github.com>
2022-11-03 23:31:20 +0400

    btcaplha fix

--- js/btcalpha.js
+++ js/btcalpha.js
@@ -53,7 +53,8 @@ module.exports = class btcalpha extends Exchange {
                 'fetchOpenInterestHistory': false,
                 'fetchOpenOrders': true,
                 'fetchOrder': true,
-                'fetchOrderBook': true,
+                'fetchOrderBook': undefined, // their api doesn't work atm for orderbook
+                'fetchL2OrderBook': undefined, // same as above
                 'fetchOrders': true,
                 'fetchPosition': false,
                 'fetchPositionMode': false,
@@ -288,7 +289,8 @@ module.exports = class btcalpha extends Exchange {
         //
         const marketId = this.safeString (trade, 'pair');
         market = this.safeMarket (marketId, market, '_');
-        const timestamp = this.safeTimestamp (trade, 'timestamp');
+        const timestampRaw = this.safeString (trade, 'timestamp');
+        const timestamp = this.parseNumber (Precise.stringMul (timestampRaw, '1000000'));
         const priceString = this.safeString (trade, 'price');
         const amountString = this.safeString (trade, 'amount');
         const id = this.safeString (trade, 'id');
