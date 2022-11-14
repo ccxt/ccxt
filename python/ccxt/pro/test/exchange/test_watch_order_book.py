58605d9916670c2fd277672250a4fd8d03b9e8d6
Pablo Criado-Perez <pablo@criadoperez.com>
2022-11-14 04:11:55 +0100

    skipped public tests that require authentication

--- python/ccxt/pro/test/exchange/test_watch_order_book.py
+++ python/ccxt/pro/test/exchange/test_watch_order_book.py
@@ -11,6 +11,7 @@ async def test_watch_order_book(exchange, symbol):
     skipped_exchanges = [
         'ripio',
         'gopax',  # requires authentication for watch_order_book
+        'luno', # requires authentication for watch_order_book
     ]
     if exchange.id in skipped_exchanges:
         print(exchange.id, method, 'test skipped')
