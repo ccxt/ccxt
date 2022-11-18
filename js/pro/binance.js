1a52fa33439f38911d9f24c88cdf99ad100694d6
Pablo Criado-Perez <pablo@criadoperez.com>
2022-11-18 12:59:50 -0500

    reset throttle

--- js/pro/binance.js
+++ js/pro/binance.js
@@ -71,7 +71,7 @@ module.exports = class binance extends binanceRest {
                 'wallet': 'wb', // wb = wallet balance, cw = cross balance
                 'listenKeyRefreshRate': 1200000, // 20 mins
                 'ws': {
-                    'cost': 0,
+                    'cost': 5,
                 },
             },
         });
