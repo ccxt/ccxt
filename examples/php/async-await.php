19f2bda26fc27aa2e65acfb3fb8036efc08c1bef
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-11-21 12:57:04 -0800

    minor comment order edit

--- examples/php/async-await.php
+++ examples/php/async-await.php
@@ -10,14 +10,13 @@ await($exchange->load_markets());
 $symbols = array('BTC/USDT', 'ETH/USDT');
 
 
-echo "########## Combined await ##########\n";
 $promises = [];
 foreach ($symbols as $symbol) {
     $promises[] = $exchange->fetch_ticker($symbol);
 }
 $tickers = await(React\Promise\all($promises));
+echo "########## Combined await ##########\n";
 echo "{$tickers[0]['symbol']} {$tickers[0]['close']}  |  {$tickers[1]['symbol']} {$tickers[1]['close']}\n";
-
 
 
 
@@ -29,20 +28,20 @@ foreach ($symbols as $symbol) {
 
 
 
-echo "########## Callback style ##########\n";
 $exchange->fetch_ticker($symbols[0])->then(function($ticker){
+    echo "########## Callback style ##########\n";
     echo "### Callback->then finished: {$ticker['symbol']} {$ticker['close']}\n";
 });
 
 
 
 
-echo "########## Custom async function ##########\n";
 function myFunc ($exchange, $symbol) {
     return async(function () use ($exchange, $symbol) {
         // example sleep
         await(React\Promise\Timer\sleep(0.5));
         $ticker = await($exchange->fetch_ticker($symbol));
+        echo "########## Custom async function ##########\n";
         echo "### Custom async function : {$ticker['symbol']} {$ticker['close']}\n";
     });
 }
