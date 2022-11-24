bb0262eefa5ac583521d189f9448e3cda016c02b
Travis CI <travis@travis-ci.org>
2022-11-24 15:44:37 +0000

    2.2.24
    [ci skip]

--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -34,11 +34,11 @@ use Exception;
 
 include 'Throttle.php';
 
-$version = '2.2.23';
+$version = '2.2.24';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '2.2.23';
+    const VERSION = '2.2.24';
 
     public $browser;
     public $marketsLoading = null;
