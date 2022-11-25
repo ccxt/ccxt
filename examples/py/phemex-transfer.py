c6c53438e05d8da28abca8c2566c6baa5e556202
Dan <dan.krm@outlook.com>
2022-11-25 17:07:32 -0600

    Phemex: update transfer example
    Updated the Phemex transfer example

--- examples/py/phemex-transfer.py
+++ examples/py/phemex-transfer.py
@@ -2,24 +2,79 @@
 
 import os
 import sys
+from pprint import pprint
 
 root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(root + '/python')
 
 import ccxt  # noqa: E402
 
-exchange = ccxt.phemex({
-    'apiKey': 'YOUR_API_KEY',
-    'secret': 'YOUR_SECRET',
-})
+# -----------------------------------------------------------------------------
 
-# Example 1: Transfer from main account to sub account
-code = 'USDT'
-fromAccount = '4018341'
-toAccount = '4184432'
-amount = 1
+print('CCXT Version:', ccxt.__version__)
 
-params = {}
+# -----------------------------------------------------------------------------
 
-transfer = exchange.transfer (code, amount, fromAccount, toAccount, params = {})
-print(transfer)
+
+# Example 1: Transfer from spot main-account to future main-account
+def main_account_transfer():
+    exchange = ccxt.phemex({
+        'apiKey': 'YOUR_API_KEY',
+        'secret': 'YOUR_SECRET',
+        # 'verbose': True,  # for debug output
+    })
+    code = 'USDT'
+    amount = 10
+    fromAccount = 'spot'
+    toAccount = 'future'
+    params = {}
+
+    try:
+        transfer = exchange.transfer(code, amount, fromAccount, toAccount, params=params)
+        pprint(transfer)
+    except Exception as err:
+        print(err)
+
+
+# Example 3: Transfer from main account to sub account (Requires the main and sub-account UID's found on the sub-accounts page on Phemex)
+def transfer_between_main_and_sub_accounts():
+    exchange = ccxt.phemex({
+        'apiKey': 'YOUR_API_KEY',
+        'secret': 'YOUR_SECRET',
+        # 'verbose': True,  # for debug output
+    })
+    code = 'USDT'
+    amount = 10
+    fromAccount = '4148428'
+    toAccount = '4663243'
+    params = {}
+
+    try:
+        transfer = exchange.transfer(code, amount, fromAccount, toAccount, params=params)
+        pprint(transfer)
+    except Exception as err:
+        print(err)
+
+# Example 2: Transfer from spot sub-account to future sub-account (Requires a sub-account API key and secret)
+def sub_account_transfer():
+    exchange = ccxt.phemex({
+        'apiKey': 'YOUR_SUB_ACCOUNT_API_KEY',
+        'secret': 'YOUR_SUB_ACCOUNT_SECRET',
+        # 'verbose': True,  # for debug output
+    })
+    code = 'USDT'
+    amount = 10
+    fromAccount = 'spot'
+    toAccount = 'future'
+    params = {}
+
+    try:
+        transfer = exchange.transfer(code, amount, fromAccount, toAccount, params=params)
+        pprint(transfer)
+    except Exception as err:
+        print(err)
+
+
+main_account_transfer()
+# transfer_between_main_and_sub_accounts()
+# sub_account_transfer()
