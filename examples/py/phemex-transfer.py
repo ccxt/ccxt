cc28979452e02b17803cba2d36ac5a245787a119
Dan <dan.krm@outlook.com>
2022-11-25 17:09:30 -0600

    Comment edits

--- examples/py/phemex-transfer.py
+++ examples/py/phemex-transfer.py
@@ -36,7 +36,7 @@ def main_account_transfer():
         print(err)
 
 
-# Example 3: Transfer from main account to sub account (Requires the main and sub-account UID's found on the sub-accounts page on Phemex)
+# Example 2: Transfer from main-account to sub-account (Requires the main and sub-account UID's found on the account/sub-accounts page on Phemex)
 def transfer_between_main_and_sub_accounts():
     exchange = ccxt.phemex({
         'apiKey': 'YOUR_API_KEY',
@@ -55,7 +55,8 @@ def transfer_between_main_and_sub_accounts():
     except Exception as err:
         print(err)
 
-# Example 2: Transfer from spot sub-account to future sub-account (Requires a sub-account API key and secret)
+
+# Example 3: Transfer from spot sub-account to future sub-account (Requires a sub-account API key and secret)
 def sub_account_transfer():
     exchange = ccxt.phemex({
         'apiKey': 'YOUR_SUB_ACCOUNT_API_KEY',
