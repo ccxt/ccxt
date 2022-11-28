bd63662db44909783b7e5ee0fc9e73e3ecdbc706
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-11-28 15:49:28 +0000

    typo

--- js/bybit.js
+++ js/bybit.js
@@ -2487,7 +2487,7 @@ module.exports = class bybit extends Exchange {
             if (market['spot']) {
                 feeCurrencyCode = this.safeString (trade, 'commissionAsset');
             } else {
-                feeCurrencyCode = market['inverse'] ? market['base'] : market['setlle'];
+                feeCurrencyCode = market['inverse'] ? market['base'] : market['settle'];
             }
             fee = {
                 'cost': feeCostString,
