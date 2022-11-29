43d3c1a1f20452f8f2737324f9ba62a7454ecd79
T.Todua <7117978+ttodua@users.noreply.github.com>
2022-11-29 14:00:54 +0400

    coinex - address & memo

--- js/coinex.js
+++ js/coinex.js
@@ -2588,6 +2588,7 @@ module.exports = class coinex extends Exchange {
         //          code: 0,
         //          data: {
         //            coin_address: '1P1JqozxioQwaqPwgMAQdNDYNyaVSqgARq',
+        //            // coin_address: 'xxxxxxxxxxxxxx:yyyyyyyyy', // with embedded tag/memo
         //            is_bitcoin_cash: false
         //          },
         //          message: 'Success'
@@ -2627,12 +2628,21 @@ module.exports = class coinex extends Exchange {
         //         is_bitcoin_cash: false
         //     }
         //
-        const address = this.safeString (depositAddress, 'coin_address');
+        const coinAddress = this.safeString (depositAddress, 'coin_address');
+        const parts = coinAddress.split (':');
+        let address = undefined;
+        let tag = undefined;
+        if (parts.length > 1) {
+            address = parts[0];
+            tag = parts[1];
+        } else {
+            address = coinAddress;
+        }
         return {
             'info': depositAddress,
             'currency': this.safeCurrencyCode (undefined, currency),
             'address': address,
-            'tag': undefined,
+            'tag': tag,
             'network': undefined,
         };
     }
