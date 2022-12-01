7271de91bb41803a7074e694d2d31881e5490ca2
Travis CI <travis@travis-ci.org>
2022-12-01 10:13:40 +0000

    2.2.47
    [ci skip]

--- php/Exchange.php
+++ php/Exchange.php
@@ -36,7 +36,7 @@ use Elliptic\EdDSA;
 use BN\BN;
 use Exception;
 
-$version = '2.2.46';
+$version = '2.2.47';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -55,7 +55,7 @@ const PAD_WITH_ZERO = 1;
 
 class Exchange {
 
-    const VERSION = '2.2.46';
+    const VERSION = '2.2.47';
 
     private static $base58_alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
     private static $base58_encoder = null;
