afc439855f7628c204ac0d65f2e4e641a1fb15d0
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-12-12 10:31:50 +0000

    build

--- ts/src/binance.ts
+++ ts/src/binance.ts
@@ -4662,7 +4662,7 @@ export default class binance extends Exchange {
          * @returns {[object]} a list of [fee structures]{@link https://docs.ccxt.com/en/latest/manual.html#fee-structure}
          */
         await this.loadMarkets ();
-        const response = await this.sapiGetCapitalConfigGetall (params);
+        const response = await (this as any).sapiGetCapitalConfigGetall (params);
         //
         //    [
         //        {
