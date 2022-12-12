456b6ab4722ed1d326e4456541b20d96c336b2bc
Sam Germain <sam.germain@usask.ca>
2022-12-12 16:10:39 -0500

    exchange.assignDefaultDepositWithdrawFees removed defaultNetworks because the networks in the fee argument should already have the correct names

--- js/base/Exchange.js
+++ js/base/Exchange.js
@@ -2979,25 +2979,20 @@ module.exports = class Exchange {
          * @method
          * @description Takes a depositWithdrawFee structure and assigns the default values for withdraw and deposit
          * @param {object} fee A deposit withdraw fee structure
+         * @param {object} currency A currency structure, the response from this.currency ()
          * @returns {object} A deposit withdraw fee structure
          */
         const networkKeys = Object.keys (fee['networks']);
         const numNetworks = networkKeys.length;
-        const defaultChains = {
-            'ETH': 'ERC20',
-            'TRX': 'TRC20',
-            'BNB': 'BEP20',
-        };
         if (numNetworks === 1) {
             fee['withdraw'] = fee['networks'][networkKeys[0]]['withdraw'];
             fee['deposit'] = fee['networks'][networkKeys[0]]['deposit'];
             return fee;
         }
         const currencyCode = this.safeString (currency, 'code');
-        const codeAsChain = this.safeString (defaultChains, currencyCode, currencyCode);
         for (let i = 0; i < numNetworks; i++) {
             const network = networkKeys[i];
-            if (network === codeAsChain) {
+            if (network === currencyCode) {
                 fee['withdraw'] = fee['networks'][networkKeys[i]]['withdraw'];
                 fee['deposit'] = fee['networks'][networkKeys[i]]['deposit'];
             }
