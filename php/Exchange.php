dc9612a305af0c9194f04c29da950f0b44119cbe
Travis CI <travis@travis-ci.org>
2022-12-16 16:58:03 +0000

    2.4.22
    [ci skip]

--- php/Exchange.php
+++ php/Exchange.php
@@ -36,7 +36,7 @@ use Elliptic\EdDSA;
 use BN\BN;
 use Exception;
 
-$version = '2.4.21';
+$version = '2.4.22';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -55,7 +55,7 @@ const PAD_WITH_ZERO = 1;
 
 class Exchange {
 
-    const VERSION = '2.4.21';
+    const VERSION = '2.4.22';
 
     private static $base58_alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
     private static $base58_encoder = null;
--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -34,11 +34,11 @@ use Exception;
 
 include 'Throttle.php';
 
-$version = '2.4.21';
+$version = '2.4.22';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '2.4.21';
+    const VERSION = '2.4.22';
 
     public $browser;
     public $marketsLoading = null;
