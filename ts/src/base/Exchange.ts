e9fbb1b3d2a43e1e879aa57cb3c6d10867980f18
Sam Germain <sam.germain@usask.ca>
2022-12-16 14:11:05 -0500

    exchange.fetchDepositWithdrawals

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -457,6 +457,7 @@ export default class Exchange {
                 'fetchDepositAddresses': undefined,
                 'fetchDepositAddressesByNetwork': undefined,
                 'fetchDeposits': undefined,
+                'fetchDepositWithdrawals': undefined,
                 'fetchTransactionFee': undefined,
                 'fetchTransactionFees': undefined,
                 'fetchFundingHistory': undefined,
@@ -4178,6 +4179,24 @@ export default class Exchange {
         const firstMarket = this.safeString (symbols, 0);
         const market = this.market (firstMarket);
         return market;
+    }
+
+    async fetchDepositWithdrawals (code = undefined, since = undefined, limit = undefined, params = {}) {
+        /**
+         * @method
+         * @name exchange#fetchDepositWithdrawals
+         * @description fetch history of deposits and withdrawals
+         * @param {string|undefined} code unified currency code for the currency of the deposit/withdrawals, default is undefined
+         * @param {int|undefined} since timestamp in ms of the earliest deposit/withdrawal, default is undefined
+         * @param {int|undefined} limit max number of deposit/withdrawals to return, default is undefined
+         * @param {object} params extra parameters specific to the exchange api endpoint
+         * @returns {object} a list of [transaction structures]{@link https://docs.ccxt.com/en/latest/manual.html#transaction-structure}
+         */
+        if (this.has['fetchTransactions']) {
+            return await this.fetchTransactions (code, since, limit, params);
+        } else {
+            throw new NotSupported (this.id + ' fetchDepositWithdrawals () is not supported yet');
+        }
     }
 }
 
