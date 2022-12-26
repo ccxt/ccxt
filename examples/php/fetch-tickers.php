b5c8da8e623de210ac30c8c2ee230a2bfef591f1
T.Todua <7117978+ttodua@users.noreply.github.com>
2022-12-26 17:44:42 +0400

    duplicated ticker examples remove

--- examples/php/fetch-tickers.php
+++ examples/php/fetch-tickers.php
@@ -11,13 +11,30 @@ $exchange =new \ccxt\binance(array(
     'timeout' => 30000,
 ));
 
+$markets = $exchange->load_markets();
+
 try {
-    $result = $exchange->fetch_tickers (); // note, don't call it for binance more than once in every few seconds.
-    print_r ($result);
-} catch (\ccxt\NetworkError $e) {
-    echo '[Network Error] ' . $e->getMessage() . "\n";
+    if ($exchange->has['fetchTickers']) {
+        // one API call for all tickers (preferred way)
+        $result = $exchange->fetch_tickers (); // note, don't call it for specifically binance more than once in every few seconds.
+        echo "Called fetchTickers() for all tickers at once. Results count: " . count($result) . "\n";
+    } else if ($exchange->has['fetchTicker']) {
+        // Individual API calls for all tickers one by one (non-preferred way)
+        echo "fetchTickers() is not supported by " . $exchange->id . ", calling individual fetchTicker() for each symbol instead.\n";
+        // fetch one by one (not recommended)
+        $i = 0;
+        $test_symbols_amount = 4;
+        foreach ($markets as $symbol => $m) {
+            if ($i++ && $i > $test_symbols_amount) {
+                echo "Stopping after getting " . $test_symbols_amount . " test symbols.\n";
+                break;
+            }
+            $result = $exchange->fetch_ticker($symbol);
+            echo "Fetched ticker for " . $result['symbol'] . ", 24hr high: " . $result['high'] . "\n";
+        }
+    } else {
+        echo "fetchTicker/s() not supported by " . $exchange->id . ", skipping.\n";
+    }
 } catch (Exception $e) {
     echo '[Error] ' . $e->getMessage() . "\n";
 }
-
-?>
