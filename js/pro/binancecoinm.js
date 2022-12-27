cda379c6d53e9c40bb5eff8e2618133da87505e2
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2022-12-27 17:43:59 +0000

    pro classes

--- js/pro/binancecoinm.js
+++ js/pro/binancecoinm.js
@@ -15,7 +15,8 @@ module.exports = class binancecoinm extends binance {
                 'logo': 'https://user-images.githubusercontent.com/1294454/117738721-668c8d80-b205-11eb-8c49-3fad84c4a07f.jpg',
             },
             'options': {
-                'defaultType': 'delivery',
+                'defaultType': 'swap',
+                'defaultSubType': 'linear',
             },
         });
     }
