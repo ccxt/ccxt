6a39a0825b95087dc53484c3040f75aa70407ba6
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-12-29 14:15:26 -0800

    actual rewrite

--- python/fastflake.sh
+++ python/fastflake.sh
@@ -2,12 +2,5 @@
 
 # saves time over the other running flake8 on all the files
 
-files=$(git diff --name-only --relative origin/master | sed -n '/py$/p' | xargs)
-command="flake8 --ignore=F722,F841,F821,W504,E402,E501,E275,E902 --exclude static_dependencies,node_modules,.tox"
-execute="${command} ${files}"
-echo -e "\n${execute}\n\n"
-
-if [[ -n ${files} ]]; then
-  # only one variable expansion happens here so it is safe
-  ${execute}
-fi
+echo -e '\n checking syntax using flake8 over the following files:\n'
+git diff --name-only --relative origin/master | sed -n '/py$/p' | tee /dev/stderr | xargs -L 0 flake8 --ignore=F722,F841,F821,W504,E402,E501,E275,E902 --exclude static_dependencies,node_modules,.tox
