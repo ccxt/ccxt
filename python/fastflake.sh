c55e70f33b369f0ed7b734e8560d5f8c2b244b1b
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-12-29 14:13:36 -0800

    rewrite fastflake

--- python/fastflake.sh
+++ python/fastflake.sh
@@ -5,7 +5,7 @@
 files=$(git diff --name-only --relative origin/master | sed -n '/py$/p' | xargs)
 command="flake8 --ignore=F722,F841,F821,W504,E402,E501,E275,E902 --exclude static_dependencies,node_modules,.tox"
 execute="${command} ${files}"
-printf "\n${execute}\n\n"
+echo -e "\n${execute}\n\n"
 
 if [[ -n ${files} ]]; then
   # only one variable expansion happens here so it is safe
