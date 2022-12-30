c798062823f5d0f8d831befea816863c87857b8d
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-12-30 10:55:38 -0800

    more edits to the test manage

--- tests-manager.sh
+++ tests-manager.sh
@@ -45,8 +45,28 @@ function run_tests {
   wait $rest_pid && wait $ws_pid
 }
 
+set -x
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+  # we are in a merge commit
+  # the previous commit is either a direct push or a commit on another branch
+  # if it is a direct push then the commit would be parent 1 otherwise the commit will be parent 2
+  parents="$(git show -s --format=%P "$TRAVIS_COMMIT")"
+  travis_number=$(xargs git show -s --format=%ce <<< "$parents" | grep -n 'travis@travis-ci.org')
+  number=${travis_number:0:1}
+  other_commit_index=$(( ((number - 1) ^ 1) + 1  ))
+  other_commit_hash=$(cut -d ' ' -f$other_commit_index <<< "$parents")
+  status=$(curl  -H "Accept: application/json"  -H "Authorization: Bearer $GITHUB_AUTH_TOKEN"  -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/repos/ccxt/ccxt/commits/$(git show -s --format=%P "$other_commit_hash")/check-runs | jq '.check_runs[0].conclusion'")
+  # we check the travis build status of the previous commit
+  if [ "$status" = "completed" ]; then
+    exit 0
+  fi
+fi
+set +x
+
 if [ "$delta" -gt $six_hours ] || grep -q -E 'Exchange.php|/test|/base|^build|static_dependencies|^run-tests' <<< "$diff"; then
-  run_tests && date -u +%s > "$timestamp_file"
+  # shellcheck disable=SC2155
+  run_tests && export LAST_RUN=$(date -u +%s)
 else
   run_tests "$(sed -E -n 's:^js/([^/]+)\.js$:\1:p' <<< "$diff" | xargs)" "$(sed -E -n 's:^js/pro/([^/]+)\.js$:\1:p' <<< "$diff" | xargs)"
 fi
