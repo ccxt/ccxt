e0da61c3fee56acd1c05b0f692a48c7ff05c07bd
Carlo Revelli <carlo.revelli@berkeley.edu>
2022-12-31 10:58:39 -0800

    update travis tests manager

--- tests-manager.sh
+++ tests-manager.sh
@@ -1,25 +1,66 @@
 #!/usr/bin/env bash
 
+set -x
+
+if [ "${BASH_VERSION:0:1}" -lt 4 ]; then
+  echo "EPROGMISMATCH: bash version must be at least 4" >&2
+  exit 75
+fi
+
 if [ $# -gt 0 ]; then
-  echo "E2BIG: too many arguments"
+  echo "E2BIG: too many arguments" >&2
   exit 7
 fi
 
-timestamp_file="$TRAVIS_BUILD_DIR/.cache/last-run.txt"
-if ! [ -f "$timestamp_file" ]; then
-  echo "file not found"
-  echo '0' > "$timestamp_file"
+cache_path="$TRAVIS_BUILD_DIR/.cache"
+if ! [ -d "$cache_path" ]; then
+  echo "ENOENT: .cache not found" >&2
+  exit 2
+fi
+
+timestamps_path="$cache_path/timestamps"
+urls_path="$cache_path/urls"
+
+# slug is of the form owner_name/repo_name
+# our file name is of the form owner_name:remote_branch_name
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  slug="$TRAVIS_REPO_SLUG"
+  branch="$TRAVIS_BRANCH"
+else
+  slug="$TRAVIS_PULL_REQUEST_SLUG"
+  branch="$TRAVIS_PULL_REQUEST_BRANCH"
+fi
+cache_file_name="$(cut -d '/' -f1 <<< "$slug"):$branch"
+
+mkdir "$timestamps_path" 2> /dev/null
+mkdir "$urls_path" 2> /dev/null
+
+cached_timestamp_file="$timestamps_path/$cache_file_name.txt"
+cached_url_file="$urls_path/$cache_file_name.txt"
+
+if ! [ -f "$cached_timestamp_file" ]; then
+  # we initialise the cached timestamp to the current time
+  # even before running a successful build so that pull requests
+  date -u +%s > "$cached_timestamp_file"
+fi
+last_run=$(< "$cached_timestamp_file")
+if [ "$last_run" -ne "$last_run" ] 2> /dev/null; then
+  # check that last run is an integer
+  last_run=0
 fi
 
 now=$(date -u +%s)
-last_run=$(< "$timestamp_file")
 delta=$((now - last_run))
 six_hours=$((60 * 60 * 6))
 diff=$(git diff master --name-only)
-echo "$last_run"
-git log -2 --format=%H
-git show -s --format=%H HEAD
-git show -s --format=%H "$TRAVIS_COMMIT"
+
+# begin debug
+echo "$cached_timestamp_file"
+echo "$cached_url_file"
+# end debug
+
+echo "last build url: $(cat "$cached_url_file")" 2> /dev/null
+echo "completed at: $(date -r "$last_run") - $((delta / 3600)) hours ago" 2> /dev/null
 
 function run_tests {
   local rest_args=
@@ -46,7 +87,7 @@ function run_tests {
     node run-tests-ws --js --python-async --php-async $ws_args &
     local ws_pid=$!
   fi
-  wait $rest_pid && wait $ws_pid
+  wait $rest_pid && wait $ws_pid && echo "$TRAVIS_BUILD_WEB_URL" > "$cached_url_file"
 }
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ] && git show -s --format=%ce HEAD~1 | grep -q 'travis@travis-ci.org'; then
@@ -58,7 +99,7 @@ fi
 
 if [ "$delta" -gt $six_hours ] || grep -q -E 'Exchange.php|/test|/base|^build|static_dependencies|^run-tests' <<< "$diff"; then
   # shellcheck disable=SC2155
-  run_tests && date -u +%s > "$timestamp_file"
+  run_tests && date -u +%s > "$cached_timestamp_file"
 else
   run_tests "$(sed -E -n 's:^js/([^/]+)\.js$:\1:p' <<< "$diff" | xargs)" "$(sed -E -n 's:^js/pro/([^/]+)\.js$:\1:p' <<< "$diff" | xargs)"
 fi
