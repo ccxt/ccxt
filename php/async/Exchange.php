3f91128fab5ea3406dce87fed845eb255314615d
Travis CI <travis@travis-ci.org>
2023-01-02 12:41:06 +0000

    2.5.11
    [ci skip]

--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -34,11 +34,11 @@ use Exception;
 
 include 'Throttle.php';
 
-$version = '2.5.10';
+$version = '2.5.11';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '2.5.10';
+    const VERSION = '2.5.11';
 
     public $browser;
     public $marketsLoading = null;
@@ -1304,7 +1304,7 @@ class Exchange extends \ccxt\Exchange {
         for ($i = 0; $i < count($response); $i++) {
             $item = $response[$i];
             $id = $this->safe_string($item, $marketIdKey);
-            $market = $this->safe_market($id);
+            $market = $this->safe_market($id, null, null, $this->safe_string($this->options, 'defaultType'));
             $symbol = $market['symbol'];
             $contract = $this->safe_value($market, 'contract', false);
             if ($contract && (($symbols === null) || $this->in_array($symbol, $symbols))) {
--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -4,7 +4,7 @@
 
 # -----------------------------------------------------------------------------
 
-__version__ = '2.5.10'
+__version__ = '2.5.11'
 
 # -----------------------------------------------------------------------------
 
@@ -2678,7 +2678,7 @@ class Exchange(object):
         for i in range(0, len(response)):
             item = response[i]
             id = self.safe_string(item, marketIdKey)
-            market = self.safe_market(id)
+            market = self.safe_market(id, None, None, self.safe_string(self.options, 'defaultType'))
             symbol = market['symbol']
             contract = self.safe_value(market, 'contract', False)
             if contract and ((symbols is None) or self.in_array(symbol, symbols)):
