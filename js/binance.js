766ac0988248bb7e6b0660a7b02bbec6a4a0f774
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-01-04 18:45:51 +0000

    small fix

--- js/binance.js
+++ js/binance.js
@@ -1415,7 +1415,7 @@ module.exports = class binance extends Exchange {
                     } else {
                         unifiedSubType = defaultSubType;
                     }
-                    const isLegacyLinearOrSubTypeLinear = isLegacyLinear || unifiedSubType;
+                    const isLegacyLinearOrSubTypeLinear = isLegacyLinear || (unifiedSubType === 'linear');
                     const settle = isLegacyLinearOrSubTypeLinear ? market['quote'] : market['base'];
                     return super.market (symbol + ':' + settle);
                 } else {
