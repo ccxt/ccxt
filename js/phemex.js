439b087298c80644485959605b26e5fedfd9537d
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-01-12 14:36:01 +0000

    fix fetchTicker

--- js/phemex.js
+++ js/phemex.js
@@ -1213,7 +1213,7 @@ module.exports = class phemex extends Exchange {
         };
         let method = 'v1GetMdSpotTicker24hr';
         if (market['swap']) {
-            if (!market['linear']) {
+            if (market['inverse'] || market['settle'] === 'USD') {
                 method = 'v1GetMdTicker24hr';
             } else {
                 method = 'v2GetMdV2Ticker24hr';
