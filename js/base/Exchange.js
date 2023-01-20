52d87993f8d9d03f639746e8e05849e8bb74f6d7
neeeekitos <nikita.a.terekhov@gmail.com>
2023-01-20 14:27:29 +0100

    fix: errors

--- js/base/Exchange.js
+++ js/base/Exchange.js
@@ -2035,7 +2035,7 @@ module.exports = class Exchange {
     async fetch2(path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined, config = {}, context = {}) {
         if (this.enableRateLimit) {
             const cost = this.calculateRateLimiterCost(api, method, path, params, config, context);
-            await this.throttle(cost, path, params?.customThrottleConfig);
+            await this.throttle(cost, path, params?.customExpireInterval, params?.customPriority);
             delete params?.customExpireInterval;
             delete params?.customPriority;
         }
