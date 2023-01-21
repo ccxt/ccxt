4f601bf7f1c8090889d493f4e483767bd838afe4
Travis CI <travis@travis-ci.org>
2023-01-21 17:44:09 +0000

    2.6.58
    [ci skip]

--- php/async/bitget.php
+++ php/async/bitget.php
@@ -196,6 +196,7 @@ class bitget extends Exchange {
                         'get' => array(
                             'account/account' => 2,
                             'account/accounts' => 2,
+                            'account/accountBill' => 2,
                             'account/open-count' => 1,
                             'order/current' => 2,
                             'order/history' => 2,
