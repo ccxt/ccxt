a158774fdfb5aa7686da2f6e2aee784caf089092
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-01-21 13:38:01 +0000

    fix merge

--- js/kucoin.js
+++ js/kucoin.js
@@ -1661,9 +1661,7 @@ module.exports = class kucoin extends Exchange {
         if (stop) {
             method = 'privateGetStopOrder';
         }
-        if (marginMode !== undefined) {
-            request['tradeType'] = this.safeString (this.options['marginModes'], marginMode, marginMode);
-        }
+        request['tradeType'] = this.safeString (this.options['marginModes'], marginMode, 'TRADE');
         const response = await this[method] (this.extend (request, query));
         //
         //     {
