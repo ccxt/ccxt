157861c8efeec79b8053822f97dc336c6fa62519
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-01-24 15:11:45 +0000

    typo

--- js/coinbase.js
+++ js/coinbase.js
@@ -2073,7 +2073,7 @@ module.exports = class coinbase extends Exchange {
             'status': this.parseOrderStatus (this.safeString (order, 'status')),
             'fee': {
                 'cost': this.safeString (order, 'total_fees'),
-                'current': undefined,
+                'currency': undefined,
             },
             'trades': undefined,
         }, market);
