9d79ac27a473470b408d0f760d6a98e1d97af07a
Oskar Firlej <25161992+Firlej@users.noreply.github.com>
2023-02-04 01:03:14 +0100

    add assert statement to verify that profit_margin will be higher than fees

--- examples/bots/py/binance-spot-scalping-bot.py
+++ examples/bots/py/binance-spot-scalping-bot.py
@@ -7,24 +7,13 @@ print('CCXT Version:', ccxt.__version__)
 ####################################################################################
 # Simple implementation of a trading bot using a scalping strategy. Periodically  ##
 # buys a product using a market buy for the minimum amount allowed by the         ##
-# exchange on the selected market pair and immediately places a limit sell order  ##
+# exchange on the selected market pair and immidietaly places a limit sell order  ##
 # for a slightly higher price.                                                    ##
 #                                                                                 ##
 # Disclaimer: this bot is for educational purposes only. Use at your own risk.    ##
 ####################################################################################
 
-# Define the trading symbol
-symbol = "BTC/BUSD"
-
-# Define the minimum profit margin as a multiplier
-# Remeber to set it higher than the trading fees
-profit_margin = 1.002
-
-# Define the sleep time between each iteration
-sleep_time_seconds = 60 * 30
-
-# Load the .env file
-load_dotenv()
+# SET THE PARAMETERS HERE
 
 # Initiate the exchange
 exchange = ccxt.binance({
@@ -32,17 +21,36 @@ exchange = ccxt.binance({
     'secret': 'YOUR_SECRET',
 })
 
+# Define the trading symbol
+symbol = "BTC/BUSD"
+
+# Define the profit margin as a price multiplier
+profit_margin = 1.003
+
+# Define the sleep time between each iteration
+sleep_time_seconds = 60 * 30
+
+# END OF PARAMETERS
+
+# Fetch the maker and taker trading fees
+fees = exchange.fetch_trading_fee(symbol=symbol)
+taker_fee = fees['taker']
+maker_fee = fees['maker']
+
+print("Fees for symbol:", symbol, "Taker:", taker_fee, "Maker:", maker_fee)
+
+# Calculate the minimum profit margin to cover for the market buy and limit sell fees
+min_profit_margin = 1 / (1 - taker_fee) / (1 - maker_fee)
+assert profit_margin >= min_profit_margin, f"The profit margin is too low. The minimum profit margin to not incur losses is: {min_profit_margin}"
+
 # Load the market
 market = exchange.load_markets()[symbol]
-
 min_cost = market['limits']['cost']['min']
 min_amount = market['limits']['amount']['min']
-
 print(f"The minimum cost: {min_cost} {market['quote']}")
 print(f"The minimum amount: {min_amount} {market['base']}")
 
 print("Starting the bot...")
-print("Press Ctrl+C to stop the bot")
 
 # Start the main loop
 while True:
@@ -55,7 +63,7 @@ while True:
         price = exchange.fetch_ticker(symbol)["last"]
         print("The current price:", price)
 
-        # Calculate the minimum order amount and round it up to the nearest multiple of min_amount
+        # Recalculate the minimum order amount and round it up to the nearest multiple of min_amount
         min_order_amount = math.ceil(min_cost / price / min_amount) * min_amount
         print("The minimum order amount :", min_order_amount)
 
@@ -69,7 +77,7 @@ while True:
         # Place the limit sell order
         sell_order = exchange.create_order(symbol=symbol, type="limit", side="sell", amount=min_order_amount, price=sell_price)
 
-        # Calculate the profit earned after the sell order will be filled
+        # Calculate the profit earned after the sell order is filled
         profit = (sell_order["price"] - buy_order["price"]) * buy_order["filled"]
         print(f"Created limit sell order for {sell_order['amount']} {market['base']} at {sell_order['price']} {market['quote']} for a profit of {profit} {market['quote']}")
 
