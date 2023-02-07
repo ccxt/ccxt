a2e754db43fca33844c90555856e4dff140af1bb
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-02-07 10:17:42 +0000

    add missing order status

--- js/bybit.js
+++ js/bybit.js
@@ -3212,6 +3212,7 @@ module.exports = class bybit extends Exchange {
             'New': 'open',
             'Rejected': 'rejected', // order is triggered but failed upon being placed
             'PartiallyFilled': 'open',
+            'PartiallyFilledCancelled': 'canceled',
             'Filled': 'closed',
             'PendingCancel': 'open',
             'Cancelled': 'canceled',
