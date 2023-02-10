ac8a7e927987a0a97b04f7492eb34657795bd72b
Sam Germain <sam.germain@usask.ca>
2023-02-09 20:16:22 -0500

    huobi transfer removed throwing error response

--- js/huobi.js
+++ js/huobi.js
@@ -5203,13 +5203,6 @@ module.exports = class huobi extends Exchange {
         //        'print-log': true
         //    }
         //
-        const success = this.safeValue2 (response, 'success', 'status');
-        if (!success) {
-            const message = this.safeString (response, 'message');
-            const errorCode = this.safeString (response, 'code');
-            this.throwExactlyMatchedException (this.exceptions['exact'], errorCode, message);
-            throw new Error (message);
-        }
         return this.parseTransfer (response, currency);
     }
 
