08841f4b7ca67f17c9bb15eecc4075b0bb6535c4
sc0Vu <alk03073135@gmail.com>
2023-02-15 16:55:53 +0800

    okx: support authenticate for public ws ccxt/ccxt#16853

--- js/pro/okx.js
+++ js/pro/okx.js
@@ -305,6 +305,9 @@ module.exports = class okx extends okxRest {
         // 3. Data feeds will be delivered every 100ms (vs. every 200ms now)
         //
         const depth = this.safeString (options, 'depth', 'books');
+        if ((depth === 'books-l2-tbt') || (depth === 'books50-l2-tbt')) {
+            await this.authenticate ({ 'access': 'public' });
+        }
         const orderbook = await this.subscribe ('public', depth, symbol, params);
         return orderbook.limit ();
     }
@@ -520,7 +523,9 @@ module.exports = class okx extends okxRest {
 
     authenticate (params = {}) {
         this.checkRequiredCredentials ();
-        const url = this.urls['api']['ws']['private'];
+        const access = this.safeString (params, 'access', 'private');
+        params = this.omit (params, [ 'access' ]);
+        const url = this.urls['api']['ws'][access];
         const messageHash = 'authenticated';
         const client = this.client (url);
         let future = this.safeValue (client.subscriptions, messageHash);
