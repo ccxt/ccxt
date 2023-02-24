440645d68988ec88677acf667d61d9a97ab94fcb
Travis CI <travis@travis-ci.org>
2023-02-24 10:49:05 +0000

    2.8.33
    [ci skip]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### 2.8.33 (2023-02-24)
+
 ### 2.8.32 (2023-02-23)
 
 ### 2.8.31 (2023-02-22)
--- ccxt.js
+++ ccxt.js
@@ -37,7 +37,7 @@ const Exchange  = require ('./js/base/Exchange')
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '2.8.32'
+const version = '2.8.33'
 
 Exchange.ccxtVersion = version
 
