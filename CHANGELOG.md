91a0591d5c5f9c4a43668e59b4566ff4c1ead52b
Travis CI <travis@travis-ci.org>
2023-02-25 14:16:51 +0000

    2.8.39
    [ci skip]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### 2.8.39 (2023-02-25)
+
 ### 2.8.38 (2023-02-25)
 
 ### 2.8.37 (2023-02-24)
--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -34,11 +34,11 @@ use Exception;
 
 include 'Throttle.php';
 
-$version = '2.8.38';
+$version = '2.8.39';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '2.8.38';
+    const VERSION = '2.8.39';
 
     public $browser;
     public $marketsLoading = null;
