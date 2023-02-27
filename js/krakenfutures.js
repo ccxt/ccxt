28e59ee5ff967f7018576b5a0700366260718a4e
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-02-27 10:20:37 +0000

    revert krakenfutures

--- js/krakenfutures.js
+++ js/krakenfutures.js
@@ -510,8 +510,16 @@ module.exports = class krakenfutures extends Exchange {
         const percentage = Precise.stringMul (Precise.stringDiv (change, open), '100');
         const average = Precise.stringDiv (Precise.stringAdd (open, last), '2');
         const volume = this.safeString (ticker, 'vol24h');
-        const baseVolume = (!market['index'] && market['linear']) ? volume : undefined;
-        const quoteVolume = (!market['index'] && market['inverse']) ? volume : undefined;
+        let baseVolume = undefined;
+        let quoteVolume = undefined;
+        const isIndex = this.safeValue (market, 'index', false);
+        if (!isIndex) {
+            if (market['linear']) {
+                baseVolume = volume;
+            } else if (market['inverse']) {
+                quoteVolume = volume;
+            }
+        }
         return this.safeTicker ({
             'symbol': symbol,
             'timestamp': timestamp,
