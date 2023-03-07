f983b66e4a6d9aaffdb7cbc9f07da0b2a7e5e068
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-03-07 16:02:15 +0000

    add missing filter

--- js/bithumb.js
+++ js/bithumb.js
@@ -441,7 +441,7 @@ module.exports = class bithumb extends Exchange {
                 result[symbol] = this.parseTicker (ticker, market);
             }
         }
-        return result;
+        return this.filterByArray (result, 'symbol', symbols);
     }
 
     async fetchTicker (symbol, params = {}) {
