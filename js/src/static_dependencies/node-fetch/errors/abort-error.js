a7dfd2257cd36426df2ea0fe3b7a6d86b2884ad2
Carlo Revelli <carlo.revelli@berkeley.edu>
2023-03-13 14:49:24 -0700

    run tsc to regenerate files [ci deploy]

---
+++ js/src/static_dependencies/node-fetch/errors/abort-error.js
@@ -0,0 +1,9 @@
+import { FetchBaseError } from './base.js';
+/**
+ * AbortError interface for cancelled requests
+ */
+export class AbortError extends FetchBaseError {
+    constructor(message, type = 'aborted') {
+        super(message, type);
+    }
+}
---
+++ js/src/static_dependencies/node-fetch/utils/is-redirect.js
@@ -0,0 +1,10 @@
+const redirectStatus = new Set([301, 302, 303, 307, 308]);
+/**
+ * Redirect code matching
+ *
+ * @param {number} code - Status code
+ * @return {boolean}
+ */
+export const isRedirect = code => {
+    return redirectStatus.has(code);
+};
