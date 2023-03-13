e80eb290c892dc6821b4ba9fac05fa6f77ad57ed
Travis CI <travis@travis-ci.org>
2023-03-13 18:11:25 +0000

    3.0.1
    [ci skip]

--- js/ccxt.js
+++ js/ccxt.js
@@ -37,7 +37,7 @@ import * as functions from './src/base/functions.js';
 import * as errors from './src/base/errors.js';
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
-const version = '2.9.16';
+const version = '3.0.1';
 Exchange.ccxtVersion = version;
 //-----------------------------------------------------------------------------
 import ace from './src/ace.js';
--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -36,11 +36,11 @@ use \ccxt\pro\ClientTrait;
 
 include 'Throttle.php';
 
-$version = '2.9.16';
+$version = '3.0.1';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '2.9.16';
+    const VERSION = '3.0.1';
 
     public $browser;
     public $marketsLoading = null;
