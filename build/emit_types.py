92f625dcb455d1e69335eddee578436a454cb3d3
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-03-14 18:40:50 +0000

    update emitTypes.js

--- build/emit_types.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-# using this script to run it on windows and linux while supressing the output
-# because it is expected to fail on the static_dependencies folder
-# but the build process should not be stopped
-command = 'tsc --emitDeclarationOnly --declaration'
-subprocess.getoutput(command)
-print("Type declarations emitted!")
