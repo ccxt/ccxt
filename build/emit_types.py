3c1fcbca645d6dc4d8236473777af87854cc818a
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-03-14 15:33:40 +0000

    improve comment

--- build/emit_types.py
+++ build/emit_types.py
@@ -1,4 +1,6 @@
 import subprocess
-# using this script to run it on windows and linux
+# using this script to run it on windows and linux while supressing the output
+# because it is expected to fail on the static_dependencies folder
+# but the build process should not be stopped
 command = 'tsc --emitDeclarationOnly --declaration'
 subprocess.getoutput(command)
