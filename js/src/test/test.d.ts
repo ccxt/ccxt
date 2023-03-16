f2434f7e4c752cc2a4953b8bf984be146cb76edb
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-03-16 12:28:26 +0400

    -

--- js/src/test/test.d.ts
+++ js/src/test/test.d.ts
@@ -1,1 +1,18 @@
+declare class emptyClass {
+}
+export default class testMainClass extends emptyClass {
+    init(exchange: any, symbol: any): Promise<void>;
+    expandSettings(exchange: any, symbol: any): void;
+    testMethod(methodName: any, exchange: any, args: any, isPublic: any): Promise<any>;
+    testSafe(methodName: any, exchange: any, args: any, isPublic: any): Promise<boolean>;
+    runPublicTests(exchange: any, symbol: any): Promise<void>;
+    loadExchange(exchange: any): Promise<void>;
+    getTestSymbol(exchange: any, isSpot: any, symbols: any): any;
+    getExchangeCode(exchange: any, codes?: any): any;
+    getMarketsFromExchange(exchange: any, spot?: boolean): {};
+    getValidSymbol(exchange: any, spot?: boolean): any;
+    testExchange(exchange: any, providedSymbol?: any): Promise<void>;
+    runPrivateTests(exchange: any, symbol: any): Promise<void>;
+    startTest(exchange: any, symbol: any): Promise<void>;
+}
 export {};
