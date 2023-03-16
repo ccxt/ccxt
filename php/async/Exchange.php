d1af2acda22156fa019636e19ab87cdbbfdc062c
Travis CI <travis@travis-ci.org>
2023-03-16 17:06:44 +0000

    3.0.13
    [ci skip]

--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -36,11 +36,11 @@ use \ccxt\pro\ClientTrait;
 
 include 'Throttle.php';
 
-$version = '3.0.11';
+$version = '3.0.13';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '3.0.11';
+    const VERSION = '3.0.13';
 
     public $browser;
     public $marketsLoading = null;
