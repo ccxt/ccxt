61c664c596272b1206c131d563b6efe21d22a892
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-03-17 15:09:11 +0000

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import json from "@rollup/plugin-json";
 import execute from 'rollup-plugin-execute';
 
 export default {
-  inlineDynamicImports: true,
+  preserveModules: true,
   input: "./js/ccxt.js",
   output: [
     {
@@ -17,6 +17,10 @@ export default {
       transformMixedEsModules: true,
       dynamicRequireTargets: ["**/js/src/static_dependencies/**/*.cjs"],
     }),
-    execute("echo '{ \"type\": \"commonjs\" }' > ./dist/cjs/package.json")
-  ]
+    execute("echo '{ \"type\": \"commonjs\" }' > ./dist/cjs/package.json") // this is needed to make node treat files inside dist/cjs as CJS modules
+  ],
+  onwarn: ( warning, next ) => {
+    if ( warning.message.indexOf('is implicitly using "default" export mode') > -1 ) return;
+    next( warning );
+},
 };
