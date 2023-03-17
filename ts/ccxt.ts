aca8410321a41606144f71f6af4fa096928b9d5c
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-03-17 10:22:25 +0000

    fix: restored named Exceptions exports

--- ts/ccxt.ts
+++ ts/ccxt.ts
@@ -34,6 +34,8 @@ import { Precise }   from './src/base/Precise.js'
 import * as functions from './src/base/functions.js'
 import * as errors   from './src/base/errors.js'
 import { Market, Trade , Fee, Ticker, OrderBook, Order, Transaction, Tickers, Currency, Balance, DepositAddress, WithdrawalResponse, DepositAddressResponse, OHLCV, Balances, PartialBalances } from './src/base/types.js'
+import { BaseError, ExchangeError, PermissionDenied, AccountNotEnabled, AccountSuspended, ArgumentsRequired, BadRequest, BadSymbol, MarginModeAlreadySet, BadResponse, NullResponse, InsufficientFunds, InvalidAddress, InvalidOrder, OrderNotFound, OrderNotCached, CancelPending, OrderImmediatelyFillable, OrderNotFillable, DuplicateOrderId, NotSupported, NetworkError, DDoSProtection, RateLimitExceeded, ExchangeNotAvailable, OnMaintenance, InvalidNonce, RequestTimeout, AuthenticationError, AddressPending }  from './src/base/errors.js'
+
 
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
@@ -409,6 +411,36 @@ export {
     Precise,
     functions,
     errors,
+    BaseError,
+    ExchangeError,
+    PermissionDenied,
+    AccountNotEnabled,
+    AccountSuspended,
+    ArgumentsRequired,
+    BadRequest,
+    BadSymbol,
+    MarginModeAlreadySet,
+    BadResponse,
+    NullResponse,
+    InsufficientFunds,
+    InvalidAddress,
+    InvalidOrder,
+    OrderNotFound,
+    OrderNotCached,
+    CancelPending,
+    OrderImmediatelyFillable,
+    OrderNotFillable,
+    DuplicateOrderId,
+    NotSupported,
+    NetworkError,
+    DDoSProtection,
+    RateLimitExceeded,
+    ExchangeNotAvailable,
+    OnMaintenance,
+    InvalidNonce,
+    RequestTimeout,
+    AuthenticationError,
+    AddressPending,
     Market,
     Trade,
     Fee,
