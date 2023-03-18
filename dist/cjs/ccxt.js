24a59c76a038c3ca5a2e5b4474d1f46cb052eb0d
Travis CI <travis@travis-ci.org>
2023-03-18 16:40:11 +0000

    3.0.22
    [ci skip]

--- dist/cjs/ccxt.js
+++ dist/cjs/ccxt.js
@@ -214,7 +214,7 @@ var zb$1 = require('./src/pro/zb.js');
 
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
-const version = '3.0.21';
+const version = '3.0.22';
 Exchange["default"].ccxtVersion = version;
 const exchanges = {
     'ace': ace,
