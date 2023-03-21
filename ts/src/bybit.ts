9bd67a3ec174ec44233071c714e6150bf6be1974
Dan <dan.krm@outlook.com>
2023-03-21 15:02:03 -0600

    feat(bybit): fetchOHLCV baseVolume for inverse markets
    ### Bybit fetchOHLCV:
    ```
    bybit.fetchOHLCV (BTC/USD:BTC, 1m, , 3)
    2023-03-21T20:57:35.348Z iteration 0 passed in 256 ms
    
    1679432100000 |   28153 | 28165.5 | 28150.5 |   28152 | 6.57921134
    1679432160000 |   28152 |   28153 | 28142.5 | 28152.5 | 5.64614585
    1679432220000 | 28152.5 | 28153.5 | 28135.5 | 28135.5 | 2.61354273
    3 objects
    ```

--- ts/src/bybit.ts
+++ ts/src/bybit.ts
@@ -1952,13 +1952,14 @@ export default class bybit extends Exchange {
         //         "2.4343353100000003"
         //     ]
         //
+        const volumeIndex = (market['inverse']) ? 6 : 5;
         return [
             this.safeInteger (ohlcv, 0),
             this.safeNumber (ohlcv, 1),
             this.safeNumber (ohlcv, 2),
             this.safeNumber (ohlcv, 3),
             this.safeNumber (ohlcv, 4),
-            this.safeNumber (ohlcv, 5),
+            this.safeNumber (ohlcv, volumeIndex),
         ];
     }
 
