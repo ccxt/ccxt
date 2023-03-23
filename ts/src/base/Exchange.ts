10deb45d1ee39ce6874bdc68d5e617cacd0ec6bd
Carlo Revelli <carlo.revelli@berkeley.edu>
2023-03-22 21:37:25 -0700

    restore inflate

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -113,6 +113,7 @@ const {
 // TODO: remove these imports to make browser package smaller
 import { secp256k1 } from '../static_dependencies/noble-curves/secp256k1.js'
 import { keccak_256 as keccak } from '../static_dependencies/noble-hashes/sha3.js';
+import { inflate, inflate64, gunzip } from './ws/functions.js'
 
 // import exceptions from "./errors.js"
 
@@ -408,6 +409,10 @@ export default class Exchange {
     isArray = isArray
     base64ToString = base64ToString
     crc32 = crc32
+
+    inflate = inflate
+    inflate64 = inflate64
+    gunzip = gunzip
 
     describe () {
         return {
