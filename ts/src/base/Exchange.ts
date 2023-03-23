02ee7a853c66469c4d5b2d7d59afc0c2d0cecbf6
Carlo Revelli <carlo.revelli@berkeley.edu>
2023-03-22 23:41:19 -0700

    remove ts-ignore line

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -1101,8 +1101,7 @@ export default class Exchange {
         // still takes the input as a hex string
         // same as above but returns a string instead of an object
         const signature = this.signMessage (message, privateKey)
-        // @ts-ignore
-        return signature['r'] + this.remove0xPrefix (signature['s']) + this.binaryToBase16 (this.numberToBE (signature['v']))
+        return signature['r'] + this.remove0xPrefix (signature['s']) + this.binaryToBase16 (this.numberToBE (signature['v'], 1))
     }
 
     parseNumber (value: string | number, d: number = undefined): number {
