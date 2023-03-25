b47a16d9fd585c910e7e3ca9991b018218224926
Carlo Revelli <carlo.revelli@berkeley.edu>
2023-03-24 18:37:10 -0700

    safeMarket refix #17310

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -154,7 +154,6 @@ export default class Exchange {
     options: {
         [key: string]: any;
     }
-    fetchOptions: any;
     userAgents: any;
     headers: any;
 
@@ -2897,13 +2896,13 @@ export default class Exchange {
                 if (numMarkets === 1) {
                     return markets[0];
                 } else {
-                    if (marketType === undefined && market === undefined) {
+                    if ((marketType === undefined) && (market === undefined)) {
                         throw new ArgumentsRequired (this.id + ' safeMarket() requires a fourth argument for ' + marketId + ' to disambiguate between different markets with the same market id');
                     }
-                    const inferedMarketType = (market !== undefined) ? market['type'] : marketType;
+                    const inferredMarketType = (marketType === undefined) ? market['type'] : marketType;
                     for (let i = 0; i < markets.length; i++) {
                         const market = markets[i];
-                        if (market[inferedMarketType]) {
+                        if (market[inferredMarketType]) {
                             return market;
                         }
                     }
