8e7fafa7e9d1a86676e21e755fc681202643dae7
Travis CI <travis@travis-ci.org>
2023-03-27 23:21:22 +0000

    3.0.40
    [ci skip]

--- dist/cjs/ccxt.js
+++ dist/cjs/ccxt.js
@@ -181,7 +181,7 @@ var zb$1 = require('./src/pro/zb.js');
 
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
-const version = '3.0.39';
+const version = '3.0.40';
 Exchange["default"].ccxtVersion = version;
 const exchanges = {
     'ace': ace,
--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -36,11 +36,11 @@ use \ccxt\pro\ClientTrait;
 
 include 'Throttle.php';
 
-$version = '3.0.39';
+$version = '3.0.40';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '3.0.39';
+    const VERSION = '3.0.40';
 
     public $browser;
     public $marketsLoading = null;
