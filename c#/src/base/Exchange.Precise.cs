4c29992452fdeddb8f189402f26b60657036a5f6
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-03-28 12:40:56 +0100

    first Precise implementation

--- c#/src/base/Exchange.Precise.cs
+++ c#/src/base/Exchange.Precise.cs
@@ -1,73 +1,291 @@
 using System.Text;
 using System.Security.Cryptography;
 using System.Globalization;
+using System.Linq;
 
 namespace Main;
 
 
 public partial class Exchange
 {
+    // first Precise implementation check this later
 
     public class Precise
     {
         public object decimals = null;
-        public Precise(object arg)
+
+        public object integer = null;
+
+        public object baseNumber = null;
+
+        public Precise(string number, int decimals = -1)
         {
+            if (decimals == -1)
+            {
+                var modified = 0;
+                var numberLowerCase = ((string)number).ToLower();
+                if (numberLowerCase.IndexOf('e') > -1)
+                {
+                    var parts = numberLowerCase.Split('e');
+                    number = parts[0];
+                    modified = int.Parse(parts[1]);
+                }
+                var decimalIndex = number.IndexOf('.');
+                this.decimals = (decimalIndex > -1) ? number.Length - decimalIndex - 1 : 0;
+                var integerString = number.Replace(".", "");
+                this.integer = long.Parse(integerString);
+                this.decimals = (int)this.decimals - modified;
+            }
+            else
+            {
+                this.integer = number;
+                this.decimals = decimals;
+            }
 
         }
 
-        public void reduce()
+        public Precise mul(Precise other)
+        {
+            var integer = (long)this.integer * (long)other.integer;
+            var decimals = (int)this.decimals + (int)other.decimals;
+            return new Precise(integer.ToString(), decimals);
+        }
+
+        public Precise div(Precise other, int precision = 18)
+        {
+            var distance = precision - (int)this.decimals + (int)other.decimals;
+            object numerator = null;
+            if (distance == 0)
+            {
+                numerator = this.integer;
+            }
+            else if (distance < 0)
+            {
+                var exponent = Math.Pow((double)baseNumber, -distance);
+                numerator = (int)this.integer / exponent;
+            }
+            else
+            {
+                var exponent = Math.Pow((double)baseNumber, distance);
+                numerator = (int)this.integer * exponent;
+            }
+            var result = (int)numerator / (int)other.integer;
+            return new Precise(result.ToString(), precision);
+        }
+
+        public Precise add(Precise other)
+        {
+            if (this.decimals == other.decimals)
+            {
+                var integerResult = (long)this.integer + (long)other.integer;
+                return new Precise(integerResult.ToString(), (int)this.decimals);
+            }
+            else
+            {
+                Precise smaller = null;
+                Precise bigger = null;
+                if ((int)this.decimals < (int)other.decimals)
+                {
+                    smaller = this;
+                    bigger = other;
+                }
+                else
+                {
+                    smaller = other;
+                    bigger = this;
+                }
+                var exponent = (int)bigger.decimals - (int)smaller.decimals;
+                var normalized = (int)smaller.integer * Math.Pow((double)baseNumber, exponent);
+                var result = normalized + (int)bigger.integer;
+                return new Precise(result.ToString(), (int)bigger.decimals);
+            }
+        }
+
+        public Precise mod(Precise other)
+        {
+            var rationizerNumerator = Math.Max(-(int)this.decimals + (int)other.decimals, 0);
+            var numerator = (int)this.integer * Math.Pow((double)this.baseNumber, rationizerNumerator);
+            var rationizerDenominator = Math.Max(-(int)this.decimals + (int)other.decimals, 0);
+            var denominator = (int)other.integer * Math.Pow((double)this.baseNumber, rationizerDenominator);
+            var result = numerator % denominator;
+            return new Precise(result.ToString(), rationizerDenominator + (int)other.decimals);
+        }
+
+        public Precise sub(Precise other)
+        {
+            var negative = new Precise((-(int)other.integer).ToString(), (int)other.decimals);
+            return this.add(negative);
+        }
+
+        public Precise neg()
+        {
+            return new Precise((-(int)this.integer).ToString(), (int)this.decimals);
+        }
+
+        public Precise min(Precise other)
+        {
+            return this.lt(other) ? this : other;
+        }
+
+        public Precise max(Precise other)
+        {
+            return this.gt(other) ? this : other;
+        }
+
+        public bool gt(Precise other)
+        {
+            var sum = this.sub(other);
+            return (int)sum.integer > 0;
+        }
+
+        public bool ge(Precise other)
+        {
+            var sum = this.sub(other);
+            return (int)sum.integer >= 0;
+        }
+
+        public bool lt(Precise other)
+        {
+            return other.gt(this);
+        }
+
+        public bool le(Precise other)
+        {
+            return other.ge(this);
+        }
+
+        public Precise abs()
+        {
+            var result = (int)this.integer < 0 ? (int)this.integer * -1 : this.integer;
+            return new Precise(result.ToString(), (int)this.decimals);
+        }
+
+        public Precise reduce()
         {
 
+            var str = this.integer.ToString();
+            var start = str.Length - 1;
+            if (start == 0)
+            {
+                if (str == "0")
+                {
+                    this.decimals = 0;
+                }
+                return this;
+            }
+            var i = 0;
+            for (i = start; i > 0; i--)
+            {
+                if (str[i] != '0')
+                {
+                    break;
+                }
+            }
+            var difference = start - i;
+            if (difference == 0)
+            {
+                return this;
+            }
+            this.decimals = (int)this.decimals - difference;
+            this.integer = long.Parse(str.Substring(0, i + 1));
+            return this;
         }
-        static public string stringMul(object a, object b)
+
+        public bool equals(Precise other)
         {
-            if (a == null || b == null)
-                return null;
-            var first = float.Parse((string)a, CultureInfo.InvariantCulture);
-            var second = float.Parse((string)b, CultureInfo.InvariantCulture);
-            return (first * second).ToString();// tmp
+            this.reduce();
+            other.reduce();
+            return this.integer == other.integer && this.decimals == other.decimals;
         }
-        static public string stringSub(object a, object b)
+
+        public override string ToString()
         {
-            if (a == null || b == null)
-                return null;
-            var first = float.Parse((string)a, CultureInfo.InvariantCulture);
-            var second = float.Parse((string)b, CultureInfo.InvariantCulture);
-            return (first - second).ToString();// tmp
+            this.reduce();
+            var sign = "";
+            int abs = 0;
+            if ((int)this.integer < 0)
+            {
+                sign = "-";
+                abs = -(int)this.integer;
+            }
+            else
+            {
+                abs = (int)this.integer;
+            }
+            var integerArray = abs.ToString().PadLeft((int)this.decimals, '0').ToString().ToList().Select(x => x.ToString()).ToList();
+            var index = integerArray.Count - (int)this.decimals;
+            var item = "";
+            if (index == 0)
+            {
+                item = "0.";
+            }
+            else if ((int)this.decimals < 0)
+            {
+                item = string.Concat(Enumerable.Repeat("0", -(int)this.decimals));
+            }
+            else if ((int)this.decimals == 0)
+            {
+                item = "";
+            }
+            else
+            {
+                item = ".";
+            }
+            integerArray.Insert(index, item);
+            return sign + string.Join("", integerArray);
         }
-        static public string stringAdd(object a, object b)
+
+
+        static public string stringMul(object string1, object string2)
         {
-            if (a == null || b == null)
+            if (string1 == null || string2 == null)
                 return null;
-            var first = float.Parse((string)a, CultureInfo.InvariantCulture);
-            var second = float.Parse((string)b, CultureInfo.InvariantCulture);
-            return (first + second).ToString();// tmp
+            return (new Precise(string1.ToString()).mul(new Precise(string2.ToString()))).ToString();
         }
-        static public string stringDiv(object a, object b, object c = null)
+
+        static public string stringDiv(object string1, object string2, object c = null)
         {
-            if (a == null || b == null)
+            if (string1 == null || string2 == null)
                 return null;
-            var first = float.Parse((string)a, CultureInfo.InvariantCulture);
-            var second = float.Parse((string)b, CultureInfo.InvariantCulture);
-            return (first / second).ToString();// tmp
+
+            var string2Precise = new Precise(string2.ToString());
+            if ((int)string2Precise.integer == 0)
+            {
+                return null;
+            }
+            return (new Precise(string1.ToString())).div(string2Precise).ToString();
         }
+
+        static public string stringSub(object string1, object string2)
+        {
+            if (string1 == null || string2 == null)
+                return null;
+            return (new Precise(string1.ToString()).sub(new Precise(string2.ToString()))).ToString();
+        }
+        static public string stringAdd(object string1, object string2)
+        {
+            if (string1 == null && string2 == null)
+                return null;
+            if (string1 == null)
+                return string2.ToString();
+            if (string2 == null)
+                return string1.ToString();
+
+            return (new Precise(string1.ToString()).add(new Precise(string2.ToString()))).ToString();
+        }
+
         static public bool stringGt(object a, object b)
         {
             if (a == null || b == null)
                 return false;
-            var first = float.Parse((string)a, CultureInfo.InvariantCulture);
-            var second = float.Parse((string)b, CultureInfo.InvariantCulture);
-            return first > second;// tmp
+            return (new Precise(a.ToString()).gt(new Precise(b.ToString())));
         }
 
         static public bool stringEq(object a, object b)
         {
             if (a == null || b == null)
                 return false;
-            var first = float.Parse((string)a, CultureInfo.InvariantCulture);
-            var second = float.Parse((string)b, CultureInfo.InvariantCulture);
-            return (first == second);// tmp;
+            return (new Precise(a.ToString()).equals(new Precise(b.ToString())));
         }
 
         static public string stringMax(object a, object b)
@@ -88,12 +306,16 @@ public partial class Exchange
 
         static public string stringEquals(object a, object b)
         {
-            return "";
+            if (a == null || b == null)
+                return null;
+            return (new Precise(a.ToString()).equals(new Precise(b.ToString()))).ToString();
         }
 
-        static public string stringMin(object a, object b)
+        static public string stringMin(object string1, object string2)
         {
-            return "";
+            if (string1 == null || string2 == null)
+                return null;
+            return (new Precise(string1.ToString()).min(new Precise(string2.ToString()))).ToString();
         }
 
         static public bool stringLt(object a, object b)
@@ -105,26 +327,30 @@ public partial class Exchange
 
         static public string stringAbs(object a)
         {
-            return ((string)a);
+            if (a == null)
+                return null;
+            return (new Precise((string)a)).abs().ToString();
         }
 
         static public string stringNeg(object a)
         {
-            return "-" + ((string)a);
+            if (a == null)
+                return null;
+            return (new Precise((string)a)).neg().ToString();
         }
 
         static public bool stringLe(object a, object b)
         {
-            var first = float.Parse((string)a, CultureInfo.InvariantCulture);
-            var second = float.Parse((string)b, CultureInfo.InvariantCulture);
-            return first <= second;
+            if (a == null || b == null)
+                return false;
+            return (new Precise(a.ToString()).le(new Precise(b.ToString())));
         }
 
         static public bool stringGe(object a, object b)
         {
-            var first = float.Parse((string)a, CultureInfo.InvariantCulture);
-            var second = float.Parse((string)b, CultureInfo.InvariantCulture);
-            return first >= second;
+            if (a == null || b == null)
+                return false;
+            return (new Precise(a.ToString()).ge(new Precise(b.ToString())));
         }
 
     }
