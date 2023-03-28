eb2d3395b885925b5517585005b1f299b890115b
ndubel <38309641+ndubel@users.noreply.github.com>
2023-03-28 16:31:50 +0300

    bitget cancelOrders, fetchOpenOrders edits

--- ts/src/bitget.ts
+++ ts/src/bitget.ts
@@ -174,6 +174,7 @@ export default class bitget extends Exchange {
                             'trade/batch-orders': 4,
                             'trade/cancel-order': 2,
                             'trade/cancel-batch-orders': 4,
+                            'trade/cancel-batch-orders-v2': 4,
                             'trade/orderInfo': 1,
                             'trade/open-orders': 1,
                             'trade/history': 1,
@@ -2202,6 +2203,7 @@ export default class bitget extends Exchange {
             'new': 'open',
             'init': 'open',
             'not_trigger': 'open',
+            'partial_fill': 'open',
             'triggered': 'closed',
             'full_fill': 'closed',
             'filled': 'closed',
@@ -2625,11 +2627,9 @@ export default class bitget extends Exchange {
         const request = {};
         let method = undefined;
         if (type === 'spot') {
-            method = 'apiPostOrderOrdersBatchcancel';
-            request['method'] = 'batchcancel';
-            const jsonIds = this.json (ids);
-            const parts = jsonIds.split ('"');
-            request['order_ids'] = parts.join ('');
+            method = 'privateSpotPostTradeCancelBatchOrdersV2';
+            request['symbol'] = market['id'];
+            request['orderIds'] = ids;
         } else if (type === 'swap') {
             method = 'privateMixPostOrderCancelBatchOrders';
             request['symbol'] = market['id'];
@@ -2641,18 +2641,17 @@ export default class bitget extends Exchange {
         //     spot
         //
         //     {
-        //         "status": "ok",
+        //         "code": "00000",
+        //         "msg": "success",
+        //         "requestTime": "1680008815965",
         //         "data": {
-        //             "success": [
-        //                 "673451224205135872",
-        //             ],
-        //             "failed": [
+        //             "resultList": [
         //                 {
-        //                 "err-msg": "invalid record",
-        //                 "order-id": "673451224205135873",
-        //                 "err-code": "base record invalid"
-        //                 }
-        //             ]
+        //                     "orderId": "1024598257429823488",
+        //                     "clientOrderId": "876493ce-c287-4bfc-9f4a-8b1905881313"
+        //                 },
+        //             ],
+        //             "failed": []
         //         }
         //     }
         //
@@ -2862,7 +2861,7 @@ export default class bitget extends Exchange {
             }
             query = this.omit (query, 'stop');
         }
-        const response = await this[method] (this.extend (request, query));
+        let response = await this[method] (this.extend (request, query));
         //
         //  spot
         //     {
@@ -2966,6 +2965,9 @@ export default class bitget extends Exchange {
         //         }
         //     }
         //
+        if (typeof response === 'string') {
+            response = JSON.parse (response);
+        }
         let data = this.safeValue (response, 'data', []);
         if (!Array.isArray (data)) {
             data = this.safeValue (data, 'orderList', []);
