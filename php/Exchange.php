2718e197a6abf11cb1ac3a28e4248c60a591abbe
Travis CI <travis@travis-ci.org>
2023-04-04 21:47:35 +0000

    3.0.53
    [ci skip]

--- php/Exchange.php
+++ php/Exchange.php
@@ -36,7 +36,7 @@ use Elliptic\EdDSA;
 use BN\BN;
 use Exception;
 
-$version = '3.0.52';
+$version = '3.0.53';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -55,7 +55,7 @@ const PAD_WITH_ZERO = 1;
 
 class Exchange {
 
-    const VERSION = '3.0.52';
+    const VERSION = '3.0.53';
 
     private static $base58_alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
     private static $base58_encoder = null;
--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -34,11 +34,11 @@ use Exception;
 
 include 'Throttle.php';
 
-$version = '3.0.52';
+$version = '3.0.53';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '3.0.52';
+    const VERSION = '3.0.53';
 
     public $browser;
     public $marketsLoading = null;
