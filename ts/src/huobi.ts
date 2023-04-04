b997fa6f6b07ce99b8cb2682ba6efbe0971f7f3a
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-04-04 23:33:26 +0400

    last commit

--- ts/src/huobi.ts
+++ ts/src/huobi.ts
@@ -4329,7 +4329,8 @@ export default class huobi extends Exchange {
             throw new NotSupported (this.id + ' createOrder() supports tp_trigger_price + tp_order_price for take profit orders and/or sl_trigger_price + sl_order price for stop loss orders, stop orders are supported only with open long orders and open short orders');
         }
         params = this.omit (params, [ 'sl_order_price', 'sl_trigger_price', 'tp_order_price', 'tp_trigger_price' ]);
-        const postOnly = this.safeValue (params, 'postOnly', false);
+        let postOnly = undefined;
+        [ postOnly, params ] = this.handlePostOnly (type === 'market', type === 'post_only', params);
         if (postOnly) {
             type = 'post_only';
         }
