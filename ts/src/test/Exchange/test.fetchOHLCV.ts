0b2e5ce0897aa69bb6253517335946bbc1f0a756
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-04-12 01:01:52 +0400

    tf key fix

--- ts/src/test/Exchange/test.fetchOHLCV.ts
+++ ts/src/test/Exchange/test.fetchOHLCV.ts
@@ -4,14 +4,17 @@ import testOHLCV from './base/test.ohlcv.js';
 
 async function testFetchOHLCV (exchange, symbol) {
     const method = 'fetchOHLCV';
-    const timeframes = Object.keys (exchange.timeframes);
-    assert (timeframes.length > 0, exchange.id + ' ' + method + ' - no timeframes found');
+    const timeframeKeys = Object.keys (exchange.timeframes);
+    assert (timeframeKeys.length > 0, exchange.id + ' ' + method + ' - no timeframes found');
     // prefer 1m timeframe if available, otherwise return the first one
-    const timeframe = exchange.safeString (exchange.timeframes, '1m', timeframes[0]);
+    let chosenTimeframeKey = '1m';
+    if (!exchange.inArray (chosenTimeframeKey, timeframeKeys)) {
+        chosenTimeframeKey = timeframeKeys[0];
+    }
     const limit = 10;
-    const duration = exchange.parseTimeframe (timeframe);
+    const duration = exchange.parseTimeframe (chosenTimeframeKey);
     const since = exchange.milliseconds () - duration * limit * 1000 - 1000;
-    const ohlcvs = await exchange.fetchOHLCV (symbol, timeframe, since, limit);
+    const ohlcvs = await exchange.fetchOHLCV (symbol, chosenTimeframeKey, since, limit);
     assert (Array.isArray (ohlcvs), exchange.id + ' ' + method + ' must return an array, returned ' + exchange.json (ohlcvs));
     const now = exchange.milliseconds ();
     for (let i = 0; i < ohlcvs.length; i++) {
