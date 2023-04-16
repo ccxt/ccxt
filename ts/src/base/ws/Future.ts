4464bc88773666dedb892498ec821bc64fae91eb
Pablo Criado-Perez <pablo@criadoperez.com>
2023-04-16 16:43:38 +0200

    fix: ws client send

--- ts/src/base/ws/Future.ts
+++ ts/src/base/ws/Future.ts
@@ -1,6 +1,11 @@
 // @ts-nocheck
 
-export default function Future () {
+interface Future extends Promise<unknown> {
+    resolve(value: unknown): void;
+    reject(reason?: any): void;
+}
+
+export default function Future (): Future {
 
     let resolve = undefined
         , reject = undefined
