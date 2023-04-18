268bad3f95781f97efb1293333f815c2de629d64
Philipp Looser <phlooser@gmail.com>
2023-04-18 10:45:54 +0200

    force re-transpile mexc3

--- ts/src/mexc3.ts
+++ ts/src/mexc3.ts
@@ -4,6 +4,7 @@
 import mexc from './mexc.js';
 
 // ---------------------------------------------------------------------------
+// this is a dummy change to force re-transpile of mexc3's py/php-files. This line can be removed again on next change
 
 export default class mexc3 extends mexc {
     describe () {
