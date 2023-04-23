cef53fdf319af271bdd070f54604a581555e3c07
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-04-23 16:09:38 +0100

    and examples project and dotnet installation to travis

---
+++ examples/c#/Program.cs
@@ -0,0 +1,14 @@
+﻿using ccxt;
+
+namespace examples;
+
+class Examples
+{
+    public static void Main(string[] args)
+    {
+        var exchange = new Binance();
+        var markets = exchange.FetchMarkets();
+        markets.Wait();
+        Console.WriteLine(JsonConvert.SerializeObject(result.Result, Formatting.Indented));
+    }
+}
