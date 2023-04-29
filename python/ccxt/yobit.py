8f460ec9cae58b7e5f3e36541b0bc670293ffe76
Travis CI <travis@travis-ci.org>
2023-04-29 15:53:19 +0000

    Merge pull request #17707 from sc0Vu/update-yobit-apis-20230428
    yobit: add links to jsdoc
    
    [ci skip]

--- python/ccxt/yobit.py
+++ python/ccxt/yobit.py
@@ -310,6 +310,7 @@ class yobit(Exchange):
 
     def fetch_balance(self, params={}):
         """
+        see https://yobit.net/en/api
         query for balance and get the amount of funds available for trading or funds locked in orders
         :param dict params: extra parameters specific to the yobit api endpoint
         :returns dict: a `balance structure <https://docs.ccxt.com/en/latest/manual.html?#balance-structure>`
@@ -345,6 +346,7 @@ class yobit(Exchange):
 
     def fetch_markets(self, params={}):
         """
+        see https://yobit.net/en/api
         retrieves data on all markets for yobit
         :param dict params: extra parameters specific to the exchange api endpoint
         :returns [dict]: an array of objects representing market data
@@ -437,6 +439,7 @@ class yobit(Exchange):
 
     def fetch_order_book(self, symbol: str, limit: Optional[int] = None, params={}):
         """
+        see https://yobit.net/en/api
         fetches information on open orders with bid(buy) and ask(sell) prices, volumes and other data
         :param str symbol: unified symbol of the market to fetch the order book for
         :param int|None limit: the maximum amount of order book entries to return
@@ -459,6 +462,7 @@ class yobit(Exchange):
 
     def fetch_order_books(self, symbols: Optional[List[str]] = None, limit: Optional[int] = None, params={}):
         """
+        see https://yobit.net/en/api
         fetches information on open orders with bid(buy) and ask(sell) prices, volumes and other data for multiple markets
         :param [str]|None symbols: list of unified market symbols, all symbols fetched if None, default is None
         :param int|None limit: max number of entries per orderbook to return, default is None
@@ -532,6 +536,7 @@ class yobit(Exchange):
 
     def fetch_tickers(self, symbols: Optional[List[str]] = None, params={}):
         """
+        see https://yobit.net/en/api
         fetches price tickers for multiple markets, statistical calculations with the information calculated over the past 24 hours each market
         :param [str]|None symbols: unified symbols of the markets to fetch the ticker for, all market tickers are returned if not assigned
         :param dict params: extra parameters specific to the yobit api endpoint
@@ -566,6 +571,7 @@ class yobit(Exchange):
 
     def fetch_ticker(self, symbol: str, params={}):
         """
+        see https://yobit.net/en/api
         fetches a price ticker, a statistical calculation with the information calculated over the past 24 hours for a specific market
         :param str symbol: unified symbol of the market to fetch the ticker for
         :param dict params: extra parameters specific to the yobit api endpoint
@@ -650,6 +656,7 @@ class yobit(Exchange):
 
     def fetch_trades(self, symbol: str, since: Optional[int] = None, limit: Optional[int] = None, params={}):
         """
+        see https://yobit.net/en/api
         get the list of most recent trades for a particular symbol
         :param str symbol: unified symbol of the market to fetch trades for
         :param int|None since: timestamp in ms of the earliest trade to fetch
@@ -687,6 +694,7 @@ class yobit(Exchange):
 
     def fetch_trading_fees(self, params={}):
         """
+        see https://yobit.net/en/api
         fetch the trading fees for multiple markets
         :param dict params: extra parameters specific to the yobit api endpoint
         :returns dict: a dictionary of `fee structures <https://docs.ccxt.com/#/?id=fee-structure>` indexed by market symbols
@@ -735,6 +743,7 @@ class yobit(Exchange):
 
     def create_order(self, symbol: str, type, side: OrderSide, amount, price=None, params={}):
         """
+        see https://yobit.net/en/api
         create a trade order
         :param str symbol: unified symbol of the market to create an order in
         :param str type: must be 'limit'
@@ -781,6 +790,7 @@ class yobit(Exchange):
 
     def cancel_order(self, id: str, symbol: Optional[str] = None, params={}):
         """
+        see https://yobit.net/en/api
         cancels an open order
         :param str id: order id
         :param str|None symbol: not used by yobit cancelOrder()
@@ -927,6 +937,7 @@ class yobit(Exchange):
 
     def fetch_order(self, id: str, symbol: Optional[str] = None, params={}):
         """
+        see https://yobit.net/en/api
         fetches information on an order made by the user
         :param str|None symbol: not used by yobit fetchOrder
         :param dict params: extra parameters specific to the yobit api endpoint
@@ -959,6 +970,7 @@ class yobit(Exchange):
 
     def fetch_open_orders(self, symbol: Optional[str] = None, since: Optional[int] = None, limit: Optional[int] = None, params={}):
         """
+        see https://yobit.net/en/api
         fetch all unfilled currently open orders
         :param str symbol: unified market symbol
         :param int|None since: the earliest time in ms to fetch open orders for
@@ -1003,6 +1015,7 @@ class yobit(Exchange):
 
     def fetch_my_trades(self, symbol: Optional[str] = None, since: Optional[int] = None, limit: Optional[int] = None, params={}):
         """
+        see https://yobit.net/en/api
         fetch all trades made by the user
         :param str symbol: unified market symbol
         :param int|None since: the earliest time in ms to fetch trades for
@@ -1059,6 +1072,7 @@ class yobit(Exchange):
 
     def create_deposit_address(self, code: str, params={}):
         """
+        see https://yobit.net/en/api
         create a currency deposit address
         :param str code: unified currency code of the currency for the deposit address
         :param dict params: extra parameters specific to the yobit api endpoint
@@ -1079,6 +1093,7 @@ class yobit(Exchange):
 
     def fetch_deposit_address(self, code: str, params={}):
         """
+        see https://yobit.net/en/api
         fetch the deposit address for a currency associated with self account
         :param str code: unified currency code
         :param dict params: extra parameters specific to the yobit api endpoint
@@ -1111,6 +1126,7 @@ class yobit(Exchange):
 
     def withdraw(self, code: str, amount, address, tag=None, params={}):
         """
+        see https://yobit.net/en/api
         make a withdrawal
         :param str code: unified currency code
         :param float amount: the amount to withdraw
