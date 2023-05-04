1febf7ed033438fd60482e75687ac09bac0a2c69
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-05-04 18:56:43 +0400

    krakenfutures - fix contract

--- ts/src/krakenfutures.ts
+++ ts/src/krakenfutures.ts
@@ -306,8 +306,9 @@ export default class krakenfutures extends Exchange {
             let settleId = undefined;
             const amountPrecision = this.parseNumber (this.parsePrecision (this.safeString (market, 'contractValueTradePrecision', '0')));
             const pricePrecision = this.safeNumber (market, 'tickSize');
-            const contract = (swap || future);
-            if (contract) {
+            const contract = (swap || future || index);
+            const swapOrFutures = (swap || future);
+            if (swapOrFutures) {
                 const exchangeType = this.safeString (market, 'type');
                 if (exchangeType === 'futures_inverse') {
                     settle = base;
