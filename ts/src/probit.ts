d2a7843ad260ae0a5cf4370b1d6134d995640d77
sc0Vu <alk03073135@gmail.com>
2023-05-05 19:42:54 +0800

    probit: add @see

--- ts/src/probit.ts
+++ ts/src/probit.ts
@@ -241,6 +241,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#fetchMarkets
+         * @see https://docs-en.probit.com/reference/market
          * @description retrieves data on all markets for probit
          * @param {object} params extra parameters specific to the exchange api endpoint
          * @returns {[object]} an array of objects representing market data
@@ -343,6 +344,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#fetchCurrencies
+         * @see https://docs-en.probit.com/reference/currency
          * @description fetches all available currencies on an exchange
          * @param {object} params extra parameters specific to the probit api endpoint
          * @returns {object} an associative dictionary of currencies
@@ -488,6 +490,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#fetchBalance
+         * @see https://docs-en.probit.com/reference/balance
          * @description query for balance and get the amount of funds available for trading or funds locked in orders
          * @param {object} params extra parameters specific to the probit api endpoint
          * @returns {object} a [balance structure]{@link https://docs.ccxt.com/en/latest/manual.html?#balance-structure}
@@ -512,6 +515,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#fetchOrderBook
+         * @see https://docs-en.probit.com/reference/order_book
          * @description fetches information on open orders with bid (buy) and ask (sell) prices, volumes and other data
          * @param {string} symbol unified symbol of the market to fetch the order book for
          * @param {int|undefined} limit the maximum amount of order book entries to return
@@ -542,6 +546,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#fetchTickers
+         * @see https://docs-en.probit.com/reference/ticker
          * @description fetches price tickers for multiple markets, statistical calculations with the information calculated over the past 24 hours each market
          * @param {[string]|undefined} symbols unified symbols of the markets to fetch the ticker for, all market tickers are returned if not assigned
          * @param {object} params extra parameters specific to the probit api endpoint
@@ -578,6 +583,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#fetchTicker
+         * @see https://docs-en.probit.com/reference/ticker
          * @description fetches a price ticker, a statistical calculation with the information calculated over the past 24 hours for a specific market
          * @param {string} symbol unified symbol of the market to fetch the ticker for
          * @param {object} params extra parameters specific to the probit api endpoint
@@ -661,6 +667,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#fetchMyTrades
+         * @see https://docs-en.probit.com/reference/trade
          * @description fetch all trades made by the user
          * @param {string|undefined} symbol unified market symbol
          * @param {int|undefined} since the earliest time in ms to fetch trades for
@@ -713,6 +720,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#fetchTrades
+         * @see https://docs-en.probit.com/reference/trade-1
          * @description get the list of most recent trades for a particular symbol
          * @param {string} symbol unified symbol of the market to fetch trades for
          * @param {int|undefined} since timestamp in ms of the earliest trade to fetch
@@ -834,6 +842,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#fetchTime
+         * @see https://docs-en.probit.com/reference/time
          * @description fetches the current integer timestamp in milliseconds from the exchange server
          * @param {object} params extra parameters specific to the probit api endpoint
          * @returns {int} the current integer timestamp in milliseconds from the exchange server
@@ -885,6 +894,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#fetchOHLCV
+         * @see https://docs-en.probit.com/reference/candle
          * @description fetches historical candlestick data containing the open, high, low, and close price, and the volume of a market
          * @param {string} symbol unified symbol of the market to fetch OHLCV data for
          * @param {string} timeframe the length of time each candle represents
@@ -975,6 +985,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#fetchOpenOrders
+         * @see https://docs-en.probit.com/reference/open_order-1
          * @description fetch all unfilled currently open orders
          * @param {string|undefined} symbol unified market symbol
          * @param {int|undefined} since the earliest time in ms to fetch open orders for
@@ -999,6 +1010,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#fetchClosedOrders
+         * @see https://docs-en.probit.com/reference/order
          * @description fetches information on multiple closed orders made by the user
          * @param {string|undefined} symbol unified market symbol of the market orders were made in
          * @param {int|undefined} since the earliest time in ms to fetch orders for
@@ -1032,6 +1044,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#fetchOrder
+         * @see https://docs-en.probit.com/reference/order-3
          * @description fetches information on an order made by the user
          * @param {string} symbol unified symbol of the market the order was made in
          * @param {object} params extra parameters specific to the probit api endpoint
@@ -1141,6 +1154,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#createOrder
+         * @see https://docs-en.probit.com/reference/order-1
          * @description create a trade order
          * @param {string} symbol unified symbol of the market to create an order in
          * @param {string} type 'market' or 'limit'
@@ -1230,6 +1244,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#cancelOrder
+         * @see https://docs-en.probit.com/reference/order-2
          * @description cancels an open order
          * @param {string} id order id
          * @param {string} symbol unified symbol of the market the order was made in
@@ -1271,6 +1286,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#fetchDepositAddress
+         * @see https://docs-en.probit.com/reference/deposit_address
          * @description fetch the deposit address for a currency associated with this account
          * @param {string} code unified currency code
          * @param {object} params extra parameters specific to the probit api endpoint
@@ -1325,6 +1341,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#fetchDepositAddresses
+         * @see https://docs-en.probit.com/reference/deposit_address
          * @description fetch deposit addresses for multiple currencies and chain types
          * @param {[string]|undefined} codes list of unified currency codes, default is undefined
          * @param {object} params extra parameters specific to the probit api endpoint
@@ -1349,6 +1366,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#withdraw
+         * @see https://docs-en.probit.com/reference/withdrawal
          * @description make a withdrawal
          * @param {string} code unified currency code
          * @param {float} amount the amount to withdraw
@@ -1451,6 +1469,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name poloniex#fetchDepositWithdrawFees
+         * @see https://docs-en.probit.com/reference/currency
          * @description fetch deposit and withdraw fees
          * @see https://docs.poloniex.com/#public-endpoints-reference-data-currency-information
          * @param {[string]|undefined} codes list of unified currency codes
@@ -1638,6 +1657,7 @@ export default class probit extends Exchange {
         /**
          * @method
          * @name probit#signIn
+         * @see https://docs-en.probit.com/reference/token
          * @description sign in, must be called prior to using other authenticated methods
          * @param {object} params extra parameters specific to the probit api endpoint
          * @returns response from exchange
