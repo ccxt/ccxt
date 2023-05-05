fbddef9ae48cd5b9484d7d04a502b3709bf1fe67
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-05-05 14:51:19 +0100

    fix bit2c

--- ts/src/bit2c.ts
+++ ts/src/bit2c.ts
@@ -342,7 +342,12 @@ export default class bit2c extends Exchange {
         if (limit !== undefined) {
             request['limit'] = limit; // max 100000
         }
-        const response = await this[method] (this.extend (request, params));
+        let response = undefined;
+        if (method === 'public_get_exchanges_pair_trades') {
+            response = await this.publicGetExchangesPairTrades (this.extend (request, params));
+        } else {
+            response = await this.publicGetExchangesPairLasttrades (this.extend (request, params));
+        }
         //
         //     [
         //         {"date":1651785980,"price":127975.68,"amount":0.3750321,"isBid":true,"tid":1261018},
