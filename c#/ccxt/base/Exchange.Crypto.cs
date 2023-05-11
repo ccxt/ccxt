ecea0e8dc6c3c8022c59d7855aca352e39d7fbf0
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-05-11 09:29:06 +0100

    make rsa standard2.0 compatible

--- c#/ccxt/base/Exchange.Crypto.cs
+++ c#/ccxt/base/Exchange.Crypto.cs
@@ -179,20 +179,50 @@ public partial class Exchange
 
     public static string Rsa(object data, object publicKey, Delegate hash = null)
     {
-        var algo = hash.DynamicInvoke() as string;
-        var dataToSign = Encoding.UTF8.GetBytes((string)data);
-        using var rsa = RSA.Create();
-#if NETSTANDARD2_0
-        throw new NotSupportedException("RSA signing is notccxt supported on .NET Standard 2.0 yet");
-#endif
-#if !NETSTANDARD2_1 && !NETCOREAPP3_1 && !NETSTANDARD2_0
-        rsa.ImportFromPem((string)publicKey);
-#elif !NETSTANDARD2_0
-        rsa.ImportRSAPrivateKey(StringToByteArray((string)publicKey), out _);
-#endif
-        var rsaFormatter = new RSAPKCS1SignatureFormatter(rsa);
-        var sign = rsa.SignData(dataToSign, stringToHashAlgorithmName(algo), RSASignaturePadding.Pkcs1);
-        return binaryToBase64(sign);
+        var pk = ((string)publicKey);
+        var pkParts = pk.Split(new[] { ((string)"\n") }, StringSplitOptions.None).ToList();
+        pkParts.RemoveAt(0);
+        pkParts.RemoveAt(pkParts.Count - 1);
+        var newPk = string.Join("", pkParts);
+        byte[] Data = Encoding.UTF8.GetBytes((string)data);
+        byte[] privatekey;
+        privatekey = Convert.FromBase64String(newPk);
+        // https://gist.github.com/GaiaAnn/29071961482462ff5334a121bd103166
+        RSACryptoServiceProvider rsa = DecodeRSAPrivateKey(privatekey);
+        object sh;
+        var algorithm = "md5";
+        if (hash != null)
+        {
+            algorithm = hash.DynamicInvoke() as String;
+        }
+
+        if (algorithm == "sha1")
+        {
+            sh = new SHA1CryptoServiceProvider();
+        }
+        else if (algorithm == "sha256")
+        {
+            sh = new SHA256CryptoServiceProvider();
+        }
+        else if (algorithm == "sha384")
+        {
+            sh = new SHA384CryptoServiceProvider();
+        }
+        else if (algorithm == "sha512")
+        {
+            sh = new SHA512CryptoServiceProvider();
+        }
+        else if (algorithm == "md5")
+        {
+            sh = new MD5CryptoServiceProvider();
+        }
+        else
+        {
+            throw new ArgumentException("Invalid hash algorithm name");
+        }
+        byte[] signData = rsa.SignData(Data, sh);
+
+        return Convert.ToBase64String(signData);
     }
 
     private static HashAlgorithmName stringToHashAlgorithmName(string hashAlgorithmName)
@@ -317,5 +347,115 @@ public partial class Exchange
         var result = ~arrayOfBytes.Aggregate(0xFFFFFFFF, (checksumRegister, currentByte) =>
                       (m_checksumTable[(checksumRegister & 0xFF) ^ Convert.ToByte(currentByte)] ^ (checksumRegister >> 8)));
         return (int)result;
+    }
+
+    private static RSACryptoServiceProvider DecodeRSAPrivateKey(byte[] privkey)
+    {
+        byte[] MODULUS, E, D, P, Q, DP, DQ, IQ;
+
+        // ---------  Set up stream to decode the asn.1 encoded RSA private key  ------
+        MemoryStream mem = new MemoryStream(privkey);
+        BinaryReader binr = new BinaryReader(mem);    //wrap Memory Stream with BinaryReader for easy reading
+        byte bt = 0;
+        ushort twobytes = 0;
+        int elems = 0;
+        try
+        {
+            twobytes = binr.ReadUInt16();
+            if (twobytes == 0x8130) //data read as little endian order (actual data order for Sequence is 30 81)
+                binr.ReadByte();    //advance 1 byte
+            else if (twobytes == 0x8230)
+                binr.ReadInt16();   //advance 2 bytes
+            else
+                return null;
+
+            twobytes = binr.ReadUInt16();
+            if (twobytes != 0x0102) //version number
+                return null;
+            bt = binr.ReadByte();
+            if (bt != 0x00)
+                return null;
+
+
+            //------  all private key components are Integer sequences ----
+            elems = GetIntegerSize(binr);
+            MODULUS = binr.ReadBytes(elems);
+
+            elems = GetIntegerSize(binr);
+            E = binr.ReadBytes(elems);
+
+            elems = GetIntegerSize(binr);
+            D = binr.ReadBytes(elems);
+
+            elems = GetIntegerSize(binr);
+            P = binr.ReadBytes(elems);
+
+            elems = GetIntegerSize(binr);
+            Q = binr.ReadBytes(elems);
+
+            elems = GetIntegerSize(binr);
+            DP = binr.ReadBytes(elems);
+
+            elems = GetIntegerSize(binr);
+            DQ = binr.ReadBytes(elems);
+
+            elems = GetIntegerSize(binr);
+            IQ = binr.ReadBytes(elems);
+
+            // ------- create RSACryptoServiceProvider instance and initialize with public key -----
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+            RSAParameters RSAparams = new RSAParameters();
+            RSAparams.Modulus = MODULUS;
+            RSAparams.Exponent = E;
+            RSAparams.D = D;
+            RSAparams.P = P;
+            RSAparams.Q = Q;
+            RSAparams.DP = DP;
+            RSAparams.DQ = DQ;
+            RSAparams.InverseQ = IQ;
+            RSA.ImportParameters(RSAparams);
+            return RSA;
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+        finally { binr.Close(); }
+    }
+
+    private static int GetIntegerSize(BinaryReader binr)
+    {
+        byte bt = 0;
+        byte lowbyte = 0x00;
+        byte highbyte = 0x00;
+        int count = 0;
+        bt = binr.ReadByte();
+        if (bt != 0x02)     //expect integer
+            return 0;
+        bt = binr.ReadByte();
+
+        if (bt == 0x81)
+            count = binr.ReadByte();    // data size in next byte
+        else
+            if (bt == 0x82)
+        {
+            highbyte = binr.ReadByte(); // data size in next 2 bytes
+            lowbyte = binr.ReadByte();
+            byte[] modint = { lowbyte, highbyte, 0x00, 0x00 };
+            count = BitConverter.ToInt32(modint, 0);
+        }
+        else
+        {
+            count = bt;     // we already have the data size
+        }
+
+
+
+        while (binr.ReadByte() == 0x00)
+        {   //remove high order zeros in data
+            count -= 1;
+        }
+        binr.BaseStream.Seek(-1, SeekOrigin.Current);       //last ReadByte wasn't a removed zero, so back up a byte
+        return count;
     }
 }
