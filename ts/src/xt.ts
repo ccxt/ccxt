0d7300c9de0a974fcf9b42c5e66e6671ec231071
ndubel <38309641+ndubel@users.noreply.github.com>
2023-05-16 14:39:34 +0300

    xt parseTransaction fixes

--- ts/src/xt.ts
+++ ts/src/xt.ts
@@ -3708,12 +3708,14 @@ export default class xt extends Exchange {
         //         "id": 950898
         //     }
         //
+        const type = ('fromAddr' in transaction) ? 'deposit' : 'withdraw';
         const timestamp = this.safeInteger (transaction, 'createdTime');
         const address = this.safeString (transaction, 'address');
         const memo = this.safeString (transaction, 'memo');
         const currencyCode = this.safeCurrencyCode (this.safeString (transaction, 'currency'), currency);
         const fee = this.safeNumber (transaction, 'fee');
         const feeCurrency = (fee !== undefined) ? currencyCode : undefined;
+        const networkId = this.safeString (transaction, 'chain');
         return {
             'info': transaction,
             'id': this.safeString (transaction, 'id'),
@@ -3727,10 +3729,10 @@ export default class xt extends Exchange {
             'tagFrom': undefined,
             'tagTo': undefined,
             'tag': memo,
-            'type': undefined,
+            'type': type,
             'amount': this.safeNumber (transaction, 'amount'),
             'currency': currencyCode,
-            'network': this.safeString (transaction, 'chain'),
+            'network': this.networkIdToCode (networkId, currencyCode),
             'status': this.parseTransactionStatus (this.safeString (transaction, 'status')),
             'comment': memo,
             'fee': {
