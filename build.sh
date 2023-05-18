8a36b1469d37ace6aa1f627e9884dd09d41fcf5b
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-05-18 11:23:13 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,50 +1,71 @@
 #!/usr/bin/env bash
 
-build_all_and_test_base () {
+function run_tests {
+  local rest_args=
+  local ws_args=
+  if [ $# -eq 2 ]; then
+    rest_args="$1"
+    ws_args="$2"
+    if [ -z "$rest_args" ]; then
+      : &
+      local rest_pid=$!
+    fi
+    if [ -z "$ws_args" ]; then
+      : &
+      local ws_pid=$!
+    fi
+  fi
+  if [ -z "$rest_pid" ]; then
+    # shellcheck disable=SC2086
+    node test-commonjs.cjs && node run-tests --js --python-async --php-async $rest_args &
+    local rest_pid=$!
+  fi
+  if [ -z "$ws_pid" ]; then
+    # shellcheck disable=SC2086
+    node run-tests-ws --js --python-async --php-async $ws_args &
+    local ws_pid=$!
+  fi
+  wait $rest_pid && wait $ws_pid
+}
+
+build_and_test_all () {
   npm run force-build
   npm run test-base
   npm run test-base-ws
+  run_tests
   exit
 }
 
 ### CHECK IF THIS IS A PR ###
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   echo "not a PR, will build everything"
-  build_all_and_test_base
+  build_and_test_all
 fi
 
 ##### DETECT CHANGES #####
-rest_pattern='ts\/src\/([A-Za-z0-9_-]+).ts' # \w not working for some reason
-ws_pattern='ts\/src\/pro\/([A-Za-z0-9_-]+)\.ts'
 diff=$(git diff origin/master --name-only)
 
+is_critical_change=$(grep -q -E 'Client(Trait)?\.php$|Exchange\.php$|/test|/base|^build|static_dependencies|^run-tests|package(-lock)?\.json$' <<< "$diff")
+
+if [ "$is_critical_change" ]; then
+  echo "detected critical change, will build/test everything"
+  build_and_test_all
+fi
+
 readarray -t y <<<"$diff"
+rest_pattern='ts\/src\/([A-Za-z0-9_-]+).ts' # \w not working for some reason
+ws_pattern='ts\/src\/pro\/([A-Za-z0-9_-]+)\.ts'
 
 REST_EXCHANGES=()
 WS_EXCHANGES=()
 transpile_all=false
-for file in "${y[@]}"; do
   if [[ "$file" =~ $rest_pattern ]]; then
     modified_exchange="${BASH_REMATCH[1]}"
     REST_EXCHANGES+=($modified_exchange)
   elif [[ "$file" =~ $ws_pattern ]]; then
     modified_exchange="${BASH_REMATCH[1]}"
     WS_EXCHANGES+=($modified_exchange)
-  else
-    if [[ "$file" != "build.sh" && "$file" != ".travis.yml" ]]; then
-      echo "detected non derived file: $file, will build all"
-      unset $transpile_all
-      transpile_all=true
-      break
-    fi
-  fi
 done
-##### END DETECT CHANGES #####
-
-### RUN REGULAR BUILD SCRIPT AND EXIT IF NEEDED ###
-if [[ "$transpile_all" = true ]]; then
-  build_all_and_test_base
-fi
 
 ### BUILD SPECIFIC EXCHANGES ###
 npm run pre-transpile
@@ -57,3 +78,6 @@ for exchange in "${WS_EXCHANGES[@]}"; do
   node build/transpileWS.js $exchange --force --child
 done
 npm run post-transpile
+
+### RUN SPECIFIC TESTS ###
+run_tests "${REST_EXCHANGES[*]}" "${WS_EXCHANGES[*]}"
