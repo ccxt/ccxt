7f04c577a326c884714ae03fe6a5472041b6d6df
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-05-31 16:47:52 +0400

    typo

--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -1674,6 +1674,15 @@ class Exchange(object):
     def un_camel_case(self, str):
         return re.sub('(?!^)([A-Z]+)', r'_\1',str).lower()
 
+    def get_property(self, prop, defaultValue=None):
+        return getattr(self, prop)
+
+    def set_property(self, prop, value):
+        setattr(self, prop, value)
+
+    def property_exists(self, prop):
+        if hasattr(self, prop)
+
     # ########################################################################
     # ########################################################################
     # ########################################################################
