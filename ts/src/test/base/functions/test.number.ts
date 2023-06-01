9aed9b71ee8195093852521e7c89c0a402d3098d
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-06-01 10:39:54 +0100

    fix python

--- ts/src/test/base/functions/test.number.ts
+++ ts/src/test/base/functions/test.number.ts
@@ -1,7 +1,6 @@
 import assert from 'assert';
 import { functions } from '../../../../ccxt.js';
 import Precise from '../../../base/Precise.js';
-
 const { numberToString, decimalToPrecision, ROUND, TRUNCATE, DECIMAL_PLACES, TICK_SIZE, PAD_WITH_ZERO, SIGNIFICANT_DIGITS } = functions;
 // ----------------------------------------------------------------------------
 // numberToString
