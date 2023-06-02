cbbe4bdf0a6b0c256706826bccc06e3e3314c0d5
Sam Germain <sam.germain@usask.ca>
2023-06-02 17:03:28 -0400

    Revert "krakenfutures.pro bug fix"
    This reverts commit 284591ffdf5f3d314ad46b664c5d564ba535ce20.

--- ts/src/pro/krakenfutures.ts
+++ ts/src/pro/krakenfutures.ts
@@ -3,6 +3,7 @@
 import krakenfuturesRest from '../krakenfutures.js';
 import { ArgumentsRequired, AuthenticationError } from '../base/errors.js';
 import { ArrayCache, ArrayCacheBySymbolById } from '../base/ws/Cache.js';
+import { Precise } from '../base/Precise.js';
 import { sha256 } from '../static_dependencies/noble-hashes/sha256.js';
 import { sha512 } from '../static_dependencies/noble-hashes/sha512.js';
 import { Int, Market, Order, Trade } from '../base/types.js';
