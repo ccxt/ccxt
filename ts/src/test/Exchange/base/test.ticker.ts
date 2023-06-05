e8e0a12f9683195ae71d82d3bfbb0230a03a1059
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-06-05 11:05:49 +0400

    tests - orderbook granular check

--- ts/src/test/Exchange/base/test.ticker.ts
+++ ts/src/test/Exchange/base/test.ticker.ts
@@ -74,8 +74,11 @@ function testTicker (exchange, skippedProperties, method, entry, symbol) {
     if (!('ask' in skippedProperties) && !('bid' in skippedProperties)) {
         const askString = exchange.safeString (entry, 'ask');
         const bidString = exchange.safeString (entry, 'bid');
-        if ((askString !== undefined) && (bidString !== undefined)) {
-            testSharedMethods.assertGreater (exchange, skippedProperties, method, entry, 'ask', exchange.safeString (entry, 'bid'));
+        // check bid-ask spread to be > 0
+        if (!('compareBidAsk' in skippedProperties)) {
+            if ((askString !== undefined) && (bidString !== undefined)) {
+                testSharedMethods.assertGreater (exchange, skippedProperties, method, entry, 'ask', exchange.safeString (entry, 'bid'));
+            }
         }
         // todo: rethink about this
         // else {
