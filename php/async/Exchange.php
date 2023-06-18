af6d914285b0147c422feb066918498c67cff104
Travis CI <travis@travis-ci.org>
2023-06-18 14:03:50 +0000

    3.1.44
    [ci skip]

--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -34,11 +34,11 @@ use Exception;
 
 include 'Throttle.php';
 
-$version = '3.1.43';
+$version = '3.1.44';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '3.1.43';
+    const VERSION = '3.1.44';
 
     public $browser;
     public $marketsLoading = null;
