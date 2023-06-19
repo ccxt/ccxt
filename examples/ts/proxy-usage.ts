752c9ae95b50e7f3d233b8f5cb8fd0d41d049442
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-06-19 23:01:06 +0400

    example

--- examples/ts/proxy-usage.ts
+++ examples/ts/proxy-usage.ts
@@ -6,7 +6,8 @@ import ccxt from '../../js/ccxt.js';
 async function example_1 () {
     const myEx = new ccxt.kucoin ();
     myEx.proxyUrl = 'https://cors-anywhere.herokuapp.com/'; // It prepends redirect url to requests, so requests leads to call url i.e.: https://cors-anywhere.herokuapp.com/?https://target_url.com . It might be useful for simple redirection or CORS bypassing purposes (Note, this will not work for websocket connections, but only for REST calls).
-    // you can also set ".proxyUrlCallback" to callback function with with signature `(url, method, headers, body)` and from there return the proxy url string.
+    // or
+    // myEx.socksProxy = myCallback;
     console.log (await myEx.fetch ('https://api.ipify.org/'));
 }
 
@@ -14,6 +15,8 @@ async function example_2 () {
     const myEx = new ccxt.kucoin ();
     // same as httpProxy
     myEx.httpsProxy = 'http://51.83.140.52:11230'; // It sets a real proxy for communication, so calls are made directly to url https://target_url.com , but tunneled through a proxy server (Note, this might work for websocket connections too).
+    // or
+    // myEx.socksProxy = myCallback;
     console.log (await myEx.fetch ('https://api.ipify.org/'));
 }
 
@@ -21,13 +24,18 @@ async function example_2 () {
 async function example_3 () {
     const myEx = new ccxt.kucoin ();
     myEx.socksProxy = 'socks5://127.0.0.1:1080'; // It is for socks5 or socks5h proxy (Note, this might work for websocket connections too).
+    // or
+    // myEx.socksProxy = myCallback;
     console.log (await myEx.fetch ('https://api.ipify.org/'));
 }
 
 
 function myCallback (url, method, headers, body) {
     // in JS it sets .agent property for fetch, in PHP it sets .userAgent, in Python (sync) it returns dict of proxies for `Requests` module, in Python (async) it returns signle proxy entry
-    return {}; //
+    return 'http(s)://my-proxy.ext:123';
 }
 
+
 await example_1 ();
+// await example_2 ();
+// await example_3 ();
