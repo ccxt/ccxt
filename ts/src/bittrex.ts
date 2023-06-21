0110b371f07c668e6be611ccfab1b81ac7f7dd2f
AlgoQ <kobejanssens@outlook.com>
2023-06-21 11:48:52 +0200

    remove see value - no specific keypoints in docs

--- ts/src/bittrex.ts
+++ ts/src/bittrex.ts
@@ -2092,7 +2092,6 @@ export default class bittrex extends Exchange {
          * @method
          * @name bittrex#fetchDepositWithdrawFees
          * @description fetch deposit and withdraw fees
-         * @see https://github.com/Bitrue-exchange/Spot-official-api-docs#exchangeInfo_endpoint
          * @param {[string]|undefined} codes list of unified currency codes
          * @param {object} params extra parameters specific to the bitrue api endpoint
          * @returns {object} a list of [fee structures]{@link https://docs.ccxt.com/en/latest/manual.html#fee-structure}
