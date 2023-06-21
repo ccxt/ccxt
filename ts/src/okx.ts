991578bb539b3df37070206b62870ddd27f2d134
AlgoQ <kobejanssens@outlook.com>
2023-06-21 17:42:21 +0200

    okx - createOrder js doc update type 3

--- ts/src/okx.ts
+++ ts/src/okx.ts
@@ -2263,6 +2263,12 @@ export default class okx extends Exchange {
          * @param {object} params extra parameters specific to the okx api endpoint
          * @param {bool|undefined} params.reduceOnly MARGIN orders only, or swap/future orders in net mode
          * @param {bool|undefined} params.postOnly true to place a post only order
+         * @param {object|undefined} params.takeProfit *takeProfit object in params* containing the triggerPrice at which the attached take profit order will be triggered (perpetual swap markets only)
+         * @param {float|undefined} params.takeProfit.triggerPrice take profit trigger price
+         * @param {float|undefined} params.takeProfit.price used for take profit limit orders, not used for take profit market price orders
+         * @param {object|undefined} params.stopLoss *stopLoss object in params* containing the triggerPrice at which the attached stop loss order will be triggered (perpetual swap markets only)
+         * @param {float|undefined} params.stopLoss.triggerPrice stop loss trigger price
+         * @param {float|undefined} params.stopLoss.price used for stop loss limit orders, not used for stop loss market price orders
          * @returns {object} an [order structure]{@link https://docs.ccxt.com/#/?id=order-structure}
          */
         await this.loadMarkets ();
