f5a0fb251db1c3611ee990531551fb963e5bfc2e
Pravin Battu <pravinbattu@Pravins-MacBook-Pro.local>
2023-06-24 12:09:18 -0300

    Initial commit - ProtonDEX

--- examples/js/protondex-test.js
+++ examples/js/protondex-test.js
@@ -1,8 +1,7 @@
-"use strict";
 
 // ----------------------------------------------------------------------------
 
-const ccxt = require ('../../ccxt.js')
+import ccxt from '../../js/ccxt.js';
 
 // ----------------------------------------------------------------------------
 
@@ -14,16 +13,38 @@ const ccxt = require ('../../ccxt.js')
     })
 
     try {
-
+        // API doc: https://api-docs.protondex.com/reference/what-is-proton-dex
         // const response = await exchange.loadMarkets ()
-        // const response = await exchange.fetchTrades ()
-        const response = await exchange.fetchRecentTrades ('XPR_XUSDC', 0, 100)
-        // const response = await exchange.fetchOrdersHistory ('XPR_XUSDC', 'pbtest1', 0, 100)
+        // 1. Fetch markets
         // const response = await exchange.fetchMarkets ()
-        // const response = await exchange.fetchOrderBook ('XPR_XUSDC', 10, 100)
-        // const response = await exchange.fetchOpenOrders ('XPR_XUSDC',  'pbtest1', 0, 100)
+        // 2. Fetch ticker
+        // const response = await exchange.fetchTickers ()
+        // const response = await exchange.fetchTicker ('XPR_XMD')
+        // 3. Fetch order book
+        // const response = await exchange.fetchOrderBook ('XPR_XMD', 100, {'step': 10000})
+        // 4. Fetch recent trades
+        // const response = await exchange.fetchTrades ('XPR_XMD', 1, 100, {'offset': 0})
+        // 5. Fetch OHLCV
+        // const response = await exchange.fetchOHLCV ("XPR_XMD", 60, 1, 100, {'from_time': '2023-04-12T17:20:13Z', 'to_time': '2023-05-12T17:20:13Z'})
+        // 6. Fetch account balances
+        // const response = await exchange.fetchBalance ({'account': 'testme1'})
+        // 7. Fetch order
+        // const response = await exchange.fetchOrder ('1603774', {'ordinal_order_id': ''})
+        // 8. Fetch open orders
+        // const response = await exchange.fetchOpenOrders ('XPR_XMD', 1, 100, {'account': 'testme1', 'offset': 0, 'ordinal_order_ids': ''})
+        // 9. Fetch orders history
+        // const response = await exchange.fetchOrders ('XPR_XMD', 1, 100, {'account': 'testme1', 'offset': 0, 'ordinal_order_ids': '', 'trx_id': '', 'status': ''})
+        // const response = await exchange.fetchOrders ('XPR_XMD', 1, 100, {'account': 'testme1', 'offset': '0', 'ordinal_order_ids': '293d13c47603e5c81c270a2d8e197823584cec071d139631f6e1990e62ae8ed8', 'trx_id': '', 'status': ''})
+        // 10. Fetch trades history
+        // const response = await exchange.fetchMyTrades ('XPR_XMD', 1, 10, {'account': 'testme1', 'offset': 0})
         // const response = await exchange.fetchStatusSync ()
-        // const response = await exchange.fetchOHLCV (60, "XPR_XUSDC", '2022-10-12T17:20:13Z', '2022-11-12T17:20:13Z', 100)
+        // 11. Create order
+        // const response = await exchange.createOrder ('XPR_XMD', 1, 2, 700, 0.0018, {'account': 'testme1', 'filltype': 0, 'triggerprice': 0})
+        // 12. Cancel order
+        // const response = await exchange.cancelOrder (4065203, 'XPR_XMD', {'account': 'testme1'});
+        // 13. Cancel all orders for specific symbol
+         const response = await exchange.cancelAllOrders ('XPR_XMD', {'account': 'testme1'});
+
         console.log (response);
         console.log ('Succeeded.')
 
