502a01af6c5be44a9b766dd87dcdcdbc2102fbbb
T. Todua <7117978+ttodua@users.noreply.github.com>
2023-06-25 21:36:09 +0400

    [ci skip]

--- examples/test.js
+++ examples/test.js
@@ -7,7 +7,7 @@ const binance = new ccxt.binance({});
 async function t(){
     while (true) {
         const t1 = new Date().getTime();
-        await binance.sleep(50);
+        // await binance.sleep(50); // neither adding this "tricks" throttler
         const ts = await binance.fetchTrades('BNB/USDT:USDT');
         console.log("FETCH ELAPSED", new Date().getTime() - t1);
     }
