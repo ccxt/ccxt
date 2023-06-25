6a77cb11224109c1fe8604cc78d334e2a0cd8d80
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-06-25 19:16:20 +0100

    fix(mexc): change marginType to marginMode

--- ts/src/mexc.ts
+++ ts/src/mexc.ts
@@ -4373,7 +4373,7 @@ export default class mexc extends Exchange {
             'unrealizedProfit': undefined,
             'leverage': this.parseNumber (leverage),
             'percentage': undefined,
-            'marginType': marginType,
+            'marginMode': marginType,
             'notional': undefined,
             'markPrice': undefined,
             'lastPrice': undefined,
