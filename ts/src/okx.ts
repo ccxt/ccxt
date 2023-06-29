55cd01f10b399c10c446b2a40076bc005fc09d45
Igor Kroitor <igor.kroitor@gmail.com>
2023-06-29 07:30:00 +0300

    okx.ts minor edits in comments

--- ts/src/okx.ts
+++ ts/src/okx.ts
@@ -1899,7 +1899,8 @@ export default class okx extends Exchange {
             const now = this.milliseconds ();
             const difference = now - since;
             const durationInMilliseconds = duration * 1000;
-            // if the since timestamp is more than limit candles back in the past (additional one bar because of maximal offset to round the current day to UTC)
+            // if the since timestamp is more than limit candles back in the past
+            // additional one bar for max offset to round the current day to UTC
             const calc = (1440 - limit - 1) * durationInMilliseconds;
             if (difference > calc) {
                 defaultType = 'HistoryCandles';
