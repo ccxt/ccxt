1568f5d04ff7cc4c2d80a19df391955a7878cf28
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-06-30 14:13:55 +0400

    comment

--- examples/ts/sample-local-proxy-server-with-cors.ts
+++ examples/ts/sample-local-proxy-server-with-cors.ts
@@ -9,6 +9,7 @@ import cors from 'cors-anywhere'; // npm install cors-anywhere
 
 const port = (process.argv.length > 2) ? parseInt (process.argv[2]) : 8080; // if not provided from cli, default to 8080
 cors.createServer ({
+    // you can set origin, if needed by exchange
     // setHeaders: { 'origin': 'https://www.bitmex.com' }
 }).listen (port, 'localhost');
 console.log ('Running CORS Anywhere on localhost:' + port);
