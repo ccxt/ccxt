aaa02002f12e8b9aa7b0ad2b5175972b4669c91a
ndubel <38309641+ndubel@users.noreply.github.com>
2023-07-04 20:11:49 +0300

    bitget parseTransaction edits
    also we have withdraw here

--- ts/src/bitget.ts
+++ ts/src/bitget.ts
@@ -94,7 +94,7 @@ export default class bitget extends Exchange {
                 'setMarginMode': true,
                 'setPositionMode': true,
                 'transfer': true,
-                'withdraw': false,
+                'withdraw': true,
             },
             'timeframes': {
                 '1m': '1m',
@@ -1668,17 +1668,27 @@ export default class bitget extends Exchange {
         //         "amount": "19.44800000",
         //         "status": "success",
         //         "toAddress": "TRo4JMfZ1XYHUgnLsUMfDEf8MWzcWaf8uh",
-        //         "fee": null,
+        //         "fee": "-3.06388160",
         //         "chain": "TRC20",
         //         "confirm": null,
+        //         "tag": null,
         //         "cTime": "1656407912259",
         //         "uTime": "1656407940148"
         //     }
         //
+        const currencyId = this.safeString (transaction, 'coin');
+        const code = this.safeCurrencyCode (currencyId);
         const timestamp = this.safeInteger (transaction, 'cTime');
         const networkId = this.safeString (transaction, 'chain');
         const currencyId = this.safeString (transaction, 'coin');
         const status = this.safeString (transaction, 'status');
+        const tag = this.safeString (transaction, 'tag');
+        const feeCost = this.safeString (transaction, 'fee');
+        const feeCostAbs = Precise.stringAbs (feeCost);
+        let fee = undefined;
+        if (feeCostAbs !== undefined) {
+            fee = { 'currency': code, 'cost': this.parseNumber (feeCostAbs) };
+        }
         return {
             'id': this.safeString (transaction, 'id'),
             'info': transaction,
@@ -1691,14 +1701,14 @@ export default class bitget extends Exchange {
             'addressTo': this.safeString (transaction, 'toAddress'),
             'amount': this.safeNumber (transaction, 'amount'),
             'type': this.safeString (transaction, 'type'),
-            'currency': this.safeCurrencyCode (currencyId),
+            'currency': code,
             'status': this.parseTransactionStatus (status),
             'updated': this.safeNumber (transaction, 'uTime'),
             'tagFrom': undefined,
-            'tag': undefined,
-            'tagTo': undefined,
+            'tag': tag,
+            'tagTo': tag,
             'comment': undefined,
-            'fee': undefined,
+            'fee': fee,
         };
     }
 
