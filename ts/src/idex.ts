889eef6ec8a6def25fc3eb82115ea4bec3c841f6
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-07-05 13:19:35 +0400

    update

--- ts/src/idex.ts
+++ ts/src/idex.ts
@@ -154,6 +154,7 @@ export default class idex extends Exchange {
                 'defaultTimeInForce': 'gtc',
                 'defaultSelfTradePrevention': 'cn',
                 'network': 'MATIC',
+                'supportedBlochchains': [ 'MATIC' ],
             },
             'exceptions': {
                 'INVALID_ORDER_QUANTITY': InvalidOrder,
@@ -751,6 +752,33 @@ export default class idex extends Exchange {
             const currencyId = this.safeString (entry, 'symbol');
             const code = this.safeCurrencyCode (currencyId);
             const precision = this.parseNumber (this.parsePrecision (this.safeString (entry, 'exchangeDecimals')));
+            const networks = {};
+            const supportedChains = this.safeValue (this.options, 'supportedBlochchains', []);
+            const length = supportedChains.length;
+            // commonly they have only one chain
+            if (length === 1) {
+                const networkCode = this.networkIdToCode (supportedChains[0]);
+                networks[networkCode] = {
+                    'info': entry,
+                    'id': networkCode,
+                    'network': networkCode,
+                    'active': true,
+                    'deposit': true,
+                    'withdraw': true,
+                    'fee': undefined,
+                    'precision': precision,
+                    'limits': {
+                        'deposit': {
+                            'min': undefined,
+                            'max': undefined,
+                        },
+                        'withdraw': {
+                            'min': undefined,
+                            'max': undefined,
+                        },
+                    },
+                };
+            }
             result[code] = {
                 'id': currencyId,
                 'code': code,
@@ -762,6 +790,7 @@ export default class idex extends Exchange {
                 'withdraw': undefined,
                 'fee': undefined,
                 'precision': precision,
+                'networks': networks,
                 'limits': {
                     'amount': { 'min': precision, 'max': undefined },
                     'withdraw': { 'min': precision, 'max': undefined },
