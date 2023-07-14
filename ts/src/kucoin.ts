19e29afec371e891903b9a253b317499924fc523
sc0Vu <alk03073135@gmail.com>
2023-07-14 11:19:19 +0800

    kucoin: update repayMargin ArgumentsRequired error

--- ts/src/kucoin.ts
+++ ts/src/kucoin.ts
@@ -4051,7 +4051,7 @@ export default class kucoin extends Exchange {
         };
         if (isIsolated) {
             if (symbol === undefined) {
-                throw new ArgumentsRequired (this.id + ' borrowMargin() requires a symbol parameter for isolated margin');
+                throw new ArgumentsRequired (this.id + ' repayMargin() requires a symbol parameter for isolated margin');
             }
             const market = this.market (symbol);
             request['symbol'] = market['id'];
