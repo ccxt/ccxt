e7febccb9d2b9854c00c5be3a43c05891c8691b6
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-07-16 00:23:58 +0400

    -

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -2687,7 +2687,7 @@ export default class Exchange {
          * @ignore
          * @method
          * @name exchange#networkIdToCode
-         * @description tries to convert the provided exchange-specific networkId to an unified network Code. In order to achieve this, derived class needs to have 'options['networksById']' defined.
+         * @description tries to convert the provided exchange-specific networkId to an unified network Code. In order to achieve this, derived class needs to have "options['networksById']" defined.
          * @param {string} networkId exchange specific network id/title, like: TRON, Trc-20, usdt-erc20, etc
          * @param {string|undefined} currencyCode unified currency code, but this argument is not required by default, unless there is an exchange (like huobi) that needs an override of the method to be able to pass currencyCode argument additionally
          * @returns {string|undefined} unified network code
