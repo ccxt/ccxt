d724b1c8451a59855005b9c434f2b634a4b81950
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-07-19 16:21:21 +0100

    fix

--- ts/src/bitfinex2.ts
+++ ts/src/bitfinex2.ts
@@ -2508,7 +2508,7 @@ export default class bitfinex2 extends Exchange {
         }
         if (statusCode === 500) {
             // See https://docs.bitfinex.com/docs/abbreviations-glossary#section-errorinfo-codes
-            const errorCode = this.safeString (response[1], '');
+            const errorCode = this.safeString (response, 1, '');
             const errorText = this.safeString (response, 2, '');
             const feedback = this.id + ' ' + errorText;
             this.throwBroadlyMatchedException (this.exceptions['broad'], errorText, feedback);
