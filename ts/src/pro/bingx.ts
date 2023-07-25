00892ccaf1a791a39cc3f9b6a2c3a0364f530ef0
AlgoQ <kobejanssens@outlook.com>
2023-07-25 12:59:27 +0200

    watchTrades

--- ts/src/pro/bingx.ts
+++ ts/src/pro/bingx.ts
@@ -2,6 +2,8 @@
 //  ---------------------------------------------------------------------------
 
 import bingxRest from '../bingx.js';
+import { BadRequest } from '../base/errors.js';
+import { Int } from '../base/types.js';
 import Client from '../base/ws/Client.js';
 
 //  ---------------------------------------------------------------------------
@@ -27,6 +29,41 @@ export default class bingx extends bingxRest {
                 },
             },
         });
+    }
+
+    async watchTrades (symbol: string, since: Int = undefined, limit: Int = undefined, params = {}) {
+        /**
+         * @method
+         * @name bingx#watchTrades
+         * @description watches information on multiple trades made in a market
+         * @see https://bingx-api.github.io/docs/#/spot/socket/market.html#Subscribe%20to%20tick-by-tick
+         * @param {string} symbol unified market symbol of the market orders were made in
+         * @param {int} [since] the earliest time in ms to fetch orders for
+         * @param {int} [limit] the maximum number of  orde structures to retrieve
+         * @param {object} [params] extra parameters specific to the bingx api endpoint
+         * @returns {object[]} a list of [order structures]{@link https://docs.ccxt.com/#/?id=order-structure
+         */
+        await this.loadMarkets ();
+        const market = this.market (symbol);
+        const [ marketType, query ] = this.handleMarketTypeAndParams ('createOrder', market, params);
+        const url = this.safeValue (this.urls['api']['ws'], marketType);
+        if (url === undefined) {
+            throw new BadRequest (this.id + ' fetchTicker is not supported for ' + marketType + ' markets.');
+        }
+        const messageHash = market['id'] + '@trade';
+        const uuid = this.uuid ();
+        const request = {
+            'id': uuid,
+            'dataType': messageHash,
+        };
+        if (marketType === 'swap') {
+            request['reqType'] = 'sub';
+        }
+        const trades = await this.watch (url, messageHash, this.extend (request, query), messageHash);
+        if (this.newUpdates) {
+            limit = trades.getLimit (symbol, limit);
+        }
+        return this.filterBySinceLimit (trades, since, limit, 'timestamp', true);
     }
 
     handleMessage (client: Client, message) {
