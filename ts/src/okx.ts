8d5f1c250d4403a324c6703ca6944f632d9bfaa4
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-07-31 12:40:58 +0400

    feat(okx) - fetchPositionsBySymbol/fetchPositionsForSymbol

--- ts/src/okx.ts
+++ ts/src/okx.ts
@@ -94,6 +94,7 @@ export default class okx extends Exchange {
                 'fetchPermissions': undefined,
                 'fetchPosition': true,
                 'fetchPositions': true,
+                'fetchPositionsBySymbol': true,
                 'fetchPositionsRisk': false,
                 'fetchPremiumIndexOHLCV': false,
                 'fetchSettlementHistory': true,
