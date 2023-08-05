7479aca3aeb802d9f25c2bde2009223b8223f124
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-08-05 21:58:43 +0400

    bitstamp

--- ts/src/pro/bitstamp.ts
+++ ts/src/pro/bitstamp.ts
@@ -33,6 +33,7 @@ export default class bitstamp extends bitstampRest {
                 'watchOrderBook': {
                     'snapshotDelay': 6,
                     'maxRetries': 3,
+                    'initialFetchOrderBookMaxRetries': 3,
                 },
                 'tradesLimit': 1000,
                 'OHLCVLimit': 1000,
