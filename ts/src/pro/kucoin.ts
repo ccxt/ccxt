68004d7f30dd2335d2d6e3b5c00df4f8990739a6
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-08-05 21:59:01 +0400

    kucoin

--- ts/src/pro/kucoin.ts
+++ ts/src/pro/kucoin.ts
@@ -31,6 +31,7 @@ export default class kucoin extends kucoinRest {
                 'watchOrderBook': {
                     'snapshotDelay': 5,
                     'maxRetries': 3,
+                    'initialFetchOrderBookMaxRetries': 3,
                 },
             },
             'streaming': {
