9ebf171bd23debd423656cdc2e773f2076d7f2cc
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-08-05 21:59:25 +0400

    poloniexfutures

--- ts/src/pro/poloniexfutures.ts
+++ ts/src/pro/poloniexfutures.ts
@@ -44,6 +44,7 @@ export default class poloniexfutures extends poloniexfuturesRest {
                     'method': '/contractMarket/level2', // can also be '/contractMarket/level3v2'
                     'snapshotDelay': 5,
                     'maxRetries': 3,
+                    'initialFetchOrderBookMaxRetries': 3,
                 },
                 'streamLimit': 5, // called tunnels by poloniexfutures docs
                 'streamBySubscriptionsHash': {},
