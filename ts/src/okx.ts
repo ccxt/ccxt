b5bd47964302e74f050abd866fcbae588cba3094
Igor Kroitor <igor.kroitor@gmail.com>
2023-08-11 11:07:00 +0300

    okx personal trades rate limit temporary workaround

--- ts/src/okx.ts
+++ ts/src/okx.ts
@@ -248,7 +248,7 @@ export default class okx extends Exchange {
                         'trade/orders-history': 1 / 2,
                         'trade/orders-history-archive': 1,
                         'trade/fills': 1 / 3,
-                        'trade/fills-history': 2,
+                        'trade/fills-history': 2.2,
                         'trade/order-algo': 1,
                         'trade/orders-algo-pending': 1,
                         'trade/orders-algo-history': 1,
