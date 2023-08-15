9e2f13f5d1bf8967a9c145d7b405a3da828380bf
Travis CI <travis@travis-ci.org>
2023-08-15 10:19:07 +0000

    Merge pull request #18892 from sc0Vu/add-bingx-apis-20230815
    bingx: add apis
    
    [ci skip]

--- dist/cjs/src/bingx.js
+++ dist/cjs/src/bingx.js
@@ -65,6 +65,8 @@ class bingx extends bingx$1 {
                     'swap': 'https://open-api.{hostname}/openApi',
                     'contract': 'https://open-api.{hostname}/openApi',
                     'wallets': 'https://open-api.{hostname}/openApi',
+                    'subAccount': 'https://open-api.{hostname}/openApi',
+                    'account': 'https://open-api.{hostname}/openApi',
                 },
                 'www': 'https://bingx.com/',
                 'doc': 'https://bingx-api.github.io/docs/',
@@ -111,6 +113,9 @@ class bingx extends bingx$1 {
                                 'asset/transfer': 3,
                                 'capital/deposit/hisrec': 3,
                                 'capital/withdraw/history': 3,
+                            },
+                            'post': {
+                                'post/asset/transfer': 3,
                             },
                         },
                     },
@@ -183,6 +188,35 @@ class bingx extends bingx$1 {
                             },
                             'post': {
                                 'capital/withdraw/apply': 3,
+                                'capital/innerTransfer/apply': 3,
+                                'capital/subAccountInnerTransfer/apply': 3,
+                            },
+                        },
+                    },
+                },
+                'subAccount': {
+                    'v1': {
+                        'private': {
+                            'get': {
+                                'list': 3,
+                                'assets': 3,
+                            },
+                            'post': {
+                                'create': 3,
+                                'apiKey/create': 3,
+                                'apiKey/edit': 3,
+                                'apiKey/del': 3,
+                                'updateStatus': 3,
+                            },
+                        },
+                    },
+                },
+                'account': {
+                    'v1': {
+                        'private': {
+                            'post': {
+                                'uid': 3,
+                                'innerTransfer/authorizeSubAccount': 3,
                             },
                         },
                     },
