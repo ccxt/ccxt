8c13dba88744b557b803c491eb8fdf0ada75d9e7
Sam Germain <sam.germain@usask.ca>
2023-08-16 21:57:00 -0400

    poloniex parseOrder minor fix

--- ts/src/poloniex.ts
+++ ts/src/poloniex.ts
@@ -1095,7 +1095,7 @@ export default class poloniex extends Exchange {
             };
         }
         const clientOrderId = this.safeString (order, 'clientOrderId');
-        const triggerPrice = this.safeString (order, 'triggerPrice', 'stopPrice');
+        const triggerPrice = this.safeString2 (order, 'triggerPrice', 'stopPrice');
         return this.safeOrder ({
             'info': order,
             'id': id,
