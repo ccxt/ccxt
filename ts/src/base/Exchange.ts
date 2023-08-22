9ce8b65a7b6416e951e66fcd8c480df3cdd0449f
Sam Germain <sam.germain@usask.ca>
2023-08-22 18:28:30 -0400

    feat(Exchange): Add cache of trigger orders to base exchange

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -223,6 +223,7 @@ export default class Exchange {
     orderbooks   = {}
     tickers      = {}
     orders       = undefined
+    triggerOrders = undefined
     trades: any
     transactions = {}
     ohlcvs: any
