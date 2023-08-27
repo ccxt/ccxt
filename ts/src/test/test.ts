522ec264c1466885309743e32ccb9413958bedb2
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-08-28 01:09:14 +0400

    fix(test) - array length

--- ts/src/test/test.ts
+++ ts/src/test/test.ts
@@ -355,8 +355,9 @@ export default class testMainClass extends baseMainTestClass {
             }
             // we don't throw exception for public-tests, see comments under 'testSafe' method
             let failedMsg = '';
-            if (errors.length) {
-                failedMsg = ' | Failed methods: ' + errors.join (', ');
+            const len = errors.length;
+            if (len > 0) {
+                failedMsg = ' | Failed methods : ' + errors.join (', ');
             }
             dump (this.addPadding ('[INFO:PUBLIC_TESTS_END] ' + market['type'] + failedMsg, 25), exchange.id);
         }
