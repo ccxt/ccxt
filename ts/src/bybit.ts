7bf1d592a84ea0159f5691d45d20214171ee8e19
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-09-03 14:22:53 +0100

    fix(bybit): remove editOrder check

--- ts/src/bybit.ts
+++ ts/src/bybit.ts
@@ -4401,9 +4401,6 @@ export default class bybit extends Exchange {
     }
 
     async editUnifiedAccountOrder (id: string, symbol, type, side, amount = undefined, price = undefined, params = {}) {
-        if (amount === undefined && price === undefined) {
-            throw new InvalidOrder (this.id + ' editOrder requires either a price argument or an amount argument');
-        }
         await this.loadMarkets ();
         const market = this.market (symbol);
         if (!market['linear'] && !market['option']) {
@@ -4482,9 +4479,6 @@ export default class bybit extends Exchange {
     }
 
     async editUnifiedMarginOrder (id: string, symbol, type, side, amount, price = undefined, params = {}) {
-        if (amount === undefined && price === undefined) {
-            throw new InvalidOrder (this.id + ' editOrder requires either a price argument or an amount argument');
-        }
         await this.loadMarkets ();
         const market = this.market (symbol);
         if (!market['linear'] && !market['option']) {
@@ -4578,9 +4572,6 @@ export default class bybit extends Exchange {
     }
 
     async editContractV3Order (id: string, symbol, type, side, amount = undefined, price = undefined, params = {}) {
-        if (amount === undefined && price === undefined) {
-            throw new InvalidOrder (this.id + ' editOrder requires either a price argument or an amount argument');
-        }
         await this.loadMarkets ();
         const market = this.market (symbol);
         const request = {
