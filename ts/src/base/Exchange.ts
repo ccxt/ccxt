97264dd667096a580302aba69983f830f13af791
Pablo Criado-Perez <pablo@criadoperez.com>
2023-09-04 02:14:24 +0200

    add watchPositionForSymbols alias

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -3103,6 +3103,10 @@ export default class Exchange {
     async watchPositions (symbols: string[] = undefined, since: Int = undefined, limit: Int = undefined, params = {}): Promise<Position[]> {
         throw new NotSupported (this.id + ' watchPositions() is not supported yet');
     }
+    
+    async watchPositionForSymbols (symbols: string[] = undefined, since: Int = undefined, limit: Int = undefined, params = {}): Promise<Position[]> {
+        return this.watchPositions (symbols, since, limit, params);
+    }
 
     async fetchPositionsBySymbol (symbol: string, params = {}): Promise<Position[]> {
         /**
