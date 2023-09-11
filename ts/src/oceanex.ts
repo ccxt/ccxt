293871365584f3b6ef567528fdde31fedb5d4c43
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-09-12 00:40:49 +0400

    fix(oceanex) - public trade takerOrMaker

--- ts/src/oceanex.ts
+++ ts/src/oceanex.ts
@@ -503,7 +503,7 @@ export default class oceanex extends Exchange {
         //      }
         //
         const data = this.safeValue (response, 'data');
-        return this.parseTrades (data, market, since, limit);
+        return this.parseTrades (data, market, since, limit, { 'takerOrMaker': 'taker' });
     }
 
     parseTrade (trade, market = undefined) {
