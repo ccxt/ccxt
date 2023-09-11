042873191ea65c3b57598e23c7130c0c20192cad
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-09-11 19:29:52 +0400

    fix(bingx) - public trade takerOrMaker

--- ts/src/bingx.ts
+++ ts/src/bingx.ts
@@ -748,7 +748,7 @@ export default class bingx extends Exchange {
         //    }
         //
         const trades = this.safeValue (response, 'data', []);
-        return this.parseTrades (trades, market, since, limit);
+        return this.parseTrades (trades, market, since, limit, { 'takerOrMaker': 'taker' });
     }
 
     parseTrade (trade, market = undefined) {
