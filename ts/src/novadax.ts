3dcd0e4b71db75255eda682504492bd8dd60c5a3
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-09-12 00:36:00 +0400

    fix(novadax) - public trade takerOrMaker

--- ts/src/novadax.ts
+++ ts/src/novadax.ts
@@ -596,7 +596,7 @@ export default class novadax extends Exchange {
         //     }
         //
         const data = this.safeValue (response, 'data', []);
-        return this.parseTrades (data, market, since, limit);
+        return this.parseTrades (data, market, since, limit, { 'takerOrMaker': 'taker' });
     }
 
     async fetchOHLCV (symbol: string, timeframe = '1m', since: Int = undefined, limit: Int = undefined, params = {}) {
