55323f2e03165822f755e055fcd6bc6e36af7a12
Travis CI <travis@travis-ci.org>
2023-09-13 15:33:48 +0000

    4.0.94
    [ci skip]

--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -40,11 +40,11 @@ use Exception;
 
 include 'Throttle.php';
 
-$version = '4.0.93';
+$version = '4.0.94';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '4.0.93';
+    const VERSION = '4.0.94';
 
     public $browser;
     public $marketsLoading = null;
--- ts/ccxt.ts
+++ ts/ccxt.ts
@@ -40,7 +40,7 @@ import { BaseError, ExchangeError, PermissionDenied, AccountNotEnabled, AccountS
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '4.0.93';
+const version = '4.0.94';
 
 (Exchange as any).ccxtVersion = version
 
