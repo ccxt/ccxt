52c939a22259f89bebf4014ddf26620eaba2cdf2
sc0Vu <alk03073135@gmail.com>
2023-09-15 11:32:45 +0800

    bithumb: add apis, update api params

--- ts/src/bithumb.ts
+++ ts/src/bithumb.ts
@@ -82,15 +82,14 @@ export default class bithumb extends Exchange {
             'api': {
                 'public': {
                     'get': [
-                        'ticker/{currency}',
-                        'ticker/all',
-                        'ticker/ALL_BTC',
-                        'ticker/ALL_KRW',
-                        'orderbook/{currency}',
-                        'orderbook/all',
-                        'transaction_history/{currency}',
-                        'transaction_history/all',
-                        'candlestick/{currency}/{interval}',
+                        'ticker/ALL_{quoteId}',
+                        'ticker/{baseId}_{quoteId}',
+                        'orderbook/ALL_{quoteId}',
+                        'orderbook/{baseId}_{quoteId}',
+                        'transaction_history/{baseId}_{quoteId}',
+                        'assetsstatus/ALL',
+                        'assetsstatus/{baseId}',
+                        'candlestick/{baseId}_{quoteId}/{interval}',
                     ],
                 },
                 'private': {
@@ -109,6 +108,7 @@ export default class bithumb extends Exchange {
                         'trade/krw_withdrawal',
                         'trade/market_buy',
                         'trade/market_sell',
+                        'trade/stop_limit',
                     ],
                 },
             },
@@ -200,8 +200,10 @@ export default class bithumb extends Exchange {
             const quote = quotes[i];
             const quoteId = quote;
             const extension = this.safeValue (quoteCurrencies, quote, {});
-            const method = 'publicGetTickerALL' + quote;
-            const response = await this[method] (params);
+            const request = {
+                'quoteId': quoteId,
+            };
+            const response = await this.publicGetTickerALLQuoteId (this.extend (request, params));
             const data = this.safeValue (response, 'data');
             const currencyIds = Object.keys (data);
             for (let j = 0; j < currencyIds.length; j++) {
@@ -315,12 +317,13 @@ export default class bithumb extends Exchange {
         await this.loadMarkets ();
         const market = this.market (symbol);
         const request = {
-            'currency': market['base'] + '_' + market['quote'],
+            'baseId': market['baseId'],
+            'quoteId': market['quoteId'],
         };
         if (limit !== undefined) {
             request['count'] = limit; // default 30, max 30
         }
-        const response = await this.publicGetOrderbookCurrency (this.extend (request, params));
+        const response = await this.publicGetOrderbookBaseIdQuoteId (this.extend (request, params));
         //
         //     {
         //         "status":"0000",
@@ -410,8 +413,11 @@ export default class bithumb extends Exchange {
         const quotes = Object.keys (quoteCurrencies);
         for (let i = 0; i < quotes.length; i++) {
             const quote = quotes[i];
-            const method = 'publicGetTickerALL' + quote;
-            const response = await this[method] (params);
+            const quoteId = quote;
+            const request = {
+                'quoteId': quoteId,
+            };
+            const response = await this.publicGetTickerALLQuoteId (this.extend (request, params));
             //
             //     {
             //         "status":"0000",
@@ -462,9 +468,10 @@ export default class bithumb extends Exchange {
         await this.loadMarkets ();
         const market = this.market (symbol);
         const request = {
-            'currency': market['base'],
+            'baseId': market['baseId'],
+            'quoteId': market['quoteId'],
         };
-        const response = await this.publicGetTickerCurrency (this.extend (request, params));
+        const response = await this.publicGetTickerBaseIdQuoteId (this.extend (request, params));
         //
         //     {
         //         "status":"0000",
@@ -524,10 +531,11 @@ export default class bithumb extends Exchange {
         await this.loadMarkets ();
         const market = this.market (symbol);
         const request = {
-            'currency': market['base'],
+            'baseId': market['baseId'],
+            'quoteId': market['quoteId'],
             'interval': this.safeString (this.timeframes, timeframe, timeframe),
         };
-        const response = await this.publicGetCandlestickCurrencyInterval (this.extend (request, params));
+        const response = await this.publicGetCandlestickBaseIdQuoteIdInterval (this.extend (request, params));
         //
         //     {
         //         'status': '0000',
@@ -647,12 +655,13 @@ export default class bithumb extends Exchange {
         await this.loadMarkets ();
         const market = this.market (symbol);
         const request = {
-            'currency': market['base'] + '_' + market['quote'],
+            'baseId': market['baseId'],
+            'quoteId': market['quoteId'],
         };
         if (limit !== undefined) {
             request['count'] = limit; // default 20, max 100
         }
-        const response = await this.publicGetTransactionHistoryCurrency (this.extend (request, params));
+        const response = await this.publicGetTransactionHistoryBaseIdQuoteId (this.extend (request, params));
         //
         //     {
         //         "status":"0000",
