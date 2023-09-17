553d221856633596cc23fdf2c45e9c2ed73bb7bb
Travis CI <travis@travis-ci.org>
2023-09-17 15:37:02 +0000

    Merge pull request #19285 from ttodua/bitfinex2-shadow-var
    fix(bitfinex2) - shadow vars rename (QUICK)
    
    [ci skip]

--- js/src/pro/bitfinex2.js
+++ js/src/pro/bitfinex2.js
@@ -598,9 +598,9 @@ export default class bitfinex2 extends bitfinex2Rest {
                     const size = (delta[2] < 0) ? -delta[2] : delta[2];
                     const side = (delta[2] < 0) ? 'asks' : 'bids';
                     const bookside = orderbook[side];
-                    const id = this.safeString(delta, 0);
+                    const idString = this.safeString(delta, 0);
                     const price = this.safeFloat(delta, 1);
-                    bookside.store(price, size, id);
+                    bookside.store(price, size, idString);
                 }
             }
             else {
@@ -620,12 +620,12 @@ export default class bitfinex2 extends bitfinex2Rest {
         }
         else {
             const deltas = message[1];
-            const orderbook = this.orderbooks[symbol];
+            const orderbookItem = this.orderbooks[symbol];
             if (isRaw) {
                 const price = this.safeString(deltas, 1);
                 const size = (deltas[2] < 0) ? -deltas[2] : deltas[2];
                 const side = (deltas[2] < 0) ? 'asks' : 'bids';
-                const bookside = orderbook[side];
+                const bookside = orderbookItem[side];
                 // price = 0 means that you have to remove the order from your book
                 const amount = Precise.stringGt(price, '0') ? size : '0';
                 bookside.store(this.parseNumber(price), this.parseNumber(amount), id);
@@ -636,7 +636,7 @@ export default class bitfinex2 extends bitfinex2Rest {
                 const price = this.safeString(deltas, 0);
                 const size = Precise.stringLt(amount, '0') ? Precise.stringNeg(amount) : amount;
                 const side = Precise.stringLt(amount, '0') ? 'asks' : 'bids';
-                const bookside = orderbook[side];
+                const bookside = orderbookItem[side];
                 bookside.store(this.parseNumber(price), this.parseNumber(size), this.parseNumber(counter));
             }
             client.resolve(orderbook, messageHash);
