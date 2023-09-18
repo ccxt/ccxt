6f3014b111b6c047a46a37b87c5d2f20180224d9
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-09-18 16:32:13 +0100

    add pagination to bingx

--- ts/src/bingx.ts
+++ ts/src/bingx.ts
@@ -600,6 +600,11 @@ export default class bingx extends Exchange {
          * @returns {[[int]]} A list of candles ordered as timestamp, open, high, low, close, volume
          */
         await this.loadMarkets ();
+        let paginate = false;
+        [ paginate, params ] = this.handleOptionAndParams (params, 'fetchOHLCV', 'paginate', false);
+        if (paginate) {
+            return await this.fetchPaginatedCallDeterministic ('fetchOHLCV', symbol, since, limit, timeframe, params, 1440);
+        }
         const market = this.market (symbol);
         const request = {
             'symbol': market['id'],
@@ -612,6 +617,11 @@ export default class bingx extends Exchange {
             request['limit'] = limit;
         } else {
             request['limit'] = 50;
+        }
+        const until = this.safeInteger2 (params, 'until', 'startTime');
+        if (until !== undefined) {
+            params = this.omit (params, [ 'until' ]);
+            request['startTime'] = until;
         }
         let response = undefined;
         if (market['spot']) {
@@ -996,10 +1006,16 @@ export default class bingx extends Exchange {
          * @param {int} [since] timestamp in ms of the earliest funding rate to fetch
          * @param {int} [limit] the maximum amount of [funding rate structures]{@link https://github.com/ccxt/ccxt/wiki/Manual#funding-rate-history-structure} to fetch
          * @param {object} [params] extra parameters specific to the bingx api endpoint
+         * @param {int} [params.until] timestamp in ms of the latest funding rate to fetch
          * @returns {[object]} a list of [funding rate structures]{@link https://github.com/ccxt/ccxt/wiki/Manual#funding-rate-history-structure}
          */
         this.checkRequiredSymbol ('fetchFundingRateHistory', symbol);
         await this.loadMarkets ();
+        let paginate = false;
+        [ paginate, params ] = this.handleOptionAndParams (params, 'fetchFundingRateHistory', 'paginate');
+        if (paginate) {
+            return await this.fetchPaginatedCallDeterministic ('fetchFundingRateHistory', symbol, since, limit, '8h', params);
+        }
         const market = this.market (symbol);
         const request = {
             'symbol': market['id'],
@@ -1009,6 +1025,11 @@ export default class bingx extends Exchange {
         }
         if (limit !== undefined) {
             request['limit'] = limit;
+        }
+        const until = this.safeInteger2 (params, 'until', 'startTime');
+        if (until !== undefined) {
+            params = this.omit (params, [ 'until' ]);
+            request['startTime'] = until;
         }
         const response = await this.swapV2PublicGetQuoteFundingRate (this.extend (request, params));
         //
