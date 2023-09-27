67e6d170a7be1ce8de3bc83b1002f3c3a07a6004
Travis CI <travis@travis-ci.org>
2023-09-27 10:04:34 +0000

    4.0.108
    [ci skip]

--- dist/cjs/src/bitforex.js
+++ dist/cjs/src/bitforex.js
@@ -156,6 +156,7 @@ class bitforex extends bitforex$1 {
          * @method
          * @name bitforex#fetchMarkets
          * @description retrieves data on all markets for bitforex
+         * @see https://apidoc.bitforex.com/#exchange-information
          * @param {object} [params] extra parameters specific to the exchange api endpoint
          * @returns {object[]} an array of objects representing market data
          */
@@ -311,6 +312,7 @@ class bitforex extends bitforex$1 {
          * @method
          * @name bitforex#fetchTrades
          * @description get the list of most recent trades for a particular symbol
+         * @see https://apidoc.bitforex.com/#recent-trades-list
          * @param {string} symbol unified symbol of the market to fetch trades for
          * @param {int} [since] timestamp in ms of the earliest trade to fetch
          * @param {int} [limit] the maximum amount of trades to fetch
@@ -423,6 +425,7 @@ class bitforex extends bitforex$1 {
          * @method
          * @name bitforex#fetchBalance
          * @description query for balance and get the amount of funds available for trading or funds locked in orders
+         * @see https://apidoc.bitforex.com/#user-all-asset-information-user_data
          * @param {object} [params] extra parameters specific to the bitforex api endpoint
          * @returns {object} a [balance structure]{@link https://github.com/ccxt/ccxt/wiki/Manual#balance-structure}
          */
@@ -472,6 +475,7 @@ class bitforex extends bitforex$1 {
          * @method
          * @name bitforex#fetchTicker
          * @description fetches a price ticker, a statistical calculation with the information calculated over the past 24 hours for a specific market
+         * @see https://apidoc.bitforex.com/#exchange-information
          * @param {string} symbol unified symbol of the market to fetch the ticker for
          * @param {object} [params] extra parameters specific to the bitforex api endpoint
          * @returns {object} a [ticker structure]{@link https://github.com/ccxt/ccxt/wiki/Manual#ticker-structure}
@@ -526,6 +530,7 @@ class bitforex extends bitforex$1 {
          * @method
          * @name bitforex#fetchOHLCV
          * @description fetches historical candlestick data containing the open, high, low, and close price, and the volume of a market
+         * @see https://apidoc.bitforex.com/#kline
          * @param {string} symbol unified symbol of the market to fetch OHLCV data for
          * @param {string} timeframe the length of time each candle represents
          * @param {int} [since] timestamp in ms of the earliest candle to fetch
@@ -562,6 +567,7 @@ class bitforex extends bitforex$1 {
          * @method
          * @name bitforex#fetchOrderBook
          * @description fetches information on open orders with bid (buy) and ask (sell) prices, volumes and other data
+         * @see https://apidoc.bitforex.com/#order-book
          * @param {string} symbol unified symbol of the market to fetch the order book for
          * @param {int} [limit] the maximum amount of order book entries to return
          * @param {object} [params] extra parameters specific to the bitforex api endpoint
@@ -650,6 +656,7 @@ class bitforex extends bitforex$1 {
          * @method
          * @name bitforex#fetchOrder
          * @description fetches information on an order made by the user
+         * @see
          * @param {string} symbol unified symbol of the market the order was made in
          * @param {object} [params] extra parameters specific to the bitforex api endpoint
          * @returns {object} An [order structure]{@link https://github.com/ccxt/ccxt/wiki/Manual#order-structure}
@@ -715,6 +722,7 @@ class bitforex extends bitforex$1 {
          * @method
          * @name bitforex#createOrder
          * @description create a trade order
+         * @see https://apidoc.bitforex.com/#new-order-trade
          * @param {string} symbol unified symbol of the market to create an order in
          * @param {string} type 'market' or 'limit'
          * @param {string} side 'buy' or 'sell'
@@ -750,6 +758,7 @@ class bitforex extends bitforex$1 {
          * @method
          * @name bitforex#cancelOrder
          * @description cancels an open order
+         * @see https://apidoc.bitforex.com/#cancel-order-trade
          * @param {string} id order id
          * @param {string} symbol unified symbol of the market the order was made in
          * @param {object} [params] extra parameters specific to the bitforex api endpoint
--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -40,11 +40,11 @@ use Exception;
 
 include 'Throttle.php';
 
-$version = '4.0.107';
+$version = '4.0.108';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '4.0.107';
+    const VERSION = '4.0.108';
 
     public $browser;
     public $marketsLoading = null;
