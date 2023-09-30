5143d35c37889da23991d171bc949edd4382da0f
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-09-30 16:22:34 +0400

    further proxy updates [ci skip]

--- examples/ts/proxy-usage.ts
+++ examples/ts/proxy-usage.ts
@@ -5,24 +5,24 @@ import ccxt from '../../ts/ccxt.js';
 
 const targetSite = 'https://api.ipify.org/';
 
-async function example_1 () {
+async function example_proxyUrl () {
     const myEx = new ccxt.kucoin ();
     myEx.proxyUrl = 'http://5.75.153.75:8090/proxy.php?url=';
     console.log (await myEx.fetch (targetSite));
 }
 
-async function example_2 () {
+async function example_httpProxy () {
     const myEx = new ccxt.kucoin ();
     myEx.httpsProxy = 'http://5.75.153.75:8888'; // "httpProxy" or "httpsProxy" (depending on your proxy protocol)
     console.log (await myEx.fetch (targetSite));
 }
 
-async function example_3 () {
+async function example_socksProxy () {
     const myEx = new ccxt.kucoin ();
     myEx.socksProxy = 'socks5://127.0.0.1:1080'; // socks5 or socks5h
     console.log (await myEx.fetch (targetSite));
 }
 
-await example_1 ();
-// await example_2 ();
-// await example_3 ();
+await example_proxyUrl ();
+// await example_httpProxy ();
+// await example_socksProxy ();
