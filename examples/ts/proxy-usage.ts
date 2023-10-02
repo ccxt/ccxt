a6dc07e3bf595ced0b704342f5c62d60787533ee
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-10-02 12:39:33 +0400

    transpile & example

--- examples/ts/proxy-usage.ts
+++ examples/ts/proxy-usage.ts
@@ -27,10 +27,11 @@ async function example_webSockets () {
     const myEx = new ccxt.kucoin ();
     myEx.httpProxy = 'http://5.75.153.75:8888'; // "httpProxy" or "httpsProxy" (depending on your proxy protocol)
     await myEx.initializeProxies ();
-    myEx.handleMessage = function (client, message) {
-        console.log ('WS received:', message);
-    };
+    myEx.handleMessage = ws_helper_callback;
     await myEx.watch ('ws://5.75.153.75:9876', 'sample');
+}
+function ws_helper_callback (client, message) {
+    console.log ('WS received:', message);
 }
 
 // await example_proxyUrl ();
