e110cd7f032b45c7fd90583d0f4dda5a4ea90e09
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-10-02 20:12:32 +0400

    -

--- examples/ts/proxy-usage.ts
+++ examples/ts/proxy-usage.ts
@@ -26,7 +26,7 @@ async function example_socksProxy () {
 async function example_webSockets () {
     const myEx = new ccxt.kucoin ();
     myEx.httpProxy = 'http://5.75.153.75:8002'; // "httpProxy" or "httpsProxy" (depending on your proxy protocol)
-    await myEx.initializeProxies ();
+    await myEx.initializeProxies (); // only needed in JAVASCRIPT, not in PYTHON or PHP
     myEx.handleMessage = ws_helper_callback;
     await myEx.watch ('ws://5.75.153.75:9876', 'empty');
     console.log('WS proxy test finished');
