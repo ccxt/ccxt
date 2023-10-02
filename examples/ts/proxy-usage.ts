79aa97c5d58579f691ad66cf241db67055af7b5d
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-10-02 20:11:29 +0400

    proxy test sample

--- examples/ts/proxy-usage.ts
+++ examples/ts/proxy-usage.ts
@@ -1,7 +1,7 @@
 
 import ccxt from '../../ts/ccxt.js';
 
-// AUTO-TRANSPILE //
+// // AUTO-TRANSPILE //
 
 const targetSite = 'https://api.ipify.org/';
 
@@ -25,16 +25,18 @@ async function example_socksProxy () {
 
 async function example_webSockets () {
     const myEx = new ccxt.kucoin ();
-    myEx.httpProxy = 'http://5.75.153.75:8888'; // "httpProxy" or "httpsProxy" (depending on your proxy protocol)
+    myEx.httpProxy = 'http://5.75.153.75:8002'; // "httpProxy" or "httpsProxy" (depending on your proxy protocol)
     await myEx.initializeProxies ();
     myEx.handleMessage = ws_helper_callback;
-    await myEx.watch ('ws://5.75.153.75:9876', 'sample');
+    await myEx.watch ('ws://5.75.153.75:9876', 'empty');
+    console.log('WS proxy test finished');
 }
+
 function ws_helper_callback (client, message) {
     console.log ('WS received:', message);
 }
 
-// await example_proxyUrl ();
+await example_proxyUrl ();
 // await example_httpProxy ();
 // await example_socksProxy ();
-await example_webSockets ();
+// await example_webSockets ();
