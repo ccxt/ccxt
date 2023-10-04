219dca12cc71116bb5df133c613b72447a26c91c
Sam Germain <sam.germain@usask.ca>
2023-10-04 18:43:24 -0400

    feat(kucoinfutures): fetchTime uses safeInteger instead of safeNumber

--- ts/src/kucoinfutures.ts
+++ ts/src/kucoinfutures.ts
@@ -530,7 +530,7 @@ export default class kucoinfutures extends kucoin {
         //        data: 1637385119302,
         //    }
         //
-        return this.safeNumber (response, 'data');
+        return this.safeInteger (response, 'data');
     }
 
     async fetchOHLCV (symbol: string, timeframe = '1m', since: Int = undefined, limit: Int = undefined, params = {}) {
