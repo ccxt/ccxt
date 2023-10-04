2cd3885d809e28ee576656cd0320323e9a69a06b
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-10-04 14:16:01 +0400

    extend

--- examples/php/method-overload-override.php
+++ examples/php/method-overload-override.php
@@ -6,10 +6,28 @@ error_reporting(E_ALL | E_STRICT);
 date_default_timezone_set('UTC');
 // -----------------------------------------------------------------------------
 
+// ###############################################
+// ####### APPROACH 1: Overload the method #######
+// ###############################################
+function my_1() {
+    $ex = new \ccxt\kucoin();
+    $ex->add_method('fetch_ticker', function($symbol, $params = []) {
+        return 'hello world';
+    });
+    var_dump($ex->call_method('fetch_ticker', ['BTC/USDT']));
+}
+my_1();
 
-$ex = new \ccxt\kucoin();
-$ex->add_method('fetch_tickers', function ($params = null) {
-    return 'hello world';
-});
-var_dump($ex->call_method('fetch_tickers'));
 
+// ###############################################
+// ####### APPROACH 2: extend the class    #######
+// ###############################################
+function my_2() {
+    $ex = new class extends \ccxt\kucoin {
+        public function fetch_ticker($symbol, $params = []) {
+            return 'Hello from the anonymous class!';
+        }
+    };
+    $ex->fetch_ticker('fetch_ticker', ['BTC/USDT']);
+}
+my_2();
