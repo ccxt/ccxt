351b295b7f34b1139c5119cd678ee30c5baf940b
Carlos Gonçalves <43336371+carlosmiei@users.noreply.github.com>
2023-10-05 10:13:35 +0100

    Update ts/src/base/Exchange.ts
    Co-authored-by: Pablo Criado-Perez <pablo@criadoperez.com>

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -4698,7 +4698,7 @@ export default class Exchange {
                 }
             } catch (e) {
                 errors += 1;
-                if (i > maxRetries) {
+                if (errors > maxRetries) {
                     throw e;
                 }
             }
