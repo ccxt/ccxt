5fbc16389e3cef4ed40e7d79b71d2570d59bdf6c
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-10-06 01:38:19 +0400

    upd proxyUrl [ci skip]

--- ts/src/test/Exchange/test.proxyUrl.ts
+++ ts/src/test/Exchange/test.proxyUrl.ts
@@ -6,7 +6,9 @@ async function testProxyUrl (exchange, skippedProperties) {
     const proxyServerIp = '5.75.153.75';
     const currentProxyValue = exchange.proxyUrl;
     exchange.proxyUrl = 'http://' + proxyServerIp + ':8090/proxy.php?url=';
-    const ipCheckUrl = 'https://api.ipify.org/';
+    const encodedColon = '%3A';
+    const encodedSlash = '%2F';
+    const ipCheckUrl = 'https' + encodedColon + encodedSlash + encodedSlash + 'api.ipify.org';
     const response = await exchange.fetch (ipCheckUrl);
     assert (response === proxyServerIp, exchange.id + ' ' + method + ' test failed. Returned response is ' + response + ' while it should be "' + proxyServerIp + '"');
     // reset the instance property
