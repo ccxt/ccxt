f0a0459e7eba93704c52142c715a8cf04fe165dc
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-10-08 20:25:18 +0400

    port 8002

--- ts/src/test/Exchange/test.httpProxy.ts
+++ ts/src/test/Exchange/test.httpProxy.ts
@@ -5,7 +5,7 @@ async function testHttpProxy (exchange, skippedProperties) {
     const method = 'httpProxy';
     const proxyServerIp = '5.75.153.75';
     const currentProxyValue = exchange.httpProxy;
-    exchange.httpProxy = 'http://' + proxyServerIp + ':8888';
+    exchange.httpProxy = 'http://' + proxyServerIp + ':8002';
     const ipCheckUrl = 'https://api.ipify.org/';
     const response = await exchange.fetch (ipCheckUrl);
     assert (response === proxyServerIp, exchange.id + ' ' + method + ' test failed. Returned response is ' + response + ' while it should be "' + proxyServerIp + '"');
