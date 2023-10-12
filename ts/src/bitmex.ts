36edd62126c2dcf613c9dbb362f50fb62d538b28
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-10-13 00:05:02 +0400

    feat(bitmex) - pair created ts

--- ts/src/bitmex.ts
+++ ts/src/bitmex.ts
@@ -664,6 +664,7 @@ export default class bitmex extends Exchange {
                         'max': positionIsQuote ? maxOrderQty : undefined,
                     },
                 },
+                'created': this.parse8601 (this.safeString (market, 'listing')),
                 'info': market,
             });
         }
