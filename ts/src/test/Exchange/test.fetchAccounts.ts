b4d14d8f09c97557e8891e9de51278a02ba5725c
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-10-12 18:03:39 +0100

    fix accounts and borrow rate

--- ts/src/test/Exchange/test.fetchAccounts.ts
+++ ts/src/test/Exchange/test.fetchAccounts.ts
@@ -6,8 +6,7 @@ async function testFetchAccounts (exchange, skippedProperties) {
     const method = 'fetchAccounts';
     const accounts = await exchange.fetchAccounts ();
     assert (Array.isArray (accounts), exchange.id + ' ' + method + ' must return an object. ' + exchange.json (accounts));
-    const accountValues = Object.values (accounts);
-    for (let i = 0; i < accountValues.length; i++) {
+    for (let i = 0; i < accounts.length; i++) {
         testAccount (exchange, skippedProperties, method, accounts[i]);
     }
 }
