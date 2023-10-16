9ec5e13577b0a7c58dffb5b3342d0135f1b89e0a
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-10-16 14:03:58 +0400

    fix

--- ts/src/kucoinfutures.ts
+++ ts/src/kucoinfutures.ts
@@ -509,7 +509,7 @@ export default class kucoinfutures extends kucoin {
                         'max': this.safeNumber (market, 'quoteMaxSize'),
                     },
                 },
-                'created': this.parse8601 (this.safeString (market, 'firstOpenDate')),
+                'created': this.safeInteger (market, 'firstOpenDate'),
                 'info': market,
             });
         }
