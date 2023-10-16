306bdf9d499fbce967764d181ae41b5e3bae029c
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-10-16 17:39:18 +0400

    remove created

--- ts/src/test/Exchange/base/test.market.ts
+++ ts/src/test/Exchange/base/test.market.ts
@@ -51,10 +51,9 @@ function testMarket (exchange, skippedProperties, method, market) {
                 'max': exchange.parseNumber ('1000'), // order cost should be < max
             },
         },
-        'created': 1601234567890, // pair "creation" timestamp
         'info': {},
     };
-    const emptyAllowedFor = [ 'linear', 'inverse', 'settle', 'settleId', 'expiry', 'expiryDatetime', 'optionType', 'strike', 'margin', 'contractSize', 'created' ];
+    const emptyAllowedFor = [ 'linear', 'inverse', 'settle', 'settleId', 'expiry', 'expiryDatetime', 'optionType', 'strike', 'margin', 'contractSize' ];
     testSharedMethods.assertStructure (exchange, skippedProperties, method, market, format, emptyAllowedFor);
     testSharedMethods.assertSymbol (exchange, skippedProperties, method, market, 'symbol');
     const logText = testSharedMethods.logTemplate (exchange, method, market);
@@ -197,7 +196,6 @@ function testMarket (exchange, skippedProperties, method, market) {
         testSharedMethods.assertValidCurrencyIdAndCode (exchange, skippedProperties, method, market, market['quoteId'], market['quote']);
         testSharedMethods.assertValidCurrencyIdAndCode (exchange, skippedProperties, method, market, market['settleId'], market['settle']);
     }
-    testSharedMethods.assertTimestamp (exchange, skippedProperties, method, market, undefined, 'created');
 }
 
 export default testMarket;
