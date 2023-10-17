164c6597b8e16cf921ce4ed1bac1405e11f502d0
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-10-17 19:11:10 +0400

    fix(woo) - safeTimestamp pair creation

--- ts/src/woo.ts
+++ ts/src/woo.ts
@@ -403,7 +403,7 @@ export default class woo extends Exchange {
                         'max': undefined,
                     },
                 },
-                'created': this.safeIntegerProduct (market, 'created_time', 1000),
+                'created': this.safeTimestamp (market, 'created_time'),
                 'info': market,
             });
         }
