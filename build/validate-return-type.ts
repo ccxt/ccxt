a8563808051e7a6f468d7dbe8f2a98f5b5616542
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-10-18 17:26:36 +0100

    add skipMethods and skipExchanges

--- build/validate-return-type.ts
+++ build/validate-return-type.ts
@@ -1,6 +1,17 @@
 
 import ts from 'typescript';
 import fs from 'fs'
+import log from 'ololog'
+
+const skipMethods = [
+    'fetchMarkets'
+]
+
+const skipExchanges = [
+    'someExchange'
+    // place exchanges here
+]
+
 
 const exchanges = JSON.parse (fs.readFileSync("./exchanges.json", "utf8")).ids;
 
@@ -38,28 +49,51 @@ function isUserFacingMethod(method: string) {
         || method.startsWith('deposit');
 }
 
+function isUknownReturnType(type: string) {
+    return type === 'any'
+        || type === 'Promise<{}>'
+        || type === 'Promise<unknown>'
+        || type === 'Promise<any>'
+        || type.startsWith('{')
+        || type.startsWith('Promise<{')
+}
+
+
 function main() {
     const basePath = './js/src/';
 
-    const sourceOfTruth = extractMethodsAndReturnTypes(basePath + 'Exchange.d.ts');
-
+    const sourceOfTruth = extractMethodsAndReturnTypes(basePath + 'base/Exchange.d.ts');
+    let foundIssues = false;
     for (const exchange of exchanges) {
+        if (skipExchanges.includes(exchange)) {
+            continue;
+        }
         const path = basePath + exchange + '.d.ts';
         const methodsInfo = extractMethodsAndReturnTypes(path);
         for (const method in methodsInfo) {
+            if (skipMethods.includes(method)) {
+                continue;
+            }
             const returnType = methodsInfo[method];
             if (method in sourceOfTruth) {
+                const targetReturnType = sourceOfTruth[method];
                 if (isUserFacingMethod(method)) {
-                    if (returnType.indexOf('any') === -1 && !returnType.startsWith('{')) { // ignore any/untyped methods
+                    if (!isUknownReturnType(targetReturnType)) { // ignore any/untyped methods
                         if (sourceOfTruth[method] !== returnType) {
-                            console.log('Difference found', exchange, method, returnType, sourceOfTruth[method]);
+                            foundIssues = true;
+                            log.magenta('Difference found', exchange, method, returnType, targetReturnType);
                         }
                     }
                 }
             }
         }
     }
-
+    if (!foundIssues) {
+        log.bright.green('No type differences found');
+    } else {
+        log.bright.red('Type differences found');
+        process.exit(1);
+    }
 }
 
 main()
