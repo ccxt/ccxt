abb29106a5b44609635ba790431a57ebc7721a8c
Pablo Criado-Perez <pablo@criadoperez.com>
2023-10-29 23:48:28 +0100

    docs: fix build docs and serve

--- ts/src/binance.ts
+++ ts/src/binance.ts
@@ -5079,13 +5079,13 @@ export default class binance extends Exchange {
          * @description cancel multiple orders
          * @see https://binance-docs.github.io/apidocs/futures/en/#cancel-multiple-orders-trade
          * @see https://binance-docs.github.io/apidocs/delivery/en/#cancel-multiple-orders-trade
-         * @param {[string]} ids order ids
+         * @param {string[]} ids order ids
          * @param {string} [symbol] unified market symbol
          * @param {object} [params] extra parameters specific to the bingx api endpoint
          *
          * EXCHANGE SPECIFIC PARAMETERS
-         * @param {[string]} [params.origClientOrderIdList] max length 10 e.g. ["my_id_1","my_id_2"], encode the double quotes. No space after comma
-         * @param {[int]} [params.recvWindow]
+         * @param {string[]} [params.origClientOrderIdList] max length 10 e.g. ["my_id_1","my_id_2"], encode the double quotes. No space after comma
+         * @param {int[]} [params.recvWindow]
          * @returns {object} an list of [order structures]{@link https://docs.ccxt.com/#/?id=order-structure}
          */
         this.checkRequiredSymbol ('cancelOrders', symbol);
