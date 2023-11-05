d0f5ae5632615269b640f0d731f912d9eba0c800
pcriadoperez <pablo@criadoperez.com>
2023-11-05 01:45:15 +0100

    remove abstract file

--- ts/src/abstract/krakenfutures.ts
+++ ts/src/abstract/krakenfutures.ts
@@ -9,13 +9,11 @@ import { implicitReturnType } from '../base/types.js';
 import { Exchange as _Exchange } from '../base/Exchange.js';
 
 interface Exchange {
-    publicGetFeeschedules (params?: {}): Promise<implicitReturnType>;
     publicGetInstruments (params?: {}): Promise<implicitReturnType>;
     publicGetOrderbook (params?: {}): Promise<implicitReturnType>;
     publicGetTickers (params?: {}): Promise<implicitReturnType>;
     publicGetHistory (params?: {}): Promise<implicitReturnType>;
     publicGetHistoricalfundingrates (params?: {}): Promise<implicitReturnType>;
-    privateGetFeeschedulesVolumes (params?: {}): Promise<implicitReturnType>;
     privateGetOpenpositions (params?: {}): Promise<implicitReturnType>;
     privateGetNotifications (params?: {}): Promise<implicitReturnType>;
     privateGetAccounts (params?: {}): Promise<implicitReturnType>;
@@ -42,6 +40,7 @@ interface Exchange {
     historyGetAccountlogcsv (params?: {}): Promise<implicitReturnType>;
     historyGetMarketSymbolOrders (params?: {}): Promise<implicitReturnType>;
     historyGetMarketSymbolExecutions (params?: {}): Promise<implicitReturnType>;
+    feeschedulesGetVolumes (params?: {}): Promise<implicitReturnType>;
 }
 abstract class Exchange extends _Exchange {}
 
