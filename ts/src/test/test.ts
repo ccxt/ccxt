10077e4449bfd9463184d380ed1eb4cac2b6475f
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-11-07 15:38:11 +0000

    fix(tests): skip signIn call

--- ts/src/test/test.ts
+++ ts/src/test/test.ts
@@ -952,7 +952,7 @@ export default class testMainClass extends baseMainTestClass {
 
     initOfflineExchange (exchangeName: string) {
         const markets = this.loadMarketsFromFile (exchangeName);
-        return initExchange (exchangeName, { 'markets': markets, 'rateLimit': 1, 'httpsProxy': 'http://fake:8080', 'apiKey': 'key', 'secret': 'secretsecret', 'password': 'password', 'uid': 'uid', 'accounts': [ { 'id': 'myAccount' } ], 'options': { 'enableUnifiedAccount': true, 'enableUnifiedMargin': false }});
+        return initExchange (exchangeName, { 'markets': markets, 'rateLimit': 1, 'httpsProxy': 'http://fake:8080', 'apiKey': 'key', 'secret': 'secretsecret', 'password': 'password', 'uid': 'uid', 'accounts': [ { 'id': 'myAccount' } ], 'options': { 'enableUnifiedAccount': true, 'enableUnifiedMargin': false, 'accessToken': 'token', 'expires': 999999999999999 }});
     }
 
     async testExchangeStatically (exchangeName: string, exchangeData: object, testName: string = undefined) {
