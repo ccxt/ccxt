bbaa22d0c4057f7446bbd609fa0fba64f879b1ea
Carlo Revelli <carlo.revelli@berkeley.edu>
2023-11-07 13:59:46 -0800

    minor coinlist fix

--- ts/src/coinlist.ts
+++ ts/src/coinlist.ts
@@ -485,7 +485,7 @@ export default class coinlist extends Exchange {
          * @name coinlist#fetchTickers
          * @description fetches price tickers for multiple markets, statistical calculations with the information calculated over the past 24 hours each market
          * @see https://trade-docs.coinlist.co/?javascript--nodejs#get-symbol-summaries
-         * @param {string[]|} [symbols] unified symbols of the markets to fetch the ticker for, all market tickers are returned if not assigned
+         * @param {string[]} [symbols] unified symbols of the markets to fetch the ticker for, all market tickers are returned if not assigned
          * @param {object} [params] extra parameters specific to the coinlist api endpoint
          * @returns {object} a dictionary of [ticker structures]{@link https://github.com/ccxt/ccxt/wiki/Manual#ticker-structure}
          */
