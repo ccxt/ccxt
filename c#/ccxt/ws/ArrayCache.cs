20214a314222c24e71446f82697cf4fe96aa312c
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-11-16 17:38:31 +0000

    several array cache fixes

--- c#/ccxt/ws/ArrayCache.cs
+++ c#/ccxt/ws/ArrayCache.cs
@@ -6,113 +6,285 @@ namespace ccxt;
 
 using dict = Dictionary<string, object>;
 
-public partial class Exchange
+// public partial class Exchange
+// {
+
+public class BaseCache : List<object>
 {
+    // Add any custom properties or methods
+    public int? maxSize;
 
-    public class BaseCache<T> : List<T>
+    // public int? length;
+    public BaseCache(int? maxSixe = null) : base()
     {
-        // Add any custom properties or methods
-        public int? maxSize;
+        this.maxSize = maxSixe;
 
-        public int? length;
-        public BaseCache(int? maxSixe = null) : base()
-        {
-            this.maxSize = maxSixe;
-
-        }
-
-        public void clear()
-        {
-            this.length = 0;
-        }
     }
 
-    public class ArrayCache<T> : BaseCache<T>
+    public void clear()
     {
-        // Add any custom properties or methods
-        private bool nestedNewUpdatesBySymbol;
-        private Dictionary<string, object> newUpdatesBySymbol;
-        private Dictionary<string, object> clearUpdatesBySymbol;
-        private int allNewUpdates;
-        private bool clearAllUpdates;
-        public ArrayCache(int? maxSixe = null) : base(maxSixe)
+        // this.Count = 0;
+        this.Clear();
+    }
+}
+
+public class ArrayCache : BaseCache
+{
+    // Add any custom properties or methods
+    protected bool nestedNewUpdatesBySymbol;
+    protected Dictionary<string, object> newUpdatesBySymbol = new Dictionary<string, object>();
+    protected Dictionary<string, object> clearUpdatesBySymbol = new Dictionary<string, object>();
+    protected int allNewUpdates;
+    protected bool clearAllUpdates;
+    public ArrayCache(int? maxSixe = null) : base(maxSixe)
+    {
+
+    }
+
+    public int getLimit(object symbol2, object limit2)
+    {
+        // var limit = (int)limit2;
+        int? newUpdatesValue = null;
+
+        if (symbol2 == null)
         {
-
+            newUpdatesValue = this.allNewUpdates;
+            this.clearAllUpdates = true;
         }
-
-        public int getLimit(object symbol2, object limit2)
+        else
         {
             var symbol = symbol2.ToString();
-            var limit = (int)limit2;
-            int? newUpdatesValue = null;
-
-            if (symbol == null)
+            var tempNewUpdates = this.newUpdatesBySymbol[symbol];
+            if ((tempNewUpdates != null) && this.nestedNewUpdatesBySymbol)
             {
-                newUpdatesValue = this.allNewUpdates;
-                this.clearAllUpdates = true;
+                newUpdatesValue = ((HashSet<object>)tempNewUpdates).Count;
             }
-            else
-            {
-                var tempNewUpdates = this.newUpdatesBySymbol[symbol];
-                if ((tempNewUpdates != null) && this.nestedNewUpdatesBySymbol)
-                {
-                    newUpdatesValue = ((HashSet<int>)tempNewUpdates).Count;
-                }
-                this.clearUpdatesBySymbol[symbol] = true;
-            }
-
-            if (newUpdatesValue == null)
-            {
-                return limit;
-            }
-            else if (limit != null)
-            {
-                return Math.Min((int)newUpdatesValue, limit);
-            }
-            else
-            {
-                return (int)newUpdatesValue;
-            }
+            this.clearUpdatesBySymbol[symbol] = true;
         }
 
-        public void append(object item)
+        if (newUpdatesValue == null)
         {
-            if (this.maxSize != null && this.length == this.maxSize)
-            {
-                this.RemoveAt(0);
-            }
-            this.Add((T)item);
-            if (this.clearAllUpdates)
-            {
-                this.clearAllUpdates = false;
-                this.clearUpdatesBySymbol = new Dictionary<string, object>();
-                this.allNewUpdates = 0;
-                this.newUpdatesBySymbol = new Dictionary<string, object>();
-            }
-
-            var itemSymbol = Exchange.SafeString(item, "symbol");
-            var clearUpdateBySymbol = (this.clearUpdatesBySymbol.ContainsKey(itemSymbol)) ? this.clearUpdatesBySymbol[itemSymbol] : null;
-            if (clearUpdateBySymbol != null)
-            {
-                this.clearUpdatesBySymbol[itemSymbol] = false;
-                this.newUpdatesBySymbol[itemSymbol] = 0;
-            }
-            var defaultValue = (this.newUpdatesBySymbol.ContainsKey(itemSymbol)) ? (int)this.newUpdatesBySymbol[itemSymbol] : 0;
-            this.newUpdatesBySymbol[itemSymbol] = defaultValue + 1;
-            this.allNewUpdates = this.allNewUpdates + 1;
-
+            return (int)limit2;
+        }
+        else if (limit2 != null)
+        {
+            return Math.Min((int)newUpdatesValue, (int)limit2);
+        }
+        else
+        {
+            return (int)newUpdatesValue;
         }
     }
 
-
-    public class ArrayCacheByTimestamp<T> : BaseCache<T>
+    public void append(object item)
     {
-        public Dictionary<string, object> hashmap = new Dictionary<string, object>();
-        public HashSet<int> sizeTracker = new HashSet<int>();
-        public ArrayCacheByTimestamp(int? maxSixe = null) : base(maxSixe)
+        if (this.maxSize != null && this.Count == this.maxSize)
         {
-
+            this.RemoveAt(0);
         }
+        this.Add(item);
+        if (this.clearAllUpdates)
+        {
+            this.clearAllUpdates = false;
+            this.clearUpdatesBySymbol = new Dictionary<string, object>();
+            this.allNewUpdates = 0;
+            this.newUpdatesBySymbol = new Dictionary<string, object>();
+        }
+
+        var itemSymbol = Exchange.SafeString(item, "symbol");
+        var clearUpdateBySymbol = (this.clearUpdatesBySymbol.ContainsKey(itemSymbol)) ? this.clearUpdatesBySymbol[itemSymbol] : null;
+        if (clearUpdateBySymbol != null)
+        {
+            this.clearUpdatesBySymbol[itemSymbol] = false;
+            this.newUpdatesBySymbol[itemSymbol] = 0;
+        }
+        var defaultValue = (this.newUpdatesBySymbol.ContainsKey(itemSymbol)) ? (int)this.newUpdatesBySymbol[itemSymbol] : 0;
+        this.newUpdatesBySymbol[itemSymbol] = defaultValue + 1;
+        this.allNewUpdates = this.allNewUpdates + 1;
+
+    }
+}
+
+
+public class ArrayCacheByTimestamp : BaseCache
+{
+    public Dictionary<string, object> hashmap = new Dictionary<string, object>();
+    public HashSet<object> sizeTracker = new HashSet<object>();
+
+    public int newUpdates = 0;
+
+    public bool clearUpdates = false;
+
+    public ArrayCacheByTimestamp(int? maxSixe = null) : base(maxSixe)
+    {
     }
 
+    public int getLimit(object symbol, object limit2)
+    {
+        if (limit2 == null)
+        {
+            return this.newUpdates;
+        }
+        var limit = (int)limit2;
+        return Math.Min(this.newUpdates, limit);
+    }
+
+    public void append(object item)
+    {
+        var firstValue = Exchange.SafeString(item, 0);
+        if (firstValue != null && (this.hashmap.ContainsKey(firstValue)))
+        {
+            var prevRef = this.hashmap[firstValue];
+            var index = this.IndexOf(prevRef);
+            this.hashmap[firstValue] = item; // check this out
+            this[index] = item;
+
+        }
+        else
+        {
+            this.hashmap[firstValue.ToString()] = item;
+            if (this.maxSize != null && (this.Count == this.maxSize))
+            {
+                var deletedReference = this[0];
+                var deletedFirstValue = Exchange.SafeValue(deletedReference, 0);
+                this.hashmap.Remove(deletedFirstValue.ToString());
+            }
+            this.Add(item);
+        }
+
+        if (this.clearUpdates)
+        {
+            this.clearUpdates = false;
+            this.sizeTracker.Clear();
+        }
+        this.sizeTracker.Add(firstValue);
+        this.newUpdates = this.sizeTracker.Count;
+    }
 }
+
+
+public class ArrayCacheBySymbolById : ArrayCache
+{
+    public Dictionary<string, object> hashmap = new Dictionary<string, object>();
+    // public Deque<int> index = new Queue<int>();
+    public ArrayCacheBySymbolById(int? maxSixe = null) : base(maxSixe)
+    {
+        this.nestedNewUpdatesBySymbol = true;
+    }
+
+    public void append(object item)
+    {
+        var itemSymbol = Exchange.SafeString(item, "symbol");
+        var itemId = Exchange.SafeString(item, "id");
+        var byId = (this.hashmap.ContainsKey(itemSymbol)) ? this.hashmap[itemSymbol] as Dictionary<string, object> : new Dictionary<string, object>();
+        this.hashmap[itemSymbol] = byId;
+        if (byId.ContainsKey(itemId))
+        {
+            var reference = byId[itemId];
+            if (reference != item)
+            {
+                reference = item;
+            }
+            item = reference;
+            // index = new Queue<int>(index.Where(x => x != (int)reference));
+            // this.index.Remove
+            var value = this.Find(x => Exchange.SafeString(x, "id") == itemId);
+            var indexInt = this.IndexOf(value);
+            this.RemoveAt(indexInt);
+            // this.index.Remove(indexInt);
+        }
+        else
+        {
+            byId[itemId] = item;
+        }
+
+        if (this.maxSize != null && (this.Count == this.maxSize))
+        {
+            var first = this[0];
+            this.RemoveAt(0);
+            var deletedSymbol = Exchange.SafeString(first, "symbol");
+            var deletedId = Exchange.SafeString(first, "id");
+            var secondHashMap = (this.hashmap[deletedSymbol] as Dictionary<string, object>);
+            if (secondHashMap != null)
+            {
+                secondHashMap.Remove(deletedId);
+            }
+        }
+        this.Add(item);
+
+        if (this.clearAllUpdates)
+        {
+            this.clearAllUpdates = false;
+            this.clearUpdatesBySymbol = new Dictionary<string, object>();
+            this.allNewUpdates = 0;
+            this.newUpdatesBySymbol = new Dictionary<string, object>();
+        }
+
+        var newUpdatesBySymbolValue = (this.newUpdatesBySymbol.ContainsKey(itemSymbol)) ? this.newUpdatesBySymbol[itemSymbol] : null;
+        if (newUpdatesBySymbolValue == null)
+        {
+            this.newUpdatesBySymbol[itemSymbol] = new HashSet<object>();
+        }
+
+        var clearUpdatesBySymbolValue = (this.clearUpdatesBySymbol.ContainsKey(itemSymbol)) ? this.clearUpdatesBySymbol[itemSymbol] : null;
+        if (clearUpdatesBySymbolValue != null)
+        {
+            this.clearUpdatesBySymbol[itemSymbol] = false;
+            (this.newUpdatesBySymbol[itemSymbol] as HashSet<object>).Clear();
+        }
+
+        // in case an exchange updates the same order id twice
+        var idSet = this.newUpdatesBySymbol[itemSymbol] as HashSet<object>;
+        var beforeLength = idSet.Count;
+        idSet.Add(itemId);
+        var afterLength = idSet.Count;
+        var defaultAllNewUpdates = (this.allNewUpdates == null) ? 0 : this.allNewUpdates;
+        this.allNewUpdates = defaultAllNewUpdates + (afterLength - beforeLength);
+    }
+}
+
+public class ArrayCacheBySymbolBySide : ArrayCache
+{
+    // tbd incomplete
+    public Dictionary<string, object> hashmap = new Dictionary<string, object>();
+    public ArrayCacheBySymbolBySide(int? maxSixe = null) : base(maxSixe)
+    {
+        this.nestedNewUpdatesBySymbol = true;
+    }
+
+    public void append(object item)
+    {
+        var itemSymbol = Exchange.SafeString(item, "symbol");
+        var itemSide = Exchange.SafeString(item, "side");
+        var bySide = this.hashmap[itemSymbol] as Dictionary<string, object>;
+        if (bySide.ContainsKey(itemSide))
+        {
+            var reference = bySide[itemSide];
+            if (reference != item)
+            {
+                reference = item;
+            }
+            item = reference;
+        }
+        else
+        {
+            bySide[itemSide] = item;
+        }
+
+        if (this.maxSize != null && (this.Count == this.maxSize))
+        {
+            var first = this[0];
+            this.RemoveAt(0);
+            var deletedSymbol = Exchange.SafeString(first, "symbol");
+            var deletedSide = Exchange.SafeString(first, "side");
+            var secondHashMap = (this.hashmap[deletedSymbol] as Dictionary<string, object>)[deletedSide] as Dictionary<string, object>;
+            if (secondHashMap != null)
+            {
+                secondHashMap.Remove(deletedSide);
+            }
+        }
+        this.Add(item);
+
+    }
+}
+// }
