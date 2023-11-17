a4f286e7f6d6af24f1a1cef11d75c4fba7c152f5
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-11-17 14:38:51 +0000

    finish basic structures

--- c#/ccxt/ws/OrderBook.cs
+++ c#/ccxt/ws/OrderBook.cs
@@ -36,22 +36,22 @@ public class OrderBook : dict
         defaults["nonce"] = Exchange.SafeValue(snapshotCopy, "nonce", defaults["nonce"]);
         defaults["symbol"] = Exchange.SafeValue(snapshotCopy, "symbol", defaults["symbol"]);
 
-        if (!(defaults["bids"] is OrderBookSide))
-        {
-            defaults["bids"] = new Bids(defaults["bids"], depth);
-            this.bids = defaults["bids"] as Bids;
-        }
-
-        if (!(defaults["asks"] is Asks))
-        {
-            defaults["asks"] = new Asks(defaults["asks"], depth);
-            this.asks = defaults["asks"] as Asks;
-        }
-
         // this.Merge(defaults);
         for (var i = 0; i < defaults.Count; i++)
         {
             this.Add(defaults.Keys.ElementAt(i), defaults.Values.ElementAt(i));
+        }
+
+        if (!(this["bids"] is OrderBookSide))
+        {
+            this["bids"] = new Bids(this["bids"], depth);
+            this.bids = this["bids"] as Bids;
+        }
+
+        if (!(this["asks"] is Asks))
+        {
+            this["asks"] = new Asks(this["asks"], depth);
+            this.asks = this["asks"] as Asks;
         }
     }
 
@@ -60,5 +60,65 @@ public class OrderBook : dict
         this.asks.limit();
         this.bids.limit();
         return this;
+    }
+
+    public OrderBook update(object snapshot)
+    {
+        var snapshotNonce = Exchange.SafeValue(snapshot as dict, "nonce");
+        if (snapshotNonce != null && this["nonce"] != null && (long)snapshotNonce <= (long)this["nonce"])
+        {
+            return this;
+        }
+
+        this["nonce"] = snapshotNonce;
+        this["timestamp"] = Exchange.SafeValue(snapshot as dict, "timestamp", this["timestamp"]);
+        this["datetime"] = Exchange.SafeValue(snapshot as dict, "datetime", this["datetime"]);
+        this.reset(snapshot);
+        return null;
+    }
+
+    public void reset(object snapshot = null)
+    {
+        this.asks._index.Clear();
+        this.asks.Clear();
+
+        var snapshotAsks = Exchange.SafeValue(snapshot as dict, "asks") as List<object>;
+        for (var i = 0; i < snapshotAsks.Count; i++)
+        {
+            this.asks.storeArray(snapshotAsks[i] as object[]);
+        }
+
+        this.bids._index.Clear();
+        this.bids.Clear();
+        var snapshotBids = Exchange.SafeValue(snapshot as dict, "bids") as List<object>;
+        for (var i = 0; i < snapshotBids.Count; i++)
+        {
+            this.bids.storeArray(snapshotBids[i] as object[]);
+        }
+        this["nonce"] = Exchange.SafeValue(snapshot as dict, "nonce", this["nonce"]);
+        this["timestamp"] = Exchange.SafeValue(snapshot as dict, "timestamp", this["timestamp"]);
+        this["datetime"] = Exchange.Iso8601(Exchange.SafeValue(snapshot as dict, "datetime", this["datetime"]));
+        this["symbol"] = Exchange.SafeValue(snapshot as dict, "symbol", this["symbol"]);
+    }
+}
+
+public class CountedOrderBook : OrderBook
+{
+    public CountedOrderBook(object snapshot = null, object depth2 = null) : base(Exchange.Extend(snapshot, new dict {
+       {"asks", new CountedOrderBookSide(Exchange.SafeValue(snapshot, "asks", new List<object>()), depth2)},
+       {"bids", new CountedOrderBookSide(Exchange.SafeValue(snapshot, "bids", new List<object>()), depth2)}
+    }), depth2)
+    {
+    }
+}
+
+
+public class IndexedOrderBook : OrderBook
+{
+    public IndexedOrderBook(object snapshot = null, object depth2 = null) : base(Exchange.Extend(snapshot, new dict {
+       {"asks", new IndexedAsks(Exchange.SafeValue(snapshot, "asks", new List<object>()), depth2)},
+       {"bids", new IndexedBids(Exchange.SafeValue(snapshot, "bids", new List<object>()), depth2)}
+    }), depth2)
+    {
     }
 }
