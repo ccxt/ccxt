125e79077e6be3caae55eb08ec6b2fc0b2edb19d
Sam Germain <sam.germain@usask.ca>
2023-11-21 09:08:55 -0500

    feat(gate): closePositions method implementation

--- ts/src/gate.ts
+++ ts/src/gate.ts
@@ -6925,6 +6925,21 @@ export default class gate extends Exchange {
         };
     }
 
+    async closePositions (symbol: string, side: OrderSide = undefined, params = {}): Promise<Order[]> {
+        /**
+         * @method
+         * @name gate#closePositions
+         * @description closes open positions for a market
+         * @see https://www.okx.com/docs-v5/en/#order-book-trading-trade-post-close-positions
+         * @param {string} symbol Unified CCXT market symbol
+         * @param {string} side 'buy' or 'sell'
+         * @param {object} [params] extra parameters specific to the okx api endpoint
+         * @returns {[object]} [A list of position structures]{@link https://docs.ccxt.com/#/?id=position-structure}
+         */
+        const order = await this.createOrder (symbol, 'market', side, 0, undefined, params);
+        return [ order ];
+    }
+
     handleErrors (code, reason, url, method, headers, body, response, requestHeaders, requestBody) {
         if (response === undefined) {
             return undefined;
