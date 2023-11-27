6c754003fd2f77fe81e070db23ae14337fe1acc3
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-11-27 12:57:56 +0000

    fix(Exchange): check_conflicting_proxies snake case

--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -564,7 +564,7 @@ class Exchange(object):
             proxies['http'] = socksProxy
             proxies['https'] = socksProxy
         proxyAgentSet = proxies is not None
-        self.checkConflictingProxies(proxyAgentSet, proxyUrl)
+        self.check_conflicting_proxies(proxyAgentSet, proxyUrl)
         # specifically for async-python, there is ".proxies" property maintained
         if (self.proxies is not None):
             if (proxyAgentSet or proxyUrl):
