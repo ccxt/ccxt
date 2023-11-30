7557119eb19681f420e1030db0caa013fd056557
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-11-30 16:30:44 +0000

    okx fix

--- ts/src/pro/okx.ts
+++ ts/src/pro/okx.ts
@@ -736,7 +736,7 @@ export default class okx extends okxRest {
             const message = this.extend (request, params);
             this.watch (url, messageHash, message, messageHash);
         }
-        return future;
+        return await future;
     }
 
     async watchBalance (params = {}) {
