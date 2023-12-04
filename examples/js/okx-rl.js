72b2121bed218a9fd70c42a21a507a9e77820099
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-12-04 20:49:21 +0400

    example upd

--- examples/js/okx-rl.js
+++ examples/js/okx-rl.js
@@ -5,15 +5,16 @@ import ccxt from '../../js/ccxt.js'
 // instantiate the exchange
 let exchange = new ccxt.okx  ({});
 exchange.throttler.config.maxCapacity = 1000; 
+// exchange.throttler.costMultiplier = 1.1;
 
 async function checkOrders(){
-    const arr = [];
+    const promises = [];
+    let results = [];
     for (let i = 0; i < 1000; i++) {
-        const prom = exchange.fetchTrades ('BTC/USDT', 1600000000000);
-        arr.push(prom);
+        promises.push(exchange.fetchTrades ('BTC/USDT'));
     }
     try {
-        await Promise.all(arr);
+        results = await Promise.all(promises);
     } catch (e) {
         console.log(e);
     }
