74570fa36e3bd954b7df0cb87f5a4a6e9f98aec2
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-12-11 15:07:28 +0400

    sample WS proxy test script

---
+++ ts/src/test/base/custom/proxy-usage.ts
@@ -0,0 +1,21 @@
+
+import { pro } from '../../../../../js/ccxt.js';
+
+// AUTO-TRANSPILE //
+
+
+async function example_webSockets () {
+    const myEx = new pro.kucoin ();
+    myEx.wsProxy = 'http://5.75.153.75:8002'; // "wsProxy" or "wssProxy" (depending on your proxy protocol)
+    await myEx.loadMarkets ();
+    myEx.handleMessage = ws_helper_callback; // todo for PHP: specifically this custom example does not work in PHP to retrieve the target message, however proxies do work in PHP for websockets independently from this example
+    const fakeExchangeWsUrl = 'ws://5.75.153.75:9876';
+    await myEx.watch (fakeExchangeWsUrl, 'test', 'test');
+    console.log ('WS proxy test finished');
+}
+
+function ws_helper_callback (client, message) {
+    console.log ('WS received:', message);
+}
+
+await example_webSockets ();
