2fbbe5f1765e15da2b7c30427d1be450183f2dfa
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-12-12 18:12:54 +0000

    missing ;

--- ts/src/pro/coinbase.ts
+++ ts/src/pro/coinbase.ts
@@ -4,7 +4,7 @@ import coinbaseRest from '../coinbase.js';
 import { ArgumentsRequired, ExchangeError } from '../base/errors.js';
 import { ArrayCacheBySymbolById } from '../base/ws/Cache.js';
 import { sha256 } from '../static_dependencies/noble-hashes/sha256.js';
-import { Ticker, Int, Trade, OrderBook, Order } from '../base/types.js'
+import { Ticker, Int, Trade, OrderBook, Order } from '../base/types.js';
 
 //  ---------------------------------------------------------------------------
 
