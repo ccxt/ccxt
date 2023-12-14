924c67f1fa5383606c799bf253e18bdb1b87f1b0
Sam Germain <sam.germain@usask.ca>
2023-12-14 10:06:02 -0500

    has["repayCrossMargin"] === false, and repayIsolatedMargin

--- ts/src/coinsph.ts
+++ ts/src/coinsph.ts
@@ -107,6 +107,8 @@ export default class coinsph extends Exchange {
                 'fetchWithdrawals': true,
                 'fetchWithdrawalWhitelist': false,
                 'reduceMargin': false,
+                'repayCrossMargin': false,
+                'repayIsolatedMargin': false,
                 'setLeverage': false,
                 'setMargin': false,
                 'setMarginMode': false,
