5def56ef0bdf7438664b4cd866f8305825dfa912
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-12-15 17:40:57 +0400

    ordebook Ws tests - remove checks

--- ts/src/pro/test/Exchange/test.watchOrderBook.ts
+++ ts/src/pro/test/Exchange/test.watchOrderBook.ts
@@ -18,24 +18,25 @@ async function testWatchOrderBook (exchange, skippedProperties, symbol) {
             now = exchange.milliseconds ();
             continue;
         }
-        response = fixPhpObjectArray (exchange, response);
-        // temporary fix, because of json removes 'undefined' members
-        skippedProperties['timestamp'] = true;
-        skippedProperties['datetime'] = true;
-        assert (typeof response === 'object', exchange.id + ' ' + method + ' ' + symbol + ' must return an object. ' + exchange.json (response));
+        // the below manipulation leads PHP to end up with unordered bids & asks. so, till we find a better solution, comment the below lines
+        // response = fixPhpObjectArray (exchange, response);
+        // // temporary fix, because of json removes 'undefined' members
+        // skippedProperties['timestamp'] = true;
+        // skippedProperties['datetime'] = true;
+        // assert (typeof response === 'object', exchange.id + ' ' + method + ' ' + symbol + ' must return an object. ' + exchange.json (response));
         now = exchange.milliseconds ();
         testOrderBook (exchange, skippedProperties, method, response, symbol);
     }
 }
 
-function fixPhpObjectArray (exchange, response) {
-    // temp fix for php 'Pro\OrderBook' object, to turn it into array
-    const existingJqMode = exchange.getProperty (exchange, 'quoteJsonNumbers');
-    exchange.setExchangeProperty ('quoteJsonNumbers', false);
-    const result = exchange.parseJson (exchange.json (response));
-    exchange.setExchangeProperty ('quoteJsonNumbers', existingJqMode);
-    return result;
-}
+// function fixPhpObjectArray (exchange, response) {
+//     // temp fix for php 'Pro\OrderBook' object, to turn it into array
+//     const existingJqMode = exchange.getProperty (exchange, 'quoteJsonNumbers');
+//     exchange.setExchangeProperty ('quoteJsonNumbers', false);
+//     const result = exchange.parseJson (exchange.json (response));
+//     exchange.setExchangeProperty ('quoteJsonNumbers', existingJqMode);
+//     return result;
+// }
 
 
 
