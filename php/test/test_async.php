ed879c4c199baa60567d8daad4cdb778d3876d6c
T.Todua <7117978+ttodua@users.noreply.github.com>
2023-12-15 17:11:41 +0400

    ws CLOSE in php

--- php/test/test_async.php
+++ php/test/test_async.php
@@ -252,8 +252,11 @@ function is_null_value($value) {
 }
 
 function close($exchange) {
-    return Async\async (function() {
-        // stub
+    return Async\async (function() use ($exchange) {
+        // for WS classes
+        if (method_exists($exchange, 'close')) {
+            return Async\await($exchange->close());
+        }
         return true;
     })();
 }
