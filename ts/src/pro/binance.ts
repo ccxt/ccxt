2fac49fca2ba5e875bc68684594c21ad5bd6a876
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2023-12-15 14:36:10 +0000

    small fix

--- ts/src/pro/binance.ts
+++ ts/src/pro/binance.ts
@@ -1980,7 +1980,7 @@ export default class binance extends binanceRest {
         if (this.newUpdates) {
             limit = orders.getLimit (symbol, limit);
         }
-        return this.filterBySymbolSinceLimit (this.orders, symbol, since, limit, true);
+        return this.filterBySymbolSinceLimit (orders, symbol, since, limit, true);
     }
 
     parseWsOrder (order, market = undefined) {
