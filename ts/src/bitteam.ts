d46d1f9013d9d547a0998698944891aa270c16f7
yzh-pelle <81404729+yzh-pelle@users.noreply.github.com>
2023-12-18 20:17:08 +0300

    Update bitteam.ts

--- ts/src/bitteam.ts
+++ ts/src/bitteam.ts
@@ -126,8 +126,7 @@ export default class bitteam extends Exchange {
                 '1d': '1D',
             },
             'urls': {
-                'logo': 'https://private-user-images.githubusercontent.com/43336371/291263231-aa2e0d61-edba-44a8-b9bf-68797dc417bd.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDI4OTg2NDksIm5iZiI6MTcwMjg5ODM0OSwicGF0aCI6Ii80MzMzNjM3MS8yOTEyNjMyMzEtYWEyZTBkNjEtZWRiYS00NGE4LWI5YmYtNjg3OTdkYzQxN2JkLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFJV05KWUFYNENTVkVINTNBJTJGMjAyMzEyMTglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjMxMjE4VDExMTkwOVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWE2OGM4M2E4Y2M2Y2RlOGFiZjNhMjU4ODk3MzIxYjZkMjZmZjIyZmFlZTkzNzlhMjNjNjVjOGI4YzI2ODYwN2YmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.pc0DpI9xblboo8v2QTcsp-vCy6GWpFGvuRuHGWqXVpE',
-                'referral': 'https://bit.team/auth/sign-up?ref=bitboy2023',
+                'logo': '', // todo
                 'api': {
                     'history': 'https://history.bit.team',
                     'public': 'https://bit.team',
