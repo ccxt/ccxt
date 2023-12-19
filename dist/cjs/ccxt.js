fdb1368e8c73e31a995fd443f16a71586216ce9a
Travis CI <travis@travis-ci.org>
2023-12-19 15:50:59 +0000

    4.1.93
    [ci skip]

--- dist/cjs/ccxt.js
+++ dist/cjs/ccxt.js
@@ -168,7 +168,7 @@ var woo$1 = require('./src/pro/woo.js');
 
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
-const version = '4.1.92';
+const version = '4.1.93';
 Exchange["default"].ccxtVersion = version;
 const exchanges = {
     'ace': ace,
--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -41,11 +41,11 @@ use React\EventLoop\Loop;
 
 use Exception;
 
-$version = '4.1.92';
+$version = '4.1.93';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '4.1.92';
+    const VERSION = '4.1.93';
 
     public $browser;
     public $marketsLoading = null;
