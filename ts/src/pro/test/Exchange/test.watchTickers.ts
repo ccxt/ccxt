30b239b4d95570ec9832e7f9f59af26065ecc501
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-01-02 00:24:26 +0400

    feat(tests) - test WatchTickers

---
+++ ts/src/pro/test/Exchange/test.watchTickers.ts
@@ -0,0 +1,40 @@
+
+import assert from 'assert';
+import testTicker from '../../../test/Exchange/base/test.ticker.js';
+import testSharedMethods from '../../../test/Exchange/base/test.sharedMethods.js';
+
+async function testWatchTickers (exchange, skippedProperties, symbol) {
+    const withoutSymbol = testWatchTickersHelper (exchange, skippedProperties, undefined);
+    const withSymbol = testWatchTickersHelper (exchange, skippedProperties, [ symbol ]);
+    await Promise.all ([ withSymbol, withoutSymbol ]);
+}
+
+async function testWatchTickersHelper (exchange, skippedProperties, argSymbols, argParams = {}) {
+    const method = 'watchTickers';
+    let now = exchange.milliseconds ();
+    const ends = now + 15000;
+    while (now < ends) {
+        let response = undefined;
+        try {
+            response = await exchange.watchTickers (argSymbols, argParams);
+        } catch (e) {
+            if (!testSharedMethods.isTemporaryFailure (e)) {
+                throw e;
+            }
+            now = exchange.milliseconds ();
+            continue;
+        }
+        assert (typeof response === 'object', exchange.id + ' ' + method + ' ' + exchange.json (argSymbols) + ' must return an object. ' + exchange.json (response));
+        const values = Object.values (response);
+        let checkedSymbol = undefined;
+        if (argSymbols !== undefined && argSymbols.length === 1) {
+            checkedSymbol = argSymbols[0];
+        }
+        for (let i = 0; i < values.length; i++) {
+            const ticker = values[i];
+            testTicker (exchange, skippedProperties, method, ticker, checkedSymbol);
+        }
+    }
+}
+
+export default testWatchTickers;
