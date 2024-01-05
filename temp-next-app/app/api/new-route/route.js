722da811b254068a0810cbf65c37b6b7569c5869
T. Todua <7117978+ttodua@users.noreply.github.com>
2024-01-05 23:53:54 +0400

    Update route.js

--- temp-next-app/app/api/new-route/route.js
+++ temp-next-app/app/api/new-route/route.js
@@ -1,3 +1,9 @@
+import { NextResponse } from "next/server";
+export async function GET(request) { return NextResponse.json({ message: "Hello World" }); }
+export async function POST(request) { return NextResponse.json({ message: "Hello World" }); }
+
+
+
 import CCXT from 'ccxt';
 const exchange = new CCXT['binance']()
 console.log(await exchange.fetch('https://api.ipify.org'));
