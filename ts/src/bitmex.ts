7fa9e31c411c7669e7a75c3f2b273cd19f8eeaf7
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-01-13 14:59:02 +0000

    fix(build): disabled bitmex tests

--- ts/src/bitmex.ts
+++ ts/src/bitmex.ts
@@ -2634,8 +2634,7 @@ export default class bitmex extends Exchange {
         }
         const currency = this.currency (code);
         let currencyId = currency['id'];
-        const idLength = currencyId.length;
-        currencyId = currencyId.slice (0, idLength - 1) + currencyId.slice (idLength - 1, idLength).toLowerCase ();  // make the last letter lowercase
+        currencyId = currencyId.slice (0, currencyId.length - 1) + currencyId.slice (currencyId.length - 1, currencyId.length).toLowerCase (); // make the last letter lowercase
         params = this.omit (params, 'network');
         const request = {
             'currency': currencyId,
