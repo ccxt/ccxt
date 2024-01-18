0842f1749d48c2b23035d0946e016a9798f8198b
Peter Lai <alk03073135@gmail.com>
2024-01-18 18:20:36 +0800

    binance: add apis (#20857)
    * binance: add apis
    
    * binance: fetchMyDustTrades add accountType & add static-tests
    
    * remove duplicated
    
    ---------
    
    Co-authored-by: carlosmiei <43336371+carlosmiei@users.noreply.github.com>

--- ts/src/binance.ts
+++ ts/src/binance.ts
@@ -224,7 +224,7 @@ export default class binance extends Exchange {
                         'margin/allPairs': 0.1,
                         'margin/priceIndex': 1,
                         // these endpoints require this.apiKey + this.secret
-                        'spot/delist-schedule': 0.1,
+                        'spot/delist-schedule': 10,
                         'asset/assetDividend': 1,
                         'asset/dribblet': 0.1,
                         'asset/transfer': 0.1,
@@ -5500,6 +5500,7 @@ export default class binance extends Exchange {
          * @param {int} [since] the earliest time in ms to fetch my dust trades for
          * @param {int} [limit] the maximum number of dust trades to retrieve
          * @param {object} [params] extra parameters specific to the exchange API endpoint
+         * @param {string} [params.type] 'spot' or 'margin', default spot
          * @returns {object[]} a list of [trade structures]{@link https://docs.ccxt.com/#/?id=trade-structure}
          */
         //
@@ -5513,6 +5514,11 @@ export default class binance extends Exchange {
         if (since !== undefined) {
             request['startTime'] = since;
             request['endTime'] = this.sum (since, 7776000000);
+        }
+        const accountType = this.safeStringUpper (params, 'type');
+        params = this.omit (params, 'type');
+        if (accountType !== undefined) {
+            request['accountType'] = accountType;
         }
         const response = await this.sapiGetAssetDribblet (this.extend (request, params));
         //     {
