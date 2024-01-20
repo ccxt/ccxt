f8a9847161ab0bfe12cb598d05dc6101a7f13c6e
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-01-20 14:29:13 +0000

    feat(bitpanda): add alias

---
+++ ts/src/bitpanda.ts
@@ -0,0 +1,15 @@
+
+// ---------------------------------------------------------------------------
+
+import onetrading from './onetrading.js';
+
+// ---------------------------------------------------------------------------
+
+export default class bitpanda extends onetrading {
+    describe () {
+        return this.deepExtend (super.describe (), {
+            'id': 'bitpanda',
+            'alias': true,
+        });
+    }
+}
