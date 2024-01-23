d67cacd8661ea54b27fed8e16db43c23fff1918b
Peter Lai <alk03073135@gmail.com>
2024-01-23 17:25:05 +0800

    fix(bitopro): fix ws url and signature (#20928)
    * bitopro: update ws port & authenticate error
    
    * bitopro: add @see

--- ts/src/pro/bitopro.ts
+++ ts/src/pro/bitopro.ts
@@ -26,8 +26,8 @@ export default class bitopro extends bitoproRest {
             },
             'urls': {
                 'ws': {
-                    'public': 'wss://stream.bitopro.com:9443/ws/v1/pub',
-                    'private': 'wss://stream.bitopro.com:9443/ws/v1/pub/auth',
+                    'public': 'wss://stream.bitopro.com:443/ws/v1/pub',
+                    'private': 'wss://stream.bitopro.com:443/ws/v1/pub/auth',
                 },
             },
             'requiredCredentials': {
@@ -58,6 +58,7 @@ export default class bitopro extends bitoproRest {
          * @method
          * @name bitopro#watchOrderBook
          * @description watches information on open orders with bid (buy) and ask (sell) prices, volumes and other data
+         * @see https://github.com/bitoex/bitopro-offical-api-docs/blob/master/ws/public/order_book_stream.md
          * @param {string} symbol unified symbol of the market to fetch the order book for
          * @param {int} [limit] the maximum amount of order book entries to return
          * @param {object} [params] extra parameters specific to the exchange API endpoint
@@ -124,6 +125,7 @@ export default class bitopro extends bitoproRest {
          * @method
          * @name bitopro#watchTrades
          * @description get the list of most recent trades for a particular symbol
+         * @see https://github.com/bitoex/bitopro-offical-api-docs/blob/master/ws/public/trade_stream.md
          * @param {string} symbol unified symbol of the market to fetch trades for
          * @param {int} [since] timestamp in ms of the earliest trade to fetch
          * @param {int} [limit] the maximum amount of trades to fetch
@@ -185,6 +187,7 @@ export default class bitopro extends bitoproRest {
          * @method
          * @name bitopro#watchTicker
          * @description watches a price ticker, a statistical calculation with the information calculated over the past 24 hours for a specific market
+         * @see https://github.com/bitoex/bitopro-offical-api-docs/blob/master/ws/public/ticker_stream.md
          * @param {string} symbol unified symbol of the market to fetch the ticker for
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object} a [ticker structure]{@link https://docs.ccxt.com/#/?id=ticker-structure}
@@ -240,7 +243,7 @@ export default class bitopro extends bitoproRest {
             'identity': this.login,
         });
         const payload = this.stringToBase64 (rawData);
-        const signature = this.hmac (payload, this.encode (this.secret), sha384);
+        const signature = this.hmac (this.encode (payload), this.encode (this.secret), sha384);
         const defaultOptions = {
             'ws': {
                 'options': {
@@ -267,6 +270,7 @@ export default class bitopro extends bitoproRest {
          * @method
          * @name bitopro#watchBalance
          * @description watch balance and get the amount of funds available for trading or funds locked in orders
+         * @see https://github.com/bitoex/bitopro-offical-api-docs/blob/master/ws/private/user_balance_stream.md
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object} a [balance structure]{@link https://docs.ccxt.com/#/?id=balance-structure}
          */
