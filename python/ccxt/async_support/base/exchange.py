7b4de6eddcc5051102320bc849ee34c62c8f8ab6
pcriadoperez <pablo@criadoperez.com>
2024-01-27 20:51:29 -0600

    fix(ws): freezing eror

--- python/ccxt/async_support/base/exchange.py
+++ python/ccxt/async_support/base/exchange.py
@@ -430,9 +430,9 @@ class Exchange(BaseExchange):
                     try:
                         await client.send(message)
                     except ConnectionError as e:
-                        for subscribe_hash in missing_subscriptions:
-                            del client.subscriptions[subscribe_hash]
-                        future.reject(e)
+                        client.on_error (e)
+                    except Exception as e:
+                        client.on_error (e)
                 asyncio.ensure_future(send_message())
 
         if missing_subscriptions:
@@ -468,8 +468,9 @@ class Exchange(BaseExchange):
                     try:
                         await client.send(message)
                     except ConnectionError as e:
-                        del client.subscriptions[subscribe_hash]
-                        future.reject(e)
+                        client.on_error (e)
+                    except Exception as e:
+                        client.on_error (e)
                 asyncio.ensure_future(send_message())
 
         if not subscribed:
