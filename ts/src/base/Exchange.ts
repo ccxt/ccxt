9dacfb09c97cbe4900913a59fea00844c5dd94ea
Pablo Criado-Perez <pablo@criadoperez.com>
2024-01-28 04:46:53 -0600

    fix(ws): freezing eror (#20988)
    * fix(ws): freezing eror
    
    * python lint

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -1519,14 +1519,12 @@ export default class Exchange {
                         client.throttle (cost).then (() => {
                             client.send (message);
                         }).catch ((e) => {
-                            delete client.subscriptions[subscribeHash];
-                            future.reject (e);
+                            client.onError (e);
                         });
                     } else {
                         client.send (message)
                         .catch ((e) => {
-                            delete client.subscriptions[subscribeHash];
-                            future.reject (e);
+                            client.onError (e);
                         });
                     }
                 }
