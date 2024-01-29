6d4f8280ff6ceda7ea0f9a2693ce32dfe5202a2c
Travis CI <travis@travis-ci.org>
2024-01-29 11:36:03 +0000

    fix(coinbase): add params to body when using token (#20997)
    * fix(coinbase): add params to body when using token
    
    * extra check
    
    [ci skip]

--- js/src/coinbase.js
+++ js/src/coinbase.js
@@ -3340,11 +3340,16 @@ export default class coinbase extends Exchange {
                     'Content-Type': 'application/json',
                 };
             }
-            else if (this.token) {
+            else if (this.token && !this.checkRequiredCredentials(false)) {
                 headers = {
                     'Authorization': 'Bearer ' + this.token,
                     'Content-Type': 'application/json',
                 };
+                if (method !== 'GET') {
+                    if (Object.keys(query).length) {
+                        body = this.json(query);
+                    }
+                }
             }
             else {
                 this.checkRequiredCredentials();
