ebd5d25314a67be780f977ab134f8ff5670f9f60
pcriadoperez <pablo@criadoperez.com>
2024-01-30 02:28:10 -0600

    update example

--- examples/ts/subscribe-ws-tickers.ts
+++ examples/ts/subscribe-ws-tickers.ts
@@ -14,14 +14,23 @@ async function storeInDb (message: Message) {
     console.log ('stored in DB index: ' + message.metadata.index);
 }
 
+function priceAlert (message: Message) {
+    if (message.payload['last'] > 10000) {
+        console.log ('Price is over 10000!!!!!!!!!!');
+    }
+}
+
 async function example () {
     // load markets
     await exchange.watchTickers ();
 
-    // subscribe asynchronously to all tickers
+    // subscribe synchronously to all tickers with a sync function
     exchange.stream.subscribe ('tickers', printMessage, true);
 
-    // subscribe synchronously to a single ticker
+    // subscribe asynchronously to all tickers with a sync function
+    exchange.stream.subscribe ('tickers', priceAlert, false);
+
+    // subscribe synchronously to a single ticker with an async function
     exchange.stream.subscribe ('tickers.BTC/USDT', storeInDb, true);
 
     await sleep (10000);
