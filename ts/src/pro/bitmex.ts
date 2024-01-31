e560d5fb7c9a1bfbcd2ebc41a280d5c4dd257a59
T. Todua <7117978+ttodua@users.noreply.github.com>
2024-01-31 18:43:42 +0400

    feat(bitmex) - watchTickers (#21033)
    * feat(bitmex) - watchTickers
    
    * cr updates
    
    * fix return type
    
    ---------
    
    Co-authored-by: carlosmiei <43336371+carlosmiei@users.noreply.github.com>

--- ts/src/pro/bitmex.ts
+++ ts/src/pro/bitmex.ts
@@ -5,7 +5,7 @@ import bitmexRest from '../bitmex.js';
 import { AuthenticationError, ExchangeError, RateLimitExceeded } from '../base/errors.js';
 import { ArrayCache, ArrayCacheByTimestamp, ArrayCacheBySymbolById, ArrayCacheBySymbolBySide } from '../base/ws/Cache.js';
 import { sha256 } from '../static_dependencies/noble-hashes/sha256.js';
-import type { Int, Str, Strings, OrderBook, Order, Trade, Ticker, OHLCV, Position, Balances } from '../base/types.js';
+import type { Int, Str, Strings, OrderBook, Order, Trade, Ticker, Tickers, OHLCV, Position, Balances } from '../base/types.js';
 import Client from '../base/ws/Client.js';
 
 //  ---------------------------------------------------------------------------
@@ -23,7 +23,7 @@ export default class bitmex extends bitmexRest {
                 'watchOrders': true,
                 'watchPostions': true,
                 'watchTicker': true,
-                'watchTickers': false,
+                'watchTickers': true,
                 'watchTrades': true,
                 'watchTradesForSymbols': true,
             },
@@ -76,6 +76,46 @@ export default class bitmex extends bitmexRest {
             ],
         };
         return await this.watch (url, messageHash, this.extend (request, params), messageHash);
+    }
+
+    async watchTickers (symbols: Strings = undefined, params = {}): Promise<Tickers> {
+        /**
+         * @method
+         * @name bitmex#watchTickers
+         * @description watches a price ticker, a statistical calculation with the information calculated over the past 24 hours for all markets of a specific list
+         * @param {string[]} symbols unified symbol of the market to fetch the ticker for
+         * @param {object} [params] extra parameters specific to the exchange API endpoint
+         * @returns {object} a [ticker structure]{@link https://docs.ccxt.com/#/?id=ticker-structure}
+         */
+        await this.loadMarkets ();
+        symbols = this.marketSymbols (symbols, undefined, true);
+        const name = 'instrument';
+        const url = this.urls['api']['ws'];
+        const messageHashes = [];
+        if (symbols !== undefined) {
+            for (let i = 0; i < symbols.length; i++) {
+                const symbol = symbols[i];
+                const market = this.market (symbol);
+                const hash = name + ':' + market['id'];
+                messageHashes.push (hash);
+            }
+        } else {
+            messageHashes.push (name);
+        }
+        const request = {
+            'op': 'subscribe',
+            'args': messageHashes,
+        };
+        const ticker = await this.watchMultiple (url, messageHashes, this.extend (request, params), messageHashes);
+        if (this.newUpdates) {
+            if (symbols === undefined) {
+                return ticker;
+            }
+            const result = {};
+            result[ticker['symbol']] = ticker;
+            return result;
+        }
+        return this.filterByArray (this.tickers, 'symbol', symbols);
     }
 
     handleTicker (client: Client, message) {
@@ -306,19 +346,22 @@ export default class bitmex extends bitmexRest {
         //     }
         //
         const table = this.safeString (message, 'table');
-        const data = this.safeValue (message, 'data', []);
+        const data = this.safeList (message, 'data', []);
+        const tickers = {};
         for (let i = 0; i < data.length; i++) {
             const update = data[i];
-            const marketId = this.safeValue (update, 'symbol');
+            const marketId = this.safeString (update, 'symbol');
             const market = this.safeMarket (marketId);
             const symbol = market['symbol'];
             const messageHash = table + ':' + marketId;
-            let ticker = this.safeValue (this.tickers, symbol, {});
-            const info = this.safeValue (ticker, 'info', {});
+            let ticker = this.safeDict (this.tickers, symbol, {});
+            const info = this.safeDict (ticker, 'info', {});
             ticker = this.parseTicker (this.extend (info, update), market);
+            tickers[symbol] = ticker;
             this.tickers[symbol] = ticker;
             client.resolve (ticker, messageHash);
         }
+        client.resolve (tickers, 'instrument');
         return message;
     }
 
