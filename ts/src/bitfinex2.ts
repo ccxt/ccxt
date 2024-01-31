34d63fb2656714ca2a10aaed6aa4691c7acb217b
Dan <dan.krm@outlook.com>
2024-01-31 00:51:38 -0600

    feat(bitfinex2): createTriggerOrder
    ```
    bitfinex2.createTriggerOrder (BTC/USDT, limit, buy, 0.0001, 40000, 41000)
    2024-01-31T06:49:04.523Z iteration 0 passed in 1034 ms
    
    {
      info: [
        139454329685,          null,
        1706683745645,         'tBTCUST',
        1706683745645,         1706683745645,
        0.0001,                0.0001,
        'EXCHANGE STOP LIMIT', null,
        null,                  null,
        0,                     'ACTIVE',
        null,                  null,
        41000,                 0,
        0,                     40000,
        null,                  null,
        null,                  0,
        0,                     null,
        null,                  null,
        'API>BFX',             null,
        null,                  {}
      ],
      id: '139454329685',
      clientOrderId: '1706683745645',
      timestamp: 1706683745645,
      datetime: '2024-01-31T06:49:05.645Z',
      symbol: 'BTC/USDT',
      type: 'limit',
      timeInForce: 'GTC',
      postOnly: false,
      side: 'buy',
      price: 40000,
      stopPrice: 41000,
      triggerPrice: 41000,
      amount: 0.0001,
      cost: 0,
      filled: 0,
      remaining: 0.0001,
      status: 'open',
      trades: [],
      fees: []
    }
    ```

--- ts/src/bitfinex2.ts
+++ ts/src/bitfinex2.ts
@@ -39,6 +39,7 @@ export default class bitfinex2 extends Exchange {
                 'createStopLimitOrder': true,
                 'createStopMarketOrder': true,
                 'createStopOrder': true,
+                'createTriggerOrder': true,
                 'createTrailingAmountOrder': true,
                 'createTrailingPercentOrder': false,
                 'editOrder': false,
@@ -1525,7 +1526,7 @@ export default class bitfinex2 extends Exchange {
          * @param {float} amount the amount of currency to trade
          * @param {float} [price] price of the order
          * @param {object} [params] extra parameters specific to the exchange API endpoint
-         * @param {float} [params.stopPrice] The price at which a trigger order is triggered at
+         * @param {float} [params.stopPrice] the price that triggers a trigger order
          * @param {string} [params.timeInForce] "GTC", "IOC", "FOK", or "PO"
          * @param {boolean} [params.postOnly] set to true if you want to make a post only order
          * @param {boolean} [params.reduceOnly] indicates that the order is to reduce the size of a position
@@ -1538,76 +1539,57 @@ export default class bitfinex2 extends Exchange {
          */
         await this.loadMarkets ();
         const market = this.market (symbol);
-        // order types "limit" and "market" immediatley parsed "EXCHANGE LIMIT" and "EXCHANGE MARKET"
-        // note: same order types exist for margin orders without the EXCHANGE prefix
-        const orderTypes = this.safeValue (this.options, 'orderTypes', {});
-        let orderType = type.toUpperCase ();
-        if (market['spot']) {
-            // although they claim that type needs to be 'exchange limit' or 'exchange market'
-            // in fact that's not the case for swap markets
-            orderType = this.safeStringUpper (orderTypes, type, type);
-        }
+        let amountString = this.amountToPrecision (symbol, amount);
+        amountString = (side === 'buy') ? amountString : Precise.stringNeg (amountString);
+        const request = {
+            'symbol': market['id'],
+            'amount': amountString,
+        };
         const stopPrice = this.safeString2 (params, 'stopPrice', 'triggerPrice');
         const trailingAmount = this.safeString (params, 'trailingAmount');
         const timeInForce = this.safeString (params, 'timeInForce');
         const postOnlyParam = this.safeValue (params, 'postOnly', false);
         const reduceOnly = this.safeValue (params, 'reduceOnly', false);
         const clientOrderId = this.safeValue2 (params, 'cid', 'clientOrderId');
-        params = this.omit (params, [ 'triggerPrice', 'stopPrice', 'timeInForce', 'postOnly', 'reduceOnly', 'price_aux_limit', 'trailingAmount' ]);
-        let amountString = this.amountToPrecision (symbol, amount);
-        amountString = (side === 'buy') ? amountString : Precise.stringNeg (amountString);
-        const request = {
-            // 'gid': 0123456789, // int32,  optional group id for the order
-            // 'cid': 0123456789, // int32 client order id
-            'type': orderType,
-            'symbol': market['id'],
-            // 'price': this.numberToString (price),
-            'amount': amountString,
-            // 'flags': 0, // int32, https://docs.bitfinex.com/v2/docs/flag-values
-            // 'lev': 10, // leverage for a derivative orders, the value should be between 1 and 100 inclusive, optional, 10 by default
-            // 'price_trailing': this.numberToString (priceTrailing),
-            // 'price_aux_limit': this.numberToString (stopPrice),
-            // 'price_oco_stop': this.numberToString (ocoStopPrice),
-            // 'tif': '2020-01-01 10:45:23', // datetime for automatic order cancellation
-            // 'meta': {
-            //     'aff_code': 'AFF_CODE_HERE'
-            // },
-        };
+        params = this.omit (params, [ 'triggerPrice', 'stopPrice', 'timeInForce', 'postOnly', 'reduceOnly', 'trailingAmount' ]);
+        let orderType = type.toUpperCase ();
         if (trailingAmount !== undefined) {
-            request['type'] = 'TRAILING STOP';
+            orderType = 'TRAILING STOP';
             request['price_trailing'] = trailingAmount;
+        } else if (stopPrice !== undefined) {
+            // request['price'] is taken as stopPrice for stop orders
+            request['price'] = this.priceToPrecision (symbol, stopPrice);
+            if (type === 'limit') {
+                orderType = 'STOP LIMIT';
+                request['price_aux_limit'] = this.priceToPrecision (symbol, price);
+            } else {
+                orderType = 'STOP';
+            }
         }
-        const stopLimit = ((orderType === 'EXCHANGE STOP LIMIT') || ((orderType === 'EXCHANGE LIMIT') && (stopPrice !== undefined)));
-        const exchangeStop = (orderType === 'EXCHANGE STOP');
-        const exchangeMarket = (orderType === 'EXCHANGE MARKET');
-        const stopMarket = (exchangeStop || (exchangeMarket && (stopPrice !== undefined)));
-        const ioc = ((orderType === 'EXCHANGE IOC') || (timeInForce === 'IOC'));
-        const fok = ((orderType === 'EXCHANGE FOK') || (timeInForce === 'FOK'));
+        const ioc = (timeInForce === 'IOC');
+        const fok = (timeInForce === 'FOK');
         const postOnly = (postOnlyParam || (timeInForce === 'PO'));
         if ((ioc || fok) && (price === undefined)) {
             throw new InvalidOrder (this.id + ' createOrder() requires a price argument with IOC and FOK orders');
         }
-        if ((ioc || fok) && exchangeMarket) {
+        if ((ioc || fok) && (type === 'market')) {
             throw new InvalidOrder (this.id + ' createOrder() does not allow market IOC and FOK orders');
         }
-        if ((orderType !== 'MARKET') && (!exchangeMarket) && (!exchangeStop)) {
+        if ((type !== 'market') && (stopPrice === undefined)) {
             request['price'] = this.priceToPrecision (symbol, price);
         }
-        if (stopLimit || stopMarket) {
-            // request['price'] is taken as stopPrice for stop orders
-            request['price'] = this.priceToPrecision (symbol, stopPrice);
-            if (stopMarket) {
-                request['type'] = 'EXCHANGE STOP';
-            } else if (stopLimit) {
-                request['type'] = 'EXCHANGE STOP LIMIT';
-                request['price_aux_limit'] = this.priceToPrecision (symbol, price);
-            }
-        }
         if (ioc) {
-            request['type'] = 'EXCHANGE IOC';
+            orderType = 'IOC';
         } else if (fok) {
-            request['type'] = 'EXCHANGE FOK';
+            orderType = 'FOK';
         }
+        let marginMode = undefined;
+        [ marginMode, params ] = this.handleMarginModeAndParams ('createOrder', params);
+        if (market['spot'] && (marginMode === undefined)) {
+            // The EXCHANGE prefix is only required for non margin spot markets
+            orderType = 'EXCHANGE ' + orderType;
+        }
+        request['type'] = orderType;
         // flag values may be summed to combine flags
         let flags = 0;
         if (postOnly) {
