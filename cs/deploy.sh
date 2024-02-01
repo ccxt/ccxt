10e7cd4cc2edd7c83b6e19af8304d3d88e751567
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-02-01 14:40:43 +0000

    typo

--- cs/deploy.sh
+++ cs/deploy.sh
@@ -8,4 +8,4 @@ bin_path="${bin_folder}${bin_file}"
 
 echo "Will publish nugget package: ${bin_path}"
 
-dotnet nuget push ${bin_path} -k ${NUGET_TOKEN} -s https://api.nuget.org/v3/index.json
+dotnet nuget push ${bin_path} -k ${NUGGET_TOKEN} -s https://api.nuget.org/v3/index.json
