ba127cc434fe1e3f87156bb90faa56ca7984e74f
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-02-02 16:12:53 +0000

    fix precision

--- ts/src/blofin.ts
+++ ts/src/blofin.ts
@@ -400,7 +400,7 @@ export default class blofin extends Exchange {
             'optionType': optionType,
             'created': this.safeInteger (market, 'listTime'),
             'precision': {
-                'amount': this.safeNumber (market, 'lotSize'),
+                'amount': this.safeNumber (market, 'tickSize'),
                 'price': this.parseNumber (tickSize),
             },
             'limits': {
