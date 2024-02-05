1930a64495ed69b49303e5bd0a5b9d674b6bf5e7
Sam Germain <sam.germain@usask.ca>
2024-02-05 17:20:52 -0500

    fix(bybit): remove marginMode from parsePosition (#21117)
    * fix(bybit): remove marginMode from parsePosition
    
    the tradeMode field has been depreciated by the bybit api and now always returns 0, so it cannot be used to decide the marginMode
    
    fixes: #21079
    
    * update static tests
    
    ---------
    
    Co-authored-by: carlosmiei <43336371+carlosmiei@users.noreply.github.com>

--- ts/src/bybit.ts
+++ ts/src/bybit.ts
@@ -6006,9 +6006,6 @@ export default class bybit extends Exchange {
         if (timestamp === undefined) {
             timestamp = this.safeIntegerN (position, [ 'updatedTime', 'updatedAt' ]);
         }
-        // default to cross of USDC margined positions
-        const tradeMode = this.safeInteger (position, 'tradeMode', 0);
-        const marginMode = tradeMode ? 'isolated' : 'cross';
         let collateralString = this.safeString (position, 'positionBalance');
         const entryPrice = this.omitZero (this.safeString2 (position, 'entryPrice', 'avgPrice'));
         const liquidationPrice = this.omitZero (this.safeString (position, 'liqPrice'));
@@ -6070,7 +6067,7 @@ export default class bybit extends Exchange {
             'markPrice': this.safeNumber (position, 'markPrice'),
             'lastPrice': undefined,
             'collateral': this.parseNumber (collateralString),
-            'marginMode': marginMode,
+            'marginMode': undefined,
             'side': side,
             'percentage': undefined,
             'stopLossPrice': this.safeNumber2 (position, 'stop_loss', 'stopLoss'),
