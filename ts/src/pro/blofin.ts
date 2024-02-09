d7dc91aafa5d5ad4b2c847e750d2e088a76432e6
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-02-09 17:22:31 +0400

    watchTrades

--- ts/src/pro/blofin.ts
+++ ts/src/pro/blofin.ts
@@ -4,7 +4,7 @@
 import blofinRest from '../blofin.js';
 import { NotSupported } from '../base/errors.js';
 import { ArrayCache } from '../base/ws/Cache.js';
-import type { Int, OrderBook, Trade, Ticker } from '../base/types.js';
+import type { Int, Trade } from '../base/types.js';
 import Client from '../base/ws/Client.js';
 
 //  ---------------------------------------------------------------------------
@@ -19,7 +19,10 @@ export default class blofin extends blofinRest {
             'urls': {
                 'api': {
                     'ws': {
-                        'public': 'wss://openapi.blofin.com/ws/public',
+                        'swap': {
+                            'public': 'wss://openapi.blofin.com/ws/public',
+                            'private': 'wss://openapi.blofin.com/ws/private',
+                        },
                     },
                 },
             },
@@ -29,5 +32,117 @@ export default class blofin extends blofinRest {
         });
     }
 
+    async watchTrades (symbol: string, since: Int = undefined, limit: Int = undefined, params = {}): Promise<Trade[]> {
+        /**
+         * @method
+         * @name blofin#watchTrades
+         * @see https://docs.blofin.com/index.html#ws-trades-channel
+         * @description get the list of most recent trades for a particular symbol
+         * @param {string} symbol unified symbol of the market to fetch trades for
+         * @param {int} [since] timestamp in ms of the earliest trade to fetch
+         * @param {int} [limit] the maximum amount of trades to fetch
+         * @param {object} [params] extra parameters specific to the exchange API endpoint
+         * @returns {object[]} a list of [trade structures]{@link https://docs.ccxt.com/#/?id=public-trades}
+         */
+        await this.loadMarkets ();
+        symbol = this.symbol (symbol);
+        return await this.watchTradesForSymbols ([ symbol ], since, limit, params);
+    }
 
+    async watchTradesForSymbols (symbols: string[], since: Int = undefined, limit: Int = undefined, params = {}): Promise<Trade[]> {
+        /**
+         * @method
+         * @name bitmart#watchTradesForSymbols
+         * @see https://docs.blofin.com/index.html#ws-trades-channel
+         * @description get the list of most recent trades for a list of symbols
+         * @param {string[]} symbols unified symbol of the market to fetch trades for
+         * @param {int} [since] timestamp in ms of the earliest trade to fetch
+         * @param {int} [limit] the maximum amount of trades to fetch
+         * @param {object} [params] extra parameters specific to the exchange API endpoint
+         * @returns {object[]} a list of [trade structures]{@link https://docs.ccxt.com/#/?id=public-trades}
+         */
+        await this.loadMarkets ();
+        const trades = await this.watchMultipleSymbols ('trades', 'watchTradesForSymbols', symbols, limit, params);
+        if (this.newUpdates) {
+            const first = this.safeDict (trades, 0);
+            const tradeSymbol = this.safeString (first, 'symbol');
+            limit = trades.getLimit (tradeSymbol, limit);
+        }
+        return this.filterBySinceLimit (trades, since, limit, 'timestamp', true);
+    }
+
+    async watchMultipleSymbols (channelName: string, methodName: string, symbols: string[], limit: Int = undefined, params = {}) {
+        await this.loadMarkets ();
+        let firstMarket = undefined;
+        symbols = this.marketSymbols (symbols, undefined, true, true);
+        if (symbols !== undefined) {
+            firstMarket = this.market (symbols[0]);
+        }
+        let marketType = undefined;
+        [ marketType, params ] = this.handleMarketTypeAndParams (methodName, firstMarket, params);
+        if (marketType === 'spot') {
+            throw new NotSupported (this.id + ' ' + methodName + '() is not supported for spot markets');
+        }
+        // const length = symbols.length;
+        // if (length > 20) {
+        //     throw new NotSupported (this.id + ' ' + methodName + '() accepts a maximum of 20 symbols in one request');
+        // }
+        const rawSubscriptions = [];
+        const messageHashes = [];
+        for (let i = 0; i < symbols.length; i++) {
+            const market = this.market (symbols[i]);
+            const message = {
+                'channel': channelName,
+                'instId': market['id'],
+            };
+            rawSubscriptions.push (message);
+            messageHashes.push (channelName + ':' + market['symbol']);
+        }
+        const request = {
+            'op': 'subscribe',
+            'args': rawSubscriptions,
+        };
+        const url = this.implodeHostname (this.urls['api']['ws'][marketType]['public']);
+        return await this.watchMultiple (url, messageHashes, this.deepExtend (request, params), messageHashes);
+    }
+
+    handleMessage (client: Client, message) {
+        const methods = {
+            'trade': this.handleTrades,
+        };
+        const methodName = this.safeString (message, 'type');
+        const method = this.safeValue (methods, methodName);
+        if (method) {
+            method.call (this, client, message);
+        }
+    }
+
+    handleTrades (client: Client, message) {
+        // { type: "trade",
+        //   "code": "KRW-BTC",
+        //   "timestamp": 1584508285812,
+        //   "trade_date": "2020-03-18",
+        //   "trade_time": "05:11:25",
+        //   "trade_timestamp": 1584508285000,
+        //   "trade_price": 6747000,
+        //   "trade_volume": 0.06499468,
+        //   "ask_bid": "ASK",
+        //   "prev_closing_price": 6774000,
+        //   "change": "FALL",
+        //   "change_price": 27000,
+        //   "sequential_id": 1584508285000002,
+        //   "stream_type": "REALTIME" }
+        const trade = this.parseTrade (message);
+        const symbol = trade['symbol'];
+        let stored = this.safeValue (this.trades, symbol);
+        if (stored === undefined) {
+            const limit = this.safeInteger (this.options, 'tradesLimit', 1000);
+            stored = new ArrayCache (limit);
+            this.trades[symbol] = stored;
+        }
+        stored.append (trade);
+        const marketId = this.safeString (message, 'code');
+        const messageHash = 'trade:' + marketId;
+        client.resolve (stored, messageHash);
+    }
 }
