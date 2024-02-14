f467ebb4c1e5f5933af3b82174a6d43b360e1b13
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-02-14 23:53:08 +0400

    fix(transpile.bat) - simplify and better check

--- build/transpile.bat
+++ build/transpile.bat
@@ -1,26 +1,16 @@
 @echo off
-setlocal EnableDelayedExpansion
-
-set /a ws=0
-for /f "tokens=*" %%a in ("%*") do (
-  if /i "%%a" == "--ws" (
-    set /a ws=1
-    goto :break
-  )
-)
-
-:break
 
 set exchange_name=%1
+set is_ws=%2
 
-if !exchange_name! == "" (
-  echo Exchange name not provided.
+if "%exchange_name%" == "" (
+  echo "Exchange name not provided"
   exit /b 1
-)
+) 
 
 cd %~dp0/../
 
-if %ws% == 1 (
+if "%is_ws%" == "--ws" (
   echo Transpiling WS version of %exchange_name% 
   node .\build\transpileWs.js %exchange_name% --ws
   node --loader ts-node/esm .\build\csharpTranspiler.ts %exchange_name% --ws
