5c4584ef73db023c1ccfc5a01d99485c8d836328
sc0Vu <alk03073135@gmail.com>
2024-02-20 10:38:01 +0800

    idex: add @see

--- ts/src/idex.ts
+++ ts/src/idex.ts
@@ -200,6 +200,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchMarkets
          * @description retrieves data on all markets for idex
+         * @see https://api-docs-v3.idex.io/#get-markets
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object[]} an array of objects representing market data
          */
@@ -331,6 +332,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchTicker
          * @description fetches a price ticker, a statistical calculation with the information calculated over the past 24 hours for a specific market
+         * @see https://api-docs-v3.idex.io/#get-tickers
          * @param {string} symbol unified symbol of the market to fetch the ticker for
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object} a [ticker structure]{@link https://docs.ccxt.com/#/?id=ticker-structure}
@@ -368,6 +370,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchTickers
          * @description fetches price tickers for multiple markets, statistical information calculated over the past 24 hours for each market
+         * @see https://api-docs-v3.idex.io/#get-tickers
          * @param {string[]|undefined} symbols unified symbols of the markets to fetch the ticker for, all market tickers are returned if not assigned
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object} a dictionary of [ticker structures]{@link https://docs.ccxt.com/#/?id=ticker-structure}
@@ -446,6 +449,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchOHLCV
          * @description fetches historical candlestick data containing the open, high, low, and close price, and the volume of a market
+         * @see https://api-docs-v3.idex.io/#get-candles
          * @param {string} symbol unified symbol of the market to fetch OHLCV data for
          * @param {string} timeframe the length of time each candle represents
          * @param {int} [since] timestamp in ms of the earliest candle to fetch
@@ -509,6 +513,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchTrades
          * @description get the list of most recent trades for a particular symbol
+         * @see https://api-docs-v3.idex.io/#get-trades
          * @param {string} symbol unified symbol of the market to fetch trades for
          * @param {int} [since] timestamp in ms of the earliest trade to fetch
          * @param {int} [limit] the maximum amount of trades to fetch
@@ -624,6 +629,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchTradingFees
          * @description fetch the trading fees for multiple markets
+         * @see https://api-docs-v3.idex.io/#get-api-account
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object} a dictionary of [fee structures]{@link https://docs.ccxt.com/#/?id=fee-structure} indexed by market symbols
          */
@@ -670,6 +676,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchOrderBook
          * @description fetches information on open orders with bid (buy) and ask (sell) prices, volumes and other data
+         * @see https://api-docs-v3.idex.io/#get-order-books
          * @param {string} symbol unified symbol of the market to fetch the order book for
          * @param {int} [limit] the maximum amount of order book entries to return
          * @param {object} [params] extra parameters specific to the exchange API endpoint
@@ -735,6 +742,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchCurrencies
          * @description fetches all available currencies on an exchange
+         * @see https://api-docs-v3.idex.io/#get-assets
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object} an associative dictionary of currencies
          */
@@ -802,6 +810,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchBalance
          * @description query for balance and get the amount of funds available for trading or funds locked in orders
+         * @see https://api-docs-v3.idex.io/#get-balances
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object} a [balance structure]{@link https://docs.ccxt.com/#/?id=balance-structure}
          */
@@ -845,6 +854,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchMyTrades
          * @description fetch all trades made by the user
+         * @see https://api-docs-v3.idex.io/#get-fills
          * @param {string} symbol unified market symbol
          * @param {int} [since] the earliest time in ms to fetch trades for
          * @param {int} [limit] the maximum number of trades structures to retrieve
@@ -912,6 +922,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchOrder
          * @description fetches information on an order made by the user
+         * @see https://api-docs-v3.idex.io/#get-orders
          * @param {string} symbol unified symbol of the market the order was made in
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object} An [order structure]{@link https://docs.ccxt.com/#/?id=order-structure}
@@ -927,6 +938,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchOpenOrders
          * @description fetch all unfilled currently open orders
+         * @see https://api-docs-v3.idex.io/#get-orders
          * @param {string} symbol unified market symbol
          * @param {int} [since] the earliest time in ms to fetch open orders for
          * @param {int} [limit] the maximum number of  open orders structures to retrieve
@@ -944,6 +956,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchClosedOrders
          * @description fetches information on multiple closed orders made by the user
+         * @see https://api-docs-v3.idex.io/#get-orders
          * @param {string} symbol unified market symbol of the market orders were made in
          * @param {int} [since] the earliest time in ms to fetch orders for
          * @param {int} [limit] the maximum number of order structures to retrieve
@@ -1159,6 +1172,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#createOrder
          * @description create a trade order, https://docs.idex.io/#create-order
+         * @see https://api-docs-v3.idex.io/#create-order
          * @param {string} symbol unified symbol of the market to create an order in
          * @param {string} type 'market' or 'limit'
          * @param {string} side 'buy' or 'sell'
@@ -1352,6 +1366,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#withdraw
          * @description make a withdrawal
+         * @see https://api-docs-v3.idex.io/#withdraw-funds
          * @param {string} code unified currency code
          * @param {float} amount the amount to withdraw
          * @param {string} address the address to withdraw to
@@ -1406,6 +1421,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#cancelAllOrders
          * @description cancel all open orders
+         * @see https://api-docs-v3.idex.io/#cancel-order
          * @param {string} symbol unified market symbol, only orders in the market of this symbol are cancelled when symbol is not undefined
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object[]} a list of [order structures]{@link https://docs.ccxt.com/#/?id=order-structure}
@@ -1446,6 +1462,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#cancelOrder
          * @description cancels an open order
+         * @see https://api-docs-v3.idex.io/#cancel-order
          * @param {string} id order id
          * @param {string} symbol unified symbol of the market the order was made in
          * @param {object} [params] extra parameters specific to the exchange API endpoint
@@ -1499,6 +1516,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchDeposit
          * @description fetch information on a deposit
+         * @see https://api-docs-v3.idex.io/#get-deposits
          * @param {string} id deposit id
          * @param {string} code not used by idex fetchDeposit ()
          * @param {object} [params] extra parameters specific to the exchange API endpoint
@@ -1520,6 +1538,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchDeposits
          * @description fetch all deposits made to an account
+         * @see https://api-docs-v3.idex.io/#get-deposits
          * @param {string} code unified currency code
          * @param {int} [since] the earliest time in ms to fetch deposits for
          * @param {int} [limit] the maximum number of deposits structures to retrieve
@@ -1537,6 +1556,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchTime
          * @description fetches the current integer timestamp in milliseconds from the exchange server
+         * @see https://api-docs-v3.idex.io/#get-time
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {int} the current integer timestamp in milliseconds from the exchange server
          */
@@ -1552,6 +1572,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchWithdrawal
          * @description fetch data on a currency withdrawal via the withdrawal id
+         * @see https://api-docs-v3.idex.io/#get-withdrawals
          * @param {string} id withdrawal id
          * @param {string} code not used by idex.fetchWithdrawal
          * @param {object} [params] extra parameters specific to the exchange API endpoint
@@ -1573,6 +1594,7 @@ export default class idex extends Exchange {
          * @method
          * @name idex#fetchWithdrawals
          * @description fetch all withdrawals made from an account
+         * @see https://api-docs-v3.idex.io/#get-withdrawals
          * @param {string} code unified currency code
          * @param {int} [since] the earliest time in ms to fetch withdrawals for
          * @param {int} [limit] the maximum number of withdrawals structures to retrieve
