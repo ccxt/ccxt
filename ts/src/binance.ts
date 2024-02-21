5c2f08f86ed41ec988062c3df3369865b8d1cc24
Sam Germain <sam.germain@usask.ca>
2024-02-21 07:04:35 -0500

    feat(binance): fetchPositionsForSymbol

--- ts/src/binance.ts
+++ ts/src/binance.ts
@@ -130,6 +130,7 @@ export default class binance extends Exchange {
                 'fetchPositionHistory': false,
                 'fetchPositionMode': true,
                 'fetchPositions': true,
+                'fetchPositionsForSymbol': 'emulated',
                 'fetchPositionsHistory': false,
                 'fetchPositionsRisk': true,
                 'fetchPremiumIndexOHLCV': true,
@@ -10094,6 +10095,10 @@ export default class binance extends Exchange {
         } else {
             throw new NotSupported (this.id + '.options["fetchPositions"]/params["method"] = "' + defaultMethod + '" is invalid, please choose between "account", "positionRisk" and "option"');
         }
+    }
+
+    async fetchPositionsForSymbol (symbol: string, params = {}) {
+        return await this.fetchPositions ([ symbol ], params);
     }
 
     async fetchAccountPositions (symbols: Strings = undefined, params = {}) {
