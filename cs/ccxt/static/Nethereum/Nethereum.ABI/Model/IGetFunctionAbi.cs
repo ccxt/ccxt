66ae89ca3cae498048985498b44e06aab83770b6
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-02-22 10:44:10 +0000

    feat(cs): add Nethereum static dep

---
+++ cs/ccxt/static/Nethereum/Nethereum.ABI/Model/IGetFunctionAbi.cs
@@ -0,0 +1,11 @@
+﻿namespace Nethereum.ABI.Model
+{
+    public interface IGetFunctionAbi
+    {
+        FunctionABI GetFunctionAbi();
+        void SetValue(string parameterName, object value);
+        object GetValue(string parameterName);
+    }
+
+   
+}
