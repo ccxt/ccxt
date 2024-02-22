2d87553eb757f7710e3d83f97ff5a338c0662f8f
Carlos Gonçalves <43336371+carlosmiei@users.noreply.github.com>
2024-02-22 11:15:39 +0000

    feat(build): add skip-tests feature (#21324)

--- build.sh
+++ build.sh
@@ -90,6 +90,13 @@ build_and_test_all () {
 ### CHECK IF THIS IS A PR ###
 # for appveyor, when PR is from fork, APPVEYOR_REPO_BRANCH is "master" and "APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH" is branch name. if PR is from same repo, only APPVEYOR_REPO_BRANCH is set (and it is branch name)
 if { [ "$IS_TRAVIS" = "TRUE" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; } || { [ "$IS_TRAVIS" != "TRUE" ] && [ -z "$APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH" ]; }; then
+
+  $last_commit_message=$(git show --format="%s" -s HEAD)
+  echo "Last commit: $last_commit_message" # for debugging
+  if [[ "$last_commit_message" == *"skip-tests"* ]]; then
+      echo "[SKIP-TESTS] Will skip tests!"
+      exit
+  fi
   echo "$msgPrefix This is a master commit (not a PR), will build everything"
   build_and_test_all
 fi
