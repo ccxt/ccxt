1f1c3b713699b81ba0a0e79d0ea49581ff89b72a
Travis CI <travis@travis-ci.org>
2024-02-23 18:26:00 +0000

    4.2.50
    [ci skip]

--- php/pro/bequant.php
+++ php/pro/bequant.php
@@ -11,10 +11,8 @@ class bequant extends \ccxt\pro\hitbtc {
 
     public function describe() {
         // eslint-disable-next-line new-cap
-        $restInstance = new \ccxt\async\bequant ();
-        $restDescribe = $restInstance->describe ();
-        $extended = $this->deep_extend(parent::describe(), $restDescribe);
-        return $this->deep_extend($extended, array(
+        $describeExtended = $this->get_describe_for_extended_ws_exchange(new \ccxt\async\bequant (), new \ccxt\async\hitbtc (), parent::describe());
+        return $this->deep_extend($describeExtended, array(
             'id' => 'bequant',
             'name' => 'Bequant',
             'countries' => array( 'MT' ), // Malta
