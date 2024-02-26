554757108e2759684f256e7874d8e6702dc66225
Sam Germain <sam.germain@usask.ca>
2024-02-26 07:29:45 -0500

    feat(binance): fetchTradingLimits

--- ts/src/binance.ts
+++ ts/src/binance.ts
@@ -121,7 +121,7 @@ export default class binance extends Exchange {
                 'fetchTrades': true,
                 'fetchTradingFee': true,
                 'fetchTradingFees': true,
-                'fetchTradingLimits': undefined,
+                'fetchTradingLimits': 'emulated',
                 'fetchTransactionFee': 'emulated',
                 'fetchTransactionFees': true,
                 'fetchTransactions': false,
@@ -11822,5 +11822,19 @@ export default class binance extends Exchange {
             'underlyingPrice': undefined,
             'info': greeks,
         };
+    }
+
+    async fetchTradingLimits (symbols: Strings = undefined, params = {}) {
+        // this method should not be called directly, use loadTradingLimits () instead
+        const markets = await this.fetchMarkets ();
+        const tradingLimits = {};
+        for (let i = 0; i < markets.length; i++) {
+            const market = markets[i];
+            const symbol = market['symbol'];
+            if ((symbols === undefined) || (this.inArray (symbol, symbols))) {
+                tradingLimits[symbol] = market['limits']['amount'];
+            }
+        }
+        return tradingLimits;
     }
 }
