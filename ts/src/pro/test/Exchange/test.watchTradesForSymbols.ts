767b5b26c1fc5b80c497401902ed54674b8319fc
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-02-29 19:36:10 +0400

    feat(tests) - watchTradesForSymbols

---
+++ ts/src/pro/test/Exchange/test.watchTradesForSymbols.ts
@@ -0,0 +1,30 @@
+
+import assert from 'assert';
+import testTrade from '../../../test/Exchange/base/test.trade.js';
+import testSharedMethods from '../../../test/Exchange/base/test.sharedMethods.js';
+
+async function watchTradesForSymbols (exchange, skippedProperties, symbol) {
+    const method = 'watchTradesForSymbols';
+    let now = exchange.milliseconds ();
+    const ends = now + 15000;
+    while (now < ends) {
+        let response = undefined;
+        try {
+            response = await exchange.watchTradesForSymbols (symbol);
+        } catch (e) {
+            if (!testSharedMethods.isTemporaryFailure (e)) {
+                throw e;
+            }
+            now = exchange.milliseconds ();
+            continue;
+        }
+        assert (Array.isArray (response), exchange.id + ' ' + method + ' ' + symbol + ' must return an array. ' + exchange.json (response));
+        now = exchange.milliseconds ();
+        for (let i = 0; i < response.length; i++) {
+            testTrade (exchange, skippedProperties, method, response[i], symbol, now);
+        }
+        testSharedMethods.assertTimestampOrder (exchange, method, symbol, response);
+    }
+}
+
+export default watchTradesForSymbols;
