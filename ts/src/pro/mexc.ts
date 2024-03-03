88720c006574f46a8a19dafa474d82ea94c163c7
T. Todua <7117978+ttodua@users.noreply.github.com>
2024-03-03 13:00:43 +0400

    fix(mexc) - ws ob nonce (#21499)
    * fix(mexc) - ws ob nonce
    
    * equal nonces
    
    * comment
    
    * remove exc

--- ts/src/pro/mexc.ts
+++ ts/src/pro/mexc.ts
@@ -2,7 +2,7 @@
 //  ---------------------------------------------------------------------------
 
 import mexcRest from '../mexc.js';
-import { ExchangeError, AuthenticationError } from '../base/errors.js';
+import { AuthenticationError } from '../base/errors.js';
 import { ArrayCache, ArrayCacheBySymbolById, ArrayCacheByTimestamp } from '../base/ws/Cache.js';
 import { sha256 } from '../static_dependencies/noble-hashes/sha256.js';
 import type { Int, OHLCV, Str, OrderBook, Order, Trade, Ticker, Balances } from '../base/types.js';
@@ -514,10 +514,12 @@ export default class mexc extends mexcRest {
     }
 
     handleDelta (orderbook, delta) {
-        const nonce = this.safeInteger (orderbook, 'nonce');
+        const existingNonce = this.safeInteger (orderbook, 'nonce');
         const deltaNonce = this.safeInteger2 (delta, 'r', 'version');
-        if (deltaNonce !== nonce && deltaNonce !== nonce + 1) {
-            throw new ExchangeError (this.id + ' handleOrderBook received an out-of-order nonce');
+        if (deltaNonce < existingNonce) {
+            // even when doing < comparison, this happens: https://app.travis-ci.com/github/ccxt/ccxt/builds/269234741#L1809
+            // so, we just skip old updates
+            return;
         }
         orderbook['nonce'] = deltaNonce;
         const asks = this.safeValue (delta, 'asks', []);
