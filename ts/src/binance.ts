9c97c7a23ea66b97fa5a23613382d9c2f4d83b2e
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-03-07 23:07:03 +0400

    binance no arr

--- ts/src/binance.ts
+++ ts/src/binance.ts
@@ -3052,10 +3052,7 @@ export default class binance extends Exchange {
                 }
                 const oldSymbol = symbol + ':' + settle + '-' + this.yymmdd (expiry);
                 symbol = symbol + ':' + settle + '-' + deliveryPrefix + this.yymmdd (expiry);
-                if (!(oldSymbol in this.futuresOldSymbolsMap)) {
-                    this.futuresOldSymbolsMap[oldSymbol] = [];
-                }
-                this.futuresOldSymbolsMap[oldSymbol].push (symbol);
+                this.futuresOldSymbolsMap[oldSymbol] = symbol;
             } else if (option) {
                 symbol = symbol + ':' + settle + '-' + this.yymmdd (expiry) + '-' + strike + '-' + this.safeString (optionParts, 3);
             }
