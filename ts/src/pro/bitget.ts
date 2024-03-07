08f1b20827456e080f05457c9646574263ae1d36
Carlos Gonçalves <43336371+carlosmiei@users.noreply.github.com>
2024-03-07 10:02:36 +0000

    fix(bitget): remove filled calculation from parseWsOrder (#21567)
    * fix(bitget): remove filled calculation from parseWsOrder
    
    * parse fee

--- ts/src/pro/bitget.ts
+++ ts/src/pro/bitget.ts
@@ -1310,7 +1310,7 @@ export default class bitget extends bitgetRest {
         if (feeAmount !== undefined) {
             const feeCurrency = this.safeString (fee, 'feeCoin');
             feeObject = {
-                'cost': Precise.stringAbs (feeAmount),
+                'cost': this.parseNumber (Precise.stringAbs (feeAmount)),
                 'currency': this.safeCurrencyCode (feeCurrency),
             };
         }
@@ -1323,10 +1323,10 @@ export default class bitget extends bitgetRest {
         if (side === 'buy' && market['spot'] && (type === 'market')) {
             cost = this.safeString (order, 'newSize', cost);
         }
-        let filled = this.safeString2 (order, 'accBaseVolume', 'baseVolume');
-        if (market['spot'] && (rawStatus !== 'live')) {
-            filled = Precise.stringDiv (cost, avgPrice);
-        }
+        const filled = this.safeString2 (order, 'accBaseVolume', 'baseVolume');
+        // if (market['spot'] && (rawStatus !== 'live')) {
+        //     filled = Precise.stringDiv (cost, avgPrice);
+        // }
         let amount = this.safeString (order, 'baseVolume');
         if (!market['spot'] || !(side === 'buy' && type === 'market')) {
             amount = this.safeString (order, 'newSize', amount);
