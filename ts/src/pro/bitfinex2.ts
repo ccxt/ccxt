b4e44672465e9feb178cdb0b5eec1d19d68dc136
T. Todua <7117978+ttodua@users.noreply.github.com>
2024-03-08 21:59:14 +0400

    Discard changes to ts/src/pro/bitfinex2.ts

--- ts/src/pro/bitfinex2.ts
+++ ts/src/pro/bitfinex2.ts
@@ -331,11 +331,10 @@ export default class bitfinex2 extends bitfinex2Rest {
         const messageLength = message.length;
         if (messageLength === 2) {
             // initial snapshot
-            const trades = this.safeList (message, 1, []);
+            let trades = this.safeList (message, 1, []);
             // needs to be reversed to make chronological order
-            const tradesLength = trades.length;
-            const maxLength = Math.max (0, tradesLength - 1);
-            for (let i = maxLength; i >= 0; i--) {
+            trades = trades.reverse ();
+            for (let i = 0; i < trades.length; i++) {
                 const parsed = this.parseWsTrade (trades[i], market);
                 stored.append (parsed);
             }
