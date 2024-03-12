27ca1d5573f5fa0c1caeec6e480c7f735a10d7f0
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-03-12 19:56:03 +0400

    watch Trades

--- ts/src/pro/test/Exchange/test.watchTradesForSymbols.ts
+++ ts/src/pro/test/Exchange/test.watchTradesForSymbols.ts
@@ -3,14 +3,14 @@ import assert from 'assert';
 import testTrade from '../../../test/Exchange/base/test.trade.js';
 import testSharedMethods from '../../../test/Exchange/base/test.sharedMethods.js';
 
-async function watchTradesForSymbols (exchange, skippedProperties, symbol) {
+async function watchTradesForSymbols (exchange, skippedProperties, symbols) {
     const method = 'watchTradesForSymbols';
     let now = exchange.milliseconds ();
     const ends = now + 15000;
     while (now < ends) {
         let response = undefined;
         try {
-            response = await exchange.watchTradesForSymbols (symbol);
+            response = await exchange.watchTradesForSymbols (symbols);
         } catch (e) {
             if (!testSharedMethods.isTemporaryFailure (e)) {
                 throw e;
@@ -18,10 +18,14 @@ async function watchTradesForSymbols (exchange, skippedProperties, symbol) {
             now = exchange.milliseconds ();
             continue;
         }
-        assert (Array.isArray (response), exchange.id + ' ' + method + ' ' + symbol + ' must return an array. ' + exchange.json (response));
+        assert (Array.isArray (response), exchange.id + ' ' + method + ' ' + exchange.json (symbols) + ' must return an array. ' + exchange.json (response));
         now = exchange.milliseconds ();
+        let symbol = undefined;
         for (let i = 0; i < response.length; i++) {
-            testTrade (exchange, skippedProperties, method, response[i], symbol, now);
+            const trade = response[i];
+            symbol = trade['symbol'];
+            testTrade (exchange, skippedProperties, method, trade, symbol, now);
+            testSharedMethods.assertInArray (exchange, skippedProperties, method, trade, 'symbol', symbols);
         }
         testSharedMethods.assertTimestampOrder (exchange, method, symbol, response);
     }
