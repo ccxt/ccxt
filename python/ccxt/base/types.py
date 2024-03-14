e8dcc04c64a50881d560d876cc4e809a3d715f3c
Sam Germain <sam.germain@usask.ca>
2024-03-14 08:25:43 -0400

    chore: add types LastPrice, LastPrices (#21711)

--- python/ccxt/base/types.py
+++ python/ccxt/base/types.py
@@ -336,5 +336,16 @@ class CurrencyInterface(TypedDict):
     precision: Num
 
 
+class LastPrice(TypedDict):
+    symbol: Str
+    timestamp: Int
+    datetime: Str
+    price: Num
+    side: OrderSide
+    info: Dict[str, Any]
+
+
+LastPrices = Dict[Str, LastPrice]
+
 Market = Optional[MarketInterface]
 Currency = Optional[CurrencyInterface]
