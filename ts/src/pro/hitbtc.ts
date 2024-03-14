5ab0ec29957498017c3c5cb9c60bdd18bc072e83
CH-EdwinB <121802024+CH-EdwinB@users.noreply.github.com>
2024-03-14 15:40:49 +0100

    Update hitbtc.ts

--- ts/src/pro/hitbtc.ts
+++ ts/src/pro/hitbtc.ts
@@ -254,8 +254,8 @@ export default class hitbtc extends hitbtcRest {
         //        }
         //    }
         //
-        const snapshot = this.safeValue (message, 'snapshot', undefined);
-        const update = this.safeValue (message, 'update', undefined);
+        const snapshot = this.safeValue (message, 'snapshot');
+        const update = this.safeValue (message, 'update');
         const data = snapshot ? snapshot : update;
         const type = snapshot ? 'snapshot' : 'update';
         const marketIds = Object.keys (data);
