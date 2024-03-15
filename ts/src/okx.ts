9d9983ee8bf5007cf360db6f125e6786447b25d4
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-03-15 11:27:53 +0000

    fix(okx): fundingRateHistory pagination

--- ts/src/okx.ts
+++ ts/src/okx.ts
@@ -2243,7 +2243,7 @@ export default class okx extends Exchange {
         let paginate = false;
         [ paginate, params ] = this.handleOptionAndParams (params, 'fetchFundingRateHistory', 'paginate');
         if (paginate) {
-            return await this.fetchPaginatedCallDeterministic ('fetchFundingRateHistory', symbol, since, limit, '8h', params) as FundingRateHistory[];
+            return await this.fetchPaginatedCallDeterministic ('fetchFundingRateHistory', symbol, since, limit, '8h', params, 100) as FundingRateHistory[];
         }
         const market = this.market (symbol);
         const request = {
