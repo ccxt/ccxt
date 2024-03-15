287aa69ced50dbd68b5b93ad85ceb68f9212bc47
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-03-15 12:07:01 +0400

    ololog - stacktracey replacement

---
+++ ts/cli/static_dependencies/ololog.cjs
@@ -0,0 +1,370 @@
+"use strict";
+
+/*  ------------------------------------------------------------------------ */
+
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+function _defineEnumerableProperties(obj, descs) { for (var key in descs) { var desc = descs[key]; desc.configurable = desc.enumerable = true; if ("value" in desc) desc.writable = true; Object.defineProperty(obj, key, desc); } return obj; }
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+function _toArray(arr) { return Array.isArray(arr) ? arr : Array.from(arr); }
+
+var O = Object,
+    // StackTracey = require('stacktracey'),
+    ansi = require('ansicolor'),
+    bullet = require('string.bullet'),
+    pipez = require('pipez');
+
+/*  ------------------------------------------------------------------------ */
+
+var _stringify = require('string.ify').configure({
+    formatter: function formatter(x, stringify) {
+
+        if (x instanceof Error && !(typeof Symbol !== 'undefined' && x[Symbol.for('String.ify')])) {
+
+            if (stringify.state.depth > 0) return '<Error: ' + x.message + '>'; // prevents unwanted pretty printing for Errors that are properties of complex objects
+
+            var indent = '    ',
+                why = stringify.limit((x.message || '').replace(/\r|\n/g, '').trim(), stringify.state.maxErrorMessageLength || 120),
+                // stack = new StackTracey(x).withSources().asTable(),
+                stack = (new Error()).stack;
+                stackIndented = stack.split('\n').map(function (x) {
+                return indent + x;
+            }).join('\n'),
+                isAssertion = 'actual' in x && 'expected' in x,
+                type = x.constructor.name || 'Error';
+
+            if (isAssertion) {
+
+                var str = stringify.configure({ maxStringLength: Number.MAX_VALUE, maxDepth: 8 });
+
+                var actual = bullet(indent + 'actual:   ', str(x.actual)),
+                    expected = bullet(indent + 'expected: ', str(x.expected));
+
+                if (actual.split('\n').length > 1 || expected.split('\n').length > 1) // if multiline actual/expected, need extra whitespace inbetween
+                    actual += '\n';
+
+                return '[' + type + '] ' + why + '\n\n' + ansi.red(actual) + '\n' + ansi.green(expected) + '\n\n' + stackIndented + '\n';
+            } else {
+                return '[' + type + '] ' + why + '\n\n' + stackIndented + '\n';
+            }
+        }
+    }
+});
+
+/*  ------------------------------------------------------------------------ */
+
+var _require = require('printable-characters'),
+    isBlank = _require.isBlank,
+    blank = _require.blank,
+    changeLastNonemptyLine = function changeLastNonemptyLine(lines, fn) {
+
+    for (var i = lines.length - 1; i >= 0; i--) {
+
+        if (i === 0 || !isBlank(lines[i])) {
+
+            lines[i] = fn(lines[i]);
+            break;
+        }
+    }
+    return lines;
+};
+
+/*  ------------------------------------------------------------------------ */
+
+var log = pipez({
+
+    /*  ------------------------------------------------------------------------ */
+
+    stringify: function stringify(args, cfg) {
+        var print = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : cfg.print || _stringify.configure(cfg);
+        return args.map(function (arg) {
+            return typeof arg === 'string' ? arg : print(arg);
+        });
+    },
+
+    trim: function trim(tokens, _ref) {
+        var _ref$max = _ref.max,
+            max = _ref$max === undefined ? undefined : _ref$max;
+        return !max ? tokens : tokens.map(function (t) {
+            return _stringify.limit(t, max);
+        });
+    },
+
+    lines: function lines(tokens, _ref2) {
+        var _ref2$linebreak = _ref2.linebreak,
+            linebreak = _ref2$linebreak === undefined ? '\n' : _ref2$linebreak;
+
+
+        var lines = [[]];
+        var leftPad = [];
+
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+            for (var _iterator = tokens[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                var t = _step.value;
+
+                var _t$split = t.split(linebreak),
+                    _t$split2 = _toArray(_t$split),
+                    first = _t$split2[0],
+                    rest = _t$split2.slice(1);
+
+                lines[lines.length - 1].push(first);
+                lines = [].concat(_toConsumableArray(lines), _toConsumableArray(rest.map(function (t) {
+                    return t ? [].concat(leftPad, [t]) : [];
+                })));
+
+                var pad = blank(!rest.length ? t : rest[rest.length - 1]);
+
+                if (pad) {
+                    leftPad.push(pad);
+                }
+            }
+        } catch (err) {
+            _didIteratorError = true;
+            _iteratorError = err;
+        } finally {
+            try {
+                if (!_iteratorNormalCompletion && _iterator.return) {
+                    _iterator.return();
+                }
+            } finally {
+                if (_didIteratorError) {
+                    throw _iteratorError;
+                }
+            }
+        }
+
+        return lines;
+    },
+
+    concat: function concat(lines, _ref3) {
+        var _ref3$separator = _ref3.separator,
+            separator = _ref3$separator === undefined ? ' ' : _ref3$separator;
+        return lines.map(function (tokens) {
+            return tokens.join(separator);
+        });
+    },
+
+    indent: function indent(lines, _ref4) {
+        var _ref4$level = _ref4.level,
+            level = _ref4$level === undefined ? 0 : _ref4$level,
+            _ref4$pattern = _ref4.pattern,
+            pattern = _ref4$pattern === undefined ? '\t' : _ref4$pattern;
+        return lines.map(function (line) {
+            return pattern.repeat(level) + line;
+        });
+    },
+
+    tag: function tag(lines, _ref5) {
+        var _ref5$level = _ref5.level,
+            level = _ref5$level === undefined ? '' : _ref5$level,
+            _ref5$levelColor = _ref5.levelColor,
+            levelColor = _ref5$levelColor === undefined ? {
+            'info': ansi.cyan,
+            'warn': ansi.yellow,
+            'debug': ansi.blue,
+            'error': ansi.bright.red } : _ref5$levelColor;
+        return bullet((levelColor[level] || function (s) {
+            return s;
+        })(level.toUpperCase().padStart(6) + '\t'), lines);
+    },
+
+    time: function time(lines, _ref6) {
+        var _ref6$when = _ref6.when,
+            when = _ref6$when === undefined ? new Date() : _ref6$when,
+            _ref6$format = _ref6.format,
+            format = _ref6$format === undefined ? 'locale' : _ref6$format,
+            _ref6$locale = _ref6.locale,
+            locale = _ref6$locale === undefined ? [] : _ref6$locale,
+            _ref6$options = _ref6.options,
+            options = _ref6$options === undefined ? {} : _ref6$options,
+            _ref6$print = _ref6.print,
+            print = _ref6$print === undefined ? function (when) {
+            return ansi.darkGray(format === 'iso' ? when.toISOString() : format === 'locale' ? when.toLocaleString(locale, options) : format === 'utc' ? when.toUTCString() : when.toString()) + '\t';
+        } : _ref6$print;
+        return bullet(print(when), lines);
+    },
+
+    locate: function locate(lines, _ref7) {
+        var _ref7$shift = _ref7.shift,
+            shift = _ref7$shift === undefined ? 0 : _ref7$shift,
+            _ref7$where = _ref7.where,
+            where = _ref7$where === undefined ? new StackTracey().clean().at(1 + shift) : _ref7$where,
+            _ref7$join = _ref7.join,
+            join = _ref7$join === undefined ? function (a, sep, b) {
+            return a && b ? a + sep + b : a || b;
+        } : _ref7$join,
+            _ref7$print = _ref7.print,
+            print = _ref7$print === undefined ? function (_ref8) {
+            var calleeShort = _ref8.calleeShort,
+                _ref8$fileName = _ref8.fileName,
+                fileName = _ref8$fileName === undefined ? [] : _ref8$fileName,
+                _ref8$line = _ref8.line,
+                line = _ref8$line === undefined ? [] : _ref8$line;
+            return ansi.darkGray('(' + join(calleeShort, ' @ ', join(fileName, ':', line)) + ')');
+        } : _ref7$print;
+        return changeLastNonemptyLine(lines, function (line) {
+            return join(line, ' ', print(where));
+        });
+    },
+
+    join: function join(lines, _ref9) {
+        var _ref9$linebreak = _ref9.linebreak,
+            linebreak = _ref9$linebreak === undefined ? '\n' : _ref9$linebreak;
+        return lines.join(linebreak);
+    },
+
+    render: function render(text, _ref10) {
+        var _ref10$engine = _ref10.engine,
+            engine = _ref10$engine === undefined ? typeof window !== 'undefined' && window.window === window && window.navigator ? navigator.userAgent.indexOf('Chrome') >= 0 ? 'chrome' : 'generic' : 'ansi' : _ref10$engine,
+            _ref10$engines = _ref10.engines,
+            engines = _ref10$engines === undefined ? {/* configurable */} : _ref10$engines,
+            _ref10$consoleMethod = _ref10.consoleMethod,
+            consoleMethod = _ref10$consoleMethod === undefined ? 'log' : _ref10$consoleMethod,
+            _ref10$defaults = _ref10.defaults,
+            defaults = _ref10$defaults === undefined ? {
+
+            ansi: function ansi(s) {
+                return console[consoleMethod](s);
+            },
+            chrome: function chrome(s) {
+                var _console;
+
+                return (_console = console)[consoleMethod].apply(_console, _toConsumableArray(ansi.parse(s).asChromeConsoleLogArguments));
+            },
+            generic: function generic(s) {
+                return console[consoleMethod](ansi.strip(s));
+            }
+        } : _ref10$defaults;
+        return text && O.assign(defaults, engines)[engine](text), text;
+    },
+
+    returnValue: function returnValue(__, _ref11) {
+        var _ref11$initialArgumen = _slicedToArray(_ref11.initialArguments, 1),
+            firstArgument = _ref11$initialArgumen[0];
+
+        return firstArgument;
+    }
+
+    /*  ------------------------------------------------------------------------ */
+
+}).configure({
+
+    time: false, // disables some steps (until enabled back explicitly)
+    tag: false
+
+    /*  ------------------------------------------------------------------------ */
+
+}).methods({
+
+    get noop() {
+        return pipez({ returnValue: function returnValue(args) {
+                return args[0];
+            } }).methods(this.methods_);
+    },
+    get null() {
+        return this.noop;
+    }, // LEGACY, DEPRECATED (left here for backward compatibility)
+
+    indent: function indent(level) {
+        return this.configure({ indent: { level: level } });
+    },
+
+
+    get error() {
+        return this.configure({ tag: { level: 'error' }, render: { consoleMethod: 'error' } });
+    },
+    get warn() {
+        return this.configure({ tag: { level: 'warn' }, render: { consoleMethod: 'warn' } });
+    },
+    get info() {
+        return this.configure({ tag: { level: 'info' }, render: { consoleMethod: 'info' } });
+    },
+    get debug() {
+        return this.configure({ tag: { level: 'debug' }, render: { consoleMethod: 'debug' } });
+    },
+
+    maxArrayLength: function maxArrayLength(n) {
+        return this.configure({ stringify: { maxArrayLength: n } });
+    },
+    maxObjectLength: function maxObjectLength(n) {
+        return this.configure({ stringify: { maxObjectLength: n } });
+    },
+    maxDepth: function maxDepth(n) {
+        return this.configure({ stringify: { maxDepth: n } });
+    },
+    maxLength: function maxLength(n) {
+        return this.configure({ stringify: { maxLength: n } });
+    },
+
+
+    get unlimited() {
+        return this.configure({ stringify: { maxStringLength: Number.MAX_VALUE,
+                maxObjectLength: Number.MAX_VALUE,
+                maxArrayLength: Number.MAX_VALUE,
+                maxDepth: Number.MAX_VALUE,
+                maxErrorMessageLength: Number.MAX_VALUE } });
+    },
+
+    get noPretty() {
+        return this.configure({ stringify: { pretty: false } });
+    },
+    get noFancy() {
+        return this.configure({ stringify: { fancy: false } });
+    },
+    get noRightAlignKeys() {
+        return this.configure({ stringify: { rightAlignKeys: false } });
+    },
+    get noLocate() {
+        return this.configure({ locate: false });
+    },
+    precision: function precision(n) {
+        return this.configure({ stringify: { precision: n } });
+    },
+
+
+    get serialize() {
+        return this.before('render');
+    },
+    get deserialize() {
+        return this.from('render');
+    },
+
+    newline: function newline() {
+        return this.from('join')(['']);
+    },
+    handleNodeErrors: function handleNodeErrors() {
+        var _this = this;
+
+        process.on('uncaughtException', function (e) {
+            _this.bright.red.error.noLocate(e);process.exit(1);
+        });
+        process.on('unhandledRejection', function (e) {
+            _this.bright.red.error.noLocate(e);process.exit(1);
+        });
+        return this;
+    }
+});
+
+/*  ------------------------------------------------------------------------ */
+
+ansi.names.forEach(function (color) {
+    var _log$methods, _mutatorMap;
+
+    log.methods((_log$methods = {}, _mutatorMap = {}, _mutatorMap[color] = _mutatorMap[color] || {}, _mutatorMap[color].get = function () {
+        return this.configure({ 'concat+': function concat(lines) {
+                return lines.map(ansi[color]);
+            } });
+    }, _defineEnumerableProperties(_log$methods, _mutatorMap), _log$methods));
+});
+
+/*  ------------------------------------------------------------------------ */
+
+module.exports = log;
+
+/*  ------------------------------------------------------------------------ */
