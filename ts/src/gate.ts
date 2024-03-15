80fe8f88f8b81d7f6e5e8eb5ab3a1926119e28b8
T. Todua <7117978+ttodua@users.noreply.github.com>
2024-03-15 14:53:54 +0400

    fix(gate) - fetchOHLCV since & limit for contracts (#21705)
    * fix(gate) - fetchOHLCV since & limit for contracts
    
    * feat(gate) - static tests
    
    * until fix

--- ts/src/gate.ts
+++ ts/src/gate.ts
@@ -2913,7 +2913,7 @@ export default class gate extends Exchange {
         let request = {};
         [ request, params ] = this.prepareRequest (market, undefined, params);
         request['interval'] = this.safeString (this.timeframes, timeframe, timeframe);
-        let maxLimit = 1000;
+        let maxLimit = market['contract'] ? 1999 : 1000;
         limit = (limit === undefined) ? maxLimit : Math.min (limit, maxLimit);
         let until = this.safeInteger (params, 'until');
         if (until !== undefined) {
