a066b721573d96f1f15f04a85de5f7d2687a5eae
Dan <83686770+Dan-krm@users.noreply.github.com>
2024-03-16 05:44:45 -0600

    coinex: update documentation links (#21751)
    * coinex: change documentation links
    
    Changed the coinex API documentation links
    from:
    https://github.com/coinexcom/coinex_exchange_api/wiki
    to:
    https://viabtc.github.io/coinex_api_en_doc/general
    
    * removed /general from links
    
    * revert paths
    
    * revert
    
    ---------
    
    Co-authored-by: carlosmiei <43336371+carlosmiei@users.noreply.github.com>

--- ts/src/coinex.ts
+++ ts/src/coinex.ts
@@ -136,7 +136,7 @@ export default class coinex extends Exchange {
                     'perpetualPrivate': 'https://api.coinex.com/perpetual',
                 },
                 'www': 'https://www.coinex.com',
-                'doc': 'https://github.com/coinexcom/coinex_exchange_api/wiki',
+                'doc': 'https://viabtc.github.io/coinex_api_en_doc',
                 'fees': 'https://www.coinex.com/fees',
                 'referral': 'https://www.coinex.com/register?refer_code=yw5fz',
             },
@@ -5152,7 +5152,7 @@ export default class coinex extends Exchange {
          * @method
          * @name coinex#borrowIsolatedMargin
          * @description create a loan to borrow margin
-         * @see https://github.com/coinexcom/coinex_exchange_api/wiki/086margin_loan
+         * @see https://viabtc.github.io/coinex_api_en_doc/spot/#docsspot002_account017_margin_loan
          * @param {string} symbol unified market symbol, required for coinex
          * @param {string} code unified currency code of the currency to borrow
          * @param {float} amount the amount to borrow
@@ -5190,7 +5190,7 @@ export default class coinex extends Exchange {
          * @method
          * @name coinex#repayIsolatedMargin
          * @description repay borrowed margin and interest
-         * @see https://github.com/coinexcom/coinex_exchange_api/wiki/087margin_flat
+         * @see https://viabtc.github.io/coinex_api_en_doc/spot/#docsspot002_account018_margin_flat
          * @param {string} symbol unified market symbol, required for coinex
          * @param {string} code unified currency code of the currency to repay
          * @param {float} amount the amount to repay
