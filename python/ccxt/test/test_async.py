b74f28641cb5a8ac2cdb75d4750f40d6335dfa41
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-03-20 23:57:11 +0400

    test.py

--- python/ccxt/test/test_async.py
+++ python/ccxt/test/test_async.py
@@ -1463,5 +1463,6 @@ class testMainClass(baseMainTestClass):
 
 if __name__ == '__main__':
     argvSymbol = argv.symbol if argv.symbol and '/' in argv.symbol else None
+    # in python, we check it through "symbol" arg (as opposed to JS/PHP) because argvs were already built above
     argvMethod = argv.symbol if argv.symbol and '()' in argv.symbol else None
     asyncio.run(testMainClass().init(argv.exchange, argvSymbol, argvMethod))
