9fb4275116a625d4ab31a711259d5f1f852ccb13
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-03-20 23:02:45 +0400

    fetchTrades tests final updates

--- ts/src/test/Exchange/test.fetchTrades.ts
+++ ts/src/test/Exchange/test.fetchTrades.ts
@@ -8,59 +8,59 @@ import Precise from '../../base/Precise.js';
 async function testFetchTrades (exchange, skippedProperties, symbol) {
     const method = 'fetchTrades';
     const trades = await exchange.fetchTrades (symbol, undefined, 12000); // lets test with unrealistically high amount
-    testSharedMethods.assertNonEmtpyArray (exchange, skippedProperties, method, trades);
-    await testFetchTrades_Structure (exchange, skippedProperties, symbol, method, trades);
-    await testFetchTrades_ArrayValues (exchange, skippedProperties, symbol, method, trades);
-    await testFetchTrades_Side (exchange, skippedProperties, symbol, method, trades);
+    await testFetchTradesResponse (exchange, skippedProperties, symbol, method, trades);
 }
 
-async function testFetchTrades_Structure (exchange, skippedProperties, symbol, method, trades) {
-    const now = exchange.milliseconds ();
-    for (let i = 0; i < trades.length; i++) {
-        testTrade (exchange, skippedProperties, method, trades[i], symbol, now);
-    }
+async function testFetchTradesResponse (exchange, skippedProperties, symbol, method, trades) {
+    testSharedMethods.assertNonEmtpyArray (exchange, skippedProperties, method, trades);
+    await testFetchTradesStructure (exchange, skippedProperties, symbol, method, trades);
     if (!('timestamp' in skippedProperties)) {
         testSharedMethods.assertTimestampOrder (exchange, method, symbol, trades);
     }
+    if (!('side' in skippedProperties) && trades.length > 50) {
+        await testFetchTradesSideValues (exchange, skippedProperties, symbol, method, trades);
+    }
+    if (('side' in skippedProperties) || ('timestamp' in skippedProperties)) {
+        await testFetchTradesSideSequence (exchange, skippedProperties, symbol, method, trades);
+    }
 }
 
-async function testFetchTrades_ArrayValues (exchange, skippedProperties, symbol, method, trades) {
-    //
-    // address accuracy issues: https://github.com/ccxt/ccxt/issues/18986
-    //
-    const collectedSides = {};
+async function testFetchTradesStructure (exchange, skippedProperties, symbol, method, trades) {
+    const now = exchange.milliseconds ();
+    const isPublicTrade = true;
     for (let i = 0; i < trades.length; i++) {
-        const trade = trades[i];
-        testSharedMethods.assertInArray (exchange, skippedProperties, method, trade, 'takerOrMaker', [ 'taker', undefined ]);
-        testSharedMethods.assertInArray (exchange, skippedProperties, method, trade, 'side', [ 'buy', 'sell' ]);
-        collectedSides[trade['side']] = true;
-    }
-    // ensure both 'buy' & 'sell' are returned (if trades are > 50, it'd be happening in most cases)
-    if (trades.length >= 50 && !('bothSides' in skippedProperties)) {
-        const msg = ' side of trades are not being returned, if this error happens consistently, then it might be an implementation issue' + testSharedMethods.logTemplate (exchange, method, trades);
-        assert (('buy' in collectedSides), 'buy' + msg);
-        assert (('sell' in collectedSides), 'sell' + msg);
+        testTrade (exchange, skippedProperties, method, trades[i], symbol, now, isPublicTrade);
     }
 }
 
-async function testFetchTrades_Side (exchange, skippedProperties, symbol, method, trades) {
-    if (('timestamp' in skippedProperties) || ('side' in skippedProperties)) {
-        return;
-    }
-    // Check whether side is correct. This can be found out deterministically,
-    // by checking an order that has been filled with multiple trades at the
-    // same time (but on different prices). The price between first and last
-    // trade will definitely be directional. for example, an order with 3 fills:
-    //     - 1600000000073 : ...
-    //     - 1600000000111 : 0.4 ETH at 1750.40
-    //     - 1600000000111 : 0.9 ETH at 1750.41
-    //     - 1600000000111 : 0.33 ETH at 1750.42
-    //     - 1600000000252 : ...
-    // here it's definitely visible that the trades have been `buy` as it happened
-    // on same timestamp and trades are increasing in price. if it was `sell` the
-    // prices would have been in decreasing order. it's nearly impossible to happen
-    // otherwie (if such rare event happens ever, the test can be restarted and
-    // the new run would not meet such exceptional case)
+async function testFetchTradesSideValues (exchange, skippedProperties, symbol, method, trades) {
+    /*
+        Check whether both "buy" and "sell" are returned from trades, when there are more than 50 trades
+    */
+    const grouped = exchange.groupBy (trades, 'side');
+    const keys = Object.keys (grouped);
+    const msg = 'Both sides of trades are not being returned, instead only one side is being returned. If this error happens consistently, then it might be an implementation issue' + testSharedMethods.logTemplate (exchange, method, trades);
+    assert (('buy' in keys), msg);
+    assert (('sell' in keys), msg);
+}
+
+async function testFetchTradesSideSequence (exchange, skippedProperties, symbol, method, trades) {
+    /*
+        Check whether side is correct. This can be found out deterministically,
+        by checking an order that has been filled with multiple trades at the
+        same time (but on different prices). The price between first and last
+        trade will definitely be directional. for example, an order with 3 fills:
+            - 1600000000073 : ...
+            - 1600000000111 : 0.4 ETH at 1750.40
+            - 1600000000111 : 0.9 ETH at 1750.41
+            - 1600000000111 : 0.33 ETH at 1750.42
+            - 1600000000252 : ...
+        here it's definitely visible that the trades have been `buy` as it happened
+        on same timestamp and trades are increasing in price. if it was `sell` the
+        prices would have been in decreasing order. it's nearly impossible to happen
+        otherwie (if such rare event happens ever, the test can be restarted and
+        the new run would not meet such exceptional case)
+    */
     let lastTs = undefined;
     let lastPrice = undefined;
     let lastSide = undefined;
@@ -77,7 +77,7 @@ async function testFetchTrades_Side (exchange, skippedProperties, symbol, method
         const isSameSide = side === lastSide;
         lastSide = side;
         //
-        // we are only interested in trades, that have: same timestamp, same side but different price
+        // we are only interested in trades that have: same timestamp, same side, but different(!) price
         if (!isSameTs || !isSameSide || isSamePrice) {
             continue;
         }
