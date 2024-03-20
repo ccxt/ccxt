f338c03dc728a3d616d342facabc8d3f2c83f8d1
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-03-21 00:12:43 +0400

    upd

--- ts/src/pro/cex.ts
+++ ts/src/pro/cex.ts
@@ -1076,8 +1076,7 @@ export default class cex extends cexRest {
         await this.loadMarkets ();
         const market = this.market (symbol);
         symbol = market['symbol'];
-        timeframe = this.safeString (this.timeframes, timeframe, timeframe);
-        const messageHash = 'ohlcv-' + timeframe + ':' + symbol;
+        const messageHash = 'ohlcv:' + symbol;
         const url = this.urls['api']['ws'];
         const request = {
             'e': 'init-ohlcv',
@@ -1164,7 +1163,7 @@ export default class cex extends cexRest {
         const data = this.safeValue (message, 'data', {});
         const pair = this.safeString (data, 'pair');
         const symbol = this.pairToSymbol (pair);
-        const messageHash = 'ohlcv-1m:' + symbol;
+        const messageHash = 'ohlcv:' + symbol;
         const ohlcv = [
             this.safeTimestamp (data, 'time'),
             this.safeNumber (data, 'o'),
