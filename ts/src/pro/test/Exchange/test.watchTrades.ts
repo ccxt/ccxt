ea7df5fdba43958db309c64ca990dde2728493a1
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-03-20 23:03:08 +0400

    wT

--- ts/src/pro/test/Exchange/test.watchTrades.ts
+++ ts/src/pro/test/Exchange/test.watchTrades.ts
@@ -21,7 +21,7 @@ async function testWatchTrades (exchange, skippedProperties, symbol) {
         testSharedMethods.assertNonEmtpyArray (exchange, skippedProperties, method, response);
         now = exchange.milliseconds ();
         for (let i = 0; i < response.length; i++) {
-            testTrade (exchange, skippedProperties, method, response[i], symbol, now);
+            testTrade (exchange, skippedProperties, method, response[i], symbol, now, true);
         }
         if (!('timestamp' in skippedProperties)) {
             testSharedMethods.assertTimestampOrder (exchange, method, symbol, response);
