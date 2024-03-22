36bbe780427ef44f4560a9cd77238e3960d0d417
T. Todua <7117978+ttodua@users.noreply.github.com>
2024-03-22 16:34:43 +0400

    feat(tests) - watchBidsAsks (#21854)
    * feat(tests) - watchBidsAsks
    
    * wBA
    
    * ticker test upd
    
    * watchbidsasks add
    
    * gemini fix
    
    * ticker includes
    
    * ticker method type
    
    * astt fix
    
    * str to bool fix
    
    * tostring

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -4561,6 +4561,10 @@ export default class Exchange {
         throw new NotSupported (this.id + ' fetchOrderBooks() is not supported yet');
     }
 
+    async watchBidsAsks (symbols: string[] = undefined, params = {}): Promise<Tickers> {
+        throw new NotSupported (this.id + ' watchBidsAsks() is not supported yet');
+    }
+
     async watchTickers (symbols: string[] = undefined, params = {}): Promise<Tickers> {
         throw new NotSupported (this.id + ' watchTickers() is not supported yet');
     }
