da5bc96345ddb91c0b21a7f8cb7e446d08037b7d
Carlos Gonçalves <43336371+carlosmiei@users.noreply.github.com>
2024-03-25 10:17:27 +0000

    fix(hyperliquid): setmarginMOde (#21909)
    * fix(hyperliquid): setmarginMOde
    
    * static tests

--- ts/src/hyperliquid.ts
+++ ts/src/hyperliquid.ts
@@ -1815,7 +1815,7 @@ export default class hyperliquid extends Exchange {
             throw new ArgumentsRequired (this.id + ' setMarginMode() requires a leverage parameter');
         }
         const asset = this.parseToInt (market['baseId']);
-        const isCross = (marginMode === 'isolated');
+        const isCross = (marginMode === 'cross');
         const nonce = this.milliseconds ();
         params = this.omit (params, [ 'leverage' ]);
         const updateAction = {
@@ -1831,9 +1831,10 @@ export default class hyperliquid extends Exchange {
                 vaultAddress = vaultAddress.replace ('0x', '');
             }
         }
-        const signature = this.signL1Action (updateAction, nonce, vaultAddress);
+        const extendedAction = this.extend (updateAction, params);
+        const signature = this.signL1Action (extendedAction, nonce, vaultAddress);
         const request = {
-            'action': updateAction,
+            'action': extendedAction,
             'nonce': nonce,
             'signature': signature,
             // 'vaultAddress': vaultAddress,
@@ -1841,7 +1842,7 @@ export default class hyperliquid extends Exchange {
         if (vaultAddress !== undefined) {
             request['vaultAddress'] = vaultAddress;
         }
-        const response = await this.privatePostExchange (this.extend (request, params));
+        const response = await this.privatePostExchange (request);
         //
         //     {
         //         'response': {
