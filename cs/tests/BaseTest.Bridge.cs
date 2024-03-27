9249a45500e7658728cf760409d50991af74c73a
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-03-28 00:05:07 +0400

    divide cs

--- cs/tests/BaseTest.Bridge.cs
+++ cs/tests/BaseTest.Bridge.cs
@@ -98,6 +98,7 @@ public partial class BaseTest
     public static object add(object a, object b) => Exchange.add(a, b);
     public static object multiply(object a, object b) => Exchange.multiply(a, b);
     public static object subtract(object a, object b) => Exchange.subtract(a, b);
+    public static object divide(object a, object b) => Exchange.divide(a, b);
     public static string toStringOrNull(object a) => Exchange.toStringOrNull(a);
     public static bool isEqual(object a, object b) => Exchange.isEqual(a, b);
     public static bool isTrue(object a) => Exchange.isTrue(a);
