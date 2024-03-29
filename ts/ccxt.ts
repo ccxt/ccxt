8c8d8cce9c1846ae819b8ad9a2c0f7d8352f76c1
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-03-29 21:09:47 +0400

    fix(import-export) - all errors

--- ts/ccxt.ts
+++ ts/ccxt.ts
@@ -34,8 +34,12 @@ import { Precise }   from './src/base/Precise.js'
 import * as functions from './src/base/functions.js'
 import * as errors   from './src/base/errors.js'
 import type { Market, Trade , Fee, Ticker, OrderBook, Order, Transaction, Tickers, Currency, Balance, DepositAddress, WithdrawalResponse, DepositAddressResponse, OHLCV, Balances, PartialBalances, Dictionary, MinMax, Position, FundingRateHistory, Liquidation, FundingHistory, MarginMode, Greeks, Leverage, Leverages, Option, OptionChain } from './src/base/types.js'
-import { BaseError, ExchangeError, PermissionDenied, AccountNotEnabled, AccountSuspended, ArgumentsRequired, BadRequest, BadSymbol, MarginModeAlreadySet, BadResponse, NullResponse, InsufficientFunds, InvalidAddress, InvalidOrder, OrderNotFound, OrderNotCached, CancelPending, OrderImmediatelyFillable, OrderNotFillable, DuplicateOrderId, NotSupported, NetworkError, DDoSProtection, RateLimitExceeded, ExchangeNotAvailable, OnMaintenance, InvalidNonce, RequestTimeout, AuthenticationError, AddressPending, NoChange }  from './src/base/errors.js'
 
+import { BaseError, ExchangeClosedByUser, ExchangeError, PermissionDenied, AccountNotEnabled, AccountSuspended, ArgumentsRequired, BadRequest, BadSymbol, MarginModeAlreadySet, BadResponse, NullResponse, InsufficientFunds, InvalidAddress, InvalidOrder, OrderNotFound, OrderNotCached, CancelPending, OrderImmediatelyFillable, OrderNotFillable, DuplicateOrderId, NotSupported, NetworkError, DDoSProtection, RateLimitExceeded, ExchangeNotAvailable, OnMaintenance, InvalidNonce, RequestTimeout, AuthenticationError, AddressPending, ContractUnavailable, NoChange, OperationRejected, OperationFailed, ProxyError }  from './src/base/errors.js'
+
+export {
+    BaseError, ExchangeClosedByUser, ExchangeError, PermissionDenied, AccountNotEnabled, AccountSuspended, ArgumentsRequired, BadRequest, BadSymbol, MarginModeAlreadySet, BadResponse, NullResponse, InsufficientFunds, InvalidAddress, InvalidOrder, OrderNotFound, OrderNotCached, CancelPending, OrderImmediatelyFillable, OrderNotFillable, DuplicateOrderId, NotSupported, NetworkError, DDoSProtection, RateLimitExceeded, ExchangeNotAvailable, OnMaintenance, InvalidNonce, RequestTimeout, AuthenticationError, AddressPending, ContractUnavailable, NoChange, OperationRejected, OperationFailed, ProxyError
+}
 
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
@@ -413,37 +417,6 @@ export {
     Precise,
     functions,
     errors,
-    BaseError,
-    ExchangeError,
-    PermissionDenied,
-    AccountNotEnabled,
-    AccountSuspended,
-    ArgumentsRequired,
-    BadRequest,
-    BadSymbol,
-    MarginModeAlreadySet,
-    BadResponse,
-    NullResponse,
-    InsufficientFunds,
-    InvalidAddress,
-    InvalidOrder,
-    OrderNotFound,
-    OrderNotCached,
-    CancelPending,
-    OrderImmediatelyFillable,
-    OrderNotFillable,
-    DuplicateOrderId,
-    NotSupported,
-    NetworkError,
-    DDoSProtection,
-    RateLimitExceeded,
-    ExchangeNotAvailable,
-    OnMaintenance,
-    InvalidNonce,
-    RequestTimeout,
-    AuthenticationError,
-    AddressPending,
-    NoChange,
     Market,
     Trade,
     Fee,
