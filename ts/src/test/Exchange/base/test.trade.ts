d7f5ced65c0c08842cb3670a9568d5d536759fd6
T. Todua <7117978+ttodua@users.noreply.github.com>
2024-03-30 10:16:42 +0400

    Update test.trade.ts

--- ts/src/test/Exchange/base/test.trade.ts
+++ ts/src/test/Exchange/base/test.trade.ts
@@ -1,7 +1,7 @@
 import { Exchange } from "../../../../ccxt";
 import testSharedMethods from './test.sharedMethods.js';
 
-function testTrade (exchange, skippedProperties, method, entry, symbol = undefined, now = undefined) {
+function testTrade (exchange: Exchange, skippedProperties: string[], method: string, entry: object, symbol: string, now: number) {
     const format = {
         'info': { },
         'id': '12345-67890:09876/54321', // string trade id
