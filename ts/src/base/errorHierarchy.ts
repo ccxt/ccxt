cebda38978b03050146919bef352c71d3d9c70cb
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-04-01 20:40:25 +0400

    orderbooknoncemismatch

--- ts/src/base/errorHierarchy.ts
+++ ts/src/base/errorHierarchy.ts
@@ -46,6 +46,7 @@ const errorHierarchy = {
                 'InvalidNonce': {},
                 'RequestTimeout': {},
             },
+            'OrderBookNonceMismatch': {},
         },
     },
 };
