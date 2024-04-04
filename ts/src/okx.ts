707a790db73ede503d73957f3482068da4c10a17
rayBastard <roman@president-kids.ru>
2024-04-04 20:47:05 +0300

    Update `fetchAccounts` method

--- ts/src/okx.ts
+++ ts/src/okx.ts
@@ -1304,7 +1304,7 @@ export default class okx extends Exchange {
         //         "msg": ""
         //     }
         //
-        const data = this.safeValue (response, 'data', []);
+        const data = this.safeList (response, 'data', []);
         const result = [];
         for (let i = 0; i < data.length; i++) {
             const account = data[i];
