186ebfdefbafb16fdd027f8b6eb1bdbeb10b75a4
sc0vu <alk03073135@gmail.com>
2024-04-06 15:11:41 +0800

    fix(gemini): replace * when fetch market from web

--- ts/src/gemini.ts
+++ ts/src/gemini.ts
@@ -449,7 +449,8 @@ export default class gemini extends Exchange {
             //         '<td>0.01 USD', // quote currency price increment
             //         '</tr>'
             //     ]
-            const marketId = cells[0].replace ('<td>', '');
+            let marketId = cells[0].replace ('<td>', '');
+            marketId = marketId.replace ('*', '');
             // const base = this.safeCurrencyCode (baseId);
             const minAmountString = cells[1].replace ('<td>', '');
             const minAmountParts = minAmountString.split (' ');
