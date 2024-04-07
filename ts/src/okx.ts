b686507f647055610910b72f7dac3991e31b12c8
rayBastard <roman@president-kids.ru>
2024-04-07 15:09:13 +0300

    Update `fetchTransfer`

--- ts/src/okx.ts
+++ ts/src/okx.ts
@@ -5697,7 +5697,7 @@ export default class okx extends Exchange {
         //         "msg": ""
         //     }
         //
-        const data = this.safeValue (response, 'data', []);
+        const data = this.safeList (response, 'data', []);
         const transfer = this.safeDict (data, 0);
         return this.parseTransfer (transfer);
     }
