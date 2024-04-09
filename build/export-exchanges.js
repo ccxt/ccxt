14538f8ac99854cc4dfd66830fc7107d01fe38fb
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-04-09 15:53:23 +0400

    export ws exchanges

--- build/export-exchanges.js
+++ build/export-exchanges.js
@@ -521,6 +521,7 @@ async function exportEverything () {
     const ids = getIncludedExchangeIds ('./ts/src')
 
     const wsIds = getIncludedExchangeIds ('./ts/src/pro')
+    const wsIdsWithSuffix = wsIds.map (id => id + "Pro");
 
     const errorHierarchy = getErrorHierarchy()
     const flat = flatten (errorHierarchy);
@@ -529,8 +530,7 @@ async function exportEverything () {
 
     const typeExports = ['Market', 'Trade' , 'Fee', 'Ticker', 'OrderBook', 'Order', 'Transaction', 'Tickers', 'Currency', 'Balance', 'DepositAddress', 'WithdrawalResponse', 'DepositAddressResponse', 'OHLCV', 'Balances', 'PartialBalances', 'Dictionary', 'MinMax', 'Position', 'FundingRateHistory', 'Liquidation', 'FundingHistory', 'MarginMode', 'Greeks', 'Leverage', 'Leverages', 'Option', 'OptionChain' ]
     const staticExports = ['version', 'Exchange', 'exchanges', 'pro', 'Precise', 'functions', 'errors'].concat(errorsExports).concat(typeExports)
-
-    const fullExports  = staticExports.concat(ids)
+    const fullExports  = staticExports.concat(ids).concat(wsIdsWithSuffix)
 
     const ccxtFileDir = './ts/ccxt.ts'
     const replacements = [
