1683ec45825a3a3049d3e3252d62e93c7e7d2775
sc0vu <alk03073135@gmail.com>
2024-04-09 15:13:00 +0800

    bingx: update

--- ts/src/bingx.ts
+++ ts/src/bingx.ts
@@ -6,7 +6,7 @@ import { AuthenticationError, PermissionDenied, AccountSuspended, ExchangeError,
 import { Precise } from './base/Precise.js';
 import { sha256 } from './static_dependencies/noble-hashes/sha256.js';
 import { DECIMAL_PLACES } from './base/functions/number.js';
-import type { TransferEntry, Int, OrderSide, OHLCV, FundingRateHistory, Order, OrderType, OrderRequest, Str, Trade, Balances, Transaction, Ticker, OrderBook, Tickers, Market, Strings, Currency, Position, Dict, Leverage, MarginMode, Num, MarginModification, Currencies } from './base/types.js';
+import type { TransferEntry, Int, OrderSide, OHLCV, FundingRateHistory, Order, OrderType, OrderRequest, Str, Trade, Balances, Transaction, Ticker, OrderBook, Tickers, Market, Strings, Currency, Position, Dict, Leverage, MarginMode, Num, MarginModification, Currencies, Bool } from './base/types.js';
 
 //  ---------------------------------------------------------------------------
 
@@ -2755,7 +2755,7 @@ export default class bingx extends Exchange {
         return response;
     }
 
-    async cancelAllOrdersAfter (timeout: number, activated: boolean, params = {}) {
+    async cancelAllOrdersAfter (timeout: number, activated: Bool = undefined, params = {}) {
         /**
          * @method
          * @name bingx#cancelAllOrdersAfter
