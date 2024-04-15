62d6148a18404e8abfd9f2dc4b95d42bde93810c
pcriadoperez <pablo@criadoperez.com>
2024-04-15 12:59:29 +0700

    clean test_close

--- python/ccxt/pro/test/base/test_close.py
+++ python/ccxt/pro/test/base/test_close.py
@@ -18,13 +18,12 @@ async def watch_ticker_loop(exchange):
         raise e
 
 
-async def watch_order_book_for_symbols_loop(exchange):
+async def watch_trades_for_symbols_loop(exchange):
     try:
         while True:
             trades = await exchange.watch_trades_for_symbols(['BTC/USDT', 'ETH/USDT', 'LTC/USDT'])
             print('trades received')
     except Exception as e:
-        print(f"{e}")
         raise e
 
 
@@ -37,29 +36,28 @@ async def test_close():
     exchange = ccxt.pro.binance()
     # exchange.verbose = True
     # --------------------------------------------
-    print('Testing exchange.close(): No future awaiting, should close with no errors')
+    print('---- Testing exchange.close(): No future awaiting, should close with no errors')
     await exchange.watch_ticker('BTC/USD')
     print('ticker received')
     await exchange.close()
     print('PASSED - exchange closed with no errors')
     # --------------------------------------------
-    print('Testing exchange.close(): Open watch multiple, resolve, should close with no errors')
+    print('---- Testing exchange.close(): Open watch multiple, resolve, should close with no errors')
     await exchange.watch_trades_for_symbols(['BTC/USD', 'ETH/USD', 'LTC/USD'])
     print('ticker received')
     await exchange.close()
     print('PASSED - exchange closed with no errors')
     # --------------------------------------------
-    print('Testing exchange.close(): Awaiting future should throw ClosedByUser')
+    print('---- Testing exchange.close(): Awaiting future should throw ClosedByUser')
     try:
         await gather(close_after(exchange, 4), watch_ticker_loop(exchange))
         assert False, "Expected Future rejected with ClosedByUser"
     except ExchangeClosedByUser:
         assert True
     # --------------------------------------------
-        await sleep(4)
-    print('Testing exchange.close(): Call watch_multiple unhandled futures are canceled')
+    print('---- Testing exchange.close(): Call watch_multiple unhandled futures are canceled')
     try:
-        await gather(close_after(exchange, 4), watch_order_book_for_symbols_loop(exchange))
+        await gather(close_after(exchange, 4), watch_trades_for_symbols_loop(exchange))
         assert False, "Expected ExchangeClosedByUser error"
     except ExchangeClosedByUser:
         assert True
