7565b2d5ea4ebf91710da59451d3283706134b30
sc0vu <alk03073135@gmail.com>
2024-04-16 22:17:15 +0800

    okx: update

--- ts/src/okx.ts
+++ ts/src/okx.ts
@@ -3266,20 +3266,19 @@ export default class okx extends Exchange {
         return this.parseOrders (ordersData, market, undefined, undefined, params);
     }
 
-    async cancelAllOrdersAfter (timeout: Int, activated: Bool = undefined, params = {}) {
+    async cancelAllOrdersAfter (timeout: Int, params = {}) {
         /**
          * @method
          * @name okx#cancelAllOrdersAfter
          * @description dead man's switch, cancel all orders after the given timeout
          * @see https://www.okx.com/docs-v5/en/#order-book-trading-trade-post-cancel-all-after
-         * @param {number} countdown time in seconds
-         * @param {boolean} activated countdown
+         * @param {number} countdown time in milliseconds, 0 represents cancel the timer
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object} the api result
          */
         await this.loadMarkets ();
         const request: Dict = {
-            'timeOut': (activated) ? timeout : 0,
+            'timeOut': (timeout > 0) ? (timeout / 1000) : 0,
         };
         const response = await this.privatePostTradeCancelAllAfter (this.extend (request, params));
         //
