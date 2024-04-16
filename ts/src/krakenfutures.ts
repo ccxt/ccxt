ea4b3da917a1fbd4742836fbabb3a4a1a82d61a8
sc0vu <alk03073135@gmail.com>
2024-04-16 22:14:00 +0800

    krakenfutures: update

--- ts/src/krakenfutures.ts
+++ ts/src/krakenfutures.ts
@@ -1273,20 +1273,19 @@ export default class krakenfutures extends Exchange {
         return response;
     }
 
-    async cancelAllOrdersAfter (timeout: Int, activated: Bool = undefined, params = {}) {
+    async cancelAllOrdersAfter (timeout: Int, params = {}) {
         /**
          * @method
          * @name krakenfutures#cancelAllOrdersAfter
          * @description dead man's switch, cancel all orders after the given timeout
          * @see https://docs.futures.kraken.com/#http-api-trading-v3-api-order-management-dead-man-39-s-switch
-         * @param {number} countdown time in seconds
-         * @param {boolean} activated countdown
+         * @param {number} countdown time in miliseconds, 0 represents cancel the timer
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object} the api result
          */
         await this.loadMarkets ();
         const request: Dict = {
-            'timeout': (activated) ? timeout : 0,
+            'timeout': (timeout > 0) ? (timeout / 1000) : 0,
         };
         const response = await this.privatePostCancelallordersafter (this.extend (request, params));
         //
