648236380a155e8a44725a868e8b2295aebc1664
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-04-17 23:28:17 +0400

    some methods

--- cs/tests/BaseTest.Bridge.cs
+++ cs/tests/BaseTest.Bridge.cs
@@ -96,9 +96,12 @@ public partial class BaseTest
     public static bool isGreaterThanOrEqual(object a, object b) => Exchange.isGreaterThanOrEqual(a, b);
     public static bool isLessThanOrEqual(object a, object b) => Exchange.isLessThanOrEqual(a, b);
     public static object postFixIncrement(ref object a) => Exchange.postFixIncrement(ref a);
+    public static object mathMax(object a, object b) => Exchange.mathMax(a, b);
     public static object add(object a, object b) => Exchange.add(a, b);
     public static object multiply(object a, object b) => Exchange.multiply(a, b);
     public static object subtract(object a, object b) => Exchange.subtract(a, b);
+    public static object divide(object a, object b) => Exchange.divide(a, b);
+    public static object parseFloat(object a) => Exchange.parseFloat(a);
     public static string toStringOrNull(object a) => Exchange.toStringOrNull(a);
     public static bool isEqual(object a, object b) => Exchange.isEqual(a, b);
     public static bool isTrue(object a) => Exchange.isTrue(a);
