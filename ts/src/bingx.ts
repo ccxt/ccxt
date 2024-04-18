02eea00c7764a97b996db24aa4a01f02df98fe59
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-04-18 17:08:50 +0400

    statc & minor

--- ts/src/bingx.ts
+++ ts/src/bingx.ts
@@ -396,7 +396,7 @@ export default class bingx extends Exchange {
                     '100414': AccountSuspended, // {"code":100414,"msg":"Code: 100414, Msg: risk control check fail,code(1)","debugMsg":""}
                     '100419': PermissionDenied, // {"code":100419,"msg":"IP does not match IP whitelist","success":false,"timestamp":1705274099347}
                     '100437': BadRequest, // {"code":100437,"msg":"The withdrawal amount is lower than the minimum limit, please re-enter.","timestamp":1689258588845}
-                    '101204': InsufficientFunds, // bingx {"code":101204,"msg":"","data":{}}
+                    '101204': InsufficientFunds, // {"code":101204,"msg":"","data":{}}
                 },
                 'broad': {},
             },
