79c14ae798cdab8f0233099299b53b5bc244695a
Carlos Gonçalves <43336371+carlosmiei@users.noreply.github.com>
2024-04-19 10:17:08 +0100

    feat(hyperliquid): add cancelOrdersForSymbols (#22204)
    * feat(Exchange): add cancelOrdersForSymbols stub
    
    * add c# type
    
    * feat(hyperliquid): add cancelOrdersForSymbols
    
    * fix market id
    
    * static test
    
    * add flag
    
    * adjust return type
    
    * add missing tpye
    
    * add import
    
    * fix market ids

--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -31,7 +31,7 @@ from ccxt.base.decimal_to_precision import decimal_to_precision
 from ccxt.base.decimal_to_precision import DECIMAL_PLACES, TICK_SIZE, NO_PADDING, TRUNCATE, ROUND, ROUND_UP, ROUND_DOWN, SIGNIFICANT_DIGITS
 from ccxt.base.decimal_to_precision import number_to_string
 from ccxt.base.precise import Precise
-from ccxt.base.types import BalanceAccount, Currency, IndexType, OrderSide, OrderType, Trade, OrderRequest, Market, MarketType, Str, Num, Strings
+from ccxt.base.types import BalanceAccount, Currency, IndexType, OrderSide, OrderType, Trade, OrderRequest, Market, MarketType, Str, Num, Strings, CancellationRequest
 
 # -----------------------------------------------------------------------------
 
