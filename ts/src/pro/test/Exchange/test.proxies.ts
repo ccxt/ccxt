6188bf30ce196e10d816bc23802d6900767d3876
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-04-24 17:09:17 +0400

    ws test proxies

--- ts/src/pro/test/Exchange/test.proxies.ts
+++ ts/src/pro/test/Exchange/test.proxies.ts
@@ -4,29 +4,47 @@ import testSharedMethods from '../../../test/Exchange/base/test.sharedMethods.js
 
 async function testWsProxies (exchange: Exchange, skippedProperties: object, language: string) {
     // todo: other proxies, i.e. 'socksWsProxy' etc
+    if (exchange.inArray (language, [ 'js', 'ts' ])) {
+        await exchange.loadHttpProxyAgent ();
+    } else if (exchange.inArray (language, [ 'cs', 'php' ])) {
+        // skip them for now
+        return;
+    }
     await testWsProxy (exchange, skippedProperties);
     // todo: below like in "REST" proxy tests
     // await testProxyForExceptions (exchange, skippedProperties);
 }
 
+const testVpsServerIp = '5.75.153.75';
+let promise = null;
 
 async function testWsProxy (exchange: Exchange, skippedProperties: object) {
     const method = 'wsProxy';
-    const proxyServerIp = '5.75.153.75';
     const [ proxyUrl, httpProxy, httpsProxy, socksProxy ] = testSharedMethods.removeProxyOptions (exchange, skippedProperties);
-    exchange.httpProxy = 'http://' + proxyServerIp + ':8002'; // "httpProxy" or "httpsProxy" (depending on your proxy protocol)
-    exchange.wsProxy = 'http://' + proxyServerIp + ':8002'; // "wsProxy" or "wssProxy" (depending on your proxy protocol)
+    exchange.httpProxy = 'http://' + testVpsServerIp + ':8002'; // "httpProxy" or "httpsProxy" (depending on your proxy protocol)
+    exchange.wsProxy = 'http://' + testVpsServerIp + ':8002'; // "wsProxy" or "wssProxy" (depending on your proxy protocol)
     exchange.handleMessage = ws_helper_callback; // todo for PHP: specifically this custom example does not work in PHP to retrieve the target message, however proxies do work in PHP for websockets independently from this example
-    const demoServerUrl = 'ws://5.75.153.75:9876';
-    const response = await exchange.watch (demoServerUrl, 'test', 'test');
-    console.log ('WS proxy test finished');
-    assert (response === proxyServerIp, exchange.id + ' ' + method + ' test failed. Returned response is ' + response + ' while it should be "' + proxyServerIp + '"');
+    const demoServerUrl = 'ws://' + testVpsServerIp + ':9876';
+    promise = exchange.watch (demoServerUrl, 'test', 'test');
     // reset the instance property
     testSharedMethods.setProxyOptions (exchange, skippedProperties, proxyUrl, httpProxy, httpsProxy, socksProxy);
+    return await promise;
 }
 
 function ws_helper_callback (client, message) {
-    console.log ('WS received:', message);
+    const method = 'wsProxy';
+    // message like: { msg: "hi from test server", your_ip: "::ffff:5.75.153.75" }
+    try {
+        if ('your_ip' in message) {
+            let ipString = message['your_ip'];
+            ipString = ipString.replace ('::ffff:', '');
+            assert (ipString === testVpsServerIp,  method + ' test failed. Returned response is ' + ipString + ' while it should be "' + testVpsServerIp + '"');
+            console.log ('WS proxy test finished');
+        }
+    }
+    catch (e) {
+        console.log (e);
+    }
 }
 
 
