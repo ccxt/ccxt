6e910fe25efe82b8917bbde9b46f7730a20b5495
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-04-24 19:12:28 +0400

    upd

--- ts/src/pro/test/Exchange/test.proxies.ts
+++ ts/src/pro/test/Exchange/test.proxies.ts
@@ -25,27 +25,21 @@ async function testWsProxy (exchange: Exchange, skippedProperties: object) {
     exchange.wsProxy = 'http://' + testVpsServerIp + ':8002'; // "wsProxy" or "wssProxy" (depending on your proxy protocol)
     exchange.handleMessage = ws_helper_callback; // todo for PHP: specifically this custom example does not work in PHP to retrieve the target message, however proxies do work in PHP for websockets independently from this example
     const demoServerUrl = 'ws://' + testVpsServerIp + ':9876';
-    exchange.options['tempWatchPromise'] = exchange.watch (demoServerUrl, 'test', 'test');
+    const promise = exchange.watch (demoServerUrl, 'ws_proxy_test', 'ws_proxy_test');
     // reset the instance property
     testSharedMethods.setProxyOptions (exchange, skippedProperties, proxyUrl, httpProxy, httpsProxy, socksProxy);
-    return await exchange.options['tempWatchPromise'];
+    return await promise;
 }
 
 function ws_helper_callback (client, message) {
     const method = 'wsProxy';
     const testVpsServerIp = '5.75.153.75';
     // message like: { msg: "hi from test server", your_ip: "::ffff:5.75.153.75" }
-    try {
-        if ('your_ip' in message) {
-            let ipString = message['your_ip'];
-            ipString = ipString.replace ('::ffff:', '');
-            assert (ipString === testVpsServerIp,  method + ' test failed. Returned response is ' + ipString + ' while it should be "' + testVpsServerIp + '"');
-            console.log ('WS proxy test finished');
-            client.resolve ();
-        }
-    }
-    catch (e) {
-        console.log (e);
+    if ('your_ip' in message) {
+        let ipString = message['your_ip'];
+        ipString = ipString.replace ('::ffff:', '');
+        assert (ipString === testVpsServerIp,  method + ' test failed. Returned response is ' + ipString + ' while it should be "' + testVpsServerIp + '"');
+        client.resolve (null, 'ws_proxy_test');
     }
 }
 
