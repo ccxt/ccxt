a6ba94e1952f117e69e569762559c09f97fec83a
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-04-26 17:56:32 +0400

    -

--- t1.js
+++ t1.js
@@ -5,13 +5,13 @@ const isPJP = process.env.RUNSTEP === 'PY_JS_PHP';
 async function wait () {
     await new Promise(resolve => setTimeout(resolve, 7 * 1000));
     console.log("read start !"); 
-    const rawData = fs.readFileSync('/tmp/data.json');
+    const rawData = fs.readFileSync('/var/data.json');
     const jsonData = JSON.parse(rawData);
     console.log("read done"); 
 }
 if (isPJP) {
     wait();
 } else {
-    fs.writeFileSync('/tmp/data.json', JSON.stringify({ key1: "value1" }));
+    fs.writeFileSync('/var/data.json', JSON.stringify({ key1: "value1" }));
     console.log("WRITE COMPLETE!")
 }
