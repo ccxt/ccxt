5d653d95a78cb38d68596e427c30eb37a658ca24
Carlos Gonçalves <43336371+carlosmiei@users.noreply.github.com>
2024-04-29 17:37:53 +0100

    fix(Exchange): add fetchConvertTrade/History flags (#22331)

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -553,6 +553,8 @@ export default class Exchange {
                 'fetchClosedOrdersWs': undefined,
                 'fetchConvertCurrencies': undefined,
                 'fetchConvertQuote': undefined,
+                'fetchConvertTrade': undefined,
+                'fetchConvertTradeHistory': undefined,
                 'fetchCrossBorrowRate': undefined,
                 'fetchCrossBorrowRates': undefined,
                 'fetchCurrencies': 'emulated',
