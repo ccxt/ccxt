4b96910ca0fd7791f4c8e97ca678f3f54367a313
Travis CI <travis@travis-ci.org>
2024-05-02 16:28:10 +0000

    feat(binance): fetchIsolatedBorrowRates (#22206)
    * chore: new type IsolatedBorrowRate
    
    * refactor: changed BorrowRate type to CrossBorrowRate
    
    * feat(base/exchange): parseIsolatedBorrowRates, parseIsolatedBorrowRate
    
    * feat: new types - CrossBorrowRate, IsolatedBorrowRate
    
    * fetchCrossBorrowRate(s) return type
    
    * feat(binance): fetchIsolatedBorrowRates
    
    * fetch(Cross|Isolated)BorrowRate(s) return types
    
    * base/Exchange import CrossBorrowRate
    
    * Exchange.ts removed BorrowRate type
    
    * types.py linting
    
    * fix crossborrowRates transpiling
    
    * binance.fetchIsolatedBorrowRate update method signature and return type
    
    * binance.parseIsolatedBorrowRate minor fix
    
    * add static tests
    
    ---------
    
    Co-authored-by: carlosmiei <43336371+carlosmiei@users.noreply.github.com>
    
    [ci skip]

--- js/src/binance.d.ts
+++ js/src/binance.d.ts
@@ -1,5 +1,5 @@
 import Exchange from './abstract/binance.js';
-import type { TransferEntry, Int, OrderSide, Balances, OrderType, Trade, OHLCV, Order, FundingRateHistory, OpenInterest, Liquidation, OrderRequest, Str, Transaction, Ticker, OrderBook, Tickers, Market, Greeks, Strings, Currency, MarketInterface, MarginMode, MarginModes, Leverage, Leverages, Num, Option, MarginModification, TradingFeeInterface, Currencies, TradingFees, Conversion, CrossBorrowRate } from './base/types.js';
+import type { TransferEntry, Int, OrderSide, Balances, OrderType, Trade, OHLCV, Order, FundingRateHistory, OpenInterest, Liquidation, OrderRequest, Str, Transaction, Ticker, OrderBook, Tickers, Market, Greeks, Strings, Currency, MarketInterface, MarginMode, MarginModes, Leverage, Leverages, Num, Option, MarginModification, TradingFeeInterface, Currencies, TradingFees, Conversion, CrossBorrowRate, IsolatedBorrowRates, IsolatedBorrowRate } from './base/types.js';
 /**
  * @class binance
  * @augments Exchange
@@ -304,6 +304,8 @@ export default class binance extends Exchange {
     reduceMargin(symbol: string, amount: number, params?: {}): Promise<MarginModification>;
     addMargin(symbol: string, amount: number, params?: {}): Promise<MarginModification>;
     fetchCrossBorrowRate(code: string, params?: {}): Promise<CrossBorrowRate>;
+    fetchIsolatedBorrowRate(symbol: string, params?: {}): Promise<IsolatedBorrowRate>;
+    fetchIsolatedBorrowRates(params?: {}): Promise<IsolatedBorrowRates>;
     fetchBorrowRateHistory(code: string, since?: Int, limit?: Int, params?: {}): Promise<any>;
     parseBorrowRateHistory(response: any, code: any, since: any, limit: any): any;
     parseBorrowRate(info: any, currency?: Currency): {
@@ -313,6 +315,17 @@ export default class binance extends Exchange {
         timestamp: number;
         datetime: string;
         info: any;
+    };
+    parseIsolatedBorrowRate(info: any, market?: Market): {
+        info: any;
+        symbol: string;
+        base: string;
+        baseRate: number;
+        quote: string;
+        quoteRate: number;
+        period: number;
+        timestamp: any;
+        datetime: any;
     };
     createGiftCode(code: string, amount: any, params?: {}): Promise<{
         info: any;
