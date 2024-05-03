fd42288a692fb22e89d9aa574bd2990bcbf67485
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-05-03 11:59:41 +0100

    revert file

--- ts/src/base/errors.ts
+++ ts/src/base/errors.ts
@@ -1,217 +1,217 @@
 /* eslint-disable max-classes-per-file */
 
 class BaseError extends Error {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'BaseError';
     }
 }
 class ExchangeError extends BaseError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'ExchangeError';
     }
 }
 class AuthenticationError extends ExchangeError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'AuthenticationError';
     }
 }
 class PermissionDenied extends AuthenticationError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'PermissionDenied';
     }
 }
 class AccountNotEnabled extends PermissionDenied {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'AccountNotEnabled';
     }
 }
 class AccountSuspended extends AuthenticationError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'AccountSuspended';
     }
 }
 class ArgumentsRequired extends ExchangeError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'ArgumentsRequired';
     }
 }
 class BadRequest extends ExchangeError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'BadRequest';
     }
 }
 class BadSymbol extends BadRequest {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'BadSymbol';
     }
 }
 class OperationRejected extends ExchangeError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'OperationRejected';
     }
 }
 class NoChange extends OperationRejected {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'NoChange';
     }
 }
 class MarginModeAlreadySet extends NoChange {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'MarginModeAlreadySet';
     }
 }
 class BadResponse extends ExchangeError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'BadResponse';
     }
 }
 class NullResponse extends BadResponse {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'NullResponse';
     }
 }
 class InsufficientFunds extends ExchangeError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'InsufficientFunds';
     }
 }
 class InvalidAddress extends ExchangeError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'InvalidAddress';
     }
 }
 class AddressPending extends InvalidAddress {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'AddressPending';
     }
 }
 class InvalidOrder extends ExchangeError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'InvalidOrder';
     }
 }
 class OrderNotFound extends InvalidOrder {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'OrderNotFound';
     }
 }
 class OrderNotCached extends InvalidOrder {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'OrderNotCached';
     }
 }
 class CancelPending extends InvalidOrder {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'CancelPending';
     }
 }
 class OrderImmediatelyFillable extends InvalidOrder {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'OrderImmediatelyFillable';
     }
 }
 class OrderNotFillable extends InvalidOrder {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'OrderNotFillable';
     }
 }
 class DuplicateOrderId extends InvalidOrder {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'DuplicateOrderId';
     }
 }
 class ContractUnavailable extends InvalidOrder {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'ContractUnavailable';
     }
 }
 class NotSupported extends ExchangeError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'NotSupported';
     }
 }
 class ProxyError extends ExchangeError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'ProxyError';
     }
 }
 class ExchangeClosedByUser extends ExchangeError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'ExchangeClosedByUser';
     }
 }
 class OperationFailed extends BaseError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'OperationFailed';
     }
 }
 class NetworkError extends OperationFailed {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'NetworkError';
     }
 }
 class DDoSProtection extends NetworkError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'DDoSProtection';
     }
 }
 class RateLimitExceeded extends NetworkError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'RateLimitExceeded';
     }
 }
 class ExchangeNotAvailable extends NetworkError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'ExchangeNotAvailable';
     }
 }
 class OnMaintenance extends ExchangeNotAvailable {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'OnMaintenance';
     }
 }
 class InvalidNonce extends NetworkError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'InvalidNonce';
     }
 }
 class RequestTimeout extends NetworkError {
-    constructor (message: string) {
+    constructor (message) {
         super (message);
         this.name = 'RequestTimeout';
     }
