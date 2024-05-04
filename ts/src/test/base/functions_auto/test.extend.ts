8153b3debb58584aa2744c8cd04220eeea19b5cb
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-05-04 18:25:31 +0400

    ext

--- ts/src/test/base/functions_auto/test.extend.ts
+++ ts/src/test/base/functions_auto/test.extend.ts
@@ -27,8 +27,8 @@ function testBaseFunctionsExtend (exchange: any) {
         "d": "not_undefined",
         "e": undefined,
         "sub": {
-            "a": 1,
-            "b": [ 1, 2 ],
+            "a": 2,
+            "b": [ 3, 4 ],
             "c": [ { "test1": 2, "test3": 3 } ],
             "d": "not_undefined",
             "e": undefined,
@@ -39,26 +39,27 @@ function testBaseFunctionsExtend (exchange: any) {
 
     // extend
     const extended = exchange.extend (obj1, obj2);
-    checkExtended (extended);
-    assert ("sub" in extended);
+    checkExtended (extended, true);
     // deepExtend
     const deepExtended = exchange.deepExtend (obj1, obj2);
-    checkExtended (extended);
-    assert ("sub" in extended);
-    checkExtended (deepExtended["sub"]);
+    checkExtended (extended, true);
+    checkExtended (deepExtended["sub"], false);
 }
 
-function checkExtended (extended: any) {
+function checkExtended (extended: any, hasSub: boolean) {
     assert (extended["a"] === 2);
     assert (extended["b"][0] === 3);
     assert (extended["b"][1] === 4);
     assert (extended["c"][0]["test1"] === 2);
-    assert (extended["c"][0]["test2"] === 1);
+    assert (!("test2" in extended["c"][0]));
     assert (extended["c"][0]["test3"] === 3);
     assert (extended["d"] === "not_undefined");
     assert (extended["e"] === undefined);
     assert (extended["other1"] === "x");
     assert (extended["other2"] === "y");
+    if (hasSub) {
+        assert ("sub" in extended);
+    }
 }
 
-export default testBasePartExtend;
+export default testBaseFunctionsExtend;
