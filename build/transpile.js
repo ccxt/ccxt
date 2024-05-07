6b016f64c98961671b078df481f15755b83aabb5
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-05-07 10:29:49 +0100

    snake case method

--- build/transpile.js
+++ build/transpile.js
@@ -351,6 +351,7 @@ class Transpiler {
             [ /\.setTakeProfitAndStopLossParams\s/g, '.set_take_profit_and_stop_loss_params'],
             [ /\.randomBytes\s/g, '.random_bytes'],
             [ /\.createAuthToken\s/g, '.create_auth_token'],
+            [ /\.cancelOrderRequest\s/g, '.cancel_order_request'],
             [ /\ssha(1|256|384|512)([,)])/g, ' \'sha$1\'$2'], // from js imports to this
             [ /\s(md5|secp256k1|ed25519|keccak)([,)])/g, ' \'$1\'$2'], // from js imports to this
 
