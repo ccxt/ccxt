66d7a13a331bffca880521d5cb334bb77f7b2a8f
Sam Germain <sam.germain@usask.ca>
2024-05-10 04:59:31 -0400

    parseConversion types (#22437)

--- ts/src/bitget.ts
+++ ts/src/bitget.ts
@@ -8601,7 +8601,7 @@ export default class bitget extends Exchange {
         return this.parseConversions (dataList, code, 'fromCoin', 'toCoin', since, limit);
     }
 
-    parseConversion (conversion, fromCurrency: Currency = undefined, toCurrency: Currency = undefined): Conversion {
+    parseConversion (conversion: Dict, fromCurrency: Currency = undefined, toCurrency: Currency = undefined): Conversion {
         //
         // fetchConvertQuote
         //
