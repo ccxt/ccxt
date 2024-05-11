149c8fb0fdcb633d433a476d5f5bc7268e45423c
pcriadoperez <pablo@criadoperez.com>
2024-05-11 02:12:22 +0200

    fix(htx): ws authentication error handling

--- ts/src/pro/htx.ts
+++ ts/src/pro/htx.ts
@@ -1899,7 +1899,7 @@ export default class htx extends htxRest {
         //        "data": { "user-id": "35930539" }
         //    }
         //
-        const promise = client.futures['authenticated'];
+        const promise = client.futures['auth'];
         promise.resolve (message);
     }
 
@@ -1928,6 +1928,12 @@ export default class htx extends htxRest {
         //         'err-msg': "Non - single account user is not available, please check through the cross and isolated account asset interface",
         //         "ts": 1698419490189
         //     }
+        //     {
+        //         "action":"req",
+        //         "code":2002,
+        //         "ch":"auth",
+        //         "message":"auth.fail"
+        //     }
         //
         const status = this.safeString (message, 'status');
         if (status === 'error') {
@@ -1938,6 +1944,7 @@ export default class htx extends htxRest {
                 const errorCode = this.safeString (message, 'err-code');
                 try {
                     this.throwExactlyMatchedException (this.exceptions['ws']['exact'], errorCode, this.json (message));
+                    throw new ExchangeError (this.json (message));
                 } catch (e) {
                     const messageHash = this.safeString (subscription, 'messageHash');
                     client.reject (e, messageHash);
@@ -1949,11 +1956,12 @@ export default class htx extends htxRest {
             }
             return false;
         }
-        const code = this.safeInteger2 (message, 'code', 'err-code');
-        if (code !== undefined && ((code !== 200) && (code !== 0))) {
+        const code = this.safeString2 (message, 'code', 'err-code');
+        if (code !== undefined && ((code !== '200') && (code !== '0'))) {
             const feedback = this.id + ' ' + this.json (message);
             try {
                 this.throwExactlyMatchedException (this.exceptions['ws']['exact'], code, feedback);
+                throw new ExchangeError (feedback);
             } catch (e) {
                 if (e instanceof AuthenticationError) {
                     client.reject (e, 'auth');
@@ -2320,7 +2328,7 @@ export default class htx extends htxRest {
             throw new ArgumentsRequired (this.id + ' authenticate requires a url, hostname and type argument');
         }
         this.checkRequiredCredentials ();
-        const messageHash = 'authenticated';
+        const messageHash = 'auth';
         const relativePath = url.replace ('wss://' + hostname, '');
         const client = this.client (url);
         const future = client.future (messageHash);
