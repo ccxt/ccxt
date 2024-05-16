6ddfb2a15c5ea18617738db8788a6bb1d2260c7c
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-05-16 18:55:56 +0400

    ts

--- ts/src/test/tests_init.ts
+++ ts/src/test/tests_init.ts
@@ -1,65 +1,38 @@
-// ----------------------------------------------------------------------------
+
+
+// ########## imports ##########
 
 import assert from 'assert';
 import { Exchange } from '../../ccxt.js';
 import { Str } from '../base/types.js';
+import { DIR_NAME, AuthenticationError, NotSupported, ExchangeError, ProxyError, ExchangeNotAvailable, OperationFailed, OnMaintenance, getCliArgValue, proxyTestFileName, baseMainTestClass, dump, jsonParse, jsonStringify, convertAscii, getTestName, ioFileExists, ioFileRead, ioDirRead, callMethod, callExchangeMethodDynamically, callOverridenMethod, exceptionMessage, exitScript, getExchangeProp, setExchangeProp, initExchange, importTestFile, getTestFiles, setFetchResponse, isNullValue, close, argvExchange, argvSymbol, argvMethod, } from './helpers_for_tests.js';
 
 
-import {
-    DIR_NAME,
-    // errors
-    AuthenticationError,
-    NotSupported,
-    ExchangeError,
-    ProxyError,
-    ExchangeNotAvailable,
-    OperationFailed,
-    OnMaintenance,
-    // shared
-    getCliArgValue,
-    //
-    proxyTestFileName,
-    baseMainTestClass,
-    dump,
-    jsonParse,
-    jsonStringify,
-    convertAscii,
-    getTestName,
-    ioFileExists,
-    ioFileRead,
-    ioDirRead,
-    callMethod,
-    callExchangeMethodDynamically,
-    callOverridenMethod,
-    exceptionMessage,
-    exitScript,
-    getExchangeProp,
-    setExchangeProp,
-    initExchange,
-    importTestFile,
-    getTestFiles,
-    setFetchResponse,
-    isNullValue,
-    close,
-    argvExchange,
-    argvSymbol,
-    argvMethod,
-} from './helpers_for_tests.js';
+import testBaseFunctionsExtend from './base/functions_auto/test.extend.js';
 
-// *********************************
-import BaseFunctionalitiesTestClass from './base_auto.js';
 import testMainClass from './test.js';
-// *********************************
+
+import base from './base/test.base.js';
+
+var _ = base; // eslint-disable-line
+
+
+
+
+// ########### args ###########
 
 const isBaseTests = getCliArgValue ('--baseTests');
 const isExchangeTests = getCliArgValue ('--exchangeTests');
 const reqResTests = getCliArgValue ('--responseTests') || getCliArgValue ('--requestTests');
 const isAllTest = !reqResTests && !isBaseTests && !isExchangeTests; // if neither was chosen
 
-
 // ####### base tests #######
 if (isBaseTests || isAllTest) {
-    (new BaseFunctionalitiesTestClass ()).init ();
+    const exchange = new Exchange ({
+        'id': 'xyzexchange',
+    });
+
+    testBaseFunctionsExtend (exchange);
     console.log ('base tests passed!');
 }
 
