d7979d73cb26b0279ddf5698e8a904251174bb7e
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-05-17 13:48:48 +0400

    tokocrypto

--- ts/src/tokocrypto.ts
+++ ts/src/tokocrypto.ts
@@ -966,7 +966,7 @@ export default class tokocrypto extends Exchange {
         let fee = undefined;
         if ('commission' in trade) {
             fee = {
-                'cost': this.safeString (trade, 'commission'),
+                'cost': this.safeNumber (trade, 'commission'),
                 'currency': this.safeCurrencyCode (this.safeString (trade, 'commissionAsset')),
             };
         }
