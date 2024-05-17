7a238f99eb44692675434f71fdd932fabf3fe561
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-05-17 12:27:42 +0400

    bingx

--- ts/src/bingx.ts
+++ ts/src/bingx.ts
@@ -1079,7 +1079,6 @@ export default class bingx extends Exchange {
             'fee': {
                 'cost': this.parseNumber (Precise.stringAbs (this.safeString2 (trade, 'commission', 'n'))),
                 'currency': currencyCode,
-                'rate': undefined,
             },
         }, market);
     }
