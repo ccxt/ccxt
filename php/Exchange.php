78338faffd5180dfb4ec25ab1bb96b52234689fd
Travis CI <travis@travis-ci.org>
2024-05-20 13:17:14 +0000

    4.3.28
    [ci skip]

--- php/Exchange.php
+++ php/Exchange.php
@@ -39,7 +39,7 @@ use BN\BN;
 use Sop\ASN1\Type\UnspecifiedType;
 use Exception;
 
-$version = '4.3.27';
+$version = '4.3.28';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -58,7 +58,7 @@ const PAD_WITH_ZERO = 6;
 
 class Exchange {
 
-    const VERSION = '4.3.27';
+    const VERSION = '4.3.28';
 
     private static $base58_alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
     private static $base58_encoder = null;
--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -42,11 +42,11 @@ use React\EventLoop\Loop;
 
 use Exception;
 
-$version = '4.3.27';
+$version = '4.3.28';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '4.3.27';
+    const VERSION = '4.3.28';
 
     public $browser;
     public $marketsLoading = null;
