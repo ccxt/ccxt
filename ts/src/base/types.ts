0f1c8678b87cbe12bb8de23ce3f17485ccde11c3
Oleg Taranenko MP <olegtaranenko@gmail.com>
2024-05-27 21:12:17 +0200

    merge regression

--- ts/src/base/types.ts
+++ ts/src/base/types.ts
@@ -290,6 +290,8 @@ export interface FundingRate {
     previousFundingDatetime?: string;
     previousFundingRate?: number;
     previousFundingTimestamp?: number;
+    symbol: string;
+    timestamp?: number;
 }
 
 export interface FundingRates extends Dictionary<FundingRate> {
