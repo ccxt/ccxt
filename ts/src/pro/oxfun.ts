7f6317d1ed18fc1c74b2863b16781a8917cf2cb8
yzh-pelle <81404729+yzh-pelle@users.noreply.github.com>
2024-05-30 02:07:46 +0300

    createOrderWs updated

--- ts/src/pro/oxfun.ts
+++ ts/src/pro/oxfun.ts
@@ -803,14 +803,13 @@ export default class oxfun extends oxfunRest {
          */
         await this.loadMarkets ();
         await this.authenticate ();
-        const market = this.market (symbol);
         const messageHash = this.nonce ().toString ();
         const request: Dict = {
             'op': 'placeorder',
             'tag': messageHash,
         };
         params = this.omit (params, 'tag'); // todo: check
-        const orderRequest: Dict = this.createOrderRequest (market, type, side, amount, price, params);
+        const orderRequest: Dict = this.createOrderRequest (symbol, type, side, amount, price, params);
         const timestamp = this.safeInteger (orderRequest, 'timestamp');
         if (timestamp === undefined) {
             orderRequest['timestamp'] = this.milliseconds ();
