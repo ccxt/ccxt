f15beb2aac27c79a99af5e6f7869ecc7a098bb6a
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-05-31 14:47:24 +0400

    php updates

---
+++ php/test/base/tests_init.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ccxt;
+
+use Exception; // a common import
+
+require_once (__DIR__ . '/test_number.php');
+require_once (__DIR__ . '/test_crypto.php');
+require_once (__DIR__ . '/test_extend.php');
+
+function test_base_rest_all($exchange) {
+    test_number_all();
+    test_crypto_all();
+    test_base_functions_extend($exchange);
+}
--- php/test/tests_init.php
+++ php/test/tests_init.php
@@ -11,6 +11,7 @@ if (is_synchronous) {
 }
 
 
+$isWs = get_cli_arg_value ('--ws');
 $isBaseTests = get_cli_arg_value ('--baseTests');
 $isExchangeTests = get_cli_arg_value ('--exchangeTests');
 $reqResTests = get_cli_arg_value ('--responseTests') || get_cli_arg_value ('--requestTests');
@@ -19,17 +20,18 @@ $isAllTest = !$reqResTests && !$isBaseTests && !$isExchangeTests; // if neither 
 
 // ####### base tests #######
 if ($isBaseTests || $isAllTest) {
-    // test base things
-    require_once (__DIR__ . '/base/test_number.php');
-    require_once (__DIR__ . '/base/test_crypto.php');
-    require_once (__DIR__ . '/base/functions/test_extend.php');
+    if ($isWs) {
+        // no ws tests yet
+    } else {
+        // test base things
+        require_once (__DIR__ . '/base/tests_init.php');
+        $exchange = new Exchange ([
+            'id'=> 'xyzexchange',
+        ]);
+        test_base_rest_all($exchange);
+        print('base tests passed!');
+    }
 
-    $exchange = new Exchange ([
-        'id'=> 'xyzexchange',
-    ]);
-
-    test_base_functions_extend($exchange);
-    print('base tests passed!');
 }
 
 // ####### exchange tests #######
