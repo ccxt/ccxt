ba2496133e1da131974a17217227cfa3647e242e
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-05-31 21:41:16 +0400

    py

--- python/ccxt/test/base/tests_init.py
+++ python/ccxt/test/base/tests_init.py
@@ -4,9 +4,9 @@ import sys
 root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 sys.path.append(root)
 
-from test_extend import test_base_functions_extend  # noqa: F401
-from test_number import test_number_all  # noqa: F401
-from test_crypto import test_crypto_all  # noqa: F401
+from ccxt.test.base.test_extend import test_base_functions_extend  # noqa: F401
+from ccxt.test.base.test_number import test_number_all  # noqa: F401
+from ccxt.test.base.test_crypto import test_crypto_all  # noqa: F401
 
 
 
