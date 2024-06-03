9215b43765053f0c1718732ea6eb379d600b6472
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-06-03 22:34:48 +0400

    -

--- python/ccxt/test/base/tests_init.py
+++ python/ccxt/test/base/tests_init.py
@@ -4,15 +4,15 @@ import sys
 root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 sys.path.append(root)
 
-from ccxt.test.base.test_extend import test_base_functions_extend  # noqa: F401
-from ccxt.test.base.test_number import test_base_number_all  # noqa: F401
-from ccxt.test.base.test_datetime import test_base_datetime_all  # noqa: F401
-from ccxt.test.base.test_crypto import testBaseCryptoAll  # noqa: F401
+from ccxt.test.base.test_extend import test_base_extend  # noqa: F401
+from ccxt.test.base.test_number import test_base_number  # noqa: F401
+from ccxt.test.base.test_datetime import test_base_datetime  # noqa: F401
+from ccxt.test.base.test_crypto import testBaseCryptography as test_base_cryptography  # noqa: F401
 
 
 
 def test_base_init_rest(exchange):
-    test_base_functions_extend(exchange)
-    test_base_datetime_all()
-    test_base_number_all()
-    testBaseCryptoAll()
+    test_base_extend(exchange)
+    test_base_datetime()
+    test_base_number()
+    test_base_cryptography()
