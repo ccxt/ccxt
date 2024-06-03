f1f676781d95545d91d40b4539142e64fbbd0bfc
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-06-03 23:44:58 +0400

    async py

--- python/ccxt/pro/test/base/tests_init.py
+++ python/ccxt/pro/test/base/tests_init.py
@@ -4,6 +4,8 @@ import sys
 root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
 sys.path.append(root)
 
+from asyncio import run, sleep, gather
+
 from ccxt.pro.test.base.test_order_book import test_ws_order_book  # noqa: F401
 from ccxt.pro.test.base.test_cache import test_ws_cache  # noqa: F401
 from ccxt.pro.test.base.test_close import test_ws_close  # noqa: F401
@@ -12,5 +14,5 @@ from ccxt.pro.test.base.test_future import test_ws_future  # noqa: F401
 def test_base_init_ws():
     test_ws_order_book()
     test_ws_cache()
-    test_ws_close()
-    test_ws_future()
+    run(test_ws_close())
+    run(test_ws_future())
