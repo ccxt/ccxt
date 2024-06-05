271288f9a5fde957cd07960a8eddc3f3f1764caf
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-06-05 15:39:09 +0100

    fix(cs): update charset and fix uuid()

--- cs/ccxt/base/Exchange.cs
+++ cs/ccxt/base/Exchange.cs
@@ -232,6 +232,7 @@ public partial class Exchange
 #endif
 
                 var stringContent = body != null ? new StringContent(body, Encoding.UTF8, contentTypeHeader) : null;
+                stringContent.Headers.ContentType.CharSet = "";
                 request.Content = stringContent;
 
                 if (method == "POST")
