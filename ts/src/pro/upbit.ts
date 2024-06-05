ab68b937d2140f62eecc0e7505f349e861c5a0b1
sc0vu <alk03073135@gmail.com>
2024-06-05 11:59:04 +0800

    upbit: update doc

--- ts/src/pro/upbit.ts
+++ ts/src/pro/upbit.ts
@@ -58,6 +58,7 @@ export default class upbit extends upbitRest {
          * @method
          * @name upbit#watchTicker
          * @description watches a price ticker, a statistical calculation with the information calculated over the past 24 hours for a specific market
+         * @see https://global-docs.upbit.com/reference/websocket-ticker
          * @param {string} symbol unified symbol of the market to fetch the ticker for
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object} a [ticker structure]{@link https://docs.ccxt.com/#/?id=ticker-structure}
@@ -70,6 +71,7 @@ export default class upbit extends upbitRest {
          * @method
          * @name upbit#watchTrades
          * @description get the list of most recent trades for a particular symbol
+         * @see https://global-docs.upbit.com/reference/websocket-trade
          * @param {string} symbol unified symbol of the market to fetch trades for
          * @param {int} [since] timestamp in ms of the earliest trade to fetch
          * @param {int} [limit] the maximum amount of trades to fetch
@@ -90,6 +92,7 @@ export default class upbit extends upbitRest {
          * @method
          * @name upbit#watchOrderBook
          * @description watches information on open orders with bid (buy) and ask (sell) prices, volumes and other data
+         * @see https://global-docs.upbit.com/reference/websocket-orderbook
          * @param {string} symbol unified symbol of the market to fetch the order book for
          * @param {int} [limit] the maximum amount of order book entries to return
          * @param {object} [params] extra parameters specific to the exchange API endpoint
