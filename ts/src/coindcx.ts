aa5a53d8a1f51337f3e508732398c7d58f96c235
rayBastard <roman@president-kids.ru>
2024-06-06 21:45:21 +0300

    fetchTrades, fetchOHLCV, fetchOrderBook added

--- ts/src/coindcx.ts
+++ ts/src/coindcx.ts
@@ -3,7 +3,7 @@
 
 import Exchange from './abstract/coindcx.js';
 import { TICK_SIZE } from './base/functions/number.js';
-import type { Dict, Market, Num, Strings, Ticker, Tickers } from './base/types.js';
+import type { Dict, Int, Market, Num, OHLCV, OrderBook, Strings, Ticker, Tickers, Trade } from './base/types.js';
 
 //  ---------------------------------------------------------------------------
 
@@ -80,12 +80,12 @@ export default class coindcx extends Exchange {
                 'fetchMarkets': true,
                 'fetchMarkOHLCV': false,
                 'fetchMyTrades': false,
-                'fetchOHLCV': false,
+                'fetchOHLCV': true,
                 'fetchOpenInterestHistory': false,
                 'fetchOpenOrder': false,
                 'fetchOpenOrders': false,
                 'fetchOrder': false,
-                'fetchOrderBook': false,
+                'fetchOrderBook': true,
                 'fetchOrderBooks': false,
                 'fetchOrders': false,
                 'fetchOrderTrades': false,
@@ -125,7 +125,19 @@ export default class coindcx extends Exchange {
                 'ws': false,
             },
             'timeframes': {
-                // todo: complete list of timeframes
+                '1m': '1m',
+                '5m': '5m',
+                '15m': '15m',
+                '30m': '30m',
+                '1h': '1h',
+                '2h': '2h',
+                '4h': '4h',
+                '6h': '6h',
+                '8h': '8h',
+                '1d': '1d',
+                '3d': '3d',
+                '1w': '1w',
+                '1M': '1M',
             },
             'urls': {
                 'logo': '', // todo: add a logo
@@ -203,7 +215,7 @@ export default class coindcx extends Exchange {
             },
             'exceptions': {
                 'exact': {
-                    // todo: add more error codes
+                    // {"code":400,"message":"Invalid Request.","status":"error"}
                 },
                 'broad': {
                     // todo: add more error codes
@@ -257,7 +269,7 @@ export default class coindcx extends Exchange {
     }
 
     parseMarket (market: Dict): Market {
-        const marketId = this.safeString (market, 'coindcx_name');
+        const marketId = this.safeString (market, 'pair'); // todo how to set encode
         const baseId = this.safeString (market, 'target_currency_short_name');
         const quoteId = this.safeString (market, 'base_currency_short_name');
         const base = this.safeCurrencyCode (baseId);
@@ -394,6 +406,215 @@ export default class coindcx extends Exchange {
             'baseVolume': this.safeString (ticker, 'change_24_hour'),
             'quoteVolume': undefined,
             'info': ticker,
+        }, market);
+    }
+
+    async fetchOHLCV (symbol: string, timeframe = '1m', since: Int = undefined, limit: Int = undefined, params = {}): Promise<OHLCV[]> {
+        /**
+         * @method
+         * @name coindcx#fetchOHLCV
+         * @description fetches historical candlestick data containing the open, high, low, and close price, and the volume of a market
+         * @see https://docs.coindcx.com/?javascript#candles
+         * @param {string} symbol unified symbol of the market to fetch OHLCV data for
+         * @param {string} timeframe the length of time each candle represents
+         * @param {int} [since] timestamp in ms of the earliest candle to fetch (default 24 hours ago)
+         * @param {int} [limit] the maximum amount of candles to fetch (default 500, max 1000)
+         * @param {object} [params] extra parameters specific to the exchange API endpoint
+         * @param {int} [params.until] timestamp in ms of the latest candle to fetch (default now)
+         * @returns {int[][]} A list of candles ordered as timestamp, open, high, low, close, volume
+         */
+        await this.loadMarkets ();
+        const market = this.market (symbol);
+        timeframe = this.safeString (this.timeframes, timeframe, timeframe);
+        const request: Dict = {
+            'pair': market['id'],
+            'interval': timeframe,
+        };
+        if (since !== undefined) {
+            request['startTime'] = since; // startTime and endTime must be within 7 days of each other
+        }
+        if (limit !== undefined) {
+            request['limit'] = limit;
+        }
+        const until = this.safeInteger (params, 'until');
+        if (until !== undefined) {
+            request['endTime'] = until;
+            params = this.omit (params, 'until');
+        }
+        const response = await this.public2GetMarketDataCandles (this.extend (request, params));
+        //
+        //     [
+        //         {
+        //             "open":70938.55,
+        //             "high":70938.55,
+        //             "low":70926.27,
+        //             "volume":7.60989,
+        //             "close":70931.98,
+        //             "time":1717692120000
+        //         }
+        //     ]
+        //
+        return this.parseOHLCVs (response, market, timeframe, since, limit);
+    }
+
+    parseOHLCV (ohlcv, market: Market = undefined): OHLCV {
+        return [
+            this.safeInteger (ohlcv, 'time'),
+            this.safeNumber (ohlcv, 'open'),
+            this.safeNumber (ohlcv, 'high'),
+            this.safeNumber (ohlcv, 'low'),
+            this.safeNumber (ohlcv, 'close'),
+            this.safeNumber (ohlcv, 'volume'),
+        ];
+    }
+
+    async fetchOrderBook (symbol: string, limit: Int = undefined, params = {}): Promise<OrderBook> {
+        /**
+         * @method
+         * @name coindcx#fetchOrderBook
+         * @description fetches information on open orders with bid (buy) and ask (sell) prices, volumes and other data
+         * @see https://docs.coindcx.com/?javascript#order-book
+         * @param {string} symbol unified symbol of the market to fetch the order book for
+         * @param {int} [limit] the maximum amount of order book entries to return
+         * @param {object} [params] extra parameters specific to the exchange API endpoint
+         * @returns {object} A dictionary of [order book structures]{@link https://docs.ccxt.com/#/?id=order-book-structure} indexed by market symbols
+         */
+        await this.loadMarkets ();
+        const market = this.market (symbol);
+        const request: Dict = {
+            'pair': market['id'],
+        };
+        const response = await this.public2GetMarketDataOrderbook (this.extend (request, params));
+        //
+        //     {
+        //         "timestamp": 1717694482206,
+        //         "asks":
+        //         {
+        //             "71198.00": "7.630",
+        //             "71198.10": "0.116",
+        //             "71198.40": "0.069",
+        //             "71198.50": "0.056",
+        //             "71198.70": "0.046",
+        //             "71198.80": "0.258",
+        //             "71198.90": "0.095",
+        //             "71199.00": "0.324",
+        //             ...
+        //         },
+        //         "bids":
+        //         {
+        //             "71197.90": "8.563",
+        //             "71197.80": "0.011",
+        //             "71196.80": "0.561",
+        //             "71196.70": "0.892",
+        //             "71196.60": "0.019",
+        //             "71196.30": "0.100",
+        //             "71196.10": "0.007",
+        //             "71196.00": "0.002",
+        //             ...
+        //         }
+        //     }
+        //
+        const timestamp = this.safeInteger (response, 'timestamp');
+        return this.parseOrderBook (response, market['symbol']);
+    }
+
+    parseBidsAsks (bidsAsks) {
+        const bidsAsksKeys = Object.keys (bidsAsks);
+        const result = [];
+        for (let i = 0; i < bidsAsksKeys.length; i++) {
+            const price = bidsAsksKeys[i];
+            const amount = bidsAsks[price];
+            result.push ([ this.parseNumber (price), this.parseNumber (amount) ]);
+        }
+        return result;
+    }
+
+    parseOrderBook (orderbook: object, symbol: string, limit: Int = undefined): OrderBook {
+        const responseBids = this.safeDict (orderbook, 'bids');
+        const responseAsks = this.safeDict (orderbook, 'asks');
+        const bids = this.parseBidsAsks (responseBids);
+        const asks = this.parseBidsAsks (responseAsks);
+        const timestamp = this.safeString (orderbook, 'timestamp');
+        return {
+            'symbol': symbol,
+            'bids': this.filterByLimit (this.sortBy (bids, 0, true), limit),
+            'asks': this.filterByLimit (this.sortBy (asks, 0), limit),
+            'timestamp': timestamp,
+            'datetime': this.iso8601 (timestamp),
+            'nonce': undefined,
+        } as any;
+    }
+
+    async fetchTrades (symbol: string, since: Int = undefined, limit: Int = undefined, params = {}): Promise<Trade[]> {
+        /**
+         * @method
+         * @name coindcx#fetchTrades
+         * @description get the list of most recent trades for a particular symbol
+         * @see https://docs.coindcx.com/#trades
+         * @param {string} symbol unified symbol of the market to fetch trades for
+         * @param {int} [since] timestamp in ms of the earliest trade to fetch (default 24 hours ago)
+         * @param {int} [limit] the maximum amount of trades to fetch (default 30, max 500)
+         * @param {object} [params] extra parameters specific to the exchange API endpoint
+         * @returns {Trade[]} a list of [trade structures]{@link https://docs.ccxt.com/#/?id=public-trades}
+         */
+        await this.loadMarkets ();
+        const market = this.market (symbol);
+        const request: Dict = {
+            'pair': market['id'],
+        };
+        if (limit !== undefined) {
+            request['limit'] = limit;
+        }
+        const response = await this.public2GetMarketDataTradeHistory (this.extend (request, params));
+        //
+        //     [
+        //         {
+        //             "p": 0.00000153,
+        //             "q": 10971,
+        //             "s": "SNTBTC",
+        //             "T": 1663742387385,
+        //             "m": true
+        //         }
+        //     ]
+        //
+        return this.parseTrades (response, market, since, limit);
+    }
+
+    parseTrade (trade, market: Market = undefined): Trade {
+        //
+        // public fetchTrades
+        //
+        //     {
+        //         "p": 0.00000153,
+        //         "q": 10971,
+        //         "s": "SNTBTC",
+        //         "T": 1663742387385,
+        //         "m": true
+        //     }
+        //
+        const marketId = this.safeString (trade, 's');
+        market = this.safeMarket (marketId, market);
+        const symbol = market['symbol'];
+        const timestamp = this.safeInteger (trade, 'T');
+        const maker = this.safeBool (trade, 'm');
+        let isMaker = 'maker';
+        if (!maker) {
+            isMaker = 'taker';
+        }
+        return this.safeTrade ({
+            'id': undefined,
+            'timestamp': timestamp,
+            'datetime': this.iso8601 (timestamp),
+            'symbol': symbol,
+            'type': undefined,
+            'order': undefined,
+            'side': undefined,
+            'takerOrMaker': isMaker,
+            'price': this.safeString (trade, 'p'),
+            'amount': this.safeString (trade, 'q'),
+            'cost': undefined,
+            'fee': undefined,
+            'info': trade,
         }, market);
     }
 
