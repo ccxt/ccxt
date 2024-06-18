65a99d4d3b8fba21e78546af6133b83387855d7a
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-06-18 14:17:05 +0400

    transp

--- build/transpile.js
+++ build/transpile.js
@@ -1068,7 +1068,7 @@ class Transpiler {
         const errorImports = []
 
         for (let error in errors) {
-            const regex = new RegExp ("[^\\'\"]" + error + "[^\\'\"]")
+            const regex = new RegExp ("[^\\w\\'\"]" + error + "[^\\w\\'\"]")
             if (bodyAsString.match (regex)) {
                 errorImports.push ('from ccxt.base.errors import ' + error)
             }
