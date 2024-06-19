c090bb12ae8f1b6a87d85587dec7c6dff4ba5009
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-06-20 00:42:26 +0400

    c# parts

--- build/csharpTranspiler.ts
+++ build/csharpTranspiler.ts
@@ -1081,8 +1081,8 @@ class NewTranspiler {
 
     transpileCryptoTestsToCSharp (outDir: string) {
 
-        const jsFile = './ts/src/test/base/test.crypto.ts';
-        const csharpFile = `${outDir}/Crypto.cs`;
+        const jsFile = './ts/src/test/base/test.cryptography.ts';
+        const csharpFile = `${outDir}/Cryptography.cs`;
 
         log.magenta ('[csharp] Transpiling from', (jsFile as any).yellow)
 
