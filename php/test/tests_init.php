530e6b060c4f53d39bbdb43cb86d8e33f32c7e75
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-06-20 13:11:51 +0100

    call base_tests_init_Ws

--- php/test/tests_init.php
+++ php/test/tests_init.php
@@ -22,7 +22,7 @@ $isAllTest = get_cli_arg_value ('--all'); // if neither was chosen
 if ($isBaseTests) {
     if ($isWs) {
         require_once (__DIR__ . '/../pro/test/base/tests_init.php');
-        // base_tests_init_ws();
+        \ccxt\pro\base_tests_init_ws();
     } else {
         // test base things
         require_once (__DIR__ . '/base/tests_init.php');
