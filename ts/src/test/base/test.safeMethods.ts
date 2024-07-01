f89c16e79eea9189b9f8a8067e8b2133ad538eb6
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-07-01 11:48:10 +0100

    complete test

--- ts/src/test/base/test.safeMethods.ts
+++ ts/src/test/base/test.safeMethods.ts
@@ -31,12 +31,27 @@ function testSafeMethods () {
 
     const inputList = [ "hi", 2 ];
 
+    // safeValue
     assert (exchange.safeValue (inputDict, 'a') === 1);
     assert (exchange.safeValue (inputDict, 'bool') === true);
     assert (equals (exchange.safeValue (inputDict, 'list'), [ 1, 2, 3 ]));
     const dictObject = exchange.safeValue (inputDict, 'dict');
     assert (equals (dictObject, { 'a': 1 }));
     assert (exchange.safeValue (inputList, 0) === 'hi');
+
+    // safeValue2
+    assert (exchange.safeValue2 (inputDict, 'b', 'a') === 1);
+    assert (exchange.safeValue2 (inputDict, 's', 'bool') === true);
+
+    // safeString
+    assert (exchange.safeString (inputDict, 'a') === '1');
+    // assert (exchange.safeString (inputDict, 'bool') === 'true'); returns True in python and 'true' in js
+    assert (exchange.safeString (inputList, 0) === 'hi');
+    assert (exchange.safeString (inputDict, 'str') === 'hello');
+
+    // safeInteger
+    assert (exchange.safeInteger (inputDict, 'a') === 1);
+    assert (exchange.safeInteger2 (inputDict, 'b', 'a') === 1);
 }
 
 
