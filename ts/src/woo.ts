f241c1999f96bfab751fcc876e28121d3c3a7f8d
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-07-01 15:04:22 +0100

    fix(woo): spot market parsing

--- ts/src/woo.ts
+++ ts/src/woo.ts
@@ -1614,7 +1614,7 @@ export default class woo extends Exchange {
         const side = this.safeStringLower (order, 'side');
         const filled = this.omitZero (this.safeValue2 (order, 'executed', 'totalExecutedQuantity'));
         const average = this.omitZero (this.safeString2 (order, 'average_executed_price', 'averageExecutedPrice'));
-        const remaining = Precise.stringSub (cost, filled);
+        // const remaining = Precise.stringSub (cost, filled);
         const fee = this.safeValue2 (order, 'total_fee', 'totalFee');
         const feeCurrency = this.safeString2 (order, 'fee_asset', 'feeAsset');
         const transactions = this.safeValue (order, 'Transactions');
@@ -1656,7 +1656,7 @@ export default class woo extends Exchange {
             'average': average,
             'amount': amount,
             'filled': filled,
-            'remaining': remaining, // TO_DO
+            'remaining': undefined, // TO_DO
             'cost': cost,
             'trades': transactions,
             'fee': {
