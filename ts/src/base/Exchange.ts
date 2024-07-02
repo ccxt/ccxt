7bca49f3881e70a1d77ad5d843339a7381450c04
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-07-02 14:20:56 +0100

    defaults

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -4217,10 +4217,10 @@ export default class Exchange {
         this.last_request_body = request['body'];
         this.last_request_url = request['url'];
         let retries = undefined;
-        [ retries, params ] = this.handleOptionAndParams (params, path, 'maxRetriesOnFailure', 1);
+        [ retries, params ] = this.handleOptionAndParams (params, path, 'maxRetriesOnFailure', 0);
         let retryDelay = undefined;
         [ retryDelay, params ] = this.handleOptionAndParams (params, path, 'maxRetriesOnFailureDelay', 0);
-        for (let i = 0; i < retries; i++) {
+        for (let i = 0; i < retries + 1; i++) {
             try {
                 return await this.fetch (request['url'], request['method'], request['headers'], request['body']);
             } catch (e) {
