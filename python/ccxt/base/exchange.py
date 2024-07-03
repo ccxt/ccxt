f3ba5f8568def68bdec7dff74e7a26908b02cef2
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-07-03 17:33:25 +0100

    fix(eddsa): missing reassignment

--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -1449,7 +1449,7 @@ class Exchange(object):
     @staticmethod
     def eddsa(request, secret, curve='ed25519'):
         if isinstance(secret, str):
-            Exchange.encode(secret)
+            secret = Exchange.encode(secret)
         private_key = ed25519.Ed25519PrivateKey.from_private_bytes(secret) if len(secret) == 32 else load_pem_private_key(secret, None)
         return Exchange.binary_to_base64(private_key.sign(request))
 
