4c13528bef01599deb36c72f6c6f3eb7821ec979
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-07-06 20:14:20 +0400

    ren

--- ts/src/pro/binance.ts
+++ ts/src/pro/binance.ts
@@ -981,7 +981,7 @@ export default class binance extends binanceRest {
         for (let i = 0; i < symbols.length; i++) {
             const symbol = symbols[i];
             const market = this.market (symbol);
-            messageHashes.push (this.messageHash ('trades', symbol));
+            messageHashes.push (this.messageHash ('trade', symbol) + '@' + name);
             const rawHash = market['lowercaseId'] + '@' + name;
             subParams.push (rawHash);
         }
@@ -1191,7 +1191,8 @@ export default class binance extends binanceRest {
         const marketId = this.safeString (message, 's');
         const market = this.safeMarket (marketId, undefined, undefined, marketType);
         const symbol = market['symbol'];
-        const messageHash = this.messageHash ('trades', symbol);
+        const event = this.safeString (message, 'e');
+        const messageHash = this.messageHash ('trade', symbol) + '@' + event;
         const trade = this.parseWsTrade (message, market);
         let tradesArray = this.safeValue (this.trades, symbol);
         if (tradesArray === undefined) {
