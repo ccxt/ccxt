70d23d58844c7fc6a372a810b4fe982ef6f39763
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-07-12 10:30:51 +0100

    add to the docs

--- ts/src/pro/kucoin.ts
+++ ts/src/pro/kucoin.ts
@@ -1012,6 +1012,7 @@ export default class kucoin extends kucoinRest {
          * @param {int} [since] the earliest time in ms to fetch trades for
          * @param {int} [limit] the maximum number of trade structures to retrieve
          * @param {object} [params] extra parameters specific to the exchange API endpoint
+         * @param {string} [params.method] '/spotMarket/tradeOrders' or '/spot/tradeFills' default is '/spotMarket/tradeOrders'
          * @returns {object[]} a list of [trade structures]{@link https://docs.ccxt.com/#/?id=trade-structure
          */
         await this.loadMarkets ();
