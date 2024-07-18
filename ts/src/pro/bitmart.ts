fff829fa854bed131fc7d1ed18674a1e0fd817a2
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-07-18 22:22:50 +0400

    bitmart

--- ts/src/pro/bitmart.ts
+++ ts/src/pro/bitmart.ts
@@ -770,7 +770,7 @@ export default class bitmart extends bitmartRest {
         const symbol = market['symbol'];
         const openTimestamp = this.safeInteger (position, 'create_time');
         const timestamp = this.safeInteger (position, 'update_time');
-        const side = this.safeNumber (position, 'position_type');
+        const side = this.safeInteger (position, 'position_type');
         const marginModeId = this.safeNumber (position, 'open_type');
         return this.safePosition ({
             'info': position,
