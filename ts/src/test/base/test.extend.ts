6597725a435a9ae4159da68b5258d1a3b1a4d07c
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-07-23 22:41:41 +0400

    extend

--- ts/src/test/base/test.extend.ts
+++ ts/src/test/base/test.extend.ts
@@ -1,13 +1,14 @@
-// @ts-nocheck
+
 // AUTO_TRANSPILE_ENABLED
 
 import assert from 'assert';
 import ccxt from '../../../ccxt.js';
+import testSharedMethods from '../Exchange/base/test.sharedMethods.js';
 
 function testExtend () {
 
     const exchange = new ccxt.Exchange ({
-        'id': 'regirock',
+        'id': 'sampleexchange',
     });
 
     const obj1 = {
@@ -46,28 +47,49 @@ function testExtend () {
 
     // extend
     const extended = exchange.extend (obj1, obj2);
-    tbfeCheckExtended (extended, true);
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testExtend', extended, {
+        "a": 2,
+        "b": [ 3, 4 ],
+        "c": [ {
+            "test1": 2,
+            "test3": 3,
+        } ],
+        "d": "not_undefined",
+        "sub": {
+            "a": 2,
+            "b": [ 3, 4 ],
+            "c": [ {
+                "test1": 2,
+                "test3": 3,
+            } ],
+            "d": "not_undefined",
+            "other2": "y",
+        },
+        "other1": "x",
+        "other2": "y",
+    });
     // deepExtend
     const deepExtended = exchange.deepExtend (obj1, obj2);
-    tbfeCheckExtended (extended, true);
-    // todo !
-    // tbfeCheckExtended (deepExtended["sub"], false);
-}
-
-function tbfeCheckExtended (extended: any, hasSub: boolean) {
-    assert (extended["a"] === 2);
-    assert (extended["b"][0] === 3);
-    assert (extended["b"][1] === 4);
-    assert (extended["c"][0]["test1"] === 2);
-    assert (!("test2" in extended["c"][0]));
-    assert (extended["c"][0]["test3"] === 3);
-    assert (extended["d"] === "not_undefined");
-    assert (extended["e"] === undefined);
-    assert (extended["other1"] === "x");
-    assert (extended["other2"] === "y");
-    if (hasSub) {
-        assert ("sub" in extended);
-    }
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testExtend', deepExtended, {
+        "a": 2,
+        "b": [ 3, 4 ],
+        "c": [ {
+            "test1": 2,
+            "test3": 3,
+        } ],
+        "d": "not_undefined",
+        "sub": {
+            "a": 2,
+            "b": [ 3, 4 ],
+            "c": [ { "test1": 2, "test3": 3 } ],
+            "d": "not_undefined",
+            "e": "not_undefined",
+            "other1": "x",
+            "other2": "y",
+        },
+        "other1": "x",
+        "other2": "y",
+    });
 }
 
 export default testExtend;
