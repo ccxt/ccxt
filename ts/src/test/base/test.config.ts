5330a6a24cc890e914415e70bc39aba13d96505d
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-07-23 16:40:40 +0400

    config

--- ts/src/test/base/test.config.ts
+++ ts/src/test/base/test.config.ts
@@ -1,25 +1,30 @@
-// @ts-nocheck
 
-import assert, { strictEqual, deepEqual } from 'assert';
-import ccxt, { Exchange, functions } from '../../../ccxt.js';
+// AUTO_TRANSPILE_ENABLED
 
-const { index, aggregate, unCamelCase } = functions;
-
-const equal = strictEqual;
+import ccxt from '../../../ccxt.js';
+import testSharedMethods from '../Exchange/base/test.sharedMethods.js';
 
 function testExchangeConfigExtension () {
-    const cost = { 'min': 0.001, 'max': 1000 }
-    const precision = { 'amount': 3 }
-    const exchange = new Exchange ({
-        'id': 'mock',
-        'markets': {
-            'ETH/BTC': { 'limits': { cost }, precision },
-        },
+    const cost = { 'min': 0.001, 'max': 1000 };
+    const precision = { 'amount': 3 };
+    const markets = {
+        'ETH/BTC': { 'limits': { cost }, precision },
+    };
+    const exchange = new ccxt.Exchange ({
+        'id': 'sampleexchange',
+        'markets': markets,
     });
 
-    deepEqual (exchange.markets['ETH/BTC'].limits.cost, cost);
-    deepEqual (exchange.markets['ETH/BTC'].precision, { 'price': 0.000001, 'amount': 0.001 });
-    deepEqual (exchange.markets['ETH/BTC'].symbol, 'ETH/BTC');
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testExchangeConfigExtension', exchange.markets['ETH/BTC']['limits']['cost'], cost);
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testExchangeConfigExtension', exchange.markets['ETH/BTC']['precision'], { 'price': 0.000001, 'amount': 0.001 });
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testExchangeConfigExtension', exchange.markets['ETH/BTC']['symbol'], 'ETH/BTC');
+
+    // check if constructor correctly sets
+    const exchange2 = new ccxt.Exchange ({
+        'id': 'sampleexchange2',
+    });
+    exchange2.setMarkets (markets);
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testExchangeConfigExtension', exchange.markets['ETH/BTC'], exchange2.markets['ETH/BTC']);
 }
 
 export default testExchangeConfigExtension;
