a7c531e86cde50262fe5899f607779ad0cf77bda
amirhosseinfaghan <www.amir9203joyandeh@gmail.com>
2024-07-28 11:39:05 +0330

    [fix]: fix nobitex FetchTickers bug

--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -42,11 +42,11 @@ use React\EventLoop\Loop;
 
 use Exception;
 
-$version = '4.3.47';
+$version = '4.3.46-0.1';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '4.3.47';
+    const VERSION = '4.3.46-0.1';
 
     public $browser;
     public $marketsLoading = null;
