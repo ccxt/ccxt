436f57b069354bc379c75752c7190d09e0b34034
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-07-30 09:51:02 +0400

    config

--- ts/src/test/base/test.config.ts
+++ ts/src/test/base/test.config.ts
@@ -3,10 +3,21 @@ import ccxt from '../../../ccxt.js';
 import testSharedMethods from '../Exchange/base/test.sharedMethods.js';
 
 function testExchangeConfigExtension () {
-    const cost = { 'min': 0.001, 'max': 1000 };
-    const precision = { 'amount': 3 };
+    const cost = {
+        'min': 0.001,
+        'max': 1000,
+    };
+    const precision = {
+        'price': 0.000001,
+        'amount': 0.001,
+    };
     const markets = {
-        'ETH/BTC': { 'limits': { cost }, precision },
+        'ETH/BTC': {
+            'limits': {
+                'cost': cost,
+            },
+            'precision': precision,
+        },
     };
     const exchange = new ccxt.Exchange ({
         'id': 'sampleexchange',
@@ -14,7 +25,7 @@ function testExchangeConfigExtension () {
     });
 
     testSharedMethods.assertDeepEqual (exchange, undefined, 'testExchangeConfigExtension', exchange.markets['ETH/BTC']['limits']['cost'], cost);
-    testSharedMethods.assertDeepEqual (exchange, undefined, 'testExchangeConfigExtension', exchange.markets['ETH/BTC']['precision'], { 'price': 0.000001, 'amount': 0.001 });
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testExchangeConfigExtension', exchange.markets['ETH/BTC']['precision'], precision);
     testSharedMethods.assertDeepEqual (exchange, undefined, 'testExchangeConfigExtension', exchange.markets['ETH/BTC']['symbol'], 'ETH/BTC');
 
     // check if constructor correctly sets
