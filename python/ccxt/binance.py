61343ab53fb5565c56f57dbdea40bb5d8d4ad544
Travis CI <travis@travis-ci.org>
2024-08-08 13:59:00 +0000

    feat(binance): fix fetchCurrencies return params (#23365)
    * feat(binance): add deposit min param inside fetchCurrencies return
    
    * feat(binance): fix fetchCurrencies networks fee param
    
    [ci skip]

--- python/ccxt/binance.py
+++ python/ccxt/binance.py
@@ -2784,7 +2784,7 @@ class binance(Exchange, ImplicitAPI):
                     'active': depositEnable and withdrawEnable,
                     'deposit': depositEnable,
                     'withdraw': withdrawEnable,
-                    'fee': self.parse_number(fee),
+                    'fee': withdrawFee,
                     'precision': self.parse_number(precisionTick),
                     'limits': {
                         'withdraw': {
@@ -2792,7 +2792,7 @@ class binance(Exchange, ImplicitAPI):
                             'max': self.safe_number(networkItem, 'withdrawMax'),
                         },
                         'deposit': {
-                            'min': None,
+                            'min': self.safe_number(networkItem, 'depositDust'),
                             'max': None,
                         },
                     },
