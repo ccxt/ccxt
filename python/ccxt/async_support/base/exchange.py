82daf91d3b3366385ab119f8472a78051cc002bd
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-08-09 19:42:28 +0400

    py async

--- python/ccxt/async_support/base/exchange.py
+++ python/ccxt/async_support/base/exchange.py
@@ -74,12 +74,10 @@ class Exchange(BaseExchange):
         self.own_session = 'session' not in config
         self.cafile = config.get('cafile', certifi.where())
         super(Exchange, self).__init__(config)
-        self.throttle = None
-        self.init_rest_rate_limiter()
         self.markets_loading = None
         self.reloading_markets = False
 
-    def init_rest_rate_limiter(self):
+    def init_throttler(self):
         self.throttle = Throttler(self.tokenBucket, self.asyncio_loop)
 
     def get_event_loop(self):
