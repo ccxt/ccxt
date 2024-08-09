76238755c5bdbc4aa538d976d1f9d255a6e6f66b
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-08-09 23:44:43 +0400

    toString

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -2147,7 +2147,7 @@ export default class Exchange {
         const uniqChars = (this.unique (this.stringToCharsArray (address)));
         const length = uniqChars.length; // py transpiler trick
         if (length === 1 || address.length < this.minFundingAddressLength || (address.toString ().indexOf (' ') > -1)) {
-            throw new InvalidAddress (this.id + ' address is invalid or has less than ' + this.minFundingAddressLength.toString () + ' characters: "' + this.json (address) + '"');
+            throw new InvalidAddress (this.id + ' address is invalid or has less than ' + this.minFundingAddressLength.toString () + ' characters: "' + address.toString () + '"');
         }
         return address;
     }
