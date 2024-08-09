5d202b87f3a92415669856ec5337a603d31278b2
T.Todua <7117978+ttodua@users.noreply.github.com>
2024-08-09 19:46:17 +0400

    cs

--- cs/ccxt/base/Exchange.cs
+++ cs/ccxt/base/Exchange.cs
@@ -22,21 +22,14 @@ public partial class Exchange
         var empty = new List<string>();
         transformApiNew(this.api);
 
-        this.initRestRateLimiter();
         this.initHttpClient();
 
-        if (this.markets != null)
-        {
-            this.setMarkets(this.markets);
-        }
         this.afterConstruct();
+    }
 
-        var isSandbox2 = this.safeBool2(this.options, "sandbox", "testnet", false);
-        var isSandbox = (isSandbox2 != null) ? (bool)isSandbox2 : false;
-        if (isSandbox)
-        {
-            this.setSandboxMode(isSandbox);
-        }
+    private void initThrottler()
+    {
+        this.throttler = new Throttler(this.tokenBucket);
     }
 
     private void initHttpClient()
@@ -560,22 +553,6 @@ public partial class Exchange
     public async Task throttle(object cost)
     {
         await (await this.throttler.throttle(cost));
-    }
-
-    public void initRestLimiter()
-    {
-        if (this.id != null && this.rateLimit == -1)
-        {
-            throw new Exception(this.id + ".rateLimit property is not configured'");
-        }
-        this.tokenBucket = (dict)this.extend(new dict() {
-            {"delay" , 0.001},
-            {"capacity" , 1},
-            {"cost" , 1},
-            {"maxCapacity", 1000},
-            {"refillRate", (this.rateLimit > 0) ? 1 / this.rateLimit : float.MaxValue},
-        }, this.tokenBucket);
-        this.throttler = new Throttler(this.tokenBucket);
     }
 
     public object clone(object o)
