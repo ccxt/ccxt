4fe82feac3b8b5c54b1d34c1d782f6ed42c22d4d
Igor Kroitor <igorkroitor@Igors-MacBook-Pro.local>
2024-08-14 01:40:59 +0300

    bybit withdraw handleOptionAndParams default accountType SPOT

--- ts/src/bybit.ts
+++ ts/src/bybit.ts
@@ -6186,6 +6186,9 @@ export default class bybit extends Exchange {
          * @returns {object} a [transaction structure]{@link https://docs.ccxt.com/#/?id=transaction-structure}
          */
         [ tag, params ] = this.handleWithdrawTagAndParams (tag, params);
+        // support withdrawals from SPOT or FUND account
+        let accountType = undefined;
+        [ accountType, params ] = this.handleOptionAndParams (params, 'withdraw', 'accountType', 'SPOT');
         await this.loadMarkets ();
         this.checkAddress (address);
         const currency = this.currency (code);
@@ -6194,6 +6197,7 @@ export default class bybit extends Exchange {
             'amount': this.numberToString (amount),
             'address': address,
             'timestamp': this.milliseconds (),
+            'accountType': accountType,
         };
         if (tag !== undefined) {
             request['tag'] = tag;
