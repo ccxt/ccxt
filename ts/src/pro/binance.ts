63ce07cf5ed941441b9f83092da818d6279e4c38
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-08-16 10:34:25 +0100

    fix(binance): watchOHLCV - exchange-specific id

--- ts/src/pro/binance.ts
+++ ts/src/pro/binance.ts
@@ -1234,6 +1234,9 @@ export default class binance extends binanceRest {
          * @param {object} [params.timezone] if provided, kline intervals are interpreted in that timezone instead of UTC, example '+08:00'
          * @returns {int[][]} A list of candles ordered as timestamp, open, high, low, close, volume
          */
+        await this.loadMarkets ();
+        const market = this.market (symbol);
+        symbol = market['symbol'];
         params['callerMethodName'] = 'watchOHLCV';
         const result = await this.watchOHLCVForSymbols ([ [ symbol, timeframe ] ], since, limit, params);
         return result[symbol][timeframe];
@@ -1285,7 +1288,7 @@ export default class binance extends binanceRest {
             const suffix = '@+08:00';
             const utcSuffix = shouldUseUTC8 ? suffix : '';
             rawHashes.push (marketId + '@' + klineType + '_' + interval + utcSuffix);
-            messageHashes.push ('ohlcv::' + symbolString + '::' + timeframeString);
+            messageHashes.push ('ohlcv::' + market['symbol'] + '::' + timeframeString);
         }
         const url = this.urls['api']['ws'][type] + '/' + this.stream (type, 'multipleOHLCV');
         const requestId = this.requestId (url);
