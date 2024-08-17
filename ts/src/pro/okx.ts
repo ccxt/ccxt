5a0b55f6855ef94afe08e826ce1e06a43b9ad94a
Carlos G <43336371+carlosmiei@users.noreply.github.com>
2024-08-17 15:34:31 +0100

    fix(okx): watchLiquidationsForSymbols subscription (#23451)
    * fix(okx): watchLiquidationsForSymbols subscription
    
    * fix request

--- ts/src/pro/okx.ts
+++ ts/src/pro/okx.ts
@@ -454,9 +454,15 @@ export default class okx extends okxRest {
          */
         await this.loadMarkets ();
         symbols = this.marketSymbols (symbols, undefined, true, true);
-        let messageHash = 'liquidations';
+        const messageHash = 'liquidations';
+        const messageHashes = [];
         if (symbols !== undefined) {
-            messageHash += '::' + symbols.join (',');
+            for (let i = 0; i < symbols.length; i++) {
+                const symbol = symbols[i];
+                messageHashes.push (messageHash + '::' + symbol);
+            }
+        } else {
+            messageHashes.push (messageHash);
         }
         const market = this.getMarketFromSymbols (symbols);
         let type = undefined;
@@ -469,9 +475,16 @@ export default class okx extends okxRest {
         }
         const uppercaseType = type.toUpperCase ();
         const request = {
-            'instType': uppercaseType,
+            'op': 'subscribe',
+            'args': [
+                {
+                    'channel': channel,
+                    'instType': uppercaseType,
+                },
+            ],
         };
-        const newLiquidations = await this.subscribe ('public', messageHash, channel, undefined, this.extend (request, params));
+        const url = this.getUrl (channel, 'public');
+        const newLiquidations = await this.watchMultiple (url, messageHashes, request, messageHashes);
         if (this.newUpdates) {
             return newLiquidations;
         }
