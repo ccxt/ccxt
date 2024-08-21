85dd3b0a8dcf9b92e1324f7e57500cdacfabe215
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-08-21 14:44:04 +0100

    fix(errors): UnsubscribeError

--- ts/src/base/errorHierarchy.ts
+++ ts/src/base/errorHierarchy.ts
@@ -51,8 +51,8 @@ const errorHierarchy = {
             },
             'CancelPending': {},
         },
+        'UnsubscribeError': {},
     },
-    'UnsubscribeError': {},
 };
 
 export default errorHierarchy;
