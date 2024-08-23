02e2ac655833c9701cab385397bcfbc191a4c7bf
Peter Lai <alk03073135@gmail.com>
2024-08-23 18:10:38 +0800

    fix(base): paginationTimestamp shoud add one for forward (#23519)

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -6608,7 +6608,7 @@ export default class Exchange {
                     errors = 0;
                     result = this.arrayConcat (result, response);
                     const last = this.safeValue (response, responseLength - 1);
-                    paginationTimestamp = this.safeInteger (last, 'timestamp') - 1;
+                    paginationTimestamp = this.safeInteger (last, 'timestamp') + 1;
                     if ((until !== undefined) && (paginationTimestamp >= until)) {
                         break;
                     }
