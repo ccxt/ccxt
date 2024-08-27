69d3cc146625e3d23fda6f77330eeb86731d8061
yzh-pelle <81404729+yzh-pelle@users.noreply.github.com>
2024-08-27 14:18:19 +0300

    `extend`, `deepExtend` and `clone` added

--- ts/src/test/base/test.genericMethods.ts
+++ ts/src/test/base/test.genericMethods.ts
@@ -3,6 +3,7 @@
 
 import assert from 'assert';
 import ccxt from '../../../ccxt.js';
+import { isObject } from '../../base/functions.js';
 
 function deepEquals (a, b) {
     const aKeys = Object.keys (a);
@@ -14,8 +15,8 @@ function deepEquals (a, b) {
     }
     for (let i = 0; i < arrayALength; i++) {
         const key = aKeys[i];
-        if (ccxt.isObject (a[key])) {
-            if (!ccxt.isObject (b[key])) {
+        if (isObject (a[key])) {
+            if (isObject (b[key])) {
                 return false;
             } else if (!deepEquals (a[key], b[key])) {
                 return false;
@@ -48,6 +49,40 @@ function testGenericMethods () {
     const inputList = [ 'Hi', 2 ];
     const inputListKeys = [ '0', '1' ]; // todo check if it is good
     const inputListValues = [ 'Hi', 2 ];
+
+    const extendingDict = {
+        'a': 1,
+        'i': 100,
+        'f': 100.0,
+        'bool': false,
+        'list': [ 1, 2, 3, 4 ],
+        'dict': { 'a': 'a', 'c': 3 },
+        'str': 'hi',
+        'strNumber': '100',
+    };
+
+    const extendedDict = {
+        'a': 1,
+        'i': 1,
+        'f': 0.123,
+        'bool': true,
+        'list': [ 1, 2, 3 ],
+        'dict': { 'a': 1, 'b': '2' },
+        'str': 'heLlo',
+        'strNumber': '3',
+    };
+
+    const deepExtendedDict = {
+        'a': 1,
+        'i': 1,
+        'f': 0.123,
+        'bool': true,
+        'list': [ 1, 2, 3 ],
+        'dict': { 'a': 1, 'b': '2', 'c': 3 },
+        'str': 'heLlo',
+        'strNumber': '3',
+    };
+
     // keys
     assert (deepEquals (exchange.keys (inputDict), inputDictKeys));
     assert (deepEquals (exchange.keys (inputList), inputListKeys));
@@ -55,6 +90,16 @@ function testGenericMethods () {
     // values
     assert (deepEquals (exchange.values (inputDict), inputDictValues));
     assert (deepEquals (exchange.values (inputList), inputListValues));
+
+    // extend
+    assert (deepEquals (exchange.extend (extendingDict, inputDict), extendedDict));
+
+    // deepExtend
+    assert (deepEquals (exchange.deepExtend (extendingDict, inputDict), deepExtendedDict));
+
+    // clone
+    assert (deepEquals (exchange.clone (inputDict), inputDict));
+    assert (deepEquals (exchange.clone (inputList), inputList));
 }
 
 export default testGenericMethods;
