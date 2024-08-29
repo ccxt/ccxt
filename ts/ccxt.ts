d387bf653ede4c2418a1a6d72da41d11776ce50b
Travis CI <travis@travis-ci.org>
2024-08-29 08:08:31 +0000

    4.3.90
    [ci skip]

--- ts/ccxt.ts
+++ ts/ccxt.ts
@@ -40,7 +40,7 @@ import {BaseError, ExchangeError, AuthenticationError, PermissionDenied, Account
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '4.3.89';
+const version = '4.3.90';
 
 (Exchange as any).ccxtVersion = version
 
