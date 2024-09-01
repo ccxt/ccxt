4c36de4e3515d05d357661989f92c0f8254de747
t.t <7117978+ttodua@users.noreply.github.com>
2024-09-01 19:11:26 +0400

    multi symbol

--- utils/update-static-json.ts
+++ utils/update-static-json.ts
@@ -1,9 +1,7 @@
 //
 // Usage:
 //
-//     tsx ./utils/update-static-json.ts binance BTC/USDT
-// or
-//     npm run static-data-updater binance BTC/USDT
+//   tsx ./utils/update-static-json.ts binance BTC/USDT ETH/USDT
 //
 import fs from 'fs';
 import { platform } from 'process'
@@ -20,58 +18,72 @@ const rootDir = __dirname + '/../';
 function write (filename, data) {
     return fs.writeFileSync(filename, JSON.stringify(data, null, 2));
 }
+function die (errorMessage = undefined, code = 1) {
+    console.log (errorMessage || 'Please specify correct format, e.g. `node ./utils/update-static-json.js binance BTC/USDT ETH/USDT` (you can also pass a CurrencyCode instead of a symbol)');
+    process.exit(code);
+}
 
 const exchangeId = args[0];
-const symbolOrCurrency = args[1];
-const errorMessage = 'Please specify correct format, e.g. `node ./utils/update-static-json.js binance BTC/USDT` (you can also pass a single CurrencyCode instead of a symbol)';
-
-if (!exchangeId || !symbolOrCurrency) {
-    console.log(errorMessage);
-    process.exit(1);
-}
-if (!ccxt.exchanges.includes(exchangeId)) {
-    console.log('Exchange id ' + exchangeId + ' not found in exchanges.json');
-    process.exit(1);
-}
-
-const isMarketOrCurrency = symbolOrCurrency.includes('/');
 const filePathForMarkets = rootDir + `/ts/src/test/static/markets/${exchangeId}.json`;
 const marketsJson = JSON.parse (fs.readFileSync(filePathForMarkets, "utf8"));
 const filePathForCurrencies = rootDir + `/ts/src/test/static/currencies/${exchangeId}.json`;
 const currenciesJson = JSON.parse (fs.readFileSync(filePathForCurrencies, "utf8"));
 
-async function main() {
+if (!exchangeId) {
+    die ();
+}
+
+// remove first item
+args.shift ();
+const symbolsOrCurrencies = args;
+
+async function main () {
     try {
         const exchange = new ccxt[exchangeId]();
         await exchange.loadMarkets();
-        // check whether it's market or currency
-        const targetObject = isMarketOrCurrency ? exchange.markets[symbolOrCurrency] : exchange.currencies[symbolOrCurrency];
-        if (!targetObject) {
-            console.log('Symbol or Currency not found in ' + exchangeId);
-            process.exit(1);
+
+        for (const symbolOrCurrency of symbolsOrCurrencies) {
+
+            if (!symbolOrCurrency) {
+                die ();
+            }
+            if (!ccxt.exchanges.includes(exchangeId)) {
+                console.log('Exchange id ' + exchangeId + ' not found in exchanges.json');
+                process.exit(1);
+            }
+            // check whether it's market or currency
+            const isMarketOrCurrency = symbolOrCurrency.includes('/');
+
+            const targetObject = isMarketOrCurrency ? exchange.markets[symbolOrCurrency] : exchange.currencies[symbolOrCurrency];
+            if (!targetObject) {
+                // @ts-expect-error
+                die ('Symbol or Currency not found in ' + exchangeId, 1);
+            }
+            // write to file
+            if (isMarketOrCurrency) {
+                // if it's market, then write market object and currencies too
+                // market object
+                marketsJson[symbolOrCurrency] = targetObject;
+                write (filePathForMarkets, marketsJson);
+                // base & quote currency objects
+                const base = targetObject.base;
+                const quote = targetObject.quote;
+                const baseCurrency = exchange.currencies[base];
+                const quoteCurrency = exchange.currencies[quote];
+                currenciesJson[base] = baseCurrency;
+                currenciesJson[quote] = quoteCurrency;
+                write (filePathForCurrencies, currenciesJson);
+            } else {
+                // if currency, then only write currency object
+                currenciesJson[symbolOrCurrency] = targetObject;
+                write(filePathForCurrencies, currenciesJson);
+            }
         }
-        // replace existing data
-        if (isMarketOrCurrency) {
-            // if it's market, then write market object and currencies too
-            marketsJson[symbolOrCurrency] = targetObject;
-            write(filePathForMarkets, marketsJson);
-            const base = targetObject.base;
-            const quote = targetObject.quote;
-            const baseCurrency = exchange.currencies[base];
-            const quoteCurrency = exchange.currencies[quote];
-            currenciesJson[base] = baseCurrency;
-            currenciesJson[quote] = quoteCurrency;
-            write(filePathForCurrencies, currenciesJson);
-        } else {
-            // if currency, then only write currency object
-            currenciesJson[symbolOrCurrency] = targetObject;
-            write(filePathForCurrencies, currenciesJson);
-        }
-        console.log('Finished! ', exchangeId,' > ', (isMarketOrCurrency ? 'markets': 'currencies'), ' > ',  symbolOrCurrency);
-        process.exit(0);
+        // @ts-expect-error
+        die ('Finished! ' + exchangeId + ' > ' + JSON.stringify (symbolsOrCurrencies), 0);
     } catch (e) {
-        console.log('Static data write error: ', e);
-        process.exit (1);
+        // @ts-expect-error
+        die ('Static data write error: ' + e.stack.toString (), 1);
     }
 }
 
