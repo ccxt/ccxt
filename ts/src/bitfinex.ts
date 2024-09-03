7781a9a1adb7a3875b6bbb612c9d0d3f80e092a8
T. Todua <7117978+ttodua@users.noreply.github.com>
2024-09-03 14:56:38 +0400

    exchanges - promise.all (#23600)
    * fix(ascendex) - promise all markets
    
    * fix(ascendex) - currencies promise.all
    
    * fix(ascendex) - promise.all
    
    * fix(coinbase) - promise.all
    
    * fix(gate) - promise.all
    
    * fix(gemini) - promise.all
    
    * fix(bitfinex2) - promise.all
    
    * fix(kraken) - promise.all
    
    * fix(phemex) - promise.all
    
    * fix(coinbaseexchange) - promise.all
    
    * fix(htx) - promise.all
    
    * fix(huobijp) - promise.all
    
    * fix(kucoin) - promise.all
    
    * fix(luno) - promise.all
    
    * fix(ndax) - promise.all
    
    * Discard changes to ts/src/pro/kraken.ts
    
    * disabled description support
    
    * Discard changes to ts/src/coinbase.ts
    
    * Discard changes to ts/src/coinbaseexchange.ts
    
    * Discard changes to ts/src/htx.ts
    
    * Discard changes to ts/src/huobijp.ts
    
    * Discard changes to ts/src/kucoin.ts
    
    * Discard changes to ts/src/luno.ts
    
    * Discard changes to ts/src/ndax.ts
    
    * Discard changes to ts/src/pro/bitfinex2.ts
    
    * Discard changes to ts/src/pro/phemex.ts
    
    * promise all
    
    * bitfinex2
    
    * bitfinex
    
    * independentreserve

--- ts/src/bitfinex.ts
+++ ts/src/bitfinex.ts
@@ -568,11 +568,11 @@ export default class bitfinex extends Exchange {
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object[]} an array of objects representing market data
          */
-        const ids = await this.publicGetSymbols ();
+        const idsPromise = this.publicGetSymbols ();
         //
         //     [ "btcusd", "ltcusd", "ltcbtc" ]
         //
-        const details = await this.publicGetSymbolsDetails ();
+        const detailsPromise = this.publicGetSymbolsDetails ();
         //
         //     [
         //         {
@@ -587,6 +587,7 @@ export default class bitfinex extends Exchange {
         //         },
         //     ]
         //
+        const [ ids, details ] = await Promise.all ([ idsPromise, detailsPromise ]);
         const result = [];
         for (let i = 0; i < details.length; i++) {
             const market = details[i];
