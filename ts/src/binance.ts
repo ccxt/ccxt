cb6a98fdee79b17317cf5d7c4a21b15d50e2905d
t.t <7117978+ttodua@users.noreply.github.com>
2024-09-04 00:21:11 +0400

    binance rls

--- ts/src/binance.ts
+++ ts/src/binance.ts
@@ -23,7 +23,7 @@ export default class binance extends Exchange {
             'id': 'binance',
             'name': 'Binance',
             'countries': [ 'JP', 'MT' ], // Japan, Malta
-            'rateLimit': 50,
+            'rateLimit': 10,
             'certified': true,
             'pro': true,
             // new metainfo2 interface
@@ -242,10 +242,12 @@ export default class binance extends Exchange {
                     // UID (sapi) request rate limit of 180 000 per minute
                     // 1 UID (sapi) => cost = 0.006667 => (1000 / (50 * 0.006667)) * 60 = 180000
                     'get': {
-                        // copy trading
-                        'copyTrading/futures/userStatus': 2,
-                        'copyTrading/futures/leadSymbol': 2,
-                        'system/status': 0.1,
+                        // copy trading (https://developers.binance.com/docs/copy_trading/)
+                        'copyTrading/futures/userStatus': 20,
+                        'copyTrading/futures/leadSymbol': 20,
+                        // wallet (https://developers.binance.com/docs/wallet/others/)
+                        'system/status': 1,
+                        'spot/delist-schedule': 100,
                         // these endpoints require this.apiKey
                         'accountSnapshot': 240, // Weight(IP): 2400 => cost = 0.1 * 2400 = 240
                         'account/info': 0.1,
