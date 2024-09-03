97cdb7c2373d741cfc1c2ef8572d79ac30d84e66
Travis CI <travis@travis-ci.org>
2024-09-03 14:41:39 +0000

    4.3.95
    [ci skip]

--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -43,11 +43,11 @@ use React\EventLoop\Loop;
 
 use Exception;
 
-$version = '4.3.94';
+$version = '4.3.95';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '4.3.94';
+    const VERSION = '4.3.95';
 
     public $browser;
     public $marketsLoading = null;
