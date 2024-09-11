696073d646142e0f195f180708924fcbed9d5d43
Travis CI <travis@travis-ci.org>
2024-09-11 01:06:06 +0000

    cryptocom referral discount
    [ci skip]

--- python/ccxt/cryptocom.py
+++ python/ccxt/cryptocom.py
@@ -156,7 +156,7 @@ class cryptocom(Exchange, ImplicitAPI):
                 'www': 'https://crypto.com/',
                 'referral': {
                     'url': 'https://crypto.com/exch/kdacthrnxt',
-                    'discount': 0.15,
+                    'discount': 0.75,
                 },
                 'doc': [
                     'https://exchange-docs.crypto.com/exchange/v1/rest-ws/index.html',
