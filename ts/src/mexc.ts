1386a17b64bc2d552fab69a3001ef1baf4139083
T. Todua <7117978+ttodua@users.noreply.github.com>
2024-09-14 18:21:13 +0400

    fix(mexc) - loading rl (#23698)
    * fix(mexc) - loading rl
    
    * comment
    
    * cs
    
    * adjust
    
    * fix prop

--- ts/src/mexc.ts
+++ ts/src/mexc.ts
@@ -1018,8 +1018,9 @@ export default class mexc extends Exchange {
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object[]} an array of objects representing market data
          */
-        const spotMarket = await this.fetchSpotMarkets (params);
-        const swapMarket = await this.fetchSwapMarkets (params);
+        const spotMarketPromise = this.fetchSpotMarkets (params);
+        const swapMarketPromise = this.fetchSwapMarkets (params);
+        const [ spotMarket, swapMarket ] = await Promise.all ([ spotMarketPromise, swapMarketPromise ]);
         return this.arrayConcat (spotMarket, swapMarket);
     }
 
@@ -1160,7 +1161,10 @@ export default class mexc extends Exchange {
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object[]} an array of objects representing market data
          */
+        const currentRl: number = this.rateLimit;
+        this.setProperty (this, 'rateLimit', 10); // see comment: https://github.com/ccxt/ccxt/pull/23698
         const response = await this.contractPublicGetDetail (params);
+        this.setProperty (this, 'rateLimit', currentRl);
         //
         //     {
         //         "success":true,
