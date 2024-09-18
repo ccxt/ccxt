2ee3485acc538c359aec1c533bedc3e032a9a9c9
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-09-18 10:25:59 +0100

    helpers fix

--- go/ccxt/exchange_helpers.go
+++ go/ccxt/exchange_helpers.go
@@ -348,6 +348,9 @@ func ToFloat64(v interface{}) float64 {
 	case reflect.Float64:
 		result = val.Float()
 	case reflect.String:
+		if result, err := strconv.ParseFloat(val.String(), 64); err != nil {
+			return result
+		}
 		result = 0 // Convert string to float64, example implementation
 	}
 	return result
