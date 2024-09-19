6da47361ee61f94d69b5d942e4f85ada68cbb2d5
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-09-19 14:15:12 +0100

    fix build

--- ts/src/pro/binance.ts
+++ ts/src/pro/binance.ts
@@ -15,7 +15,6 @@ import Client from '../base/ws/Client.js';
 // -----------------------------------------------------------------------------
 
 export default class binance extends binanceRest {
-
     describe () {
         return this.deepExtend (super.describe (), this.describeData ());
     }
