de183bab3d4622bae6a6f1c97d92aaceeb8cd9d3
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-09-26 09:24:21 +0200

    add mapping

--- ts/src/gate.ts
+++ ts/src/gate.ts
@@ -593,6 +593,8 @@ export default class gate extends Exchange {
             },
             // copied from gatev2
             'commonCurrencies': {
+                'ORT': 'XREATORS',
+                'ASS': 'ASSF',
                 '88MPH': 'MPH',
                 'AXIS': 'AXISDEFI',
                 'BIFI': 'BITCOINFILE',
