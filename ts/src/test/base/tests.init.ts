7b0bd3cac08bb051e3791bba4404f500c9c9b62e
t.t <7117978+ttodua@users.noreply.github.com>
2024-10-03 10:22:03 +0400

    next bunch of renames

--- ts/src/test/base/tests.init.ts
+++ ts/src/test/base/tests.init.ts
@@ -1,21 +1,36 @@
+
 // AUTO_TRANSPILE_ENABLED
 
 import testNumber from './test.number.js';
 import testDatetime from './test.datetime.js';
 import testCryptography from './test.cryptography.js';
 import testExtend from './test.extend.js';
-import testLanguageSpecific from './test.languageSpecific.js';
+import testDeepExtend from './test.deepExtend.js';
+import testLanguageSpecific from './language_specific/test.languageSpecific.js';
 import testSafeMethods from './test.safeMethods.js';
-// import testJson from './test.json.js';
+import testSortBy from './test.sortBy.js';
+import testSum from './test.sum.js';
+import testOmit from './test.omit.js';
+import testJson from './test.json.js';
+import testGroupBy from './test.groupBy.js';
+import testFilterBy from './test.filterBy.js';
+import testConfig from './test.config.js';
 
 function baseTestsInit () {
     testLanguageSpecific ();
     testExtend ();
+    testDeepExtend ();
     testCryptography ();
     testDatetime ();
     testNumber ();
     testSafeMethods ();
-    // testJson (); // remove temporarily
+    testSortBy ();
+    testSum ();
+    testOmit ();
+    testJson ();
+    testGroupBy ();
+    testFilterBy ();
+    testConfig ();
 }
 
 export default baseTestsInit;
