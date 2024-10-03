c40949474a0bf8017d2a594dc095a110fb652bc8
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-10-03 11:32:00 +0100

    small fix

--- ts/src/pro/binance.ts
+++ ts/src/pro/binance.ts
@@ -2077,14 +2077,14 @@ export default class binance extends binanceRest {
     parseWsTicker (message, marketType) {
         // markPrice
         //   {
-        //       "e": "markPriceUpdate",  	// Event type
-        //       "E": 1562305380000,      	// Event time
-        //       "s": "BTCUSDT",          	// Symbol
-        //       "p": "11794.15000000",   	// Mark price
-        //       "i": "11784.62659091",		// Index price
-        //       "P": "11784.25641265",		// Estimated Settle Price, only useful in the last hour before the settlement starts
-        //       "r": "0.00038167",       	// Funding rate
-        //       "T": 1562306400000       	// Next funding time
+        //       "e": "markPriceUpdate",   // Event type
+        //       "E": 1562305380000,       // Event time
+        //       "s": "BTCUSDT",           // Symbol
+        //       "p": "11794.15000000",    // Mark price
+        //       "i": "11784.62659091",    // Index price
+        //       "P": "11784.25641265",    // Estimated Settle Price, only useful in the last hour before the settlement starts
+        //       "r": "0.00038167",        // Funding rate
+        //       "T": 1562306400000        // Next funding time
         //   }
         //
         // ticker
