ce8ccff52e1c9301da40a64c58ab97d6e55747ce
todocambia <notmeee00@gmail.com>
2024-10-04 12:15:05 -0400

    Fixed sign function

--- ts/src/cube.ts
+++ ts/src/cube.ts
@@ -765,7 +765,7 @@ export default class cube extends Exchange {
         return this.safeBalance (result);
     }
 
-    async sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
+    sign (path, api = 'public', method = 'GET', params = {}, headers = undefined, body = undefined) {
         // Construct the base URL
         let url = this.urls['api'][api] + '/' + this.implodeParams (path, params);
         const query = this.omit (params, this.extractParams (path));
