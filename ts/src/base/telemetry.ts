1a39cd408662910716bc5e5c1a7cf99eb41c8de1
pcriadoperez <pablo@criadoperez.com>
2024-10-06 13:24:49 +0300

    working commit

--- ts/src/base/telemetry.ts
+++ ts/src/base/telemetry.ts
@@ -1,61 +1,93 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { Resource } from '@opentelemetry/resources';
-import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-import { trace, context, Span } from '@opentelemetry/api';
+import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { trace, SpanStatusCode, Tracer } from '@opentelemetry/api';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-node';
+import ccxt from '../../ccxt';
 
-const sdk = new NodeSDK({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: 'ccxt-library',
-  }),
-  traceExporter: new OTLPTraceExporter({
-    url: 'http://signoz:4318/v1/traces', // Adjust this URL to match your SigNoz setup
-  }),
-});
+let defaultTracer: Tracer | undefined;
+let defaultExporter: OTLPTraceExporter | undefined;
+let sdk: NodeSDK | undefined;
 
-sdk.start();
+const isTelemetryEnabled = process.env.CCXT_TELEMETRY_ENABLED !== 'false';
+const SERVICE_NAME = 'ccxt';
 
-export const tracer = trace.getTracer('ccxt-tracer');
+function initializeTelemetry() {
+  if (sdk || !isTelemetryEnabled) {
+    return; // SDK already initialized or telemetry is disabled
+  }
 
-export function traceMethod<T>(
+  defaultExporter = new OTLPTraceExporter({
+    url: 'https://ingest.eu.signoz.cloud:443/v1/traces',
+    headers: {
+      "signoz-access-token": "52913854-ebf0-4b8f-96bc-6ed6d029b698",
+    },
+  });
+
+  const batchProcessor = new BatchSpanProcessor(defaultExporter);
+
+  sdk = new NodeSDK({
+    resource: new Resource({
+      [ATTR_SERVICE_NAME]: SERVICE_NAME,
+    }),
+    instrumentations: [getNodeAutoInstrumentations()],
+    spanProcessor: batchProcessor,
+  });
+
+  sdk.start();
+  defaultTracer = trace.getTracer('ccxt');
+}
+
+export function getTracer(): Tracer | undefined {
+  if (!isTelemetryEnabled) {
+    return undefined;
+  }
+  if (!sdk) {
+    initializeTelemetry();
+  }
+  return defaultTracer;
+}
+
+export function traceMethod<T extends (...args: any[]) => any>(
   target: any,
   propertyKey: string,
   descriptor: TypedPropertyDescriptor<T>
 ): TypedPropertyDescriptor<T> {
-  const originalMethod = descriptor.value as Function;
+  const originalMethod = descriptor.value;
 
-  descriptor.value = function (...args: any[]) {
-    const span = tracer.startSpan(propertyKey);
-    const filteredArgs = args.map(arg => filterSensitiveData(arg));
+    descriptor.value = async function (this: any, ...args: any[]) {
+      const tracer = getTracer();
+      if (!tracer) {
+        return originalMethod.apply(this, args);
+      }
 
-    context.with(trace.setSpan(context.active(), span), () => {
+      console.log('traceMethod', propertyKey, args);
+      const filteredArgs = args.map(arg => filterSensitiveData(arg));
+
+    return tracer.startActiveSpan(propertyKey, async (span) => {
+      console.log('starting span')
       span.setAttributes({
+        'ccxt.version': ccxt.version,
+        'method.exchange': this.id,
         'method.name': propertyKey,
         'method.args': JSON.stringify(filteredArgs),
       });
 
       try {
-        const result = originalMethod.apply(this, args);
-        if (result instanceof Promise) {
-          return result.then(
-            (value) => {
-              span.end();
-              return value;
-            },
-            (error) => {
-              span.recordException(error);
-              span.end();
-              throw error;
-            }
-          );
-        } else {
-          span.end();
-          return result;
-        }
-      } catch (error) {
+        const result = await originalMethod.call(this, ...args);
+        span.setStatus({ code: SpanStatusCode.OK });
+        return result;
+      } catch (error: any) {
+        span.setStatus({
+          code: SpanStatusCode.ERROR,
+          message: error.message,
+        });
         span.recordException(error);
-        span.end();
         throw error;
+      } finally {
+        span.end();
       }
     });
   } as any;
@@ -64,16 +96,132 @@ export function traceMethod<T>(
 }
 
 function filterSensitiveData(data: any): any {
-  if (typeof data === 'object' && data !== null) {
-    const filtered = Array.isArray(data) ? [] : {};
-    for (const key in data) {
-      if (key === 'apiKey' || key === 'secret' || key === 'password') {
-        filtered[key] = '***FILTERED***';
-      } else {
-        filtered[key] = filterSensitiveData(data[key]);
+  const stack = [{ obj: data, filtered: undefined, key: undefined }];
+  const sensitiveKeys = ['apiKey', 'secret', 'password', 'passphrase', 'key'];
+
+  while (stack.length > 0) {
+    const { obj, filtered, key } = stack.pop()!;
+
+    if (typeof obj === 'object' && obj !== null) {
+      const isArray = Array.isArray(obj);
+      const newFiltered = isArray ? [] : {};
+
+      if (filtered !== undefined && key !== undefined) {
+        filtered[key] = newFiltered;
       }
+
+      for (const k in obj) {
+        if (sensitiveKeys.includes(k)) {
+          newFiltered[k] = '***FILTERED***';
+        } else if (typeof obj[k] === 'object' && obj[k] !== null) {
+          stack.push({ obj: obj[k], filtered: newFiltered, key: k });
+        } else {
+          newFiltered[k] = obj[k];
+        }
+      }
+
+      if (filtered === undefined) {
+        return newFiltered;
+      }
+    } else if (filtered !== undefined && key !== undefined) {
+      filtered[key] = obj;
     }
-    return filtered;
   }
+
   return data;
+}
+
+export function wrapExchange() {
+  return function <T extends { new(...args: any[]): {} }>(constructor: T) {
+    return class extends constructor {
+      constructor(...args: any[]) {
+        super(...args);
+        if (isTelemetryEnabled) {
+          wrapExchangeMethods(this);
+        }
+      }
+    };
+  };
+}
+
+export function wrapExchangeMethods(exchangeInstance: any) {
+  function wrapMethod(obj: any, propertyName: string) {
+    const camelCaseName = propertyName.replace(/_([a-z])/g, (g) => g[1].toUpperCase());
+    const descriptor = Object.getOwnPropertyDescriptor(obj, propertyName) || 
+                       Object.getOwnPropertyDescriptor(obj, camelCaseName);
+
+    if (!descriptor || typeof descriptor.value !== 'function' || propertyName === 'constructor') {
+      return;
+    }
+    if (descriptor.value.constructor.name !== 'AsyncFunction') return;
+
+    const originalMethod = descriptor.value;
+    console.log(`Wrapping method: ${propertyName}`);
+
+    const wrappedMethod = function (this: any, ...args: any[]) {
+      const tracer = getTracer();
+      if (!tracer) {
+        return originalMethod.call(this, ...args);
+      }
+
+      const filteredArgs = args.map(arg => filterSensitiveData(arg));
+      return tracer.startActiveSpan(propertyName, async (span) => {
+        console.log(`Starting span for ${propertyName}`);
+        span.setAttributes({
+          'ccxt.version': ccxt.version,
+          'method.exchange': this.id,
+          'method.name': propertyName,
+          'method.args': JSON.stringify(filteredArgs),
+        });
+
+        try {
+          const result = await Promise.resolve(originalMethod.call(this, args));
+          span.setStatus({ code: SpanStatusCode.OK });
+          return result;
+        } catch (error: any) {
+          span.setStatus({
+            code: SpanStatusCode.ERROR,
+            message: error.message,
+          });
+          span.recordException(error);
+          throw error;
+        } finally {
+          span.end();
+          console.log(`Span ended for ${propertyName}`);
+        }
+      });
+    };
+
+    // Define the wrapped method for both the original name and the camelCase version
+    Object.defineProperty(obj, propertyName, {
+      value: wrappedMethod,
+      writable: true,
+      enumerable: false,
+      configurable: true,
+    });
+
+    if (propertyName !== camelCaseName) {
+      Object.defineProperty(obj, camelCaseName, {
+        value: wrappedMethod,
+        writable: true,
+        enumerable: false,
+        configurable: true,
+      });
+    }
+  }
+
+  function wrapObjectMethods(obj: any) {
+    const properties = new Set([
+      ...Object.getOwnPropertyNames(obj),
+      ...Object.getOwnPropertyNames(Object.getPrototypeOf(obj))
+    ]);
+
+    properties.forEach(prop => {
+      if (typeof obj[prop] === 'function' && prop !== 'constructor') {
+        wrapMethod(obj, prop);
+      }
+    });
+  }
+
+  wrapObjectMethods(exchangeInstance);
 }
