6b0205db53217d81d723cb5cfa06d7c1b4d9e004
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-10-06 15:14:01 +0100

    fix(mexc): fetchDepositAddress/createDepositAddress network id

--- ts/src/mexc.ts
+++ ts/src/mexc.ts
@@ -4551,7 +4551,16 @@ export default class mexc extends Exchange {
         const networkCode = this.safeString (params, 'network');
         let networkId = undefined;
         if (networkCode !== undefined) {
-            networkId = this.networkCodeToId (networkCode, code);
+            // createDepositAddress and fetchDepositAddress use a different network-id compared to withdraw
+            const networkUnified = this.networkIdToCode (networkCode, code);
+            const networks = this.safeDict (currency, 'networks', {});
+            if (networkUnified in networks) {
+                const network = this.safeDict (networks, networkUnified, {});
+                const networkInfo = this.safeValue (network, 'info', {});
+                networkId = this.safeString (networkInfo, 'network');
+            } else {
+                networkId = this.networkCodeToId (networkCode, code);
+            }
         }
         if (networkId !== undefined) {
             request['network'] = networkId;
@@ -4593,7 +4602,17 @@ export default class mexc extends Exchange {
         if (networkCode === undefined) {
             throw new ArgumentsRequired (this.id + ' createDepositAddress requires a `network` parameter');
         }
-        const networkId = this.networkCodeToId (networkCode, code);
+        // createDepositAddress and fetchDepositAddress use a different network-id compared to withdraw
+        let networkId = undefined;
+        const networkUnified = this.networkIdToCode (networkCode, code);
+        const networks = this.safeDict (currency, 'networks', {});
+        if (networkUnified in networks) {
+            const network = this.safeDict (networks, networkUnified, {});
+            const networkInfo = this.safeValue (network, 'info', {});
+            networkId = this.safeString (networkInfo, 'network');
+        } else {
+            networkId = this.networkCodeToId (networkCode, code);
+        }
         if (networkId !== undefined) {
             request['network'] = networkId;
         }
@@ -4620,7 +4639,6 @@ export default class mexc extends Exchange {
          * @returns {object} an [address structure]{@link https://docs.ccxt.com/#/?id=address-structure}
          */
         const network = this.safeString (params, 'network');
-        params = this.omit (params, [ 'network' ]);
         const addressStructures = await this.fetchDepositAddressesByNetwork (code, params);
         let result = undefined;
         if (network !== undefined) {
