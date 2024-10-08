4eed73a51bd7e8c3f18303ffbbe980ebcce134fd
t.t <7117978+ttodua@users.noreply.github.com>
2024-10-09 00:11:57 +0400

    fix(base) - disable last json response

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -291,7 +291,7 @@ export default class Exchange {
         quote?: Num,
     } = undefined
 
-    enableLastJsonResponse: boolean = true
+    enableLastJsonResponse: boolean = false
     enableLastHttpResponse: boolean = true
     enableLastResponseHeaders: boolean = true
     last_http_response    = undefined
