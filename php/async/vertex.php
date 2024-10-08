beb34a96f7e04acf4eb66233488352099ee03855
Travis CI <travis@travis-ci.org>
2024-10-08 11:33:21 +0000

    feat(vertex): update encoding (#23903)
    * fix(exchange): decode gzip / deflate in php async
    
    * exchange: set header to lower case
    
    * feat(vertex): apply compressed encoding
    
    * base(ratchet): support permessage deflate in stream
    
    * base(cs): set AutomaticDecompression
    
    [ci skip]

--- php/async/vertex.php
+++ php/async/vertex.php
@@ -2999,6 +2999,16 @@ class vertex extends Exchange {
                 $url .= '?' . $this->urlencode($params);
             }
         }
+        if ($path !== 'execute') {
+            // required encoding for public methods
+            if ($headers !== null) {
+                $headers['Accept-Encoding'] = 'gzip';
+            } else {
+                $headers = array(
+                    'Accept-Encoding' => 'gzip',
+                );
+            }
+        }
         return array( 'url' => $url, 'method' => $method, 'body' => $body, 'headers' => $headers );
     }
 }
