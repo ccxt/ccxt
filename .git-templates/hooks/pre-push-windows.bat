cf2e6d905b21c2c152a426eb692a0d719dcb6cff
t.t <7117978+ttodua@users.noreply.github.com>
2024-10-17 12:45:08 +0400

    update

--- .git-templates/hooks/pre-push-windows.bat
+++ .git-templates/hooks/pre-push-windows.bat
@@ -1,4 +1,6 @@
 
 set current_dir=%~dp0
-cd %current_dir%/../../
-call "%ProgramFiles%\Git\git-bash.exe" "%current_dir%pre-push" "--sleep"
+:: need to double backslash it before passing into arg of command
+set current_dir=%current_dir:\=\\\\%
+:: we need to add "read" in the end to prevent the window from auto-closing immediately
+"%ProgramFiles%\Git\git-bash.exe" -c "%current_dir%pre-push; read"
