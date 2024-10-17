f4c41e58811402fe319da77d166970760aaee873
t.t <7117978+ttodua@users.noreply.github.com>
2024-10-17 13:17:50 +0400

    update windows script

--- .git-templates/hooks/pre-push-windows.bat
+++ .git-templates/hooks/pre-push-windows.bat
@@ -1,6 +1,10 @@
+:: Note, you can either:
+::  A) run this file
+::    or
+::  B) directly add the last line (with double backslashed absolute paths) in your Windows preferred Git's pre-push command
 
 set current_dir=%~dp0
 :: need to double backslash it before passing into arg of command
 set current_dir=%current_dir:\=\\\\%
-:: we need to add "read" in the end to prevent the window from auto-closing immediately
-"%ProgramFiles%\Git\git-bash.exe" -c "%current_dir%pre-push; read"
+:: in case of exit-code is not 0, we should prevent the window from auto-closing immediately
+"%ProgramFiles%\Git\git-bash.exe" -c "%current_dir%pre-push; exit_code=$?; if [ $exit_code -ne 0 ]; then read; fi;"
