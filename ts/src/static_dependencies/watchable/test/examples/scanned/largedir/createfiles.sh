50ada1b9b0787211bbe29dea2895bd786dcb6514
pcriadoperez <pablo@criadoperez.com>
2024-10-19 13:36:01 +0300

    remove vite and fix static_dependency

--- ts/src/static_dependencies/watchable/test/examples/scanned/largedir/createfiles.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-for i in {1..10000}; do touch $i; done\n
--- ts/src/static_dependencies/watchable/test/util.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/promise-function-async
-export function sleep(delayMs: number) {
-  return new Promise((resolve) => setTimeout(resolve, delayMs));
-}
