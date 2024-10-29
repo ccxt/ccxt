f5985e69fd34970124a72b5ebfdf4ffc7d38f409
t.t <7117978+ttodua@users.noreply.github.com>
2024-10-29 18:26:13 +0400

    remove worker

--- build/csharp-worker.js
+++ build/csharp-worker.js
@@ -1,11 +1,12 @@
 import { Transpiler } from 'ast-transpiler';
 import log from 'ololog'
+const logsEnabled = !process.env.DISABLE_EXTRA_BUILD_LOGS;
 export default async ({transpilerConfig, files}) => {
     const transpiler = new Transpiler(transpilerConfig);
 
     const result = [];
     for (const filePath of files) {
-        log.blue('[worker][csharp] Transpiling', filePath);
+        if (logsEnabled) log.blue('[worker][csharp] Transpiling', filePath);
         const transpiled = transpiler.transpileCSharpByPath(filePath);
         result.push(transpiled);
     }
