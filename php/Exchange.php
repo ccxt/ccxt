f16d1bac00976d078051a9393fc6672f1e965358
Travis CI <travis@travis-ci.org>
2024-10-29 12:38:49 +0000

    4.4.25
    [ci skip]

--- php/Exchange.php
+++ php/Exchange.php
@@ -43,7 +43,7 @@ use BN\BN;
 use Sop\ASN1\Type\UnspecifiedType;
 use Exception;
 
-$version = '4.4.24';
+$version = '4.4.25';
 
 // rounding mode
 const TRUNCATE = 0;
@@ -62,7 +62,7 @@ const PAD_WITH_ZERO = 6;
 
 class Exchange {
 
-    const VERSION = '4.4.24';
+    const VERSION = '4.4.25';
 
     private static $base58_alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
     private static $base58_encoder = null;
