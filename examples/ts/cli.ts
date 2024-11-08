4285f8fb396b605c3805c7e6b186ec520fcbfe56
T. Todua <7117978+ttodua@users.noreply.github.com>
2024-11-08 18:10:55 +0400

    fix(cli) - auto injection fixes (#24199)

--- examples/ts/cli.ts
+++ examples/ts/cli.ts
@@ -47,7 +47,14 @@ let [processPath, , exchangeId, methodName, ... params] = process.argv.filter (x
 let foundDescription = undefined;
 for (let i = 0; i < process.argv.length; i++) {
     if (process.argv[i] === '--name') {
-        foundDescription = process.argv[i + 1]; 
+        foundDescription = process.argv[i + 1];
+        // search that string in `params` and remove it
+        for (let j = 0; j < params.length; j++) {
+            if (params[j] === foundDescription) {
+                params.splice(j, 1);
+                break;
+            }
+        }
         break;
     }
 }
