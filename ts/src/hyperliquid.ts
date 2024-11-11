d8ed342e63a3a7919542daacb2399d4b13779f3d
cheunhong <chlin6755@gmail.com>
2024-11-11 23:27:01 +0800

    feat(hyperliquid) fetchFundingRates

--- ts/src/hyperliquid.ts
+++ ts/src/hyperliquid.ts
@@ -8,7 +8,7 @@ import { ROUND, SIGNIFICANT_DIGITS, DECIMAL_PLACES } from './base/functions/numb
 import { keccak_256 as keccak } from './static_dependencies/noble-hashes/sha3.js';
 import { secp256k1 } from './static_dependencies/noble-curves/secp256k1.js';
 import { ecdsa } from './base/functions/crypto.js';
-import type { Market, TransferEntry, Balances, Int, OrderBook, OHLCV, Str, FundingRateHistory, Order, OrderType, OrderSide, Trade, Strings, Position, OrderRequest, Dict, Num, MarginModification, Currencies, CancellationRequest, int, Transaction, Currency, TradingFeeInterface, Ticker, Tickers, LedgerEntry } from './base/types.js';
+import type { Market, TransferEntry, Balances, Int, OrderBook, OHLCV, Str, FundingRateHistory, Order, OrderType, OrderSide, Trade, Strings, Position, OrderRequest, Dict, Num, MarginModification, Currencies, CancellationRequest, int, Transaction, Currency, TradingFeeInterface, Ticker, Tickers, LedgerEntry, FundingRates, FundingRate } from './base/types.js';
 
 //  ---------------------------------------------------------------------------
 
@@ -761,6 +761,125 @@ export default class hyperliquid extends Exchange {
             result[symbol] = ticker;
         }
         return this.filterByArrayTickers (result, 'symbol', symbols);
+    }
+
+    async fetchFundingRates (symbols?: Strings, params?: {}): Promise<FundingRates> {
+        /**
+         * @method
+         * @name hyperliquid#fetchFundingRates
+         * @description retrieves data on all swap markets for hyperliquid
+         * @see https://hyperliquid.gitbook.io/hyperliquid-docs/for-developers/api/info-endpoint/perpetuals#retrieve-perpetuals-asset-contexts-includes-mark-price-current-funding-open-interest-etc
+         * @param {object} [params] extra parameters specific to the exchange API endpoint
+         * @returns {object[]} an array of objects representing market data
+         */
+        const request: Dict = {
+            'type': 'metaAndAssetCtxs',
+        };
+        const response = await this.publicPostInfo (this.extend (request, params));
+        //
+        //     [
+        //         {
+        //             "universe": [
+        //                 {
+        //                     "maxLeverage": 50,
+        //                     "name": "SOL",
+        //                     "onlyIsolated": false,
+        //                     "szDecimals": 2
+        //                 }
+        //             ]
+        //         },
+        //         [
+        //             {
+        //                 "dayNtlVlm": "9450588.2273",
+        //                 "funding": "0.0000198",
+        //                 "impactPxs": [
+        //                     "108.04",
+        //                     "108.06"
+        //                 ],
+        //                 "markPx": "108.04",
+        //                 "midPx": "108.05",
+        //                 "openInterest": "10764.48",
+        //                 "oraclePx": "107.99",
+        //                 "premium": "0.00055561",
+        //                 "prevDayPx": "111.81"
+        //             }
+        //         ]
+        //     ]
+        //
+        //
+        const meta = this.safeDict (response, 0, {});
+        const universe = this.safeList (meta, 'universe', []);
+        const assetCtxs = this.safeList (response, 1, []);
+        const result = [];
+        for (let i = 0; i < universe.length; i++) {
+            const data = this.extend (
+                this.safeDict (universe, i, {}),
+                this.safeDict (assetCtxs, i, {})
+            );
+            result.push (data);
+        }
+        const funding_rates = this.parseFundingRates (result);
+        return this.filterByArray (funding_rates, 'symbol', symbols);
+    }
+
+    parseFundingRate (info, market: Market = undefined): FundingRate {
+        //
+        //     {
+        //         "maxLeverage": "50",
+        //         "name": "ETH",
+        //         "onlyIsolated": false,
+        //         "szDecimals": "4",
+        //         "dayNtlVlm": "1709813.11535",
+        //         "funding": "0.00004807",
+        //         "impactPxs": [
+        //             "2369.3",
+        //             "2369.6"
+        //         ],
+        //         "markPx": "2369.6",
+        //         "midPx": "2369.45",
+        //         "openInterest": "1815.4712",
+        //         "oraclePx": "2367.3",
+        //         "premium": "0.00090821",
+        //         "prevDayPx": "2381.5"
+        //     }
+        //
+        const quoteId = 'USDC';
+        const base = this.safeString (info, 'name');
+        const quote = this.safeCurrencyCode (quoteId);
+        const settleId = 'USDC';
+        const settle = this.safeCurrencyCode (settleId);
+        let symbol = base + '/' + quote;
+        const contract = true;
+        const swap = true;
+        if (contract) {
+            if (swap) {
+                symbol = symbol + ':' + settle;
+            }
+        }
+        const funding = this.safeNumber (info, 'funding');
+        const markPx = this.safeNumber (info, 'markPx');
+        const oraclePx = this.safeNumber (info, 'oraclePx');
+        const fundingTimestamp = (Math.floor (this.milliseconds () / 60 / 60 / 1000) + 1) * 60 * 60 * 1000;
+        return {
+            'info': info,
+            'symbol': symbol,
+            'markPrice': markPx,
+            'indexPrice': oraclePx,
+            'interestRate': undefined,
+            'estimatedSettlePrice': undefined,
+            'timestamp': undefined,
+            'datetime': undefined,
+            'fundingRate': funding,
+            'fundingTimestamp': fundingTimestamp,
+            'fundingDatetime': this.iso8601 (fundingTimestamp),
+            'nextFundingRate': undefined,
+            'nextFundingTimestamp': undefined,
+            'nextFundingDatetime': undefined,
+            'previousFundingRate': undefined,
+            'previousFundingTimestamp': undefined,
+            'previousFundingDatetime': undefined,
+            'interval': '1h',
+        } as FundingRate;
     }
 
     parseTicker (ticker: Dict, market: Market = undefined): Ticker {
