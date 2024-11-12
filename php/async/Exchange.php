dfd391302adf768b64db085c1faaf993d214fe2a
Travis CI <travis@travis-ci.org>
2024-11-12 15:04:35 +0000

    4.4.30
    [ci skip]

--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -44,11 +44,11 @@ use React\EventLoop\Loop;
 
 use Exception;
 
-$version = '4.4.29';
+$version = '4.4.30';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '4.4.29';
+    const VERSION = '4.4.30';
 
     public $browser;
     public $marketsLoading = null;
--- ts/ccxt.ts
+++ ts/ccxt.ts
@@ -40,7 +40,7 @@ import {BaseError, ExchangeError, AuthenticationError, PermissionDenied, Account
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
 
-const version = '4.4.29';
+const version = '4.4.30';
 
 (Exchange as any).ccxtVersion = version
 
