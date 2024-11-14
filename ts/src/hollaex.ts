ad533a58f351f05eb4c2b9764b51cc01a76de6ef
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-11-14 14:12:39 +0000

    missing type

--- ts/src/hollaex.ts
+++ ts/src/hollaex.ts
@@ -972,7 +972,7 @@ export default class hollaex extends Exchange {
      * @name hollaex#fetchOrder
      * @description fetches information on an order made by the user
      * @see https://apidocs.hollaex.com/#get-order
-     * @param id
+     * @param {string} id
      * @param {string} symbol unified symbol of the market the order was made in
      * @param {object} [params] extra parameters specific to the exchange API endpoint
      * @returns {object} An [order structure]{@link https://docs.ccxt.com/#/?id=order-structure}
