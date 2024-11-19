651fbd69fd8084b8690c5fefd29ebf1d8b20620c
t.t <7117978+ttodua@users.noreply.github.com>
2024-11-19 16:51:41 +0400

    fix(binance) - error codes

--- ts/src/binance.ts
+++ ts/src/binance.ts
@@ -1952,6 +1952,8 @@ export default class binance extends Exchange {
                         '-4088': PermissionDenied, // User can not place order currently
                         '-4114': BadRequest, // INVALID_CLIENT_TRAN_ID_LEN
                         '-4115': BadRequest, // DUPLICATED_CLIENT_TRAN_ID
+                        '-4116': BadRequest, // DUPLICATED_CLIENT_ORDER_ID
+                        '-4117': OperationRejected, // STOP_ORDER_TRIGGERING
                         '-4118': OperationRejected, // REDUCE_ONLY_MARGIN_CHECK_FAILED
                         '-4131': OperationRejected, // The counterparty's best price does not meet the PERCENT_PRICE filter limit
                         '-4140': BadRequest, // Invalid symbol status for opening position
