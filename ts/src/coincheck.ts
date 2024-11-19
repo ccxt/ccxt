1f183f9ba3959015312438a246c1f75843a480a6
Ares <81414185+AresArtemius@users.noreply.github.com>
2024-11-19 14:33:51 +0300

    editing with safeValue to safeDict/List

--- ts/src/coincheck.ts
+++ ts/src/coincheck.ts
@@ -224,7 +224,7 @@ export default class coincheck extends Exchange {
             market = this.market (symbol);
         }
         const response = await this.privateGetExchangeOrdersOpens (params);
-        const rawOrders = this.safeValue (response, 'orders', []);
+        const rawOrders = this.safeList (response, 'orders', []);
         const parsedOrders = this.parseOrders (rawOrders, market, since, limit);
         const result = [];
         for (let i = 0; i < parsedOrders.length; i++) {
@@ -426,7 +426,7 @@ export default class coincheck extends Exchange {
             } else if (this.safeString (trade, 'liquidity') === 'M') {
                 takerOrMaker = 'maker';
             }
-            const funds = this.safeValue (trade, 'funds', {});
+            const funds = this.safeDict (trade, 'funds', {});
             amountString = this.safeString (funds, baseId);
             costString = this.safeString (funds, quoteId);
             fee = {
@@ -566,12 +566,12 @@ export default class coincheck extends Exchange {
         //         }
         //     }
         //
-        const fees = this.safeValue (response, 'exchange_fees', {});
+        const fees = this.safeDict (response, 'exchange_fees', {});
         const result: Dict = {};
         for (let i = 0; i < this.symbols.length; i++) {
             const symbol = this.symbols[i];
             const market = this.market (symbol);
-            const fee = this.safeValue (fees, market['id'], {});
+            const fee = this.safeDict (fees, market['id'], {});
             result[symbol] = {
                 'info': fee,
                 'symbol': symbol,
