b7a5ff0b01275206d059bee603662dbc73b73ecb
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-11-21 10:35:25 +0000

    fix(docs): add jsdoc to set_sandbox_mode

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -2314,6 +2314,12 @@ export default class Exchange {
         return this.filterByLimit (result, limit, key, sinceIsDefined);
     }
 
+    /**
+     * @method
+     * @name Exchange#setSandboxMode
+     * @description set the sandbox mode for the exchange
+     * @param {boolean} enabled true to enable sandbox mode, false to disable it
+     */
     setSandboxMode (enabled: boolean) {
         if (enabled) {
             if ('test' in this.urls) {
