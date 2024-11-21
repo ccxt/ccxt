c33bed23e416b22c76d010ca8ff9cbde2ac9e9d5
sc0Vu <alk03073135@gmail.com>
2024-11-21 17:40:08 +0800

    fix(gate): return value when loadUnifiedStatus

--- ts/src/gate.ts
+++ ts/src/gate.ts
@@ -900,7 +900,7 @@ export default class gate extends Exchange {
 
     /**
      * @method
-     * @name gate#isUnifiedEnabled
+     * @name gate#loadUnifiedStatus
      * @param {object} [params] extra parameters specific to the exchange API endpoint
      * @description returns unifiedAccount so the user can check if the unified account is enabled
      * @see https://www.gate.io/docs/developers/apiv4/#get-account-detail
@@ -931,6 +931,7 @@ export default class gate extends Exchange {
                 this.options['unifiedAccount'] = false;
             }
         }
+        return this.options['unifiedAccount'];
     }
 
     async upgradeUnifiedTradeAccount (params = {}) {
