e49a11d8c9b7c92b59ac238abc9296999687eab2
t.t <7117978+ttodua@users.noreply.github.com>
2024-11-22 23:27:54 +0400

    feat(features) - marginMode

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -2817,6 +2817,7 @@ export default class Exchange {
         if ('createOrder' in featuresObj) {
             const value = this.safeDict (featuresObj['createOrder'], 'attachedStopLossTakeProfit');
             if (value !== undefined) {
+                featuresObj['createOrder'] = this.omit (featuresObj['createOrder'], 'attachedStopLossTakeProfit'); // omit duplicate, as we fill them on below lines
                 featuresObj['createOrder']['stopLoss'] = value;
                 featuresObj['createOrder']['takeProfit'] = value;
             }
