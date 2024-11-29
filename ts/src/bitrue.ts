090e4ba36a5370968bd85c0cd32d17b72b7b9e1e
Peter Lai <alk03073135@gmail.com>
2024-11-29 18:12:53 +0800

    fix(bitrue): correct listenkey (#24402)
    * fix(bitrue): backward support listenKey
    
    * bitrue: update apis in pro

--- ts/src/bitrue.ts
+++ ts/src/bitrue.ts
@@ -3077,7 +3077,7 @@ export default class bitrue extends Exchange {
         const version = this.safeString (api, 1);
         const access = this.safeString (api, 2);
         let url = undefined;
-        if (type === 'api' && version === 'kline') {
+        if ((type === 'api' && version === 'kline') || (type === 'open' && path.indexOf ('listenKey') >= 0)) {
             url = this.urls['api'][type];
         } else {
             url = this.urls['api'][type] + '/' + version;
@@ -3087,7 +3087,7 @@ export default class bitrue extends Exchange {
         if (access === 'private') {
             this.checkRequiredCredentials ();
             const recvWindow = this.safeInteger (this.options, 'recvWindow', 5000);
-            if (type === 'spot') {
+            if (type === 'spot' || type === 'open') {
                 let query = this.urlencode (this.extend ({
                     'timestamp': this.nonce (),
                     'recvWindow': recvWindow,
