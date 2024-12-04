70a316d71ffe5febfc0525533832e1b1cadc84e1
t.t <7117978+ttodua@users.noreply.github.com>
2024-12-04 20:06:28 +0400

    feat(hyperliquid) - fetchCurrencies precision

--- ts/src/hyperliquid.ts
+++ ts/src/hyperliquid.ts
@@ -264,7 +264,7 @@ export default class hyperliquid extends Exchange {
                 'id': id,
                 'name': name,
                 'code': code,
-                'precision': undefined,
+                'precision': this.parseNumber (this.parsePrecision (this.safeString (data, 'szDecimals'))),
                 'info': data,
                 'active': undefined,
                 'deposit': undefined,
