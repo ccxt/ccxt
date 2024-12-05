540d9aedd3480993760cde144354a3d863c87af4
T. Todua <7117978+ttodua@users.noreply.github.com>
2024-12-05 14:45:30 +0400

    fix(probit) - watchTrades (#24456)

--- ts/src/pro/probit.ts
+++ ts/src/pro/probit.ts
@@ -163,6 +163,7 @@ export default class probit extends probitRest {
      */
     async watchTrades (symbol: string, since: Int = undefined, limit: Int = undefined, params = {}): Promise<Trade[]> {
         const channel = 'recent_trades';
+        symbol = this.safeSymbol (symbol);
         const trades = await this.subscribePublic ('watchTrades', symbol, 'trades', channel, params);
         if (this.newUpdates) {
             limit = trades.getLimit (symbol, limit);
