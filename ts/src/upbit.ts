52b4c1a90022e0701c6809b04efc4f49bb05248d
Pablo <pablo@criadoperez.com>
2024-12-08 16:52:13 +0100

    fix upbit compile

--- ts/src/upbit.ts
+++ ts/src/upbit.ts
@@ -1214,7 +1214,7 @@ export default class upbit extends Exchange {
         //             "amount": "100000.0",
         //             "fee": "0.0"
         //         },
-        //         ...,
+        //         ...
         //     ]
         //
         return this.parseTransactions (response, currency, since, limit);
@@ -1299,7 +1299,7 @@ export default class upbit extends Exchange {
         //             "fee": "0.0",
         //             "krw_amount": "80420.0"
         //         },
-        //         ...,
+        //         ...
         //     ]
         //
         return this.parseTransactions (response, currency, since, limit);
