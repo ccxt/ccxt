63e1a656a52da8033d8bc350d28ad6f01414fcdb
t.t <7117978+ttodua@users.noreply.github.com>
2024-12-19 01:37:35 +0400

    comment

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -4958,7 +4958,7 @@ export default class Exchange {
         let value = undefined;
         [ value, params ] = this.handleOptionAndParams (params, methodName1, optionName1, defaultValue);
         if (value !== undefined) {
-            // let's omit optionName2 too
+            // omit optionName2 too
             params = this.omit (params, optionName2);
             return [ value, params ];
         }
