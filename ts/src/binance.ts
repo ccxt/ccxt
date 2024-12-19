cfe74dedb7227c0251a3da5f5ab0cd4d9cf19664
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2024-12-19 11:19:08 +0100

    add weight

--- ts/src/binance.ts
+++ ts/src/binance.ts
@@ -636,8 +636,8 @@ export default class binance extends Exchange {
                         'portfolio/bnb-transfer': 150, // Weight(IP): 1500 => cost = 0.1 * 1500 = 150
                         'portfolio/repay-futures-switch': 150, // Weight(IP): 1500 => cost = 0.1 * 1500 = 150
                         'portfolio/repay-futures-negative-balance': 150, // Weight(IP): 1500 => cost = 0.1 * 1500 = 150
-                        'portfolio/mint': 2,
-                        'portfolio/redeem': 2,
+                        'portfolio/mint': 20,
+                        'portfolio/redeem': 20,
                         'lending/auto-invest/plan/add': 0.1, // Weight(IP): 1 => cost = 0.1 * 1 = 0.1
                         'lending/auto-invest/plan/edit': 0.1, // Weight(IP): 1 => cost = 0.1 * 1 = 0.1
                         'lending/auto-invest/plan/edit-status': 0.1, // Weight(IP): 1 => cost = 0.1 * 1 = 0.1
