fc47bb23264429d6385bf4e11883ca75c5567f24
Travis CI <travis@travis-ci.org>
2024-12-20 23:38:31 +0000

    4.4.42
    [ci skip]

--- dist/cjs/ccxt.js
+++ dist/cjs/ccxt.js
@@ -200,7 +200,7 @@ var xt$1 = require('./src/pro/xt.js');
 
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
-const version = '4.4.41';
+const version = '4.4.42';
 Exchange["default"].ccxtVersion = version;
 const exchanges = {
     'ace': ace,
