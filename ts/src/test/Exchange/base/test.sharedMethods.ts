35ba2df45b16be869cb8c1a24b07821f1ed433f4
t.t <7117978+ttodua@users.noreply.github.com>
2024-12-20 21:46:55 +0400

    fix contract size calc

--- ts/src/test/Exchange/base/test.sharedMethods.ts
+++ ts/src/test/Exchange/base/test.sharedMethods.ts
@@ -425,13 +425,20 @@ function assertAmountPriceCost (exchange: Exchange, skippedProperties: any, meth
     const logText = logTemplate (exchange, method, entry);
     // check `cost, amount, price` correlation
     const market = exchange.market (symbol);
+    if (!exchange.inArray (market['type'], [ 'spot', 'swap', 'future' ]) || !exchange.inArray (market['subType'], [ undefined, 'linear' ])) {
+        // atm, skip inverse contracts (todo)
+        return;
+    }
     const precision = market['precision'];
+    const contractSize = exchange.safeString (market, 'contractSize');
     const amountPrecision = exchange.safeString (precision, 'amount');
     const amount = exchange.safeString (entry, amountKey);
+    // let consider contractSize too for non-spot markets
+    const amountMultipliesContractSize = (contractSize !== undefined) ? Precise.stringMul (amount, contractSize) : amount;
     const price = exchange.safeString (entry, priceKey);
     const cost = exchange.safeString (entry, costKey);
     const amountCalculated = Precise.stringDiv (cost, price);
-    const compareResult = Precise.stringSub (amount, amountCalculated);
+    const compareResult = Precise.stringSub (amountMultipliesContractSize, amountCalculated);
     // the remainder should be >= 0 and < amountPrecision
     const isValid = Precise.stringGe (compareResult, '0') && Precise.stringLt (compareResult, amountPrecision);
     assert (isValid, 'cost & amount & price math is not correct' + logText);
