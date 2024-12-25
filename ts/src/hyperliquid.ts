dcd126638397cd8c3551a96848b06a5835b0466d
t.t <7117978+ttodua@users.noreply.github.com>
2024-12-25 20:19:32 +0400

    hyperliquid

--- ts/src/hyperliquid.ts
+++ ts/src/hyperliquid.ts
@@ -238,7 +238,12 @@ export default class hyperliquid extends Exchange {
                             'GTD': false,
                         },
                         'hedged': false,
-                        'trailing': false,
+                        'trailing': false, 
+                        'leverage': false,
+                        'marketBuyByCost': false,
+                        'marketBuyRequiresPrice': false,
+                        'selfTradePrevention': false,
+                        'iceberg': false,
                     },
                     'createOrders': {
                         'max': 1000,
