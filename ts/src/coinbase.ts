50c30541b8cf50747f45f1a2624002e56cdc7601
t.t <7117978+ttodua@users.noreply.github.com>
2024-12-25 19:48:51 +0400

    coinbase

--- ts/src/coinbase.ts
+++ ts/src/coinbase.ts
@@ -377,7 +377,7 @@ export default class coinbase extends Exchange {
                 'user_native_currency': 'USD', // needed to get fees for v3
             },
             'features': {
-                'spot': {
+                'default': {
                     'sandbox': false,
                     'createOrder': {
                         'marginMode': true,
@@ -394,7 +394,12 @@ export default class coinbase extends Exchange {
                             'GTD': true,
                         },
                         'hedged': false,
-                        'trailing': false,
+                        'trailing': false, 
+                        'leverage': true,
+                        'marketBuyByCost': true,
+                        'marketBuyRequiresPrice': true,
+                        'selfTradePrevention': false,
+                        'iceberg': false,
                     },
                     'createOrders': undefined,
                     'fetchMyTrades': {
@@ -435,21 +440,20 @@ export default class coinbase extends Exchange {
                         'limit': 350,
                     },
                 },
+                'spot': {
+                    'extends': 'default',
+                },
                 'swap': {
                     'linear': {
-                        'extends': 'spot',
+                        'extends': 'default',
                     },
-                    'inverse': {
-                        'extends': 'spot',
-                    },
+                    'inverse': undefined,
                 },
                 'future': {
                     'linear': {
-                        'extends': 'spot',
+                        'extends': 'default',
                     },
-                    'inverse': {
-                        'extends': 'spot',
-                    },
+                    'inverse': undefined,
                 },
             },
         });
