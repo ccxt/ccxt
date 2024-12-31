b2f87cf177c44331c26b69559e468523f536cb84
t.t <7117978+ttodua@users.noreply.github.com>
2024-12-31 22:42:57 +0400

    fix(binance) - fix createOrder, omited margin params

--- ts/src/binance.ts
+++ ts/src/binance.ts
@@ -6119,10 +6119,9 @@ export default class binance extends Exchange {
         await this.loadMarkets ();
         const market = this.market (symbol);
         const marketType = this.safeString (params, 'type', market['type']);
-        let marginMode = undefined;
-        [ marginMode, params ] = this.handleMarginModeAndParams ('createOrder', params);
-        let isPortfolioMargin = undefined;
-        [ isPortfolioMargin, params ] = this.handleOptionAndParams2 (params, 'createOrder', 'papi', 'portfolioMargin', false);
+        // don't omit params (hanldeX methods) here
+        const marginMode = this.safeString (params, 'marginMode');
+        const isPortfolioMargin = this.safeBool2 (params, 'papi', 'portfolioMargin', false);
         const triggerPrice = this.safeString2 (params, 'triggerPrice', 'stopPrice');
         const stopLossPrice = this.safeString (params, 'stopLossPrice');
         const takeProfitPrice = this.safeString (params, 'takeProfitPrice');
