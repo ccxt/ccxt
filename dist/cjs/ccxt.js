f396db7e5170f8df6075b3d8f9c7e531cc848e56
Travis CI <travis@travis-ci.org>
2025-01-03 10:46:51 +0000

    4.4.45
    [ci skip]

--- dist/cjs/ccxt.js
+++ dist/cjs/ccxt.js
@@ -202,7 +202,7 @@ var xt$1 = require('./src/pro/xt.js');
 
 //-----------------------------------------------------------------------------
 // this is updated by vss.js when building
-const version = '4.4.44';
+const version = '4.4.45';
 Exchange["default"].ccxtVersion = version;
 const exchanges = {
     'ace': ace,
--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -44,11 +44,11 @@ use React\EventLoop\Loop;
 
 use Exception;
 
-$version = '4.4.44';
+$version = '4.4.45';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '4.4.44';
+    const VERSION = '4.4.45';
 
     public $browser;
     public $marketsLoading = null;
