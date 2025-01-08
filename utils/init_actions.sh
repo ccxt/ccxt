45f5ac71054a6d53db0c315d2ba0fcf144da44e7
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-01-08 17:26:20 +0000

    dry logic

---
+++ utils/init_actions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+OUTPUT=$(./utils/check_modified_files.sh | tr -d '\n')
+# echo "1"
+IMPORTANT_MODIFIED=$(echo "$OUTPUT" | jq -r '.important_modified')
+# echo "2"
+REST_FILES=$(echo "$OUTPUT" | jq -r '.rest_exchanges | join(" ")')
+WS_FILES=$(echo "$OUTPUT" | jq -r '.ws_exchanges | join(" ")')
+# echo "4"
+echo "important_modified=$IMPORTANT_MODIFIED" >> $GITHUB_ENV
+# echo "5"
+echo "rest_files=$REST_FILES" >> $GITHUB_ENV
+# echo "6"
+echo "ws_files=$WS_FILES" >> $GITHUB_ENV
+# echo "7"
