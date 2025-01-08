635433edd1215c69c60fd87b0421a3933145f714
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-01-08 15:02:17 +0000

    chore: test action

---
+++ utils/check_modified_files.sh
@@ -0,0 +1,17 @@
+
+
+diff=$(git diff origin/master --name-only)
+diff=$(echo "$diff" | sed -e "s/^build\.sh//")
+diff=$(echo "$diff" | sed -e "s/^skip\-tests\.json//")
+diff_without_statics=$(echo "$diff" | sed -e "s/^ts\/src\/test\/static.*json//")
+
+critical_pattern='Client(Trait)?\.php|Exchange\.php|\/base|^build|static_dependencies|^run-tests|package(-lock)?\.json|composer\.json|ccxt\.ts|__init__.py|test' # add \/test|
+
+
+if [[ "$diff_without_statics" =~ $critical_pattern ]]; then
+    echo "$msgPrefix Important changes detected - doing full build & test"
+else
+    echo "$msgPrefix Unimportant changes detected - build & test only specific exchange(s)"
+fi
+
+echo "$diff_without_statics"
