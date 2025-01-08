931151f6309b84933267299b93501600b34b61c9
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-01-08 15:31:23 +0000

    add diff

--- utils/check_modified_files.sh
+++ utils/check_modified_files.sh
@@ -7,6 +7,7 @@ diff_without_statics=$(echo "$diff" | sed -e "s/^ts\/src\/test\/static.*json//")
 
 critical_pattern='Client(Trait)?\.php|Exchange\.php|\/base|^build|static_dependencies|^run-tests|package(-lock)?\.json|composer\.json|ccxt\.ts|__init__.py|test' # add \/test|
 
+echo "Diff:: $diff"
 
 if [[ "$diff_without_statics" =~ $critical_pattern ]]; then
     echo "$msgPrefix Important changes detected - doing full build & test"
