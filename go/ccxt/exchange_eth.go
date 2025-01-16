c953a5e2ae43f9ff7ff06df4714f03e0239475a7
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-01-16 14:09:59 +0000

    eth encoding fixes

--- go/ccxt/exchange_eth.go
+++ go/ccxt/exchange_eth.go
@@ -1,19 +1,30 @@
 package ccxt
 
 import (
-	"encoding/hex"
 	"fmt"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/signer/core/apitypes"
 	"github.com/vmihailenco/msgpack/v5"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
-func ethEncodeStructuredData(primaryType string, domain apitypes.TypedDataDomain, messageTypes map[string][]apitypes.Type, messageData map[string]interface{}) ([]byte, error) {
+func ethEncodeStructuredData(primaryType string, domain apitypes.TypedDataDomain, messageTypes map[string][]apitypes.Type, messageData map[string]interface{}) (string, error) {
 	// domain {"chainId":1337,"name":"Exchange","verifyingContract":"0x0000000000000000000000000000000000000000","version":"1"}
 	// agent: {"Agent":[{"name":"source","type":"string"},{"name":"connectionId","type":"bytes32"}]}
 	// phantom: {"source":"a","connectionId":{"0":81,"1":132,"2":60,"3":100,"4":202,"5":146,"6":114,"7":128,"8":99,"9":200,"10":106,"11":37,"12":220,"13":61,"14":150,"15":236,"16":173,"17":119,"18":83,"19":11,"20":205,"21":91,"22":222,"23":149,"24":201,"25":182,"26":71,"27":103,"28":74,"29":0,"30":223,"31":202}}
+
+	var domainTypesList []apitypes.Type = []apitypes.Type{
+		{Name: "name", Type: "string"},
+		{Name: "version", Type: "string"},
+		{Name: "chainId", Type: "uint256"},
+		{Name: "verifyingContract", Type: "address"},
+		// {Name: "salt", Type: "bytes32"},
+	}
+
+	messageTypes["EIP712Domain"] = domainTypesList
+
 	typedData := apitypes.TypedData{
 		Domain:      domain,
 		Types:       messageTypes,
@@ -21,14 +32,46 @@ func ethEncodeStructuredData(primaryType string, domain apitypes.TypedDataDomain
 		Message:     messageData,
 	}
 
-	// Encode the structured data
-	encodedData, err := typedData.HashStruct(typedData.PrimaryType, typedData.Message)
+	// domainMap := map[string]interface{}{
+	// 	"name":              domain.Name,
+	// 	"version":           domain.Version,
+	// 	"chainId":           domain.ChainId,
+	// 	"verifyingContract": domain.VerifyingContract,
+	// }
+
+	// domainType := apitypes.Type{
+	// 	Name: "EIP712Domain",
+	// 	Members: []apitypes.Member{
+	// 		{Name: "name", Type: "string"},
+	// 		{Name: "version", Type: "string"},
+	// 		{Name: "chainId", Type: "uint256"},
+	// 		{Name: "verifyingContract", Type: "address"},
+	// 	},
+	// }
+	// // Encode the structured data
+	// // _, str, err := apitypes.TypedDataAndHash(typedData)
+	// // hashDomain
+
+	encodedDomain, err := typedData.HashStruct("EIP712Domain", domain.Map())
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
+	encodedData, err := typedData.HashStruct(typedData.PrimaryType, typedData.Message)
+	if err != nil {
+		return "", err
+	}
+
+	// encodedBytes := append(encodedDomain, encodedData...)
+
+	domainHex := hexutil.Encode(encodedDomain) // comes with 0x, remove
+	dataHex := hexutil.Encode(encodedData)
+
+	encodedHex := "1901" + domainHex + dataHex
+	return encodedHex, nil
+
 	// // Convert to hex
-	hexData := hex.EncodeToString(encodedData)
+	// hexData := hex.EncodeToString(encodedData)
 
 	// // Slice the last 132 characters
 	// if len(hexData) < 132 {
@@ -37,12 +80,10 @@ func ethEncodeStructuredData(primaryType string, domain apitypes.TypedDataDomain
 	// slicedHex := hexData[len(hexData)-132:]
 
 	// Convert hex to binary
-	binaryData, err := hex.DecodeString(hexData)
-	if err != nil {
-		return nil, err
-	}
-
-	return binaryData, nil
+	// binaryData, err := hex.DecodeString(hexData)
+	// if err != nil {
+	// 	return nil, err
+	// }
 }
 
 func (this *Exchange) EthEncodeStructuredData(domain2 interface{}, messageTypes2 interface{}, messageData2 interface{}) []uint8 {
@@ -77,18 +118,65 @@ func (this *Exchange) EthEncodeStructuredData(domain2 interface{}, messageTypes2
 		}
 	}
 
-	binaryData, err := ethEncodeStructuredData(primaryType, domainTyped, messageTypesTyped, messageData)
+	// domainTypes := map[string]apitypes.Type{
+	// 	"name": apitypes.Type{
+	// 		Name: "name",
+	// 		Type: "string",
+	// 	},
+	// 	"version": apitypes.Type{
+	// 		Name: "version",
+	// 		Type: "string",
+	// 	},
+	// 	"chainId": apitypes.Type{
+	// 		Name: "chainId",
+	// 		Type: "uint256",
+	// 	},
+	// 	"verifyingContract": apitypes.Type{
+	// 		Name: "verifyingContract",
+	// 		Type: "address",
+	// 	},
+	// 	"salt": apitypes.Type{
+	// 		Name: "salt",
+	// 		Type: "bytes32",
+	// 	},
+	// }
+
+	// domainMap := map[string]interface{}{
+	// 	"EIP712Domain": domainTypes,
+	// }
+
+	hexData, err := ethEncodeStructuredData(primaryType, domainTyped, messageTypesTyped, messageData)
 	if err != nil {
 		// log.Fatalf("Error encoding data: %v", err)
-		str, _ := fmt.Printf("Binary Data: %x\n", binaryData)
+		str, _ := fmt.Printf("Binary Data: %x\n", hexData)
 		panic(str)
 	}
-	return binaryData
+	return this.Base16ToBinary(hexData)
 }
 
-func (this *Exchange) Packb(data2 interface{}) []uint8 {
-	data := data2.(map[string]interface{})
-	packed, err := msgpack.Marshal(data)
+func ConvertInt64ToString(data interface{}) interface{} {
+	switch v := data.(type) {
+	case map[string]interface{}:
+		for key, value := range v {
+			v[key] = ConvertInt64ToString(value)
+		}
+		return v
+	case []interface{}:
+		for i, item := range v {
+			v[i] = ConvertInt64ToString(item)
+		}
+		return v
+	case int64:
+		return fmt.Sprintf("%d", v)
+	default:
+		return v // Leave other types unchanged
+	}
+}
+
+func (this *Exchange) Packb(data interface{}) []uint8 {
+	// data := data2.(map[string]interface{})
+	converted := ConvertInt64ToString(data) // avoid this error: position 7: int64 marker - cannot parse uint64 to javascript, setting to Infinity
+	packed, err := msgpack.Marshal(converted)
 	if err != nil {
 		panic(err)
 	}
