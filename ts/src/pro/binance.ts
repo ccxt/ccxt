b48732acdc6359274d69a8c2b54deea540c5da70
sc0Vu <alk03073135@gmail.com>
2025-01-25 15:03:22 +0800

    fix(binance): keepAliveListenKey

--- ts/src/pro/binance.ts
+++ ts/src/pro/binance.ts
@@ -2385,6 +2385,7 @@ export default class binance extends binanceRest {
             let response = undefined;
             if (isPortfolioMargin) {
                 response = await this.papiPostListenKey (params);
+                params = this.extend (params, { 'portfolioMargin': true });
             } else if (type === 'future') {
                 response = await this.fapiPrivatePostListenKey (params);
             } else if (type === 'delivery') {
@@ -2435,6 +2436,7 @@ export default class binance extends binanceRest {
         try {
             if (isPortfolioMargin) {
                 await this.papiPutListenKey (this.extend (request, params));
+                params = this.extend (params, { 'portfolioMargin': true });
             } else if (type === 'future') {
                 await this.fapiPrivatePutListenKey (this.extend (request, params));
             } else if (type === 'delivery') {
