0172ba2000650cbc688403d482924e23d8e9a313
Carlos G <43336371+carlosmiei@users.noreply.github.com>
2025-01-27 13:59:20 +0000

    chore: relesae twine install (#25068)
    * chore: relesae twine install
    
    * revert version

--- php/async/Exchange.php
+++ php/async/Exchange.php
@@ -44,11 +44,11 @@ use React\EventLoop\Loop;
 
 use Exception;
 
-$version = '4.4.52';
+$version = '4.4.50';
 
 class Exchange extends \ccxt\Exchange {
 
-    const VERSION = '4.4.52';
+    const VERSION = '4.4.50';
 
     public $browser;
     public $marketsLoading = null;
