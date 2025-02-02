14449a36f43ff5afe683ddac2fc05397f5bf225c
T. Todua <7117978+ttodua@users.noreply.github.com>
2025-02-03 01:19:04 +0400

    fix(bingx) - withdraw fix (#25149)
    * fix(bingx) - withdraw fix
    
    * currencies & markets

--- ts/src/bingx.ts
+++ ts/src/bingx.ts
@@ -5789,7 +5789,7 @@ export default class bingx extends Exchange {
         const request: Dict = {
             'coin': currency['id'],
             'address': address,
-            'amount': this.numberToString (amount),
+            'amount': this.currencyToPrecision (code, amount),
             'walletType': walletType,
         };
         const network = this.safeStringUpper (params, 'network');
