30dc2cf615aecfc96399309342936bc1e8f9d538
t.t <7117978+ttodua@users.noreply.github.com>
2025-02-12 19:51:25 +0400

    dict fix

--- cs/ccxt/base/Exchange.Options.cs
+++ cs/ccxt/base/Exchange.Options.cs
@@ -255,7 +255,8 @@ public partial class Exchange
             var concurrentExtendedDict = new ConcurrentDictionary<string, object>(extendedDict);
             this.options = concurrentExtendedDict;
         } else {
-            this.options =  new ConcurrentDictionary<string, object>(this.getDefaultOptions());
+            var dict2 = this.getDefaultOptions() as dict;
+            this.options =  new ConcurrentDictionary<string, object>(dict2);
         }
         this.verbose = (bool)this.safeValue(extendedProperties, "verbose", false);
         this.timeframes = SafeValue(extendedProperties, "timeframes", new dict()) as dict;
