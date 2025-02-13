ca75a8a0b29b2460583a015ceae0b304e54bb089
Carlo Revelli <carlo.revelli@berkeley.edu>
2025-02-13 22:29:20 +0400

    minor transpile edit

--- build/transpile.ts
+++ build/transpile.ts
@@ -782,7 +782,7 @@ class Transpiler {
         }
         const matchObject = {
             'Account': /-> (?:List\[)?Account/,
-            'Any': /: (?:List\[)?Any/,
+            'Any': /(?:->|:) (?:List\[)?Any/,
             'BalanceAccount': /-> BalanceAccount:/,
             'Balances': /-> Balances:/,
             'BorrowInterest': /-> BorrowInterest:/,
