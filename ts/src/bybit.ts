86494c6ea847e614e2aceee9bdff9133be1c4482
Dan <83686770+Dan-krm@users.noreply.github.com>
2025-02-13 05:30:01 -0600

    fix(bybit): createOrder, spot market buy amount edit (#25273)

--- ts/src/bybit.ts
+++ ts/src/bybit.ts
@@ -4074,7 +4074,7 @@ export default class bybit extends Exchange {
                 } else if (price !== undefined) {
                     request['qty'] = this.getCost (symbol, Precise.stringMul (amountString, priceString));
                 } else {
-                    request['qty'] = this.getCost (symbol, this.numberToString (amount));
+                    request['qty'] = amountString;
                 }
             }
         } else {
