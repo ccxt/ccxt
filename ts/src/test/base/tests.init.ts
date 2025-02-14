44974ce4ae4cb37ba85d67bca7fe2947f8d47d67
t.t <7117978+ttodua@users.noreply.github.com>
2025-02-14 21:46:14 +0400

    test(base) - afterConstructor

--- ts/src/test/base/tests.init.ts
+++ ts/src/test/base/tests.init.ts
@@ -14,9 +14,11 @@ import testSum from './test.sum.js';
 import testOmit from './test.omit.js';
 import testGroupBy from './test.groupBy.js';
 import testFilterBy from './test.filterBy.js';
+import afterConstructor from './test.afterConstructor.js';
 
 function baseTestsInit () {
     testLanguageSpecific ();
+    afterConstructor ();
     testExtend ();
     testDeepExtend ();
     testCryptography ();
