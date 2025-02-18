fd02ca05e119013e5ad58ded8e22bd07ea5b2935
pablo <pablo@criadoperez.com>
2025-02-18 01:02:19 -0600

    increase test php memory

--- php/test/tests_helpers.php
+++ php/test/tests_helpers.php
@@ -17,7 +17,7 @@ use Exception; // a common import
 
 error_reporting(E_ALL);
 date_default_timezone_set('UTC');
-ini_set('memory_limit', '512M');
+ini_set('memory_limit', '1024M');
 
 define('rootDir', __DIR__ . '/../../');
 
