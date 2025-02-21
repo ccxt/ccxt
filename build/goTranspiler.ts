d5b399116106d007ae26859d230cafad82927211
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-02-21 10:46:11 +0000

    fix method

--- build/goTranspiler.ts
+++ build/goTranspiler.ts
@@ -139,7 +139,7 @@ const VIRTUAL_BASE_METHODS = {
     "safeMarket": false, // try to remove custom implementations
     "market": false,
     "setSandboxMode": false,
-    "parseConversion": true,
+    "parseConversion": false,
     "sign": false
 }
 
