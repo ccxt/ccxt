8d68ac53a40682a59ad589d5700cdf0ff0606e9c
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-03-06 09:48:19 +0000

    add logo

--- ts/src/cryptomus.ts
+++ ts/src/cryptomus.ts
@@ -117,7 +117,7 @@ export default class cryptomus extends Exchange {
             },
             'timeframes': {},
             'urls': {
-                'logo': '', // todo
+                'logo': 'https://github.com/user-attachments/assets/8e0b1c48-7c01-4177-9224-f1b01d89d7e7',
                 'api': {
                     'public': 'https://api.cryptomus.com',
                     'private': 'https://api.cryptomus.com',
