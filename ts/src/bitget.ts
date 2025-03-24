f2b8e2a076b8cd8cb73b1305580190502aa299ba
flytam <tanjiahui2015@foxmail.com>
2025-03-24 19:52:43 +0800

    fix(bitget): adjust leverage parsing for cross margin mode (#25534)
    * fix(bitget): adjust leverage parsing for cross margin mode
    
    The leverage parsing logic was updated to correctly handle cross margin mode by checking the `marginMode` field and adjusting the leverage values accordingly. This ensures accurate representation of leverage for both isolated and cross margin modes.
    
    * fix transpiling
    
    * add response tests
    
    * fix response tests
    
    ---------
    
    Co-authored-by: carlosmiei <43336371+carlosmiei@users.noreply.github.com>

--- ts/src/bitget.ts
+++ ts/src/bitget.ts
@@ -7386,12 +7386,15 @@ export default class bitget extends Exchange {
     }
 
     parseLeverage (leverage: Dict, market: Market = undefined): Leverage {
+        const isCrossMarginMode = this.safeString (leverage, 'marginMode') === 'crossed';
+        const longLevKey = isCrossMarginMode ? 'crossedMarginLeverage' : 'isolatedLongLever';
+        const shortLevKey = isCrossMarginMode ? 'crossedMarginLeverage' : 'isolatedShortLever';
         return {
             'info': leverage,
             'symbol': market['symbol'],
-            'marginMode': 'isolated',
-            'longLeverage': this.safeInteger (leverage, 'isolatedLongLever'),
-            'shortLeverage': this.safeInteger (leverage, 'isolatedShortLever'),
+            'marginMode': isCrossMarginMode ? 'cross' : 'isolated',
+            'longLeverage': this.safeInteger (leverage, longLevKey),
+            'shortLeverage': this.safeInteger (leverage, shortLevKey),
         } as Leverage;
     }
 
