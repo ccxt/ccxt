0465d1c7e5898f5f1f8d1b88de8f0d9be37fffa3
Igor Kroitor <igor.kroitor@gmail.com>
2025-04-12 18:48:56 +0300

    test.fetchCurrencies.ts inactive currencies clause fix #25698

--- ts/src/test/Exchange/test.fetchCurrencies.ts
+++ ts/src/test/Exchange/test.fetchCurrencies.ts
@@ -9,8 +9,8 @@ async function testFetchCurrencies (exchange: Exchange, skippedProperties: objec
     // const isNative = exchange.has['fetchCurrencies'] && exchange.has['fetchCurrencies'] !== 'emulated';
     const currencies = await exchange.fetchCurrencies ();
     // todo: try to invent something to avoid undefined undefined, i.e. maybe move into private and force it to have a value
-    let activeAmount = 0;
-    const minmiumActiveCurrenciesPcnt = 40; // eg. at least X% currencies should be active
+    let inactiveAmount = 0;
+    const maxInactiveCurrenciesPercentage = 60; // no more than X% currencies should be inactive
     const requiredActiveCurrencies = [ 'BTC', 'ETH', 'USDT', 'USDC' ];
     if (currencies !== undefined) {
         const values = Object.values (currencies);
@@ -26,8 +26,8 @@ async function testFetchCurrencies (exchange: Exchange, skippedProperties: objec
             testCurrency (exchange, skippedProperties, method, currencyObj);
             // detailed check for deposit/withdraw
             const active = exchange.safeBool (currencyObj, 'active', false);
-            if (active) {
-                activeAmount = activeAmount + 1;
+            if (active === false) {
+                inactiveAmount = inactiveAmount + 1;
             }
             // ensure that major currencies are not disabled for W/D
             const code = exchange.safeString (currencyObj, 'code', undefined);
@@ -36,8 +36,8 @@ async function testFetchCurrencies (exchange: Exchange, skippedProperties: objec
             }
         }
         // check at least X% of currencies are active
-        const activeCurrenciesPcnt = (activeAmount / currenciesLength) * 100;
-        assert (skipActive || (activeCurrenciesPcnt >= minmiumActiveCurrenciesPcnt), 'Percentage of active currencies is too low at ' + activeCurrenciesPcnt.toString () + '% that is less than the required minimum of ' + minmiumActiveCurrenciesPcnt.toString () + '%');
+        const inactiveCurrenciesPercentage = (inactiveAmount / currenciesLength) * 100;
+        assert (skipActive || (inactiveCurrenciesPercentage < maxInactiveCurrenciesPercentage), 'Percentage of inactive currencies is too high at ' + inactiveCurrenciesPercentage.toString () + '% that is more than the allowed maximum of ' + maxInactiveCurrenciesPercentage.toString () + '%');
     }
     return true;
 }
