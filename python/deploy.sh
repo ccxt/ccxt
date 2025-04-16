a355b78ed93e396e2ea027b076df1e17bf095883
t.t <7117978+ttodua@users.noreply.github.com>
2025-04-16 13:57:59 +0400

    44

--- python/deploy.sh
+++ python/deploy.sh
@@ -1,6 +1,6 @@
 echo "running py setup"
-echo "TOKEN ${PYPI_TOKEN:0:10}"
-echo "TOKEN ${GITHUB_TOKEN:0:10}"
+echo "TOKEN ${GITHUB_TOKEN1:0:10}"
+echo "TOKEN ${GITHUB_TOKEN2:0:10}"
 
 git remote add origin https://${GITHUB_TOKEN}@github.com/ccxt/ccxt.git
 touch test123
