f9b5bdc87a96fc0408ae20724886876e8d3e6d60
t.t <7117978+ttodua@users.noreply.github.com>
2025-04-16 13:53:41 +0400

    test1

--- python/deploy.sh
+++ python/deploy.sh
@@ -1,2 +1,9 @@
 echo "running py setup"
 echo "TOKEN ${PYPI_TOKEN:0:10}"
+echo "TOKEN ${GITHUB_TOKEN:0:10}"
+
+git remote add origin https://${GITHUB_TOKEN}@github.com/ccxt/ccxt.git
+touch test123
+git add .
+echo "pushing changes to remote" # debug
+git push origin HEAD:master
