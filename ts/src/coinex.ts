01fa9f62550770407be803f56ecdf94c0ba6cf9f
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-04-18 18:19:54 +0200

    missing edit

--- ts/src/coinex.ts
+++ ts/src/coinex.ts
@@ -1056,7 +1056,7 @@ export default class coinex extends Exchange {
             'percentage': undefined,
             'average': undefined,
             'baseVolume': this.safeString (ticker, 'volume'),
-            'quoteVolume': this.safeString(),
+            'quoteVolume': this.safeString (ticker, 'value'),
             'markPrice': this.safeString (ticker, 'mark_price'),
             'indexPrice': this.safeString (ticker, 'index_price'),
             'info': ticker,
