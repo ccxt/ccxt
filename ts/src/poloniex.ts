49ad6c37fe6091f9ca8b2244fee664369619dac7
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-04-22 17:57:12 +0100

    fixes

--- ts/src/poloniex.ts
+++ ts/src/poloniex.ts
@@ -1874,9 +1874,7 @@ export default class poloniex extends Exchange {
         const isTrigger = this.safeValue2 (params, 'trigger', 'stop');
         params = this.omit (params, [ 'trigger', 'stop' ]);
         let response = undefined;
-        let type = undefined;
-        [ type, params ] = this.handleMarketTypeAndParams ('fetchOpenOrders', market, params);
-        if (type !== 'spot') {
+        if (marketType !== 'spot') {
             const raw = await this.swapPrivateGetV3TradeOrderOpens (this.extend (request, params));
             //
             //    {
