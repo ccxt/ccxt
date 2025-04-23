cb02da8213ebc3c27e094931f232101f30196dbd
yzh-pelle <81404729+yzh-pelle@users.noreply.github.com>
2025-04-23 21:26:08 +0300

    watchTrades and watchOrderBook updated

--- ts/src/pro/tradeogre.ts
+++ ts/src/pro/tradeogre.ts
@@ -54,13 +54,12 @@ export default class tradeogre extends tradeogreRest {
      */
     async watchOrderBook (symbol: string, limit: Int = undefined, params = {}): Promise<OrderBook> {
         await this.loadMarkets ();
-        const name = 'book';
         const market = this.market (symbol);
         const url = this.urls['api']['ws'];
         const messageHash = 'orderbook' + ':' + market['symbol'];
         const request: Dict = {
             'a': 'subscribe',
-            'e': name,
+            'e': 'book',
             't': market['id'],
         };
         const orderbook = await this.watch (url, messageHash, this.deepExtend (request, params), messageHash);
@@ -71,26 +70,6 @@ export default class tradeogre extends tradeogreRest {
         //
         // initial snapshot is fetched with ccxt's fetchOrderBook
         // the feed does not include a snapshot, just the deltas
-        //
-        //     {
-        //         "data": {
-        //             "timestamp": "1583656800",
-        //             "microtimestamp": "1583656800237527",
-        //             "bids": [
-        //                 ["8732.02", "0.00002478", "1207590500704256"],
-        //                 ["8729.62", "0.01600000", "1207590502350849"],
-        //                 ["8727.22", "0.01800000", "1207590504296448"],
-        //             ],
-        //             "asks": [
-        //                 ["8735.67", "2.00000000", "1207590693249024"],
-        //                 ["8735.67", "0.01700000", "1207590693634048"],
-        //                 ["8735.68", "1.53294500", "1207590692048896"],
-        //             ],
-        //         },
-        //         "event": "data",
-        //         "channel": "diff_order_book_btcusd"
-        //     }
-        //
         //
         //     {
         //         "e": "book",
@@ -109,33 +88,31 @@ export default class tradeogre extends tradeogreRest {
         }
         const storedOrderBook = this.orderbooks[symbol];
         const nonce = this.safeInteger (storedOrderBook, 'nonce');
-        const delta = this.safeDict (message, 'd', {});
         const deltaNonce = this.safeInteger (message, 's');
         const messageHash = 'orderbook:' + symbol;
         if (nonce === undefined) {
             const cacheLength = storedOrderBook.cache.length;
-            // the rest API is very delayed
-            // usually it takes at least 4-5 deltas to resolve
             const snapshotDelay = this.handleOption ('watchOrderBook', 'snapshotDelay', 6);
             if (cacheLength === snapshotDelay) {
                 this.spawn (this.loadOrderBook, client, messageHash, symbol, null, {});
             }
-            storedOrderBook.cache.push (delta);
+            storedOrderBook.cache.push (message);
             return;
         } else if (nonce >= deltaNonce) {
             return;
         }
-        this.handleDelta (storedOrderBook, delta);
+        this.handleDelta (storedOrderBook, message);
         client.resolve (storedOrderBook, messageHash);
     }
 
     handleDelta (orderbook, delta) {
-        const timestamp = this.safeTimestamp (delta, 'timestamp');
+        const timestamp = this.milliseconds (); // todo check if this is correct
         orderbook['timestamp'] = timestamp;
         orderbook['datetime'] = this.iso8601 (timestamp);
-        orderbook['nonce'] = this.safeInteger (delta, 'microtimestamp');
-        const bids = this.safeValue (delta, 'bids', []);
-        const asks = this.safeValue (delta, 'asks', []);
+        orderbook['nonce'] = this.safeInteger (delta, 's');
+        const data = this.safeDict (delta, 'd', {});
+        const bids = this.safeDict (data, 'bids', {});
+        const asks = this.safeDict (data, 'asks', {});
         const storedBids = orderbook['bids'];
         const storedAsks = orderbook['asks'];
         this.handleBidAsks (storedBids, bids);
@@ -150,16 +127,15 @@ export default class tradeogre extends tradeogreRest {
     }
 
     getCacheIndex (orderbook, deltas) {
-        // we will consider it a fail
         const firstElement = deltas[0];
-        const firstElementNonce = this.safeInteger (firstElement, 'microtimestamp');
+        const firstElementNonce = this.safeInteger (firstElement, 's');
         const nonce = this.safeInteger (orderbook, 'nonce');
         if (nonce < firstElementNonce) {
             return -1;
         }
         for (let i = 0; i < deltas.length; i++) {
             const delta = deltas[i];
-            const deltaNonce = this.safeInteger (delta, 'microtimestamp');
+            const deltaNonce = this.safeInteger (delta, 's');
             if (deltaNonce === nonce) {
                 return i + 1;
             }
@@ -182,18 +158,7 @@ export default class tradeogre extends tradeogreRest {
         await this.loadMarkets ();
         const market = this.market (symbol);
         symbol = market['symbol'];
-        const url = this.urls['api']['ws'];
-        const request: Dict = {
-            'a': 'subscribe',
-            'e': 'trade',
-            't': market['id'],
-        };
-        const messageHash = 'trades' + ':' + symbol;
-        const trades = await this.watch (url, messageHash, this.extend (request, params), messageHash);
-        if (this.newUpdates) {
-            limit = trades.getLimit (symbol, limit);
-        }
-        return this.filterBySymbolSinceLimit (trades, symbol, since, limit, true);
+        return this.watchTradesForSymbols ([ symbol ], since, limit, params);
     }
 
     /**
@@ -223,9 +188,14 @@ export default class tradeogre extends tradeogreRest {
             'e': 'trade',
             't': '*',
         };
-        const massageHash = 'trades' + ':' + symbols.join (',');
+        const messageHashes = [];
+        for (let i = 0; i < symbols.length; i++) {
+            const symbol = symbols[i];
+            const messageHash = 'trades:' + symbol;
+            messageHashes.push (messageHash);
+        }
         const url = this.urls['api']['ws'];
-        const trades = await this.watch (url, massageHash, this.extend (request, params), 'trades:all');
+        const trades = await this.watchMultiple (url, messageHashes, this.deepExtend (request, params), [ 'trades' ]);
         if (this.newUpdates) {
             const first = this.safeDict (trades, 0);
             const tradeSymbol = this.safeString (first, 'symbol');
@@ -259,16 +229,8 @@ export default class tradeogre extends tradeogreRest {
         const cache = this.trades[symbol];
         const trade = this.parseWsTrade (data, market);
         cache.append (trade);
-        const messageHashes = this.findMessageHashes (client, 'trades:');
-        for (let i = 0; i < messageHashes.length; i++) {
-            const messageHash = messageHashes[i];
-            const parts = messageHash.split (':');
-            const symbolPart = this.safeString (parts, 1);
-            const symbols = symbolPart.split (',');
-            if (this.inArray (symbol, symbols)) {
-                client.resolve (trade, messageHash);
-            }
-        }
+        const messageHash = 'trades:' + symbol;
+        client.resolve (cache, messageHash);
     }
 
     parseWsTrade (trade, market = undefined) {
@@ -312,7 +274,7 @@ export default class tradeogre extends tradeogreRest {
 
     handleMessage (client: Client, message) {
         const methods: Dict = {
-            'orderbook': this.handleOrderBook,
+            'book': this.handleOrderBook,
             'trade': this.handleTrade,
         };
         const event = this.safeString (message, 'e');
