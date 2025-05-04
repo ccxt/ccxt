f7f74324590779cd5784c2286accbf7388029516
T. Todua <7117978+ttodua@users.noreply.github.com>
2025-05-04 17:52:37 +0400

    fix(bitmat): currency related things (#25871)
    * fix(bitmat): currency related things
    
    * -
    
    * fix(bitmart): type
    
    * -

--- ts/src/bitmart.ts
+++ ts/src/bitmart.ts
@@ -1209,6 +1209,7 @@ export default class bitmart extends Exchange {
         //                 {
         //                     "currency": "BTC",
         //                     "name": "Bitcoin",
+        //                     "recharge_minsize": '0.00000001',
         //                     "contract_address": null,
         //                     "network": "BTC",
         //                     "withdraw_enabled": true,
@@ -1230,7 +1231,8 @@ export default class bitmart extends Exchange {
             const fullId = this.safeString (currency, 'currency');
             let currencyId = fullId;
             let networkId = this.safeString (currency, 'network');
-            if (fullId.indexOf ('NFT') < 0) {
+            const isNtf = (fullId.indexOf ('NFT') >= 0);
+            if (!isNtf) {
                 const parts = fullId.split ('-');
                 currencyId = this.safeString (parts, 0);
                 const second = this.safeString (parts, 1);
@@ -1251,6 +1253,7 @@ export default class bitmart extends Exchange {
                     'withdraw': undefined,
                     'active': undefined,
                     'networks': {},
+                    'type': isNtf ? 'other' : 'crypto',
                 };
             }
             const networkCode = this.networkIdToCode (networkId);
