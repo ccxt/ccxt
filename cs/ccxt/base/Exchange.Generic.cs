afc311368f82fbe0c33e43e922b72d03c15c1a9a
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-05-06 18:14:22 +0100

    fix(cs): handle empty keys/values

--- cs/ccxt/base/Exchange.Generic.cs
+++ cs/ccxt/base/Exchange.Generic.cs
@@ -269,7 +269,10 @@ public partial class Exchange
                 var elem2 = (dict)elem;
                 if (elem2.ContainsKey((string)key2))
                 {
-                    outDict[elem2[(string)key2].ToString()] = elem2;
+                    var elem2Value = elem2[(string)key2];
+                    if (elem2Value == null)
+                        continue;
+                    outDict[elem2Value.ToString()] = elem2;
                 }
             }
             else if (elem.GetType() == typeof(List<string>) || elem.GetType() == typeof(List<object>))
@@ -280,7 +283,10 @@ public partial class Exchange
                     var elem2 = (List<string>)elem;
                     if (elem2.Count > 0)
                     {
-                        outDict[elem2[index].ToString()] = elem2;
+                        var elem2IndexValue = elem2[index];
+                        if (elem2IndexValue == null)
+                            continue;
+                        outDict[elem2IndexValue.ToString()] = elem2;
                     }
                 }
                 if (elem.GetType() == typeof(List<object>))
@@ -288,7 +294,10 @@ public partial class Exchange
                     var elem2 = (List<object>)elem;
                     if (elem2.Count > 0)
                     {
-                        outDict[elem2[index].ToString()] = elem2;
+                        var elem2IndexValue = elem2[index];
+                        if (elem2IndexValue == null)
+                            continue;
+                        outDict[elem2IndexValue.ToString()] = elem2;
                     }
                 }
 
@@ -309,6 +318,8 @@ public partial class Exchange
             if (elemDict.ContainsKey(key))
             {
                 var elem2 = (string)elemDict[key];
+                if (elem2 == null)
+                    continue;
                 if (outDict.ContainsKey(elem2))
                 {
                     var list2 = (List<object>)outDict[elem2];
