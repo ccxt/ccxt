437d57e9376d4db895dca203ffa9c7ff899ad668
T. Todua <7117978+ttodua@users.noreply.github.com>
2025-05-13 18:40:33 +0400

    fix(apex): fetchCurrencies reorg & field fixes (#25945)
    * fix(apex): fetchCurrencies reorg & field fixes
    
    * fix currency
    
    * field names
    
    * safeCurrencyStructure
    
    * skip non-functional/empty coins
    
    * undefined value
    
    * length

--- ts/src/apex.ts
+++ ts/src/apex.ts
@@ -496,11 +496,6 @@ export default class apex extends Exchange {
             const code = this.safeCurrencyCode (currencyId);
             const name = this.safeString (currency, 'displayName');
             const networks: Dict = {};
-            let minPrecision = undefined;
-            let minWithdrawFeeString = undefined;
-            let minWithdrawString = undefined;
-            let deposit = false;
-            let withdraw = false;
             for (let j = 0; j < chains.length; j++) {
                 const chain = chains[j];
                 const tokens = this.safeList (chain, 'tokens', []);
@@ -510,31 +505,22 @@ export default class apex extends Exchange {
                     if (tokenName === currencyId) {
                         const networkId = this.safeString (chain, 'chainId');
                         const networkCode = this.networkIdToCode (networkId);
-                        const precision = this.parseNumber (this.parsePrecision (this.safeString (currency, 'decimals')));
-                        minPrecision = (minPrecision === undefined) ? precision : Math.min (minPrecision, precision);
-                        const depositAllowed = !this.safeBool (chain, 'stopDeposit');
-                        deposit = (depositAllowed) ? depositAllowed : deposit;
-                        const withdrawAllowed = this.safeBool (token, 'withdrawEnable');
-                        withdraw = (withdrawAllowed) ? withdrawAllowed : withdraw;
-                        minWithdrawFeeString = this.safeString (token, 'minFee');
-                        minWithdrawString = this.safeString (token, 'minWithdraw');
-                        const minNetworkDepositString = this.safeString (chain, 'depositMin');
                         networks[networkCode] = {
                             'info': chain,
                             'id': networkId,
                             'network': networkCode,
-                            'active': depositAllowed && withdrawAllowed,
-                            'deposit': depositAllowed,
-                            'withdraw': withdrawAllowed,
-                            'fee': this.parseNumber (minWithdrawFeeString),
-                            'precision': precision,
+                            'active': undefined,
+                            'deposit': !this.safeBool (chain, 'depositDisable'),
+                            'withdraw': this.safeBool (token, 'withdrawEnable'),
+                            'fee': this.safeNumber (token, 'minFee'),
+                            'precision': this.parseNumber (this.parsePrecision (this.safeString (token, 'decimals'))),
                             'limits': {
                                 'withdraw': {
-                                    'min': this.parseNumber (minWithdrawString),
+                                    'min': this.safeNumber (token, 'minWithdraw'),
                                     'max': undefined,
                                 },
                                 'deposit': {
-                                    'min': this.parseNumber (minNetworkDepositString),
+                                    'min': this.safeNumber (chain, 'minDeposit'),
                                     'max': undefined,
                                 },
                             },
@@ -542,24 +528,28 @@ export default class apex extends Exchange {
                     }
                 }
             }
-            result[code] = {
+            const networkKeys = Object.keys (networks);
+            const networksLength = networkKeys.length;
+            const emptyChains = networksLength === 0; // non-functional coins
+            const valueForEmpty = emptyChains ? false : undefined;
+            result[code] = this.safeCurrencyStructure ({
                 'info': currency,
                 'code': code,
                 'id': currencyId,
                 'type': 'crypto',
                 'name': name,
-                'active': deposit && withdraw,
-                'deposit': deposit,
-                'withdraw': withdraw,
-                'fee': this.parseNumber (minWithdrawFeeString),
-                'precision': minPrecision,
+                'active': undefined,
+                'deposit': valueForEmpty,
+                'withdraw': valueForEmpty,
+                'fee': undefined,
+                'precision': undefined,
                 'limits': {
                     'amount': {
                         'min': undefined,
                         'max': undefined,
                     },
                     'withdraw': {
-                        'min': this.parseNumber (minWithdrawString),
+                        'min': undefined,
                         'max': undefined,
                     },
                     'deposit': {
@@ -568,7 +558,7 @@ export default class apex extends Exchange {
                     },
                 },
                 'networks': networks,
-            };
+            });
         }
         return result;
     }
