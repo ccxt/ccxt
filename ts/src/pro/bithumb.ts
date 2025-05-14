b66f9c3d3d1dd845e12eaa27397eb8349fe66871
t.t <7117978+ttodua@users.noreply.github.com>
2025-05-14 13:50:29 +0400

    fix(bithumb): ws timestamp for trades

--- ts/src/pro/bithumb.ts
+++ ts/src/pro/bithumb.ts
@@ -344,12 +344,14 @@ export default class bithumb extends bithumbRest {
         //
         const marketId = this.safeString (trade, 'symbol');
         const datetime = this.safeString (trade, 'contDtm');
+        // that date is not UTC iso8601, but exchange's local time, -9hr difference
+        const timestamp = this.parse8601 (datetime) - 32400000;
         const sideId = this.safeString (trade, 'buySellGb');
         return this.safeTrade ({
             'id': undefined,
             'info': trade,
-            'timestamp': this.parse8601 (datetime),
-            'datetime': datetime,
+            'timestamp': timestamp,
+            'datetime': this.iso8601 (timestamp),
             'symbol': this.safeSymbol (marketId, market, '_'),
             'order': undefined,
             'type': undefined,
