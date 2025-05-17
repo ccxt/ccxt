f9988f5630af5eb623419b2894627fbae01579c9
t.t <7117978+ttodua@users.noreply.github.com>
2025-05-17 20:49:30 +0400

    lint

--- ts/src/coinsph.ts
+++ ts/src/coinsph.ts
@@ -607,7 +607,7 @@ export default class coinsph extends Exchange {
         }
         return result;
     }
-    
+
     calculateRateLimiterCost (api, method, path, params, config = {}) {
         if (('noSymbol' in config) && !('symbol' in params)) {
             return config['noSymbol'];
