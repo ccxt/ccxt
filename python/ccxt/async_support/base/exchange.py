dd7cfdbb24ca14effde793d649827a7e01792c3e
Pablo <pablo@criadoperez.com>
2025-05-18 20:49:45 +0200

    first commit

--- python/ccxt/async_support/base/exchange.py
+++ python/ccxt/async_support/base/exchange.py
@@ -34,7 +34,7 @@ from ccxt.base.exchange import Exchange as BaseExchange, ArgumentsRequired
 # -----------------------------------------------------------------------------
 
 from ccxt.async_support.base.ws.functions import inflate, inflate64, gunzip
-from ccxt.async_support.base.ws.fast_client import FastClient
+from ccxt.async_support.base.ws.aiohttp_client import AiohttpClient
 from ccxt.async_support.base.ws.future import Future
 from ccxt.async_support.base.ws.order_book import OrderBook, IndexedOrderBook, CountedOrderBook
 
@@ -387,7 +387,7 @@ class Exchange(BaseExchange):
                 'throttle': Throttler(self.tokenBucket, self.asyncio_loop),
                 'asyncio_loop': self.asyncio_loop,
             }, ws_options)
-            self.clients[url] = FastClient(url, on_message, on_error, on_close, on_connected, options)
+            self.clients[url] = AiohttpClient(url, on_message, on_error, on_close, on_connected, options)
             self.clients[url].proxy = self.get_ws_proxy()
         return self.clients[url]
 
