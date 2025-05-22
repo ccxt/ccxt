f76ebd75aa3fe737c4438eb6e37a17ffd9bd908a
t.t <7117978+ttodua@users.noreply.github.com>
2025-05-22 19:53:20 +0400

    fiats add

--- ts/src/kucoin.ts
+++ ts/src/kucoin.ts
@@ -1395,7 +1395,7 @@ export default class kucoin extends Exchange {
         //
         const currenciesData = this.safeList (response, 'data', []);
         const brokenCurrencies = [ '00', 'OPEN_ERROR', 'HUF', 'BDT' ];
-        const abnormalFiats = [ 'KWD' ];
+        const abnormalFiats = [ 'KWD', 'IRR' ];
         const result: Dict = {};
         for (let i = 0; i < currenciesData.length; i++) {
             const entry = currenciesData[i];
