3d1bfc164c82401063a51d02e30f960c23f0b50d
T. Todua <7117978+ttodua@users.noreply.github.com>
2025-05-26 22:34:55 +0400

    Discard changes to examples/py/proxy-usage.py

--- examples/py/proxy-usage.py
+++ examples/py/proxy-usage.py
@@ -37,22 +37,22 @@ async def example_socks_proxy():
 
     await my_ex.close()
 
-if sys.platform == 'win32':
-	asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
 async def example_web_sockets():
     my_ex = ccxt.kucoin()
-    # my_ex.http_proxy = 'http://188.34.194.190:8911'  # even though you are using WebSockets, you might also need to set up proxy for the exchange's REST requests
-    my_ex.wsSocksProxy = 'socks5://127.0.0.1:1080'  # "wsProxy" or "wssProxy" or "wsSocksProxy" (depending on your proxy protocol)
+    my_ex.http_proxy = 'http://188.34.194.190:8911'  # even though you are using WebSockets, you might also need to set up proxy for the exchange's REST requests
+    my_ex.ws_proxy = 'http://188.34.194.190:8911'  # "wsProxy" or "wssProxy" or "wsSocksProxy" (depending on your proxy protocol)
     await my_ex.load_markets()
     #
     # To ensure your proxy works, uncomment below code and watch the log
     #
-    my_ex.verbose = True
-    await my_ex.watch ('ws://188.34.194.190:9876/', 'myip') # in the incoming logs, confirm that you  see the proxy IP in "hello" message
-    
+    # myEx.verbose = true;
+    # await myEx.loadHttpProxyAgent ();
+    # await myEx.watch ('ws://188.34.194.190:9876/'); # in the incoming logs, confirm that you  see the proxy IP in "hello" message
+    #
     print(await my_ex.watch_ticker('BTC/USDT'))
     await my_ex.close()
 
 
-asyncio.run(example_web_sockets())
+    await my_ex.close()
+
+asyncio.run(example_proxy_url())
