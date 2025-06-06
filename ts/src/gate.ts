bd1687ec19c6a6d2d5872cb7179b62734737b1fa
t.t <7117978+ttodua@users.noreply.github.com>
2025-06-06 23:55:13 +0400

    fix(gate): parse markets spot & swap 'created' timestamps

--- ts/src/gate.ts
+++ ts/src/gate.ts
@@ -1261,17 +1261,21 @@ export default class gate extends Exchange {
         //         {
         //             "id": "QTUM_ETH",
         //             "base": "QTUM",
+        //             "base_name": "Quantum",
         //             "quote": "ETH",
+        //             "quote_name": "Ethereum",
         //             "fee": "0.2",
         //             "min_base_amount": "0.01",
         //             "min_quote_amount": "0.001",
+        //             "max_quote_amount": "50000",
         //             "amount_precision": 3,
         //             "precision": 6,
         //             "trade_status": "tradable",
-        //             "sell_start": 0,
-        //             "buy_start": 0
+        //             "sell_start": 1607313600,
+        //             "buy_start": 1700492400,
+        //             "type": "normal",
+        //             "trade_url": "https://www.gate.io/trade/QTUM_ETH",
         //         }
-        //     ]
         //
         //  Margin
         //
@@ -1351,7 +1355,7 @@ export default class gate extends Exchange {
                         'max': margin ? this.safeNumber (market, 'max_quote_amount') : undefined,
                     },
                 },
-                'created': undefined,
+                'created': this.safeIntegerProduct (spotMarket, 'buy_start', 1000), // buy_start is the trading start time, while sell_start is offline orders start time
                 'info': market,
             });
         }
@@ -1423,6 +1427,7 @@ export default class gate extends Exchange {
         //        "funding_next_apply": 1610035200,
         //        "short_users": 977,
         //        "config_change_time": 1609899548,
+        //        "create_time": 1609800048,
         //        "trade_size": 28530850594,
         //        "position_size": 5223816,
         //        "long_users": 455,
@@ -1555,7 +1560,7 @@ export default class gate extends Exchange {
                     'max': undefined,
                 },
             },
-            'created': undefined,
+            'created': this.safeIntegerProduct (market, 'create_time', 1000),
             'info': market,
         };
     }
