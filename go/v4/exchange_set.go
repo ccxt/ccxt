b0d2b5cb411db1d8f1d754fe05831e68d8e62505
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-06-16 15:34:49 +0100

    feat(go): make options concurrent safe (#26219)
    * feat(go): make options concurrent safe
    
    * use safeDict
    
    * init options
    
    * fix linting
    
    * init Options
    
    * rm lock

--- go/v4/exchange_set.go
+++ go/v4/exchange_set.go
@@ -55,7 +55,7 @@ func (this *Exchange) GetHas() map[string]interface{} {
 }
 
 func (this *Exchange) GetOptions() map[string]interface{} {
-	return this.Options
+	return this.SyncMapToMap(this.Options)
 }
 
 func (this *Exchange) GetHostname() string {
@@ -129,7 +129,7 @@ func (this *Exchange) SetAccounts(accounts interface{}) {
 }
 
 func (this *Exchange) SetOptions(options interface{}) {
-	this.Options = options.(map[string]interface{})
+	this.Options = this.MapToSyncMap(options.(map[string]interface{}))
 }
 
 func (this *Exchange) SetWssProxy(wssProxy interface{}) {
