4d29e475ba2da62ae999d56d7ffb49ceca2a803e
caoilainnl <caoilainn.lynch@proton.me>
2025-06-21 20:44:04 -0600

    async swift

--- swift-wrapper/Package.swift
+++ swift-wrapper/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "CCXTSwift",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v13),
         .macOS(.v10_15)
     ],
     products: [
