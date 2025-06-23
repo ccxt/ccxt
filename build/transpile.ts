e0b4a0f51a0b7b04f6883c6bfca9cc6a150e02bc
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-06-23 08:59:23 +0100

    Revert "fix php build"
    This reverts commit 16ef27c369bfca762e3b40aba90b4b9e79309184.

--- build/transpile.ts
+++ build/transpile.ts
@@ -3024,15 +3024,15 @@ class Transpiler {
         }
 
         // const classes = this.transpileDerivedExchangeFiles (tsFolder, options, pattern, force)
-        // const classes = this.transpileDerivedExchangeFiles (tsFolder, options, '.ts', force, (child || !!exchanges.length))
+        const classes = this.transpileDerivedExchangeFiles (tsFolder, options, '.ts', force, (child || !!exchanges.length))
 
-        // if (classes === null) {
-        //     log.bright.yellow ('0 files transpiled.')
-        //     return;
-        // }
-        // if (child) {
-        //     return
-        // }
+        if (classes === null) {
+            log.bright.yellow ('0 files transpiled.')
+            return;
+        }
+        if (child) {
+            return
+        }
 
         if (!transpilingSingleExchange) {
             this.transpileBaseMethods ()
