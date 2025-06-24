481eaa0a905993f26961aec92fd2e58cf02cec8f
Carlo Revelli <carlo.revelli@berkeley.edu>
2025-06-24 21:22:58 +0400

    Merge clients (#26294)
    * merge aiohttp client and client.py into the same file
    
    * edit reference
    
    * edit

--- python/ccxt/async_support/base/exchange.py
+++ python/ccxt/async_support/base/exchange.py
@@ -34,7 +34,7 @@ from ccxt.base.exchange import Exchange as BaseExchange, ArgumentsRequired
 # -----------------------------------------------------------------------------
 
 from ccxt.async_support.base.ws.functions import inflate, inflate64, gunzip
-from ccxt.async_support.base.ws.aiohttp_client import AiohttpClient
+from ccxt.async_support.base.ws.client import Client
 from ccxt.async_support.base.ws.future import Future
 from ccxt.async_support.base.ws.order_book import OrderBook, IndexedOrderBook, CountedOrderBook
 
@@ -419,7 +419,7 @@ class Exchange(BaseExchange):
             }, ws_options)
             # we use aiohttp instead of fastClient now because of this
             # https://github.com/ccxt/ccxt/pull/25995
-            self.clients[url] = AiohttpClient(url, on_message, on_error, on_close, on_connected, options)
+            self.clients[url] = Client(url, on_message, on_error, on_close, on_connected, options)
             # set http/s proxy (socks proxy should be set in other place)
             httpProxy, httpsProxy, socksProxy = self.check_ws_proxy_settings()
             if (httpProxy or httpsProxy):
