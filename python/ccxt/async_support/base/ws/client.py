6d61501e0ae33524bfd427fb0aa3e20830e3dfa2
Carlo Revelli <carlo.revelli@berkeley.edu>
2025-06-24 14:08:58 +0400

    minor edit

--- python/ccxt/async_support/base/ws/client.py
+++ python/ccxt/async_support/base/ws/client.py
@@ -187,8 +187,7 @@ class Client(object):
         if not self.error:
             self.reject(NetworkError('Connection closed by remote server, closing code ' + str(code)))
         self.on_close_callback(self, code)
-        if not self.closed():
-            ensure_future(self.aiohttp_close(code), loop=self.asyncio_loop)
+        ensure_future(self.aiohttp_close(), loop=self.asyncio_loop)
     
     def aiohttp_close(self):
         raise NotSupported('aiohttp_close() not implemented')
