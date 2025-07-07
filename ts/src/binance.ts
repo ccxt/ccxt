ec274b93cf6aa004c5fc886045277e4ce9579a3a
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-07-07 14:47:59 +0100

    fix(binance): fetchSettlementHistory symbol not required

--- ts/src/binance.ts
+++ ts/src/binance.ts
@@ -11354,6 +11354,7 @@ export default class binance extends Exchange {
         const request: Dict = {};
         if (symbol !== undefined) {
             request['symbol'] = market['id'];
+            symbol = market['symbol'];
         }
         if (since !== undefined) {
             request['startTime'] = since;
@@ -11384,7 +11385,7 @@ export default class binance extends Exchange {
         //
         const settlements = this.parseSettlements (response, market);
         const sorted = this.sortBy (settlements, 'timestamp');
-        return this.filterBySymbolSinceLimit (sorted, market['symbol'], since, limit);
+        return this.filterBySymbolSinceLimit (sorted, symbol, since, limit);
     }
 
     parseSettlement (settlement, market) {
