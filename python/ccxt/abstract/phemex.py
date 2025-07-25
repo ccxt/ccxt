41fe4fb25e04573e2935e21717a207f1e9f02be6
Mario Arranz <mario.arranz@deep-impact.ch>
2025-07-25 15:24:06 +0200

    Add USDC support, add new endpoint PUT /g-orders/create

--- python/ccxt/abstract/phemex.py
+++ python/ccxt/abstract/phemex.py
@@ -101,6 +101,7 @@ class ImplicitAPI:
     private_put_spot_orders_create = privatePutSpotOrdersCreate = Entry('spot/orders/create', 'private', 'PUT', {'cost': 1})
     private_put_spot_orders = privatePutSpotOrders = Entry('spot/orders', 'private', 'PUT', {'cost': 1})
     private_put_orders_replace = privatePutOrdersReplace = Entry('orders/replace', 'private', 'PUT', {'cost': 1})
+    private_put_g_orders_create = privatePutGOrdersCreate = Entry('g-orders/create', 'private', 'PUT', {'cost': 1})
     private_put_g_orders_replace = privatePutGOrdersReplace = Entry('g-orders/replace', 'private', 'PUT', {'cost': 1})
     private_put_positions_leverage = privatePutPositionsLeverage = Entry('positions/leverage', 'private', 'PUT', {'cost': 5})
     private_put_g_positions_leverage = privatePutGPositionsLeverage = Entry('g-positions/leverage', 'private', 'PUT', {'cost': 5})
