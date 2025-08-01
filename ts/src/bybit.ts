ccxt/ccxt

commit f5b274d51bc38a653f3861eac85cce43f7017d37

feat(bitget): use "recent" endpoints for mark/index candles (#26537)
* feat(bitget): use "recent" endpoints for mark/index candles

* ohlcv tests and fix default limit

---------

Co-authored-by: carlosmiei <43336371+carlosmiei@users.noreply.github.com>

ts/src/bybit.ts
@@ -5093,7 +5093,7 @@ export default class bybit extends Exchange {
      * @method
      * @name bybit#fetchClosedOrder
      * @description fetches information on a closed order made by the user
-     * @see https://bybit-exchange.github.io/docs/v5/order/order-list
+     * @see https://bybinpt-exchange.github.io/docs/v5/order/order-list
      * @param {string} id order id
      * @param {string} [symbol] unified symbol of the market the order was made in
      * @param {object} [params] extra parameters specific to the exchange API endpoint

ts/src/bitget.ts
@@ -3905,6 +3905,10 @@ export default class bitget extends Exchange {
             if (historicalEndpointNeeded) {
                 response = await this.publicSpotGetV2SpotMarketHistoryCandles (this.extend (request, params));
             } else {
+                if (!limitDefined) {
+                    request['limit'] = 1000;
+                    limit = 1000;
+                }
                 response = await this.publicSpotGetV2SpotMarketCandles (this.extend (request, params));
             }
         } else {
@@ -3914,8 +3918,15 @@ export default class bitget extends Exchange {
             [ productType, params ] = this.handleProductTypeAndParams (market, params);
             request['productType'] = productType;
             const extended = this.extend (request, params);
-            // todo: mark & index also have their "recent" endpoints, but not priority now.
-            if (priceType === 'mark') {
+            if (!historicalEndpointNeeded && (priceType === 'mark' || priceType === 'index')) {
+                if (!limitDefined) {
+                    extended['limit'] = 1000;
+                    limit = 1000;
+                }
+                // Recent endpoint for mark/index prices
+                // https://www.bitget.com/api-doc/contract/market/Get-Candle-Data
+                response = await this.publicMixGetV2MixMarketCandles (this.extend ({ 'kLineType': priceType }, extended));
+            } else if (priceType === 'mark') {
                 response = await this.publicMixGetV2MixMarketHistoryMarkCandles (extended);
             } else if (priceType === 'index') {
                 response = await this.publicMixGetV2MixMarketHistoryIndexCandles (extended);
@@ -3923,6 +3934,10 @@ export default class bitget extends Exchange {
                 if (historicalEndpointNeeded) {
                     response = await this.publicMixGetV2MixMarketHistoryCandles (extended);
                 } else {
+                    if (!limitDefined) {
+                        extended['limit'] = 1000;
+                        limit = 1000;
+                    }
                     response = await this.publicMixGetV2MixMarketCandles (extended);
                 }
             }

commit 180f99372828a274cca6a3d66dad57b88643eb93

chore: revert accidental bybit comment typo
(Introduced in #26537)

ts/src/bybit.ts
@@ -5093,7 +5093,7 @@ export default class bybit extends Exchange {
      * @method
      * @name bybit#fetchClosedOrder
      * @description fetches information on a closed order made by the user
-     * @see https://bybinpt-exchange.github.io/docs/v5/order/order-list
+     * @see https://bybit-exchange.github.io/docs/v5/order/order-list
      * @param {string} id order id
      * @param {string} [symbol] unified symbol of the market the order was made in
      * @param {object} [params] extra parameters specific to the exchange API endpoint