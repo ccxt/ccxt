dadadd6cbaf559435cdfea00cb398405b31580c5
profyverya <39007853+profyverya@users.noreply.github.com>
2025-08-09 06:31:32 +0300

    Gate: unified networks

--- ts/src/gate.ts
+++ ts/src/gate.ts
@@ -672,7 +672,8 @@ export default class gate extends Exchange {
                     'ADA': 'ADA', // CARDANO
                     'AVAXC': 'AVAX_C',
                     'NEAR': 'NEAR',
-                    'ARBONE': 'ARBEVM',
+                    'ARBITRUM': 'ARBEVM',
+                    'OPTIMISM': 'OP',
                     'BASE': 'BASEEVM',
                     'SUI': 'SUI',
                     'CRONOS': 'CRO',
@@ -686,6 +687,12 @@ export default class gate extends Exchange {
                     // SEI: ['SEI', 'SEIEVM']
                     'LINEA': 'LINEAETH',
                     'BLAST': 'BLASTETH',
+                    'MODE': 'MODEETH',
+                    'MANTA': 'MANTAETH',
+                    'MTL': 'MTLETH',
+                    'KAVA': 'KAVAEVM',
+                    'KLAY': 'KAIA',
+                    'SONIC': 'S',
                     'XLM': 'XLM',
                     'RSK': 'RBTC',
                     'TON': 'TON',
@@ -694,7 +701,7 @@ export default class gate extends Exchange {
                     'CELO': 'CELO',
                     'HBAR': 'HBAR',
                     // 'FTM': SONIC REBRAND, todo
-                    'ZKSERA': 'ZKSERA',
+                    'ZKSYNC': 'ZKSERA',
                     'KLAY': 'KLAY',
                     'EOS': 'EOS',
                     'ACA': 'ACA',
@@ -710,7 +717,7 @@ export default class gate extends Exchange {
                     'GATECHAIN': 'GTEVM',
                     'KUSAMA': 'KSMSM',
                     'OKC': 'OKT',
-                    'POLKADOT': 'DOTSM', // todo: DOT for main DOT
+                    'DOT': 'DOTSM', // todo: DOT for main DOT
                     'LUNA': 'LUNC',
                 },
                 'networksById': {
