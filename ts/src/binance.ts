0cfb4cb1f6b722402e189558e64266d36498c944
pablo <pablo@criadoperez.com>
2025-08-11 02:41:42 +0200

    add missing file

--- ts/src/binance.ts
+++ ts/src/binance.ts
@@ -11941,7 +11941,7 @@ export default class binance extends Exchange {
         return {};
     }
 
-    handleErrors (code: int, reason: string, url: string, method: string, headers: Dict, body: string, response, requestHeaders, requestBody) {
+    handleErrors (code: Int, reason: Str, url: Str, method: Str, headers: Dict, body: Str, response, requestHeaders, requestBody) {
         if ((code === 418) || (code === 429)) {
             throw new DDoSProtection (this.id + ' ' + code.toString () + ' ' + reason + ' ' + body);
         }
