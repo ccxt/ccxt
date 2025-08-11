b3411069143f29bff0c90de6321ab81fd1578058
rayBastard <roman@president-kids.ru>
2025-08-11 20:08:08 +0900

    withdraw updated

--- ts/src/backpack.ts
+++ ts/src/backpack.ts
@@ -1366,7 +1366,7 @@ export default class backpack extends Exchange {
      * @param {object} [params] extra parameters specific to the exchange API endpoint
      * @returns {object} a [transaction structure]{@link https://docs.ccxt.com/#/?id=transaction-structure}
      */
-    async withdraw (code: string, amount: number, address: string, tag = undefined, params = {}): Promise<Transaction> {
+    async withdraw (code: string, amount: number, address: string, tag: Str = undefined, params = {}): Promise<Transaction> {
         await this.loadMarkets ();
         const currency = this.currency (code);
         const request: Dict = {
