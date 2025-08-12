717ea2c6a8217f54b66b8363c81215f46f533cfc
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-08-12 15:55:45 +0100

    feat(mexc):  protobuf support  (#26606)
    * feat(mexc): init protobuf support [WIP]
    
    * add ts definitions
    
    * manually tweak python imports
    
    * watchOHLCV working (typescript)
    
    * watchTrades working
    
    * orderbook
    
    * some fixes
    
    * dynamically import
    
    * add c# bindings
    
    * trade fix
    
    * throw is not found
    
    * c# version working
    
    * fix watchTrades and module import
    
    * add protobuf as dev-dep to run on actions
    
    * add protobuf to ci-requirements
    
    * add php version (working as a regular dep)
    
    * add classMap
    
    * make it optional
    
    * fix bundle-cjs issue
    
    * fix webpack
    
    * add pecl option
    
    * fix php action
    
    * read decompressBinary option
    
    * exclude protobuf from ruff
    
    * mexc: update errors
    
    * tribber build
    
    * bump ast-transpiler
    
    * upgrade ast-transpiler
    
    * fix watchBalance and orders
    
    * update docs
    
    ---------
    
    Co-authored-by: sc0Vu <alk03073135@gmail.com>

--- python/ccxt/async_support/base/ws/client.py
+++ python/ccxt/async_support/base/ws/client.py
@@ -50,6 +50,7 @@ class Client(object):
     connecting = False
     asyncio_loop: BaseEventLoop = None
     ping_looper = None
+    decompressBinary = True  # decompress binary messages by default
 
     def __init__(self, url, on_message_callback, on_error_callback, on_close_callback, on_connected_callback, config={}):
         defaults = {
@@ -218,7 +219,8 @@ class Client(object):
         if self.verbose:
             self.log(iso8601(milliseconds()), 'message', data)
         if isinstance(data, bytes):
-            data = data.decode()
+            if self.decompressBinary:
+                data = data.decode()
         # decoded = json.loads(data) if is_json_encoded_object(data) else data
         decode = None
         if is_json_encoded_object(data):
