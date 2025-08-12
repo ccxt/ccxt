41b3c76f4a640696f485a780e438a801aef36348
Dan <83686770+Dan-krm@users.noreply.github.com>
2025-08-12 08:55:16 -0600

    feat(bitget): cancelOrder, add clientOrderId support (#26620)
    * feat(bitget): cancelOrder, added clientOrderId support
    
    Added `clientOrderId` support to bitget cancelOrder
    
    * test(bitget): static tests for cancelOrder clientOrderId
    
    * test(bitget): static tests, cancelOrder, trailing with clientOrderId
    
    ```
    tsx ts/cli bitget createOrder BTC/USDT:USDT market sell 0.0001 undefined '{"clientOrderId":"a1b2c3","hedged":true,"tralingPercent":"10","triggerPrice":100000,"reduceOnly":true}'
    ```
    ```
    tsx ts/cli bitget cancelOrder undefined BTC/USDT:USDT '{"clientOrderId":"a1b2c3","trailing":true}'
    ```
    
    * fix request tests
    
    * fix php tests
    
    * response tests
    
    * disable test
    
    ---------
    
    Co-authored-by: carlosmiei <43336371+carlosmiei@users.noreply.github.com>

--- ts/src/bitget.ts
+++ ts/src/bitget.ts
@@ -5783,6 +5783,7 @@ export default class bitget extends Exchange {
      * @param {string} [params.planType] *swap only* either profit_plan, loss_plan, normal_plan, pos_profit, pos_loss, moving_plan or track_plan
      * @param {boolean} [params.trailing] set to true if you want to cancel a trailing order
      * @param {boolean} [params.uta] set to true for the unified trading account (uta), defaults to false
+     * @param {string} [params.clientOrderId] the clientOrderId of the order, id does not need to be provided if clientOrderId is provided
      * @returns {object} An [order structure]{@link https://docs.ccxt.com/#/?id=order-structure}
      */
     async cancelOrder (id: string, symbol: Str = undefined, params = {}) {
@@ -5801,13 +5802,32 @@ export default class bitget extends Exchange {
         if (!(market['spot'] && trigger)) {
             request['symbol'] = market['id'];
         }
-        if (!((market['swap'] || market['future']) && trigger)) {
-            request['orderId'] = id;
-        }
         let uta = undefined;
         [ uta, params ] = this.handleOptionAndParams (params, 'cancelOrder', 'uta', false);
+        const isPlanOrder = trigger || trailing;
+        const isContract = market['swap'] || market['future'];
+        const isContractTriggerEndpoint = isContract && isPlanOrder && !uta;
+        const clientOrderId = this.safeString2 (params, 'clientOrderId', 'clientOid');
+        if (isContractTriggerEndpoint) {
+            const orderIdList = [];
+            const orderId: Dict = {};
+            if (clientOrderId !== undefined) {
+                params = this.omit (params, 'clientOrderId');
+                orderId['clientOid'] = clientOrderId;
+            } else {
+                orderId['orderId'] = id;
+            }
+            orderIdList.push (orderId);
+            request['orderIdList'] = orderIdList;
+        } else {
+            if (clientOrderId !== undefined) {
+                params = this.omit (params, 'clientOrderId');
+                request['clientOid'] = clientOrderId;
+            } else {
+                request['orderId'] = id;
+            }
+        }
         if (uta) {
-            request['orderId'] = id;
             if (trigger) {
                 response = await this.privateUtaPostV3TradeCancelStrategyOrder (this.extend (request, params));
             } else {
@@ -5817,14 +5837,6 @@ export default class bitget extends Exchange {
             let productType = undefined;
             [ productType, params ] = this.handleProductTypeAndParams (market, params);
             request['productType'] = productType;
-            if (trigger || trailing) {
-                const orderIdList = [];
-                const orderId: Dict = {
-                    'orderId': id,
-                };
-                orderIdList.push (orderId);
-                request['orderIdList'] = orderIdList;
-            }
             if (trailing) {
                 const planType = this.safeString (params, 'planType', 'track_plan');
                 request['planType'] = planType;
@@ -5903,7 +5915,7 @@ export default class bitget extends Exchange {
         //
         const data = this.safeValue (response, 'data', {});
         let order = undefined;
-        if ((market['swap'] || market['future']) && trigger && !uta) {
+        if (isContractTriggerEndpoint) {
             const orderInfo = this.safeValue (data, 'successList', []);
             order = orderInfo[0];
         } else {
