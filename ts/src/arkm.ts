6246cfd9eb4c286c5d05e78ac736befa5298e0de
t.t <7117978+ttodua@users.noreply.github.com>
2025-08-15 20:21:21 +0400

    lint

--- ts/src/arkm.ts
+++ ts/src/arkm.ts
@@ -2015,7 +2015,7 @@ export default class arkm extends Exchange {
      * @param {object} [params] extra parameters specific to the exchange API endpoint
      * @returns {object} response from the exchange
      */
-    async setLeverage (leverage: Int, symbol: Str = undefined, params = {}) {
+    async setLeverage (leverage: int, symbol: Str = undefined, params = {}) {
         if (symbol === undefined) {
             throw new ArgumentsRequired (this.id + ' setLeverage() requires a symbol argument');
         }
@@ -2153,7 +2153,7 @@ export default class arkm extends Exchange {
      * @param {object} [params] extra parameters specific to the exchange API endpoint
      * @returns {object} a [transaction structure]{@link https://docs.ccxt.com/#/?id=transaction-structure}
      */
-    async withdraw (code: string, amount: number, address: string, tag = undefined, params = {}): Promise<Transaction> {
+    async withdraw (code: string, amount: number, address: string, tag: Str = undefined, params = {}): Promise<Transaction> {
         [ tag, params ] = this.handleWithdrawTagAndParams (tag, params);
         await this.loadMarkets ();
         const withdrawalAddresses = await this.v1PrivateGetAccountWithdrawalAddresses ();
