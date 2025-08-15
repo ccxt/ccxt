aafe33cbdcd579a3c1bc260199dcce6fa4d6a404
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-08-15 11:45:51 +0100

    add watchX support to CLI methods

---
+++ go/v4/ws/exchange_metadata.go
@@ -0,0 +1,5 @@
+package ccxtws
+
+var Version string = "4.5.0"
+
+var Exchanges []string = []string{ "Alpaca", "Apex", "Ascendex", "Bequant", "Binance", "Binancecoinm", "Binanceus", "Binanceusdm", "Bingx", "Bitfinex", "Bitget", "Bithumb", "Bitmart", "Bitmex", "Bitopro", "Bitrue", "Bitstamp", "Bittrade", "Bitvavo", "Blockchaincom", "Blofin", "Bybit", "Cex", "Coinbase", "Coinbaseadvanced", "Coinbaseexchange", "Coinbaseinternational", "Coincatch", "Coincheck", "Coinex", "Coinone", "Cryptocom", "Defx", "Deribit", "Derive", "Exmo", "Gate", "Gateio", "Gemini", "Hashkey", "Hitbtc", "Hollaex", "Htx", "Huobi", "Hyperliquid", "Independentreserve", "Kraken", "Krakenfutures", "Kucoin", "Kucoinfutures", "Lbank", "Luno", "Mexc", "Modetrade", "Myokx", "Ndax", "Okcoin", "Okx", "Okxus", "Onetrading", "Oxfun", "P2b", "Paradex", "Phemex", "Poloniex", "Probit", "Tradeogre", "Upbit", "Vertex", "Whitebit", "Woo", "Woofipro", "Xt" }
