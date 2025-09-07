8d1398b0c4b6f44a5ed1effd14e0f542dc10dd89
t.t <7117978+ttodua@users.noreply.github.com>
2025-09-07 19:49:23 +0400

    test(python): fix path

--- python/ccxt/test/tests_helpers.py
+++ python/ccxt/test/tests_helpers.py
@@ -235,7 +235,7 @@ def get_test_files_sync(properties, ws=False):
         module_string = 'ccxt.test.exchange.' + prefix + '.test_' + name_snake_case
         if (ws):
             prefix = 'pro'
-            dir_to_test = DIR_NAME + '/../' + prefix + '/test/exchange/'
+            dir_to_test = DIR_NAME + '/../' + prefix + '/test/Exchange/'
             module_string = 'ccxt.pro.test.exchange.test_' + name_snake_case
         filePathWithExt = dir_to_test + 'test_' + name_snake_case + '.py'
         if (io_file_exists (filePathWithExt)):
