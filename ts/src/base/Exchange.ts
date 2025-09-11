c27f5ef0b43f61a7b950d4143670bdbccd2115ed
T. Todua <7117978+ttodua@users.noreply.github.com>
2025-09-11 14:47:21 +0400

    fix(handleTriggerPrices): quick (#26806)
    fix(handleTriggerPrices): ss2

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -6874,7 +6874,7 @@ export default class Exchange {
 
     handleTriggerPricesAndParams (symbol, params, omitParams = true) {
         //
-        const triggerPrice = this.safeString (params, 'triggerPrice', 'stopPrice');
+        const triggerPrice = this.safeString2 (params, 'triggerPrice', 'stopPrice');
         let triggerPriceStr: Str = undefined;
         const stopLossPrice = this.safeString (params, 'stopLossPrice');
         let stopLossPriceStr: Str = undefined;
