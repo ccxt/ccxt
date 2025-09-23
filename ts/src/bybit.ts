a22ef521cff2654668ec17f7afb3a18d0cd3c2f0
t.t <7117978+ttodua@users.noreply.github.com>
2025-09-23 22:16:56 +0400

    other exch priv fetchCurrencies

--- ts/src/bybit.ts
+++ ts/src/bybit.ts
@@ -1241,6 +1241,9 @@ export default class bybit extends Exchange {
                 },
                 'spot': {
                     'extends': 'default',
+                    'fetchCurrencies': {
+                        'private': true,
+                    },
                     'createOrder': {
                         'triggerPriceType': undefined,
                         'triggerDirection': false,
