c4d7c7f3b315fac120c1ccdcdea861c0b2f2d8d8
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-09-24 17:48:05 +0100

    fix(kraken): withdraw address is optional

--- ts/src/kraken.ts
+++ ts/src/kraken.ts
@@ -3215,7 +3215,7 @@ export default class kraken extends Exchange {
      * @see https://docs.kraken.com/rest/#tag/Funding/operation/withdrawFunds
      * @param {string} code unified currency code
      * @param {float} amount the amount to withdraw
-     * @param {string} address the address to withdraw to
+     * @param {string} address the address to withdraw to, not required can be '' or undefined/none/null
      * @param {string} tag
      * @param {object} [params] extra parameters specific to the exchange API endpoint
      * @returns {object} a [transaction structure]{@link https://docs.ccxt.com/#/?id=transaction-structure}
@@ -3229,8 +3229,11 @@ export default class kraken extends Exchange {
             const request: Dict = {
                 'asset': currency['id'],
                 'amount': amount,
-                'address': address,
+                // 'address': address,
             };
+            if (address !== undefined && address !== '') {
+                request['address'] = address;
+            }
             const response = await this.privatePostWithdraw (this.extend (request, params));
             //
             //     {
