de5625bbbea2fde96faa59cc246cc9bab75465cf
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-10-07 09:24:17 -0500

    fix(coinex): watchOrderBook multiple subscriptions (#26982)

--- ts/src/pro/coinex.ts
+++ ts/src/pro/coinex.ts
@@ -734,13 +734,13 @@ export default class coinex extends coinexRest {
         let type = undefined;
         [ type, params ] = this.handleMarketTypeAndParams (callerMethodName, market, params);
         const url = this.urls['api']['ws'][type];
-        const subscriptionHashes = [ 'trades' ];
+        // const subscriptionHashes = [ 'trades' ];
         const subscribe: Dict = {
             'method': 'deals.subscribe',
             'params': { 'market_list': subscribedSymbols },
             'id': this.requestId (),
         };
-        const trades = await this.watchMultiple (url, messageHashes, this.deepExtend (subscribe, params), subscriptionHashes);
+        const trades = await this.watchMultiple (url, messageHashes, this.deepExtend (subscribe, params), messageHashes);
         if (this.newUpdates) {
             return trades;
         }
@@ -798,9 +798,9 @@ export default class coinex extends coinexRest {
             'params': { 'market_list': marketList },
             'id': this.requestId (),
         };
-        const subscriptionHashes = this.hash (this.encode (this.json (watchOrderBookSubscriptions)), sha256);
+        // const subscriptionHashes = this.hash (this.encode (this.json (watchOrderBookSubscriptions)), sha256);
         const url = this.urls['api']['ws'][type];
-        const orderbooks = await this.watchMultiple (url, messageHashes, this.deepExtend (subscribe, params), subscriptionHashes);
+        const orderbooks = await this.watchMultiple (url, messageHashes, this.deepExtend (subscribe, params), messageHashes);
         if (this.newUpdates) {
             return orderbooks;
         }
