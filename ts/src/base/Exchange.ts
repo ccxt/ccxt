c8b5295cee04d32de526e175a5774d32d62cef51
t.t <7117978+ttodua@users.noreply.github.com>
2025-10-07 22:43:32 +0400

    lint

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -166,11 +166,11 @@ const {
 // import { Market, Trade, Fee, Ticker, OHLCV, OHLCVC, Order, OrderBook, Balance, Balances, Dictionary, Transaction, Currency, MinMax, IndexType, Int, OrderType, OrderSide, Position, FundingRateHistory, OpenInterest, Liquidation, OrderRequest, FundingHistory, MarginMode, Tickers, Greeks, Str, Num, MarketInterface, CurrencyInterface, Account } from './types.js';
 export type { Market, Trade, Fee, Ticker, OHLCV, OHLCVC, Order, OrderBook, Balance, Balances, Dictionary, Transaction, Currency, MinMax, IndexType, Int, Bool, OrderType, OrderSide, Position, LedgerEntry, BorrowInterest, OpenInterest, LeverageTier, TransferEntry, CrossBorrowRate, FundingRateHistory, Liquidation, FundingHistory, OrderRequest, MarginMode, Tickers, Greeks, Option, OptionChain, Str, Num, MarketInterface, CurrencyInterface, BalanceAccount, MarginModes, MarketType, Leverage, Leverages, LastPrice, LastPrices, Account, Strings, Conversion, DepositAddress, LongShortRatio } from './types.js';
 // ----------------------------------------------------------------------------
-const dynamicImport = async (moduleName: string) => {
-  return await import(/* webpackIgnore: true */ moduleName)
-}
-
-
+//
+const dynamicImport = async (moduleName: string) => await import (/* webpackIgnore: true */ moduleName);
+//
+// ----------------------------------------------------------------------------
+//
 let protobufMexc = undefined;
 (async () => {
     try {
@@ -179,7 +179,6 @@ let protobufMexc = undefined;
         // TODO: handle error
     }
 }) ();
-
 
 // -----------------------------------------------------------------------------
 /**
