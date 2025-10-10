1ba8dec5a2e80dc96758935dfae589a449015f52
t.t <7117978+ttodua@users.noreply.github.com>
2025-10-10 14:33:34 +0400

    lint

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -3089,7 +3089,7 @@ export default class Exchange {
          * @description this method is a very deterministic to help users to know what feature is supported by the exchange
          * @param {string} [symbol] unified symbol
          * @param {string} [methodName] view currently supported methods: https://docs.ccxt.com/#/README?id=features
-         * @param {string} [paramName] unified param value, like: `triggerPrice`, `stopLoss.triggerPrice` (check docs for supported param names), 
+         * @param {string} [paramName] unified param value, like: `triggerPrice`, `stopLoss.triggerPrice` (check docs for supported param names)
          * @param {object} [defaultValue] return default value if no result found
          * @returns {object} returns feature value
          */
