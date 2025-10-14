767df745f739ce7a12d1a1b1046ba6b37162dde7
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-10-14 03:36:17 -0500

    fix(bitget): fetchPositions empty array (#27051)

--- ts/src/bitget.ts
+++ ts/src/bitget.ts
@@ -7853,7 +7853,10 @@ export default class bitget extends Exchange {
         let market = undefined;
         if (symbols !== undefined) {
             const first = this.safeString (symbols, 0);
-            market = this.market (first);
+            // symbols can be undefined or []
+            if (first !== undefined) {
+                market = this.market (first);
+            }
         }
         let productType = undefined;
         [ productType, params ] = this.handleProductTypeAndParams (market, params);
@@ -7867,7 +7870,7 @@ export default class bitget extends Exchange {
             response = await this.privateUtaGetV3PositionCurrentPosition (this.extend (request, params));
         } else if (method === 'privateMixGetV2MixPositionAllPosition') {
             let marginCoin = this.safeString (params, 'marginCoin', 'USDT');
-            if (symbols !== undefined) {
+            if (market !== undefined) {
                 marginCoin = market['settleId'];
             } else if (productType === 'USDT-FUTURES') {
                 marginCoin = 'USDT';
