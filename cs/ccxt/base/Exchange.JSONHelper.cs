d67919d6ae8d3533dff806d0b0ac8a7f722cc033
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-10-25 11:07:24 -0500

    fix(upbit): decompress binary messages in c# (#27136)
    * fix(upbit): decompress binary messages in c#
    
    * rm line carriage

--- cs/ccxt/base/Exchange.JSONHelper.cs
+++ cs/ccxt/base/Exchange.JSONHelper.cs
@@ -10,8 +10,9 @@ public partial class Exchange
 
     public static bool isValidJson(string json)
     {
-        if ((json.StartsWith("{") && json.EndsWith("}")) || json.EndsWith("}\n") || //For object
-            (json.StartsWith("[") && json.EndsWith("]")) || json.EndsWith("]\n")) //For array
+        json = json.Replace("\n", "").Replace("\r", "").Trim();
+        if ((json.StartsWith("{") && json.EndsWith("}")) || // For object
+            (json.StartsWith("[") && json.EndsWith("]"))) //For array
         {
             return true;
         }
