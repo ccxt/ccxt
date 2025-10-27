39cbe4fe5a82a2744598b545eb56a699cd7b3f52
caoilainnl <caoilainn.lynch@proton.me>
2025-10-26 21:56:31 -0600

    base exchange return types

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -8275,7 +8275,7 @@ export default class Exchange {
         throw new NotSupported (this.id + ' watchMarkPrices () is not supported yet');
     }
 
-    async withdrawWs (code: string, amount: number, address: string, tag: Str = undefined, params = {}) {
+    async withdrawWs (code: string, amount: number, address: string, tag: Str = undefined, params = {}): Promise<Transaction> {
         /**
          * @method
          * @name exchange#withdrawWs
@@ -8314,12 +8314,14 @@ export default class Exchange {
         throw new NotSupported (this.id + ' createOrdersWs () is not supported yet');
     }
 
-    async fetchOrdersByStatusWs (status: string, symbol: Str = undefined, since: Int = undefined, limit: Int = undefined, params = {}) {
+    async fetchOrdersByStatusWs (status: string, symbol: Str = undefined, since: Int = undefined, limit: Int = undefined, params = {}): Promise<Order[]> {
         /**
          * @method
          * @name exchange#fetchOrdersByStatusWs
          * @description watches information on open orders with bid (buy) and ask (sell) prices, volumes and other data
-         * @param {string} symbol unified symbol of the market to fetch the order book for
+         * @param {string} status requested order status
+         * @param {string} [symbol] unified symbol of the market to fetch the order book for
+         * @param {int} [since] the earliest time in ms to fetch orders for
          * @param {int} [limit] the maximum amount of order book entries to return
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object} A dictionary of [order book structures]{@link https://docs.ccxt.com/#/?id=order-book-structure} indexed by market symbols
