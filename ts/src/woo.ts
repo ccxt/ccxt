27fd0f125d24f18be36dd80978a55532c14a7b05
glenn-woo <glenn.ku@woo.network>
2025-11-04 23:28:51 +0800

    feat(woo): update transfer from v1 API to v3 API (#27172)
    * feat(woo): update transfer from v1 API to v3 API
    
    * fix static test
    
    ---------
    
    Co-authored-by: carlosmiei <43336371+carlosmiei@users.noreply.github.com>

--- ts/src/woo.ts
+++ ts/src/woo.ts
@@ -211,7 +211,6 @@ export default class woo extends Exchange {
                         'post': {
                             'order': 1, // 10 requests per 1 second per symbol
                             'order/cancel_all_after': 1,
-                            'asset/main_sub_transfer': 30, // 20 requests per 60 seconds
                             'asset/ltv': 30,
                             'asset/withdraw': 30,  // implemented in ccxt, disabled on the exchange side https://docx.woo.io/wootrade-documents/#token-withdraw
                             'asset/internal_withdraw': 30,
@@ -2832,17 +2831,25 @@ export default class woo extends Exchange {
         const request: Dict = {
             'token': currency['id'],
             'amount': this.parseToNumeric (amount),
-            'from_application_id': fromAccount,
-            'to_application_id': toAccount,
+            'from': {
+                'applicationId': fromAccount,
+            },
+            'to': {
+                'applicationId': toAccount,
+            },
         };
-        const response = await this.v1PrivatePostAssetMainSubTransfer (this.extend (request, params));
+        const response = await this.v3PrivatePostAssetTransfer (this.extend (request, params));
         //
         //     {
         //         "success": true,
         //         "id": 200
         //     }
         //
-        const transfer = this.parseTransfer (response, currency);
+        const data = this.safeDict (response, 'data', {});
+        data['timestamp'] = this.safeInteger (response, 'timestamp');
+        data['token'] = currency['id'];
+        data['status'] = 'ok';
+        const transfer = this.parseTransfer (data, currency);
         const transferOptions = this.safeDict (this.options, 'transfer', {});
         const fillResponseFromRequest = this.safeBool (transferOptions, 'fillResponseFromRequest', true);
         if (fillResponseFromRequest) {
@@ -2958,7 +2965,7 @@ export default class woo extends Exchange {
         //        }
         //
         const code = this.safeCurrencyCode (this.safeString (transfer, 'token'), currency);
-        const timestamp = this.safeTimestamp (transfer, 'createdTime');
+        const timestamp = this.safeTimestamp2 (transfer, 'createdTime', 'timestamp');
         const success = this.safeBool (transfer, 'success');
         let status: Str = undefined;
         if (success !== undefined) {
