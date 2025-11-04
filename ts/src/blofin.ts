97a7a3cba4eb7e8cb258f26dc8aff75792e4a41b
Hoang Nguyen <minh.hoang1297@gmail.com>
2025-11-04 11:22:58 -0800

    Fix blofin create order tpsl response

--- ts/src/blofin.ts
+++ ts/src/blofin.ts
@@ -1405,27 +1405,29 @@ export default class blofin extends Exchange {
         [ method, params ] = this.handleOptionAndParams (params, 'createOrder', 'method', 'privatePostTradeOrder');
         const isStopLossPriceDefined = this.safeString (params, 'stopLossPrice') !== undefined;
         const isTakeProfitPriceDefined = this.safeString (params, 'takeProfitPrice') !== undefined;
-        const isTriggerOrder = this.safeString (params, 'triggerPrice') !== undefined;
+        const hasTriggerPrice = this.safeString (params, 'triggerPrice') !== undefined;
         const isType2Order = (isStopLossPriceDefined || isTakeProfitPriceDefined);
         let response = undefined;
         const reduceOnly = this.safeBool (params, 'reduceOnly');
         if (reduceOnly !== undefined) {
             params['reduceOnly'] = reduceOnly ? 'true' : 'false';
         }
-        if (tpsl || (method === 'privatePostTradeOrderTpsl') || isType2Order) {
+        const isTpslOrder = tpsl || (method === 'privatePostTradeOrderTpsl') || isType2Order;
+        const isTriggerOrder = hasTriggerPrice || (method === 'privatePostTradeOrderAlgo');
+        if (isTpslOrder) {
             const tpslRequest = this.createTpslOrderRequest (symbol, type, side, amount, price, params);
             response = await this.privatePostTradeOrderTpsl (tpslRequest);
-        } else if (isTriggerOrder || (method === 'privatePostTradeOrderAlgo')) {
+        } else if (isTriggerOrder) {
             const triggerRequest = this.createOrderRequest (symbol, type, side, amount, price, params);
             response = await this.privatePostTradeOrderAlgo (triggerRequest);
         } else {
             const request = this.createOrderRequest (symbol, type, side, amount, price, params);
             response = await this.privatePostTradeOrder (request);
         }
-        if (isTriggerOrder || (method === 'privatePostTradeOrderAlgo')) {
+        if (isTpslOrder || isTriggerOrder) {
             const dataDict = this.safeDict (response, 'data', {});
-            const triggerOrder = this.parseOrder (dataDict, market);
-            return triggerOrder;
+            const order = this.parseOrder (dataDict, market);
+            return order;
         }
         const data = this.safeList (response, 'data', []);
         const first = this.safeDict (data, 0);
