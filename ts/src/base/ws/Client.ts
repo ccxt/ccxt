55d763985414ea7284254cde117d908039a0c9a5
T. Todua <7117978+ttodua@users.noreply.github.com>
2025-11-07 19:15:14 +0400

    Update Client.ts

--- ts/src/base/ws/Client.ts
+++ ts/src/base/ws/Client.ts
@@ -222,6 +222,7 @@ export default class Client {
                 if (this.ping) {
                     message = this.ping (this);
                 }
+                this.log (new Date (), 'OnPingInterval', this.url);
                 if (message) {
                     this.send (message).catch ((error) => {
                         this.onError (error);
