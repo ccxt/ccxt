898f2355923cce86fb9649347a7f161411309859
t.t <7117978+ttodua@users.noreply.github.com>
2025-11-11 22:15:29 +0400

    vers﻿

--- python/setup.py
+++ python/setup.py
@@ -92,7 +92,7 @@ setup(
             'aiodns>=1.1.1',
             'yarl>=1.7.2',
         ],
-        ':sys_platform!="win32" and python_version>="3.8" and python_version<="3.13"': [
+        ':sys_platform!="win32" and python_version>="3.9" and python_version<="3.13"': [
             'coincurve==21.0.0',
         ],
         ':sys_platform!="win32" and python_version>="3.14"': [
