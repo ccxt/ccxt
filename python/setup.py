cdb894db4844c838d41fbd403a6402b622e19d3e
T. Todua <7117978+ttodua@users.noreply.github.com>
2025-11-12 21:41:55 +0400

    fix(python): coincurve versions (#27280)
    * fix(python): coincurve for windows
    
    * vers
    
    * rev
    
    * vers﻿
    
    * win users
    
    * rev﻿

--- python/setup.py
+++ python/setup.py
@@ -92,8 +92,8 @@ setup(
             'aiodns>=1.1.1',
             'yarl>=1.7.2',
         ],
-        ':python_version>="3.9"': [
-            'coincurve==20.0.0',
+        ':python_version>="3.9" and python_version<="3.13"': [
+            'coincurve==21.0.0',
         ],
         'qa': [
             'ruff==0.0.292',
