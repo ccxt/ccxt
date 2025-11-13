2a97274640235a22338f87c7daddb1448349b4bb
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-11-13 14:45:02 +0000

    fix(setMarketsFromExchange): add currencies by id

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -3572,6 +3572,7 @@ export default class Exchange {
         this.symbols = sourceExchange.symbols;
         this.ids = sourceExchange.ids;
         this.currencies = sourceExchange.currencies;
+        this.currencies_by_id = sourceExchange.currencies_by_id;
         this.baseCurrencies = sourceExchange.baseCurrencies;
         this.quoteCurrencies = sourceExchange.quoteCurrencies;
         this.codes = sourceExchange.codes;
