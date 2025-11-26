b205acc93fd089e80a3366bd4ac24486e39907d5
sc0Vu <alk03073135@gmail.com>
2025-11-26 17:07:02 +0800

    fix(exchange): error message for retring in python

--- python/ccxt/async_support/base/exchange.py
+++ python/ccxt/async_support/base/exchange.py
@@ -980,7 +980,7 @@ class Exchange(BaseExchange):
                 if isinstance(e, OperationFailed):
                     if i < retries:
                         if self.verbose:
-                            self.log('Request failed with the error: ' + str(e) + ', retrying ' + (i + str(1)) + ' of ' + str(retries) + '...')
+                            self.log('Request failed with the error: ' + str(e) + ', retrying ' + str(i + 1) + ' of ' + str(retries) + '...')
                         if (retryDelay is not None) and (retryDelay != 0):
                             await self.sleep(retryDelay)
                     else:
