8658efa05902dd9632368f25885f01aadea7b38b
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-12-02 11:04:03 -0500

    fix(binance): cancelOrderWs algoOrder (#27436)
    * fix(binance): cancelOrderWs algoOrder
    
    * fix ob

--- ts/src/pro/binance.ts
+++ ts/src/pro/binance.ts
@@ -688,7 +688,7 @@ export default class binance extends binanceRest {
             const market = this.market (symbol);
             messageHashes.push ('orderbook::' + symbol);
             const subscriptionHash = market['lowercaseId'] + '@' + name;
-            const symbolHash = subscriptionHash + '@' + watchOrderBookRate + 'ms';
+            const symbolHash = subscriptionHash + '@' + watchOrderBookRate.toString () + 'ms';
             subParams.push (symbolHash);
         }
         const messageHashesLength = messageHashes.length;
@@ -3382,23 +3382,27 @@ export default class binance extends binanceRest {
         };
         const isConditional = this.safeBoolN (params, [ 'stop', 'trigger', 'conditional' ]);
         const clientOrderId = this.safeStringN (params, [ 'clientAlgoId', 'origClientOrderId', 'clientOrderId' ]);
+        const shouldUseAlgoOrder = market['linear'] && market['swap'] && isConditional;
         if (clientOrderId !== undefined) {
-            payload['origClientOrderId'] = clientOrderId;
-        } if (market['linear'] && market['swap'] && isConditional) {
-            payload['clientAlgoId'] = clientOrderId;
+            if (shouldUseAlgoOrder) {
+                payload['clientAlgoId'] = clientOrderId;
+            } else {
+                payload['origClientOrderId'] = clientOrderId;
+            }
         } else {
-            payload['orderId'] = this.numberToString (id);
+            if (shouldUseAlgoOrder) {
+                payload['algoId'] = this.numberToString (id);
+            } else {
+                payload['orderId'] = this.numberToString (id);
+            }
         }
         params = this.omit (params, [ 'origClientOrderId', 'clientOrderId', 'stop', 'trigger', 'conditional' ]);
-        if (market['linear'] && market['swap'] && isConditional) {
-            payload['algoId'] = id;
-        }
         const message: Dict = {
             'id': messageHash,
             'method': 'order.cancel',
             'params': this.signParams (this.extend (payload, params)),
         };
-        if (market['linear'] && market['swap'] && isConditional) {
+        if (shouldUseAlgoOrder) {
             message['method'] = 'algoOrder.cancel';
         }
         const subscription: Dict = {
