bb1e9565f15f9d85ad1a49be88e018aac9547b46
yzh-pelle <81404729+yzh-pelle@users.noreply.github.com>
2025-12-02 18:35:46 +0300

    fix(hyperliquid): handleWsTickers market symbol error fixed (#27430)
    * fix(hyperliquid): handleWsTickers market symbol error fixed
    
    According to #27428
    
    * fix(hyperliquid): handleWsTickers updated with `coinToMarketId()``
    
    * [Automated changes] C# files
    
    * fix options name
    
    * fix fetch tickers
    
    * fix fetchTickers and watchTickers
    
    * protect coinToMarketId
    
    * rm safeBool
    
    * watchTickers, support hip3 tokens
    
    * fix watchTrades docs
    
    * fix return type
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: carlosmiei <43336371+carlosmiei@users.noreply.github.com>

--- ts/src/hyperliquid.ts
+++ ts/src/hyperliquid.ts
@@ -101,10 +101,10 @@ export default class hyperliquid extends Exchange {
                 'fetchPositions': true,
                 'fetchPositionsRisk': false,
                 'fetchPremiumIndexOHLCV': false,
+                'fetchStatus': true,
                 'fetchTicker': 'emulated',
                 'fetchTickers': true,
                 'fetchTime': true,
-                'fetchStatus': true,
                 'fetchTrades': true,
                 'fetchTradingFee': true,
                 'fetchTradingFees': false,
@@ -645,7 +645,9 @@ export default class hyperliquid extends Exchange {
                 );
                 data['baseId'] = j + offset;
                 data['collateralToken'] = collateralToken;
-                const cachedCurrencies = this.safeDict (this.options, 'c', {});
+                data['hip3'] = true;
+                data['dex'] = dexName;
+                const cachedCurrencies = this.safeDict (this.options, 'cachedCurrenciesById', {});
                 // injecting collateral token name for further usage in parseMarket, already converted from like '0' to 'USDC', etc
                 if (collateralToken in cachedCurrencies) {
                     const name = this.safeString (data, 'name');
@@ -693,8 +695,6 @@ export default class hyperliquid extends Exchange {
         //     ]
         //
         //
-        // reset currency cache not needed anymore
-        this.options['cachedCurrenciesById'] = {};
         return markets;
     }
 
@@ -1241,6 +1241,7 @@ export default class hyperliquid extends Exchange {
      * @param {string[]} [symbols] unified symbols of the markets to fetch the ticker for, all market tickers are returned if not assigned
      * @param {object} [params] extra parameters specific to the exchange API endpoint
      * @param {string} [params.type] 'spot' or 'swap', by default fetches both
+     * @param {boolean} [params.hip3] set to true to fetch hip3 markets only
      * @returns {object} a dictionary of [ticker structures]{@link https://docs.ccxt.com/#/?id=ticker-structure}
      */
     async fetchTickers (symbols: Strings = undefined, params = {}): Promise<Tickers> {
@@ -1250,7 +1251,20 @@ export default class hyperliquid extends Exchange {
         let response = [];
         const type = this.safeString (params, 'type');
         params = this.omit (params, 'type');
-        if (type === 'spot') {
+        let hip3 = false;
+        [ hip3, params ] = this.handleOptionAndParams (params, 'fetchTickers', 'hip3', false);
+        if (symbols !== undefined) {
+            // infer from first symbol
+            const firstSymbol = this.safeString (symbols, 0);
+            const market = this.market (firstSymbol);
+            if (this.safeBool (this.safeDict (market, 'info'), 'hip3')) {
+                hip3 = true;
+            }
+        }
+        if (hip3) {
+            params = this.omit (params, 'hip3');
+            response = await this.fetchHip3Markets (params);
+        } else if (type === 'spot') {
             response = await this.fetchSpotMarkets (params);
         } else if (type === 'swap') {
             response = await this.fetchSwapMarkets (params);
@@ -1394,6 +1408,9 @@ export default class hyperliquid extends Exchange {
         //         "circulatingSupply": "998949190.03400207", // only in spot
         //     },
         //
+        const name = this.safeString (ticker, 'name');
+        const marketId = this.coinToMarketId (name);
+        market = this.safeMarket (marketId, market);
         const bidAsk = this.safeList (ticker, 'impactPxs');
         return this.safeTicker ({
             'symbol': market['symbol'],
@@ -4378,6 +4395,9 @@ export default class hyperliquid extends Exchange {
 
     coinToMarketId (coin: Str) {
         // handle also hip3 tokens like flx:CRCL
+        if (coin === undefined) {
+            return undefined;
+        }
         if (this.safeDict (this.options['hip3TokensByName'], coin)) {
             const hip3Dict = this.options['hip3TokensByName'][coin];
             const quote = this.safeString (hip3Dict, 'quote', 'USDC');
