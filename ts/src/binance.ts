ae4ceb2b611b248fc534c3b67eac7344bd2586ca
T. Todua <7117978+ttodua@users.noreply.github.com>
2025-12-03 16:07:18 +0400

    refactor(binance): safePosition (#27427)f

--- ts/src/binance.ts
+++ ts/src/binance.ts
@@ -10509,7 +10509,7 @@ export default class binance extends Exchange {
         }
         const positionSide = this.safeString (position, 'positionSide');
         const hedged = positionSide !== 'BOTH';
-        return {
+        return this.safePosition ({
             'info': position,
             'id': undefined,
             'symbol': symbol,
@@ -10536,7 +10536,7 @@ export default class binance extends Exchange {
             'percentage': percentage,
             'stopLossPrice': undefined,
             'takeProfitPrice': undefined,
-        };
+        });
     }
 
     async loadLeverageBrackets (reload = false, params = {}) {
