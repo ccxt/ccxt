7d7f9bfae0f8e876ed833dd02eec664c463c3846
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-12-12 11:17:31 +0000

    fix(zebpay): headers

--- ts/src/zebpay.ts
+++ ts/src/zebpay.ts
@@ -1890,6 +1890,7 @@ export default class zebpay extends Exchange {
                 signature = this.hmac (this.encode (body), this.encode (this.secret), sha256, 'hex');
             }
             headers = {
+                'Referrer': 'ccxt',
                 'X-AUTH-APIKEY': this.apiKey,
                 'X-AUTH-SIGNATURE': signature,
             };
