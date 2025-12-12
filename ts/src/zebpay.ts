b880185e29896f7019f57f93e3ba40dc30d8a3bd
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2025-12-12 11:05:34 +0000

    fix(zebpay): update url

--- ts/src/zebpay.ts
+++ ts/src/zebpay.ts
@@ -70,7 +70,7 @@ export default class zebpay extends Exchange {
             'urls': {
                 'logo': 'https://github.com/user-attachments/assets/8094e7be-55a7-46f4-a087-0ca31b48ecad',
                 'api': {
-                    'spot': 'https://api.zebpay.com',
+                    'spot': 'https://sapi.zebpay.com',
                     'swap': 'https://futuresbe.zebpay.com',
                 },
                 'test': {
