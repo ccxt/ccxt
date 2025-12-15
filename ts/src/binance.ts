83ac30e55fe9508d5cd651a0bcdfb1642f0c2ff1
Peter Lai <alk03073135@gmail.com>
2025-12-15 17:39:08 +0800

    feat(binance): add apis (#27511)

--- ts/src/binance.ts
+++ ts/src/binance.ts
@@ -849,6 +849,8 @@ export default class binance extends Exchange {
                         'lvtKlines': 1,
                         'convert/exchangeInfo': 4,
                         'insuranceBalance': 1,
+                        'symbolAdlRisk': 1,
+                        'tradingSchedule': 5,
                     },
                 },
                 'fapiData': {
@@ -880,7 +882,6 @@ export default class binance extends Exchange {
                         'commissionRate': 20,
                         'rateLimit/order': 1,
                         'apiTradingStatus': 1,
-                        'symbolAdlRisk': 1,
                         'multiAssetsMargin': 30,
                         // broker endpoints
                         'apiReferral/ifNewUser': 1,
@@ -908,6 +909,7 @@ export default class binance extends Exchange {
                         'algoOrder': 1,
                         'openAlgoOrders': { 'cost': 1, 'noSymbol': 40 },
                         'allAlgoOrders': 5,
+                        'stock/contract': 50,
                     },
                     'post': {
                         'batchOrders': 5,
