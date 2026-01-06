76f37edf3e93387d4ea9d31474b152d2b0b2a851
t.t <7117978+ttodua@users.noreply.github.com>
2026-01-06 15:05:24 +0400

    tests

--- ts/src/test/base/test.ethMethods.ts
+++ ts/src/test/base/test.ethMethods.ts
@@ -12,7 +12,8 @@ function testEthMethods () {
 
     const privateKey = '0x27c9c557bd398e354b57ba58046b055035c47788926eb53fcdb394769ef80e1b';
     const publicKey = '0x3096cD9827766E03f8b6DF58996399406DC270Af';
-    assert (exchange.ethGetAddressFromPrivateKey (privateKey) === publicKey);
+    const generatedAddress = exchange.ethGetAddressFromPrivateKey (privateKey);
+    assert (generatedAddress.toLowerCase () === publicKey.toLowerCase (), 'ethGetAddressFromPrivateKey did not generate the expected address: ' + generatedAddress + ' != ' + publicKey);
 }
 
 export default testEthMethods;
