e795927ed0213b0475f2b42bc77039edf000c3e9
T.T <7117978+ttodua@users.noreply.github.com>
2026-01-29 21:25:02 +0400

    perf: python urlencode_with_array_repeat

--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -1115,9 +1115,10 @@ class Exchange(object):
                 newParams[key] = 'true' if value else 'false'
         return _urlencode.urlencode(newParams, doseq, quote_via=_urlencode.quote)
 
-    @staticmethod
+    _ARRAY_BRACKET_PATTERN = re.compile(r'%5B\d*%5D')
+
     def urlencode_with_array_repeat(params={}):
-        return re.sub(r'%5B\d*%5D', '', Exchange.urlencode(params, True))
+        return Exchange._ARRAY_BRACKET_PATTERN.sub('', Exchange.urlencode(params, True))
 
     @staticmethod
     def urlencode_nested(params):
