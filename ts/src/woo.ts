ee3e86026d10b4a9ee4cd239e2d2832f4948bf7e
T.T <7117978+ttodua@users.noreply.github.com>
2026-01-30 00:41:39 +0400

    test(woo): fix static tests ohlcv

--- ts/src/woo.ts
+++ ts/src/woo.ts
@@ -2152,7 +2152,7 @@ export default class woo extends Exchange {
             request['limit'] = Math.min (limit, 1000);
         }
         if (since !== undefined) {
-            request['after'] = since - 1;
+            request['after'] = since - 1; // #27793
         }
         const until = this.safeInteger (params, 'until');
         params = this.omit (params, 'until');
