735b9e269f9ff3072bb6964f6e6aab0e2ff8ec10
T.T <7117978+ttodua@users.noreply.github.com>
2026-01-30 21:38:00 +0400

    test: urlencodeToBase64

--- ts/src/test/base/tests.init.ts
+++ ts/src/test/base/tests.init.ts
@@ -16,6 +16,7 @@ import testSortBy from './test.sortBy.js';
 import testSum from './test.sum.js';
 import testOmit from './test.omit.js';
 import testGroupBy from './test.groupBy.js';
+import testUrlencodeToBase64 from './test.urlencodeToBase64.js';
 import testFilterBy from './test.filterBy.js';
 import testAfterConstructor from './test.afterConstructor.js';
 import testHandleMethods from './test.handleMethods.js';
@@ -42,6 +43,7 @@ async function baseTestsInit () {
     testJson ();
     testSortBy ();
     testSum ();
+    testUrlencodeToBase64 ();
     testOmit ();
     testGroupBy ();
     testFilterBy ();
