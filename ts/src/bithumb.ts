ab718f264d8ce08defffb0a551e416e6f92cce99
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2026-02-03 16:05:15 +0000

    missing type

--- ts/src/bithumb.ts
+++ ts/src/bithumb.ts
@@ -1014,7 +1014,7 @@ export default class bithumb extends Exchange {
      * @param {object} [params] extra parameters specific to the exchange API endpoint
      * @returns {object} a [transaction structure]{@link https://docs.ccxt.com/?id=transaction-structure}
      */
-    async withdraw (code: string, amount: number, address: string, tag = undefined, params = {}): Promise<Transaction> {
+    async withdraw (code: string, amount: number, address: string, tag: Str = undefined, params = {}): Promise<Transaction> {
         [ tag, params ] = this.handleWithdrawTagAndParams (tag, params);
         this.checkAddress (address);
         await this.loadMarkets ();
