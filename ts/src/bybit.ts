1b7534b7a75d035176ef6b1bf1baf7738572b4f5
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2026-02-04 14:32:04 +0000

    feat(bybit): add  v5/account/set-limit-px-action

--- ts/src/bybit.ts
+++ ts/src/bybit.ts
@@ -544,6 +544,7 @@ export default class bybit extends Exchange {
                         'v5/account/borrow': 5,
                         'v5/account/repay': 5,
                         'v5/account/no-convert-repay': 5,
+                        'v5/account/set-limit-px-action': 5,
                         // asset
                         'v5/asset/exchange/quote-apply': 1, // 50/s
                         'v5/asset/exchange/convert-execute': 1, // 50/s
