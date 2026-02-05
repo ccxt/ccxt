f7a8830cc9024b10cd6e9fbb43cbfbc286740e28
caoilainnl <caoilainn.lynch@proton.me>
2026-02-05 03:10:19 -0600

    okx new rate limit

--- ts/src/okx.ts
+++ ts/src/okx.ts
@@ -1395,7 +1395,7 @@ export default class okx extends Exchange {
                 // the exchange refers to ERC20 version of Aeternity (AEToken)
                 'AE': 'AET', // https://github.com/ccxt/ccxt/issues/4981
             },
-            'rollingWindowSize': 1000.0,
+            'rollingWindowSize': 0.0,  // okx always receives rateLimitExceeded with rolling window
         });
     }
 
