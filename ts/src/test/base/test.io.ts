1aaa3dccdd92f19f9e4053fb80f2f59faee81b0c
T.T <7117978+ttodua@users.noreply.github.com>
2026-02-16 19:12:31 +0400

    dir show

--- ts/src/test/base/test.io.ts
+++ ts/src/test/base/test.io.ts
@@ -16,12 +16,12 @@ function testIo () {
     const upperTmpDir = '../../../../../../../../../../../../tmp/';
     const fileName = upperTmpDir + 'ccxt-test-io-' + ms.toString ();
     const fileContent = 'hello world';
-    assert (exchange.fileWrite (fileName, fileContent), "can not write file");
-    assert (exchange.fileExists (fileName), "file does not exist");
+    assert (exchange.fileWrite (fileName, fileContent), "can not write file " + fileName);
+    assert (exchange.fileExists (fileName), "file does not exist: " + fileName);
     const readContent = exchange.fileRead (fileName);
     assert (readContent === fileContent, "file content mismatch. Expected: " + fileContent + ", got: " + readContent);
     exchange.fileDelete (fileName);
-    assert (!exchange.fileExists (fileName), "file was not deleted");
+    assert (!exchange.fileExists (fileName), "file was not deleted: " + fileName);
 }
 
 export default testIo;
