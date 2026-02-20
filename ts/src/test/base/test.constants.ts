8df78a6f36cbbbbaf10a23e2959d5ade0d6a3475
T.T <7117978+ttodua@users.noreply.github.com>
2026-02-20 13:48:50 +0400

    eq

--- ts/src/test/base/test.constants.ts
+++ ts/src/test/base/test.constants.ts
@@ -5,15 +5,15 @@ import { ROUND, TRUNCATE, ROUND_UP, ROUND_DOWN, DECIMAL_PLACES, SIGNIFICANT_DIGI
 
 function testBaseConstants () {
     // assert all
-    assert.strictEqual (ROUND, 1, 'ROUND should be 1');
-    assert.strictEqual (TRUNCATE, 0, 'TRUNCATE should be 0');
-    assert.strictEqual (ROUND_UP, 2, 'ROUND_UP should be 2');
-    assert.strictEqual (ROUND_DOWN, 3, 'ROUND_DOWN should be 3');
-    assert.strictEqual (DECIMAL_PLACES, 2, 'DECIMAL_PLACES should be 2');
-    assert.strictEqual (SIGNIFICANT_DIGITS, 3, 'SIGNIFICANT_DIGITS should be 3');
-    assert.strictEqual (TICK_SIZE, 4, 'TICK_SIZE should be 4');
-    assert.strictEqual (NO_PADDING, 5, 'NO_PADDING should be 5');
-    assert.strictEqual (PAD_WITH_ZERO, 6, 'PAD_WITH_ZERO should be 6');
+    assert (ROUND === 1, 'ROUND should be 1');
+    assert (TRUNCATE === 0, 'TRUNCATE should be 0');
+    assert (ROUND_UP === 2, 'ROUND_UP should be 2');
+    assert (ROUND_DOWN === 3, 'ROUND_DOWN should be 3');
+    assert (DECIMAL_PLACES === 2, 'DECIMAL_PLACES should be 2');
+    assert (SIGNIFICANT_DIGITS === 3, 'SIGNIFICANT_DIGITS should be 3');
+    assert (TICK_SIZE === 4, 'TICK_SIZE should be 4');
+    assert (NO_PADDING === 5, 'NO_PADDING should be 5');
+    assert (PAD_WITH_ZERO === 6, 'PAD_WITH_ZERO should be 6');
 }
 
 export default testBaseConstants;
