382f2232a7d4f41fd08bc3331de09fb6d56d1592
T.T <7117978+ttodua@users.noreply.github.com>
2026-02-23 18:34:37 +0400

    tmp-io2

--- ts/src/test/base/test.io.ts
+++ ts/src/test/base/test.io.ts
@@ -11,25 +11,17 @@ async function testIo () {
         'id': 'sampleex',
     });
 
-    assert ("GO_SKIP_START");
     const ms = exchange.milliseconds ();
     const fileName = 'ccxt-test-io-' + ms.toString ();
     // upper tmp dir
-    let filePath = '../../../../../../../../../../../../tmp/' + fileName;
+    const filePath = '../../../../../../../../../../../../tmp/' + fileName;
     const fileContent: string = 'hello world';
-    try {
-        assert (exchange.fileWrite (filePath, fileContent), "can not write file " + filePath);
-    } catch (e) {
-        filePath = '/tmp/' + fileName;
-        assert (exchange.fileWrite (filePath, fileContent), "can not write file " + filePath);
-    }
+    assert (exchange.fileWrite (filePath, fileContent), "can not write file " + filePath);
     assert (exchange.fileExists (filePath), "file does not exist: " + filePath);
     const readContent = exchange.fileRead (filePath);
     assert (readContent === fileContent, "file content mismatch. Expected: " + fileContent + ", got: " + readContent);
     exchange.fileDelete (filePath);
     assert (!exchange.fileExists (filePath), "file was not deleted: " + filePath);
-    assert ("GO_SKIP_END");
-    assert (exchange.safeString (undefined, undefined) === undefined, "safeString failed on undefined input"); // go-trick
 }
 
 export default testIo;
