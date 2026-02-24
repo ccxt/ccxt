cddcd47c734031bc393725901c84295dc82e11d7
T.T <7117978+ttodua@users.noreply.github.com>
2026-02-24 23:10:46 +0400

    go b2

--- ts/src/test/base/test.unique.ts
+++ ts/src/test/base/test.unique.ts
@@ -1,6 +1,6 @@
 
 
-
+import assert from 'assert';
 import ccxt from '../../../ccxt.js';
 import testSharedMethods from '../Exchange/base/test.sharedMethods.js';
 
@@ -12,10 +12,10 @@ function testUnique () {
 
     testSharedMethods.assertDeepEqual (exchange, undefined, 'testUnique',  exchange.unique ([]), []);
     testSharedMethods.assertDeepEqual (exchange, undefined, 'testUnique',  exchange.unique ([ 1, 2, 3 ]), [ 1, 2, 3 ]);
-    testSharedMethods.assertDeepEqual (exchange, undefined, 'testUnique',  exchange.unique ([ 1, 2, 3, 1 ]), [ 1, 2, 3 ]);
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testUnique',  exchange.unique ([ 1, 2, 3, 4, 1 ]), [ 1, 2, 3, 4 ]);
     testSharedMethods.assertDeepEqual (exchange, undefined, 'testUnique',  exchange.unique ([ 'a', 'a', 'b', 'c', 'a', 'c' ]), [ 'a', 'b', 'c' ]);
-    testSharedMethods.assertDeepEqual (exchange, undefined, 'testUnique',  exchange.unique ([ 'a', 'a', 'b', 'c', 'a', 'c', null ]), [ 'a', 'b', 'c', null ]);
-    testSharedMethods.assertDeepEqual (exchange, undefined, 'testUnique',  exchange.unique ([ 'a', 'a', 'b', 'c', 'a', 'c', undefined ]), [ 'a', 'b', 'c', undefined ]);
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testUnique',  exchange.unique ([ 'a', 'a', 'b', 'a', null ]), [ 'a', 'b', null ]);
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testUnique',  exchange.unique ([ 'a', 'a', 'b', 'a', undefined ]), [ 'a', 'b', undefined ]);
 }
 
 export default testUnique;
