f3ed499fd729092f8c588b1a8f841476120ba3fc
T.T <7117978+ttodua@users.noreply.github.com>
2026-02-24 23:11:07 +0400

    skip go

--- ts/src/test/base/test.unique.ts
+++ ts/src/test/base/test.unique.ts
@@ -10,12 +10,15 @@ function testUnique () {
         'id': 'sampleexchange',
     });
 
+    assert ('GO_SKIP_START');
     testSharedMethods.assertDeepEqual (exchange, undefined, 'testUnique',  exchange.unique ([]), []);
     testSharedMethods.assertDeepEqual (exchange, undefined, 'testUnique',  exchange.unique ([ 1, 2, 3 ]), [ 1, 2, 3 ]);
     testSharedMethods.assertDeepEqual (exchange, undefined, 'testUnique',  exchange.unique ([ 1, 2, 3, 4, 1 ]), [ 1, 2, 3, 4 ]);
     testSharedMethods.assertDeepEqual (exchange, undefined, 'testUnique',  exchange.unique ([ 'a', 'a', 'b', 'c', 'a', 'c' ]), [ 'a', 'b', 'c' ]);
     testSharedMethods.assertDeepEqual (exchange, undefined, 'testUnique',  exchange.unique ([ 'a', 'a', 'b', 'a', null ]), [ 'a', 'b', null ]);
     testSharedMethods.assertDeepEqual (exchange, undefined, 'testUnique',  exchange.unique ([ 'a', 'a', 'b', 'a', undefined ]), [ 'a', 'b', undefined ]);
+    assert ('GO_SKIP_END');
+    assert (exchange.safeString (undefined, undefined) === undefined); // go trick
 }
 
 export default testUnique;
