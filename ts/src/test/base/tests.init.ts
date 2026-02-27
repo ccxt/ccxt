b6234688f83bf5f8b538ffa396fbc77c51d2c230
T.T <7117978+ttodua@users.noreply.github.com>
2026-02-27 12:22:07 +0400

    test: fix set markets await

--- ts/src/test/base/tests.init.ts
+++ ts/src/test/base/tests.init.ts
@@ -64,7 +64,7 @@ async function baseTestsInit () {
     testPrecisionFromString ();
     testExtractParams ();
     testArraysConcat ();
-    testSetMarketsFromExchange ();
+    await testSetMarketsFromExchange ();
     testEthMethods ();
     testKeysort ();
     await testSleep ();
