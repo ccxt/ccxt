7eefec3ac6d5609a057ecc751ec67cedaed1384f
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2026-03-02 13:31:33 +0400

    fix response tests and others

--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -2254,9 +2254,8 @@ class Exchange(object):
     def load_lighter_library_helper(self, path, chainId, privateKey, apiKeyIndex, accountIndex):
         if path is None:
             raise NotSupported(self.id + ' load_lighter_library() requires a path to the lighter library. You can find it here https://github.com/elliottech/lighter-python/tree/main/lighter/signers. Please download the appropriate library for your system and provide the path to it.\nExample: exchange.options["libraryPath"] = "path/to/lighter-signer-linux-arm64.so"')
-
         if not os.path.isfile(path):
-            raise NotSupported(self.id + ' the library does not exist')
+            raise NotSupported(self.id + ' the library path does not exist')
 
         lighterSigner = load_lighter_library(path)
 
