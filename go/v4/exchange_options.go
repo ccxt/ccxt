bce6aabb65768f7f5765c332829ced1c52efb9af
T.T <7117978+ttodua@users.noreply.github.com>
2026-03-02 14:21:43 +0400

    ex opts go

--- go/v4/exchange_options.go
+++ go/v4/exchange_options.go
@@ -337,16 +337,16 @@ func (this *Exchange) initializeProperties(extendedProperties map[string]interfa
 	this.cacheLoaded = false
 	reqCred := SafeValue(extendedProperties, "requiredCredentials", map[string]interface{}{})
 	this.RequiredCredentials = reqCred.(map[string]interface{})
-	this.ApiKey = SafeString(extendedProperties, "apiKey", "").(string)
-	this.Secret = SafeString(extendedProperties, "secret", "").(string)
-	this.Password = SafeString(extendedProperties, "password", "").(string)
-	this.Login = SafeString(extendedProperties, "login", "").(string)
+	this.ApiKey = SafeString(extendedProperties, "apiKey", nil).(string)
+	this.Secret = SafeString(extendedProperties, "secret", nil).(string)
+	this.Password = SafeString(extendedProperties, "password", nil).(string)
+	this.Login = SafeString(extendedProperties, "login", nil).(string)
 	this.Twofa = SafeString(extendedProperties, "twofa", nil)
-	this.PrivateKey = SafeString(extendedProperties, "privateKey", "").(string)
-	this.WalletAddress = SafeString(extendedProperties, "walletAddress", "").(string)
-	this.Token = SafeString(extendedProperties, "token", "").(string)
-	this.Uid = SafeString(extendedProperties, "uid", "").(string)
-	this.AccountId = SafeString(extendedProperties, "accountId", "").(string)
+	this.PrivateKey = SafeString(extendedProperties, "privateKey", nil).(string)
+	this.WalletAddress = SafeString(extendedProperties, "walletAddress", nil).(string)
+	this.Token = SafeString(extendedProperties, "token", nil).(string)
+	this.Uid = SafeString(extendedProperties, "uid", nil).(string)
+	this.AccountId = SafeString(extendedProperties, "accountId", nil).(string)
 
 	this.UserAgents = SafeValue(extendedProperties, "userAgents", map[string]interface{}{
 		"chrome":    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
