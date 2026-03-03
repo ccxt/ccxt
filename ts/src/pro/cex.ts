7c7f76571ea901270e41ad009e940b9d6ae7f472
T.T <7117978+ttodua@users.noreply.github.com>
2026-03-03 17:05:48 +0400

    cex watchtrades reorganize

--- ts/src/pro/cex.ts
+++ ts/src/pro/cex.ts
@@ -41,6 +41,9 @@ export default class cex extends cexRest {
             },
             'options': {
                 'orderbook': {},
+                'watchTrades': {
+                    'symbol': undefined,
+                },
             },
             'streaming': {
             },
@@ -131,13 +134,17 @@ export default class cex extends cexRest {
      * @returns {object[]} a list of [trade structures]{@link https://docs.ccxt.com/?id=public-trades}
      */
     async watchTrades (symbol: string, since: Int = undefined, limit: Int = undefined, params = {}): Promise<Trade[]> {
+        const currentSymbol = this.safeString (this.options['watchTrades'], 'symbol');
+        if (currentSymbol !== undefined && currentSymbol !== symbol) {
+            throw new ArgumentsRequired (this.id + ' : this exchange only supports watching trades for one symbol per instance. You should either set .options["watchTrades"]["symbol"] to new symbol, or create a new instance');
+        }
+        this.options['watchTrades']['symbol'] = symbol;
         await this.loadMarkets ();
         const market = this.market (symbol);
         symbol = market['symbol'];
         const url = this.urls['api']['ws'];
         const messageHash = 'trades';
         const subscriptionHash = 'old:' + symbol;
-        this.options['currentWatchTradeSymbol'] = symbol; // exchange supports only 1 symbol for this watchTrades channel
         const client = this.safeValue (this.clients, url);
         if (client !== undefined) {
             const subscriptionKeys = Object.keys (client.subscriptions);
@@ -177,30 +184,14 @@ export default class cex extends cexRest {
         //         ]
         //     }
         //
-        const data = this.safeList (message, 'data', []);
-        const limit = this.safeInteger (this.options, 'tradesLimit', 1000);
-        const stored = new ArrayCache (limit);
-        const symbol = this.safeString (this.options, 'currentWatchTradeSymbol');
-        if (symbol === undefined) {
-            return;
-        }
-        const market = this.market (symbol);
-        const dataLength = data.length;
-        for (let i = 0; i < dataLength; i++) {
-            const index = dataLength - 1 - i;
-            const rawTrade = data[index];
-            const parsed = this.parseWsOldTrade (rawTrade, market);
-            stored.append (parsed);
-        }
-        const messageHash = 'trades';
-        this.trades = stored as any; // trades don't have symbol
-        client.resolve (this.trades, messageHash);
+        this.handleTradesInner (client, message);
     }
 
     parseWsOldTrade (trade, market = undefined) {
         //
         //  snapshot trade
         //    "sell:1665467367741:3888551:19058.8:14541219"
+        //
         //  update trade
         //    ['buy', '1665467516704', '98070', "19057.7", "14541220"]
         //
@@ -238,18 +229,23 @@ export default class cex extends cexRest {
         //         ]
         //     }
         //
-        const data = this.safeValue (message, 'data', []);
-        const symbol = 'default'; // yeah, but this is what it is
+        this.handleTradesInner (client, message);
+    }
+
+    handleTradesInner (client: Client, message) {
+        const data = this.safeList (message, 'data', []);
+        const symbol = this.safeString (this.options['watchTrades'], 'symbol');
         if (!(symbol in this.trades)) {
             const limit = this.safeInteger (this.options, 'tradesLimit', 1000);
             this.trades[symbol] = new ArrayCache (limit);
         }
         const stored = this.trades[symbol];
+        const market = this.market (symbol);
         const dataLength = data.length;
         for (let i = 0; i < dataLength; i++) {
             const index = dataLength - 1 - i;
             const rawTrade = data[index];
-            const parsed = this.parseWsOldTrade (rawTrade);
+            const parsed = this.parseWsOldTrade (rawTrade, market);
             stored.append (parsed);
         }
         const messageHash = 'trades';
