2159fde0874ea27ce6372bed88d3c01ada6a3121
layla <111667698+04cb@users.noreply.github.com>
2026-03-03 16:41:44 +0800

    Fix typos in comments (#28040)

--- ts/src/kraken.ts
+++ ts/src/kraken.ts
@@ -1697,7 +1697,7 @@ export default class kraken extends Exchange {
         const result = this.safeDict (response, 'result');
         result['usingCost'] = isUsingCost;
         // it's impossible to know if the order was created using cost or base currency
-        // becuase kraken only returns something like this: { order: 'buy 10.00000000 LTCUSD @ market' }
+        // because kraken only returns something like this: { order: 'buy 10.00000000 LTCUSD @ market' }
         // this usingCost flag is used to help the parsing but omited from the order
         return this.parseOrder (result);
     }
