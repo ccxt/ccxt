2484fe5971683cc90c187219209d48c9170876db
yzh-pelle <81404729+yzh-pelle@users.noreply.github.com>
2026-03-04 18:21:46 +0300

    fix(kucoinfutures): name added

--- ts/src/kucoinfutures.ts
+++ ts/src/kucoinfutures.ts
@@ -10,6 +10,7 @@ export default class kucoinfutures extends kucoin {
     describe (): any {
         return this.deepExtend (super.describe (), {
             'id': 'kucoinfutures',
+            'name': 'KuCoin Futures',
             'urls': {
                 'logo': 'https://user-images.githubusercontent.com/1294454/147508995-9e35030a-d046-43a1-a006-6fabd981b554.jpg',
                 'www': 'https://futures.kucoin.com/',
@@ -30,7 +31,7 @@ export default class kucoinfutures extends kucoin {
                     'types': [ 'swap', 'future' ],
                     'fetchTickersFees': true,
                 },
-                'defaultType': 'contract',
+                'defaultType': 'swap',
                 'defaultAccountType': 'contract',
                 'uta': false,
             },
