5e2f525e72652e890310c5a0e98f45fd4411fdc6
yzh-pelle <81404729+yzh-pelle@users.noreply.github.com>
2026-03-10 20:20:20 +0300

    feat(kucoinfutures): original transfer() added

--- ts/src/kucoinfutures.ts
+++ ts/src/kucoinfutures.ts
@@ -2,7 +2,8 @@
 //  ---------------------------------------------------------------------------
 
 import kucoin from './kucoin.js';
-import type { Strings } from './base/types.js';
+import { BadRequest } from './base/errors.js';
+import type { Dict, Str, Strings, TransferEntry } from './base/types.js';
 
 //  ---------------------------------------------------------------------------
 
@@ -50,5 +51,83 @@ export default class kucoinfutures extends kucoin {
             'method': 'futuresPublicGetAllTickers',
         };
         return await this.fetchTickers (symbols, this.extend (request, params));
+    }
+
+    /**
+     * @method
+     * @name kucoinfutures#transfer
+     * @description transfer currency internally between wallets on the same account
+     * @param {string} code unified currency code
+     * @param {float} amount amount to transfer
+     * @param {string} fromAccount account to transfer from
+     * @param {string} toAccount account to transfer to
+     * @param {object} [params] extra parameters specific to the exchange API endpoint
+     * @returns {object} a [transfer structure]{@link https://docs.ccxt.com/?id=transfer-structure}
+     */
+    async transfer (code: string, amount: number, fromAccount: string, toAccount:string, params = {}): Promise<TransferEntry> {
+        await this.loadMarkets ();
+        const currency = this.currency (code);
+        const amountToPrecision = this.currencyToPrecision (code, amount);
+        const request: Dict = {
+            'currency': this.safeString (currency, 'id'),
+            'amount': amountToPrecision,
+        };
+        const toAccountString = this.parseTransferType (toAccount);
+        let response = undefined;
+        if (toAccountString === 'TRADE' || toAccountString === 'MAIN') {
+            request['recAccountType'] = toAccountString;
+            response = await this.futuresPrivatePostTransferOut (this.extend (request, params));
+            //
+            //     {
+            //         "code": "200000",
+            //         "data": {
+            //             "applyId": "6738754373ceee00011ec3f8",
+            //             "bizNo": "6738754373ceee00011ec3f7",
+            //             "payAccountType": "CONTRACT",
+            //             "payTag": "DEFAULT",
+            //             "remark": "",
+            //             "recAccountType": "MAIN",
+            //             "recTag": "DEFAULT",
+            //             "recRemark": "",
+            //             "recSystem": "KUCOIN",
+            //             "status": "PROCESSING",
+            //             "currency": "USDT",
+            //             "amount": "5",
+            //             "fee": "0",
+            //             "sn": 1519769124846692,
+            //             "reason": "",
+            //             "createdAt": 1731753283000,
+            //             "updatedAt": 1731753283000
+            //         }
+            //     }
+            //
+        } else if (toAccount === 'future' || toAccount === 'swap' || toAccount === 'contract') {
+            request['payAccountType'] = this.parseTransferType (fromAccount);
+            response = await this.futuresPrivatePostTransferIn (this.extend (request, params));
+            //
+            //    {
+            //        "code": "200000",
+            //        "data": {
+            //            "applyId": "5bffb63303aa675e8bbe18f9" // Transfer-out request ID
+            //        }
+            //    }
+            //
+        } else {
+            throw new BadRequest (this.id + ' transfer() only supports transfers between future/swap, spot and funding accounts');
+        }
+        const data = this.safeDict (response, 'data', {});
+        return this.extend (this.parseTransfer (data, currency), {
+            'amount': this.parseNumber (amountToPrecision),
+            'fromAccount': fromAccount,
+            'toAccount': toAccount,
+        });
+    }
+
+    parseTransferType (transferType: Str): Str {
+        const transferTypes: Dict = {
+            'spot': 'TRADE',
+            'funding': 'MAIN',
+        };
+        return this.safeStringUpper (transferTypes, transferType, transferType);
     }
 }
