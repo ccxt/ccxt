56b7adf4d256304604c29fc54cefe4d78e4381f4
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2026-03-11 09:37:06 -0500

    fix(hyperliquid): never use spot if dex is provided (#28128)

--- ts/src/hyperliquid.ts
+++ ts/src/hyperliquid.ts
@@ -1111,7 +1111,8 @@ export default class hyperliquid extends Exchange {
         [ marginMode, params ] = this.handleMarginModeAndParams ('fetchBalance', params);
         let isUnifiedEnabled = undefined;
         [ isUnifiedEnabled, params ] = await this.isUnifiedEnabled ('fetchBalance', params);
-        const isSpot = ((type === 'spot') || isUnifiedEnabled);
+        const dex = this.safeString (params, 'dex');
+        const isSpot = ((type === 'spot') || isUnifiedEnabled) && (dex === undefined);
         const request: Dict = {
             'type': (isSpot) ? 'spotClearinghouseState' : 'clearinghouseState',
             'user': userAddress,
