2b295bb7e511cdc502ce50c29784a80ec1d3f19b
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2026-03-11 09:32:41 -0500

    feat(toobit): add id (#28127)
    * feat(toobit): add id
    
    * update header key

--- ts/src/test/tests.ts
+++ ts/src/test/tests.ts
@@ -1664,7 +1664,8 @@ class testMainClass {
             this.testCryptomus (),
             this.testDerive (),
             this.testModeTrade (),
-            this.testBackpack ()
+            this.testBackpack (),
+            this.testToobit ()
         ];
         await Promise.all (promises);
         const successMessage = '[' + this.lang + '][TEST_SUCCESS] brokerId tests passed.';
@@ -2313,6 +2314,23 @@ class testMainClass {
             reqHeaders = exchange.last_request_headers;
         }
         assert (reqHeaders['X-Broker-Id'] === id, 'backpack - id: ' + id + ' not in headers.');
+        if (!isSync ()) {
+            await close (exchange);
+        }
+        return true;
+    }
+
+    async testToobit () {
+        const exchange = this.initOfflineExchange ('toobit');
+        let reqHeaders = undefined;
+        const id = '177321641268789';
+        try {
+            await exchange.createOrder ('BTC/USDT', 'limit', 'buy', 1, 20000);
+        } catch (e) {
+            // we expect an error here, we're only interested in the headers
+            reqHeaders = exchange.last_request_headers;
+        }
+        assert (reqHeaders['X-BB-API-PLATFORM'] === id, 'toobit - id: ' + id + ' not in headers.');
         if (!isSync ()) {
             await close (exchange);
         }
