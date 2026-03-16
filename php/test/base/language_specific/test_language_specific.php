80f8eb1f11543bd79f5a421ae4a16a4c16c3bb49
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2026-03-16 16:41:17 +0000

    fix(tests): php lang specific test return type

--- php/test/base/language_specific/test_language_specific.php
+++ php/test/base/language_specific/test_language_specific.php
@@ -5,5 +5,5 @@ include_once __DIR__ . '/test_throttler_performance.php';
 function test_language_specific() {
     return \React\Async\async(function () {
         test_throttler_performance();
-    });
+    })();
 }
