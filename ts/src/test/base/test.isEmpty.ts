5e037c964c32c205eb43c074b4e300e0cf132b84
T.T <7117978+ttodua@users.noreply.github.com>
2026-03-20 10:28:15 +0400

    comments

--- ts/src/test/base/test.isEmpty.ts
+++ ts/src/test/base/test.isEmpty.ts
@@ -9,20 +9,20 @@ function testIsEmpty () {
         'id': 'sampleexchange',
     });
 
-    assert (exchange.isEmpty (undefined) === true);
-    assert (exchange.isEmpty (null) === true);
-    assert (exchange.isEmpty ({}) === true);
-    assert (exchange.isEmpty ([]) === true);
+    assert (exchange.isEmpty (undefined) === true, 'undefined should be empty');
+    assert (exchange.isEmpty (null) === true, 'null should be empty');
+    assert (exchange.isEmpty ({}) === true, 'empty object should be empty');
+    assert (exchange.isEmpty ([]) === true, 'empty array should be empty');
     // @ts-expect-error
-    assert (exchange.isEmpty ('') === true);
+    assert (exchange.isEmpty ('') === true, 'empty string should be empty');
     // @ts-expect-error
-    assert (exchange.isEmpty (0) === true); // todo: hmm
+    assert (exchange.isEmpty (0) === true, 'zero should be empty'); // todo: hmm
     // @ts-expect-error
-    assert (exchange.isEmpty (false) === true);
-    assert (exchange.isEmpty ({ 'foo': 1 }) === false);
-    assert (exchange.isEmpty ([ 1, 2 ]) === false);
+    assert (exchange.isEmpty (false) === true, 'false should be empty');
+    assert (exchange.isEmpty ({ 'foo': 1 }) === false, 'non-empty object should not be empty');
+    assert (exchange.isEmpty ([ 1, 2 ]) === false, 'non-empty array should not be empty');
     // @ts-expect-error
-    assert (exchange.isEmpty ('non-empty string') === false);
+    assert (exchange.isEmpty ('non-empty string') === false, 'non-empty string should not be empty');
 }
 
 export default testIsEmpty;
