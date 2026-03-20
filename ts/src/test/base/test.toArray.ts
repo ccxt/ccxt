1457a415f6205e17064209deafe2b9539aa48ed5
T.T <7117978+ttodua@users.noreply.github.com>
2026-03-20 22:47:16 +0400

    fix toarray

--- ts/src/test/base/test.toArray.ts
+++ ts/src/test/base/test.toArray.ts
@@ -9,7 +9,7 @@ function testToArray () {
     });
 
     const obj1 = { 'a': 1, 'b': 3, 'c': 2 };
-    testSharedMethods.assertDeepEqual (exchange, undefined, 'testToArray', exchange.toArray (obj1), [ 1, 2, 3 ]);
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testToArray', exchange.toArray (obj1), [ 1, 3, 2 ]);
 
     const obj2 = { 'a': 'x', 'b': 2 };
     testSharedMethods.assertDeepEqual (exchange, undefined, 'testToArray', exchange.toArray (obj2), [ 'x', 2 ]);
