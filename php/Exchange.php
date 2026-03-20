0d59e2134febf8ea6e706f6a5c283d878670efb6
T.T <7117978+ttodua@users.noreply.github.com>
2026-03-20 11:27:19 +0400

    isempty fix

--- php/Exchange.php
+++ php/Exchange.php
@@ -828,7 +828,7 @@ class Exchange {
     }
 
     public static function is_empty($object) {
-        return empty($object) || count($object) === 0;
+        return empty($object) || (is_countable($object) && count($object) === 0);
     }
 
     public static function keysort($array) {
