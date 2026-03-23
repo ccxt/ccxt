fa33ac943f59556fe8b553bf2d6bc238e45d1ff3
T.T <7117978+ttodua@users.noreply.github.com>
2026-03-23 14:11:33 +0400

    f1

---
+++ ts/src/test/base/test.rawencode.ts
@@ -0,0 +1,13 @@
+import assert from 'assert';
+import ccxt from '../../../ccxt.js';
+
+function testRawencode () {
+
+    const exchange = new ccxt.Exchange ({
+        'id': 'sampleexchange',
+    });
+
+    
+}
+
+export default testRawencode;
