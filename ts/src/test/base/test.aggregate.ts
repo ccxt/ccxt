4ca299c520893c889648f720a28cd727ea9d1ac8
T.T <7117978+ttodua@users.noreply.github.com>
2026-03-25 00:33:06 +0400

    roundings

--- ts/src/test/base/test.aggregate.ts
+++ ts/src/test/base/test.aggregate.ts
@@ -38,24 +38,24 @@ function testAggregate () {
     testSharedMethods.assertDeepEqual (exchange, undefined, 'aggregate', exchange.aggregate ([]), []);
 
     // Test 1: Simple aggregation - same price combined
-    const result1 = exchange.aggregate ([ [ 100, 1.0 ], [ 101, 2.0 ], [ 100, 0.5 ] ]);
-    testSharedMethods.assertDeepEqual (exchange, undefined, 'testAggregate', result1, [ [ 100, 1.5 ], [ 101, 2.0 ] ]);
+    const result1 = exchange.aggregate ([ [ 100, 1.01 ], [ 101, 2.01 ], [ 100, 0.5 ] ]);
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testAggregate', result1, [ [ 100, 1.51 ], [ 101, 2.01 ] ]);
 
     // Test 2: With extra fields (should be ignored)
-    const result2 = exchange.aggregate ([ [ 100, 1.0, 'extra' ], [ 101, 2.0, 'data', 'more' ] ]);
-    testSharedMethods.assertDeepEqual (exchange, undefined, 'testAggregate', result2, [ [ 100, 1.0 ], [ 101, 2.0 ] ]);
+    const result2 = exchange.aggregate ([ [ 100, 1.01, 'extra' ], [ 101, 2.01, 'data', 'more' ] ]);
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testAggregate', result2, [ [ 100, 1.01 ], [ 101, 2.01 ] ]);
 
     // Test 3: Zero volumes should be skipped
-    const result3 = exchange.aggregate ([ [ 100, 1.0 ], [ 101, 0 ], [ 102, 2.0 ] ]);
-    testSharedMethods.assertDeepEqual (exchange, undefined, 'testAggregate', result3, [ [ 100, 1.0 ], [ 102, 2.0 ] ]);
+    const result3 = exchange.aggregate ([ [ 100, 1.01 ], [ 101, 0 ], [ 102, 2.01 ] ]);
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testAggregate', result3, [ [ 100, 1.01 ], [ 102, 2.01 ] ]);
 
     // Test 4: Empty array
     const result4 = exchange.aggregate ([]);
     testSharedMethods.assertDeepEqual (exchange, undefined, 'testAggregate', result4, []);
 
     // Test 5: Single entry
-    const result5 = exchange.aggregate ([ [ 100, 1.0 ] ]);
-    testSharedMethods.assertDeepEqual (exchange, undefined, 'testAggregate', result5, [ [ 100, 1.0 ] ]);
+    const result5 = exchange.aggregate ([ [ 100, 1.01 ] ]);
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testAggregate', result5, [ [ 100, 1.01 ] ]);
 
     // Test 6: Many same prices aggregated
     const result6 = exchange.aggregate ([ [ 100, 0.1 ], [ 100, 0.2 ], [ 100, 0.3 ], [ 100, 0.4 ] ]);
@@ -70,12 +70,12 @@ function testAggregate () {
     // testSharedMethods.assertDeepEqual (exchange, undefined, 'testAggregate', result8, [ [ 103, 1.0 ], [ 101, 1.5 ], [ 102, 1.0 ] ]);
 
     // Test 9: Decimal prices
-    const result9 = exchange.aggregate ([ [ 100.5, 1.0 ], [ 100.5, 2.0 ], [ 101.5, 1.0 ] ]);
-    testSharedMethods.assertDeepEqual (exchange, undefined, 'testAggregate', result9, [ [ 100.5, 3.0 ], [ 101.5, 1.0 ] ]);
+    const result9 = exchange.aggregate ([ [ 100.5, 1.04 ], [ 100.5, 2.04 ], [ 101.5, 1.05 ] ]);
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testAggregate', result9, [ [ 100.5, 3.08 ], [ 101.5, 1.05 ] ]);
 
     // Test 10: Mixed zero and non-zero for same price
-    const result10 = exchange.aggregate ([ [ 100, 1.0 ], [ 100, 0 ], [ 100, 2.0 ] ]);
-    testSharedMethods.assertDeepEqual (exchange, undefined, 'testAggregate', result10, [ [ 100, 3.0 ] ]);
+    const result10 = exchange.aggregate ([ [ 100, 1.04 ], [ 100, 0 ], [ 100, 2.04 ] ]);
+    testSharedMethods.assertDeepEqual (exchange, undefined, 'testAggregate', result10, [ [ 100, 3.08 ] ]);
 }
 
 export default testAggregate;
