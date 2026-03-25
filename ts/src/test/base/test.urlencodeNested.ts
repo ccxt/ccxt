2fc098d26a6087677a40cfd2005706b6b566bd56
T.T <7117978+ttodua@users.noreply.github.com>
2026-03-25 19:28:52 +0400

    test: guaranteed sorted checks for urlencode functions

--- ts/src/test/base/test.urlencodeNested.ts
+++ ts/src/test/base/test.urlencodeNested.ts
@@ -9,17 +9,22 @@ function testUrlencodeNested () {
         'id': 'sampleexchange',
     });
 
+    // todo: add nulls
+    // todo: add key sort (for different langs)
     const dict2 = {
-        'a': 1,
         'b': {
             'c': 2,
             'target': '+&'
         },
         'd': [ 1, 2 ],
     };
-    const expected2 = 'a=1&b[c]=2&b[target]=%2B%26&d[0]=1&d[1]=2';
+    const expected2a = 'b[c]=2&b[target]=%2B%26&d[0]=1&d[1]=2';
+    const expected2c = 'b[target]=%2B%26&b[c]=2&d[0]=1&d[1]=2';
+    const expected2b = 'd[0]=1&d[1]=2&b[c]=2&b[target]=%2B%26';
+    const expected2d = 'd[0]=1&d[1]=2&b[target]=%2B%26&b[c]=2';
+    const result2 = exchange.urlencodeNested (dict2);
 
-    assert (exchange.urlencodeNested (dict2) === expected2, 'urlencodeNested: expected ' + expected2 + ' but got ' + exchange.urlencodeNested (dict2));
+    assert (result2 === expected2a || result2 === expected2b || result2 === expected2c || result2 === expected2d, 'urlencodeNested: expected ' + expected2a + ' or ' + expected2b + ' or ' + expected2c + ' or ' + expected2d + ' but got ' + result2);
 }
 
 export default testUrlencodeNested;
--- ts/src/test/base/test.urlencodeWithArrayRepeat.ts
+++ ts/src/test/base/test.urlencodeWithArrayRepeat.ts
@@ -13,9 +13,11 @@ function testUrlencodeWithArrayRepeat () {
         'a': 1,
         'product_ids': [ 'AA', 'BB' ],
     };
-    const expected2 = 'a=1&product_ids=AA&product_ids=BB';
+    const expected2a = 'a=1&product_ids=AA&product_ids=BB';
+    const expected2b = 'product_ids=AA&product_ids=BB&a=1';
+    const result2 = exchange.urlencodeWithArrayRepeat (dict2);
 
-    assert (exchange.urlencodeWithArrayRepeat (dict2) === expected2, 'urlencodeWithArrayRepeat: expected ' + expected2 + ' but got ' + exchange.urlencodeWithArrayRepeat (dict2));
+    assert (result2 === expected2a || result2 === expected2b, 'urlencodeWithArrayRepeat: expected ' + expected2a + ' or ' + expected2b + ' but got ' + result2);
 }
 
 export default testUrlencodeWithArrayRepeat;
