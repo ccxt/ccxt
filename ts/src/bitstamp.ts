7a876d3fc1e1808124956b918d1232cd69ead3dd
T.T <7117978+ttodua@users.noreply.github.com>
2026-04-05 16:48:40 +0400

    lowercase fix

--- ts/src/bitstamp.ts
+++ ts/src/bitstamp.ts
@@ -1147,8 +1147,8 @@ export default class bitstamp extends Exchange {
         amountString = this.safeString (trade, market['baseId'], amountString);
         costString = this.safeString (trade, market['quoteId'], costString);
         // this endpoint is not aligned with "markets" endpoint
-        const baseIdLower = (market['baseId']).toLowerCase ();
-        const quoteIdLower = (market['quoteId']).toLowerCase ();
+        const baseIdLower = market['baseId'].toLowerCase ();
+        const quoteIdLower = market['quoteId'].toLowerCase ();
         const dashedIdLower = baseIdLower + '_' + quoteIdLower;
         if (priceString === undefined) {
             priceString = this.safeString (trade, dashedIdLower);
