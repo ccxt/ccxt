cadfd95e5e823ea1fbe7e4274234c5f74b50bfc3
Nirator <nirator.dev@gmail.com>
2026-04-09 11:28:28 +0200

    Add missing route account/asset/analysis/yesterday_pnl https://www.mexc.com/api-docs/futures/account-and-trading-endpoints#yesterdays-pnl

--- ts/src/mexc.ts
+++ ts/src/mexc.ts
@@ -308,6 +308,7 @@ export default class mexc extends Exchange {
                             'account/profit_rate/{type}': 2,
                             'account/asset/analysis/{type}': 2,
                             'account/feeDeductConfigs': 2,
+                            'account/asset/analysis/yesterday_pnl': 2,
                             'position/list/history_positions': 2,
                             'position/open_positions': 2,
                             'position/funding_records': 2,
