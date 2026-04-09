c3a41ac5a54e0268dcfb8122db013aacc5d871cc
Nirator <nirator.dev@gmail.com>
2026-04-09 13:31:11 +0200

    Add missing route market_maker/self_trade/blacklist/update  https://www.mexc.com/api-docs/futures/account-and-trading-endpoints#update-stp-group

--- ts/src/mexc.ts
+++ ts/src/mexc.ts
@@ -379,6 +379,7 @@ export default class mexc extends Exchange {
                             'trackorder/cancel': 2,
                             'trackorder/change_order': 2,
                             'market_maker/self_trade/blacklist/create': 2,
+                            'market_maker/self_trade/blacklist/update': 2,
                         },
                     },
                 },
