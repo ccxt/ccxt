5e0368bcb924d00095849d805b5feaa79eab2a68
Nirator <nirator.dev@gmail.com>
2026-04-09 11:27:29 +0200

    Add missing route account/asset/analysis/{type} https://www.mexc.com/api-docs/futures/account-and-trading-endpoints#asset-analysis

--- ts/src/mexc.ts
+++ ts/src/mexc.ts
@@ -306,6 +306,7 @@ export default class mexc extends Exchange {
                             'account/asset/{currency}': 2,
                             'account/transfer_record': 2,
                             'account/profit_rate/{type}': 2,
+                            'account/asset/analysis/{type}': 2,
                             'position/list/history_positions': 2,
                             'position/open_positions': 2,
                             'position/funding_records': 2,
