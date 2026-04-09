b79c552a4010c67bc38838dfc8e02dafc0c03d50
Nirator <nirator.dev@gmail.com>
2026-04-09 13:29:18 +0200

    Add missing route trackorder/cancel  https://www.mexc.com/api-docs/futures/account-and-trading-endpoints#cancel-trailing-order

--- ts/src/mexc.ts
+++ ts/src/mexc.ts
@@ -373,6 +373,7 @@ export default class mexc extends Exchange {
                             'stoporder/change_price': 2,
                             'stoporder/change_plan_price': 2,
                             'trackorder/place': 2,
+                            'trackorder/cancel': 2,
                         },
                     },
                 },
