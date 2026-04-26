45f41520aa3a3d6d6b6a57014b810888a0f2f8d3
Ori Kalinski <orikalinski@gmail.com>
2026-04-26 14:41:43 +0300

    Handle mexc rate limit as ddos protection (#200)

--- python/ccxt/mexc_abs.py
+++ python/ccxt/mexc_abs.py
@@ -1,9 +1,19 @@
 from ccxt.mexc import mexc
+from ccxt.base.errors import DDoSProtection
 
 MEXC = 'MEXC'
 
 
 class mexc_abs(mexc):
+    def describe(self):
+        return self.deep_extend(super(mexc_abs, self).describe(), {
+            'exceptions': {
+                'exact': {
+                    '510': DDoSProtection,  # {"success":false,"code":510,"message":"Requests are too frequent, please try again later"}
+                },
+            },
+        })
+
     def __init__(self, config={}):
         super().__init__(config)
         self.options['broker'] = 'CORNIX'
