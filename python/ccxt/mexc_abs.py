ead780e056ede948fe551359612a74709f0c7aa5
Ori Kalinski <orikalinski@gmail.com>
2026-05-07 14:41:27 +0300

    classifying the right exception for {"success":false,"code":402,"message":"API Key expired, please apply again"}

--- python/ccxt/mexc_abs.py
+++ python/ccxt/mexc_abs.py
@@ -1,5 +1,5 @@
 from ccxt.mexc import mexc
-from ccxt.base.errors import DDoSProtection
+from ccxt.base.errors import DDoSProtection, AuthenticationError
 
 MEXC = 'MEXC'
 
@@ -9,6 +9,7 @@ class mexc_abs(mexc):
         return self.deep_extend(super(mexc_abs, self).describe(), {
             'exceptions': {
                 'exact': {
+                    '402': AuthenticationError,  # {"success":false,"code":402,"message":"API Key expired, please apply again"}
                     '510': DDoSProtection,  # {"success":false,"code":510,"message":"Requests are too frequent, please try again later"}
                 },
             },
