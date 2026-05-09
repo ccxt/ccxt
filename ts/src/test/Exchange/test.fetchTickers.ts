6951891bf344ae040ad32027e6c7010c8fa33431
T.T <7117978+ttodua@users.noreply.github.com>
2026-05-09 12:25:57 +0400

    tickers exception shared

--- ts/src/test/Exchange/test.fetchTickers.ts
+++ ts/src/test/Exchange/test.fetchTickers.ts
@@ -27,33 +27,10 @@ async function testFetchTickersHelper (exchange: Exchange, skippedProperties: ob
         try {
             testTicker (exchange, skippedProperties, method, ticker, checkedSymbol);
         } catch (ex) {
-            await validateTickerException (ex, exchange, ticker);
+            await testSharedMethods.validateTickerExceptionForPercentage (ex, exchange, ticker);
         }
     }
     return response;
-}
-
-async function validateTickerException (ex: any, exchange: Exchange, ticker: any) {
-    // only skip cases of "too far price" when it's the first day of listing, otherwise rethrow abnormality
-    const eMessage = exchange.exceptionMessage (ex, false);
-    if (eMessage.indexOf ('percentage should be above') >= 0 || eMessage.indexOf ('percentage should be below') >= 0) {
-        const symbol = ticker['symbol'];
-        if (symbol !== undefined) {
-            // if it's not in markets, then maybe newly added symbol, so can can compromise there
-            if (!(symbol in exchange.markets)) {
-                return;
-            }
-            // if OHLCV supported
-            if (exchange.featureValue (symbol, 'fetchOHLCV') !== undefined) {
-                const ohlcv = await exchange.fetchOHLCV (symbol, '1d', undefined, 5);
-                if (ohlcv.length <= 1) {
-                    // if only 1 day, then allow it
-                    return;
-                }
-            }
-        }
-    }
-    assert (eMessage === '', eMessage); // trigger error
 }
 
 function testFetchTickersAmounts (exchange: Exchange, skippedProperties: object, tickers: any) {
