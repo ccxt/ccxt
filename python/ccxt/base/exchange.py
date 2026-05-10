82ca535c67acdf975dc9880e92f8d637cccc6809
rayBastard <roman@president-kids.ru>
2026-05-10 16:07:52 +0300

    feat: Add set_rate_limit method to manage rate limits

--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -495,6 +495,11 @@ class Exchange(object):
         # stub in sync
         pass
 
+    def set_rate_limit(self, rateLimit: float):
+        self.rateLimit = rateLimit
+        if self.throttler is not None:
+            self.throttler.set_rate_limit(rateLimit)
+
     def throttle(self, cost=None):
         now = float(self.milliseconds())
         elapsed = now - self.lastRestRequestTimestamp
