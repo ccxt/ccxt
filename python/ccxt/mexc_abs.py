04e67711440b53c2f28158586cb6d45c8e6019d7
Ori Kalinski <orikalinski@gmail.com>
2026-05-12 12:49:06 +0300

    adding fetch rebate and fetch refer code to mexc_abs.py (#210)

--- python/ccxt/mexc_abs.py
+++ python/ccxt/mexc_abs.py
@@ -18,3 +18,18 @@ class mexc_abs(mexc):
     def __init__(self, config={}):
         super().__init__(config)
         self.options['broker'] = 'CORNIX'
+
+    def fetch_refer_code(self, params={}):
+        self.load_markets()
+        return self.spotPrivateGetRebateReferCode(params)
+
+    def fetch_rebate_tax_query(self, relevant_date=None, page=None, params={}):
+        self.load_markets()
+        request = {'pageSize': 100}
+        if page is not None:
+            request['page'] = page
+        response = self.brokerPrivateGetRebateTaxQuery(self.extend(request, params))
+        if relevant_date is not None:
+            date_str = relevant_date.strftime('%Y%m%d')
+            response['data'] = [r for r in response.get('data', []) if r.get('time') == date_str]
+        return response
