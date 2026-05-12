89f4ea6250bb853bd0254688bc986b2e450d3bd5
T.T <7117978+ttodua@users.noreply.github.com>
2026-05-12 12:53:24 +0400

    move to top

--- examples/ts/proxy-usage.ts
+++ examples/ts/proxy-usage.ts
@@ -1,7 +1,6 @@
+// AUTO-TRANSPILE //
 
 import ccxt from '../../js/ccxt.js';
-
-// AUTO-TRANSPILE //
 
 // 1) ABOUT CCXT PROXIES, READ MORE AT: https://docs.ccxt.com/README?id=proxy
 // 2) in python, uncomment the below:
