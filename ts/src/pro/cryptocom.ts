48ee09eca05d5a118733e5b416cf486d639d839d
T. Todua <7117978+ttodua@users.noreply.github.com>
2026-05-14 10:08:57 +0400

    Update cryptocom.ts

--- ts/src/pro/cryptocom.ts
+++ ts/src/pro/cryptocom.ts
@@ -83,7 +83,7 @@ export default class cryptocom extends cryptocomRest {
      * @param {object} [params] extra parameters specific to the exchange API endpoint
      * @param {string} [params.bookSubscriptionType] The subscription type. Allowed values: SNAPSHOT full snapshot. This is the default if not specified. SNAPSHOT_AND_UPDATE delta updates
      * @param {int} [params.bookUpdateFrequency] Book update interval in ms. Allowed values: 100 for snapshot subscription 10 for delta subscription
-     * @param {string} [params.adjustLimit] true/false, to automatically ceil the passed limit number to the nearest supported value
+     * @param {boolean} [params.adjustLimit] true/false, to automatically ceil the passed limit number to the nearest supported value
      * @returns {object} A dictionary of [order book structures]{@link https://docs.ccxt.com/?id=order-book-structure} indexed by market symbols
      */
     async watchOrderBook (symbol: string, limit: Int = undefined, params = {}): Promise<OrderBook> {
