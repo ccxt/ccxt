c8be80ad9c4fb2f5b896fab5d8c05c4305ed930d
T.T <7117978+ttodua@users.noreply.github.com>
2026-05-21 13:52:13 +0400

    fix(hitbtc): currencies parsing

--- ts/src/hitbtc.ts
+++ ts/src/hitbtc.ts
@@ -960,55 +960,67 @@ export default class hitbtc extends Exchange {
         //        },
         //    }
         //
-        const result: Dict = {};
-        const currencies = Object.keys (response);
-        for (let i = 0; i < currencies.length; i++) {
-            const currencyId = currencies[i];
-            const code = this.safeCurrencyCode (currencyId);
-            const entry = response[currencyId];
-            const rawNetworks = this.safeList (entry, 'networks', []);
-            const networks: Dict = {};
-            for (let j = 0; j < rawNetworks.length; j++) {
-                const rawNetwork = rawNetworks[j];
-                const networkId = this.safeString2 (rawNetwork, 'protocol', 'network');
-                let networkCode = this.networkIdToCode (networkId);
-                networkCode = (networkCode !== undefined) ? networkCode.toUpperCase () : code; // as hitbtc is white label, ensure we safeguard from possible bugs
-                networks[networkCode] = {
-                    'info': rawNetwork,
-                    'id': networkId,
-                    'network': networkCode,
-                    'active': undefined,
-                    'fee': this.safeNumber (rawNetwork, 'payout_fee'),
-                    'deposit': this.safeBool (rawNetwork, 'payin_enabled'),
-                    'withdraw': this.safeBool (rawNetwork, 'payout_enabled'),
-                    'precision': this.safeNumber (rawNetwork, 'precision_payout'),
-                    'limits': {
-                        'withdraw': {
-                            'min': undefined,
-                            'max': undefined,
-                        },
-                    },
-                };
-            }
-            result[code] = this.safeCurrencyStructure ({
-                'info': entry,
-                'code': code,
-                'id': currencyId,
-                'precision': this.safeNumber (entry, 'precision_transfer'),
-                'name': this.safeString (entry, 'full_name'),
-                'active': !this.safeBool (entry, 'delisted'),
-                'deposit': this.safeBool (entry, 'payin_enabled'),
-                'withdraw': this.safeBool (entry, 'payout_enabled'),
-                'networks': networks,
-                'fee': undefined,
+        const enhancedArray = this.addKeyInArrayItems (response, '_coin_id');
+        return this.parseCurrencies (enhancedArray);
+    }
+
+    parseCurrency (currency: Dict): Currency {
+        const currencyId = currency['_coin_id'];
+        const code = this.safeCurrencyCode (currencyId);
+        const entry = currency;
+        const rawNetworks = this.safeList (entry, 'networks', []);
+        const networks: Dict = {};
+        for (let j = 0; j < rawNetworks.length; j++) {
+            const rawNetwork = rawNetworks[j];
+            const networkId = this.safeString2 (rawNetwork, 'protocol', 'network');
+            let networkCode = this.networkIdToCode (networkId);
+            networkCode = (networkCode !== undefined) ? networkCode.toUpperCase () : code; // as hitbtc is white label, ensure we safeguard from possible bugs
+            networks[networkCode] = {
+                'info': rawNetwork,
+                'id': networkId,
+                'network': networkCode,
+                'active': undefined,
+                'fee': this.safeNumber (rawNetwork, 'payout_fee'),
+                'deposit': this.safeBool (rawNetwork, 'payin_enabled'),
+                'withdraw': this.safeBool (rawNetwork, 'payout_enabled'),
+                'precision': this.safeNumber (rawNetwork, 'precision_payout'),
                 'limits': {
-                    'amount': {
+                    'withdraw': {
                         'min': undefined,
                         'max': undefined,
                     },
                 },
-                'type': undefined, // 'crypto' field emits incorrect values
-            });
+            };
+        }
+        return this.safeCurrencyStructure ({
+            'info': entry,
+            'code': code,
+            'id': currencyId,
+            'precision': this.safeNumber (entry, 'precision_transfer'),
+            'name': this.safeString (entry, 'full_name'),
+            'active': undefined,
+            'deposit': undefined,
+            'withdraw': undefined,
+            'networks': networks,
+            'fee': undefined,
+            'limits': {
+                'amount': {
+                    'min': undefined,
+                    'max': undefined,
+                },
+            },
+            'type': undefined, // 'crypto' field emits incorrect values
+        });
+    }
+
+    addKeyInArrayItems (obj, keyName) {
+        const result = [];
+        const keys = Object.keys (obj);
+        for (let i = 0; i < keys.length; i++) {
+            const key = keys[i];
+            const item = obj[key];
+            item[keyName] = key;
+            result.push (item);
         }
         return result;
     }
