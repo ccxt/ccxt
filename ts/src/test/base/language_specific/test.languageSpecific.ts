7ad8dfc52201e60f0eba0c84976ead341a9b309f
rayBastard <roman@president-kids.ru>
2026-05-23 00:31:58 +0300

    fix: remove testSetRateLimit from language-specific tests
    Removed the call to testSetRateLimit from the test suite.

--- ts/src/test/base/language_specific/test.languageSpecific.ts
+++ ts/src/test/base/language_specific/test.languageSpecific.ts
@@ -12,7 +12,6 @@ import testSafeBalance from './test.safeBalance.js';
 import testLegacyHas from './test.legacyHas.js';
 import testTypes from './test.type.js';
 import testThrottlerPerformance from './test.throttlerPerformance.js';
-import testSetRateLimit from './test.setRateLimit.js';
 // todo: import testConfig from './test.config.js';
 // import './test.time.js' :todo
 // import './test.timeout_hang.js' :todo
@@ -26,7 +25,6 @@ async function testLanguageSpecific () {
     testLegacyHas ();
     testTypes ();
     await testThrottlerPerformance ();
-    await testSetRateLimit ();
     // testConfig ();
 }
 
