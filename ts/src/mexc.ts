c7718c2fe495a729b69dc28fc88414bef06223d5
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2026-05-26 12:36:23 +0100

    fix parsing

--- ts/src/mexc.ts
+++ ts/src/mexc.ts
@@ -2589,7 +2589,7 @@ export default class mexc extends Exchange {
         // {"success":true,"code":0,"data":{"orderId":"814218083416790528","ts":1779795118533}}
         //
         const data = this.safeDict (response, 'data');
-        return this.safeOrder ({ 'id': this.safeString (data, 'orderId') }, market);
+        return this.safeOrder ({ 'id': this.safeString (data, 'orderId'), 'timestamp': this.safeInteger (data, 'ts') }, market);
     }
 
     /**
