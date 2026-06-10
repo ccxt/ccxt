bd6e1d68078a4d1f194696b7630736c0f1147270
T.T <7117978+ttodua@users.noreply.github.com>
2026-06-10 09:17:08 +0400

    chore(btcbox): skip further

--- ts/src/btcbox.ts
+++ ts/src/btcbox.ts
@@ -498,7 +498,7 @@ export default class btcbox extends Exchange {
         //
         //      {
         //          "date":"0",
-        //          "price":3,
+        //          "price":4,
         //          "amount":0.1,
         //          "tid":"1",
         //          "type":"buy"
