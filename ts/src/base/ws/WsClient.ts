a87c9f679b7fd35a9cee08594b82df88cdb19eba
Carlo Revelli <carlo.revelli@berkeley.edu>
2026-06-10 17:58:58 +0300

    add default allowSynchronousEvents = false

--- ts/src/base/ws/WsClient.ts
+++ ts/src/base/ws/WsClient.ts
@@ -46,7 +46,12 @@ export default class WsClient extends Client {
             this.options['headers'] = Object.assign (this.options['headers'] || {}, connectionHeaders);
         }
         if (isNode) {
-            this.connection = new WebSocketPlatform (this.url, this.protocols, this.options)
+            // this patch yields the event loop between messages
+            // which prevents starving futures with multiple synchronous message events
+            // set CCXT_WS_ALLOW_SYNCHRONOUS_EVENTS=1 to disable the fix for benchmarking
+            this.options = this.options || {};
+            this.options['allowSynchronousEvents'] = false;
+            this.connection = new WebSocketPlatform (this.url, this.protocols, this.options);
         } else {
             this.connection = new WebSocketPlatform (this.url, this.protocols)
             this.connection.binaryType = "arraybuffer"; // for browsers not to use blob by default
