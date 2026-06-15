257540b3189fa5f464f689776692ee56fb077257
T.T <7117978+ttodua@users.noreply.github.com>
2026-06-15 16:44:19 +0400

    await fix

--- ts/src/test/base/test.fetchHistory.ts
+++ ts/src/test/base/test.fetchHistory.ts
@@ -15,22 +15,19 @@ async function testFetchHistory () {
     assert (trueClause);
     // try 3 times
     try {
-        const promise1 = exchange.fetch2 ('sample1'); // todo: full urls needed
-        await promise1;
+        await exchange.fetch2 ('sample1');
     } catch (error) {
         const fetchHistoryCache = exchange.getFetchCache ();
         assert (fetchHistoryCache.length === 1, 'fetchHistoryCache should be an array with 1 element');
     }
     try {
-        const promise2 = exchange.fetch2 ('sample2');
-        await promise2;
+        await exchange.fetch2 ('sample2');
     } catch (error) {
         const fetchHistoryCache = exchange.getFetchCache ();
         assert (fetchHistoryCache.length === 2, 'fetchHistoryCache should be an array with 2 elements');
     }
     try {
-        const promise3 = exchange.fetch2 ('sample3');
-        await promise3;
+        await exchange.fetch2 ('sample3');
     } catch (error) {
         const fetchHistoryCache = exchange.getFetchCache ();
         assert (fetchHistoryCache.length === 2, 'fetchHistoryCache should be an array with 2 elements');
