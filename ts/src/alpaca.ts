52d2adffa15d4ab1619b97eec52afaa3a9ca4077
Carlo Revelli <carlo.revelli@berkeley.edu>
2026-06-19 10:37:15 +0300

    add undefined to variable

--- ts/src/alpaca.ts
+++ ts/src/alpaca.ts
@@ -1204,7 +1204,7 @@ export default class alpaca extends Exchange {
         const request: Dict = {
             'status': 'all',
         };
-        let market: Market;
+        let market: Market = undefined;
         if (symbol !== undefined) {
             market = this.market (symbol);
             request['symbols'] = market['id'];
