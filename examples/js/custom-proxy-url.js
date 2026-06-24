5931c0396a9f3d844e40f2d259562b79004a62d8
T.T <7117978+ttodua@users.noreply.github.com>
2026-06-25 02:15:31 +0400

    docs: examples generated js files

---
+++ examples/js/custom-proxy-url.js
@@ -0,0 +1,16 @@
+// @NO_AUTO_TRANSPILE
+import ccxt from '../../js/ccxt.js';
+(async function main() {
+    const kraken1 = new ccxt.kraken({
+        proxy: function (url) {
+            return 'https://example.com/?url=' + encodeURIComponent(url);
+        },
+    });
+    console.log(await kraken1.loadMarkets());
+    const kraken2 = new ccxt.kraken({
+        proxy: function (url) {
+            return 'https://cors-anywhere.herokuapp.com/' + url;
+        },
+    });
+    console.log(await kraken2.loadMarkets());
+})();
