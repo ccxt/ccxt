04e8841a338d157ae0e5d3f9025ba3af1e8cc18e
T. Todua <7117978+ttodua@users.noreply.github.com>
2026-06-25 02:25:02 +0400

    docs: examples generated js files (#28990)

---
+++ examples/js/coinone-markets.js
@@ -0,0 +1,11 @@
+// @NO_AUTO_TRANSPILE
+import log from 'ololog';
+import ccxt from '../../js/ccxt.js';
+const exchange = new ccxt.coinone({
+    'verbose': process.argv.includes('--verbose'),
+});
+(async function main() {
+    const markets = await exchange.loadMarkets();
+    log(markets);
+    log('\n' + exchange['name'] + ' supports ' + Object.keys(markets).length + ' pairs');
+})();
