96ec45674c50b875afc8e0301553bbba5bf2a3d3
T.T <7117978+ttodua@users.noreply.github.com>
2026-06-25 00:09:14 +0400

    test urls

--- ts/src/test/base/test.fetchHistory.ts
+++ ts/src/test/base/test.fetchHistory.ts
@@ -5,8 +5,7 @@ import assert from 'assert';
 import ccxt from '../../../ccxt.js';
 import testSharedMethods from '../Exchange/base/test.sharedMethods.js';
 
-async function testFetchHistory () {
-
+async function testFetchHistoryBase () {
     const exchange = new ccxt.Exchange ({
         'id': 'sampleexchange',
         'fetchHistoryCacheSize': 2,
@@ -33,5 +32,35 @@ async function testFetchHistory () {
         assert (fetchHistoryCache.length === 2, 'fetchHistoryCache should be an array with 2 elements');
     }
 }
+
+
+async function testFetchHistoryDerived () {
+    const exchange = new ccxt.binance ({
+        'id': 'sampleexchange',
+        'fetchHistoryCacheSize': 2,
+    });
+    const trueClause = testSharedMethods.exchangeProp (exchange, 'fetchHistoryCacheSize') === 2;
+    assert (trueClause);
+    // try 3 times
+    // first
+    await exchange.fetchTime (); // https://api.binance.com/api/v3/time
+    assert ((exchange.getFetchCache ()).length === 1, 'fetchHistoryCache should be an array with 1 element');
+    // second
+    await exchange.fetchOHLCV ('BTC/USDT', '1d', 1780000000000, 5); // https://api.binance.com/api/v3/klines?interval=1m&limit=5&symbol=BTCUSDT
+    assert ((exchange.getFetchCache ()).length === 2, 'fetchHistoryCache should be an array with 2 elements');
+    // third
+    await exchange.fetchStatus (); // https://api.binance.com/sapi/v1/system/status
+    assert ((exchange.getFetchCache ()).length === 2, 'fetchHistoryCache should be an array with 2 elements');
+    const finalCache = exchange.getFetchCache ();
+    assert (finalCache[0]['request']['url'].toString () === 'https://api.binance.com/api/v3/klines?interval=1d&limit=5&symbol=BTCUSDT&startTime=1780000000000', 'The first element in fetchHistoryCache should be fetchOHLCV: ' + finalCache[0]['request']['url']);
+    assert (finalCache[1]['request']['url'].toString () === 'https://api.binance.com/sapi/v1/system/status', 'The second element in fetchHistoryCache should be fetchStatus: ' + finalCache[1]['request']['url']);
+}
+
+
+async function testFetchHistory () {
+    await testFetchHistoryBase ();
+    await testFetchHistoryDerived ();
+}
+
 
 export default testFetchHistory;
