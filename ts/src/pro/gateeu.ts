25379c0fb83196f98c09b2a2cd3d2c0101c868b6
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2026-06-26 15:58:10 +0100

    fix gate etended order

--- ts/src/pro/gateeu.ts
+++ ts/src/pro/gateeu.ts
@@ -12,7 +12,7 @@ export default class gateeu extends gate {
         const restInstance = new gateeuRest ();
         const restDescribe = restInstance.describe ();
         const parentWsDescribe = super.describeData ();
-        const extended = this.deepExtend (restDescribe, parentWsDescribe);
+        const extended = this.deepExtend (parentWsDescribe, restDescribe);
         return this.deepExtend (extended, {
             'id': 'gateeu',
             'name': 'Gate EU',
@@ -23,26 +23,6 @@ export default class gateeu extends gate {
                 'api': {
                     'ws': 'wss://ws.gateeu.com/v4',
                     'spot': 'wss://api.gateeu.com/ws/v4/',
-                    'public': {
-                        'wallet': 'https://api.gateeu.com/api/v4',
-                        'margin': 'https://api.gateeu.com/api/v4',
-                        'spot': 'https://api.gateeu.com/api/v4',
-                        'sub_accounts': 'https://api.gateeu.com/api/v4',
-                        'earn': 'https://api.gateeu.com/api/v4',
-                    },
-                    'private': {
-                        'withdrawals': 'https://api.gateeu.com/api/v4',
-                        'wallet': 'https://api.gateeu.com/api/v4',
-                        'margin': 'https://api.gateeu.com/api/v4',
-                        'spot': 'https://api.gateeu.com/api/v4',
-                        'subAccounts': 'https://api.gateeu.com/api/v4',
-                        'unified': 'https://api.gateeu.com/api/v4',
-                        'rebate': 'https://api.gateeu.com/api/v4',
-                        'earn': 'https://api.gateeu.com/api/v4',
-                        'account': 'https://api.gateeu.com/api/v4',
-                        'loan': 'https://api.gateeu.com/api/v4',
-                        'otc': 'https://api.gateeu.com/api/v4',
-                    },
                 },
             },
         });
