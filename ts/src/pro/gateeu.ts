e0f7acf4435bcfd6bb38b28cb7cd8b9caee68741
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2026-06-26 16:34:51 +0100

    feat(gateeu): add gate EU (#29018)
    * feat(gateeu): add gate EU
    
    * init fix ob
    
    * several fixes
    
    * fix extend
    
    * fix unwatch
    
    * fix gate etended order
    
    * fix describe data
    
    * fix describeData

---
+++ ts/src/pro/gateeu.ts
@@ -0,0 +1,30 @@
+
+//  ---------------------------------------------------------------------------
+
+import gate from './gate.js';
+import gateeuRest from '../gateeu.js';
+
+// ---------------------------------------------------------------------------
+
+export default class gateeu extends gate {
+    describe (): any {
+        // eslint-disable-next-line new-cap
+        const restInstance = new gateeuRest ();
+        const restDescribe = restInstance.describe ();
+        const parentWsDescribe = super.describeData ();
+        const extended = this.deepExtend (parentWsDescribe, restDescribe);
+        return this.deepExtend (extended, {
+            'id': 'gateeu',
+            'name': 'Gate EU',
+            'countries': [ 'EU' ],
+            'hostname': 'gateeu.com',
+            'certified': false,
+            'urls': {
+                'api': {
+                    'ws': 'wss://ws.gateeu.com/v4',
+                    'spot': 'wss://api.gateeu.com/ws/v4/',
+                },
+            },
+        });
+    }
+}
