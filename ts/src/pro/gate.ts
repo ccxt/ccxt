0a2e6dd11d97572e4eaabe645228661f5cd2af88
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2026-06-26 15:52:26 +0100

    fix unwatch

--- ts/src/pro/gate.ts
+++ ts/src/pro/gate.ts
@@ -457,21 +457,41 @@ export default class gate extends gateRest {
     async unWatchOrderBook (symbol: string, params = {}): Promise<any> {
         await this.loadMarkets ();
         const market = this.market (symbol);
+        const url = this.getUrlByMarket (market);
         symbol = market['symbol'];
         const marketId = market['id'];
-        let interval = '100ms';
+        const isEuUrl = url.indexOf ('gateeu') >= 0;
+        const intervalDefault = (market['spot'] && !isEuUrl) ? '50' : '100ms';
+        let interval = intervalDefault;
         [ interval, params ] = this.handleOptionAndParams (params, 'watchOrderBook', 'interval', interval);
         const messageType = this.getTypeByMarket (market);
-        const channel = messageType + '.order_book_update';
-        const subMessageHash = 'orderbook' + ':' + symbol;
-        const messageHash = 'unsubscribe:orderbook' + ':' + symbol;
-        const url = this.getUrlByMarket (market);
-        const payload = [ marketId, interval ];
-        const limit = this.safeInteger (params, 'limit', 100);
-        if (market['contract']) {
+        let limit = this.safeInteger (params, 'limit');
+        if (limit === undefined) {
+            limit = (market['spot']) ? 50 : 100; // max 100 atm
+            if (messageType === 'options') {
+                limit = 50; // max 50 for options
+            }
+        }
+        let payload: List = [];
+        let channel = '';
+        if (isEuUrl) {
+            channel = 'spot.order_book_update';
+            payload = [ marketId, interval ];
+        } else if (market['spot']) {
+            channel = 'spot.obu';
+            let finalInterval = interval;
+            if (limit === 400) {
+                finalInterval = '400';
+            }
+            payload = [ 'ob.' + market['id'] + '.' + finalInterval ];
+        } else {
+            channel = messageType + '.order_book_update';
+            payload = [ marketId, interval ];
             const stringLimit = limit.toString ();
             payload.push (stringLimit);
         }
+        const subMessageHash = 'orderbook' + ':' + symbol;
+        const messageHash = 'unsubscribe:orderbook' + ':' + symbol;
         return await this.unSubscribePublicMultiple (url, 'orderbook', [ symbol ], [ messageHash ], [ subMessageHash ], payload, channel, params);
     }
 
