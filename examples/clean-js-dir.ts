16266e2981a1618aee929090a1b9bd48506634ca
T.T <7117978+ttodua@users.noreply.github.com>
2026-06-26 23:36:16 +0400

    change commands

--- examples/clean-js-dir.ts
+++ examples/clean-js-dir.ts
@@ -11,26 +11,14 @@ if (!dirToClean.endsWith('examples' + path.sep + 'js')) {
     process.exit(1);
 }
 
-// 1. Clean only .js files in the directory
+
+const extensions = ['.js', '.d.ts', '.d.ts.map'];
 if (fs.existsSync(dirToClean)) {
-    console.log(`Cleaning .js files in ${dirToClean}...`);
-    
     const files = fs.readdirSync(dirToClean);
     
     for (const file of files) {
-        if ( file.endsWith ('.js') || file.endsWith ('.d.ts') || file.endsWith ('.d.ts.map') ) {
+        if (extensions.some(ext => file.endsWith(ext))) {
             fs.unlinkSync(path.join(dirToClean, file));
         }
     }
-    console.log('Cleanup complete.');
-}
-
-// 2. Run the TSC command
-try {
-    console.log('Compiling TypeScript...');
-    execSync('tsc -p ./examples/tsconfig.json', { stdio: 'inherit' });
-    console.log('Build completed successfully.');
-} catch (error) {
-    console.error('Build failed.');
-    process.exit(1);
 }
