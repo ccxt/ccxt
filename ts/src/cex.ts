2a89faed72e99d274ec5edbb42f89392c493b517
Peter Lai <alk03073135@gmail.com>
2026-06-29 21:45:14 +0800

    docs(cex): update @see (#29028)

--- ts/src/cex.ts
+++ ts/src/cex.ts
@@ -525,6 +525,7 @@ export default class cex extends Exchange {
      * @method
      * @name cex#fetchTime
      * @description fetches the current integer timestamp in milliseconds from the exchange server
+     * @see https://trade.cex.io/docs/#rest-public-api-calls-server-time
      * @param {object} [params] extra parameters specific to the exchange API endpoint
      * @returns {int} the current integer timestamp in milliseconds from the exchange server
      */
