e79245fcd2d57ac77b393d24e5d3b7437c437c0e
Igor Kroitor <igor.kroitor@gmail.com>
2026-07-08 22:15:53 +0300

    Apply suggestion from @kroitor

--- ts/src/bit2c.ts
+++ ts/src/bit2c.ts
@@ -454,7 +454,7 @@ export default class bit2c extends Exchange {
             await this.loadMarkets ();
         }
         const market = this.market (symbol);
-        const optionValue = this.safeString (this.options, 'fetchTradesMethod');
+        const optionValue = this.safeString (this.options, 'fetchTradesMethod'); // kept here for backward compatibility #29154
         const method = this.handleOption ('fetchTrades', 'method', optionValue) as string; // public_get_exchanges_pair_trades or public_get_exchanges_pair_lasttrades
         const request: Dict = {
             'pair': market['id'],
