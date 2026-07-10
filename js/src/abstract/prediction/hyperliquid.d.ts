f7754e68c0db55c6fc69359cb74329df200baf26
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2026-07-10 13:12:00 +0400

    rm 2

--- js/src/abstract/prediction/hyperliquid.d.ts
+++
@@ -1,9 +0,0 @@
-import { implicitReturnType } from '../../base/types.js';
-import { default as _Exchange } from '../../base/PredictionExchange.js';
-interface Exchange {
-    publicPostInfo(params?: {}): Promise<implicitReturnType>;
-    privatePostExchange(params?: {}): Promise<implicitReturnType>;
-}
-declare abstract class Exchange extends _Exchange {
-}
-export default Exchange;
