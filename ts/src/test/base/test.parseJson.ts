61b5b0154901c15602fc22fb74b1a2d2f871e12e
T.T <7117978+ttodua@users.noreply.github.com>
2026-07-18 19:37:52 +0400

    ex

--- ts/src/test/base/test.parseJson.ts
+++ ts/src/test/base/test.parseJson.ts
@@ -3,7 +3,7 @@
 // todo: per https://github.com/ttodua/ccxt/blob/17fc70fd7ccd8f6f5357e2dbd08aa30a1df0948b/ts/src/test/base/test.json.ts#L1
 
 import assert from 'assert';
-import ccxt, { BadRequest } from '../../../ccxt.js';
+import ccxt from '../../../ccxt.js';
 
 function testParseJson () {
 
@@ -22,11 +22,11 @@ function testParseJson () {
     const obj2 = '{"k":123456789012345678901234}';
     const obj2Parsed = exchange.parseJson (obj2);
     assert (obj2Parsed['k'] === '123456789012345678901234');
-    exchange.setProperty ('quoteJsonNumbers', false);
+    exchange.setProperty (exchange, 'quoteJsonNumbers', false);
     const obj2ReParsed = exchange.parseJson (obj2);
     assert (typeof obj2ReParsed['k'] !== 'string');
     assert (obj2ReParsed['k'] > 0);
-    exchange.setProperty ('quoteJsonNumbers', true);
+    exchange.setProperty (exchange, 'quoteJsonNumbers', true);
     assert (obj2Parsed['k'] === '123456789012345678901234');
     //
     const obj3 = '{"k":123456789012345678901234,"k2":"{\\"k3\\":123}"}';
