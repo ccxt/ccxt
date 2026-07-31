ba31d77f1d094cd31683000614f64b18b2d2526b
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2026-07-31 12:10:26 +0100

    chore: try to fix some tests (#29371)
    * chore: try to fix some tests
    
    * fix aster
    
    * soe more fixes
    
    * more fixes
    
    * more skips
    
    * fix static tests
    
    * kucoin missing

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -7790,6 +7790,11 @@ export class BaseExchange {
             return undefined;
         }
         const firstMarket = this.safeString (symbols, 0);
+        if (firstMarket === undefined) {
+            // an empty symbols list must behave like an undefined one,
+            // this.market (undefined) would throw an unreadable error
+            return undefined;
+        }
         const market = this.market (firstMarket);
         return market;
     }
