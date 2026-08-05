ef82b572d24a631b3047b902239172120e54d6ab
Igor Kroitor <igor.kroitor@gmail.com>
2026-08-05 06:26:04 +0300

    fix(bithumb): encode spaces as + in signed body for auth compatibility

--- ts/src/bithumb.ts
+++ ts/src/bithumb.ts
@@ -1255,6 +1255,7 @@ export default class bithumb extends Exchange {
             body = this.urlencode (this.extend ({
                 'endpoint': endpoint,
             }, query));
+            body = body.split ('%20').join ('+'); // bithumb verifies signatures with PHP http_build_query conventions, spaces must be '+'
             const nonce = this.nonce ().toString ();
             const auth = endpoint + "\0" + body + "\0" + nonce; // eslint-disable-line quotes
             const signature = this.hmac (this.encode (auth), this.encode (this.secret), sha512);
