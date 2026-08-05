d6592d7271642a7b3086ff45705a4f25d4c99452
Igor Kroitor <igor.kroitor@gmail.com>
2026-08-05 08:12:00 +0300

    feat(gate): add paginate support to fetchClosedOrders (#29564)
    Fixes #22825

--- ts/src/gate.ts
+++ ts/src/gate.ts
@@ -5422,6 +5422,12 @@ export default class gate extends Exchange {
             await this.loadMarkets ();
         }
         await this.loadUnifiedStatus ();
+        let paginate = false;
+        [ paginate, params ] = this.handleOptionAndParams (params, 'fetchClosedOrders', 'paginate');
+        if (paginate) {
+            // see https://github.com/ccxt/ccxt/issues/22825
+            return await this.fetchPaginatedCallDynamic ('fetchClosedOrders', symbol, since, limit, params) as Order[];
+        }
         const until = this.safeInteger (params, 'until');
         let market: Market = undefined;
         if (symbol !== undefined) {
