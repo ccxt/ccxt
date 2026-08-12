2868328b2a6f90401e7795912f1c17edb777f73f
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2026-08-12 18:56:14 +0400

    fix c#

--- ts/src/krakenfutures.ts
+++ ts/src/krakenfutures.ts
@@ -124,7 +124,7 @@ export default class krakenfutures extends Exchange {
                 'private': {
                     'get': {
                         'feeschedules/volumes': { 'cost': 1 } as Endpoint<Dict>,
-                        'openpositions': { 'cost': 1 } as Endpoint<List>,
+                        'openpositions': { 'cost': 1 } as Endpoint<Dict>,
                         'notifications': { 'cost': 1 } as Endpoint<Dict>,
                         'accounts': { 'cost': 1 } as Endpoint<Dict>,
                         'openorders': { 'cost': 1 } as Endpoint<Dict>,
@@ -3028,8 +3028,8 @@ export default class krakenfutures extends Exchange {
         // a degraded response missing openPositions must fail loudly - a flat
         // account and "could not read positions" are not interchangeable for
         // reconciliation logic, see https://github.com/ccxt/ccxt/issues/29710
-        // (the crash guarded against in #19896 is still avoided, since we no
-        // longer call .length on a non-list value)
+        // the crash guarded against in #19896 is still avoided, since we no
+        // longer call .length on a non-list value
         const positions = this.safeList (response, 'openPositions');
         if (positions === undefined) {
             throw new ExchangeError (this.id + ' fetchPositions() returned a response without an "openPositions" list');
