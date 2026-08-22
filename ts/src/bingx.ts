a0f6817c14237d77f751e962a01ec78ae4294641
Ares <81414185+AresArtemius@users.noreply.github.com>
2026-08-22 20:27:02 +0300

    fix(bingx): parse Coin-M funding rate response (#30023)
    * fix(bingx): parse Coin-M funding rate response
    
    * fix(bingx): default funding rate data

--- ts/src/bingx.ts
+++ ts/src/bingx.ts
@@ -1721,7 +1721,13 @@ export default class bingx extends Exchange {
         //        ]
         //    }
         //
-        const data = this.safeDict (response, 'data');
+        let data: Dict;
+        if (market['inverse']) {
+            const dataList = this.safeList (response, 'data', []);
+            data = this.safeDict (dataList, 0, {});
+        } else {
+            data = this.safeDict (response, 'data', {});
+        }
         return this.parseFundingRate (data, market);
     }
 
