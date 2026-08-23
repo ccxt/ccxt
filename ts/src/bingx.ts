e008ea4f1711e74165bba6553ad7e7f84e7e0fb1
Artem Kukhar <artemius@mail.ru>
2026-08-23 13:42:05 +0300

    docs(bingx): clarify cancelOrders support

--- ts/src/bingx.ts
+++ ts/src/bingx.ts
@@ -1687,7 +1687,7 @@ export default class bingx extends Exchange {
      * @description fetch the current funding rate
      * @see https://bingx-api.github.io/docs-v3/#/en/Swap/Market%20Data/Mark%20Price%20and%20Funding%20Rate
      * @see https://bingx-api.github.io/docs-v3/#/en/Coin-M%20Futures/Market%20Data/Price%20%26%20Current%20Funding%20Rate
-     * @param {string} symbol unified market symbol
+     * @param {string} symbol unified market symbol, inverse (Coin-M) markets are not supported
      * @param {object} [params] extra parameters specific to the exchange API endpoint
      * @returns {object} a [funding rate structure]{@link https://docs.ccxt.com/?id=funding-rate-structure}
      */
