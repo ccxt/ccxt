01d0aa85aa6b685b8bd33a8a0698b2c61e99ce4f
ndubel <38309641+ndubel@users.noreply.github.com>
2026-08-25 16:09:04 +0300

    bitget parseUtaBalance fix (#30083)
    * bitget parseUtaBalance fix
    
    * bitget parseUtaBalance fix for funding accounts

--- ts/src/bitget.ts
+++ ts/src/bitget.ts
@@ -4688,7 +4688,8 @@ export default class bitget extends Exchange {
         //         "balance": "6.19300826",
         //         "available": "6.19300826",
         //         "debt": "0",
-        //         "locked": "0"
+        //         "locked": "0",
+        //         "bonus": "10"
         //     }
         //
         // funding uta
@@ -4708,7 +4709,7 @@ export default class bitget extends Exchange {
             account['debt'] = this.safeString (entry, 'debt');
             account['used'] = this.safeString2 (entry, 'locked', 'frozen');
             account['free'] = this.safeString (entry, 'available');
-            account['total'] = this.safeString (entry, 'balance');
+            account['total'] = this.safeString2 (entry, 'equity', 'balance');
             if (code !== undefined) {
                 result[code] = account;
             }
