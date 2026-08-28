46b06ecd483cf2d490d33715cae5874714dc8a9e
T.T <7117978+ttodua@users.noreply.github.com>
2026-08-28 23:08:18 +0400

    perf(php): get_object_value_from_key_array optimization

--- php/Exchange.php
+++ php/Exchange.php
@@ -691,11 +691,11 @@ class BaseExchange {
 
     public static function get_object_value_from_key_array($object, $array) {
         foreach ($array as $key) {
-            if ($key === null) {
-                continue;
-            }
-            if (isset($object[$key]) && $object[$key] !== '') {
-                return $object[$key];
+            if ($key !== null) {
+                $value = $object[$key] ?? null;
+                if ($value !== '') {
+                    return $value;
+                }
             }
         }
         return null;
