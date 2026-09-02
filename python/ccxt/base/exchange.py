b6af477cc0d10fa10bc35c2c7d20b9d72a72946f
T.T <7117978+ttodua@users.noreply.github.com>
2026-09-03 00:44:51 +0400

    perf(python): safe_float optimization

--- python/ccxt/base/exchange.py
+++ python/ccxt/base/exchange.py
@@ -725,11 +725,21 @@ class BaseExchange(object):
             return False
 
     @staticmethod
-    def safe_float(dictionary, key, default_value=None):
-        try:
-            return float(dictionary[key])
-        except Exception:
-            return default_value
+    def safe_float(container, key, default_value=None):
+        if container is not None:
+            if isinstance(container, dict):
+                value = container.get(key)
+            else:
+                try:
+                    value = container[key]
+                except (KeyError, IndexError, TypeError):
+                    value = None
+            if value is not None:
+                try:
+                    return float(value)
+                except (TypeError, ValueError, OverflowError):
+                    pass
+        return default_value
 
     @staticmethod
     def safe_string(dictionary, key, default_value=None):
@@ -808,14 +818,33 @@ class BaseExchange(object):
     # we're not using safe_float_3 either because those cases are too rare to deserve their own optimization
 
     @staticmethod
-    def safe_float_2(dictionary, key1, key2, default_value=None):
-        try:
-            return float(dictionary[key1])
-        except Exception:
-            try:
-                return float(dictionary[key2])
-            except Exception:
-                return default_value
+    def safe_float_2(container, key1, key2, default_value=None):
+        if container is not None:
+            if isinstance(container, dict):
+                value = container.get(key1)
+            else:
+                try:
+                    value = container[key1]
+                except (KeyError, IndexError, TypeError):
+                    value = None
+            if value is not None:
+                try:
+                    return float(value)
+                except (TypeError, ValueError, OverflowError):
+                    pass
+            if isinstance(container, dict):
+                value = container.get(key2)
+            else:
+                try:
+                    value = container[key2]
+                except (KeyError, IndexError, TypeError):
+                    value = None
+            if value is not None:
+                try:
+                    return float(value)
+                except (TypeError, ValueError, OverflowError):
+                    pass
+        return default_value
 
     @staticmethod
     def safe_string_2(dictionary, key1, key2, default_value=None):
@@ -932,14 +961,15 @@ class BaseExchange(object):
     # safe_method_n methods family
 
     @staticmethod
-    def safe_float_n(dictionary, key_list, default_value=None):
-        value = Exchange.get_object_value_from_key_list(dictionary, key_list)
-        if value is None:
-            return default_value
-        try:
-            return float(value)
-        except ValueError as e:
-            return default_value
+    def safe_float_n(container, key_list, default_value=None):
+        if container is not None:
+            value = Exchange.get_object_value_from_key_list(container, key_list)
+            if value is not None:
+                try:
+                    return float(value)
+                except (TypeError, ValueError, OverflowError):
+                    pass
+        return default_value
 
     @staticmethod
     def safe_string_n(dictionary, key_list, default_value=None):
