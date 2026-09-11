29ad8c6cd1895267eeb3ed4d41d3ebb2886abd4a
yzh-pelle <81404729+yzh-pelle@users.noreply.github.com>
2026-09-11 17:57:43 +0300

    fix(limitless): extend method instead of setting property

--- ts/src/prediction/limitless.ts
+++ ts/src/prediction/limitless.ts
@@ -3158,13 +3158,15 @@ export default class limitless extends Exchange {
             const timestamp = this.iso8601 (this.milliseconds ());
             const newline = "\n"; // eslint-disable-line quotes
             const payload = timestamp + newline + method + newline + url + newline + bodyString;
-            const signature = this.hmac (this.encode (payload), this.base64ToBinary (this.secret), sha256, 'base64');
+            const signature = this.hmac (this.encode (payload), this.base64ToBinary (this.secret), sha256, 'base64');   
             headers = this.extend (headers, {
                 'lmts-timestamp': timestamp,
                 'lmts-signature': signature,
             });
             const headerKey = 'lmts-api' + '-key'; // concatenating because of the php version
-            headers[headerKey] = this.apiKey;
+            const headersKey: Dict = {};
+            headersKey[headerKey] = this.apiKey;
+            headers = this.extend (headers, headersKey);
         }
         url = baseUrl + url;
         return { 'url': url, 'method': method, 'body': body, 'headers': headers };
