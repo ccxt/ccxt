55d7596d8d3e84112442184c3f8f25d668580398
AresArtemius <artemius@mail.ru>
2026-09-11 19:30:19 +0300

    docs(bingx): place Coin-M editOrder restriction correctly

--- ts/src/bingx.ts
+++ ts/src/bingx.ts
@@ -3050,7 +3050,7 @@ export default class bingx extends Exchange {
      * @method
      * @name bingx#createMarketOrderWithCost
      * @description create a market order by providing the symbol, side and cost
-     * @param {string} symbol unified symbol of the market to create an order in, inverse (Coin-M) markets are not supported
+     * @param {string} symbol unified symbol of the market to create an order in
      * @param {string} side 'buy' or 'sell'
      * @param {float} cost how much you want to trade in units of the quote currency
      * @param {object} [params] extra parameters specific to the exchange API endpoint
@@ -6641,7 +6641,7 @@ export default class bingx extends Exchange {
      * @see https://bingx-api.github.io/docs-v3/#/en/Spot/Trades%20Endpoints/Cancel%20an%20Existing%20Order%20and%20Send%20a%20New%20Order  // spot
      * @see https://bingx-api.github.io/docs-v3/#/en/Swap/Trades%20Endpoints/Cancel%20an%20Existing%20Order%20and%20Send%20a%20New%20Orde  // swap
      * @param {string} id order id
-     * @param {string} symbol unified symbol of the market to create an order in
+     * @param {string} symbol unified symbol of the market to create an order in, inverse (Coin-M) markets are not supported
      * @param {string} type 'market' or 'limit'
      * @param {string} side 'buy' or 'sell'
      * @param {float} amount how much of the currency you want to trade in units of the base currency
