4ae8c9a11ecb0722a064d220b8c04213df20846d
T.T <7117978+ttodua@users.noreply.github.com>
2026-09-12 18:55:33 +0400

    fix revolutx

--- ts/src/revolutx.ts
+++ ts/src/revolutx.ts
@@ -813,10 +813,10 @@ export default class revolutx extends Exchange {
         //     }
         //
         const data = this.safeList (response, 'data', []);
-        const result: Trade[] = [];
+        const result: any[] = [];
         for (let i = 0; i < data.length; i++) {
             const trade = this.safeDict (data, i, {});
-            result.push (this.parseTrade (trade, market));
+            result.push (trade);
         }
         return this.parseTradesDirectional (result, market, since, limit);
     }
