9852ffd0eb64863860bca29e0d74e781617a3854
carlosmiei <43336371+carlosmiei@users.noreply.github.com>
2026-09-13 16:15:03 +0100

    fix(bybit): default to header id

--- ts/src/bybit.ts
+++ ts/src/bybit.ts
@@ -10111,11 +10111,9 @@ export default class bybit extends Exchange {
             }
         }
         if (method === 'POST') {
-            const brokerId = this.safeString (this.options, 'brokerId');
-            if (brokerId !== undefined) {
-                headers = (headers === undefined) ? {} : headers;
-                headers['Referer'] = brokerId;
-            }
+            const brokerId = this.safeString (this.options, 'brokerId', 'CCXT');
+            headers = (headers === undefined) ? {} : headers;
+            headers['Referer'] = brokerId;
         }
         return { 'url': url, 'method': method, 'body': body, 'headers': headers };
     }
