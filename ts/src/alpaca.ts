e0bffba728e07733631e964d74b39e43bbcdda1f
rayBastard <romancuhari@gmail.com>
2026-09-16 15:16:25 +0300

    feat(alpaca): include crypto positions in fetchBalance
    Merges v2/account with v2/positions so held coins appear as balances.
    USD free is the venue cash (already net of open-order holds), USD total
    is equity minus the positions market value, so used reports the holds.

--- ts/src/alpaca.ts
+++ ts/src/alpaca.ts
@@ -2097,6 +2097,7 @@ export default class alpaca extends Exchange {
      * @name alpaca#fetchBalance
      * @description query for balance and get the amount of funds available for trading or funds locked in orders
      * @see https://docs.alpaca.markets/reference/getaccount-1
+     * @see https://docs.alpaca.markets/reference/getallopenpositions
      * @param {object} [params] extra parameters specific to the exchange API endpoint
      * @returns {object} a [balance structure]{@link https://docs.ccxt.com/?id=balance-structure}
      */
@@ -2104,7 +2105,11 @@ export default class alpaca extends Exchange {
         if (this.markets === undefined) {
             await this.loadMarkets ();
         }
-        const response = await this.traderPrivateGetV2Account (params);
+        const promises = [
+            this.traderPrivateGetV2Account (params),
+            this.traderPrivateGetV2Positions (),
+        ];
+        const [ account, positions ] = await Promise.all (promises);
         //
         //     {
         //         "id": "43a01bde-4eb1-64fssc26adb5",
@@ -2153,18 +2158,76 @@ export default class alpaca extends Exchange {
         //         "pending_reg_taf_fees": "0"
         //     }
         //
+        const response: Dict = {
+            'account': account,
+            'positions': positions,
+        };
         return this.parseBalance (response);
     }
 
     override parseBalance (response: any): Balances {
+        //
+        // crypto holdings live on the positions endpoint, the account endpoint carries only the cash currency
+        //
+        //     "positions": [
+        //         {
+        //             "asset_id": "64bbff51-59d6-4b3c-9351-13ad85e3c752",
+        //             "symbol": "BTCUSD",
+        //             "exchange": "CRYPTO",
+        //             "asset_class": "crypto",
+        //             "asset_marginable": false,
+        //             "qty": "0.000207296",
+        //             "avg_entry_price": "80037",
+        //             "side": "long",
+        //             "market_value": "16.592345",
+        //             "cost_basis": "16.59135",
+        //             "unrealized_pl": "0.000995",
+        //             "unrealized_plpc": "0.00006",
+        //             "current_price": "80041.8",
+        //             "qty_available": "0.000207296"
+        //         }
+        //     ]
+        //
+        const account = this.safeDict (response, 'account', {});
+        const positions = this.safeList (response, 'positions', []);
         const result: Dict = { 'info': response };
-        const account = this.account ();
-        const currencyId = this.safeString (response, 'currency');
+        const currencyId = this.safeString (account, 'currency');
         const code = this.safeCurrencyCode (currencyId);
-        account['free'] = this.safeString (response, 'cash');
-        account['total'] = this.safeString (response, 'equity');
         if (code !== undefined) {
-            result[code] = account;
+            const cashAccount = this.account ();
+            cashAccount['free'] = this.safeString (account, 'cash'); // cash already excludes the amounts held for open orders, verified live 2026-09-16
+            const equity = this.safeString (account, 'equity');
+            const positionsValue = this.safeString (account, 'position_market_value');
+            cashAccount['total'] = Precise.stringSub (equity, positionsValue); // equity minus the positions market value equals cash plus open-order holds
+            result[code] = cashAccount;
+        }
+        for (let i = 0; i < positions.length; i++) {
+            const position = positions[i];
+            const positionSymbol = this.safeString (position, 'symbol');
+            if (positionSymbol === undefined) {
+                continue;
+            }
+            let baseId: Str = undefined;
+            if (positionSymbol.indexOf ('/') >= 0) {
+                const parts = positionSymbol.split ('/');
+                baseId = this.safeString (parts, 0);
+            } else {
+                // crypto position symbols come compressed with a USD tail, e.g. BTCUSD or USDTUSD
+                const baseLength = positionSymbol.length - 3;
+                if ((baseLength > 0) && (positionSymbol.slice (baseLength) === 'USD')) {
+                    baseId = positionSymbol.slice (0, baseLength);
+                }
+            }
+            if (baseId === undefined) {
+                continue; // an unrecognized position symbol shape must not break the whole balance
+            }
+            const positionCode = this.safeCurrencyCode (baseId);
+            if ((positionCode !== undefined) && !(positionCode in result)) {
+                const positionAccount = this.account ();
+                positionAccount['free'] = this.safeString (position, 'qty_available');
+                positionAccount['total'] = this.safeString (position, 'qty');
+                result[positionCode] = positionAccount;
+            }
         }
         return this.safeBalance (result);
     }
