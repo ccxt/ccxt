2b23b25f7e0e3ca4e73bf314cb9da34b7d5ced7a
T.T <7117978+ttodua@users.noreply.github.com>
2026-09-17 16:35:25 +0400

    fix(lighter): go bfee fix

--- go/v4/exchange_eth.go
+++ go/v4/exchange_eth.go
@@ -859,6 +859,16 @@ func (this *BaseExchange) lighterCreateClient(signer any, chainId uint32, privat
 	return txClient
 }
 
+// safeIntOr mirrors safeInteger(request, key, default) used by the other
+// language bindings: a key that is absent (builderFee disabled) yields the
+// default instead of panicking in SafeInt.
+func safeIntOr(v any, defaultValue int64) int64 {
+	if v == nil {
+		return defaultValue
+	}
+	return SafeInt(v)
+}
+
 func (this *BaseExchange) lighterL2TxAttr(integratorAccountIndex int64, integratorTakerFee uint32, integratorMakerFee uint32, skipNonce uint8) types.L2TxAttributes {
 	l2TxAttr := types.L2TxAttributes{}
 	if integratorAccountIndex > 0 && integratorTakerFee > 0 && integratorMakerFee > 0 {
@@ -921,7 +931,7 @@ func (this *BaseExchange) lighterSignCreateGroupedOrders(signer *client.TxClient
 	}
 
 	nonce := int64(SafeInt(request["nonce"]))
-	l2TxAttr := this.lighterL2TxAttr(int64(SafeInt(request["integrator_account_index"])), uint32(SafeInt(request["integrator_taker_fee"])), uint32(SafeInt(request["integrator_maker_fee"])), uint8(1))
+	l2TxAttr := this.lighterL2TxAttr(safeIntOr(request["integrator_account_index"], 0), uint32(safeIntOr(request["integrator_taker_fee"], 0)), uint32(safeIntOr(request["integrator_maker_fee"], 0)), uint8(1))
 	ops := &types.TransactOpts{
 		Nonce:        &nonce,
 		TxAttributes: &l2TxAttr,
@@ -967,7 +977,7 @@ func (this *BaseExchange) lighterSignCreateOrder(signer *client.TxClient, reques
 		OrderExpiry:      orderExpiry,
 	}
 	nonce := int64(SafeInt(request["nonce"]))
-	l2TxAttr := this.lighterL2TxAttr(int64(SafeInt(request["integrator_account_index"])), uint32(SafeInt(request["integrator_taker_fee"])), uint32(SafeInt(request["integrator_maker_fee"])), uint8(1))
+	l2TxAttr := this.lighterL2TxAttr(safeIntOr(request["integrator_account_index"], 0), uint32(safeIntOr(request["integrator_taker_fee"], 0)), uint32(safeIntOr(request["integrator_maker_fee"], 0)), uint8(1))
 	ops := &types.TransactOpts{
 		Nonce:        &nonce,
 		TxAttributes: &l2TxAttr,
