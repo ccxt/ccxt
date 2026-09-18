e725cf376e9c5583a3225da03bb0f6e5ea9e07b5
T.T <7117978+ttodua@users.noreply.github.com>
2026-09-18 22:41:29 +0400

    comment

--- ts/src/test/Exchange/base/test.sharedMethods.ts
+++ ts/src/test/Exchange/base/test.sharedMethods.ts
@@ -724,6 +724,7 @@ function assertAmountPriceCost (exchange: Exchange, skippedProperties: any, meth
         isValid = true;
     } else {
         // else we need to know the amountPrecision, so we would pass the test if the remainder is less than amountPrecision
+        // todo: amountPrecision = (contractSize !== undefined) ? Precise.stringMul (amountPrecision, contractSize) : amountPrecision;
         assert (amountPrecision !== undefined, 'amount precision is not defined, you might add "amountPriceCost" in skips' + logText);
         // rounding loss more than then half of the market.precision.amount is not tolerable
         const amountPrecisionHalf = Precise.stringDiv (amountPrecision, '2');
