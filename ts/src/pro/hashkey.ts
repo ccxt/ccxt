7594a9b35d37515413778a8d4b08afabd9a08fdd
carlotestor <89560945+carlotestor@users.noreply.github.com>
2026-09-19 00:54:44 +0000

    hashkey indodax kraken: type locals for native emission (safeValue split)

--- ts/src/pro/hashkey.ts
+++ ts/src/pro/hashkey.ts
@@ -772,7 +772,7 @@ export default class hashkey extends hashkeyRest {
 
     async loadBalanceSnapshot (client: Client, messageHash: any, type: any) {
         const response = await this.fetchBalance ({ 'type': type });
-        this.balance[type] = this.extend (response, this.safeValue (this.balance, type, {}));
+        this.balance[type] = this.extend (response, this.safeDict (this.balance, type, {}));
         // don't remove the future from the .futures cache
         if (messageHash in client.futures) {
             const future = client.futures[messageHash];
