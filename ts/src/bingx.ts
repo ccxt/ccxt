3db307874151544b583c07ba1a4332d43b386e1c
Artem Kukhar <artemius@mail.ru>
2026-09-23 13:35:20 +0300

    fix(bingx): mark trading fee rates as percentage-based

--- ts/src/bingx.ts
+++ ts/src/bingx.ts
@@ -7012,7 +7012,7 @@ export default class bingx extends Exchange {
             'symbol': symbol,
             'maker': this.safeNumber (fee, 'makerCommissionRate'),
             'taker': this.safeNumber (fee, 'takerCommissionRate'),
-            'percentage': false,
+            'percentage': true,
             'tierBased': false,
         };
     }
