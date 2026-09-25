4ac9f7de6802b6fb7badbb8050936829201c7107
T.T <7117978+ttodua@users.noreply.github.com>
2026-09-25 15:56:14 +0400

    rev

--- ts/src/lighter.ts
+++ ts/src/lighter.ts
@@ -953,10 +953,10 @@ export default class lighter extends Exchange {
         if (this.markets === undefined) {
             await this.loadMarkets ();
         }
+        // non-destructively get values from opts/params
         const accIndexAndParams = await this.handleAccountIndex (params, method, 'accountIndex', 'account_index');
         let accountIndex: Int = accIndexAndParams[0];
-        params['accountIndex'] = accountIndex;
-        const apiKeyIndexAndParams = await this.handleAccountIndex (params, method, 'accountIndex', 'account_index');
+        const apiKeyIndexAndParams = await this.handleApiKeyIndex (params, method, 'apiKeyIndex', 'api_key_index');
         let apiKeyIndex: Int = apiKeyIndexAndParams[0];
         // before order-req creation, we need to know account status
         const strAccountIndex = this.numberToString (accountIndex) as string;
